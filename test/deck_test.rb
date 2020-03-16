76921160a38144dcba7588f7d3ea7b8e83fb4831
Taylor Keller <takeller17@gmail.com>
2020-03-16 16:51:01 -0600

    Add rank_of_card_at test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,4 +1,4 @@
-equire 'minitest/autorun'
+require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/deck'
 require './lib/card'
@@ -10,6 +10,15 @@ class DeckTest < Minitest::Test
     cards << Card.new(:spade, 'Jack', 11)
     cards << Card.new(:club, '4', 4)
     deck = Deck.new(cards)
+  end
+
+  def test_rank_of_card_at_method
+    cards = []
+    cards << Card.new(:diamond, 'Queen', 12)
+    cards << Card.new(:spade, 'Jack', 11)
+    cards << Card.new(:club, '4', 4)
+    deck = Deck.new(cards)
+    assert_equal 12, deck.rank_of_card_at(0)
   end
 
 
