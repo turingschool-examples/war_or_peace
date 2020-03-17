749f4767d7e205b1af59a6793e8c7116b3f514ea
michelle <foleymichelle9@gmail.com>
2020-03-17 17:18:22 -0600

    make has_lost test pass

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,13 @@
 class Player
 
-  attr_reader :name, :deck 
+  attr_reader :name, :deck
 
   def initialize(name, deck)
     @name = name
     @deck = deck
+  end
+
+  def has_lost?
+    false 
   end
 end
