fad09ed1e83c5f2acfa15240ad85374dd1b55293
michelle <foleymichelle9@gmail.com>
2020-03-17 17:16:41 -0600

    add has_lost test

--- test/player_test.rb
+++ test/player_test.rb
@@ -32,4 +32,13 @@ class PlayerTest < Minitest::Test
     player = Player.new('Clarisa', deck)
     assert_equal deck, player.deck
   end
+
+  def test_it_can_return_has_lost_status
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+    assert_equal false, player.has_lost?
+  end
 end
