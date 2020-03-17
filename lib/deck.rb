ef8c8ff142d294ba1fc8b96fabd611af7d2fc07b
Alex Pariseau <arpariseau@gmail.com>
2020-03-17 12:11:30 -0600

    Adjusting high card percentage for deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,4 +1,4 @@
-require './card'
+require './lib/card'
 
 class Deck
   attr_reader :cards
@@ -15,7 +15,7 @@ class Deck
     face_cards = []
     c = 0
     while c < @cards.length
-      if self.rank_of_card_at(c) >= 11
+      if rank_of_card_at(c) >= 11
         face_cards << @cards[c]
       end
       c += 1
@@ -25,11 +25,11 @@ class Deck
 
   def percent_high_ranking
     num_face_cards = self.high_ranking_cards.length.to_f
-    num_face_cards / @cards.length.to_f
+    ((num_face_cards / @cards.length.to_f) * 100).round(2)
   end
 
   def remove_card
-    @cards.pop
+    @cards.shift
   end
 
   def add_card(new_card)
