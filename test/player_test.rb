d9ee2e4f56c082960f770ae4754852d3240f77e7
michelle <foleymichelle9@gmail.com>
2020-03-17 16:50:33 -0600

    add test_it_exists test

--- test/player_test.rb
+++ test/player_test.rb
@@ -4,4 +4,14 @@ require './lib/card'
 require './lib/deck'
 
 class PlayerTest < Minitest::Test
-end 
+
+  def test_it_exists
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+    assert_instance_of Player, player 
+  end
+
+end
