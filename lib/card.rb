881cd8b53d23de2664b0decadfed48fe34bd8d3b
Jack Gould <johnhilsmangould@gmail.com>
2020-03-17 17:39:32 -0600

    adds deck_test.rb

---
+++ lib/card.rb
@@ -0,0 +1,17 @@
+# card class
+
+class Card
+  attr_accessor :suit, :value, :rank
+
+  def initialize(suit, value, rank)
+    @suit = suit
+    @value = value
+    @rank = rank
+  end
+
+end
+
+card1 = Card.new(:diamond, 'Queen', 12)
+card2 = Card.new(:spade, '3', 3)
+card3 = Card.new(:heart, 'Ace', 14)
+cards = [card1, card2, card3]
