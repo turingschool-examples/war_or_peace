1632047dd41fedf12801423b5ceecac1ed7515b5
Derek Borski <derek.borski@gmail.com>
2020-03-17 21:38:35 -0600

    Adds deck_test.rb

---
+++ test/deck_test.rb
@@ -0,0 +1,69 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/deck'
+require './lib/card'
+
+class DeckTest < Minitest::Test
+  def test_rank_of_cards
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal 12, deck.rank_of_card_at(0)
+    assert_equal 14, deck.rank_of_card_at(2)
+  end
+
+  def test_high_ranking_cards
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal [card1, card3], deck.high_ranking_cards
+  end
+
+  def test_percent_high_ranking
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal 66.67 , deck.percent_high_ranking
+  end
+
+  def test_add_and_remove_card_from_deck
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    card4 = Card.new(:club, '5', 5)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    deck.remove_card
+    assert_equal [card2, card3], deck.cards
+    assert_equal 50 , deck.percent_high_ranking
+
+    deck.add_card(card4)
+    assert_equal [card2, card3, card4], deck.cards
+    assert_equal [card3], deck.high_ranking_cards
+    assert_equal 33.33, deck.percent_high_ranking
+  end
+
+  # def test_add_card_to_deck
+  #   card1 = Card.new(:diamond, 'Queen', 12)
+  #   card2 = Card.new(:spade, '3', 3)
+  #   card3 = Card.new(:heart, 'Ace', 14)
+  #   card4 = Card.new(:club, '5', 5)
+  #   cards = [card1, card2, card3]
+  #   deck = Deck.new(cards)
+  #
+  #   deck.add_card(card4)
+  #   assert_equal [card1, card2, card3, card4], deck.cards
+  #   assert_equal [card1, card3], deck.high_ranking_cards
+  #   assert_equal 33.33, deck.percent_high_ranking
+  # end
+end
