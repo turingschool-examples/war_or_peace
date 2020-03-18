c66e889302a4c8e77050835e748d5d16c5ec331d
Jack Gould <johnhilsmangould@gmail.com>
2020-03-18 08:38:47 -0600

    Add high_ranking_card_method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,5 +1,5 @@
 # deck class
-
+require './lib/card.rb'
 
 class Deck
   attr_reader :cards
@@ -8,12 +8,20 @@ class Deck
     @cards = cards
   end
 
-  def rank_of_cards_at(int)
-     int
+  def rank_of_card_at(int)
+    cards[int].rank
   end
+
 
   def high_ranking_cards
-
+    high_ranking_array = []
+    cards.each do |card_rank|
+      if card_rank.rank >= 11 == true
+        high_ranking_array << card_rank
+      end
+    end
+    return high_ranking_array
   end
 
- end
+
+end
