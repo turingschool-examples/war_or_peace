5fd052162055a4c92a88d88e7a7c04909056ef4a
Derek Borski <derek.borski@gmail.com>
2020-03-17 21:34:56 -0600

    Updates card_test.rb

--- test/card_test.rb
+++ test/card_test.rb
@@ -3,19 +3,20 @@ require 'minitest/pride'
 require './lib/card'
 
 class CardTest < Minitest::Test
-  def setup
-    card = Card.new(:diamond, 'Queen', 12)
-  end
 
   def test_it_exists
-    card = Card.new(:diamond, 'Queen', 12)
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
 
-    assert_instance_of Card, card
+    assert_instance_of Card, card1
+    assert_instance_of Card, card2
+    assert_instance_of Card, card3
   end
 
   def test_it_has_readable_attributes
     card = Card.new(:diamond, 'Queen', 12)
-    
+
     assert_equal :diamond, card.suit
     assert_equal 'Queen', card.value
     assert_equal 12, card.rank
