bf578afbc6d16edef2a9f090ed02f494ea45def8
Sage Lee <sagemcklee@gmail.com>
2020-03-18 16:54:52 -0600

    Add card class

--- lib/card.rb
+++ lib/card.rb
@@ -1,9 +1,12 @@
 class Card
   attr_accessor :suit, :value, :rank
 
-def initialize (suit, value, rank)
-  @suit = suit
-  @value = value
-  @rank = rank
-end
+
+  def initialize (suit, value, rank)
+    @suit = suit
+    @value = value
+    @rank = rank
+  end
+
+
 end
