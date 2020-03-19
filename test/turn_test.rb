7a4da84b08290545f02f79682c5de913ad54aa20
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-19 16:33:51 -0600

    Fix extra . in require file statement

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -1,9 +1,9 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require '../lib/card'
-require '../lib/deck'
-require '../lib/player'
-require '../lib/turn'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
 require "pry"
 
 class TurnTest < Minitest::Test
