b415bf46825b8b4bb8799c4625211edb8495b947
Taylor Keller <takeller17@gmail.com>
2020-03-19 14:27:50 -0600

    Update deck_test with instructor suggestions

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -14,27 +14,33 @@ class DeckTest < Minitest::Test
 
   end
 
+  # Change these to assert the array is correct vs checking length of the array
   def test_rank_of_card_at_
     @deck = Deck.new(@cards)
     assert_equal 12, @deck.rank_of_card_at(0)
   end
 
   def test_high_ranking_cards
-    assert_equal 2, @deck.high_ranking_cards.length
+    assert_equal [@card1, @card2], @deck.high_ranking_cards
+  end
+
+  def test_is_high_ranking?
+    assert_equal true, @deck.is_high_ranking?(@deck.cards[1])
+    assert_equal false, @deck.is_high_ranking?(@deck.cards[2])
   end
 
   def test_percent_high_ranking
-    assert_equal (2.0/3.0) * 100, @deck.percent_high_ranking
+    assert_equal (2.0/3.0 * 100).round(2), @deck.percent_high_ranking
   end
 
   def test_remove_card
     @deck.remove_card
-    assert_equal 2, @deck.cards.length
+    assert_equal [@card2, @card3], @deck.cards
   end
 
   def test_add_card
     @deck.add_card(@card4)
-    assert_equal 4, @deck.cards.length
+    assert_equal @cards << @card4, @deck.cards
   end
 
 end
