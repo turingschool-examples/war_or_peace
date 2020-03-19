8905499867a66b1ecd7611bb9ea0fd83415e42dd
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-03-18 23:37:22 -0600

    Create player.rb

---
+++ lib/player.rb
@@ -0,0 +1,16 @@
+require './lib/card.rb'
+require './lib/deck.rb'
+
+class Player
+  attr_reader :name, :deck
+  def initialize(name, deck)
+    @name = name
+    @deck = deck
+  end
+
+  def has_lost
+    @deck.cards.length == 0
+  end
+
+
+end
