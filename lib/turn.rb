591870f9929cd1cd856a3143d1bf5098ac8e0944
Max Mitrani <mjmitrani168@gmail.com>
2020-03-19 16:19:43 -0600

    Add spoils_of_war variable and test, turn.rb

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,11 +4,12 @@ require './lib/player'
 
 
 class Turn
-  attr_reader :player1, :player2
+  attr_reader :player1, :player2, :spoils_of_war
 
   def initialize(player1, player2)
   @player1 = player1
   @player2 = player2
+  @spoils_of_war = []
   end
 
 
