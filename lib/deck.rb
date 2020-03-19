3e6e2dc4ffe5c7decf09d73ff8f03ada8ad78223
Melanie Tran <melaniexx02@gmail.com>
2020-03-18 20:27:07 -0600

    Add cards attribute

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,15 +1,15 @@
 class Deck
   attr_reader :cards
 
-  def initialize (card)
-    @cards = card
+  def initialize(cards)
+    @cards = [cards]
   end
 
-  def add_card (card)
+  def add_card (card) #add to the end
     @cards << card
   end
 
-  def remove_card
+  def remove_card #remove the last card
     @cards.shift
   end
 end
