4e5d674198e79809e5f9e5c9a253e0d977177c3b
Derek Borski <derek.borski@gmail.com>
2020-03-19 15:15:31 -0600

    Refactors deck_test to seperate methods

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -35,7 +35,7 @@ class DeckTest < Minitest::Test
     assert_equal 66.67 , deck.percent_high_ranking
   end
 
-  def test_add_and_remove_card_from_deck
+  def test_remove_card_from_deck
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
@@ -43,27 +43,19 @@ class DeckTest < Minitest::Test
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
 
-    deck.remove_card(0)
+    deck.remove_card
     assert_equal [card2, card3], deck.cards
-    assert_equal 50 , deck.percent_high_ranking
-
-    deck.add_card(card4)
-    assert_equal [card2, card3, card4], deck.cards
-    assert_equal [card3], deck.high_ranking_cards
-    assert_equal 33.33, deck.percent_high_ranking
   end
 
-  # def test_add_card_to_deck
-  #   card1 = Card.new(:diamond, 'Queen', 12)
-  #   card2 = Card.new(:spade, '3', 3)
-  #   card3 = Card.new(:heart, 'Ace', 14)
-  #   card4 = Card.new(:club, '5', 5)
-  #   cards = [card1, card2, card3]
-  #   deck = Deck.new(cards)
-  #
-  #   deck.add_card(card4)
-  #   assert_equal [card1, card2, card3, card4], deck.cards
-  #   assert_equal [card1, card3], deck.high_ranking_cards
-  #   assert_equal 33.33, deck.percent_high_ranking
-  # end
+  def test_add_card_to_deck
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    card4 = Card.new(:club, '5', 5)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    deck.add_card(card4)
+    assert_equal [card1, card2, card3, card4], deck.cards
+  end
 end
