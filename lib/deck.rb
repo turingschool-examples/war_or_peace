e916c0346bd4a214973f96efafd58c75c3d17079
Taylor Keller <takeller17@gmail.com>
2020-03-19 14:26:55 -0600

    Update deck class with instructor suggestions

--- lib/deck.rb
+++ lib/deck.rb
@@ -12,16 +12,19 @@ class Deck
   def high_ranking_cards
     high_rank_cards = []
     cards.each do |card|
-      if card.rank >= 11
+      if is_high_ranking?(card)
         high_rank_cards << card
       end
     end
     high_rank_cards
   end
 
+  def is_high_ranking?(card)
+    card.rank >= 11
+  end
+
   def percent_high_ranking
-    high_rank_cards = high_ranking_cards
-    high_rank_cards.length.to_f/cards.length.to_f * 100
+    (high_ranking_cards.length.to_f/cards.length.to_f * 100).round(2)
   end
 
   def remove_card
