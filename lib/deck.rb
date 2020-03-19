126aa61635bde3601d09e83c58bec743e2ce3d25
Mad Hallé <mad.halle@icloud.com>
2020-03-18 21:32:12 -0600

    Work on test_it_exists deck_test and deck, not passing

--- lib/deck.rb
+++ lib/deck.rb
@@ -25,32 +25,30 @@
 
 
 
-def Deck
+class Deck
   attr_reader :cards
-  def initialize (cards)
-    @cards = []
+  def initialize(cards)
+    @cards = {}
   end
-
-  def rank_of_card_at (index)
-
-    [index]
-
-  end
-
-  def high_ranking_cards
-
-  end
-
-  def percent_high_ranking
-
-  end
-
-  def remove_card
-
-  end
-
-  def add_card
-
-  end
-
 end
+  # def rank_of_card_at (index)
+  #
+  #   [index]
+  #
+  # end
+  #
+  # def high_ranking_cards
+  #
+  # end
+  #
+  # def percent_high_ranking
+  #
+  # end
+  #
+  # def remove_card
+  #
+  # end
+  #
+  # def add_card
+  #
+  # end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,9 +1,36 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require '../lib/deck'
+require '../lib/deck.rb'
 require '../lib/card'
 
 
 class DeckTest < Minitest::Test
+
+  def test_it_exists
+    card1 = Card.new(:diamond, 'Queen', 12)
+    deck1 = Deck.new[card1]
+    assert_instance_of Deck, deck1
+  end
+
+  def test_percent_high_ranking_cards
+  skip
+
+  end
+
+  def test_has_high_ranking_cards
+  skip
+  end
+
+  def test_rank_of_a_card_at_a_given_index
+  skip
+  end
+
+  def test_add_a_card_to_bottom_of_deck
+  skip
+  end
+
+  def test_remove_top_card_from_deck
+  skip
+  end
 
 end
