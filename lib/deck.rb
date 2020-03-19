fb2d573033cda72aea76f765b70746d05946ac71
Mad Hallé <mad.halle@icloud.com>
2020-03-18 21:38:46 -0600

    New test_it_exists approach in deck_test, change @cards to array in deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -28,7 +28,7 @@
 class Deck
   attr_reader :cards
   def initialize(cards)
-    @cards = {}
+    @cards = []
   end
 end
   # def rank_of_card_at (index)
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -7,8 +7,13 @@ require '../lib/card'
 class DeckTest < Minitest::Test
 
   def test_it_exists
-    card1 = Card.new(:diamond, 'Queen', 12)
-    deck1 = Deck.new[card1]
+  card1 = Card.new(:diamond, 'Queen', 12)
+  card2 = Card.new(:spade, 'Jack', 11)
+  deck1 = Deck.new[card1, card2]
+
+    @cards << card1
+    @cards << card2
+
     assert_instance_of Deck, deck1
   end
 
