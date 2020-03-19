c20ba2c50bc6cf2532e2a4a5d78910ce0c73efb1
Derek Borski <derek.borski@gmail.com>
2020-03-18 19:32:02 -0600

    Finishes turn types

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,23 +6,29 @@ class Turn
     @spoils_of_war = []
   end
 
-  def spoils_of_war
-    @spoils_of_war
-  end
+  # def spoils_of_war
+  #   @spoils_of_war
+  # end
 
   def type
-    :basic
+    player1_first_card_rank = player1.deck.rank_of_card_at(0)
+    player2_first_card_rank = player2.deck.rank_of_card_at(0)
+    player1_third_card_rank = player1.deck.rank_of_card_at(2)
+    player2_third_card_rank = player2.deck.rank_of_card_at(2)
+    if player1_first_card_rank == player2_first_card_rank && player1_third_card_rank == player2_third_card_rank
+      :mutually_assured_destruction
+    elsif player1_first_card_rank != player2_first_card_rank
+      :basic
+    else player1_first_card_rank == player2_first_card_rank
+      :war
+    end
+
+
+
   end
 
-  def winner
 
-  end
 
-  def pile_cards
-  end
-
-  def award_spoils
-  end
 
 
 
