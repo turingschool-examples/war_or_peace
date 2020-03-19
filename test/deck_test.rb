cc1bf685e25173be94a4301226f73cd31f1d795d
Mad Hallé <mad.halle@icloud.com>
2020-03-19 15:02:04 -0600

    Adding def setup to deck test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,13 +1,26 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-# require '../lib/deck'
+require '../lib/deck'
 require '../lib/card'
-require '../lib/deck1'
-
+#require '../lib/deck1'
+require"pry"
 #Test passes but getting unititialized constant error for Deck...
 class DeckTest < Minitest::Test
+  def setup
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
 
-  def test_it_exists
+    @cards = [card1, card2, card3]
+
+    @deck1 = Deck.new(cards)
+    binding.pry
+    assert_instance_of Deck, @deck1
+
+  end
+
+  def test_percent_high_ranking_cards
+
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
@@ -16,11 +29,7 @@ class DeckTest < Minitest::Test
 
     deck1 = Deck.new(cards)
 
-    assert_instance_of Deck, deck1
-  end
 
-  def test_percent_high_ranking_cards
-  skip
 
   end
 
