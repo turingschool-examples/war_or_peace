7a6b3d0a36c5d9b51bdad94f6220e1e7accd0ee6
Sage Lee <sagemcklee@gmail.com>
2020-03-18 18:34:42 -0600

    Add deck test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,32 +1,48 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require '../lib/deck'
+require './lib/deck'
+require './lib/card'
 
 class DeckTest < Minitest::Test
-  def setup
-    first = [1,2,3,4,5,6]
-    deck = Deck.new(first)
+
+card_set
+
+  def test_does_it_exist
+    deck = Deck.new (card_set)
+    assert_instance_of Deck, deck
   end
 
-  def test_it_exists
-    skip
-    deck = Deck.new(first)
-    assert_instance_of deck, Deck.new
-  end
-
-  def test_it_will_index
-    skip
-    rank = Deck.rank_of_card_at (2)
-    assert_equal :rank, deck.rank
-  end
 
   def test_it_has_readable_attributes
-skip
-    deck = Deck.new
-        deck = Deck.new(first, 2)
-
-    assert_equal :diamond, card.suit
-    assert_equal 'Queen', card.value
-    assert_equal 12, card.rank
+    deck = Deck.new (card_set)
+    assert_equal card_set , deck.cards
   end
+
+  def test_rank_of_card_at_location
+    deck = Deck.new (card_set)
+    assert_equal @rank, deck.rank_of_card_at(index)
+end
+
+def test_high_ranking_cards
+  deck = Deck.new (card_set)
+  assert_equal ["card1","card3"], eleven_deck.high_ranking_cards
+end
+
+def test_percent_high_ranking
+  deck = Deck.new (card_set)
+  deck.percent_high_ranking
+  assert_equal "66%" , deck.percent_high_ranking
+end
+
+def test_remove_card
+  deck = Deck.new (card_set)
+  assert_equal ["card2","card3"] , deck.remove_card
+end
+
+def test_add_card
+  deck = Deck.new (card_set)
+  assert_equal["card2","card3","card4"], deck.add_card
+end
+
+
 end
