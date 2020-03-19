2005fd4584ab58f89c57543717db03f509c750bb
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-19 16:32:55 -0600

    Fix extra . in require file statement

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require '../lib/card'
-require '../lib/deck'
+require './lib/card'
+require './lib/deck'
 require "pry"
 
 class DeckTest < Minitest::Test
