e25ee1bc8d093a5fc8435ad02a876c396ca0e23d
Derek Borski <derek.borski@gmail.com>
2020-03-19 15:16:35 -0600

    Refactors has_lost method

--- lib/player.rb
+++ lib/player.rb
@@ -1,16 +1,12 @@
 class Player
   attr_reader :name, :deck
+
   def initialize(name, deck)
     @name = name
     @deck = deck
   end
 
   def has_lost?
-    if deck.cards.empty?
-      true
-    else
-      false
-    end 
+    deck.cards.empty?
   end
-
 end
