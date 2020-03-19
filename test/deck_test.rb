f47c1ab6a1c6b8146806187b36ec391c1389ef86
Norma Lopez <norma_l304@yahoo.com>
2020-03-18 22:37:41 -0600

    Add test for initialize method

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -6,7 +6,12 @@ require './lib/deck'
 class DeckTest < Minitest::Test
   def test_it_exists
     card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
 
     assert_instance_of Deck, card1
+    assert_instance_of Deck, card2
+    assert_instance_of Deck, card3 
   end
 end
