b768d0a0e2e965f0799887b303b3efc52f16a01c
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-03-19 19:35:31 -0600

    Add turn.rb

---
+++ lib/turn.rb
@@ -0,0 +1,12 @@
+class Turn
+  attr_reader :spoils_of_war, :player1, :player2
+  def initialize(player1, player2, spoils_of_war = [])
+    @player1 = player1
+    @player2 = player2
+    @spoils_of_war = spoils_of_war
+  end
+
+  # def
+  #
+  # end
+end
