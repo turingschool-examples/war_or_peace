add717dbeeb3abe32f0b05848bd6dae32b76015b
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-20 15:32:34 -0600

    Felt cute, might delete later

---
+++ lib/game.rb
@@ -0,0 +1,10 @@
+class Game
+  
+
+
+def commence_game
+   p "Welcome to War! (or Peace) This game will be played with 52 cards.
+   The players today are #{player1.name} and #{player2.name}.
+   Type 'GO' to start the game!
+   ____________________________________________________________________"
+end
