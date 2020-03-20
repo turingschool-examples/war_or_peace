d1040f1a2d5e0467ad959143cb9c0a6d1b5e6a09
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-20 15:33:22 -0600

    Created test skeleton for newly created game class

---
+++ test/game_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+require './lib/turn'
+require "pry"
+
+ class GameTest < Minitest::Test
+
+   def test_it_exists
+
+   end
+ end
