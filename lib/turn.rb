bedc403efbefb69ccd06fd4ccb5c1c4edb8e7bd5
Max Mitrani <mjmitrani168@gmail.com>
2020-03-19 19:16:49 -0600

    Add .type method and testing

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,8 +12,17 @@ class Turn
   @spoils_of_war = []
   end
 
-  #def type
-  #end
+  def type
+    turn_result = nil
+    if @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
+      turn_result = :mutually_assured_destruction
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
+      turn_result = :war
+    else
+      turn_result = :basic
+    end
+      turn_result
+  end
 
 
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -79,9 +79,46 @@ class TurnTest < Minitest::Test
 
     assert_equal true, turn.player1.deck.cards[1].rank == turn.player2.deck.cards[1].rank
     assert_equal false, turn.player1.deck.cards[0].rank == turn.player2.deck.cards[0].rank
+    assert_equal true, turn.player1.deck.rank_of_card_at(1) == turn.player2.deck.rank_of_card_at(1)
+    assert_equal false, turn.player1.deck.rank_of_card_at(0) == turn.player2.deck.rank_of_card_at(0)
   end
 
 
+  def test_can_read_as_basic
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, '10', 10)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal :basic, turn.type
+
+  end
+  def test_can_read_as_war
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, 'Jack', 11)
+    card4 = Card.new(:diamond, '10', 10)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal :war, turn.type
+  end
 
 
 end
