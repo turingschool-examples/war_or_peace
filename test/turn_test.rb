1c2424f6cd7b1883d10ea9326c53349f7b5498d2
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-19 18:35:58 -0600

    Add test for not piling cards when mutually assured destruction type

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -156,6 +156,26 @@ class TurnTest < Minitest::Test
     assert_equal [card1, card2, card3, card4, card5, card6], turn.spoils_of_war
   end
 
+  def test_it_can_not_pile_cards_mutually_assured_destruction_type
+    card1 = Card.new(:diamond, '10', 10)
+    card2 = Card.new(:heart, '9', 9)
+    card3 = Card.new(:diamond, '8', 8)
+    card4 = Card.new(:heart, '10', 10)
+    card5 = Card.new(:diamond, '2', 2)
+    card6 = Card.new(:heart, '8', 8)
+    card7 = Card.new(:spade, '4', 4)
+    card8 = Card.new(:club, '6', 6)
+    deck1 = Deck.new([card1, card2, card3, card7])
+    deck2 = Deck.new([card4, card5, card6, card8])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    turn.pile_cards
+
+    assert_equal [card7], player1.deck.cards
+    assert_equal [card8], player2.deck.cards
+  end
+
   def test_it_can_award_spoils
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
