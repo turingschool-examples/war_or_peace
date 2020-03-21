28f1e996f457a56ef47d76536550818303466727
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-20 18:33:06 -0600

    Added attributes to game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,17 +1,12 @@
 class Game
-  def initialize
-    @turns = 0
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
   end
-
 
   def start
-    p "Welcome to War! (or Peace) This game will be played with 52 cards.
-    The players today are #{player1.name} and #{player2.name}.
-    Type 'GO' to start the game!
-    ________________________________________________________________"
-
-    if gets == "go"
-      puts "Here we go!"
-    end
+    p "Welcome to War! (or Peace) This game will be played with 52 cards. The players today are #{@player1.name} and #{@player2.name}.
+    Type 'GO' to start the game!"
   end
+  
 end
