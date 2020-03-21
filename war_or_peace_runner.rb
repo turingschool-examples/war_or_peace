376923edb3c991fe4bb8fb5c55143264977f262c
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-20 18:18:08 -0600

    Deleted unnecessary comments

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -5,8 +5,6 @@ require './lib/turn'
 #require './lib/game'
 require 'pry'
 
-# create 52 cards (i.e. standard deck)
-
 deck = []
 suits = [:heart, :diamond, :spade, :club]
 ranks = (2..14).to_a
@@ -33,13 +31,10 @@ suits.each do |suit|
   end
 end
 
-# split those cards into two deck, randomly
-
 shuffled_cards = deck.shuffle
 deck1 = Deck.new(shuffled_cards.slice(0, 26))
 deck2 = Deck.new(shuffled_cards.slice(26, 26))
 
-# create two players with the deck created above
 player1 = Player.new("Honey Pie", deck1)
 player2 = Player.new("Sgt Pepper", deck2)
 
