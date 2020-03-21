dbf780cf467f33f56216e27b2cf9b21f64a680c4
Nicola Kim <nicola.copsey@gmail.com>
2020-03-21 15:17:05 -0700

    add work to deck_test on deckbranch

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -4,9 +4,6 @@ require './lib/deck'
 require './lib/card'
 
 class DeckTest < Minitest::Test
-  #def setup
-  #  deck = Deck.new[]
-  #end
 
   def test_it_rank_of_card_at
     card1 = Card.new(:diamond, 'Queen', 12)
@@ -19,15 +16,48 @@ class DeckTest < Minitest::Test
     assert_equal 14, deck.rank_of_card_at(2)
   end
 
-  def test_it_high_ranking_cards
+  def test_it_can_show_high_ranking_cards
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal [card1, card3], deck.high_ranking_cards
   end
 
-  def test_it_percent_high_ranking
+# this method will return the percentage of cards that are high ranking
+  def test_it_can_percent_high_ranking
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal 66.67, deck.percent_high_ranking
+  end
+
+  def test_it_can_remove_card
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    deck.remove_card
+    assert_equal [card2, card3], deck.cards
+  end
+
+  def test_it_can_add_card
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    card4 = Card.new(:club, '5', 5)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    deck.add_card(card4)
+    assert_equal [card1, card2, card3, card4], deck.cards
   end
 
 end
-
-# tests to add:
-
-# remove_card
-# add_card
