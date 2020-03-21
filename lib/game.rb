028ef6b163d8364b05db231676c809850778ad1e
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-20 19:18:26 -0600

    Add conditional to start method

--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,13 @@
 class Game
-  def initialize(player1, player2)
-    @player1 = player1
-    @player2 = player2
+  def initialize
   end
 
-  def start
-    p "Welcome to War! (or Peace) This game will be played with 52 cards. The players today are #{@player1.name} and #{@player2.name}.
-    Type 'GO' to start the game!"
+  def start(player1, player2)
+    p "Welcome to War! (or Peace) This game will be played with 52 cards. The players today are #{player1.name} and #{player2.name}. Type 'GO' to start the game!"
+
+    reply = gets.upcase
+    unless reply = "GO"
+      puts "Type 'GO' to start the game!"
+    end
   end
-  
 end
