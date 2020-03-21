08ef5cb721ff57b7672d06da012b083366d6dd5b
Melanie Tran <melaniexx02@gmail.com>
2020-03-21 08:11:49 -0600

    Edit organization of tests to make it more readable

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -98,6 +98,7 @@ class TurnTest < Minitest::Test
     player2 = Player.new("Aurora", deck2)
     turn = Turn.new(player1, player2)
 
+    assert_equal :basic, turn.type
     assert_equal player1, turn.winner
   end
 
@@ -156,7 +157,30 @@ class TurnTest < Minitest::Test
     player2 = Player.new("Aurora", deck2)
     turn = Turn.new(player1, player2)
 
+    assert_equal :war, turn.type
     assert_equal player2, turn.winner
+  end
+
+  def test_it_can_pile_cards_for_winner_type
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    require "pry";binding.pry
+
+    assert_equal :war, turn.type
+    assert_equal player2, turn.winner
+    assert_equal [card1, card2, card5, card3, card4, card6], turn.spoils_of_war
   end
 
 #Iteration tests for mutually assured destruction type
@@ -178,7 +202,7 @@ class TurnTest < Minitest::Test
     assert_equal :mutually_assured_destruction, turn.type
   end
 
-  def test_it_has_a_winner_for_mutually_assured_destruction_type
+  def test_it_has_no_winner_for_mutually_assured_destruction_type
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
     card3 = Card.new(:heart, '9', 9)
@@ -193,6 +217,7 @@ class TurnTest < Minitest::Test
     player2 = Player.new("Aurora", deck2)
     turn = Turn.new(player1, player2)
 
+    assert_equal :mutually_assured_destruction, turn.type
     assert_equal "No Winner", turn.winner
   end
 end
