47021a7ae31d81d3dc3d7c0a9761913e62afe752
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-03-21 12:28:34 -0600

    Add method: pile_cards to turn.rb

--- lib/turn.rb
+++ lib/turn.rb
@@ -39,5 +39,22 @@ class Turn
     end
   end
 
-
+  def pile_cards
+    if type == :basic
+      spoils_of_war << player1.deck.remove_card
+      spoils_of_war << player2.deck.remove_card
+    elsif type == :war
+      3.times do
+        spoils_of_war << player1.deck.remove_card
+      end
+      3.times do
+        spoils_of_war << player2.deck.remove_card
+      end
+    elsif type == :mutually_assured_destruction
+      3.times do
+        player1.deck.remove_card
+        player2.deck.remove_card
+      end
+    end
+  end
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -93,6 +93,29 @@ class TurnTest < Minitest::Test
     assert_equal player1, turn.winner
   end
 
+  def test_it_can_pile_cards_for_basic_turn
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+    turn.pile_cards
+    # assert_equal :basic, turn.type
+    # assert_equal player1, turn.winner
+    assert_equal [card1, card3], turn.spoils_of_war
+  end
+
   def test_it_test_it_can_tell_if_is_a_war_turn
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
@@ -137,6 +160,28 @@ class TurnTest < Minitest::Test
     assert_equal player2, turn.winner
   end
 
+  def test_it_test_it_can_pile_cards_for_war_turn
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+    turn.pile_cards
+    # assert_equal :war, turn.type
+    # assert_equal player2, turn.winner
+    assert_equal [card1, card2, card5, card4, card3, card6], turn.spoils_of_war
+  end
 
   def test_it_can_tell_it_is_a_mutually_assured_destruction_turn
     card1 = Card.new(:heart, 'Jack', 11)
@@ -181,5 +226,31 @@ class TurnTest < Minitest::Test
     # assert_equal :mutually_assured_destruction, turn.type
     assert_equal "No Winner", turn.winner
   end
+
+  def test_it_can_pile_cards_for_mutually_assured_destruction_turn
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, '8', 8)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+    turn.pile_cards
+    # assert_equal :mutually_assured_destruction, turn.type
+    # assert_equal "No Winner", turn.winner
+    assert_equal [], turn.spoils_of_war
+    assert_equal [card8], turn.player1.deck.cards
+    assert_equal [card7], turn.player2.deck.cards
+  end
+
 
 end
