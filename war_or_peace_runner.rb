0faa784806c364c46a67a6d5b8dd4b140e0e1e7f
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-20 18:02:24 -0600

    Created deck, shuffled deck, split deck, dealt deck to 2 new players

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -7,34 +7,42 @@ require 'pry'
 
 # create 52 cards (i.e. standard deck)
 
-# 4 suits = symbol
-  # 2 - 10 number cards(9), jack, king, queen, ace = string
-    # 13 ranks per suit = int
+deck = []
+suits = [:heart, :diamond, :spade, :club]
+ranks = (2..14).to_a
 
-def full_deck
-  deck = []
-  suits = [:heart, :diamond, :spade, :club]
-  ranks = (2..13).to_a
-  values = (2..10).to_s, "Jack", "Queen", "King", "Ace"
-  ranks.each do |rank|
-    suits.each do |suit|
-      values.each do |value|
-      deck << Card.new(suit, value, rank)
-      end
+rank_value_pairs = {}
+ranks.each do |rank|
+  key = rank
+  value = rank.to_s
+    if value == "11"
+      value = "Jack"
+    elsif value == "12"
+      value = "Queen"
+    elsif value == "13"
+      value = "King"
+    elsif value == "14"
+      value = "Ace"
     end
-  end
-  deck
+  rank_value_pairs[key] = value
 end
 
-p full_deck
-
-
-  # value = #numers 2 - 10.to_s + "Queen", "King", "Jack", "Ace"
+suits.each do |suit|
+    rank_value_pairs.each do |rank, value|
+    deck << Card.new(suit, value, rank)
+  end
+end
 
 # split those cards into two deck, randomly
-#
+
+shuffled_cards = deck.shuffle
+deck1 = Deck.new(shuffled_cards.slice(0, 26))
+deck2 = Deck.new(shuffled_cards.slice(26, 26))
+
 # create two players with the deck created above
-#
+player1 = Player.new("Honey Pie", deck1)
+player2 = Player.new("Sgt Pepper", deck2)
+
 # START the game using a new method called START (put it into a class or create a new class)
 #
 # if input = GO the game starts and then prints to the console different turns until game is completed or no one wins.
