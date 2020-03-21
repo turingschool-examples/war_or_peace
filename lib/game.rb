a2eda4fcf70a7e0f1ae819bb578c3f8768069952
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-21 13:27:11 -0600

    Create separate conditional flow for each turn type

--- lib/game.rb
+++ lib/game.rb
@@ -1,45 +1,60 @@
 class Game
-  def initialize
+  attr_reader :player1, :player2, :turn_count
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
     @turn_count = 0
   end
 
-  def start(player1, player2)
+  def start
     p "Welcome to War! (or Peace) This game will be played with 52 cards. The players today are #{player1.name} and #{player2.name}." "Type 'GO' to start the game!"
     reply = gets.chomp.upcase
     if reply == "GO"
-      take_a_turn(player1, player2)
+      take_a_turn
     end
   end
 
-  def take_a_turn(player1, player2)
-    if @turn_count < 1000000
-      turn = Turn.new(player1, player2)
+  def take_a_turn
+    turn = Turn.new(player1, player2)
+
+    if turn.type == :basic
       winner = turn.winner
       turn.pile_cards
       turn.award_spoils(winner)
       @turn_count += 1
+      p "Turn #{@turn_count}: #{winner.name} won"
+      continue_playing
 
-        if turn.type == :basic
-          p "Turn #{@turn_count}: #{winner.name} won"
-        elsif turn.type == :mutually_assured_destruction
-          p "Turn #{@turn_count}: *mutually assured destruction* 6 cards have been removed from play"
-        elsif turn.type == :war
-          p "Turn #{@turn_count}: WAR - #{winner.name} won 6 cards"
-        end
+    elsif turn.type == :war
+      winner = turn.winner
+      turn.pile_cards
+      turn.award_spoils(winner)
+      @turn_count += 1
+      p "Turn #{@turn_count}: WAR - #{winner.name} won 6 cards"
+      continue_playing
 
-        if player1.has_lost? == false && player2.has_lost? == false
-          take_a_turn(player1, player2)
-        elsif player1.has_lost? == true
-          puts "*~*~*~* #{player2.name} has won the game! *~*~*~*"
-        elsif player2.has_lost? == true
-          puts "*~*~*~* #{player1.name} has won the game! *~*~*~*"
-        else
-          puts "---- DRAW ----"
-        end
-    else
-      puts "---- DRAW ----"
+    elsif turn.type == :mutually_assured_destruction
+      turn.pile_cards
+      @turn_count += 1
+      p "*mutually assured destruction* 6 cards removed from play"
+      continue_playing
     end
-
   end
 
+  def continue_playing
+    if @turn_count < 10000
+        if player1.has_lost? == false && player2.has_lost? == false
+          take_a_turn
+        elsif player1.has_lost? == true
+          p "*~*~*~* #{player2.name} has won the game! *~*~*~*"
+        elsif player2.has_lost? == true
+          p "*~*~*~* #{player1.name} has won the game! *~*~*~*"
+        else
+          p "---- DRAW ----"
+        end
+    else
+      p "---- DRAW ----"
+    end
+  end
 end
