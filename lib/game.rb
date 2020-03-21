7ce11a9c2568ccbad3afb4db5bafe3b77b408236
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-20 18:17:28 -0600

    Added initialize method

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,17 @@
 class Game
-  
+  def initialize
+    @turns = 0
+  end
 
 
-def commence_game
-   p "Welcome to War! (or Peace) This game will be played with 52 cards.
-   The players today are #{player1.name} and #{player2.name}.
-   Type 'GO' to start the game!
-   ____________________________________________________________________"
+  def start
+    p "Welcome to War! (or Peace) This game will be played with 52 cards.
+    The players today are #{player1.name} and #{player2.name}.
+    Type 'GO' to start the game!
+    ________________________________________________________________"
+
+    if gets == "go"
+      puts "Here we go!"
+    end
+  end
 end
