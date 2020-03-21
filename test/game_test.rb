1ea32efa17402a5b631945d34e817fe3703c3b2b
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-20 18:32:43 -0600

    Added test for game class attributes

--- test/game_test.rb
+++ test/game_test.rb
@@ -7,11 +7,32 @@ require './lib/turn'
 require './lib/Game'
 require "pry"
 
- class GameTest < Minitest::Test
+class GameTest < Minitest::Test
 
    def test_it_exists
-     game = Game.new
+     card1 = Card.new(:heart, 'Jack', 11)
+     card2 = Card.new(:heart, '10', 10)
+     deck1 = Deck.new([card1])
+     deck2 = Deck.new([card2])
+     player1 = Player.new("Megan", deck1)
+     player2 = Player.new("Aurora", deck2)
+     game = Game.new(player1, player2)
 
      assert_instance_of Game, game
    end
- end
+
+   def test_it_has_attributes
+     card1 = Card.new(:heart, 'Jack', 11)
+     card2 = Card.new(:heart, '10', 10)
+     deck1 = Deck.new([card1])
+     deck2 = Deck.new([card2])
+     player1 = Player.new("Megan", deck1)
+     player2 = Player.new("Aurora", deck2)
+     game = Game.new(player1, player2)
+
+     assert_equal "Megan", player1.name
+   end
+
+
+
+end
