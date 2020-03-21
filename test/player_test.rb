2171c877842d9974fa21bba57a48f4684565c94d
Nicola Kim <nicola.copsey@gmail.com>
2020-03-21 16:13:26 -0700

    add work on player_test on playerbranch

--- test/player_test.rb
+++ test/player_test.rb
@@ -16,4 +16,24 @@ class PlayerTest < Minitest::Test
 
     assert_instance_of Player, player
   end
-end 
+
+  def test_player_has_lost
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+
+    assert_equal false, player.has_lost?
+
+    player.deck.remove_card
+    assert_equal false, player.has_lost?
+
+    player.deck.remove_card
+    assert_equal false, player.has_lost?
+
+    player.deck.remove_card
+    assert_equal true, player.has_lost?
+
+  end
+end
