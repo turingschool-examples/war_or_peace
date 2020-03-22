afbf0d629bfc054008203c06c61b0b7a5a05f3f0
Nicola Kim <nicola.copsey@gmail.com>
2020-03-22 10:03:07 -0700

    initial commit for gamerunner branch

---
+++ war_or_peace_runner.rb
@@ -0,0 +1,12 @@
+# Create 52 Cards (A standard deck)
+# Put those card into two Decks (some randomness would be nice here!)
+# Create two players with the Decks you created
+# Start the game using a new method called start
+# This method will need to be included in a class - it is up to you which class to include it in - either existing or ✨new ✨
+# Keep in mind that your existing objects should still pass all existing tests - nothing that you add in this iteration should break anything that functioned in iterations 1 or 2!
+#
+#
+# Welcome to War! (or Peace) This game will be played with 52 cards.
+# The players today are Megan and Aurora.
+# Type 'GO' to start the game!
+# ------------------------------------------------------------------
