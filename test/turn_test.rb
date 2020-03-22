6f71580affaf963d225e50d4562b19f51eaf9dbd
Mad Hallé <mad.halle@icloud.com>
2020-03-22 14:37:14 -0600

    adding working tests for spoils of war which also tests pile cards method, adding working winner tests

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -18,24 +18,13 @@ class RunTest < Minitest::Test
 
     @deck1 = Deck.new([@card1, @card2, @card5, @card8])
     @deck2 = Deck.new([@card3, @card4, @card6, @card7])
-
-    @player1 = Player.new("Megan", @deck1)
-    @player2 = Player.new("Aurora", @deck2)
-
-    @turn = Turn.new(@player1, @player2)
-  end
-
-  def test_it_exists
-    assert_instance_of Turn, @turn
-  end
-
-  def test_turn
-
     @deck3 = Deck.new([@card1, @card2, @card5, @card8])
     @deck4 = Deck.new([@card1, @card4, @card6, @card7])
     @deck5 = Deck.new([@card1, @card2, @card5, @card8])
     @deck6 = Deck.new([@card1, @card4, @card5, @card7])
 
+    @player1 = Player.new("Megan", @deck1)
+    @player2 = Player.new("Aurora", @deck2)
     @player3 = Player.new("Megan", @deck3)
     @player4 = Player.new("Aurora", @deck4)
     @player5 = Player.new("Megan", @deck5)
@@ -45,34 +34,73 @@ class RunTest < Minitest::Test
     @turn1 = Turn.new(@player3, @player4)
     @turn2 = Turn.new(@player5, @player6)
 
+  end
+
+  def test_it_exists
+    assert_instance_of Turn, @turn
+  end
+
+  def test_turn
+
+    # @deck3 = Deck.new([@card1, @card2, @card5, @card8])
+    # @deck4 = Deck.new([@card1, @card4, @card6, @card7])
+    # @deck5 = Deck.new([@card1, @card2, @card5, @card8])
+    # @deck6 = Deck.new([@card1, @card4, @card5, @card7])
+
+    # @player3 = Player.new("Megan", @deck3)
+    # @player4 = Player.new("Aurora", @deck4)
+    # @player5 = Player.new("Megan", @deck5)
+    # @player6 = Player.new("Aurora", @deck6)
+
+    # @turn = Turn.new(@player1, @player2)
+    # @turn1 = Turn.new(@player3, @player4)
+    # @turn2 = Turn.new(@player5, @player6)
+
     assert_equal :basic, @turn.type
     assert_equal :war, @turn1.type
     assert_equal :mutually_assured_destruction, @turn2.type
   end
 
   def test_basic_turn_winner
-
-  
-    # # winner: this method will determine the winner of the turn.
-    # # if the turn has a type of :basic, it will return whichever
-    #  player has a higher rank_of_card_at(0)
-    # # if the turn has a type of :war the winner will be whichever player
-    # has a higher rank_of_card_at(2)
-    # # if the turn has a type of :mutually_assured_destruction
-    # the method will return No Winner.
     assert_equal @player1, @turn.winner
   end
 
   def test_war_turn_winner
-    assert_equal @player4, @turn1.winner
+
+    deck3 = Deck.new([@card1, @card2, @card5, @card8])
+    deck4 = Deck.new([@card1, @card4, @card6, @card7])
+    player3 = Player.new("Megan", deck3)
+    player4 = Player.new("Aurora", deck4)
+    turn1 = Turn.new(player3, player4)
+    assert_equal player4, turn1.winner
   end
 
   def test_mutually_assured_destruction_winner
-    assert_equal 'No Winner', @turn2.winner
+
+    deck5 = Deck.new([@card1, @card2, @card5, @card8])
+    deck6 = Deck.new([@card1, @card4, @card5, @card7])
+    player5 = Player.new("Megan", deck5)
+    player6 = Player.new("Aurora", deck6)
+    turn2 = Turn.new(player5, player6)
+    assert_equal 'No Winner', turn2.winner
   end
 
-  def test_spoils_of_war
-  skip
+  def test_spoils_of_war_basic_turn
+    @turn.pile_cards
+    assert_equal [@card1, @card3], @turn.spoils_of_war
   end
+
+  def test_spoils_of_war_war_turn
+
+    @turn1.pile_cards
+    assert_equal [@card1, @card2, @card5, @card1, @card4, @card6] , @turn1.spoils_of_war
+  end
+
+  def test_spoils_of_war_mutually_assured_destruction_turn
+    @turn2.pile_cards
+    assert_equal [], @turn2.spoils_of_war
+  end
+
+
 
 end
