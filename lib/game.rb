0f23986da6423edd07b67df88fbad286d63f860d
Melanie Tran <melaniexx02@gmail.com>
2020-03-21 21:22:06 -0600

    Create play method to allow players to start playing the game

--- lib/game.rb
+++ lib/game.rb
@@ -1,26 +1,47 @@
-Class Game
-  attr_accessor :player1, :olayer2
+class Game
+  attr_reader :player1, :player2
 
-  def initialize
+  def initialize (player1, player2)
+    @player1 = player1
+    @player2 = player2
   end
 
   def start
-  p "Welcome to War! (or Peace) This game will be played with 52 cards."
-  p "The players today are #{player1.name} and #{player2.name}."
-  p "Type 'GO' to start the game!"
-  p "---------------------------------------------------------------------"
-  lets_begin = gets.chomp
+    p "Welcome to War! (or Peace) This game will be played with 52 cards."
+    p "The players today are #{player1.name} and #{player2.name}."
+    p "Type 'GO' to start the game!"
+    p "---------------------------------------------------------------------"
+    lets_begin = gets.chomp.upcase
 
-    if lets_begin = 'GO'
-      take_turn
+    if lets_begin == 'GO'
+      play
     else
       puts "Until next time..."
     end
   end
 
-def take_turn
-  if type == :basic
-    turn.pile_cards
-    turn.award_spoils
+  def play
+    play_count = 0
+    move = Turn.new(player1, player2)
 
+    if move.type == :basic
+      winner = move.winner
+      move.pile_cards
+      move.award_spoils(winner)
+      play_count += 1
+      p "Turn #{play_count}: #{winner.name} won 2 cards"
+
+    elsif move.type == :war
+      winner = move.winner
+      move.pile_cards
+      move.award_spoils(winner)
+      play_count += 1
+      p "Turn #{play_count}: WAR - #{winner.name} won 6 cards"
+
+    elsif move.type == :mutually_assured_destruction
+      move.pile_cards
+      play_count += 1
+      p "Turn #{play_count}: *mutually assured destruction* 6 cards removed from play"
+    end
+  end
 end
