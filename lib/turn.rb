6befd9156553772e952dc8d8a687eed12a3417ff
Taylor Keller <takeller17@gmail.com>
2020-03-22 11:48:38 -0600

    Finalize turn class

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,3 @@
-require 'pry'
 class Turn
 
   attr_reader :player1, :player2, :spoils_of_war
@@ -20,10 +19,21 @@ class Turn
   end
 
   def winner
+    if player1.has_lost?
+      return player2
+    elsif player2.has_lost?
+      return player1
+    end
+
     if type == :basic
       return player1 unless player2.deck.rank_of_card_at(0) > player1.deck.rank_of_card_at(0)
       player2
     elsif type == :war
+      if player1.deck.cards.length < 3
+        return player2
+      elsif player2.deck.cards.length < 3
+        return player1
+      end
       return player1 unless player2.deck.rank_of_card_at(2) > player1.deck.rank_of_card_at(2)
       player2
     else
@@ -41,13 +51,12 @@ class Turn
       @spoils_of_war << player1.deck.cards[0..2]
       @spoils_of_war << player2.deck.cards[0..2]
       @spoils_of_war.flatten!
-
       3.times do
         player1.deck.remove_card
         player2.deck.remove_card
       end
 
-    else
+    elsif type == :mutually_assured_destruction
       3.times do
         player1.deck.remove_card
         player2.deck.remove_card
@@ -56,9 +65,7 @@ class Turn
   end
 
   def award_spoils(winner)
-    if type == :basic
-      winner.deck.cards << spoils_of_war
-    end
+      return winner.deck.cards.push(spoils_of_war).flatten! unless winner == "No Winner"
   end
 
 end
