5c59206fe97f99dd1fc5165ca6e3cd62e0f38c3b
Ashkan Abbasi <airashkan@yahoo.com>
2020-03-23 00:36:00 -0600

    Create Game class in runner file

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -38,3 +38,15 @@ suits.each {|suit|
 
   game = Game.new(player1, player2)
   game.start
+
+class Game
+  attr_reader :player1, :player2, :turn_count
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @turn_count = 0
+  end
+
+
+end
