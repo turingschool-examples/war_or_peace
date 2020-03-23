9cde18c3912befc4df01e865e553e5aa43dfe69e
Mad Hallé <mad.halle@icloud.com>
2020-03-22 23:27:07 -0600

    adding instances and running methods

--- lib/war_or_peace_runner.rb
+++ lib/war_or_peace_runner.rb
@@ -1,3 +1,9 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require '../lib/deck'
+require '../lib/card'
+require '../lib/player'
+require '../lib/turn'
 
 card1 = Card.new(:heart, 'Ace', 1)
 card2 = Card.new(:heart, '2', 2)
@@ -55,18 +61,21 @@ card50 = Card.new(:spade, 'Jack', 11)
 card51 = Card.new(:spade, 'Queen', 12)
 card52 = Card.new(:spade, 'King', 13)
 
-deck = Deck.new(card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11, card12, card13,
+cards = [card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11, card12, card13,
 card14, card15, card16, card17, card18, card19, card20, card21, card22,
 card23, card24, card25, card26, card27, card28, card29,
 card30, card31, card32, card33, card34, card35, card36,
 card37, card38, card39, card40, card41, card42, card43,
-card44, card45, card46, card47, card48, card49, card50, card51, card52)
+card44, card45, card46, card47, card48, card49, card50, card51, card52]
 
+deck = Deck.new(cards)
 deck.shuffle
-deck.split
 
-player1 = Player.new("Mario", deck1)
-player2 = Player.new("Luigi", deck2)
+
+
+player1 = Player.new("Mario", deck.split_deck1)
+player2 = Player.new("Luigi", deck.split_deck2)
 
 turn = Turn.new(player1, player2)
 turn.start
+turn.pile_cards
