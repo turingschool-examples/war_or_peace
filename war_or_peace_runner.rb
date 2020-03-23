2edd580c02fcd2ebaf75d8a28ebe51d7ebc3916e
Ashkan Abbasi <airashkan@yahoo.com>
2020-03-23 00:10:56 -0600

    Shuffle cards and put in player decks

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -28,3 +28,13 @@ suits.each {|suit|
     rank_value_pairs.each do |rank, value|
     deck << Card.new(suit, value, rank)
   end}
+
+  shuffled_cards = deck.shuffle
+  deck1 = Deck.new(shuffled_cards.slice(0, 26))
+  deck2 = Deck.new(shuffled_cards.slice(27, 52))
+
+  player1 = Player.new('Huxley', deck1)
+  player2 = Player.new('Petra', deck2)
+
+  game = Game.new(player1, player2)
+  game.start
