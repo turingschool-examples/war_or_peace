f559d664a9d445d6e1e8c43531309836187cce5e
Nicola Kim <nicola.copsey@gmail.com>
2020-03-22 21:13:40 -0700

    cleaning up turn class

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,17 +7,16 @@ class Turn
     @player1 = player1
     @player2 = player2
     @spoils_of_war = []
-    # require "pry"; binding.pry
   end
 
   def type
     if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
-      type = :basic
+      :basic
     elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
       if player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
-        type = :mutually_assured_destruction
+        :mutually_assured_destruction
       else
-        type = :war
+        :war
       end
     end
   end
@@ -25,36 +24,31 @@ class Turn
   def winner
     if type == :basic
       if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
-        winner = player1
-        # require "pry"; binding.pry
+        player1
       else
-        winner = player2
+        player2
       end
     elsif type == :war
       if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
-        winner = player1
+        player1
       else
-        winner = player2
-        # require "pry"; binding.pry
+        player2
       end
     elsif type == :mutually_assured_destruction
-      winner = "No winner"
+      "No winner"
     end
   end
 
   def pile_cards
     if type == :basic
-      @spoils_of_war << player1.deck.cards.first #enumberable method!
+      @spoils_of_war << player1.deck.cards.first
       player1.deck.cards.shift
-      # require "pry"; binding.pry
       @spoils_of_war << player2.deck.cards.first
       player2.deck.cards.shift
-      # require "pry"; binding.pry
     elsif type == :war
       player1.deck.cards.first(3).each do |card|
         @spoils_of_war << card
         player1.deck.cards.shift(3)
-        # require "pry"; binding.pry
       end
       player2.deck.cards.first(3).each do |card|
         @spoils_of_war << card
@@ -63,17 +57,14 @@ class Turn
     elsif type == :mutually_assured_destruction
       player1.deck.cards.shift(3)
       player2.deck.cards.shift(3)
-        # require "pry"; binding.pry
       end
     end
 
     def award_spoils(winner)
-      @spoils_of_war.each do |spwar|
-        winner.deck.cards << spwar
-        require "pry"; binding.pry
+      @spoils_of_war.each do |spoilwar|
+        winner.deck.cards << spoilwar
       end
       @spoils_of_war = []
-      # require "pry"; binding.pry
     end
 
   end
