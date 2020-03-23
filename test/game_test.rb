5ee53bee93bda2a193b1c5092abc589aefd4db75
Nicola Kim <nicola.copsey@gmail.com>
2020-03-22 22:20:41 -0700

    updates to iteration 3 game test, 1 pass, 2 skips

--- test/game_test.rb
+++ test/game_test.rb
@@ -10,7 +10,14 @@ require 'pry'
 class GameTest < Minitest::Test
 
   def test_it_exists
-    
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    deck1 = Deck.new([card2])
+    deck2 = Deck.new([card1])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    game = Game.new
+
     assert_instance_of Game, game
   end
 
