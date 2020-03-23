bac5c9b85ec9aaf19fe007bc8a8814bf649b0eb8
Jack Gould <johnhilsmangould@gmail.com>
2020-03-23 07:16:55 -0600

    add start method to player card

--- lib/card.rb
+++ lib/card.rb
@@ -1,12 +1,36 @@
 # card class
+require './lib/card.rb'
+require './lib/deck.rb'
+require './lib/player.rb'
+require './lib/turn.rb'
+
+
+
+
+
+
 
 class Card
-  attr_accessor :suit, :value, :rank
+  attr_accessor :suit, :value, :rank, :start
 
   def initialize(suit, value, rank)
     @suit = suit
     @value = value
     @rank = rank
+
   end
+
+
+  # def start
+#     p "Welcome to War! (or Peace) This game will be played with 52 cards.
+# The players today are #{player_1.name} and #{player_2.name}."
+#     p "Type 'GO' to start the game!"
+#   answer = gets.chomp
+#
+#   if answer == "GO"
+#
+#
+#
+  # end
 
 end
