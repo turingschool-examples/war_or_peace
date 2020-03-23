f9046fc3d1ae272da297e0f7cbd3e37aa755ca0d
Jack Gould <johnhilsmangould@gmail.com>
2020-03-23 08:04:58 -0600

    commented everything out. this is an used rurunner file

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,19 +1,19 @@
-require './lib/card.rb'
-require './lib/deck.rb'
-require './lib/player.rb'
-require './lib/turn.rb'
-
-
-card1 = Card.new(:diamond, 'Queen', 12)
-card2 = Card.new(:spade, '3', 3)
-card3 = Card.new(:heart, 'Ace', 14)
-card4 = Card.new(:diamond, 'Queen', 12)
-card5 = Card.new(:spade, '3', 3)
-card6 = Card.new(:heart, 'Ace', 14)
-cards = [card1, card2, card3, card4, card5, card6]
-
-deck1 = Deck.new([card1, card2, card3])
-deck2 = Deck.new([card4, card5, card6])
+# require './lib/card.rb'
+# require './lib/deck.rb'
+# require './lib/player.rb'
+# require './lib/turn.rb'
+#
+#
+# card1 = Card.new(:diamond, 'Queen', 12)
+# card2 = Card.new(:spade, '3', 3)
+# card3 = Card.new(:heart, 'Ace', 14)
+# card4 = Card.new(:diamond, 'Queen', 12)
+# card5 = Card.new(:spade, '3', 3)
+# card6 = Card.new(:heart, 'Ace', 14)
+# cards = [card1, card2, card3, card4, card5, card6]
+#
+# deck1 = Deck.new([card1, card2, card3])
+# deck2 = Deck.new([card4, card5, card6])
 # deck1.cards
 # deck2.cards
 # deck.rank_of_card_at(0)
@@ -30,13 +30,13 @@ deck2 = Deck.new([card4, card5, card6])
 # deck.cards
 # deck.high_ranking_cards
 # deck.percent_high_ranking
-
-player1 = Player.new("Jack", deck1)
-player2 = Player.new("John", deck2)
-player1.has_lost?
-player2.has_lost?
-
-turn = Turn.new(player1, player2)
-
-
-# require 'pry'; binding.pry
+#
+# player1 = Player.new("Jack", deck1)
+# player2 = Player.new("John", deck2)
+# player1.has_lost?
+# player2.has_lost?
+#
+# turn = Turn.new(player1, player2)
+#
+#
+# # require 'pry'; binding.pry
