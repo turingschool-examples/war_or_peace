0fd57159635342872e9d00f12bfd418ed99e513c
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-23 21:33:16 -0600

    Removed test which was relocated to player_test because it was actually testing the player class method has_lost?

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -149,21 +149,6 @@ class TurnTest < Minitest::Test
     assert_equal [card8], player2.deck.cards
   end
 
-  def test_it_can_still_designate_winner_if_not_enough_cards
-    card1 = Card.new(:diamond, '10', 10)
-    card2 = Card.new(:heart, '9', 9)
-    card3 = Card.new(:diamond, '8', 8)
-    card4 = Card.new(:spade, '10', 10)
-    deck1 = Deck.new([card1, card2, card3])
-    deck2 = Deck.new([card4])
-    player1 = Player.new("Megan", deck1)
-    player2 = Player.new("Aurora", deck2)
-    turn = Turn.new(player1, player2)
-    turn.pile_cards 
-
-    assert_equal true, player2.has_lost?
-  end
-
   def test_it_can_award_spoils
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
