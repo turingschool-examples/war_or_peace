e0562a23879dcbae5ba26036404a96a600227315
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-23 21:32:14 -0600

    Relocated test from turn_test because it tested has_lost? player method

--- test/player_test.rb
+++ test/player_test.rb
@@ -3,6 +3,7 @@ require 'minitest/pride'
 require './lib/card'
 require './lib/deck'
 require './lib/player'
+require './lib/turn'
 require "pry"
 
 class PlayerTest < Minitest::Test
@@ -32,11 +33,25 @@ class PlayerTest < Minitest::Test
     deck = Deck.new([card1])
     player = Player.new('Clarisa', deck)
 
-    assert_equal false,
-    player.has_lost?
-
+    assert_equal false, player.has_lost?
     deck.remove_card
 
     assert_equal true, player.has_lost?
   end
+
+  def test_it_can_still_designate_winner_if_not_enough_cards
+    card1 = Card.new(:diamond, '10', 10)
+    card2 = Card.new(:heart, '9', 9)
+    card3 = Card.new(:diamond, '8', 8)
+    card4 = Card.new(:spade, '10', 10)
+    deck1 = Deck.new([card1, card2, card3])
+    deck2 = Deck.new([card4])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    turn.pile_cards
+
+    assert_equal true, player2.has_lost?
+  end
+
 end
