ab9acdf4c74ec04f418716b8d8df5d766eb68d94
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-23 22:09:42 -0600

    Added test for new end_game method. Tried to create additional tests but couldn't figure out how to work around user input, so those are commented out

--- test/game_test.rb
+++ test/game_test.rb
@@ -21,16 +21,44 @@ class GameTest < Minitest::Test
      assert_instance_of Game, game
    end
 
-   def test_it_can_take_a_turn
-     card1 = Card.new(:heart, 'Jack', 11)
-     card2 = Card.new(:heart, '10', 10)
-     deck1 = Deck.new([card1])
-     deck2 = Deck.new([card2])
-     player1 = Player.new("Megan", deck1)
-     player2 = Player.new("Aurora", deck2)
-     game = Game.new(player1, player2)
-     game.take_a_turn
+   def test_it_can_end_game
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    deck1 = Deck.new([card1])
+    deck2 = Deck.new([card2])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    game = Game.new(player1, player2)
 
-     assert_equal false, @turn_count == 0
-   end
+    assert_equal "---- DRAW ----", game.end_game
+  end
+
 end
+   # def test_it_can_take_a_turn
+   #   card1 = Card.new(:heart, 'Jack', 11)
+   #   card2 = Card.new(:heart, '10', 10)
+   #   deck1 = Deck.new([card1])
+   #   deck2 = Deck.new([card2])
+   #   player1 = Player.new("Megan", deck1)
+   #   player2 = Player.new("Aurora", deck2)
+   #   game = Game.new(player1, player2)
+   #   game.take_a_turn
+   #   @current_turn == 1
+   #
+   #   assert_equal true, @turn_count == 1
+   # end
+
+   # def test_it_can_respond
+   #   card1 = Card.new(:heart, 'Jack', 11)
+   #   card2 = Card.new(:heart, '10', 10)
+   #   deck1 = Deck.new([card1])
+   #   deck2 = Deck.new([card2])
+   #   player1 = Player.new("Megan", deck1)
+   #   player2 = Player.new("Aurora", deck2)
+   #   game = Game.new(player1, player2)
+   #   game.response("GO")
+   #
+   #   assert_equal "GO"
+   # end
+
+   #ok to not test get_user_input since it relys entirely on ruby methods
