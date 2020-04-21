03c28c2dddb05237b57f85a507dbf8f85ecf400a
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-04-21 09:50:54 -0600

    Add setup method

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,6 +5,14 @@ require './lib/deck'
 
 class DeckTest < Minitest::Test
 
+  # Create an array of card instances
+  def setup
+    card1 = Card.new(:diamond, "Queen", 12)
+    card2 = Card.new(:spade, "3", 3)
+    card3 = Card.new(:heart, "Ace", 14)
+    cards = [card1, card2, card3]
+  end
+
   def test_it_exists
     deck = Deck.new
     assert_instance_of Deck, deck
