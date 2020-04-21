0cf52aaa929fd092af8d66fd1f029170b8d06923
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-04-21 09:47:16 -0600

    Add first test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,7 +1,13 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
+require './lib/deck'
 
 class DeckTest < Minitest::Test
+
+  def test_it_exists
+    deck = Deck.new
+    assert_instance_of Deck, deck
+  end
 
 end
