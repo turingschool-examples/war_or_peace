61abec11274137393dff57e9dc07b4cc0f125cc2
Grant Dempsey <CryptoDemps@gmail.com>
2020-05-04 20:21:20 -0600

    turn test_readable_attributes

---
+++ lib/turn.rb
@@ -0,0 +1,17 @@
+class Turn
+
+  attr_reader :spoils_of_war,
+              :player1,
+              :player2
+
+  def initialize(player1, player2)
+    @spoils_of_war = []
+    @player1 = player1
+    @player2 = player2
+  end
+
+  def type
+    
+  end
+
+end
