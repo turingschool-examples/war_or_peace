9258d00c00f508e4b31614152b17101da23bf605
Grant Dempsey <CryptoDemps@gmail.com>
2020-05-04 22:13:25 -0600

    pile_cards method and tests complete

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,15 +11,61 @@ class Turn
   end
 
   def type
-      if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
-        :basic
-      elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) &&
-        @player1.deck.rank_of_card_at(2) != @player2.deck.rank_of_card_at(2)
-        :war
-      elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) &&
-        @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
-        :mutually_assured_destruction
-      end
+    if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
+      :basic
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) &&
+      @player1.deck.rank_of_card_at(2) != @player2.deck.rank_of_card_at(2)
+      :war
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) &&
+      @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
+      :mutually_assured_destruction
+    end
+  end
+
+  def winner
+    if type == :basic && @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
+      @player1
+    elsif type == :basic && @player2.deck.rank_of_card_at(0) > @player1.deck.rank_of_card_at(0)
+      @player2
+    elsif type == :war && @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
+      @player1
+    elsif type == :war && @player2.deck.rank_of_card_at(2) > @player1.deck.rank_of_card_at(2)
+      @player2
+    elsif type == :mutually_assured_destruction
+      p "No Winner"
+    end
+  end
+
+  def pile_cards
+    if type == :basic
+      @spoils_of_war << @player1.deck.cards.first
+      @player1.deck.cards.shift
+      @spoils_of_war << @player2.deck.cards.first
+      @player2.deck.cards.shift
+      @spoils_of_war
+    elsif type == :war
+      @spoils_of_war << @player1.deck.cards.first
+      @player1.deck.cards.shift
+      @spoils_of_war << @player1.deck.cards.first
+      @player1.deck.cards.shift
+      @spoils_of_war << @player1.deck.cards.first
+      @player1.deck.cards.shift
+      @spoils_of_war << @player2.deck.cards.first
+      @player2.deck.cards.shift
+      @spoils_of_war << @player2.deck.cards.first
+      @player2.deck.cards.shift
+      @spoils_of_war << @player2.deck.cards.first
+      @player2.deck.cards.shift
+      @spoils_of_war
+    elsif type == :mutually_assured_destruction
+      # @player1.deck.cards.shift
+      # @player1.deck.cards.shift
+      @player1.deck.cards[0,1,2].shift
+      @player2.deck.cards[0,1,2].shift
+      # @player2.deck.cards.shift
+      # @player2.deck.cards.shift
+      @spoils_of_war
+    end
   end
 
 end
