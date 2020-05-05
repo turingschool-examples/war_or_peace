5b9b2f5e8563d4491a291bb2853f09f1396cb97d
Norma Lopez <norma_l304@yahoo.com>
2020-05-04 22:45:34 -0600

    Add rank_of_card_at(index) method with passing test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
 require './lib/deck'
+require 'pry'
 
 class DeckTest < Minitest::Test
   def test_it_exists
@@ -21,6 +22,17 @@ class DeckTest < Minitest::Test
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
 
-    assert_equal [card1, card2, card3], deck.cards 
+    assert_equal [card1, card2, card3], deck.cards
+  end
+
+  def test_rank_of_card_at_index
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal 12, deck.rank_of_card_at(0)
+    assert_equal 14, deck.rank_of_card_at(2)
   end
 end
