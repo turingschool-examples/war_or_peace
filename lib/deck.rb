6304d31600d3839b33922d21f06d616b50a21a24
Grant Dempsey <CryptoDemps@gmail.com>
2020-05-04 19:10:09 -0600

    remove_card method complete

--- lib/deck.rb
+++ lib/deck.rb
@@ -21,7 +21,12 @@ class Deck
   end
 
   def percent_high_ranking
-    ((high_ranking_cards.length/cards.length.to_f) * 100).round(2)
+    percentage = (high_ranking_cards.length.to_f / @cards.length.to_f) * 100
+    percentage.round(2)
+  end
+
+  def remove_card
+    @cards.shift
   end
 
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -48,7 +48,7 @@ class DeckTest < Minitest::Test
     card3 = Card.new(:heart, 'Ace', 14)
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
-    assert_equal [card2, card3], deck.remove_card
+    assert_equal card1, deck.remove_card
   end
 
 end
