af6cbcdb97545aa071cf625b63aec181ed0161c2
Gaby Mendez <gmendez90@gmail.com>
2020-05-04 23:21:32 -0400

    Add tests for Deck methods; complete Iteration 1

--- lib/deck.rb
+++ lib/deck.rb
@@ -7,25 +7,31 @@ class Deck
   def rank_of_card_at(card_index)
     # index typically 0 or 2
     # returns rank of card
+    @cards[card_index].rank
   end
 
-  def high_ranking_card
+  def high_ranking_cards
     # returns an array of cards in the deck
     # that have a rank of 11 or above
-    # (face cards and aces)
+    @cards.find_all do |card|
+      card.rank >= 11
+    end
   end
 
   def percent_high_ranking
     # return percentage of cards
     # that are high ranking
+    (self.high_ranking_cards.count.to_f / @cards.count.to_f * 100).round(2)
   end
 
   def remove_card
     # remove top card from the deck
+    @cards.shift
   end
 
-  def add_card
+  def add_card(card)
     # add one card to bottom (end) of deck
+    @cards << card
   end
 
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -6,6 +6,13 @@ require 'pry'
 
 class CardTest < Minitest::Test
 
+  def setup
+    # figure out how to properly setup
+    # card & deck instances in order to
+    # easily run multiple tests without
+    # redeclaring variables
+  end
+
   def test_it_exists
     deck = Deck.new
 
@@ -20,7 +27,29 @@ class CardTest < Minitest::Test
 
     deck = Deck.new(cards)
 
-    assert_equal cards, deck.cards 
+    assert_equal cards, deck.cards
+
+    assert_equal 12, deck.rank_of_card_at(0)
+    assert_equal 14, deck.rank_of_card_at(2)
+
+    assert_equal [card1, card3], deck.high_ranking_cards
+
+    assert_equal 66.67, deck.percent_high_ranking
+
+    assert_equal card1, deck.remove_card
+    assert_equal [card2, card3], deck.cards
+
+    assert_equal 50.0, deck.percent_high_ranking
+
+    card4 = Card.new(:club, '5', 5)
+    deck.add_card(card4)
+
+    assert_equal [card2, card3, card4], deck.cards
+
+    assert_equal [card3], deck.high_ranking_cards
+
+    assert_equal 33.33, deck.percent_high_ranking
+
   end
 
 end
