631070aef7503460a043cf5262e4ee3680678122
Grant Dempsey <CryptoDemps@gmail.com>
2020-05-04 19:57:29 -0600

    has_lost? test  and method complete

--- test/player_test.rb
+++ test/player_test.rb
@@ -13,7 +13,22 @@ class DeckTest < Minitest::Test
     deck = Deck.new([card1, card2, card3])
     player = Player.new('Clarisa', deck)
     assert_equal "Clarisa", player.name
-    assert_equal [card1, card2, card3], player.deck
+    assert_equal deck, player.deck
+  end
+
+  def test_has_lost?
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+    assert_equal false, player.has_lost?
+    player.deck.remove_card
+    assert_equal false, player.has_lost?
+    player.deck.remove_card
+    assert_equal false, player.has_lost?
+    player.deck.remove_card
+    assert_equal true, player.has_lost?
   end
 
 end
