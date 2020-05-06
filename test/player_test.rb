356e28220aa031391befe21964f4c9c69561efb2
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-06 15:01:00 -0600

    Fix missing @'s in Player test setup method

--- test/player_test.rb
+++ test/player_test.rb
@@ -9,8 +9,8 @@ class PlayerTest < Minitest::Test
     @card1 = Card.new(:diamond, 'Queen', 12)
     @card2 = Card.new(:spade, '3', 3)
     @card3 = Card.new(:heart, 'Ace', 14)
-    @deck = Deck.new([card1, card2, card3])
-    @player = Player.new('Clarisa', deck)
+    @deck = Deck.new([@card1, @card2, @card3])
+    @player = Player.new('Clarisa', @deck)
   end
 
   def test_it_exists
