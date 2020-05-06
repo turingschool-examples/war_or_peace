a84c3a2cae6ae7f596c51ef145ec1a979d838064
Norma Lopez <norma_l304@yahoo.com>
2020-05-05 21:47:04 -0600

    Test percent_high_ranking after adding card to bottom of deck

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -55,12 +55,15 @@ class DeckTest < Minitest::Test
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
+    card4 = Card.new(:club, '5', 5)
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
 
     assert_equal 66.67, deck.percent_high_ranking
     deck.remove_card
     assert_equal 50.0, deck.percent_high_ranking
+    deck.add_card(card4)
+    assert_equal 33.33, deck.percent_high_ranking
   end
 
   def test_it_can_remove_card
