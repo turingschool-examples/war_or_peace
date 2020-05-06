6edd86ca336529685465a2089e2689fe44ea2ce1
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-05 22:02:53 -0600

    Add percent_high_ranking method and test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -53,4 +53,14 @@ class DeckTest < Minitest::Test
 
     assert_equal [card1, card3], deck.high_ranking_cards
   end
+
+  def test_it_can_identify_percentage_of_high_ranking_cards
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal 66.67, deck.percent_high_ranking
+  end
 end
