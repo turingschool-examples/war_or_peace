b67a5da547d03d8f33cb8963629d8a609da35ae8
Dan Sehl <dan.sehl@gmail.com>
2020-05-05 20:47:57 -0600

    Refactor deck_test with setup method, create player and player_test file

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,6 +5,12 @@ require './lib/deck'
 
 class DeckTest < Minitest::Test
 
+  def setup
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+  end
+
   def test_deck_contains_card
     card = Card.new(:diamond, 'Queen', 12)
     deck = Deck.new(card)
@@ -12,70 +18,49 @@ class DeckTest < Minitest::Test
   end
 
   def test_deck_contains_multiple_cards
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
+    cards = [@card1, @card2, @card3]
     deck = Deck.new(cards)
     assert_equal cards, deck.cards
   end
 
   def test_rank_of_a_card
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
+    cards = [@card1, @card2, @card3]
     deck = Deck.new(cards)
     assert_equal 12, deck.rank_of_card_at(0)
   end
 
   def test_rank_of_another_card
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
+    cards = [@card1, @card2, @card3]
     deck = Deck.new(cards)
     assert_equal 14, deck.rank_of_card_at(2)
   end
 
   def test_high_ranking_cards
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
+    cards = [@card1, @card2, @card3]
     deck = Deck.new(cards)
-    assert_equal [card1, card3], deck.high_ranking_cards
+    assert_equal [@card1, @card3], deck.high_ranking_cards
   end
 
   def test_percentage_high_ranking
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
+    cards = [@card1, @card2, @card3]
     deck = Deck.new(cards)
     assert_equal (66.67), deck.percent_high_ranking
   end
 
   def test_remove_card_works
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
+    cards = [@card1, @card2, @card3]
     deck = Deck.new(cards)
     deck.remove_card
-    assert_equal [card2, card3], deck.cards
+    assert_equal [@card2, @card3], deck.cards
   end
 
   def test_add_card_works
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
+    cards = [@card1, @card2, @card3]
     deck = Deck.new(cards)
     deck.remove_card
     card4 = Card.new(:club, '5', 5)
     deck.add_card(card4)
-    assert_equal [card2, card3, card4], deck.cards
+    assert_equal [@card2, @card3, card4], deck.cards
   end
 
   def test_percent_high_ranking_works_again
---
+++ test/player_test.rb
@@ -0,0 +1,6 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+
+class DeckTest < Minitest::Test
