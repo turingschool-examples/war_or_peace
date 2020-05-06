bc8067ad90acc868e922a4b7cc189af23887f36a
Norma Lopez <norma_l304@yahoo.com>
2020-05-05 21:43:03 -0600

    Test high_ranking_cards after adding card to bottom of deck

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -40,11 +40,14 @@ class DeckTest < Minitest::Test
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
+    card4 = Card.new(:club, '5', 5)
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
 
     assert_equal [card1, card3], deck.high_ranking_cards
     deck.remove_card
+    assert_equal [card3], deck.high_ranking_cards
+    deck.add_card(card4)
     assert_equal [card3], deck.high_ranking_cards
   end
 
