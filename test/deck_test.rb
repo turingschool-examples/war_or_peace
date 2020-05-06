f2060086857088485752aad8e329158fdce1fc61
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-05 21:31:35 -0600

    Add high_ranking_cards method and test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -42,4 +42,15 @@ class DeckTest < Minitest::Test
     assert_equal 12, deck.rank_of_card_at(0)
     assert_equal 14, deck.rank_of_card_at(2)
   end
+
+  def test_it_can_identify_high_ranking_cards
+    # rank of >= 11 (face cards & aces)
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal [card1, card3], deck.high_ranking_cards
+  end
 end
