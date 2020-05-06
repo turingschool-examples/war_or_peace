8676af5791ec6746b02708d3e02d8071ed43fca6
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-06 00:02:20 -0600

    Remove extra instance variables

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -8,26 +8,24 @@ require './lib/card'
 class TurnTest < Minitest::Test
 
   def setup
-    @card1 = Card.new(:heart, "Jack", 11)
-    @card2 = Card.new(:heart, "10", 10)
-    @card3 = Card.new(:heart, "9", 9)
-    @card4 = Card.new(:diamonds, "Jack", 11)
+    card1 = Card.new(:heart, "Jack", 11)
+    card2 = Card.new(:heart, "10", 10)
+    card3 = Card.new(:heart, "9", 9)
+    card4 = Card.new(:diamonds, "Jack", 11)
 
-    @card5 = Card.new(:heart, "8", 8)
-    @card6 = Card.new(:diamond, "Queen", 12)
-    @card7 = Card.new(:heart, "3", 3)
-    @card8 = Card.new(:diamond, "2", 2)
+    card5 = Card.new(:heart, "8", 8)
+    card6 = Card.new(:diamond, "Queen", 12)
+    card7 = Card.new(:heart, "3", 3)
+    card8 = Card.new(:diamond, "2", 2)
 
-    @basic_deck_1 = Deck.new([@card1, @card2, @card5, @card8])
-    @basic_deck_2 = Deck.new([@card3, @card4, @card6, @card7])
+    @basic_deck_1 = Deck.new([card1, card2, card5, card8])
+    @basic_deck_2 = Deck.new([card3, card4, card6, card7])
 
-    @war_deck_1 = Deck.new([@card1, @card2, @card5, @card8])
-    @war_deck_2 = Deck.new([@card4, @card3, @card6, @card7])
+    @war_deck_1 = Deck.new([card1, card2, card5, card8])
+    @war_deck_2 = Deck.new([card4, card3, card6, card7])
 
-    @mad_deck_1 = Deck.new([@card1, @card2, @card5, @card8])
-    @mad_deck_2 = Deck.new([@card4, @card3, Card.new(:diamond, "8", 8), @card7])
-
-
+    @mad_deck_1 = Deck.new([card1, card2, card5, card8])
+    @mad_deck_2 = Deck.new([card4, card3, Card.new(:diamond, "8", 8), card7])
   end
 
   def test_it_exists
