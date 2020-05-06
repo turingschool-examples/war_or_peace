20e8d83ce6edf5bc30a8391c5b9e6b5d1685cf11
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-05 20:17:54 -0600

    Add it_exists test to deck_test.rb; add initialize method to deck.rb

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -11,4 +11,13 @@ class DeckTest < Minitest::Test
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
   end
+
+  def test_it_exists
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    assert_instance_of Deck, deck
+  end
 end
