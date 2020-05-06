65f9bbccb58c1ce0cc9965bba6dc60a17bdfe753
“Neeru <neeram85@gmail.com>
2020-05-05 19:56:20 -0600

    Misc. work on lib and test files

--- lib/card.rb
+++ lib/card.rb
@@ -1,11 +1,11 @@
 class Card
-  attr_reader :suit, :value, :rank
+  attr_reader :suit,
+              :value,
+              :rank,
 
   def initialize(suit, value, rank)
     @suit = suit
     @value = value
     @rank = rank
-  end
-  def new(suit, value, rank)
   end
 end
---
+++ test/deck_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/deck'
+require './lib/card'
+require 'pry'
+
+class DeckTest < Minitest::Test
+  def test_it_exists
+  end
+end
