3cfb989e79d523b9e1d28a46dedd2f848cdb6c7a
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-06 22:42:02 -0600

    Add iterator to generate a hash of 52 proto-cards

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -25,3 +25,32 @@ loop do
     p l3
   end
 end
+
+ranks = (1..13).to_a
+
+values = (2..10).to_a
+values = values.map do |value|
+  value.to_s
+end
+values.prepend("Ace")
+
+faces = ["Jack", "Queen", "King"]
+faces.each do |face|
+  values << face
+end
+
+suits = [:club, :spade, :heart, :diamond]
+
+card_arr = []
+
+suits.each do |suit|
+  values.each_with_index do |value, i|
+    card_arr << {"suit" => suit, "value" => value, "rank" => i+1}
+  end
+end
+
+# Aces
+p card_arr[0]
+p card_arr[13]
+p card_arr[26]
+p card_arr[39]
