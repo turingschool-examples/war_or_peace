18711d71405a1850a3ea7de22aa4c02986bfa7bc
Michael Chickillo <truthvendor@gmail.com>
2020-05-07 08:24:32 -0600

    Updated deck_test and added more tests to turn_test

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,25 +3,50 @@ require './lib/deck'
 require './lib/player'
 
 class Turn
-  attr_reader :player1, :player2, :spoils_of_war
+  attr_reader :player1, :player2, :spoils_of_war, :type
 
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
     @spoils_of_war = []
-    @turn = type_of_turn
+    @type = type_of_turn
   end
 
   def type_of_turn
-
+    if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) &&
+       player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+       :mutually_assured_destruction
+    elsif
+       player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+       :war
+    else
+       player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
+       :basic
+    end
   end
 
   def winner
-
+    if type == :basic
+      return player1 if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+      return player2 if player1.deck.rank_of_card_at(0) < player2.deck.rank_of_card_at(0)
+    elsif type == :war
+      return player1 if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+      return player2 if player1.deck.rank_of_card_at(2) < player2.deck.rank_of_card_at(2)
+    else type == :mutually_assured_destruction
+      return "No Winner"
+    end
   end
 
   def pile_cards
-
+    if type == :basic
+      @spoils_of_war << player1.deck.card.shift
+      @spoils_of_war << player2.deck.card.shift
+    elsif type == :war
+      @spoils_of_war << player1.deck.card.slice!(0, 1, 2)
+      @spoils_of_war << player2.deck.card.slice!(0, 1, 2)
+    else type == :mutually_assured_destruction
+      player1.deck.cards.slice!(0, 1, 2)
+      player2.deck.cards.slice!(0, 1, 2)
+    end
   end
-
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -46,7 +46,7 @@ class Decktest < Minitest::Test
     assert_equal
   end
 
-  def test_remove_card
+  def test_remove_a_card
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
