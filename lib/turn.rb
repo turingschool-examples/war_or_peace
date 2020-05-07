03cebbb169c25664530d226598c4f89055062885
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-06 20:14:09 -0600

    Move around helper methods

--- lib/turn.rb
+++ lib/turn.rb
@@ -20,30 +20,20 @@ class Turn
     end
   end
 
+  def first_same?
+    @player1.deck.first_card == @player2.deck.first_card
+  end
+
+  def third_same?
+    @player1.deck.third_card == @player2.deck.third_card
+  end
+
   def winner
     winner =  { mutually_assured_destruction: "No Winner",
                 war: war_winner,
                 basic: basic_winner }
     winner[type]
   end
-
-  def pile_cards
-    if type == :mutually_assured_destruction
-      mad_pile
-    elsif type == :war
-      war_pile
-    elsif type == :basic
-      basic_pile
-    end
-  end
-
-  def award_spoils(winner)
-    until @spoils_of_war.empty? do
-      winner.deck.cards << @spoils_of_war.shift
-    end
-  end
-
-  # helper methods
 
   def war_winner
     winner = nil
@@ -69,12 +59,14 @@ class Turn
     winner
   end
 
-  def first_same?
-    @player1.deck.first_card == @player2.deck.first_card
-  end
-
-  def third_same?
-    @player1.deck.third_card == @player2.deck.third_card
+  def pile_cards
+    if type == :mutually_assured_destruction
+      mad_pile
+    elsif type == :war
+      war_pile
+    elsif type == :basic
+      basic_pile
+    end
   end
 
   def take_card(player)
@@ -96,6 +88,12 @@ class Turn
   def basic_pile
     @players.each do |player|
       @spoils_of_war << take_card(player)
+    end
+  end
+
+  def award_spoils(winner)
+    until @spoils_of_war.empty? do
+      winner.deck.cards << @spoils_of_war.shift
     end
   end
 
