f760a76109f616cf3f040b189bf3a0e86f88ea10
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-07 15:01:33 -0600

    Add high level comments

--- lib/start.rb
+++ lib/start.rb
@@ -3,6 +3,21 @@ require './lib/deck'
 require './lib/player'
 require './lib/turn'
 
+# does the start class only query for information?
+# does it take an argument?
+# what if it takes turn as an argument?
+# what will it do with a turn?
+
+# it can ask a turn to get retvals
+# get symbols for hash table lookup in terminal
+# get info about who won the game
+# who lost the game / out of cards
+
+# what logic happens here?
+# does this class know about turn_n ?
+# what does it mean to Start.new(Turn)
+
+
 class Start
 
   def start
--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -1,8 +1,9 @@
-require './lib/card'
-require './lib/deck'
-require './lib/player'
-require './lib/turn'
-require './lib/start'
+require './lib/card' # retvals are sym, int, str data
+require './lib/deck' # retvals are array data
+require './lib/player' # retvals are str and boolean
+require './lib/turn' # retvals sym, DOES commands ie. array manip
+require './lib/start' # SHOULD this class only query?
+                      # retvals are sym, int, str?
 
 @l1 = "Welcome to War! (or Peace) This game will be played with 52 cards."
 l2 = "The players today are #{"Megan"} and #{"Aurora"}."
@@ -19,6 +20,8 @@ puts l2
 puts l3
 dashes
 
+# Infinite loops?
+# Validate data in CL?
 
 loop do
   start = gets.chomp
@@ -57,13 +60,99 @@ proto_arr.length.times do |i|
   card_arr << Card.new(proto_arr[i]["suit"], proto_arr[i]["value"], proto_arr[i]["rank"])
 end
 
-card_arr.shuffle!
+3.times { card_arr.shuffle! }
 
 deck1 = Deck.new(card_arr[0..25])
 deck2 = Deck.new(card_arr[26..52])
 
-player1 = Player.new("Megan", deck1)
-player2 = Player.new("Aurora", deck2)
+p1 = Player.new("Megan", deck1)
+p2 = Player.new("Aurora", deck2)
 
-p "We are gonna make it fam"
+# p "We are gonna make it fam"
 dashes
+
+# WHERE IS MY INFORMATION LIVING?
+  # Where is the game being played?
+
+# WHERE DO I INSTANTIATE A NEW TURN ???
+
+# turn = Turn.new(p1, p2)
+
+# start = Start.new(turn)
+# start.method
+
+# is this abstraction?
+
+# need to know what class methods should be called in the interface
+# the methods called should only query information
+# it only returns information that is "useful"
+  # what information is SEEN in the terminal?
+  # the "turn number" aka "turn_n"
+  # different lines for different turn.type
+  # lines for if someone won OR if game ends in DRAW
+
+  # ALL OF THE GAME MECHANIC LOGIC IS ENCAPSULATED IN THE TURN CLASS ???
+
+# $ Turn 1: Megan won 2 cards
+
+  # Retrieve values from turn class to print these messages?
+
+  # Megan
+    # turn.winner returns a player object
+    # assign it to a variable
+      # winner == turn.winner
+      # should be an object
+    # winner.name ??
+
+  # won 2 cards
+    # query turn.type == ___??
+    # the retval is a symbol
+    # look it up in a hash table
+    # get a string value back
+    # print it to the screen
+
+      # OR
+
+    # query turn.spoils_of_war.size
+    # BEFORE turn.award_spoils(winner) ???
+    # it will return an Integer
+    # can interpolate into string
+    # print it to the screen
+
+# $ Turn 2: WAR - Aurora won 6 cards
+# $ Turn 3: *mutually assured destruction* 6 cards removed from play
+
+  # A hash table might be the call because the lookup is fast ??
+  # the turn.type return value is a symbol
+
+
+# loop for literally a million times?
+# 10**6 == 1_000_000
+# RUN until -- (player.has_lost? == true) OR (turn_n == 10**6)
+# result = DRAW if (turn_10**6) AND (player.has_lost == false)
+
+# Turn 9324: Aurora won 2 cards
+# *~*~*~* Aurora has won the game! *~*~*~*
+
+# Turn 1000000: Aurora won 2 cards
+# ---- DRAW ----
+
+  # WHAT does start.rb need to do?
+  # WHAT IF this is the class that passes information to the runner
+  # Because then the runner only has to deal with ONE other class?
+  # Does that mess up my require statements?
+
+  # it can get information to the runner file?
+  # it needs to return some values
+  # is this the class that can call Turn methods ie. Turn.new?
+  # does it know turn_n ?
+    # who knows what?
+
+  # Does the runner pass information to anybody?
+    # it instantiates cards, decks, and players?
+    # it PASSES ARGUMENTS to Class.new parameters
+
+    # what would the runner pass to the start.rb ??
+
+  # (LATER) the runner get.chomp to start the game?
+    # any other interactions?
