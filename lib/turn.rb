d6e45887a859f6d962d49015402f8a84c5ba5d08
Norma Lopez <norma_l304@yahoo.com>
2020-05-07 13:46:42 -0600

    Add tests for winner outcomes

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,4 +19,7 @@ class Turn
       :war
     end
   end
+
+  def winner
+  end 
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -88,7 +88,7 @@ class TurnTest < Minitest::Test
       card5 = Card.new(:heart, '8', 8)
       card6 = Card.new(:diamond, '8', 8)
       card7 = Card.new(:heart, '3', 3)
-      card8 = Card.new(:diamond, '2', 2)      
+      card8 = Card.new(:diamond, '2', 2)
       deck1 = Deck.new([card1, card2, card5, card8])
       deck2 = Deck.new([card4, card3, card6, card7])
       player1 = Player.new("Megan", deck1)
@@ -96,5 +96,59 @@ class TurnTest < Minitest::Test
       turn = Turn.new(player1, player2)
 
       assert_equal :mutually_assured_destruction, turn.type
+    end
+
+    def test_it_has_basic_winner
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:heart, '9', 9)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, 'Queen', 12)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card3, card4, card6, card7])
+      player1 = Player.new("Megan", deck1)
+      player2 = Player.new("Aurora", deck2)
+      turn = Turn.new(player1, player2)
+
+      assert_equal player1, turn.winner
+    end
+
+    def test_it_has_war_winner
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:heart, '9', 9)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, 'Queen', 12)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card4, card3, card6, card7])
+      player1 = Player.new("Megan", deck1)
+      player2 = Player.new("Aurora", deck2)
+      turn = Turn.new(player1, player2)
+
+      assert_equal player2, turn.winner
+    end
+
+    def test_it_has_no_winner_when_type_is_mutually_assured_destruction
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:heart, '9', 9)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, '8', 8)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card4, card3, card6, card7])
+      player1 = Player.new("Megan", deck1)
+      player2 = Player.new("Aurora", deck2)
+      turn = Turn.new(player1, player2)
+
+      assert_equal "No Winner", turn.winner
     end
 end
