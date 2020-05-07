f4bd6df521ef6c1f9775006d5178682f88a09fa0
Dan Sehl <dan.sehl@gmail.com>
2020-05-07 09:55:13 -0600

    Write suits, values, rank arrays

---
+++ war_or_peace_runner.rb
@@ -0,0 +1,15 @@
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+
+suits = [:heart, :diamond, :spade, :club]
+values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+ranks = (1..14).to_a
+
+# cards = Hash.new
+# suits.each do |suit|
+#   cards.store(suit,
+#                   values.each { |value| value } +
+#                   ranks.each { |rank| rank } )
+#                 end
