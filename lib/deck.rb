4c42f5f540c1df30d7b34b1d3293c26e1b00a84f
“Neeru <neeram85@gmail.com>
2020-05-07 08:50:30 -0600

    Work on deck rank tests

--- lib/deck.rb
+++ lib/deck.rb
@@ -12,11 +12,36 @@ class Deck
   def high_ranking_cards
     high_rank = []
 
-    cards.each do |card|
+    @cards.each do |card|
       if card.rank > 11
         high_rank << card
       end
     end
     high_rank
   end
+
+  # def percent_high_ranking
+  #   ranks = []
+  #   total_rank_sum = 0
+  #   high_ranks = []
+  #   high_rank_cards_sum = 0
+  #
+  #   @cards.each do |card|
+  #     ranks << card.rank
+  #   end
+  #
+  #   ranks.each do |rank|
+  #     total_rank_sum += rank
+  #   end
+  #
+  #   @cards.each do |card|
+  #     if card.rank > 11
+  #       high_ranks << card.rank
+  #     end
+  #   end
+  #
+  #     high_ranks.each do |high_rank|
+  #       high_rank_cards_sum += high_rank
+  #     end
+  # end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,14 +5,6 @@ require './lib/card'
 require 'pry'
 
 class DeckTest < Minitest::Test
-  # def setup
-  #   card1 = Card.new(:diamond, 'Queen', 12)
-  #   card2 = Card.new(:spade, '3', 3)
-  #   card3 = Card.new(:heart, 'Ace', 14)
-  #   cards = [card1, card2, card3]
-  #
-  # end
-
   def test_it_exists
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
@@ -45,9 +37,18 @@ class DeckTest < Minitest::Test
     assert_equal [card1, card3], deck.high_ranking_cards
   end
 
-#
-# test deck percent of high ranking cards
-#
+  def test_it_has_percent_high_ranked_cards
+    skip
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal 50.0, deck.percent_high_ranking
+  end
+
+
 # test deck can remove cards
 #
 # test deck can add cards
