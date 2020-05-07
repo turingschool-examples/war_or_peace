52cf14f7456586be67c611746a5ce9b6fe6bf774
“Neeru <neeram85@gmail.com>
2020-05-06 19:29:13 -0600

    Added deck tests and methods

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,7 +1,22 @@
 class Deck
   attr_reader :cards
 
-  def initialize(cards)
-    @cards = []
+  def initialize(cards = [])
+    @cards = cards
+  end
+
+  def rank_of_card_at(num_param)
+    @cards[num_param].rank
+  end
+
+  def high_ranking_cards
+    high_rank = []
+
+    cards.each do |card|
+      if card.rank > 11
+        high_rank << card
+      end
+    end
+    high_rank
   end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,6 +5,51 @@ require './lib/card'
 require 'pry'
 
 class DeckTest < Minitest::Test
+  # def setup
+  #   card1 = Card.new(:diamond, 'Queen', 12)
+  #   card2 = Card.new(:spade, '3', 3)
+  #   card3 = Card.new(:heart, 'Ace', 14)
+  #   cards = [card1, card2, card3]
+  #
+  # end
+
   def test_it_exists
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_instance_of Deck, deck
   end
+
+  def test_if_card_has_rank
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal 12, deck.rank_of_card_at(0)
+    assert_equal 3, deck.rank_of_card_at(1)
+    assert_equal 14, deck.rank_of_card_at(2)
+  end
+
+  def test_if_card_has_high_rank
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal [card1, card3], deck.high_ranking_cards
+  end
+
+#
+# test deck percent of high ranking cards
+#
+# test deck can remove cards
+#
+# test deck can add cards
+
 end
