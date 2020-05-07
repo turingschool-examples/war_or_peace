45d808035700f9979fcbe8414a09af185ab28eeb
“Neeru <neeram85@gmail.com>
2020-05-07 14:32:51 -0600

    Added add_card method to deck class

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -37,7 +37,7 @@ class DeckTest < Minitest::Test
     assert_equal [card1, card3], deck.high_ranking_cards
   end
 
-  def test_it_has_percent_high_ranked_cards
+  def test_it_can_calculate_percentage_of_high_ranked_cards
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
@@ -47,8 +47,32 @@ class DeckTest < Minitest::Test
     assert_equal 66.67, deck.percent_high_ranking
   end
 
+  def test_it_can_remove_a_card
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
 
-# test deck can remove cards
-# test deck can add cards
+    assert_equal [card2, card3], deck.remove_card
+  end
+
+
+
+
+  def test_it_can_add_a_card
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    card4 = Card.new(:club, '5', 5)
+
+    assert_equal [card1, card2, card3, card4], deck.add_card(card4)
+  end
+
+
+
+
 
 end
