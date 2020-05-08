b616e713a3f97979a1d7e80f4dc9e14ba791a200
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-07 22:09:12 -0600

    Add start method to play game

--- lib/start.rb
+++ lib/start.rb
@@ -17,11 +17,31 @@ require './lib/turn'
 # does this class know about turn_n ?
 # what does it mean to Start.new(Turn)
 
+# The start class can handle generating the cards and such?
 
 class Start
+  attr_reader :turn
 
-  def start
-    p "Hello World"
+  def initialize(turn)
+    @turn = turn
+    start
   end
 
+  def start
+    until @turn.players.any? { |player| player.has_lost? == true }
+      if @turn.winner.class == String
+        p @turn.winner
+      else
+        p @turn.winner.name
+      end
+      @turn.pile_cards
+      @turn.award_spoils(@turn.winner)
+    end
+  end
+
+
 end
+
+# what does Start.new(turn) return?
+# it returns a turn obj instance
+#
