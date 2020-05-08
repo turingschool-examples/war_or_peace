69465973a7287d29d1aec4c4dbc4f550b04684e0
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-07 19:10:11 -0600

    Expect object data in assertion

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -198,7 +198,7 @@ class TurnTest < Minitest::Test
     turn = Turn.new(player1, player2)
 
     assert_equal :basic, turn.type
-    assert_equal "Megan", turn.winner
+    assert_equal player1, turn.winner
 
     winner = turn.winner
 
@@ -221,14 +221,13 @@ class TurnTest < Minitest::Test
   end
 
   def test_it_awards_spoils_to_the_war_winner
-    skip
     player1 = Player.new("Megan", @war_deck_1)
     player2 = Player.new("Aurora", @war_deck_2)
 
     turn = Turn.new(player1, player2)
 
     assert_equal :war, turn.type
-    assert_equal "Aurora", turn.winner
+    assert_equal player2, turn.winner
 
     winner = turn.winner
 
