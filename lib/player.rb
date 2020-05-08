8b8c8539fbca8b25cd395e59fb0f89b9e84a06b6
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-07 23:57:28 -0600

    Add method to tell player they have lost

--- lib/player.rb
+++ lib/player.rb
@@ -4,10 +4,15 @@ class Player
   def initialize(name, deck)
     @name = name
     @deck = deck
+    @lost = false
   end
 
   def has_lost?
-    @deck.cards.empty?
+    @deck.cards.empty? || @lost
+  end
+
+  def you_lose
+    @lost = true
   end
 
 end
