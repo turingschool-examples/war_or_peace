c2bb17b1ae1cfe2422b6f165653dd6bfc0c274d1
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-05-07 21:10:46 -0600

    Thursday night commit

---
+++ lib/turn.rb
@@ -0,0 +1,24 @@
+class Turn
+
+attr_reader :player1, :player2
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+  end
+
+  def spoils_of_war
+    []
+  end
+
+  def type
+    if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
+      :basic
+    end
+  end
+
+  def winner
+    
+  end
+
+end
---
+++ test/turn_test.rb
@@ -0,0 +1,44 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+require 'pry'
+
+class DeckTest < Minitest::Test
+
+  def setup
+    @card1 = Card.new(:heart, 'Jack', 11)
+    @card2 = Card.new(:heart, '10', 10)
+    @card3 = Card.new(:heart, '9', 9)
+    @card4 = Card.new(:diamond, 'Jack', 11)
+    @card5 = Card.new(:heart, '8', 8)
+    @card6 = Card.new(:diamond, 'Queen', 12)
+    @card7 = Card.new(:heart, '3', 3)
+    @card8 = Card.new(:diamond, '2', 2)
+    @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+    @player1 = Player.new("Megan", @deck1)
+    @player2 = Player.new("Aurora", @deck2)
+    @turn = Turn.new(@player1, @player2)
+  end
+
+  def test_player_names
+    # binding.pry
+    assert_equal @turn.player1.name, @player1.name
+    assert_equal @turn.player2.name, @player2.name
+  end
+
+  def test_spoils
+    # binding.pry
+    assert_equal [], @turn.spoils_of_war
+  end
+
+  def test_type
+    assert_equal :basic, @turn.type
+  end
+
+  
+
+end
