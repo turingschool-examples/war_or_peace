7e0db53567bfac13feb4cb815994dab2cd88fbee
Arique Aguilar <arique1104@gmail.com>
2020-05-08 00:06:30 -0500

    Reduction of additional classes added for testing purposes.  Now honoring S principle

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,18 +1,3 @@
-class Card
-attr_reader :suit, :value, :rank
-  def initialize(suit, value, rank)
-    @suit = suit
-    @value = value
-    @rank = rank
-  end
-end
-
-card1 = Card.new(:diamond, 'Queen', 12)
-card2 = Card.new(:spade, '3', 3)
-card3 = Card.new(:heart, 'Ace', 14)
-card4 = Card.new(:club, '5', 5)
-cards = [card1, card2, card3]
-
 class Deck
   attr_reader :cards
 
@@ -57,5 +42,3 @@ class Deck
 end
 
 deck = Deck.new(cards)
-
-require 'pry'; binding.pry
