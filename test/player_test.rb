2df1decd650b63b2dc30e99a572eb25fcbd65519
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-09 00:47:35 -0600

    Add test for counting player hand

--- test/player_test.rb
+++ test/player_test.rb
@@ -14,44 +14,37 @@ class PlayerTest < Minitest::Test
     @deck = Deck.new([@card1, @card2, @card3])
 
     @player = Player.new("Clarisa", @deck)
-
   end
 
   def test_it_exists
-    player = @player
-
-    assert_instance_of Player, player
+    assert_instance_of Player, @player
   end
 
   def test_it_has_a_name
-    player = @player
-
-    assert_equal "Clarisa", player.name
+    assert_equal "Clarisa", @player.name
   end
 
   def test_it_has_a_deck
-    player = @player
-
-    assert_equal @deck, player.deck
+    assert_equal @deck, @player.deck
   end
 
 
   def test_it_knows_if_lost
-    player = @player
-
-    refute player.has_lost?
+    refute @player.has_lost?
   end
 
   def test_it_has_lost_when_deck_is_empty
-    player = @player
-
     3.times do
-      player.deck.remove_card
+      @player.deck.remove_card
     end
 
-    assert_empty player.deck.cards
+    assert_empty @player.deck.cards
 
-    assert player.has_lost?
+    assert @player.has_lost?
+  end
+
+  def test_it_knows_how_many_cards_in_hand
+    assert_equal 3, @player.hand
   end
 
 end
