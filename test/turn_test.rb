a2bffbd870eee68bf68394b73874fb076a787785
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-05-09 13:35:12 -0600

    add test for mutual destuction

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -71,7 +71,7 @@ require "minitest/pride"
 
      assert_equal [], turn.spoils_of_war
    end
-   def test_it_has_a_type
+   def test_it_has_a_basic_type
      card1 = Card.new(:heart, 'Jack', 11)
      card2 = Card.new(:heart, '10', 10)
      card3 = Card.new(:heart, '9', 9)
@@ -134,6 +134,73 @@ require "minitest/pride"
 
      assert_equal 2, turn.spoils_of_war.size
 
+   end
+   def test_it_has_a_war_type
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:heart, '9', 9)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, 'Queen', 12)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+
+      deck1 = Deck.new([card1, card2, card5, card8])
+
+      deck2 = Deck.new([card4, card3, card6, card7])
+
+      player1 = Player.new("Megan", deck1)
+
+      player2 = Player.new("Aurora", deck2)
+
+      turn = Turn.new(player1, player2)
+
+      assert_equal :war , turn.type
+
+   end
+   def test_it_has_a_mutually_assured_destruction_type
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:heart, '9', 9)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, '8', 8)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+
+      deck1 = Deck.new([card1, card2, card5, card8])
+
+      deck2 = Deck.new([card4, card3, card6, card7])
+
+      player1 = Player.new("Megan", deck1)
+
+      player2 = Player.new("Aurora", deck2)
+
+      turn = Turn.new(player1, player2)
+
+      assert_equal :mutually_assured_destruction , turn.type
+   end
+   def test_it_has_winner_at_mutually_assured_destruction_type
+     card1 = Card.new(:heart, 'Jack', 11)
+     card2 = Card.new(:heart, '10', 10)
+     card3 = Card.new(:heart, '9', 9)
+     card4 = Card.new(:diamond, 'Jack', 11)
+     card5 = Card.new(:heart, '8', 8)
+     card6 = Card.new(:diamond, '8', 8)
+     card7 = Card.new(:heart, '3', 3)
+     card8 = Card.new(:diamond, '2', 2)
+
+     deck1 = Deck.new([card1, card2, card5, card8])
+
+     deck2 = Deck.new([card4, card3, card6, card7])
+
+     player1 = Player.new("Megan", deck1)
+
+     player2 = Player.new("Aurora", deck2)
+
+     turn = Turn.new(player1, player2)
+
+     assert_equal "No winner", turn.winner
    end
  end
 
