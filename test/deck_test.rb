fc2297c913d2dfc724512fc25bfdb9538dfba561
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-05-09 00:07:15 -0600

    test if you can remove card

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -58,29 +58,36 @@ class DeckTest < Minitest::Test
 
 
   end
+  def test_it_has_high_ranking_percentage
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+    deck.high_ranking_cards
+    assert_equal 66.67, deck.percent_high_ranking
+
+  end
+
+  def test_it_can_remove_card
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+    deck.remove_card
+
+    assert_equal 2 , deck.cards.size
+    assert_equal 1 , deck.high_ranking_cards.size
+    assert_equal 50.0, deck.percent_high_ranking
+    assert_equal :spade , deck.cards[0].suit
+  end
 end
 
-# pry(main)> deck.cards
-# #=> [#<Card:0x007fbfd18490e8...>, #<Card:0x007fbfd19f4fa0...>, #<Card:0x007fbfd18555a0...>]
-#
-# pry(main)> deck.high_ranking_cards
-# #=> [#<Card:0x007fbfd18490e8...>, #<Card:0x007fbfd18555a0...>]
-#
-# pry(main)> deck.percent_high_ranking
-# #=> 66.67
-#
-# pry(main)> deck.remove_card
-# #=> #<Card:0x007fbfd18490e8 @rank=12, @suit=:diamond, @value="Queen">
-#
-# pry(main)> deck.cards
-# #=> [#<Card:0x007fbfd19f4fa0...>, #<Card:0x007fbfd18555a0...>]
-#
-# pry(main)> deck.high_ranking_cards
-# #=> [#<Card:0x007fbfd18555a0...>]
-#
-# pry(main)> deck.percent_high_ranking
-# #=> 50.0
-#
 # pry(main)> card4 = Card.new(:club, '5', 5)
 # #=> #<Card:0x007fbfd2963978 @rank=5, @suit=:club, @value="5">
 #
