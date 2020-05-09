acc3f30fffd3d048b0c55dcb7542cdd0330e2a9f
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-05-09 06:47:39 -0600

    Test if player exist

--- test/player_test.rb
+++ test/player_test.rb
@@ -31,29 +31,26 @@ class PlayerTest < Minitest::Test
     assert_equal 'Clarisa', player.name
   end
 
+  def test_it_has_a_deck
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    player = Player.new('Clarisa', deck)
+
+    assert_equal 3, player.deck.cards.size
+    assert_instance_of Deck, player.deck
+  end
+  def test_it_has_lost
+
+  end
+
 
 end
-# pry(main)> card1 = Card.new(:diamond, 'Queen', 12)
-# #=> #<Card:0x007f9cc3a73a98 @rank=12, @suit=:diamond, @value="Queen">
-#
-# pry(main)> card2 = Card.new(:spade, '3', 3)
-# #=> #<Card:0x007f9cc3a03720 @rank=3, @suit=:spade, @value="3">
-#
-# pry(main)> card3 = Card.new(:heart, 'Ace', 14)
-# #=> #<Card:0x007f9cc3a44c98 @rank=14, @suit=:heart, @value="Ace">
-#
-# pry(main)> deck = Deck.new([card1, card2, card3])
-# #=> #<Deck:0x007f9cc396bdf8 @cards=[#<Card:0x007f9cc3a73a98...>, #<Card:0x007f9cc3a03720...>, #<Card:0x007f9cc3a44c98...>]>
-#
-# pry(main)> player = Player.new('Clarisa', deck)
-# #=> #<Player:0x007f9cc3b4c988 @deck=#<Deck:0x007f9cc396bdf8 @cards=[#<Card:0x007f9cc3a73a98...>, #<Card:0x007f9cc3a03720...>, #<Card:0x007f9cc3a44c98...>]>, @name="Clarisa">
-#
-# pry(main)> player.name
-# #=> "Clarisa"
-#
-# pry(main)> player.deck
-# #=> #<Deck:0x007f9cc396bdf8 @cards=[#<Card:0x007f9cc3a73a98...>, #<Card:0x007f9cc3a03720...>, #<Card:0x007f9cc3a44c98...>]>
-#
 # pry(main)> player.has_lost?
 # #=> false
 #
