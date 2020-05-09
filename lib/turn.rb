db35eed3b64d89d8c67a75f0d6a8c3b183d344ff
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-09 15:22:38 -0600

    Fix loser logic errors

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,6 +12,8 @@ class Turn
     @players = [p1, p2]
     @counter = 0
     @limit = 10**6
+    @winner = nil
+    @turn_winner = nil
   end
 
   def type
@@ -29,9 +31,18 @@ class Turn
   end
 
   def same_third_rank?
+    # if not_enough_cards?
+    # end
     player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
   end
 
+  def not_enough_cards?
+    @players.any? do |player|
+      player.short?
+    end
+  end
+
+  # a really weird getter/setter method
   def winner
     win_hash = {  basic: basic_winner,
                   war: war_winner,
@@ -40,13 +51,27 @@ class Turn
     win_hash[type]
   end
 
+  def player_lost?
+    @players.any? do |player|
+      player.has_lost?
+    end
+  end
+
+  def loser
+    @players.select do |player|
+      player.has_lost?
+    end
+  end
+
   def basic_winner
+    @players.detect { |player| player.you_lose if player.nil_check(0) }
     @players.max_by do |player|
       player.deck.rank_of_card_at(0)
     end
   end
 
   def war_winner
+    @players.detect { |player| player.you_lose if player.nil_check(2) }
     @players.max_by do |player|
       player.deck.rank_of_card_at(2)
     end
@@ -55,15 +80,31 @@ class Turn
   def pile_cards
     if type == :mutually_assured_destruction
       mad_exile
+      if @turn_winner == nil
+        "It's a tie."
+      else
+        "*mutually assured destruction* 6 cards removed from play"
+      end
     elsif type == :war
       war_pile
+      if @turn_winner == nil
+        "It's a tie."
+      else
+        "WAR - #{@turn_winner.name} won #{@spoils_of_war.size} cards"
+      end
     elsif type == :basic
       basic_pile
+      if @turn_winner == nil
+        "It's a tie."
+      else
+        "#{@turn_winner.name} won #{@spoils_of_war.size} cards"
+      end
     end
   end
 
   def basic_pile
     @players.each do |player|
+      break if player.has_lost?
       @spoils_of_war << player.deck.remove_card
     end
   end
@@ -74,23 +115,43 @@ class Turn
 
   def mad_exile
     3.times do
-      @players.each do |player|
-          @exile << player.deck.remove_card
+      @players.each do |player, i|
+        break if player.has_lost?
+        @exile << player.deck.remove_card
       end
     end
   end
 
   def award_spoils(turn_winner)
-    @spoils_of_war.shuffle!
-    @spoils_of_war.each do |spoil|
-      turn_winner.hand << spoil
+    unless @spoils_of_war.empty?
+      @spoils_of_war.shuffle!
+      @spoils_of_war.each do |spoil|
+        turn_winner.hand << spoil
+      end
+      @spoils_of_war = []
     end
-    @spoils_of_war = []
   end
 
+ # for runner file
+
   def count
-    @limit.times do
-      @counter += 1
+    @counter += 1
+  end
+
+
+  def game_over?
+    @counter == @limit || player_lost?
+  end
+
+  def start
+  until game_over? do
+    @turn_winner = winner
+    p "Turn #{count}: #{pile_cards}"
+    award_spoils(@turn_winner) unless @turn_winner == "No Winner"
+      if game_over?
+        p "GG #{@turn_winner.name}"
+        break
+      end
     end
   end
 
