c528cc746e7b2ed635b4c60ca70798ad015e3c66
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-05-09 16:57:29 -0600

    test for awarding spoils and reset the spoils

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -210,16 +210,40 @@ require "minitest/pride"
 
      assert_equal "No winner", turn.winner
    end
+
+   def test_it_award_spoils_basic
+     card1 = Card.new(:heart, 'Jack', 11)
+     card2 = Card.new(:heart, '10', 10)
+     card3 = Card.new(:heart, '9', 9)
+     card4 = Card.new(:diamond, 'Jack', 11)
+     card5 = Card.new(:heart, '8', 8)
+     card6 = Card.new(:diamond, 'Queen', 12)
+     card7 = Card.new(:heart, '3', 3)
+     card8 = Card.new(:diamond, '2', 2)
+
+     deck1 = Deck.new([card1, card2, card5, card8])
+     deck2 = Deck.new([card3, card4, card6, card7])
+
+     player1 = Player.new("Megan", deck1)
+     player2 = Player.new("Aurora", deck2)
+
+     turn = Turn.new(player1, player2)
+
+     winner = turn.winner
+     turn.pile_cards
+
+     assert_equal 2, turn.spoils_of_war.size
+
+
+     assert_equal 5 , turn.award_spoils(winner).size
+     assert_equal [], turn.spoils_of_war
+
+   end
  end
 
-# pry(main)> turn.pile_cards
-#
-# pry(main)> turn.spoils_of_war
-# #=> [#<Card:0x007fa3edaa0df0 @rank=11, @suit=:heart, @value="Jack">, #<Card:0x007fa3ed98d9b8 @rank=9, @suit=:heart, @value="9">]
-#
-# pry(main)> turn.award_spoils(winner)
-#
-# pry(main)> player1.deck
-# #=> #<Deck:0x007fa3eda472c8 @cards=[#<Card:0x007fa3eda519a8...>, #<Card:0x007fa3edb263d8...>, #<Card:0x007fa3eda89308...>, #<Card:0x007fa3edaa0df0...>, #<Card:0x007fa3ed98d9b8...>]>
-# pry(main)> player2.deck
-# #=> #<Deck:0x007fa3ee11ee48 @cards=[#<Card:0x007fa3ee14ef80...>, #<Card:0x007fa3eda3e1f0...>, #<Card:0x007fa3edad1cc0...>]>
+ # pry(main)> turn.award_spoils(winner)
+ #
+ # pry(main)> player1.deck
+ # #=> #<Deck:0x007fa3eda472c8 @cards=[#<Card:0x007fa3eda519a8...>, #<Card:0x007fa3edb263d8...>, #<Card:0x007fa3eda89308...>, #<Card:0x007fa3edaa0df0...>, #<Card:0x007fa3ed98d9b8...>]>
+ # pry(main)> player2.deck
+ # #=> #<Deck:0x007fa3ee11ee48 @cards=[#<Card:0x007fa3ee14ef80...>, #<Card:0x007fa3eda3e1f0...>, #<Card:0x007fa3edad1cc0...>]>
