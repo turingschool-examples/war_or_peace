e044c7f6c540c684dc0695e5abc0ec3da9cc6b89
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-05-09 00:07:39 -0600

    shift to remove front card

--- lib/deck.rb
+++ lib/deck.rb
@@ -7,8 +7,13 @@ class Deck
   def rank_of_card_at(index)
     @cards[index].rank
   end
-
   def high_ranking_cards
     @cards.select{|card| card.rank > 11}
+  end
+  def percent_high_ranking
+    (self.high_ranking_cards.size.to_f / @cards.size * 100).round(2)
+  end
+  def remove_card
+    @cards.shift
   end
 end
