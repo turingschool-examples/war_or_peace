6124949bf56b9926b1eff2ba70ab6c1137bbe948
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-05-09 06:58:17 -0600

    Test if player has lost the game

--- test/player_test.rb
+++ test/player_test.rb
@@ -46,31 +46,24 @@ class PlayerTest < Minitest::Test
     assert_instance_of Deck, player.deck
   end
   def test_it_has_lost
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
 
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    player = Player.new('Clarisa', deck)
+
+    assert_equal false , player.lost?
+    player.deck.remove_card
+    assert_equal false , player.lost?
+    player.deck.remove_card
+    assert_equal false , player.lost?
+    player.deck.remove_card
+    assert_equal true , player.lost?
+
+    assert_equal [], player.deck.cards
   end
-
-
 end
-# pry(main)> player.has_lost?
-# #=> false
-#
-# pry(main)> player.deck.remove_card
-# #=> #<Card:0x007f9cc3a73a98 @rank=12, @suit=:diamond, @value="Queen">
-#
-# pry(main)> player.has_lost?
-# #=> false
-#
-# pry(main)> player.deck.remove_card
-# #=> #<Card:0x007f9cc3a03720 @rank=3, @suit=:spade, @value="3">
-#
-# pry(main)> player.has_lost?
-# #=> false
-#
-# pry(main)> player.deck.remove_card
-# #=> #<Card:0x007f9cc3a44c98 @rank=14, @suit=:heart, @value="Ace">
-#
-# pry(main)> player.has_lost?
-# #=> true
-#
-# pry(main)> player.deck
-# #=> #<Deck:0x007f9cc396bdf8 @cards=[]>
