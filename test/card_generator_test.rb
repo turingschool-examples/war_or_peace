189fd10a6bf50a60bb2eccc19f03828cde58a075
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-09 23:34:43 -0600

    Add tests for starting attributes

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -17,9 +17,17 @@ class CardGeneratorTest < Minitest::Test
     assert_equal "hello", @card_generator.hi
   end
 
-  def test_it_has_an_array
+  def test_it_starts_with_an_empty_array
     assert_instance_of Array, @card_generator.arr
     assert_empty @card_generator.arr
+  end
+
+  def test_it_has_an_integer_number
+    assert_instance_of Integer, @card_generator.i
+  end
+
+  def test_it_starts_at_number_2
+    assert_equal 2, @card_generator.i
   end
 
 end
