f71b7c9e0106263916b2f9d78d6e876f205e516f
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-10 11:47:25 -0600

    Set up two separate decks; not yet randomized

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -12,8 +12,7 @@ require './lib/turn'
 @cards = [@card1, @card2, @card3]
 @deck = Deck.new(@cards)
 
-cards = []
-standard_deck = Deck.new(cards)
+standard_deck_cards = []
 rank_and_values = {
   '2': 2,
   '3': 3,
@@ -31,17 +30,24 @@ rank_and_values = {
 }
 
 rank_and_values.map do |rank, value|
-  standard_deck.add_card(Card.new(:heart, rank, value))
+  standard_deck_cards << Card.new(:heart, rank, value)
 end
 
 rank_and_values.map do |rank, value|
-  standard_deck.add_card(Card.new(:spade, rank, value))
+  standard_deck_cards << Card.new(:spade, rank, value)
 end
 
 rank_and_values.map do |rank, value|
-  standard_deck.add_card(Card.new(:diamond, rank, value))
+  standard_deck_cards << Card.new(:diamond, rank, value)
 end
 
 rank_and_values.map do |rank, value|
-  standard_deck.add_card(Card.new(:club, rank, value))
+  standard_deck_cards << Card.new(:club, rank, value)
 end
+
+half_decks = standard_deck_cards.each_slice((standard_deck_cards.size/2)).to_a
+deck1 = Deck.new(half_decks[0])
+deck2 = Deck.new(half_decks[1])
+
+
+require "pry"; binding.pry
