319e9b0d7fe03b2db6879cf6691d590dd41bce8d
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-10 12:15:31 -0600

    Add test for finding last or nil card ranks

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -40,11 +40,27 @@ class DeckTest < Minitest::Test
     assert_equal 14, deck.rank_of_card_at(2)
   end
 
-  def test_it_gives_rank_zero_when_index_not_found
+  def test_it_gives_rank_of_the_last_card_in_array_when_index_not_found
     deck = @deck
 
-    assert_equal 0, deck.rank_of_card_at(3)
+    assert_nil deck.cards[3]
+    assert_equal 14, deck.rank_of_card_at(3)
   end
+
+  def test_it_gives_rank_of_the_last_card_when_index_not_found
+    deck = Deck.new([@card1])
+
+    assert_equal 1, deck.cards.length
+    assert_equal true, deck.last_card?
+
+    deck.remove_card
+
+    assert_empty deck.cards
+    assert_nil deck.cards[0]
+
+    assert_equal "LAST CARD NOT FOUND", deck.rank_of_card_at(0)
+  end
+
 
   def test_it_knows_faces_and_aces
     deck = @deck
