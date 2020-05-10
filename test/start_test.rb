b3d5c1a92bb50ff5f4984be3d67e73012e883f3d
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-10 15:14:47 -0600

    Add requirements to Start class test file

---
+++ test/start_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+require './lib/start'
+
+class StartTest < Minitest::Test
+  def method_name
+
+  end
+end
