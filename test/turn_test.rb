cad5b8932030cc265bfabafde3769425819439c2
Judith Pillado <judith.pillado@gmail.com>
2020-05-09 21:05:17 -0500

    Add turn class

---
+++ test/turn_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+
+class TurnTest < Minitest::Test
+
+end
