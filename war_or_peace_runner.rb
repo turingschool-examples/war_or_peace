2edcd1356967cd8366e3e471913630d274f99336
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-10 15:07:44 -0600

    Add shuffling capability to decks

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -3,9 +3,6 @@ require './lib/deck'
 require './lib/player'
 require './lib/turn'
 
-
-
-
 @card1 = Card.new(:diamond, 'Queen', 12)
 @card2 = Card.new(:spade, '3', 3)
 @card3 = Card.new(:heart, 'Ace', 14)
@@ -45,9 +42,8 @@ rank_and_values.map do |rank, value|
   standard_deck_cards << Card.new(:club, rank, value)
 end
 
-half_decks = standard_deck_cards.each_slice((standard_deck_cards.size/2)).to_a
+shuffled_standard_deck_cards = standard_deck_cards.shuffle
+
+half_decks = standard_deck_cards.shuffle.each_slice((standard_deck_cards.size/2)).to_a
 deck1 = Deck.new(half_decks[0])
 deck2 = Deck.new(half_decks[1])
-
-
-require "pry"; binding.pry
