13cd3d597acddea8eb9f635a68483783ae8a96a4
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-09 23:48:51 -0600

    Add tests for moving number

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -30,4 +30,21 @@ class CardGeneratorTest < Minitest::Test
     assert_equal 0, @card_generator.i
   end
 
+  def test_it_can_have_a_different_number
+    diff = CardGenerator.new(2)
+    assert_equal 2, diff.i
+  end
+
+  def test_it_can_put_that_number_in_the_array
+    
+    assert_empty @card_generator.arr
+    @card_generator.move_it
+    assert_equal [0], @card_generator.arr
+
+    diff = CardGenerator.new(2)
+    diff.move_it
+    assert_equal [2], diff.arr
+
+  end
+
 end
