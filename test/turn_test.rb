b40e5ed88b1d7623ddd4ee6ab84b1029c0d8a27a
Judith Pillado <judith.pillado@gmail.com>
2020-05-09 21:26:27 -0500

    Add spoils of war

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -30,6 +30,10 @@ class TurnTest < Minitest::Test
   def test_it_has_readable_attributes
     assert_equal @player1, @turn.player1
     assert_equal @player2, @turn.player2
-  end 
+  end
+
+  def test_spoils_of_war_is_empty
+    assert_equal [], @turn.spoils_of_war
+  end
 
 end
