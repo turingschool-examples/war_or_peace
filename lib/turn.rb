a5480c3d2a509e6e8a43cf4660ff97df07d9ea7d
Judith Pillado <judith.pillado@gmail.com>
2020-05-09 23:05:11 -0500

    Add type method

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,4 +7,14 @@ class Turn
     @spoils_of_war = []
   end
 
+  def type
+    if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
+      return :basic
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
+      return :war
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
+      return :mutually_assured_destruction
+    end
+  end
+
 end
