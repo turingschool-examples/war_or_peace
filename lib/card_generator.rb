74192cb28a721f36f01f0cb3e3b83b3e0294a801
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-10 00:28:45 -0600

    Add move it method

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,10 +1,10 @@
 class CardGenerator
-  attr_reader :arr, :i
+  attr_reader :suits_arr, :i
 
 
-  def initialize(i=0)
-    @arr = []
-    @i = i
+  def initialize(i)
+    @i = i #gets.chomp
+    @suits_arr = []
   end
 
   def hi
@@ -12,7 +12,15 @@ class CardGenerator
   end
 
   def move_it
-    @arr << @i
+    @suits_arr << @i
   end
 
 end
+
+
+# puts "Hello. Let's build a deck of cards.
+# puts "This program will generate a text file."
+# puts "Continue? [Y]es / [N]o"
+# puts "Pick a deck style: [S]tandard / [T]arot
+# puts "What do you want to name this file?"
+# puts "Generated #{style} deck.... at #{name}.txt"
