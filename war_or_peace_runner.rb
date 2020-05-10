f7ba02b216143da9139ff725f9a42f11a573cd3e
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-10 10:50:56 -0600

    Create standard card deck

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -1,1 +1,38 @@
-standard
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+
+# suits = [:heart, :spade, :diamond, :club]
+standard_deck = []
+rank_and_values = {
+  '2': 2,
+  '3': 3,
+  '4': 4,
+  '5': 5,
+  '6': 6,
+  '7': 7,
+  '8': 8,
+  '9': 9,
+  '10': 10,
+  'jack': 11,
+  'queen': 12,
+  'king': 13,
+  'ace': 14
+}
+
+rank_and_values.map do |rank, value|
+  standard_deck << Card.new(:heart, rank, value)
+end
+
+rank_and_values.map do |rank, value|
+  standard_deck << Card.new(:spade, rank, value)
+end
+
+rank_and_values.map do |rank, value|
+  standard_deck << Card.new(:diamond, rank, value)
+end
+
+rank_and_values.map do |rank, value|
+  standard_deck << Card.new(:club, rank, value)
+end
