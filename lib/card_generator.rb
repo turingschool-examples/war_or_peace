a916d7cf75b86c78a563d99858ebdda5dea3f7ce
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-10 13:52:34 -0600

    Pass style tests for card gen class

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,14 +1,84 @@
 class CardGenerator
-  attr_reader :suits, :i, :royal, :numbers, :pecking_order
-
+  attr_reader :suits,
+              :i,
+              :royal,
+              :numbers,
+              :pecking_order,
+              :style
 
   def initialize(i)
+
+    # start
     @i = i #gets.chomp
     @suits = []
     @royal = ["Jack", "Queen", "King"]
     @numbers = str_arr
-    # puts "Pick a deck style: [S]tandard / [T]arot Minor Arcana
     @pecking_order = nil # gets.chomp
+    @style = nil
+    # get_style
+    # # do it
+    # print_msg
+  end
+
+  def print_msg
+    puts "Generating '#{@style}_deck.txt'"
+    puts "...."
+    puts "Success!"
+  end
+
+  def get_style
+    puts "Pick a deck style: [S]tandard / [T]arot Minor Arcana"
+    invalid_counter = 0
+
+    loop do
+      style = gets.chomp
+      if style == "S"
+        standardize
+        break
+      elsif style == "T"
+        tarotize
+        break
+      else
+        puts "Pick a deck style: [S]tandard / [T]arot Minor Arcana"
+        invalid_counter += 1
+        if invalid_counter == 3
+          puts "Goodbye"
+          exit
+        end
+      end
+    end
+
+
+  end
+
+  def start
+
+    puts "Hello. Let's build a deck of cards."
+    puts "This program will generate a text file."
+    puts "Continue? [Y]es / [N]o"
+    invalid_counter = 0
+
+    loop do
+
+      p = gets.chomp
+
+      if p == "Y"
+        puts "OK"
+        break
+      elsif p == "N"
+        puts "Goodbye"
+        exit
+      else
+        puts "Invalid input. Continue? [Y]es / [N]o"
+        invalid_counter += 1
+        if invalid_counter == 3
+          puts "Goodbye"
+          exit
+        end
+      end
+
+    end
+
   end
 
   def hi
@@ -22,6 +92,7 @@ class CardGenerator
   def standardize
     @suits = standard_suits
     @pecking_order = standard_order
+    @style = "Standardized"
   end
 
   def standard_suits
@@ -32,6 +103,7 @@ class CardGenerator
     @suits = tarot_suits
     @royal.unshift "Knight"
     @pecking_order = tarot_order
+    @style = "Tarotized"
   end
 
   def tarot_suits
@@ -73,11 +145,3 @@ class CardGenerator
   end
 
 end
-
-
-# puts "Hello. Let's build a deck of cards.
-# puts "This program will generate a text file."
-# puts "Continue? [Y]es / [N]o"
-# puts "Pick a deck style: [S]tandard / [T]arot Minor Arcana
-# puts "What do you want to name this file?"
-# puts "Generated #{style} deck.... at #{name}.txt"
