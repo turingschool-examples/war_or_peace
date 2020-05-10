9c963dc5744d3b33e408ea4a4f630be73e172a1f
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-09 18:10:17 -0600

    Move start methods to own class

--- lib/start.rb
+++ lib/start.rb
@@ -3,61 +3,29 @@ require './lib/deck'
 require './lib/player'
 require './lib/turn'
 
-# does the start class only query for information?
-# does it take an argument?
-# what if it takes turn as an argument?
-# what will it do with a turn?
-
-# it can ask a turn to get retvals
-# get symbols for hash table lookup in terminal
-# get info about who won the game
-# who lost the game / out of cards
-
-# what logic happens here?
-# does this class know about turn_n ?
-# what does it mean to Start.new(Turn)
-
-# The start class can handle generating the cards and such?
-
 class Start
   attr_reader :turn
 
   def initialize(turn)
     @turn = turn
-    @counter = 0
-    @winner = nil
-    start
+    play
   end
 
-  def start
-    until @counter == 10**6 || someone_lost?
-      @counter += 1
-      if @turn.winner.class == String
-        @winner = @turn.winner
-      else
-        @winner = @turn.winner.name
-      end
-      puts "Turn #{@counter}: #{@winner}."
-      # puts "She has #{@turn.winner.deck.cards.size} cards." unless @winner == "No Winner"
-      @turn.pile_cards
-      @turn.award_spoils(@turn.winner)
-      if someone_lost?
-        puts "SOMEONE LOST - END GAME"
+  def play
+    until turn.game_over? do
+      final_winner = turn.winner
+      p "Turn #{turn.count}: #{turn.pile_cards}"
+      award_spoils(final_winner) unless final_winner == "No Winner"
+      # p "Turn #{count}"
+        if turn.game_over?
+          if turn.counter == turn.limit || final_winner == "No Winner"
+            p "---- DRAW ----"
+          else
+            p "*~*~*~* #{final_winner.name} has won the game! *~*~*~*"
+          end
         break
       end
     end
-    if @counter == 10**6
-      puts "DRAW GAME"
-    end
-  end
-
-  # break if someone_lost?
-  def someone_lost?
-    @turn.players.any? { |player| player.has_lost? == true }
   end
 
 end
-
-# what does Start.new(turn) return?
-# it returns a turn obj instance
-#
