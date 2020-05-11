0a7943b7fcd4b2f13bbc563fa0ab1b3775efed6e
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-11 00:10:51 -0600

    Add working card_generator.rb

---
+++ lib/card_generator.rb
@@ -0,0 +1,37 @@
+# I did not write tests for this but I worked it out on paper first
+
+require './lib/card'
+
+class CardGenerator
+  attr_reader :filename, :cards
+
+  def initialize(filename)
+    @filename = filename
+    @cards = []
+    do_the_thing
+  end
+
+  def do_the_thing
+    p_arr = File.readlines(@filename).map do |line|
+      it = clean(line)
+      v = it[0]
+      s = it[1].downcase.lstrip.to_sym
+      r = it[2].to_i
+      {suit: s, value: v, rank: r}
+    end
+
+    p_arr.length.times do |i|
+      @cards << Card.new(p_arr[i][:suit], p_arr[i][:value], p_arr[i][:rank])
+    end
+  end
+
+  def clean(line)
+    it = line.to_s
+    it = line.split(",")
+  end
+
+end
+
+p cards = CardGenerator.new("cards.txt").cards
+# p cards = CardGenerator.new("cards.txt").cards.size
+# => 52
