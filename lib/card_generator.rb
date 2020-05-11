f9385a64defd2100ecf16ed59a09e7ea1c3b236e
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-11 00:55:32 -0600

    Refactor actual card generator

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -7,19 +7,21 @@ class CardGenerator
 
   def initialize(filename)
     @filename = filename
-    @cards = []
+    @cards = Array.new
     do_the_thing
   end
 
-  def do_the_thing
-    p_arr = File.readlines(@filename).map do |line|
+  def p_arr
+    File.readlines(@filename).map do |line|
       it = clean(line)
       v = it[0]
       s = it[1].downcase.lstrip.to_sym
       r = it[2].to_i
       {suit: s, value: v, rank: r}
     end
+  end
 
+  def do_the_thing
     p_arr.length.times do |i|
       @cards << Card.new(p_arr[i][:suit], p_arr[i][:value], p_arr[i][:rank])
     end
