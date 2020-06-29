f4448e3a46a65596249ee220d8c16647379b9606
RyanDBarnett <conner2468@gmail.com>
2020-06-29 17:50:14 -0600

    Create deck_test.rb and rank of card at index test

---
+++ test/deck_test.rb
@@ -0,0 +1,26 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+
+class DeckTest < Minitest::Test
+  def setup
+    @card1 = Card.new(:diamond, '3', 3)
+    @card2 = Card.new(:heart, 'Queen', 12)
+    @card3 = Card.new(:spade, 'King', 13)
+    @cards = [@card1, @card2, @card3]
+    @deck = Deck.new(@cards)
+  end
+
+  def test_it_exists
+    assert_instance_of Deck, @deck
+  end
+
+  def test_it_has_a_cards_attribute
+    assert_equal @cards, @deck.cards
+  end
+
+  def test_it_returns_rank_of_card_at_given_index
+    assert_equal @card1.rank, @deck.rank_of_card_at(0)
+  end
+end
