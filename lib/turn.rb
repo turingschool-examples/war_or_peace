7be2759c9078b576cbbb9cbeb4ae17aaff2d884f
Travis McKinstry <mckinstrytg@gmail.com>
2020-06-30 14:30:12 -0600

    Add variabale for player1 and player2 rank numbers

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,19 +6,22 @@ class Turn
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
+    @player1_rank = player1_rank
+    @player2_rank = player2_rank
   end
 
   def type
-    if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
+    if @player1_rank != @player2_rank
       :basic
-    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
+    elsif @player1_rank == @player2_rank
       :war
-    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
+    elsif @player1_rank == @player2_rank && @player1_rank == @player2_rank
       :mutually_assured_destruction
     end
   end
 
   def winner
+
 
   end
 
