3598e7952d2f3d435d1e34f46378f40fc3305478
Travis McKinstry <mckinstrytg@gmail.com>
2020-06-30 14:23:43 -0600

    Create 'winner' method

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,6 +16,9 @@ class Turn
     elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
       :mutually_assured_destruction
     end
+  end
+
+  def winner
 
   end
 
