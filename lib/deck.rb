c9147840b02e53fceb3b143cda297082c48db8ab
Hashim Gari <hashmaster3k@gmail.com>
2020-06-29 21:11:13 -0600

    Add Deck class and test class

---
+++ lib/deck.rb
@@ -0,0 +1,43 @@
+require './lib/card'
+require 'pry'
+
+class Deck
+
+  attr_reader :cards
+
+  #constructor
+  def initialize (cards)
+    @cards = cards
+  end
+
+  #returns the requested index for the array 'cards'
+  def rank_of_card_at (num)
+    cards.fetch(num, "Index #{num} is out of bounds.")
+  end
+
+  #returns an array of cards in the deck that have a rank of 11 or above
+  def high_ranking_cards
+
+    cards.select do |card|
+    card.rank >= 11
+
+    end
+  end
+
+  #calculates the percentage of cards with rank 11 or higher in cards array
+  def percent_high_ranking
+    percent = high_ranking_cards.length / cards.length.to_f
+  end
+
+  #adds a new card to the end of the deck
+  def add_card(new_card)
+    cards.push(new_card)
+  end
+
+  #removes the top card from the deck and returns the remainders
+  def remove_card
+    cards.shift
+    return cards
+  end
+
+end
---
+++ test/deck_test.rb
@@ -0,0 +1,46 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+
+class DeckTest < Minitest::Test
+
+  def setup
+    #create new cards
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+
+    #add cards to an array
+    @cards = [@card1, @card2, @card3]
+
+    #add array of cards to deck
+    @deck = Deck.new(@cards)
+  end
+
+  def test_it_exists
+    assert_instance_of Deck, @deck
+  end
+
+  def test_it_initialize
+    assert_equal @cards, @deck.cards
+  end
+
+  def test_high_ranking_cards
+    assert_equal [@card1, @card3], @deck.high_ranking_cards
+  end
+
+  def test_percent_high_ranking
+    assert_equal 2/3.to_f, @deck.percent_high_ranking
+  end
+
+  def test_add_card
+    card4 = Card.new(:club, '5', 5)
+    assert_equal @cards, @deck.add_card(card4)
+  end
+
+  def test_remove_card
+    assert_equal [@card2, @card3], @deck.remove_card
+  end
+
+end
