20a9ebef0f72f8201bd8c80210e0b4d05d2ec3ed
Travis McKinstry <mckinstrytg@gmail.com>
2020-06-30 14:56:45 -0600

    Add pile_cards method, award_spoils method

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,28 +1,67 @@
 
 
 class Turn
-  attr_reader :player1, :player2, :spoils_of_war
+  attr_reader :player1, :player2, :spoils_of_war, :player1_rank, :player2_rank
 
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
-    @player1_rank = player1_rank
-    @player2_rank = player2_rank
+    @player1_rank_0 = player1.deck.rank_of_card_at(0)
+    @player2_rank_0 = player2.deck.rank_of_card_at(0)
+    @spoils_of_war = []
   end
 
   def type
-    if @player1_rank != @player2_rank
+    if @player1_rank_0 != @player2_rank_0
       :basic
-    elsif @player1_rank == @player2_rank
+    elsif @player1_rank_0 == @player2_rank_0
       :war
-    elsif @player1_rank == @player2_rank && @player1_rank == @player2_rank
+    elsif @player1_rank_0 == @player2_rank_0 && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
       :mutually_assured_destruction
     end
   end
 
   def winner
+    if type == :basic
+      if @player1_rank_0 > @player2_rank_0
+        return @player1
+      elsif @player1_rank_0 < @player2_rank_0
+        return @player2
+      end
+    elsif type == :war
+      if @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
+        return @player1
+      elsif @player1.deck.rank_of_card_at(2) < @player2.deck.rank_of_card_at(2)
+        return @player2
+      end
+    elsif type == :mutually_assured_destruction
+      return 'No Winner'
+    end
+    end
+
+    def pile_cards
+      if type == :basic
+        @spoils_of_war << @player1.deck.cards[0]
+        @spoils_of_war << @player2.deck.cards[0]
+      elsif type == :war
+        @spoils_of_war << @player1.deck.cards[0]
+        @spoils_of_war << @player1.deck.cards[1]
+        @spoils_of_war << @player1.deck.cards[2]
 
 
-  end
-
+        @spoils_of_war << @player2.deck.cards[0]
+        @spoils_of_war << @player2.deck.cards[1]
+        @spoils_of_war << @player2.deck.cards[2]
+      end
+    elsif type == :mutually_assured_destruction
+        @player1.deck.cards.delete_at(0, 1, 2)
+        @player2.deck.cards.delete_at(0, 1, 2)
+    end
+    def award_spoils
+      if winner == @player1
+        @player1.deck << @spoils_of_war
+      else
+        @player2.deck << @spoils_of_war
+      end
+    end
 end
