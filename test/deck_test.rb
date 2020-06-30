0a3aba647457b32e0f413316246bbcb8c9233b88
Arique Aguilar <arique1104@gmail.com>
2020-06-30 18:20:48 -0500

    Test for add_card method

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -12,6 +12,7 @@ class DeckTest < Minitest::Test
     @card3 = Card.new(:heart, "Ace", 14)
     @cards = [@card1, @card2, @card3]
     @deck = Deck.new(@cards)
+
   end
 
   def test_it_exists
@@ -42,6 +43,23 @@ class DeckTest < Minitest::Test
   def test_it_can_remove_card
 
     assert_equal @card1, @deck.remove_card
+
+  end
+
+  def test_it_can_add_card
+    card1 = Card.new(:diamond, "Queen", 12)
+    card2 = Card.new(:spade, "3", 3)
+    card3 = Card.new(:heart, "Ace", 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    deck.remove_card
+
+    card4 = Card.new(:club, "5", 5)
+
+    deck.add_card(card4)
+
+    assert_equal [card2, card3, card4], deck.cards
 
   end
 
