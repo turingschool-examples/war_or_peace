a3b3fd1fe04581ac0172e8b78d90560f89146003
Leah Riffell <leah.riffell@gmail.com>
2020-06-29 19:21:50 -0600

    Setup deck class and test files

---
+++ test/deck_test.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require '../lib/deck'
+require '../lib/card'
+require 'pry'
+
+class DeckTest < Minitest::Test
+  def test_it_exists
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_instance_of Deck, deck
+  end
+
+  def test_it_has_readable_attributes
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal :diamond, deck.cards[0].suit
+    assert_equal '3', deck.cards[1].value
+    assert_equal 14, deck.cards[2].rank
+  end
+end
