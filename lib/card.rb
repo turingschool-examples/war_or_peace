8fe64f2b07ebfba16aaa39b7a86eb85e326a231a
Travis McKinstry <mckinstrytg@gmail.com>
2020-06-29 18:40:29 -0600

    Add deck file, change location of 'pry' to get test to pass

--- lib/card.rb
+++ lib/card.rb
@@ -1,2 +1,12 @@
 class Card
+
+  attr_accessor :suit, :value, :rank
+
+  def initialize(suit, value, rank)
+    @suit = suit
+    @value = value
+    @rank = rank
+  end
+
 end
+require "pry"; binding.pry
