f450a549aa1c93cbb07b16b75c0f89561b626eaf
Nico Rithner <nicorithner@gmail.com>
2020-06-30 07:46:45 -0600

    Add Player class player_test.rb and player.rb. Add test it has a name and a deck

---
+++ test/player_test.rb
@@ -0,0 +1,31 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+
+require "pry"
+
+class PlayerTest < Minitest::Test
+
+    def test_it_exist
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+        deck = Deck.new([card1, card2, card3])
+        player = Player.new('Clarisa', deck)
+        assert_instance_of Player, player
+    end
+
+    def test_it_has_a_name_and_a_deck
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+        deck = Deck.new([card1, card2, card3])
+        player = Player.new('Clarisa', deck)
+# binding.pry
+        assert_equal 'Clarisa', player.name
+        assert_equal deck , player.deck
+    end
+
+end
