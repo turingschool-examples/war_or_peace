55ced0437f508c14976d73362ffe893480256ff3
Arique Aguilar <arique1104@gmail.com>
2020-06-30 18:28:56 -0500

    Set up method

--- test/player_test.rb
+++ test/player_test.rb
@@ -7,6 +7,14 @@ require "./lib/player"
 
 class PlayerTest < Minitest::Test
 
+  def setup
+    @card1 = Card.new(:diamond, "Queen", 12)
+    @card2 = Card.new(:spade, "3", 3)
+    @card3 = Card.new(:heart, "Ace", 14)
+    @deck = Deck.new([@card1, @card2, @card3])
+    @player = Player.new("Clarisa", @deck)
+  end
+
   #def test_it_exists
     #assert_instance_of <Class>,
   #end
