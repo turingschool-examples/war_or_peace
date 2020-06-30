4f7ecad2b8607f2925f012242d4d5682d45ecf8a
Leah Riffell <leah.riffell@gmail.com>
2020-06-29 20:17:49 -0600

    Add method and test to find a card's rank based on index within deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,4 +4,15 @@ class Deck
   def initialize(cards)
     @cards = cards
   end
+
+  def rank_of_card_at(index)
+    @cards[index].rank
+  end
+  # 
+  # def high_ranking_cards
+  #
+  # end
+  #
+  #
+  #   : this method will return an array of cards in the deck that have a rank of 11 or above (face cards and aces)
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -26,4 +26,14 @@ class DeckTest < Minitest::Test
     assert_equal '3', deck.cards[1].value
     assert_equal 14, deck.cards[2].rank
   end
+
+  def test_get_rank_of_card_at_index
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal 12, deck.rank_of_card_at(0)
+  end
 end
