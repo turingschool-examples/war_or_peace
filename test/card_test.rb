23957cdbce4c27f5188e3000662540b100c8a0a4
Arique Aguilar <arique1104@gmail.com>
2020-06-30 09:09:08 -0500

    Card Test set and passing

---
+++ test/card_test.rb
@@ -0,0 +1,15 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "mocha/minitest"
+require "./lib/card"
+
+class CardTest < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+end
