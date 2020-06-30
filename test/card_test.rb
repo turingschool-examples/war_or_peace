97dfa14acf651fc9dd4d2b106e905e7fc93831c2
Arique Aguilar <arique1104@gmail.com>
2020-06-30 09:13:46 -0500

    Card.suit method test

--- test/card_test.rb
+++ test/card_test.rb
@@ -7,11 +7,14 @@ class CardTest < Minitest::Test
   def setup
     @card = Card.new(:heart, 'Jack', 11)
   end
+
   def test_it_exists
     assert_instance_of Card, @card
   end
 
-  #def test_it_has_attributes
-  #end
+  def test_it_has_attributes
+    assert_equal :heart, @card.suit
+    
+  end
 
 end
