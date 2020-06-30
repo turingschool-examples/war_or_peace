41477fb452838edc7bff71eddc91435fb12ea16c
Arique Aguilar <arique1104@gmail.com>
2020-06-30 18:26:51 -0500

    Require player.rb in test file with passsing code.

---
+++ test/player_test.rb
@@ -0,0 +1,17 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "mocha/minitest"
+require "./lib/card"
+require "./lib/deck"
+require "./lib/player"
+
+class PlayerTest < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+end
