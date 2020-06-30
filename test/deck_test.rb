1ef1257bdb8118891b5b445bab2431792aec3f20
Arique Aguilar <arique1104@gmail.com>
2020-06-30 11:48:02 -0500

    Require deck.rb file

---
+++ test/deck_test.rb
@@ -0,0 +1,16 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "mocha/minitest"
+require "./lib/card"
+require "./lib/deck"
+
+class DeckTest < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+end
