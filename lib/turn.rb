8c6ad2688a1c5f93b84cbcfb3aff703d03c2fbf8
Travis McKinstry <mckinstrytg@gmail.com>
2020-06-30 14:23:10 -0600

    Create if..else statement for type of turn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,6 +9,13 @@ class Turn
   end
 
   def type
+    if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
+      :basic
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
+      :war
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
+      :mutually_assured_destruction
+    end
 
   end
 
