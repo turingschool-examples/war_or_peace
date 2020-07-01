8b655408d63ee133c183eab66bba5b8cae70cf8c
Arique Aguilar <arique1104@gmail.com>
2020-06-30 19:25:09 -0500

    Require file in test with passing file

---
+++ test/turn_test.rb
@@ -0,0 +1,18 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "mocha/minitest"
+require "./lib/card"
+require "./lib/deck"
+require "./lib/player"
+require "./lib/turn"
+
+class TurnTest < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+end
