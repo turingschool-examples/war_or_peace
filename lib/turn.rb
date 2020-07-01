768c498c501106b633e37ccfda66dbdecae81607
Logan Riffell <lkriffell@gmail.com>
2020-06-30 20:03:26 -0600

    Updates test and class for turn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,4 @@
 class Turn
-
   attr_reader :player1, :player2, :spoils_of_war
   def initialize(player1, player2)
     @player1 = player1
@@ -7,18 +6,34 @@ class Turn
   end
 
   def type
-    if player1.rank_of_card_at(0) != player2.rank_of_card_at(0)
+    if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
       :basic
-    elsif player1.rank_of_card_at(0) == player2.rank_of_card_at(0)
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
       :war
-    elsif player1.rank_of_card_at(0) == player2.rank_of_card_at(0) and player1.rank_of_card_at(2) == player2.rank_of_card_at(2)
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) and player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
       :mutually_assured_destruction
     end
   end
 
   def winner
-    if 
+    if type == :basic
+      if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+        player1.name
+      else
+        player2.name
+      end
 
+    elsif type == :war
+      if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+        player1.name
+      elsif player1.deck.rank_of_card_at(2) < player2.deck.rank_of_card_at(2)
+        player2.name
+      else
+        "No Winner"
+      end
+
+    elsif type == :mutually_assured_destruction
+      "No Winner"
     end
   end
 end
