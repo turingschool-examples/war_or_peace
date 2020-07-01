8812b198eaed126a67612051f410290e13359b78
Priya Power <priyajag72@gmail.com>
2020-06-30 18:05:16 -0600

    Add card.rb and test file

--- test/card_test.rb
+++ test/card_test.rb
@@ -1,8 +1,9 @@
 require 'minitest/autorun'
-require 'minitest/pride'
+require 'minitest/nyan_cat'
 require './lib/card'
 
 class CardTest < Minitest::Test
+
   def setup
     card = Card.new(:diamond, 'Queen', 12)
   end
@@ -15,9 +16,10 @@ class CardTest < Minitest::Test
 
   def test_it_has_readable_attributes
     card = Card.new(:diamond, 'Queen', 12)
-    
+
     assert_equal :diamond, card.suit
     assert_equal 'Queen', card.value
     assert_equal 12, card.rank
   end
+
 end
