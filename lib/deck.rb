14bb5de12f8e00d7ffc116b92c99fe99a1796ce7
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-06-30 21:19:58 -0600

    Add high_ranking_cards method

--- lib/deck.rb
+++ lib/deck.rb
@@ -5,15 +5,9 @@ class Deck
   attr_reader :cards
 
   def initialize(cards)
-    @deck = []
     @cards = cards
   end
 
-  def shuffle
-    unless @cards.empty?
-      @cards.shuffle!
-    end
-  end
 
   def add_card(card)
     @deck << card
@@ -23,16 +17,36 @@ class Deck
     @deck.shift
   end
 
-  def rank_of_card_at
-
-  end
-
-  def percent_high_ranking
-
-  end
+  # def rank_of_card_at
+  #
+  # end
+  #
+  # def percent_high_ranking
+  #
+  # end
 
   def high_ranking_cards
 
+    high_ranking_cards = []
+
+    cards.each do|card|
+      if card.rank >= 11
+        high_ranking_cards << card
+      end
+    end
+    #below line is return value
+    high_ranking_cards
   end
 
 end
+
+card_one = Card.new(:heart, "King", 13)
+card_two = Card.new(:spade, "two", 2)
+card_three = Card.new(:diamond, "Queen", 12)
+cards = []
+cards << card_one
+cards << card_two
+cards << card_three
+deck = Deck.new(cards)
+#
+binding.pry
