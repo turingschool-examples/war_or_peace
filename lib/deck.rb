f3ff17d6382eb9f3d8748f8e06dad1608eea667e
Drew Williams <drewwilliams5280@gmail.com>
2020-07-01 14:17:51 -0600

    Added deck.rb

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,1 +1,29 @@
 require './lib/card'
+
+class Deck
+  attr_reader :cards
+
+  def initialize
+    @cards = []
+  end
+
+  def rank_of_card_at(position)
+    @cards.rank[position]
+  end
+
+  def high_ranking_cards
+    @cards.rank.select {|card| card >= 11}
+  end
+
+  def percent_high_ranking
+    (high_ranking_cards.length).percent_of(@cards.length)
+  end
+
+  def remove_card
+    @cards.shift
+  end
+
+  def add_card(card)
+    @cards << card
+  end
+end
