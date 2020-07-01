94f6dd84331389669f53eae65624c478b286da40
Leah Riffell <leah.riffell@gmail.com>
2020-06-30 20:02:35 -0600

    Add tests to check if it exists and has readable attributes

--- test/player_test.rb
+++ test/player_test.rb
@@ -34,11 +34,10 @@ class PlayerTest < Minitest::Test
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
     cards1 = [card1, card2, card3]
-    cards2 =[]
-    deck1 = Deck.new(cards1)
-    deck2 = Deck.new(cards2)
-    player1 = Player.new('Clarissa', deck1)
-    player2 = Player.new('Elmie', deck2)
+    deck1 = Deck.new([card1, card2, card3])
+    deck2 = Deck.new([])
+    player1 = Player.new("Clarissa", deck1)
+    player2 = Player.new("Elmie", deck2)
 
     assert_equal false, player1.has_lost?
     assert_equal true, player2.has_lost?
