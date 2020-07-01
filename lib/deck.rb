0589adea0fa33fd84a2ac28696f5fb3ba47d3f11
John Kim <abcdefghijohnk@gmail.com>
2020-07-01 08:23:44 -0600

    Add high_ranking method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,5 +1,7 @@
 class Deck
+
   attr_reader :cards
+
   def initalize(cards)
     @cards = %w[1 2 3 4 5 6 7 8 9 10 J Q K A]
   end
@@ -8,14 +10,21 @@ class Deck
   end
 
   def high_ranking_cards
+    high_cards = []
+    cards.each do |card|
+    if cards.rank >= 11
+    high_cards << cards
   end
 
   def percent_high_ranking
+    cards.each do |card|
+      if card > 9 
   end
 
   def remove_card
+
   end
 
   def add_card
   end
-end 
+end
