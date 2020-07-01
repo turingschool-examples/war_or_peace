53cafb30862b6b3b03d42dbc5f127ead0a23e6f4
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-06-30 21:26:10 -0600

    Removed deck instance at bottom of code

--- lib/deck.rb
+++ lib/deck.rb
@@ -39,14 +39,3 @@ class Deck
   end
 
 end
-
-card_one = Card.new(:heart, "King", 13)
-card_two = Card.new(:spade, "two", 2)
-card_three = Card.new(:diamond, "Queen", 12)
-cards = []
-cards << card_one
-cards << card_two
-cards << card_three
-deck = Deck.new(cards)
-#
-binding.pry
