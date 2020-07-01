a92811dcc55514ee28ba050be5f639533491393b
John Kim <abcdefghijohnk@gmail.com>
2020-06-30 18:22:33 -0600

    Create deck class file

---
+++ lib/deck.rb
@@ -0,0 +1,21 @@
+class Deck
+  attr_reader :cards
+  def initalize(cards)
+    @cards = %w[1 2 3 4 5 6 7 8 9 10 J Q K A]
+  end
+
+  def rank_of_card_at
+  end
+
+  def high_ranking_cards
+  end
+
+  def percent_high_ranking
+  end
+
+  def remove_card
+  end
+
+  def add_card
+  end
+end 
