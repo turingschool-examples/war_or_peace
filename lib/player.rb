d169896b0cc3ac2c00a950d62f92af7d8e55d242
Priya Power <priyajag72@gmail.com>
2020-07-01 17:46:31 -0600

    Tested and created a basic player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,2 +1,9 @@
 class Player
+
+  attr_reader :name, :deck
+
+  def initialize(name, deck)
+    @name = name
+    @deck = deck
+  end
 end
--- test/player_test.rb
+++ test/player_test.rb
@@ -1,9 +1,21 @@
 require 'minitest/autorun'
 require 'minitest/nyan_cat'
-require './lib/player.rb'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+
 
 class PlayerTest < Minitest::Test
+
   def test_it_exists
-    player = Player.new
+    card_1 = Card.new(:club, 'Jack', 11)
+    card_2 = Card.new(:heart, 'Seven', 7)
+    card_3 = Card.new(:spade, 'Two', 2)
+    card_4 = Card.new(:diamond, 'King', 13)
+    cards = [card_1, card_2, card_3, card_4]
+    deck = Deck.new(cards)
+
+    player = Player.new('Priya', deck)
+    require 'pry'; binding.pry
   end
 end
