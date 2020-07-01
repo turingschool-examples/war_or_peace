728015696e5e20f5ae7416c2c43464cc15858bb5
Brett Turing <bjsherman80@gmail.com>
2020-06-30 20:25:54 -0600

    Add deck class

---
+++ lib/deck.rb
@@ -0,0 +1,37 @@
+require './lib/card'
+
+class Deck
+  attr_reader :cards
+  def initialize(cards)
+    @cards = cards
+  end
+
+
+
+
+  def rank_of_card_at(index)
+    @cards[index].rank
+  end
+
+
+
+
+  def high_ranking_cards
+    @cards.map do |card|
+       if card.rank >= 11
+         card
+       end
+    end.compact
+  end
+end
+#
+#   def percent_high_ranking
+#   end
+#
+#   def remove_rcard
+#   end
+#
+#   def add_card
+#   end
+#
+# end
