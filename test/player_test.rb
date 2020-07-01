3fdddc94ac1d1bb063a4b5e5c3f92b6d31581eaa
Leah Riffell <leah.riffell@gmail.com>
2020-06-30 19:18:38 -0600

    Add method and test to inform if player has lost

--- test/player_test.rb
+++ test/player_test.rb
@@ -28,4 +28,20 @@ class PlayerTest < Minitest::Test
     assert_equal 'Clarissa', player.name
     assert_equal deck, player.deck
   end
+
+  def test_has_player_lost
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards1 = [card1, card2, card3]
+    cards2 =[]
+    deck1 = Deck.new(cards1)
+    deck2 = Deck.new(cards2)
+    player1 = Player.new('Clarissa', deck1)
+    player2 = Player.new('Elmie', deck2)
+
+    assert_equal false, player1.has_lost?
+    assert_equal true, player2.has_lost?
+    binding.pry
+  end
 end
