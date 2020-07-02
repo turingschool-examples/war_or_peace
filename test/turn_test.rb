e508c41d361420cb085938a6baca82d50adbf5f3
Garret Wolf <garretwolf26@gmail.com>
2020-07-01 22:55:42 -0400

    Create turn and turn test

---
+++ test/turn_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/deck'
+require './lib/card'
+require './lib/player'
+require './lib/turn'
+require 'pry'
+
+class TurnTest < Minitest::Test
+
+end
