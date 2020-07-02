7e59a31dfcdc9d019002ca60839e4d32e9e72f13
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-02 08:30:58 -0600

    Skip code so it wont run

--- lib/deck_runner.rb
+++ lib/deck_runner.rb
@@ -1,14 +1,14 @@
-require './lib/card'
-require './lib/deck'
-
-
-card_one = Card.new(:heart, "King", 13)
-card_two = Card.new(:spade, "two", 2)
-card_three = Card.new(:diamond, "Queen", 12)
-cards = []
-cards << card_one
-cards << card_two
-cards << card_three
-deck = Deck.new(cards)
+# require './lib/card'
+# require './lib/deck'
 #
-binding.pry
+#
+# card_one = Card.new(:heart, "King", 13)
+# card_two = Card.new(:spade, "two", 2)
+# card_three = Card.new(:diamond, "Queen", 12)
+# cards = []
+# cards << card_one
+# cards << card_two
+# cards << card_three
+# deck = Deck.new(cards)
+# #
+# binding.pry
