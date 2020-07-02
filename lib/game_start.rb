a446115ba3e604ce07b054d47b2a7c3999722494
Travis McKinstry <mckinstrytg@gmail.com>
2020-07-02 09:25:01 -0600

    Create new file to save other code to test, create loop method within Game class

--- lib/game_start.rb
+++ lib/game_start.rb
@@ -5,120 +5,77 @@ require './lib/turn'
 
 
 
-suits = %i[spades hearts diamonds clubs]
-ranks = %i[Ace two three four five six seven eight nine ten Jack Queen King]
-values = {
-  :Ace => 1,
-  :two => 2,
-  :three => 3,
-  :four => 4,
-  :five => 5,
-  :six => 6,
-  :seven => 7,
-  :eight => 8,
-  :nine => 9,
-  :ten => 10,
-  :Jack => 11,
-  :Queen => 12,
-  :King => 13
-}
-
-
-fdeck = ranks.map {|rank| suits.map {|suit| values.map{ |rank, value| Card.new(suit, value, rank)  }  } }.flatten
-full_deck = fdeck.take(52)
-
-shuffled_deck = full_deck.shuffle
-shuffled_deck
-#
-#
-deck1 = Deck.new(shuffled_deck.take(26))
-deck2 = Deck.new(shuffled_deck.drop(26))
-
-player1 = Player.new("Megan", deck1)
-player2 = Player.new("Aurora", deck2)
-
 class Game
-  attr_accessor :player1, :player2, :turn_number, :turn
+  attr_reader :player1, :player2, :turn_number, :turn, :spoils_of_war
   $turn_number = 0
 
   def initialize
-    turn = Turn.new(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @spoils_of_war = []
   end
 
   def start
-    $turn_number += 1
+    suits = %i[spades hearts diamonds clubs]
+    ranks = %i[Ace two three four five six seven eight nine ten Jack Queen King]
+    values = {
+      :Ace => 1,
+      :two => 2,
+      :three => 3,
+      :four => 4,
+      :five => 5,
+      :six => 6,
+      :seven => 7,
+      :eight => 8,
+      :nine => 9,
+      :ten => 10,
+      :Jack => 11,
+      :Queen => 12,
+      :King => 13
+    }
+
+    fdeck = ranks.map {|rank| suits.map {|suit| values.map{ |rank, value| Card.new(suit, value, rank)  }  } }.flatten
+    full_deck = fdeck.take(52)
+
+    shuffled_deck = full_deck.shuffle
+    #
+    #
+    deck1 = Deck.new(shuffled_deck.take(26))
+    deck2 = Deck.new(shuffled_deck.drop(26))
+
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+
+
+      if player1.deck.cards.length != 0 || player2.deck.cards.length != 0
+        10.times{
+          winner = turn.winner
+          turn.award_spoils(winner)
+          p "Turn #{$turn_number}: #{turn.winner} won #{turn.spoils_of_war.length} cards"
+          $turn_number += 1
+          p player1.deck.cards.length
+          p player2.deck.cards.length
+
+         }
+      else
+      puts "Draw"
+      end
   end
-
-
 end
 
-puts "Welcome to War! (or Peace) This game will be played with 52 cards. The players today are Megan and Aurora. Type 'GO' to start the game!"
-p '------------------------------------------------------------------'
 
+
+
+
+puts "Welcome to War! (or Peace) This game will be played with 52 cards.
+The players today are Megan and Aurora.
+Type 'GO' to start the game!
+------------------------------------------------------------------"
 if gets.chomp!.downcase == 'go'
-  @player1 = player1
-  @player2 = player2
-
-  while @player1.deck.cards.length > 0 || @player2.deck.cards.length > 0
-      george = Game.new
-      george.start
-      turn = Turn.new(player1, player2)
-      winner = turn.winner
-      p "Turn #{$turn_number}: #{turn.winner} won #{@spoils_of_war} cards"
-      def type
-        @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0) ? :basic : (@player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) != @player2.deck.rank_of_card_at(2) ? :war : :mutually_assured_destruction)
-      end
-      p @player1.deck.cards.length
-      p @player2.deck.cards.length
-
-      def winner
-        if type == :basic
-          @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0) ? @player1.name : @player2.name
-        elsif type == :war
-          @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2) ? @player1.name : @player2.name
-        elsif type == :mutually_assured_destruction
-          'No Winner'
-        elsif @player1.deck.cards.length == 0 || @player1.deck.cards.length == 0
-          "Game Over"
-        end
-      end
-
-      def pile_cards
-        if type == :basic
-          @spoils_of_war << @player1.deck.cards.first && @spoils_of_war << @player2.deck.cards.first
-        elsif type == :war
-          @spoils_of_war << @player1.deck.cards[0] && @spoils_of_war << @player1.deck.cards[1] && @spoils_of_war << @player1.deck.cards[2] && @spoils_of_war << @player2.deck.cards[0] && @spoils_of_war << @player2.deck.cards[1] && @spoils_of_war << @player1.deck.cards[2]
-        elsif @player1.deck.cards.length == 0 || @player1.deck.cards.length == 0
-          "Game Over"
-        end
-      end
-
-      def award_spoils(winner)
-        if winner == 'Megan' && type == :basic
-          @player1.deck.cards << @spoils_of_war
-          @player2.deck.cards = @player2.deck.cards.drop(1)
-          @player1.deck.cards = @player1.deck.cards.flatten.uniq.shuffle
-        elsif winner == 'Aurora' && type == :basic
-          @player2.deck.cards << @spoils_of_war
-          @player1.deck.cards = @player1.deck.cards.drop(1)
-          @player2.deck.cards = @player2.deck.cards.flatten.uniq.shuffle
-        elsif winner == 'Megan' && type == :war
-          @player1.deck.cards << @spoils_of_war
-          @player2.deck.cards = @player2.deck.cards.drop(3)
-          @player1.deck.cards = @player1.deck.cards.flatten.uniq.shuffle
-        elsif winner == 'Aurora' && type == :war
-          @player2.deck.cards << @spoils_of_war
-          @player1.deck.cards = @player1.deck.cards.drop(3)
-          @player2.deck.cards = @player2.deck.cards.flatten.uniq.shuffle
-        elsif winner == 'No Winner' && type == :mutually_assured_destruction
-          @player1.deck.cards = @player1.deck.cards.drop(3)
-          @player2.deck.cards = @player2.deck.cards.drop(3)
-        elsif @player1.deck.cards.length == 0 || @player1.deck.cards.length == 0
-          "Game Over"
-        end
-      end
-
-    end
-  else
-    p "No I meant, try typing the letters 'g' and 'o' together, with no space in between."
+  new_game = Game.new
+  new_game.start
+else
+  puts "Try putting 'go' or 'GO'."
 end
--- lib/turn.rb
+++ lib/turn.rb
@@ -7,53 +7,71 @@
 class Turn
   attr_accessor :player1, :player2, :spoils_of_war
 
+  $turn_number = 0
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
     @spoils_of_war = []
+    $turn_number += 1
   end
 
   def type
-    @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0) ? :basic : (@player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) != @player2.deck.rank_of_card_at(2) ? :war : :mutually_assured_destruction)
+    if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
+       :basic
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) != @player2.deck.rank_of_card_at(2)
+      :war
+    else
+      :mutually_assured_destruction
+    end
   end
 
   def winner
     if type == :basic
-      @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0) ? @player1.name : @player2.name
+      if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
+        @player1.name
+      else
+        @player2.name
+      end
     elsif type == :war
-      @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2) ? @player1.name : @player2.name
+      if @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
+        @player1.name
+      else
+        @player2.name
+      end
     elsif type == :mutually_assured_destruction
       'No Winner'
-    elsif @player1.deck.cards.length == 0 || @player1.deck.cards.length == 0
-      "Game Over"
     end
   end
 
   def pile_cards
     if type == :basic
-      @spoils_of_war << @player1.deck.cards.first && @spoils_of_war << @player2.deck.cards.first
+      @spoils_of_war << @player1.deck.cards.first + @spoils_of_war << @player2.deck.cards.first
     elsif type == :war
       @spoils_of_war << @player1.deck.cards[0] && @spoils_of_war << @player1.deck.cards[1] && @spoils_of_war << @player1.deck.cards[2] && @spoils_of_war << @player2.deck.cards[0] && @spoils_of_war << @player2.deck.cards[1] && @spoils_of_war << @player1.deck.cards[2]
     end
   end
 
   def award_spoils(winner)
-    if winner == 'Megan' && type == :basic
+    if winner == 'Megan'
+      if type == :basic
       @player1.deck.cards << @spoils_of_war
       @player2.deck.cards = @player2.deck.cards.drop(1)
-      @player1.deck.cards = @player1.deck.cards.flatten.uniq
-    elsif winner == 'Aurora' && type == :basic
-      @player2.deck.cards << @spoils_of_war
-      @player1.deck.cards = @player1.deck.cards.drop(1)
-      @player2.deck.cards = @player2.deck.cards.flatten.uniq
-    elsif winner == 'Megan' && type == :war
+      @player1.deck.cards = @player1.deck.cards.flatten.uniq.shuffle
+    else
       @player1.deck.cards << @spoils_of_war
       @player2.deck.cards = @player2.deck.cards.drop(3)
-      @player1.deck.cards = @player1.deck.cards.flatten.uniq
-    elsif winner == 'Aurora' && type == :war
+      @player1.deck.cards = @player1.deck.cards.flatten.uniq.shuffle
+    end
+    elsif winner == 'Aurora'
+      if type == :basic
       @player2.deck.cards << @spoils_of_war
-      @player1.deck.cards = @player1.deck.cards.drop(3)
-      @player2.deck.cards = @player2.deck.cards.flatten.uniq
+      @player1.deck.cards = @player1.deck.cards.drop(1)
+      @player2.deck.cards = @player2.deck.cards.flatten.uniq.shuffle
+      elsif type == :war
+        @player2.deck.cards << @spoils_of_war
+        @player1.deck.cards = @player1.deck.cards.drop(3)
+        @player2.deck.cards = @player2.deck.cards.flatten.uniq.shuffle
+      end
     elsif winner == 'No Winner' && type == :mutually_assured_destruction
       @player1.deck.cards = @player1.deck.cards.drop(3)
       @player2.deck.cards = @player2.deck.cards.drop(3)
