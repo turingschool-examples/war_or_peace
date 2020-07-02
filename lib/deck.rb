7c397a0cde9cada70edd4223ad58d157c94b1bea
Shaun James <ShaunDaneJames@Gmail.com>
2020-07-02 11:50:33 -0600

    created method rank of card

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,22 +6,30 @@ class Deck
   end
 
   def rank_of_card_at(index_location_of_card)
-    rank = cards.select{|x| if x = @rank then return x end}
+    if index_location_of_card == 0
+      rank = @cards[0].rank
+    elsif index_location_of_card == 1
+      rank = @cards[1].rank
+    else rank = @cards[2].rank
+    end 
+
   end
 
   def high_ranking_cards
     #return cards in the deck 11 or above
   end
+
   def percent_high_ranking
     #return percentage of cards which rank high
   end
 
   def remove_card
     #remove card from top of deck
+    @cards.drop(1)
   end
 
-  def add_card
-    #add one card to the bottom of Deck
+  def add_card(card1)
+    @cards << card1
   end
 
 end
