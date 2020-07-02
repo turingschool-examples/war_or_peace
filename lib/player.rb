6fe51e428bc1c9999274dd8f7560bb5c28dff347
Drew Williams <drewwilliams5280@gmail.com>
2020-07-01 22:01:40 -0600

    Player completed, early turn tests

---
+++ lib/player.rb
@@ -0,0 +1,19 @@
+require './lib/card'
+require './lib/deck'
+
+class Player
+  attr_reader :name, :deck
+
+  def initialize(name, deck)
+    @name = name
+    @deck = deck
+  end
+
+  def has_lost?
+    if @deck.cards.length > 0
+      false
+    else
+      true
+    end
+  end
+end
