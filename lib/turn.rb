f0e44ed37be48747056e4418a9a973baa6d1321b
Angela Guardia <angelaguardia20@gmail.com>
2020-07-01 19:30:05 -0700

    Add basic turn methods

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,9 +3,40 @@ require './lib/deck'
 require './lib/player'
 
 class Turn
-  attr_reader :player1, :player2
+  attr_reader :player1, :player2, :spoils_of_war
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
+    @spoils_of_war = []
   end
+
+  def type
+    if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
+      :basic
+    end
+  end
+
+  #refactor with max?
+  def winner
+    if type == :basic
+      if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+        player1
+      else
+        player2
+      end
+    end
+  end
+
+  def pile_cards
+    if type == :basic
+      @spoils_of_war << player1.deck.remove_card
+      @spoils_of_war << player2.deck.remove_card
+    end
+  end
+
+  def award_spoils(winner)
+    (winner.deck.cards << @spoils_of_war).flatten!
+    @spoils_of_war = []
+  end
+
 end
