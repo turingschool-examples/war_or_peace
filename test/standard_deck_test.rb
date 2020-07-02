eba530da488d8e692950f7ba2621486e9b7dfd75
Arique Aguilar <arique1104@gmail.com>
2020-07-01 21:25:55 -0500

    Standard Deck require file test with passing files

---
+++ test/standard_deck_test.rb
@@ -0,0 +1,19 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "mocha/minitest"
+require "./lib/card"
+require "./lib/deck"
+require "./lib/player"
+require "./lib/turn"
+require "./lib/standard_deck"
+
+class StandardDeckTest < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+end
