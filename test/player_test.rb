fb74ec9d24806590531d9d12479c18f4dc911f83
Angela Guardia <angelaguardia20@gmail.com>
2020-07-02 12:21:28 -0700

    Add test_it_has_a_deck and Refactor

--- test/player_test.rb
+++ test/player_test.rb
@@ -30,6 +30,18 @@ class PlayerTest < Minitest::Test
     assert_equal 'Clarisa', player.name
   end
 
+  def test_it_has_a_deck
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    player = Player.new('Clarisa', deck)
+
+    assert_equal deck, player.deck
+  end
+
   def test_it_knows_if_it_lost
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
@@ -41,11 +53,13 @@ class PlayerTest < Minitest::Test
 
     assert_equal false, player.has_lost?
 
-    3.times { deck.remove_card }
+    deck.remove_card
+    assert_equal false, player.has_lost?
 
+    deck.remove_card
+    assert_equal false, player.has_lost?
+
+    deck.remove_card
     assert_equal true, player.has_lost?
   end
-
-  
-
 end
