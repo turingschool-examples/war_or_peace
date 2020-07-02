47bf6ab97f8c550f59e47b9daece0ec1ed9e33bd
Leah Riffell <leah.riffell@gmail.com>
2020-07-01 21:58:27 -0600

    Add tests and methods to pile cards and award to winner

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,9 +1,12 @@
+require 'pry'
+
 class Turn
   attr_reader :player1, :player2, :spoils_of_war
 
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
+    @spoils_of_war = []
   end
 
   def type
@@ -17,20 +20,49 @@ class Turn
   end
 
   def winner
-    if type == :basic
+    # type == without `self` works here because ruby allows for an implicit self. Without it, it wasn't clear to me what object `type` was running on. Adding `self` doesn't change how this is run, but it makes it more explicit.
+    if self.type == :basic
       if @player1.deck.cards[0].rank > @player2.deck.cards[0].rank
         @player1
       else
         @player2
       end
-    elsif type == :war
+    elsif self.type == :war
       if @player1.deck.cards[2].rank > @player2.deck.cards[2].rank
         @player1
       else
         @player2
       end
-    else type == :mutually_assured_destruction
+    else self.type == :mutually_assured_destruction
       "No Winner"
+    end
+  end
+
+  def pile_cards
+    #spoils of war is the pile in the middle in a turn
+    if self.type == :basic
+      @spoils_of_war << @player1.deck.cards[0]
+      @spoils_of_war << @player2.deck.cards[0]
+    elsif self.type == :war
+      @spoils_of_war.concat(@player1.deck.cards[0..2])
+      @spoils_of_war.concat(@player2.deck.cards.concat[0..2])
+    else self.type == :mutually_assured_destruction
+      @player1.deck.cards.delete(0..2)
+      @player2.deck.cards.delete(0..2)
+    end
+  end
+
+  def award_spoils
+    # only add cards from the spoils pile if they are not already in the winner's deck of cards
+    spoil_cards_not_in_winner_deck = []
+
+    if self.type == :basic || self.type == :war
+      spoil_cards_not_in_winner_deck = @spoils_of_war.select do |card|
+        !winner.deck.cards.include?(card)
+      end
+      winner.deck.cards.concat(spoil_cards_not_in_winner_deck)
+    else self.type == :mutually_assured_destruction
+      nil
     end
   end
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -79,4 +79,86 @@ class TurnTest < Minitest::Test
 
     assert_equal player1, turn.winner
   end
+
+  def test_can_pile_cards
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    # because this is a command method, first test the assertion, then run command, then redo assertion with changed value
+    assert_equal [], turn.spoils_of_war
+    turn.pile_cards
+    assert_equal [card1, card3], turn.spoils_of_war
+  end
+
+  def test_award_spoils_pile_to_winner_in_basic_turn_type
+    # for basic type turn
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    turn.pile_cards
+    assert_equal [card1, card2, card5, card8, card3], turn.award_spoils
+  end
+
+  def test_award_spoils_pile_to_winner_in_war_turn_type
+    # skip
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    turn.pile_cards
+    assert_equal [card4, card3, card6, card7, card1, card2, card5], turn.award_spoils
+  end
+
+  def test_award_spoils_pile_to_winner_in_mutually_assured_destruction_turn_type
+    # skip
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    # diff value from all examples above
+    card6 = Card.new(:diamond, '8', 8)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    turn.pile_cards
+    assert_nil turn.award_spoils
+  end
 end
