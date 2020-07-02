1a8130f67680a8f36767ecdee395cfd01f1fcea6
Carson Jardine <carsonjardine@gmail.com>
2020-07-01 22:28:38 -0600

    Add def winner

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,10 +15,30 @@ class Turn
     if (player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)) && (player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2))
       return :mutually_assured_destruction
     elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
-      return :war 
+      return :war
     else
       return :basic
     end
   end
+
+  def winner
+    if type() == :basic
+      if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+        return @player1
+      else
+        return @player2
+      end
+    elsif type() == :war
+      if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+        return @player1
+      else
+        return @player2
+      end
+    else
+      return "No Winner"
+    end
+  end
+
+
 
 end # this is the end of the class!!!
