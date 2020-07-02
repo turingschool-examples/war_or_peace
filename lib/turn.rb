c5aa20a1d8e225a3e5fb92dda2c3484966143f9f
Angela Guardia <angelaguardia20@gmail.com>
2020-07-02 13:00:56 -0700

    Add war turn fuctionality

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,6 +13,8 @@ class Turn
   def type
     if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
       :basic
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+      :war
     end
   end
 
@@ -24,6 +26,14 @@ class Turn
       else
         player2
       end
+    elsif type == :war
+      if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+        player1
+      else
+        player2
+      end
+
+
     end
   end
 
@@ -31,6 +41,12 @@ class Turn
     if type == :basic
       @spoils_of_war << player1.deck.remove_card
       @spoils_of_war << player2.deck.remove_card
+    elsif type == :war
+      3.times do
+        @spoils_of_war << player1.deck.remove_card
+        @spoils_of_war << player2.deck.remove_card
+      end
+      require 'pry'; binding.pry
     end
   end
 
