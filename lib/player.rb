7f569827256dfaca7bc03b2ccb9eddad740aa8f7
Priya Power <priyajag72@gmail.com>
2020-07-02 07:08:30 -0600

    Working through has lost method. Found some errors, can see in comment line.

--- lib/player.rb
+++ lib/player.rb
@@ -6,4 +6,29 @@ class Player
     @name = name
     @deck = deck
   end
+
+  def has_lost?
+    # found an error with the way I originally wrote this
+    # Started researching variables equal to zero
+    # Then started researching it as an array
+    # learned that "empty arrays" may show as truthy-y in ruby
+    # I may have to try something entirely different
+    # Error message is telling me .length, .size, .count won't work...
+    # Found a method to try (.empty?)
+    # Okay... that method comes back an error
+    # I KNOW that these are array methods that should work,
+    # So... something else is the issue
+    # Taking a break to try again
+
+    # if deck == 0
+    # if deck == []
+    # if deck.length == 0
+    # if deck !![]
+    # if deck.size < 1
+    if deck.empty?
+      p "#{name} has lost the war.! The battle drums still call, perhaps your luck will be better next time."
+    else
+      p "#{name} is still in the running for the war!"
+    end
+  end
 end
--- test/player_test.rb
+++ test/player_test.rb
@@ -16,6 +16,20 @@ class PlayerTest < Minitest::Test
     deck = Deck.new(cards)
 
     player = Player.new('Priya', deck)
-    require 'pry'; binding.pry
+  end
+
+  def test_player_has_lost
+    card_1 = Card.new(:club, 'Jack', 11)
+    card_2 = Card.new(:heart, 'Seven', 7)
+    card_3 = Card.new(:spade, 'Two', 2)
+    card_4 = Card.new(:diamond, 'King', 13)
+    cards = [card_1, card_2, card_3, card_4]
+    deck = Deck.new(cards)
+    player = Player.new('Priya', deck)
+
+    assert_equal "Priya has lost the war.! The battle drums still call, perhaps your luck will be better next time.", player.has_lost?
+    # Played with pry to ensure edge cases and it looks like no matter what my
+    # deck size is, I get the has_lost message... something is wrong with my CLass file
+
   end
 end
