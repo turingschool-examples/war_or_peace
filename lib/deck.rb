11af465a3f99e2fad8b02dc51f092f592c1a039c
Carson Jardine <carsonjardine@gmail.com>
2020-07-01 19:37:38 -0600

    Update deck and deck test

--- lib/deck.rb
+++ lib/deck.rb
@@ -7,8 +7,39 @@ class Deck
     @cards = cards
   end
 
+  def rank_of_card_at(index)
+    if !@cards[index].nil?
+      return @cards[index].rank
+    else
+      return @cards[index]
+    end
+
+  end
+
+  def high_ranking_cards
+    high_rank_cards = []
+    @cards.each do |card|
+      if card.rank >= 11
+        high_rank_cards << card
+      end
+    end
+    return high_rank_cards
+  end
+
+  def percent_high_ranking
+    percent_high_rank = high_ranking_cards.length.to_f
+    ((percent_high_rank / @cards.length.to_f) * 100).round(2)
+  end
+
+  def remove_card
+    @cards.shift
+  end
+
+  def add_card(card)
+    @cards << card
+  end 
 
 
 
 
-end #this is the end of the class!!!
+end # this is the end of the class!!!
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -12,6 +12,37 @@ class DeckTest < Minitest::Test
     @deck = Deck.new(@cards)
   end
 
+  def test_it_exists
+    assert_instance_of Deck, @deck
+  end
 
+
+  def test_rank_of_card_at
+    assert_equal 12, @deck.rank_of_card_at(0)
+    assert_equal 14, @deck.rank_of_card_at(2)
+  end
+
+  def test_high_ranking_cards
+    assert_equal [@card1, @card3], @deck.high_ranking_cards
+  end
+  #test it will show percent of high ranking cards
+  def test_percent_high_ranking
+    assert_equal 66.67, @deck.percent_high_ranking
+  end
+
+  #test it will remove a card
+  def test_it_will_remove_a_card
+    @deck.remove_card
+
+    assert_equal [@card2, @card3], @deck.cards
+  end
+  #test it will add a card
+  def test_it_will_add_a_card
+
+    card4 = Card.new(:club, '5', 5)
+    @deck.add_card(card4)
+
+    assert_equal @cards, @deck.cards
+  end
 
 end #this is the end of the class!!!
