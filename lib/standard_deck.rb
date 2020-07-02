c8a89e60adb78ac12eea565decd1092cd3daf197
Arique Aguilar <arique1104@gmail.com>
2020-07-01 22:03:08 -0500

    Passing ranks and values method code

--- lib/standard_deck.rb
+++ lib/standard_deck.rb
@@ -1,8 +1,22 @@
 require_relative "./card"
 class StandardDeck
-
+attr_reader   :ranks_and_values
   def initialize
-
+    @ranks_and_values = {
+        2 => "2",
+        3 => "3",
+        4 => "4",
+        5 => "5",
+        6 => "6",
+        7 => "7",
+        8 => "8",
+        9 => "9",
+       10 => "10",
+       11 => "Jack",
+       12 => "Queen",
+       13 => "King",
+       14 => "Ace"
+    }
 
 
 
