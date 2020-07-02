b57e6543447db17d0b80551b297d15126d1ee0e8
Carson Jardine <carsonjardine@gmail.com>
2020-07-01 22:30:20 -0600

    Add winner tests

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -15,23 +15,98 @@ class TurnTest < Minitest::Test
     @card6 = Card.new(:diamond, 'Queen', 12)
     @card7 = Card.new(:heart, '3', 3)
     @card8 = Card.new(:diamond, '2', 2)
+  end
+
+  def test_it_exists
+
     @deck1 = Deck.new([@card1, @card2, @card5, @card8])
     @deck2 = Deck.new([@card3, @card4, @card6, @card7])
     @player1 = Player.new("Megan", @deck1)
     @player2 = Player.new("Aurora", @deck2)
     @turn = Turn.new(@player1, @player2)
-  end
 
-  def test_it_exists
     assert_instance_of Turn, @turn
   end
 
   def test_turn
 
+    @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+    @player1 = Player.new("Megan", @deck1)
+    @player2 = Player.new("Aurora", @deck2)
+    @turn = Turn.new(@player1, @player2)
+
     assert_equal @player1, @turn.player1
     assert_equal @player2, @turn.player2
   end
 
+  def test_basic_turn
+
+    @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+    @player1 = Player.new("Megan", @deck1)
+    @player2 = Player.new("Aurora", @deck2)
+    @turn = Turn.new(@player1, @player2)
+
+    assert_equal :basic, @turn.type
+  end
+
+  def test_mutually_assured_destruction_turn
+
+    @card8 = Card.new(:diamond, '3', 3)
+    @deck1 = Deck.new([@card1, @card2, @card8, @card5])
+    @deck2 = Deck.new([@card4, @card3, @card7, @card6])
+    @player1 = Player.new("Megan", @deck1)
+    @player2 = Player.new("Aurora", @deck2)
+    @turn = Turn.new(@player1, @player2)
+
+    assert_equal :mutually_assured_destruction, @turn.type
+  end
+
+  def test_war_turn
+
+    @deck1 = Deck.new([@card1, @card2, @card8, @card5])
+    @deck2 = Deck.new([@card4, @card3, @card7, @card6])
+    @player1 = Player.new("Megan", @deck1)
+    @player2 = Player.new("Aurora", @deck2)
+    @turn = Turn.new(@player1, @player2)
+
+    assert_equal :war, @turn.type
+  end
+
+  def test_basic_winner
+
+    @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+    @player1 = Player.new("Megan", @deck1)
+    @player2 = Player.new("Aurora", @deck2)
+    @turn = Turn.new(@player1, @player2)
+
+    assert_equal @player1, @turn.winner
+  end
+
+  def test_mutually_assured_destruction_winner
+
+    @card8 = Card.new(:diamond, '3', 3)
+    @deck1 = Deck.new([@card1, @card2, @card7, @card5])
+    @deck2 = Deck.new([@card4, @card3, @card8, @card6])
+    @player1 = Player.new("Megan", @deck1)
+    @player2 = Player.new("Aurora", @deck2)
+    @turn = Turn.new(@player1, @player2)
+
+    assert_equal "No Winner", @turn.winner
+  end
+
+  def test_war_winner
+
+    @deck1 = Deck.new([@card1, @card2, @card8, @card5])
+    @deck2 = Deck.new([@card4, @card3, @card7, @card6])
+    @player1 = Player.new("Megan", @deck1)
+    @player2 = Player.new("Aurora", @deck2)
+    @turn = Turn.new(@player1, @player2)
+
+    assert_equal @player2, @turn.winner
+  end
 
 
 end # this is the end of the class!!!
