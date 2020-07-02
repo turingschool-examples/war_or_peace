51ef3cf4983c6fba4a2bffb546652c7cb6f44e37
Arique Aguilar <arique1104@gmail.com>
2020-07-02 10:57:29 -0500

    Test for attributes ANDD passing code

--- lib/standard_deck.rb
+++ lib/standard_deck.rb
@@ -1,25 +1,15 @@
-require_relative "./card"
 class StandardDeck
-attr_reader   :ranks_and_values,
-              :hearts
-  def initialize(ranks_and_values_hash, value)
-    @ranks_and_values = {
-        2 => "2",
-        3 => "3",
-        4 => "4",
-        5 => "5",
-        6 => "6",
-        7 => "7",
-        8 => "8",
-        9 => "9",
-       10 => "10",
-       11 => "Jack",
-       12 => "Queen",
-       13 => "King",
-       14 => "Ace"
-    }
-      @hearts = :hearts
-
+ attr_reader  :ranks,
+              :values,
+              :suits
+  def initialize(all_cards_info)
+    @ranks = all_cards_info["ranks"]
+    @values = all_cards_info["values"]
+    @suits = all_cards_info["suits"]
   end
+
+  # def group_by_suits(suits, values, ranks)
+  #
+  # end
 
 end
--- test/standard_deck_test.rb
+++ test/standard_deck_test.rb
@@ -9,64 +9,29 @@ require "./lib/standard_deck"
 
 class StandardDeckTest < Minitest::Test
 
+  def setup
+    @all_cards_info = {
+      "ranks" => [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+      "values" => ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"],
+      "suits" => [:heart, :diamond, :spade, :club]
+      }
+
+    @standard_deck = StandardDeck.new(@all_cards_info)
+  end
+
   def test_it_exists
-    standard_deck = StandardDeck.new({
-        2 => "2",
-        3 => "3",
-        4 => "4",
-        5 => "5",
-        6 => "6",
-        7 => "7",
-        8 => "8",
-        9 => "9",
-       10 => "10",
-       11 => "Jack",
-       12 => "Queen",
-       13 => "King",
-       14 => "Ace"
-    }, [:heart, :spade, :diamond, :club])
-    assert_instance_of StandardDeck, standard_deck
+
+    assert_instance_of StandardDeck, @standard_deck
   end
 
   def test_it_has_attributes
-    skip
-    standard_deck = StandardDeck.new({
-        2 => "2",
-        3 => "3",
-        4 => "4",
-        5 => "5",
-        6 => "6",
-        7 => "7",
-        8 => "8",
-        9 => "9",
-       10 => "10",
-       11 => "Jack",
-       12 => "Queen",
-       13 => "King",
-       14 => "Ace"
-    }, [hearts, spades, diamonds, clubs])
+    assert_equal [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], @standard_deck.ranks
+    assert_equal ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"], @standard_deck.values
+    assert_equal [:heart, :diamond, :spade, :club], @standard_deck.suits
 
-        expected = {
-            2 => "2",
-            3 => "3",
-            4 => "4",
-            5 => "5",
-            6 => "6",
-            7 => "7",
-            8 => "8",
-            9 => "9",
-           10 => "10",
-           11 => "Jack",
-           12 => "Queen",
-           13 => "King",
-           14 => "Ace"
-        }
-        assert_equal expected, standard_deck.ranks_and_values
 
-        expected_2 = [:heart, :club, :diamond, :spade]
 
-        assert_equal expected_2, standard_deck.suits
-    #it should initialize with all 52 cards, and I still smell a hash!
+
 
   end
 
@@ -83,7 +48,41 @@ class StandardDeckTest < Minitest::Test
   # it should have a method where the total deck is divided evenly into number of players
 
   #I'm smelling a hash....I think the keys should be turn = {player => }
-
-
+#
+# Messing Around:
+# [1] pry(#<StandardDeck>)> x
+# => [[2, "2"],
+#  [3, "3"],
+#  [4, "4"],
+#  [5, "5"],
+#  [6, "6"],
+#  [7, "7"],
+#  [8, "8"],
+#  [9, "9"],
+#  [10, "10"],
+#  [11, "Jack"],
+#  [12, "Queen"],
+#  [13, "King"],
+#  [14, "Ace"]]
+# [2] pry(#<StandardDeck>)> @values
+# => nil
+# [3] pry(#<StandardDeck>)> @value
+# => [:heart, :spade, :diamond, :club]
+# [4] pry(#<StandardDeck>)> d = {}
+# => {}
+# [5] pry(#<StandardDeck>)> @value.each do |value|
+# [5] pry(#<StandardDeck>)*   d[value] = {}
+# [5] pry(#<StandardDeck>)* end
+# => [:heart, :spade, :diamond, :club]
+# [6] pry(#<StandardDeck>)> d
+# => {:heart=>{}, :spade=>{}, :diamond=>{}, :club=>{}}
+# [7] pry(#<StandardDeck>)> z = x.flat_map each do |x|
+# [7] pry(#<StandardDeck>)*   d.each do |d|
+# [7] pry(#<StandardDeck>)*     d[d] = x
+# [7] pry(#<StandardDeck>)*   end
+# [7] pry(#<StandardDeck>)* end
+# NameError: undefined local variable or method `each' for #<StandardDeck:0x00007fa7ada5a3c8>
+# from (pry):9:in `initialize'
+# [8] pry(#<StandardDeck>)>
 
 end
