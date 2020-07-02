3708928bb1635f16ac1a390a8d981cc6080ebbc8
Hashim Gari <hashmaster3k@gmail.com>
2020-07-02 13:47:41 -0600

    Removed setup method for each minitest

--- test/player_test.rb
+++ test/player_test.rb
@@ -6,32 +6,46 @@ require './lib/card'
 
 class PlayerTest < Minitest::Test
 
-  def setup
-    @card1 = Card.new(:diamond, 'Queen', 12)
-    @card2 = Card.new(:spade, '3', 3)
-    @card3 = Card.new(:heart, 'Ace', 14)
-
-    @deck = Deck.new([@card1, @card2, @card3])
-
-    @player = Player.new('Jimmy', @deck)
-  end
-
   def test_it_exists
-    assert_instance_of Player, @player
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    deck = Deck.new([card1, card2, card3])
+
+    player = Player.new('Clarisa', deck)
+
+    assert_instance_of Player, player
   end
 
   def test_it_initalize
-    assert_equal 'Jimmy', @player.name
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    deck = Deck.new([card1, card2, card3])
+
+    player = Player.new('Clarisa', deck)
+
+    assert_equal 'Clarisa', player.name
   end
 
   def test_has_lost?
-    @player.has_lost?
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
 
-    @player.deck.remove_card
-    @player.deck.remove_card
-    @player.deck.remove_card
+    deck = Deck.new([card1, card2, card3])
 
-    assert_equal true, @player.has_lost?
+    player = Player.new('Clarisa', deck)
+
+    player.has_lost?
+
+    player.deck.remove_card
+    player.deck.remove_card
+    player.deck.remove_card
+
+    assert_equal true, player.has_lost?
   end
 
 end
