f2d52dc6229af47c41ddeea179ff356711df9713
Logan Riffell <lkriffell@gmail.com>
2020-07-02 10:50:54 -0600

    Add deck test

---
+++ test/deck_test.rb
@@ -0,0 +1,55 @@
+require 'minitest/autorun'
+require 'minitest/nyan_cat'
+require './lib/card'
+require './lib/deck'
+class DeckTest < Minitest::Test
+  def test_rank_of_card_at
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    assert_equal 12, deck.rank_of_card_at(0)
+  end
+
+  def test_high_ranking_cards
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    deck.high_ranking_cards
+    assert_equal 2, deck.high_ranking_cards.size
+  end
+
+  def test_percent_high_ranking
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    deck.percent_high_ranking
+    assert_equal 66.67, deck.percent_high_ranking
+  end
+
+  def test_remove_card
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    deck.remove_card
+    assert_equal 2, cards.size
+  end
+
+  def test_add_card
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    card4 = Card.new(:club, '5', 5)
+    deck.add_card(card4)
+    assert_equal 4, cards.size
+  end
+end
