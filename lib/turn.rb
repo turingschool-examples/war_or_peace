67ef6b2cf123eb78226575529fb21ae99f4888dc
Priya Power <priyajag72@gmail.com>
2020-07-02 12:17:01 -0600

    Added working type method

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,6 @@
 class Turn
   # readable attributes for player1, player2, and spoils
-  # attr_accessor :player_1, :player_2, :spoils_of_war
+  attr_accessor :player_1, :player_2, :spoils_of_war
 
   # A turn is created with two players
   def initialize(player_1, player_2)
@@ -8,13 +8,35 @@ class Turn
     @player_2 = player_2
     # This is the one I'm confused on... do I include spoils of war here? and how?
     # After reading through project, seeing this variable is an array
-  #  @spoils_of_war = []
+    @spoils_of_war = []
   end
 
   # Create a type method
     # Will have three variables :basic, :war, :mutually_assured_destruction
     # Looks like an if statement
+  def type
+    # since i'm creating three variables in the if statement, do I need to create
+    # them up here as well? (accessor?)
 
+    # error told me rank of card method is not accessible. Probably need to call class first!
+    # fixed and success!
+    # Oh... wait... no, the test should have actually failed
+
+    # require 'pry'; binding.pry
+    # got the if statement to work properly... it's the :basic that's the issue right now
+    # Huh... it works now. Unsure what I fixed
+
+    if player_1.deck.rank_of_card_at(0) != player_2.deck.rank_of_card_at(0)
+      # p "x"
+      :basic
+    elsif (player_1.deck.rank_of_card_at(0) == player_2.deck.rank_of_card_at(0)) && (player_1.deck.rank_of_card_at(2) == player_2.deck.rank_of_card_at(2))
+      :mutually_assured_destruction
+    elsif player_1.deck.rank_of_card_at(0) == player_2.deck.rank_of_card_at(0)
+       :war
+    else
+      p "Oops"
+    end
+  end
   # Create a winner method
     # Looks like another if statement
     # :basic - return player with higher rank_of_card_at(0)
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -39,12 +39,115 @@ class TurnTest < Minitest::Test
 
     turn = Turn.new(player_1, player_2)
     # SUCCESS!!!
+
+    # require 'pry'; binding.pry
+    # Write a test to check two players
+      # Do I really need to write this as a seperate test?
+      # Can I just use pry in my it_exists test
+      # created a attr_accessor
+      # [1] pry(#<TurnTest>)> turn.player_1
+      # => #<Player:0x00007fee9d81e078
+      #  @deck=
+      #   #<Deck:0x00007fee9d81e2d0
+      #    @cards=
+      #     [#<Card:0x00007fee9d81eca8 @rank=11, @suit=:club, @value="Jack">,
+      #      #<Card:0x00007fee9d81eb68 @rank=2, @suit=:spade, @value="Two">,
+      #      #<Card:0x00007fee9d81e8c0 @rank=8, @suit=:heart, @value="Eight">,
+      #      #<Card:0x00007fee9d81e5a0 @rank=14, @suit=:spade, @value="Ace">]>,
+      #  @name="Priya">
+      # [2] pry(#<TurnTest>)> turn.player_2
+      # => #<Player:0x00007fee9d81dfb0
+      #  @deck=
+      #   #<Deck:0x00007fee9d81e190
+      #    @cards=
+      #     [#<Card:0x00007fee9d81ec08 @rank=7, @suit=:heart, @value="Seven">,
+      #      #<Card:0x00007fee9d81ea50 @rank=13, @suit=:diamond, @value="King">,
+      #      #<Card:0x00007fee9d81e780 @rank=12, @suit=:club, @value="Queen">,
+      #      #<Card:0x00007fee9d81e460 @rank=3, @suit=:diamond, @value="Three">]>,
+      #  @name="Ricky">
+      # [3] pry(#<TurnTest>)> turn.spoils_of_war
+      # => []
   end
 
-  # Write a test to check two players
-  # Write a test to check if attributes are readable
   # Write a test to check type method
+  def test_what_type_of_turn
+    card_1 = Card.new(:club, 'Jack', 11)
+    card_2 = Card.new(:heart, 'Seven', 7)
+    card_3 = Card.new(:spade, 'Two', 2)
+    card_4 = Card.new(:diamond, 'King', 13)
+    card_5 = Card.new(:heart, 'Eight', 8)
+    card_6 = Card.new(:club, 'Queen', 12)
+    card_7 = Card.new(:spade, 'Ace', 14)
+    card_8 = Card.new(:diamond, 'Three', 3)
+    deck_1 = Deck.new([card_1, card_3, card_5, card_7])
+    deck_2 = Deck.new([card_2, card_4, card_6, card_8])
+    player_1 = Player.new('Priya', deck_1)
+    player_2 = Player.new('Ricky', deck_2)
+    turn = Turn.new(player_1, player_2)
+
+    # assert_equal :basic, turn.type
+      # for basic, card_1 and card_2 are NOT the same
+      # WORKS!
+
+    # assert_equal :war, turn.type
+      # for war, card_1 and card_2 MUST be the same
+      # Works!
+
+    # assert_equal :mutually_assured_destruction, turn.type
+      # for mutual, card_1 and card_2 must equal AND card_5 and card_6 must match
+      # realized order of if statement was IMPORTANT!!
+
+    # TESTED EDGE CASE to ensure that mutually assured and war don't interfere
+  end
+
+
   # Write a test to check winner method
+  # def test_winner
+  # end
+  # card_1 = Card.new(:club, 'Jack', 11)
+  # card_2 = Card.new(:heart, 'Seven', 7)
+  # card_3 = Card.new(:spade, 'Two', 2)
+  # card_4 = Card.new(:diamond, 'King', 13)
+  # card_5 = Card.new(:heart, 'Eight', 8)
+  # card_6 = Card.new(:club, 'Queen', 12)
+  # card_7 = Card.new(:spade, 'Ace', 14)
+  # card_8 = Card.new(:diamond, 'Three', 3)
+  # deck_1 = Deck.new([card_1, card_3, card_5, card_7])
+  # deck_2 = Deck.new([card_2, card_4, card_6, card_8])
+  # player_1 = Player.new('Priya', deck_1)
+  # player_2 = Player.new('Ricky', deck_2)
+  # turn = Turn.new(player_1, player_2)
+
+
   # Write a test to check pile_cards method
+
+  # card_1 = Card.new(:club, 'Jack', 11)
+  # card_2 = Card.new(:heart, 'Seven', 7)
+  # card_3 = Card.new(:spade, 'Two', 2)
+  # card_4 = Card.new(:diamond, 'King', 13)
+  # card_5 = Card.new(:heart, 'Eight', 8)
+  # card_6 = Card.new(:club, 'Queen', 12)
+  # card_7 = Card.new(:spade, 'Ace', 14)
+  # card_8 = Card.new(:diamond, 'Three', 3)
+  # deck_1 = Deck.new([card_1, card_3, card_5, card_7])
+  # deck_2 = Deck.new([card_2, card_4, card_6, card_8])
+  # player_1 = Player.new('Priya', deck_1)
+  # player_2 = Player.new('Ricky', deck_2)
+  # turn = Turn.new(player_1, player_2)
+
   # Write a test to check award_spoils method
+
+  # card_1 = Card.new(:club, 'Jack', 11)
+  # card_2 = Card.new(:heart, 'Seven', 7)
+  # card_3 = Card.new(:spade, 'Two', 2)
+  # card_4 = Card.new(:diamond, 'King', 13)
+  # card_5 = Card.new(:heart, 'Eight', 8)
+  # card_6 = Card.new(:club, 'Queen', 12)
+  # card_7 = Card.new(:spade, 'Ace', 14)
+  # card_8 = Card.new(:diamond, 'Three', 3)
+  # deck_1 = Deck.new([card_1, card_3, card_5, card_7])
+  # deck_2 = Deck.new([card_2, card_4, card_6, card_8])
+  # player_1 = Player.new('Priya', deck_1)
+  # player_2 = Player.new('Ricky', deck_2)
+  # turn = Turn.new(player_1, player_2)
 end
