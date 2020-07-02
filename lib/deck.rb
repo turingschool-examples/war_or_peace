212a3d3196fc25892df2267181fe597502ec7f85
Corey Carter <crc2231@gmail.com>
2020-07-02 09:41:16 -0600

    Iteration 1 add rank_of_cards_at

--- lib/deck.rb
+++ lib/deck.rb
@@ -8,15 +8,26 @@ class Deck
     @cards = cards_param
   end
   #
-  # def rank_of_card_at (location)
-  #   if location == 0
-  #     card_rank = deck.cards[0].rank
-  #   elsif location == 2
-  #     card_rank = deck.cards[2].rank
+  def rank_of_card_at (location)
+    if location == 0
+      card_rank = cards[0].rank
+    elsif location == 2
+      card_rank = cards[2].rank
+    else
+      card_rank = nil
+    end
+    card_rank
+  end
+
+  # def high_ranking_cards (cards)
+  #   high_cards = []
+  #   cards.each.do |card|
+  #   if card.rank >= 11
+  #     high_cards << card
+  #     high_cards
   #   else
-  #   end
+  #     high_cards
   # end
-# rank_of_card_at: this method will take one argument that represents the index location of a card to be used (typically 0 or 2 more on this later 😉 ) and will return the rank of that card.
 # high_ranking_cards: this method will return an array of cards in the deck that have a rank of 11 or above (face cards and aces)
 # percent_high_ranking: this method will return the percentage of cards that are high ranking
 # remove_card: this method will remove the top card from the deck
--- test/DeckTest.rb
+++ test/DeckTest.rb
@@ -5,7 +5,20 @@ require './lib/card'
 
 class DeckTest < Minitest::Test
 
+###Attempting to create a setup function to streamline testing.
+# ##Tried "Let" and "Before" as well.
+# def setup
+#   @card1 = Card.new('heart', '5', 5)
+#   @card2 = Card.new('spade', 'jack', 11)
+#   @card3 = Card.new('diamond', 'ace', 14)
+#   @deck = Deck.new(cards)
+#   cards = [card1, card2, card3]
+# end
+
+
+
   def test_it_exists
+
     card1 = Card.new('heart', '5', 5)
     card2 = Card.new('spade', 'jack', 11)
     card3 = Card.new('diamond', 'ace', 14)
@@ -27,16 +40,28 @@ class DeckTest < Minitest::Test
     assert_equal 14, deck.cards[2].rank
   end
 
-  # def test_rank_card_at_method
+  def test_rank_card_at_method
+    card1 = Card.new('heart', '5', 5)
+    card2 = Card.new('spade', 'jack', 11)
+    card3 = Card.new('diamond', 'ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal 5, deck.rank_of_card_at(0)
+    assert_equal 14, deck.rank_of_card_at(2)
+    assert_nil deck.rank_of_card_at(1)
+
+  end
+  #
+  # def test_high_ranking_cards_method
   #   card1 = Card.new('heart', '5', 5)
   #   card2 = Card.new('spade', 'jack', 11)
   #   card3 = Card.new('diamond', 'ace', 14)
   #   cards = [card1, card2, card3]
   #   deck = Deck.new(cards)
   #
-  #   assert_equal 5, rank_of_card_at(0)
-  #   assert_equal 14, rank_of_card_at(2)
-  #   assert_equal nil, rank_of_card_at(1)
+  #   assert_great_than 11, high_cards(0).rank
+  #   assert_great_than 11, high_cards(1).rank
   # end
 
 end
