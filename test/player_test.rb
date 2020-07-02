0abc903c473cc1d09174a9f3cc3064903a265270
Carson Jardine <carsonjardine@gmail.com>
2020-07-01 20:29:29 -0600

    Update player test file

---
+++ test/player_test.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+
+class PlayerTest < Minitest::Test
+  def setup
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @deck = Deck.new([@card1, @card2, @card3])
+    @player = Player.new('Clarisa', @deck)
+  end
+
+  def test_it_exists
+    assert_instance_of Player, @player
+  end
+
+  def test_it_shows_name
+
+    assert_equal 'Clarisa', @player.name
+  end
+
+  def test_has_lost
+    assert_equal !@cards.nil?, @player.has_lost?
+  end
+
+end # this is the end of the class!!!
