9d99f96b114b6e0a12a0ecb7d070aadae526513a
Carson Jardine <carsonjardine@gmail.com>
2020-07-02 21:49:31 -0600

    Bring code from main branch

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,6 +35,57 @@ class Turn
     end
   end
 
+  def pile_cards
+    if type() == :basic
+      # require "pry"; binding.pry
+      @spoils_of_war << @player1.deck.cards[0]
+      @spoils_of_war << @player2.deck.cards[0]
+      @player1.deck.remove_card
+      @player2.deck.remove_card
+
+      return @spoils_of_war
+
+    elsif type() == :war
+      @spoils_of_war << @player1.deck.cards[0..2]
+      @spoils_of_war << @player2.deck.cards[0..2]
+
+      3.times do
+        @player1.deck.remove_card
+      end
+
+      3.times do
+        @player2.deck.remove_card
+      end
+
+      return @spoils_of_war
+
+    else
+      3.times do
+        @player1.deck.remove_card
+      end
+
+      3.times do
+        @player2.deck.remove_card
+      end
+
+      @player1.deck.cards
+      @player2.deck.cards
+    end
+  end
+
+def award_spoils
+  if type() == :basic
+    if @player1.winner == true
+      award_spoils = @player1.deck.cards << @spoils_of_war.pile_cards
+    else
+      award_spoils = @player2.deck.cards << @spoils_of_war.pile_cards
+    end
+  else
+    return "NOPEEEEE"
+  end
+end
+
+
 
 
 end # this is the end of the class!!!
