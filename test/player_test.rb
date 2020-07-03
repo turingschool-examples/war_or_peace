fdbc76fcff69b86cb2253fd29c59143a8736cc59
Corey Carter <crc2231@gmail.com>
2020-07-02 21:09:33 -0600

    Iteration 2 - Add test_player_has_deck

--- test/player_test.rb
+++ test/player_test.rb
@@ -11,13 +11,15 @@ class DeckTest < Minitest::Test
     @card1 = Card.new('heart', '5', 5)
     @card2 = Card.new('spade', 'jack', 11)
     @card3 = Card.new('diamond', 'ace', 14)
-    @deck1 = Deck.new([@card1, @card2, @card3])
+    @cards1 = [@card1, @card2, @card3]
+    @deck1 = Deck.new(@cards1)
     @player1 = Player.new('Megan', @deck1 )
 
     @card4 = Card.new('club', '5', 5)
     @card5 = Card.new('heart', 'king', 13)
     @card6 = Card.new('club', '8', 8)
-    @deck2 = Deck.new([@card4, @card5, @card6])
+    @cards2 = [@card4, @card5, @card6]
+    @deck2 = Deck.new(@cards2)
     @player2 = Player.new('Aurora', @deck2)
 
   end
@@ -27,10 +29,12 @@ class DeckTest < Minitest::Test
     assert_instance_of Player, @player1
 
   end
-  #
-  # def test_player_has_cards
-  #   assert_equal 'diamond', @player1.deck.cards[3].suit
-  # end
+
+  def test_player_has_deck
+
+    assert_instance_of Deck, @player1.deck
+    assert_equal 'spade', @player1.deck.cards[1].suit
+  end
 
   # def player_has_lost
   #   assert_equal 'false', player.has_lost?
