bee886b71d77657468d0de8d1f7bf6041571b991
Garret Wolf <garretwolf26@gmail.com>
2020-07-03 18:02:33 -0400

    Create runner file

---
+++ war_or_peace_runner.rb
@@ -0,0 +1,5 @@
+# Create 52 cards (standard deck)
+# Put cards into 2 decks(random)
+# Create 2 players with the decks
+# Start game with a start method
+# start method can be in a new class or existing class
