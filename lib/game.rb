05a2245a080f0d633fe22a45c37d4d707eb5ef5b
Angela Guardia <angelaguardia20@gmail.com>
2020-07-03 14:20:06 -0700

    Add game logic

--- lib/game.rb
+++ lib/game.rb
@@ -9,30 +9,22 @@ class Game
   def start
     display_greeting
     get_user_input
+
     if @signal == "GO"
       counter = 0
       until @player1.has_lost? || @player2.has_lost?
-        # TODO: Make this it's own method
-        if counter == 999_999
-          puts "---- DRAW ----"
-          break
-        end
+        break if end_game?(counter)
         turn = Turn.new(@player1, @player2)
         turn_winner = turn.winner
+        turn_message(turn, counter, turn_winner)
         turn.pile_cards
-        unless turn_winner == "No Winner"
-          turn.award_spoils(turn_winner)
-        end
-        turn_message(turn, turn_winner, counter)
+        determine_spoils(turn, turn_winner)
         counter += 1
       end
-    require 'pry'; binding.pry
-      # if @player1.has_lost?
-      #   turn
 
-
+      game_winner = determine_winner()
+      announce_winner(game_winner)
     end
-
   end
 
   def display_greeting
@@ -43,14 +35,38 @@ class Game
     @signal = gets.chomp.upcase
   end
 
-  def turn_message(turn, winner, counter)
+  def end_game?(counter)
+    if counter == 999_999
+      puts "---- DRAW ----"
+      true
+    end
+  end
+
+  def turn_message(turn, counter, turn_winner)
     if turn.type == :basic
-      puts "Turn #{counter + 1}: #{winner.name} won 2 cards"
+      puts "Turn #{counter + 1}: #{turn_winner.name} won 2 cards - for a total of #{turn_winner.deck.cards.length + 2 }"
     elsif turn.type == :war
-      puts "Turn #{counter + 1}: WAR - #{winner.name} won 6 cards"
+      puts "Turn #{counter + 1}: WAR - #{turn_winner.name} won 6 cards - for a total of #{turn_winner.deck.cards.length + 6 }"
     else
       puts "*mutually assured destruction* 6 cards removed from play"
     end
   end
 
+  def determine_spoils(turn, turn_winner)
+    unless turn_winner == "No Winner"
+      turn.award_spoils(turn_winner)
+    end
+  end
+
+  def determine_winner
+    if @player1.has_lost?
+      @player2
+    else
+      @player1
+    end
+  end
+
+  def announce_winner(winner)
+    puts "*~*~*~* #{winner.name} has won the game! *~*~*~*"
+  end
 end
