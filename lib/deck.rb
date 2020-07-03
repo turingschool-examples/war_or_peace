117b55a67eaeeb04ef4e3a3ffe000d39b13b5981
Corey Carter <crc2231@gmail.com>
2020-07-02 18:09:09 -0600

    Iteration 1 - Add Remove Card Method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,6 +1,7 @@
+require './lib/card'
 
 class Deck
-  attr_reader :cards
+  attr_reader :cards, :high_cards
   # initialize: this method will take one argument - an array of cards.
   # cards: an attr_reader to read the @cards attribute
 
@@ -19,18 +20,30 @@ class Deck
     card_rank
   end
 
-  # def high_ranking_cards (cards)
-  #   high_cards = []
-  #   cards.each.do |card|
-  #   if card.rank >= 11
-  #     high_cards << card
-  #     high_cards
-  #   else
-  #     high_cards
+  def high_ranking_cards
+
+    high_cards = []
+    cards.each do |card|
+      if card.rank >= 11
+        high_cards << card
+      end
+    end
+    high_cards
+
+  end
+
+  def percent_high_ranking
+    high_rank_length = high_ranking_cards.length
+    ((high_rank_length / @cards.length.to_f) * 100).round(2)
+  end
+
+  def remove_card
+    @cards.shift
+  end
+
+  # def add_card (suit_param, value_param, rank_param)
+  #   new_card = Card.new
   # end
-# high_ranking_cards: this method will return an array of cards in the deck that have a rank of 11 or above (face cards and aces)
-# percent_high_ranking: this method will return the percentage of cards that are high ranking
-# remove_card: this method will remove the top card from the deck
 # add_card: this method will add one card to the bottom (end) of the deck
 
 end
---
+++ test/deck_test.rb
@@ -0,0 +1,59 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/deck'
+require './lib/card'
+
+class DeckTest < Minitest::Test
+
+def setup
+  @card1 = Card.new('heart', '5', 5)
+  @card2 = Card.new('spade', 'jack', 11)
+  @card3 = Card.new('diamond', 'ace', 14)
+  @cards = [@card1, @card2, @card3]
+  @deck = Deck.new(@cards)
+end
+
+
+
+  def test_it_exists
+
+    assert_instance_of Deck, @deck
+  end
+
+  def test_cards_have_readable_attributes
+
+    assert_equal 'heart', @deck.cards[0].suit
+    assert_equal 'jack', @deck.cards[1].value
+    assert_equal 14, @deck.cards[2].rank
+  end
+
+  def test_rank_card_at_method
+
+    assert_equal 5, @deck.rank_of_card_at(0)
+    assert_equal 14, @deck.rank_of_card_at(2)
+    assert_nil @deck.rank_of_card_at(1)
+
+  end
+
+  def test_high_ranking_cards_method
+
+    assert_operator 10, :<, @deck.high_ranking_cards[0].rank
+    assert_operator 10, :<, @deck.high_ranking_cards[1].rank
+
+  end
+
+  def test_percent_high_ranking
+
+    assert_equal 66.67, @deck.percent_high_ranking
+
+  end
+
+  def test_add_card_method
+    @deck.remove_card
+
+    assert_equal 2, @cards.length
+    assert_equal 'spade', @deck.cards[0].suit
+
+  end
+
+end
