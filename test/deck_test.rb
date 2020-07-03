5ec1abbc33171b32ed31210ff65b1846d0e7f076
PhilipDeFraties <philipjfry338@gmail.com>
2020-07-02 20:14:08 -0600

    Comment out binding.pry

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -24,18 +24,18 @@ class DeckTest < Minitest::Test
   #   assert_equal [card1, card2, card3], deck.cards
   # end
 
-#   def test_cards_have_rank
-#     card1 = Card.new(:diamond, 'Queen', 12)
-#     card2 = Card.new(:spade, '3', 3)
-#     card3 = Card.new(:heart, 'Ace', 14)
-#
-#     cards = [card1, card2, card3]
-#
-#     deck = Deck.new(cards)
-#
-#     assert_equal 12, deck.rank_of_card_at(0)
-#     binding.pry
-#   end
+  # def test_cards_have_rank
+  #   card1 = Card.new(:diamond, 'Queen', 12)
+  #   card2 = Card.new(:spade, '3', 3)
+  #   card3 = Card.new(:heart, 'Ace', 14)
+  #
+  #   cards = [card1, card2, card3]
+  #
+  #   deck = Deck.new(cards)
+  #
+  #   assert_equal 12, deck.rank_of_card_at(0)
+  #   #binding.pry
+  # end
 
 
   # def test_percentage_of_high_ranking_cards
@@ -48,7 +48,7 @@ class DeckTest < Minitest::Test
   #   deck = Deck.new(cards)
   #
   #   assert_equal 66.67, deck.percent_high_ranking
-  #   binding.pry
+  #   # binding.pry
   # end
 
     # def test_card_can_be_removed
