0cfd1fe26d20cca7e0897eef0e9c80de9d47ee5a
Travis McKinstry <mckinstrytg@gmail.com>
2020-07-03 14:45:57 -0600

    split each character up into a method so each character can be focused on

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,6 +1,6 @@
 class CardGenerator
 
-  attr_reader :results, :file
+  attr_reader :results, :file,
 
   def initialize
     file = File.open("./lib/cards.txt")
@@ -8,9 +8,17 @@ class CardGenerator
   end
 
   def show_text_file
-    @results.length
+    @results
+  end
+
+  def show_each_character_by_type
+    @results.split("\n").collect{|x| sep = x.split(" ")
+      values = sep[0]
+      ranks = sep[2]
+      suits = sep[1]
+}
   end
 end
 
-# new_cards = CardGenerator.new
-# new_cards.show_text_file
+new_cards = CardGenerator.new
+new_cards.show_each_character_by_type
--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -10,7 +10,9 @@ class CardGeneratorTest < Minitest::Test
     assert_instance_of CardGenerator, new_cards
   end
 
+
   def test_it_has_a_text_file_to_read
+    skip
     new_cards = CardGenerator.new
 
 
@@ -20,4 +22,6 @@ class CardGeneratorTest < Minitest::Test
   def test_it_can_read_each_word_independently
     new_cards = CardGenerator.new
 
+    assert_equal new_cards.show_each_character, new_cards.show_text_file
+  end
 end
