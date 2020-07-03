547927f2a6b0df6903e61450f3ecf3457bd518ed
Arique Aguilar <arique1104@gmail.com>
2020-07-02 20:16:16 -0500

    Required files complete

---
+++ test/game_test.rb
@@ -0,0 +1,20 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "mocha/minitest"
+require "./lib/card"
+require "./lib/deck"
+require "./lib/player"
+require "./lib/turn"
+require "./lib/standard_deck"
+require "./lib/game"
+
+class GameTest < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+end
