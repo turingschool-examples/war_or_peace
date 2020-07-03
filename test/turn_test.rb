4c56e8b5f33c2769a6f44a7cf41ae35382cbaa2c
Garret Wolf <garretwolf26@gmail.com>
2020-07-03 09:48:30 -0400

    Add tests for each turn type and winners

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -22,4 +22,34 @@ class TurnTest < Minitest::Test
     @turn = Turn.new(@player1, @player2)
   end
 
+  def test_it_exists
+    assert_instance_of Turn, @turn
+  end
+
+# Testing the basic turn type
+  def test_it_has_basic_turn_type
+    assert_equal :basic, @turn.type
+  end
+
+  def test_it_returns_winner_for_basic
+    assert_equal @player1, @turn.winner
+  end
+
+# Testing the war turn type
+  def test_it_has_war_turn_type
+    assert_equal :war, @turn.type
+  end
+
+  def test_it_returns_winner_for_war
+    assert_equal @player2, @turn.winner
+  end
+  
+# Testing the M.A.D turn type
+  def test_it_has_mutually_assured_destruction_turn_type
+    assert_equal :mutually_assured_destruction, @turn.winner
+  end
+
+  def test_it_returns_winner_for_mutually_assured_destruction
+    assert_equal "No Winner", @turn.winner
+
 end
