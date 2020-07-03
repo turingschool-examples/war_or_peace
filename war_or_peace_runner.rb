4d79f68950af4beb32bd63a1b80252aaadd8496c
Angela Guardia <angelaguardia20@gmail.com>
2020-07-03 15:26:29 -0700

    Add small decks for testing

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -33,6 +33,36 @@ deck2 = Deck.new(cards2)
 player1 = Player.new("Linda", deck1)
 player2 = Player.new("Bob", deck2)
 
+# card1 = Card.new(:heart, 'Jack', 11)
+# card2 = Card.new(:heart, '10', 10)
+# card3 = Card.new(:heart, '9', 9)
+# card4 = Card.new(:diamond, 'Jack', 11)
+# card5 = Card.new(:heart, '8', 8)
+# card6 = Card.new(:diamond, 'Queen', 12)
+# card7 = Card.new(:heart, '3', 3)
+# card8 = Card.new(:diamond, '2', 2)
+#
+# # Bacic Turn 11, 10, 8, 2
+# deck1 = Deck.new([card1, card2, card5, card8])
+# deck2 = Deck.new([card3, card4, card6, card7])
+#
+# player1 = Player.new("Megan", deck1)
+# player2 = Player.new("Aurora", deck2)
+#
+# # War Turn
+# deck3 = Deck.new([card4, card3, card6, card7])
+# player3 = Player.new("Harry", deck3)
+#
+# # Mutually Assured Destruction (MAD) Turn
+# card9 = Card.new(:diamond, '8', 8)
+# deck4 = Deck.new([card4, card3, card9, card7])
+# player4 = Player.new("Charles", deck4)
+#
+# # One player has 2 Cards and there is war
+# # 9, 2, 8, 11
+# deck5 = Deck.new([card3, card8, card5, card1])
+# player5 = Player.new("Bubba", deck5)
+
 # Start Game
 game = Game.new(player1, player2)
 game.start
