6dcfeff8fb3920c96e135adc80dc2227ad97f179
Shaun James <ShaunDaneJames@Gmail.com>
2020-07-03 09:08:20 -0600

    Fixed has lost method

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,19 @@
 class Player
-
-  def initialize()
+  attr_reader :name, :deck
+  def initialize(name, deck)
+    @name = name
+    @deck = deck
   end
-end 
+
+  def has_lost?
+    #there has to be a better way. I couldn't get the follow two ideas to work
+    # true if self.deck.cards == []
+    #self.deck.cards == []
+    if self.deck.cards == []
+      true
+    else false
+    end
+  end
+
+
+end
