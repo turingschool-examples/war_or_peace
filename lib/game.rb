5e7ec328c99dca42e6513cfafa623b59fae84140
Arique Aguilar <arique1104@gmail.com>
2020-07-02 20:20:43 -0500

    Passing test_it_exists code

--- lib/game.rb
+++ lib/game.rb
@@ -1,1 +1,10 @@
 require_relative "./turn"
+
+class Game
+ #attr_reader
+  def initialize(player1, player2)
+
+
+  end
+
+end
--- test/game_test.rb
+++ test/game_test.rb
@@ -11,7 +11,16 @@ require "./lib/game"
 class GameTest < Minitest::Test
 
   def test_it_exists
+    standard_deck = StandardDeck.new
+    cards = standard_deck.cards.shuffle
+    deck1 = Deck.new(cards[0..25])
+    deck2 = Deck.new(cards[26..52])
+
+    player1 = Player.new("Saryn", deck1)
+    player2 = Player.new("Dan", deck2)
+
     game = Game.new(player1, player2)
+
     assert_instance_of Game, game
   end
 
