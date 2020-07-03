62342b851184c9464052f22dbc07429c25a3ecf1
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-02 20:06:53 -0600

    Complete turn.rb file

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,29 +12,38 @@ class Turn
     #in this method, it is possible to only create one other condition to be met, then the last else could be the last turn type.
     if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
       :basic
-    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) != player2.deck.rank_of_card_at(2)
       :war
+    else
+      :mutually_assured_destruction
     end
+
   end
 
   def winner
-    if type == :basic && player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
-      return  player1
-    else
-      return  player2
-    end
-    if type == :war && player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
-      return  player1
-    else
-      return  player2
-    end
+    return player1 if type == :basic && player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+    return player2 if type == :basic && player1.deck.rank_of_card_at(0) < player2.deck.rank_of_card_at(0)
+    return player1 if type == :war && player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+    return player2 if type == :war && player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) < player2.deck.rank_of_card_at(2)
+    return "No Winner" if type == :mutually_assured_destruction
   end
 
   def pile_cards
     #remove_card has two actions, it removes a card from the deck and it returns that card. The returned card is being shoveled in to the spoils_of_war empty array
+    if type == :basic
 
-    @spoils_of_war << player1.deck.remove_card
-    @spoils_of_war << player2.deck.remove_card
+      @spoils_of_war << player1.deck.remove_card
+      @spoils_of_war << player2.deck.remove_card
+      @spoils_of_war.flatten!
+    elsif type == :war
+      @spoils_of_war << player1.deck.cards.shift(3)
+      @spoils_of_war << player2.deck.cards.shift(3)
+      @spoils_of_war.flatten!
+    else type == :mutually_assured_destruction
+      player1.deck.cards.shift(3)
+      player2.deck.cards.shift(3)
+    end
+
   end
 
   def awards_spoils(winner)
