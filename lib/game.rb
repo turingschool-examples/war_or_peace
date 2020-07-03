a42620143227ed183c869f01a106b8b6a0d2f82e
Angela Guardia <angelaguardia20@gmail.com>
2020-07-03 15:25:17 -0700

    Fix game logic

--- lib/game.rb
+++ lib/game.rb
@@ -4,6 +4,7 @@ class Game
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
+    @players = [@player1, @player2]
   end
 
   def start
@@ -12,16 +13,23 @@ class Game
 
     if @signal == "GO"
       counter = 0
+      turn = Turn.new(@player1, @player2)
       until @player1.has_lost? || @player2.has_lost?
         break if end_game?(counter)
-        turn = Turn.new(@player1, @player2)
+        # Ends game when a player doesn't have enough cards to play
+        if (@player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)) && (@player1.deck.cards.length < 3 || @player2.deck.cards.length < 3)
+          loser = @players.min { |player1, player2| player1.deck.cards.length <=> player2.deck.cards.length }
+          loser.deck.cards = []
+          break
+        end
         turn_winner = turn.winner
-        turn_message(turn, counter, turn_winner)
+        current_turn_type = turn.type
         turn.pile_cards
         determine_spoils(turn, turn_winner)
+        turn_message(counter, turn_winner, current_turn_type)
         counter += 1
       end
-
+      exit if counter == 999_999
       game_winner = determine_winner()
       announce_winner(game_winner)
     end
@@ -42,11 +50,11 @@ class Game
     end
   end
 
-  def turn_message(turn, counter, turn_winner)
-    if turn.type == :basic
-      puts "Turn #{counter + 1}: #{turn_winner.name} won 2 cards - for a total of #{turn_winner.deck.cards.length + 2 }"
-    elsif turn.type == :war
-      puts "Turn #{counter + 1}: WAR - #{turn_winner.name} won 6 cards - for a total of #{turn_winner.deck.cards.length + 6 }"
+  def turn_message(counter, turn_winner, current_turn_type)
+    if current_turn_type == :basic
+      puts "Turn #{counter + 1}: #{turn_winner.name} won 2 cards - for a total of #{turn_winner.deck.cards.length}"
+    elsif current_turn_type == :war
+      puts "Turn #{counter + 1}: WAR - #{turn_winner.name} won 6 cards - for a total of #{turn_winner.deck.cards.length}"
     else
       puts "*mutually assured destruction* 6 cards removed from play"
     end
