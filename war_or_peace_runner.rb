897935905f5216b6435299540a21634efad4d182
Angela Guardia <angelaguardia20@gmail.com>
2020-07-03 21:14:20 -0700

    Include new card generator in runner

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -2,22 +2,28 @@ require './lib/card'
 require './lib/deck'
 require './lib/player'
 require './lib/game'
+require './lib/Card_Generator'
 
+# ORIGINAL CARD GENERATION
+# # Generating the standad deck
+# card_ranks = (2..14).to_a
+# high_card_values = { 11 => "Jack", 12 => "Queen", 13 => "King", 14 => "Ace" }
+# suits = [:heart, :spade, :club, :diamond]
+#
+# standard_deck = card_ranks.map do |rank|
+#   suits.map do |suit|
+#     if rank < 11
+#       Card.new(suit, rank.to_s, rank)
+#     else
+#       Card.new(suit, high_card_values[rank], rank)
+#     end
+#   end
+# end.flatten!
 
-# Generating the standad deck
-card_ranks = (2..14).to_a
-high_card_values = { 11 => "Jack", 12 => "Queen", 13 => "King", 14 => "Ace" }
-suits = [:heart, :spade, :club, :diamond]
-
-standard_deck = card_ranks.map do |rank|
-  suits.map do |suit|
-    if rank < 11
-      Card.new(suit, rank.to_s, rank)
-    else
-      Card.new(suit, high_card_values[rank], rank)
-    end
-  end
-end.flatten!
+# ITERATION 4 CARD GENERATION
+card_gen = CardGenerator.new("cards.txt")
+card_gen.generate_cards
+standard_deck = card_gen.cards
 
 # Generating Player Decks
 random_deck = standard_deck.shuffle!
