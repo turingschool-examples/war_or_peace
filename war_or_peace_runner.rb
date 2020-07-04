fdd67fbdec108e4eb87e89f2b41dac575805cc16
Garret Wolf <garretwolf26@gmail.com>
2020-07-04 17:37:33 -0400

    Create class for new game

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -3,3 +3,12 @@
 # Create 2 players with the decks
 # Start game with a start method
 # start method can be in a new class or existing class
+
+require './lib/deck'
+require './lib/card'
+require './lib/player'
+require './lib/turn'
+
+class NewGame
+
+end
