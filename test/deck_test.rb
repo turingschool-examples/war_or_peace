50f248afd3be7c19a2eb1f923a254e6267f28fe0
PhilipDeFraties <philipjfry338@gmail.com>
2020-07-03 20:33:56 -0600

    Add turn class, test, and runner files with initialize method and test.

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -12,7 +12,7 @@ class DeckTest < Minitest::Test
   #   assert_instance_of Deck, deck
   # end
 
-  # def test_it_has_attributes
+  # def test_it_has_readable_attributes
   #   card1 = Card.new(:diamond, 'Queen', 12)
   #   card2 = Card.new(:spade, '3', 3)
   #   card3 = Card.new(:heart, 'Ace', 14)
