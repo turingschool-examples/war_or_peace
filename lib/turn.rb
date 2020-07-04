e780ec5e2e4864aad6ac8c4b4553072d5d17d34e
Daniel Lessenden <dless27@gmail.com>
2020-07-04 14:34:42 -0500

    Add turn.rb

---
+++ lib/turn.rb
@@ -0,0 +1,71 @@
+class Turn
+attr_reader :player1, :player2, :spoils_of_war
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @spoils_of_war = []
+    #@winner = nil
+  end
+
+  def type
+    if @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
+      :mutually_assured_destruction
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
+      :war
+    elsif @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
+      :basic
+    end
+  end
+
+   def winner
+    if type() == :mutually_assured_destruction
+      p "No winner"
+    elsif type() == :basic
+        if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
+          # @winner =
+          @player1
+        else @player1.deck.rank_of_card_at(0) < @player2.deck.rank_of_card_at(0)
+          # @winner =
+          @player2
+         end
+    elsif type() == :war
+        if @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
+          # @winner =
+          @player1
+        else @player1.deck.rank_of_card_at(2) < @player2.deck.rank_of_card_at(2)
+          # @winner =
+          @player2
+        end
+      end
+     end
+
+   def pile_cards
+
+      if type() == :basic
+        @spoils_of_war << @player1.deck.cards.slice!(0)
+        @spoils_of_war << @player2.deck.cards.slice!(0)
+      elsif type() == :war
+        @spoils_of_war.concat(player1.deck.cards.slice!(0..2))
+        @spoils_of_war.concat(player2.deck.cards.slice!(0..2))
+      else :mutually_assured_destruction
+        3.times do
+          @player1.deck.remove_card
+        end
+        3.times do
+          @player2.deck.remove_card
+        end
+      end
+   end
+
+   def award_spoils(winner)
+     winner.deck.cards.concat(@spoils_of_war)
+   end
+end
+
+
+
+# add an if to basic, if first cardvalue=
+#need a turn counter
+#may be trouble with @winner as an instance variable
+#def winner use @player.name
