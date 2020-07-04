bcedf33bf348c0702920d827cbb021361575baba
Priya Power <priyajag72@gmail.com>
2020-07-04 16:50:16 -0600

    Decided to nuke game class and start over.

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -1,85 +1,147 @@
-class Game
-
-  # Create a game with turns and a standard deck
-  # can call upon other classes
-  # Class cards = help with creation of cards
-  # Class deck = help with creation of deck
-  # Class player = help with creation of player
-  # Class turn = help with creation of turns
-
-  # Hmmmm..... this isn't working...
-  # What are the variables associated with a game
-  # There are two players, lots of turns, a standard
-  # deck, each players deck, etc...
-  # Well... turns are dealt with as a class
-  # the decks are also a class
-  # players are a class...
-  # so what does the Game Class actually need
-  # Okay.. after researching what initialize does
-  # figured out I don't want to initialize turn with a new instance of game
-  # because we need the turns to apply to the game
-  # Don't need to initialize a deck, same reason
-  # I probably need to initialize two players
-
-
-  # attr_accessor :turn, :standard_deck
-  # def initialize(turn, standard_deck)
-  #   @turn = turn
-  #   @standard_deck = standard_deck
-  # end
-
-  attr_accessor :player_1, :player_2
-
-  def initialize(player_1, player_2)
-    @player_1 = player_1
-    @player_2 = player_2
-  end
-
-  def standard_deck
-    # Hmmm... how do I create a deck
-    # I'm gonna want to use .times (or similar)
-    # I can try and create a section for each suit
-    # the integer cards can be .times
-    # might have to create the royals (is there an easier way??)
-    # RESEARCH!!
-    # Let's also look at my own deck.rb file
-    # My deck file doesn't harm or help my standard_deck method
-    # Research tells me I can use .each
-    # Read through deck building for ruby on Stack Overflow
-    # Found something worth testing or trying
-    # changed it and annoted it to match my files
-    # testing
-
-    # created accumulator to hold array of cards created
-    deck_holder = []
-    # created array of ranks
-    # %w is a shortcut for for writing an array of strings seperated by space vs comma
-    ranks = %w{Two Three Four Five Six Seven Eight Nine Ten Jack Queen King Ace}
-    # created array of suits
-    suits = %w{Clubs, Diamonds, Hearts, Spades}
-    # called array suits and applied .each (block variable = suit)
-    # .each will iterate through each suit
-    suits.each do |suit|
-      # called array ranks
-      # .size returns the number of elements in the array
-      # In this case the .size = 13
-      # .times will repeat
-      # So in combination, .size.times will now do this 13 times
-      # Or, one for every element in the array!
-      # block variable is integer (so will start at 0)
-      ranks.size.times do |i|
-        # my array will get added to by calling an instance of the Card Class
-        # New card created from suits array, ranks array, and then adding two to the index value of the integer from do block
-        deck_holder << Card.new(suit, ranks[i], i+2)
-      end
-    end
-    deck_holder
-  end
-
-  # Looking ahead at iteration 4... I'm unsure about the hard coded deck
-  # I don't think mine is a hard-coded deck
-  # Should I be adding that in?
+# Needed a major break... concluded I can NUKE it
+# Realized I should probably create the deck and shuffle in the test file
+# This way, I can also practice SETUP method
+# See old code waaay down below
 
 
 
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # class Game
+        #
+        #   # Create a game with turns and a standard deck
+        #   # can call upon other classes
+        #   # Class cards = help with creation of cards
+        #   # Class deck = help with creation of deck
+        #   # Class player = help with creation of player
+        #   # Class turn = help with creation of turns
+        #
+        #   # Hmmmm..... this isn't working...
+        #   # What are the variables associated with a game
+        #   # There are two players, lots of turns, a standard
+        #   # deck, each players deck, etc...
+        #   # Well... turns are dealt with as a class
+        #   # the decks are also a class
+        #   # players are a class...
+        #   # so what does the Game Class actually need
+        #   # Okay.. after researching what initialize does
+        #   # figured out I don't want to initialize turn with a new instance of game
+        #   # because we need the turns to apply to the game
+        #   # Don't need to initialize a deck, same reason
+        #   # I probably need to initialize two players
+        #
+        #
+        #   # attr_accessor :turn, :standard_deck
+        #   # def initialize(turn, standard_deck)
+        #   #   @turn = turn
+        #   #   @standard_deck = standard_deck
+        #   # end
+        #
+        #   attr_accessor :player_1, :player_2, :deck_holder
+        #
+        #   def initialize(player_1, player_2)
+        #     @player_1 = player_1
+        #     @player_2 = player_2
+        #     @deck_holder = []
+        #   end
+        #
+        #   def standard_deck
+        #     # Hmmm... how do I create a deck
+        #     # I'm gonna want to use .times (or similar)
+        #     # I can try and create a section for each suit
+        #     # the integer cards can be .times
+        #     # might have to create the royals (is there an easier way??)
+        #     # RESEARCH!!
+        #     # Let's also look at my own deck.rb file
+        #     # My deck file doesn't harm or help my standard_deck method
+        #     # Research tells me I can use .each
+        #     # Read through deck building for ruby on Stack Overflow
+        #     # Found something worth testing or trying
+        #     # changed it and annoted it to match my files
+        #     # testing
+        #
+        #     # created accumulator to hold array of cards created
+        #     # created array of ranks
+        #     # %w is a shortcut for for writing an array of strings seperated by space vs comma
+        #     ranks = %w{Two Three Four Five Six Seven Eight Nine Ten Jack Queen King Ace}
+        #     # created array of suits
+        #     suits = %w{Clubs, Diamonds, Hearts, Spades}
+        #     # called array suits and applied .each (block variable = suit)
+        #     # .each will iterate through each suit
+        #     suits.each do |suit|
+        #       # called array ranks
+        #       # .size returns the number of elements in the array
+        #       # In this case the .size = 13
+        #       # .times will repeat
+        #       # So in combination, .size.times will now do this 13 times
+        #       # Or, one for every element in the array!
+        #       # block variable is integer (so will start at 0)
+        #       ranks.size.times do |i|
+        #         # my array will get added to by calling an instance of the Card Class
+        #         # New card created from suits array, ranks array, and then adding two to the index value of the integer from do block
+        #         deck_holder << Card.new(suit, ranks[i], i+2)
+        #       end
+        #     end
+        #     deck_holder
+        #   end
+        #
+        #   # Looking ahead at iteration 4... I'm unsure about the hard coded deck
+        #   # I don't think mine is a hard-coded deck
+        #   # Should I be adding that in?
+        #   # Let's first get the game running before I research fully attaching
+        #   # text file and how that might affect my standard_deck method
+        #
+        #   # How to randomize two decks?
+        #   # Found .shuffle in Ruby Docs!!
+        #
+        #   # def shuffle
+        #   #   deck_1 = []
+        #   #   deck_2 = []
+        #   #   standard_deck
+        #   #   deck_holder.shuffle
+        #   #   deck_1 << deck_holder[0..25]
+        #   #   deck_2 << deck_holder[26..51]
+        #   # end
+        #
+        #
+        # end
--- war_or_peace_runner_test.rb
+++ war_or_peace_runner_test.rb
@@ -1,11 +1,3 @@
-require 'minitest/autorun'
-require 'minitest/nyan_cat'
-require './lib/card'
-require './lib/deck'
-require './lib/player'
-require './lib/turn'
-require './war_or_peace_runner'
-
 # A few key points to keep in mind as you work on this iteration:
 # Use p to display a line of text output to the user
 # In this iteration we’ll introduce a new !le called a “runner” – its job is to serve as the main entry point to our program by starting up a new game
@@ -15,80 +7,126 @@ require './war_or_peace_runner'
 # iteration should break anything that functioned in
 # iterations 1 or 2!
 
+# Needed to Nuke and start over
+# Old code way below
+require 'minitest/autorun'
+require 'minitest/nyan_cat'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+require './war_or_peace_runner'
+
+
 class GameTest < Minitest::Test
-  def test_it_exists
-    card_1 = Card.new(:club, 'Jack', 11)
-    card_2 = Card.new(:heart, 'Seven', 7)
-    card_3 = Card.new(:spade, 'Two', 2)
-    card_4 = Card.new(:diamond, 'King', 13)
-    card_5 = Card.new(:heart, 'Eight', 12)
-    card_6 = Card.new(:club, 'Queen', 8)
-    card_7 = Card.new(:spade, 'Ace', 14)
-    card_8 = Card.new(:diamond, 'Three', 3)
-    deck_1 = Deck.new([card_1, card_3, card_5, card_7])
-    deck_2 = Deck.new([card_2, card_4, card_6, card_8])
-    player_1 = Player.new('Priya', deck_1)
-    player_2 = Player.new('Ricky', deck_2)
-    turn = Turn.new(player_1, player_2)
-    # wrong initialization
-    # game = Game.new(turn, standard_deck)
-    game = Game.new('Priya', 'Ricky')
-    # So far, so good!!!
-    assert_instance_of Game, game
-    # Yay!!!
-  end
-
-  # Create 52 cards - standard_deck
-  def test_it_has_a_full_deck
-    card_1 = Card.new(:club, 'Jack', 11)
-    card_2 = Card.new(:heart, 'Seven', 7)
-    card_3 = Card.new(:spade, 'Two', 2)
-    card_4 = Card.new(:diamond, 'King', 13)
-    card_5 = Card.new(:heart, 'Eight', 12)
-    card_6 = Card.new(:club, 'Queen', 8)
-    card_7 = Card.new(:spade, 'Ace', 14)
-    card_8 = Card.new(:diamond, 'Three', 3)
-    deck_1 = Deck.new([card_1, card_3, card_5, card_7])
-    deck_2 = Deck.new([card_2, card_4, card_6, card_8])
-    player_1 = Player.new('Priya', deck_1)
-    player_2 = Player.new('Ricky', deck_2)
-    turn = Turn.new(player_1, player_2)
-    game = Game.new('Priya', 'Ricky')
-
-    assert_equal [], game.standard_deck
-    # Tested, full deck created!!
-  end
-
-  def test_deck_can_shuffle
-    skip
-  end
-
-  # Randomally assign the standard_deck to deck_1 and deck_2
-  def test_random_assignment_for_player_decks
-    skip
-  end
-
-  # Assign player_1 and player_2 their decks
-  def test_deck_assignment_to_players
-    skip
-  end
-
-  # Start Method: RESEARCH!!!
-    # Welcome to War! (or Peace) This game will be played with 52 cards.
-    # The players today are Megan and Aurora.
-    # Type 'GO' to start the game!
-    # --------------------------------------------------------------
-    # Then a user will be able to type GO , and a game will start.
-    # The user will then see each turn being played, like this:
-    # Turn 1: Megan won 2 cards
-    # Turn 2: WAR - Aurora won 6 cards
-    # Turn 3: *mutually assured destruction* 6 cards removed from play
-    # ...
-    # ...
-    # ...
-    # Turn 9324: Aurora won 2 cards
-    # *~*~*~* Aurora has won the game! *~*~*~*
-  def test_start_method_begins_game
-    skip
-  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      # class GameTest < Minitest::Test
+      #   def test_it_exists
+      #     card_1 = Card.new(:club, 'Jack', 11)
+      #     card_2 = Card.new(:heart, 'Seven', 7)
+      #     card_3 = Card.new(:spade, 'Two', 2)
+      #     card_4 = Card.new(:diamond, 'King', 13)
+      #     card_5 = Card.new(:heart, 'Eight', 12)
+      #     card_6 = Card.new(:club, 'Queen', 8)
+      #     card_7 = Card.new(:spade, 'Ace', 14)
+      #     card_8 = Card.new(:diamond, 'Three', 3)
+      #     deck_1 = Deck.new([card_1, card_3, card_5, card_7])
+      #     deck_2 = Deck.new([card_2, card_4, card_6, card_8])
+      #     player_1 = Player.new('Priya', deck_1)
+      #     player_2 = Player.new('Ricky', deck_2)
+      #     turn = Turn.new(player_1, player_2)
+      #     # wrong initialization
+      #     # game = Game.new(turn, standard_deck)
+      #     game = Game.new('Priya', 'Ricky')
+      #     # So far, so good!!!
+      #     assert_instance_of Game, game
+      #     # Yay!!!
+      #   end
+      #
+      #   # Create 52 cards - standard_deck
+      #   def test_it_has_a_full_deck
+      #     card_1 = Card.new(:club, 'Jack', 11)
+      #     card_2 = Card.new(:heart, 'Seven', 7)
+      #     card_3 = Card.new(:spade, 'Two', 2)
+      #     card_4 = Card.new(:diamond, 'King', 13)
+      #     card_5 = Card.new(:heart, 'Eight', 12)
+      #     card_6 = Card.new(:club, 'Queen', 8)
+      #     card_7 = Card.new(:spade, 'Ace', 14)
+      #     card_8 = Card.new(:diamond, 'Three', 3)
+      #     deck_1 = Deck.new([card_1, card_3, card_5, card_7])
+      #     deck_2 = Deck.new([card_2, card_4, card_6, card_8])
+      #     player_1 = Player.new('Priya', deck_1)
+      #     player_2 = Player.new('Ricky', deck_2)
+      #     turn = Turn.new(player_1, player_2)
+      #     game = Game.new('Priya', 'Ricky')
+      #
+      #     # assert_equal [], game.standard_deck
+      #     # Tested, full deck created!!
+      #   end
+      #
+      #   # Randomally assign the standard_deck to deck_1 and deck_2
+      #   def test_random_assignment_for_player_decks
+      #     game = Game.new('Priya', 'Ricky')
+      #     game.shuffle
+      #     player_1 = Player.new('Priya', deck_1)
+      #     player_2 = Player.new('Ricky', deck_2)
+      #     turn = Turn.new(player_1, player_2)
+      #
+      #     assert_equal [], player_1.deck
+      #   end
+      #
+      #   # Assign player_1 and player_2 their decks
+      #   def test_deck_assignment_to_players
+      #     skip
+      #   end
+      #
+      #   # Start Method: RESEARCH!!!
+      #     # Welcome to War! (or Peace) This game will be played with 52 cards.
+      #     # The players today are Megan and Aurora.
+      #     # Type 'GO' to start the game!
+      #     # --------------------------------------------------------------
+      #     # Then a user will be able to type GO , and a game will start.
+      #     # The user will then see each turn being played, like this:
+      #     # Turn 1: Megan won 2 cards
+      #     # Turn 2: WAR - Aurora won 6 cards
+      #     # Turn 3: *mutually assured destruction* 6 cards removed from play
+      #     # ...
+      #     # ...
+      #     # ...
+      #     # Turn 9324: Aurora won 2 cards
+      #     # *~*~*~* Aurora has won the game! *~*~*~*
+      #   def test_start_method_begins_game
+      #     skip
+      #   end
+      # end
