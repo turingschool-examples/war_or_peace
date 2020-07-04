34329b61e204b693899f0d4fb2213c554922b175
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-07-04 17:30:51 -0600

    Found rouge comma, deleted it

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,7 +3,7 @@ require './lib/deck'
 require './lib/player'
 
 class Turn
-  attr_reader :player1, :player2, #:spoils_of_war
+  attr_reader :player1, :player2 #:spoils_of_war
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
