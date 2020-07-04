a98e51c1e88a1858294e2cbce97b833a2d15e6de
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-07-04 17:42:22 -0600

    Seeing if an If will do the job

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,22 +3,32 @@ require './lib/deck'
 require './lib/player'
 
 class Turn
-  attr_reader :player1, :player2 #:spoils_of_war
+  attr_reader :player1, :player2, :spoils_of_war
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
-    # @spoils_of_war = []
+    @spoils_of_war = []
   end
 end
 
-# def type
-# end
+def type
+  if player1.deck1.cards.rank(0) != player2.deck2.cards.rank(0)
+    #something happens
+  elsif player1.deck1.cards.rank(0) == player.deck2.cards.rank(0)
+    if player1.deck1.cards.rank(2) != player2.deck2.cards.rank(2)
+      #something happens which will likely mirror the above thing
+    elsif player1.deck1.cards.rank(2) == player2.deck2.cards.rank(2)
+      #mutually assured destruction
+    end
+  end
+end
 
 # def winner
 # end
 #
 # def pile_cards
+#   @spoils of war <<
 # end
-#
+# 
 # def award_spoils
 # end
