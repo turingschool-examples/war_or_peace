a701e4e6457f7865f6944dcdbcf920e352c693dc
Daniel Lessenden <dless27@gmail.com>
2020-07-04 14:30:43 -0500

    Add card.rb

---
+++ lib/card.rb
@@ -0,0 +1,22 @@
+class Card
+attr_reader :suit, :value, :rank
+def initialize(suit, value, rank)
+  @suit = suit
+  @value = value
+  @rank = rank
+end
+
+  # def suit
+  #   @suit
+  # end
+  #
+  # def value
+  #   @value
+  # end
+  #
+  # def rank
+  #   @rank
+  # end
+  # USE ATTR_READER OR THE METHOD THTS BLANKED OUT
+  #NOT BOTH 
+end
