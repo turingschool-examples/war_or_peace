860ca1b5e7afc2061b101af8ff83f0f39a2c2afa
RyanDBarnett <conner2468@gmail.com>
2020-07-05 09:34:02 -0600

    Create type method in Turn class

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,4 +6,16 @@ class Turn
     @player2 = player2
     @spoils_of_war = []
   end
+
+  def type
+    first_pair_matches = @player1.deck.cards[0].rank === @player2.deck.cards[0].rank
+    second_pair_matches = @player1.deck.cards[2].rank === @player2.deck.cards[2].rank
+    if (first_pair_matches && second_pair_matches)
+      return :mutually_assured_destruction
+    elsif (first_pair_matches)
+      return :war
+    else
+      return :basic
+    end
+  end
 end
