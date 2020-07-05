4171a3fd31557882c035e6cf29900f9b8e8f848e
Daniel Lessenden <dless27@gmail.com>
2020-07-05 15:35:29 -0500

    Add new test

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -106,7 +106,7 @@ class TurnTest < Minitest::Test
     turn.pile_cards
     assert_equal turn.spoils_of_war, turn.pile_cards
 
-
+    #
     @deck1 = Deck.new([@card1, @card2, @card3, @card7])
     @deck2 = Deck.new([@card4, @card5, @card3, @card8])
     @player1 = Player.new("Megan", @deck2)
@@ -116,8 +116,10 @@ class TurnTest < Minitest::Test
     turn.player2
     turn.type
     turn.winner
-    turn.pile_cards
+    #turn.pile_cards
+    turn.award_spoils(turn.winner)
     assert_equal [@card7], @player2.deck.cards
+    assert_equal [@card8], @player1.deck.cards
     #mutually_assured_destruction
 
     # @deck1 = Deck.new([@card1, @card2, @card3])
@@ -132,7 +134,7 @@ class TurnTest < Minitest::Test
   end
 
   def test_award_spoils
-  
+
     player1 = Player.new("Megan", @deck1)
     player2 = Player.new("Aurora", @deck2)
     turn = Turn.new(@player1, @player2)
@@ -143,7 +145,7 @@ class TurnTest < Minitest::Test
     turn.pile_cards
     turn.award_spoils(winner)
     assert_equal @player2.deck.cards, [@card4, @card6, @card7] #card4, card6, card7,]#winner.deck.cards.concat(spoils_of_war)
-    assert_equal @player1.deck.cards, [@card2, @card5, @card8, @card1, @card3]
+    assert_equal @player1.deck.cards.length, 5 #[@card2, @card5, @card8, @card1, @card3]
   end
 
   def test_war_spoils
@@ -165,11 +167,12 @@ class TurnTest < Minitest::Test
     winner = turn.winner
     turn.pile_cards
     turn.award_spoils(winner)
-    assert_equal @player1.deck.cards.length, @player1.deck.cards.length == 7
+    assert_equal @player1.deck.cards.length,  7
     assert_equal @player2.deck.cards, [@card7]         #card5,  card4 , card3
   end
 
     def test_mutual_loss
+      #skip
       @card1 = Card.new(:heart, 'Jack', 11) #
       @card2 = Card.new(:heart, '10', 10) #
       @card3 = Card.new(:heart, '9', 9)
@@ -177,19 +180,18 @@ class TurnTest < Minitest::Test
       @card5 = Card.new(:heart, '8', 8)
       @card6 = Card.new(:diamond, 'Queen', 12) #
       @card7 = Card.new(:heart, '3', 3)
-      @card8 = Card.new(:diamond, '2', 3) #
-      @deck1 = Deck.new([@card1, @card2, @card7, @card5])
-      @deck2 = Deck.new([@card4, @card3, @card8, @card6])
+      @card8 = Card.new(:diamond, '3', 3) #
+      @deck1 = Deck.new([@card1, @card2, @card8, @card5])
+      @deck2 = Deck.new([@card1, @card3, @card8, @card6])
       @player1 = Player.new("Megan", @deck1)
       @player2 = Player.new("Aurora", @deck2)
       turn = Turn.new(@player1, @player2)
       turn.type
       turn.winner
       winner = turn.winner
-      turn.pile_cards
-      #turn.award_spoils(winner)
-      assert_equal @player1.deck.cards, [@card5]
-      assert_equal @player2.deck.cards, [@card6]
+      #turn.pile_cards
+      turn.award_spoils(turn.winner)
+      assert_equal [@card5], @player1.deck.cards
     end
 
 
