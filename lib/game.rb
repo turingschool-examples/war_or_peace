5b5ba1837727bcc28b601955894018253f1236b0
Priya Power <priyajag72@gmail.com>
2020-07-05 14:17:17 -0500

    Created user_start method which will play the game

--- lib/game.rb
+++ lib/game.rb
@@ -19,7 +19,7 @@ class Game
     puts "> "
     player_2 = $stdin.gets.chomp
     p "The players today are #{player_1} and #{player_2}"
-    p "Type 'GO' to start the game!"
+    p "Type 'GO' to start the game! (type 'EXIT' to leave the game...)"
     p "-------------------------------------------------"
     user_input_start = ""
     user_input_start = gets.chomp
@@ -27,12 +27,61 @@ class Game
       # start game
       p "#{player_1} and #{player_2} declare WAR!"
       user_start
+    elsif user_input_start.upcase == "EXIT"
+      exit
     else
       # start welcome message over
       p "Misfire... read the instructions next time :)"
       start
     end
-
   end
 
+  # Ugghhh.. how do I begin the game?
+  # Okay... so we came here after the game.start method was called
+  # We can pull Class player parameter of @name from game.start chomp
+  # runner file has deck of cards created, shuffled, then assigned to two decks
+  # Project says:
+  # Turn 1: Megan won 2 cards
+  # Turn 2: WAR - Aurora won 6 cards
+  # Turn 3: *mutually assured destruction* 6 cards removed from play
+  # ...
+  # ...
+  # ...
+  # Turn 9324: Aurora won 2 cards
+  # *~*~*~* Aurora has won the game! *~*~*~*
+  # NOTE - only 1,000,000 turns will be allowed.
+  # If no player has all cards after 1,000,000 turns,
+  # the result of the game is a draw.
+  def user_start
+    # Creates instance of turn class, since this mostly has to do with turns
+    # May also use player class
+    turn = Turn.new(player_1, player_2)
+    # Realized I needed something to account for the 100000 max turn rule
+    turn_counter = 0
+    # Researched loops
+    # While - as long as 'something' is true, this will run
+    # For - preffered when you know the number of loops
+    # Do..While - loop do has a condition and then a break point (exit controlled loop)
+    # Until - as long as 'something' is false, will run
+
+    # until seems best (will run until the Player Class method has_lost?
+    # returns true.)
+    # Tested! After closing it, it works!!
+    until (player_1.has_lost? || player_2.has_lost?)
+      # if turn.type == :basic
+      #   p "basic turn testing"
+      #   turn_counter += 1
+      # elsif turn.type == :war
+      #   turn_counter += 1
+      # elsif turn.type == :mutually_assured_destruction
+      #   turn_counter += 1
+      # else
+      #   p "OOOOOOOPS! MISTAKE IN USER_START METHOD"
+      # end
+      
+      # every time the turn instance runs, it will add on a +1 to the turn_counter
+      # This will cause the until to stop once turn_counter = 100000
+      break if turn_counter == 100000
+    end
+  end
 end
