b441e3017577344468802f74311260db8c8debd0
John Kim <abcdefghijohnk@gmail.com>
2020-07-05 14:26:17 -0600

    Made card generator

---
+++ lib/card_generator.rb
@@ -0,0 +1,20 @@
+# # f = File.open('cards.txt', "r")
+# File.foreach('cards.txt'){ |line|
+#   puts line
+# }
+# # f.close
+
+
+# def run_cards
+#   File.readlines('cards.txt').each do |line|
+#   p line
+#   end
+# end
+#
+# # file='cards.txt'
+# # f = File.open(file, "r")
+# # f.each_line { |line|
+# #   p line
+# # }
+# # f.close
+# run_cards
