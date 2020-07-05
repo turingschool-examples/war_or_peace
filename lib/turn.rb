91bfbf1192030ce84849f5c68a76b2f7d62ac831
John Kim <abcdefghijohnk@gmail.com>
2020-07-04 22:22:43 -0600

    Edit methods

--- lib/turn.rb
+++ lib/turn.rb
@@ -23,9 +23,9 @@ attr_reader :player1, :player2, :spoils_of_war
   def winner
     if type() == :basic
     if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
-       return @player1
+      @player1
     else
-       return @player2
+      @player2
     end
 
    elsif type() == :war
@@ -54,27 +54,23 @@ attr_reader :player1, :player2, :spoils_of_war
       @spoils_of_war << @player2.deck.cards[0..2]
 
 
-      @player1.deck.remove_card
-      @player1.deck.remove_card
-      @player1.deck.remove_card
-      @player2.deck.remove_card
-      @player2.deck.remove_card
-      @player2.deck.remove_card
+      3.times {@player1.deck.remove_card}
+
+      3.times {@player2.deck.remove_card}
+
 
     elsif type() == :mutually_assured_destruction
 
-       @player1.deck.remove_card
-       @player1.deck.remove_card
-       @player1.deck.remove_card
-       @player2.deck.remove_card
-       @player2.deck.remove_card
-       @player2.deck.remove_card
+      3.times {@player1.deck.remove_card}
+
+      3.times {@player2.deck.remove_card}
     end
   end
 
 
   def award_spoils(winner)
       winner.deck.cards << @spoils_of_war
+      winner.deck.cards.flatten
       winner
     end
   end
