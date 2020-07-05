aee0ff39b76cd3c89ed4477b28afec406faf2d49
Daniel Lessenden <dless27@gmail.com>
2020-07-04 23:41:02 -0500

    Updat:  works until it doesn't

--- test/war_or_peace_runner.rb
+++ test/war_or_peace_runner.rb
@@ -2,6 +2,7 @@ require './lib/deck'
 require './lib/card'
 require './lib/player'
 require './lib/turn'
+require 'pry'
 
 deck = []
 deck2 = []
@@ -67,18 +68,7 @@ deck2.concat(deck.slice!(0..25))
 @turn = Turn.new(@player1, @player2)
 p @player1.deck.cards.size
 p @player2.deck.cards.size
-#
-# def over
-#   if @player1.has_lost? == true
-#     p "*~*~*~* #{@player2.name} has won the game! *~*~*~*"
-#     p "#{@player1.deck.cards.length} p1"
-#     p "#{@player2.deck.cards.length} p2"
-#   elsif @player2.has_lost? == true
-#     p "*~*~*~* #{@player1.name} has won the game! *~*~*~*"
-#      p "#{@player1.deck.cards.length} p1"
-#      p "#{@player2.deck.cards.length} p2"
-#   end
-# end
+
 
 def start
     p "Welcome to War! (or Peace) This game will be played with 52 cards."
@@ -88,37 +78,36 @@ def start
     input = gets.chomp!
       if input == "Go" ||input == "GO" ||input == "go"
         turn_count = 1
-         until @player1.has_lost? == true || @player2.has_lost? == true || @turn_count == 1000000
-
+          until @player1.has_lost? == true || @player2.has_lost? == true || @turn_count == 1000000 do
            @turn.type
            #if type is MAD or war and player has less than 3
            #other player wins!
             if (@turn.type == :mutually_assured_destruction || @turn.type == :war) &&
               if @player1.deck.cards.length < 3
                 p "*~*~*~* #{@player2.name} has won the game! *~*~*~*"
+                p "MAD"
               elsif @player2.deck.cards.length < 3
                 p "*~*~*~* #{@player1.name} has won the game! *~*~*~*"
+                p "MAD"
               end #nested
               break
             end #if type
 
-
-
            @turn.winner
            @turn.spoils_of_war
-           @turn.pile_cards
+           #@turn.pile_cards
            @turn.award_spoils(@turn.winner)
+           binding.pry
+           #deck length
+           p "#{@player1.deck.cards.length} p1"
+           p "#{@player2.deck.cards.length} p2"
             if @turn.type == :basic
               p "Turn #{turn_count}: #{@turn.type} - #{@turn.winner.name} won #{@turn.pile_cards.count} cards!"
-            # p "#{@player1.deck.cards.length} p1"
-            # p "#{@player2.deck.cards.length} p2"
             elsif @turn.type == :mutually_assured_destruction
               p "Turn #{turn_count}: *mutually assured destruction* 6 cards removed from play"
             elsif @turn.type == :war
               p "Turn #{turn_count}: WAR - #{@turn.winner.name} won #{@turn.pile_cards.count} cards!"
-#check to see if in spoils of war
             end #if turn type end
-
           turn_count += 1
 
         end #until
@@ -126,18 +115,20 @@ def start
           p "*~*~*~* #{@player2.name} has won the game! *~*~*~*"
           p "#{@player1.deck.cards.length} p1"
           p "#{@player2.deck.cards.length} p2"
+          p "#{@turn.spoils_of_war} pile cards"
+
         elsif @player2.has_lost? == true
           p "*~*~*~* #{@player1.name} has won the game! *~*~*~*"
           p "#{@player1.deck.cards.length} p1"
           p "#{@player2.deck.cards.length} p2"
+          p "#{@turn.spoils_of_war} pile cards"
         end
 
       end #if input end
     end  #def start end
-
-
+#TYPE IS CHANGING
 p start
 
-
+# make a test file and have MAD or war come up with not enough cards in player deck
 
 #   #until @player1.has_lost? || @player2.has_lost? || turn_count == 0
