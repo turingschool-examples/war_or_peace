70b89a7aff02b2f4d64fdfe5b5bd18600032ea3f
Angela Guardia <angelaguardia20@gmail.com>
2020-07-04 18:05:42 -0700

    Clean up player tests with setup method

--- test/player_test.rb
+++ test/player_test.rb
@@ -6,60 +6,42 @@ require './lib/player'
 
 class PlayerTest < Minitest::Test
 
+  def setup
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @cards = [@card1, @card2, @card3]
+    @deck = Deck.new(@cards)
+
+    @player = Player.new('Clarisa', @deck)
+  end
+
   def test_it_exists
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
 
-    player = Player.new('Clarisa', deck)
-
-    assert_instance_of Player, player
+    assert_instance_of Player, @player
   end
 
   def test_it_has_name
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
 
-    player = Player.new('Clarisa', deck)
-
-    assert_equal 'Clarisa', player.name
+    assert_equal 'Clarisa', @player.name
   end
 
   def test_it_has_a_deck
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
 
-    player = Player.new('Clarisa', deck)
-
-    assert_equal deck, player.deck
+    assert_equal @deck, @player.deck
   end
 
   def test_it_knows_if_it_lost
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
 
-    player = Player.new('Clarisa', deck)
+    assert_equal false, @player.has_lost?
 
-    assert_equal false, player.has_lost?
+    @deck.remove_card
+    assert_equal false, @player.has_lost?
 
-    deck.remove_card
-    assert_equal false, player.has_lost?
+    @deck.remove_card
+    assert_equal false, @player.has_lost?
 
-    deck.remove_card
-    assert_equal false, player.has_lost?
-
-    deck.remove_card
-    assert_equal true, player.has_lost?
+    @deck.remove_card
+    assert_equal true, @player.has_lost?
   end
 end
