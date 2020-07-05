ead8de9d1ca87d6d88545c711062b8238768c482
Daniel Lessenden <dless27@gmail.com>
2020-07-04 20:19:49 -0500

    Update pile_cards & award_spoils

--- lib/turn.rb
+++ lib/turn.rb
@@ -23,49 +23,51 @@ attr_reader :player1, :player2, :spoils_of_war
       p "No winner"
     elsif type() == :basic
         if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
-          @player1
+        @player1
         else @player1.deck.rank_of_card_at(0) < @player2.deck.rank_of_card_at(0)
-          @player2
+        @player2
          end
     elsif type() == :war
         if @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
-          @player1
+        @player1
         else @player1.deck.rank_of_card_at(2) < @player2.deck.rank_of_card_at(2)
-          @player2
+         @player2
         end
       end
      end
 
    def pile_cards
       if type() == :mutually_assured_destruction
-        # @player1.deck.cards.delete_at(0..2)
-        # @player2.deck.cards.delete_at(0..2)
-        @player1.deck.cards.shift(3)
-        @player2.deck.cards.shift(3)
-
-        # 3.times do
-        #   @player1.deck.cards.remove_card
-        #
-        # end
-        # 3.times do
-        #   @player2.deck.cards.remove_card
-        # end
+        3.times do
+          @player1.deck.remove_card
+        end
+        3.times do
+          @player2.deck.remove_card
+        end
       elsif type() == :war
-        @spoils_of_war.concat(player1.deck.cards.slice!(0..2))
-        @spoils_of_war.concat(player2.deck.cards.slice!(0..2))
+        # @spoils_of_war.concat(player1.deck.cards.slice!(0..2))
+        # @spoils_of_war.concat(player2.deck.cards.slice!(0..2))
+        3.times do
+          @spoils_of_war << player1.deck.remove_card
+        end
+        3.times do
+          @spoils_of_war << player2.deck.remove_card
+        end
       else type() == :basic
-        @spoils_of_war << @player1.deck.cards.slice!(0)
-        @spoils_of_war << @player2.deck.cards.slice!(0)
-        #@spoils_of_war.concat(player1.deck.cards.slice!(0))
-        #@spoils_of_war.concat(player2.deck.cards.slice!(0))
-      end
-   end
+         @spoils_of_war << @player1.deck.remove_card
+         @spoils_of_war << @player2.deck.remove_card
+      end #og if statement
+    end #class
 
    def award_spoils(winner)
      if type() == :basic || type() == :war
        @spoils_of_war.shuffle!
-       winner.deck.cards.concat(@spoils_of_war)
-       @spoils_of_war = []
+       #winner.deck.cards.concat(@spoils_of_war)
+       until @spoils_of_war.empty? do
+         winner.deck.add_card(@spoils_of_war.shift)
+       end
+
+       #@spoils_of_war = []
      else
        nil
      end
@@ -75,6 +77,6 @@ end #class
 
 
 # add an if to basic, if first cardvalue=
-#need a turn counter
+
 #may be trouble with @winner as an instance variable
 #def winner use @player.name
