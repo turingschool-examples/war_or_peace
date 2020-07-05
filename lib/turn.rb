6d62dd3170745d1ab642ffb160f95c1a731dc19e
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-07-04 19:51:46 -0600

    Type working, but with weird  thing in there. Onto winner method

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,18 +11,20 @@ class Turn
   end
 end
 
+public  #????????????
 def type
-  if player1.deck1.cards.rank(0) == player2.deck2.cards.rank(0) && player1.deck1.cards.rank(2) == player2.deck2.cards.rank(2)
-    :mutually_assured_destruction
-  elsif player1.deck1.cards.rank(0) == player.deck2.cards.rank(0)
+  if (player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2))
+      :mutually_assured_destruction
+  elsif (player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0))
     :war
   else
     :basic
   end
 end
 
-# def winner
-# end
+def winner
+  
+end
 #
 # def pile_cards
 #   @spoils of war <<
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -5,7 +5,7 @@ require './lib/deck'
 require './lib/player'
 require './lib/turn'
 
-class PlayerTurn < Minitest::Test
+class TurnTest < Minitest::Test
   def test_it_exists
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
@@ -17,7 +17,6 @@ class PlayerTurn < Minitest::Test
     card8 = Card.new(:diamond, '2', 2)
     deck1 = Deck.new([card1, card2, card5, card8])
     deck2 = Deck.new([card3, card4, card6, card7])
-    name = name
     player1 = Player.new("Megan", deck1)
     player2 = Player.new("Aurora", deck2)
     turn = Turn.new(player1, player2)
@@ -25,8 +24,23 @@ class PlayerTurn < Minitest::Test
     assert_instance_of Turn, turn
   end
 
-  # def test_type
-  # end
+  def test_type
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal :basic, turn.type
+  end
   #
   # def test_winner
   # end
