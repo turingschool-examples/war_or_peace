66f12d0338c3d9525b28ced58b59a4f91d22e0d7
Daniel Lessenden <dless27@gmail.com>
2020-07-04 22:16:09 -0500

    Test methods

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -120,40 +120,19 @@ class TurnTest < Minitest::Test
     assert_equal [@card7], @player2.deck.cards
     #mutually_assured_destruction
 
-    @deck1 = Deck.new([@card1, @card2, @card3])
-    @deck2 = Deck.new([@card4, @card5, @card6])
-    @player1 = Player.new("Megan", @deck2)
-    @player2 = Player.new("Aurora", @deck1)
-    turn = Turn.new(@player1, @player2)
-    turn.player1
-    turn.player2
-    turn.type
-    assert_equal turn.spoils_of_war, turn.pile_cards
-  end
-
-  def test_award_spoils
-    #skip
     # @deck1 = Deck.new([@card1, @card2, @card3])
     # @deck2 = Deck.new([@card4, @card5, @card6])
-    # @player1 = Player.new("Megan", @deck1)
-    # @player2 = Player.new("Aurora", @deck2)
+    # @player1 = Player.new("Megan", @deck2)
+    # @player2 = Player.new("Aurora", @deck1)
     # turn = Turn.new(@player1, @player2)
     # turn.player1
     # turn.player2
     # turn.type
-    # turn.winner #player2
-    # winner = turn.winner
+    # assert_equal turn.spoils_of_war, turn.pile_cards
+  end
 
-    # card1 = Card.new(:heart, 'Jack', 11)
-    # card2 = Card.new(:heart, '10', 10)
-    # card3 = Card.new(:heart, '9', 9)
-    # card4 = Card.new(:diamond, 'Jack', 11)
-    # card5 = Card.new(:heart, '8', 8)
-    # card6 = Card.new(:diamond, 'Queen', 12)
-    # card7 = Card.new(:heart, '3', 3)
-    # card8 = Card.new(:diamond, '2', 2)
-    # deck1 = Deck.new([card1, card2, card8])
-    # deck2 = Deck.new([card3, card4, card7])
+  def test_award_spoils
+  
     player1 = Player.new("Megan", @deck1)
     player2 = Player.new("Aurora", @deck2)
     turn = Turn.new(@player1, @player2)
@@ -186,8 +165,8 @@ class TurnTest < Minitest::Test
     winner = turn.winner
     turn.pile_cards
     turn.award_spoils(winner)
-    assert_equal @player1.deck.cards, [@card8, @card1, @card2, @card6, @card4, @card3, @card5]
-    assert_equal @player2.deck.cards, [@card7]
+    assert_equal @player1.deck.cards.length, @player1.deck.cards.length == 7
+    assert_equal @player2.deck.cards, [@card7]         #card5,  card4 , card3
   end
 
     def test_mutual_loss
