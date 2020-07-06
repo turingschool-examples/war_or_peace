59f59fd05926b01778a854d65b8f031bc2f0e8dc
Travis McKinstry <mckinstrytg@gmail.com>
2020-07-05 18:53:28 -0600

    Add another test to turn_test to check if it can have 'war' type

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -27,7 +27,7 @@ class TurnTest < Minitest::Test
       assert_instance_of Turn, turn
     end
 
-    def test_it_has_3_types
+    def test_it_can_have_basic_type
       card1 = Card.new(:diamond, 'Queen', 12)
       card2 = Card.new(:spades, '3', 3)
       card3 = Card.new(:heart, 'Ace', 11)
@@ -43,6 +43,28 @@ class TurnTest < Minitest::Test
       turn = Turn.new(player1, player2)
 
       assert_equal turn.type, :basic
+    end
+
+    def test_it_can_have_war
+      card1 = Card.new(:diamond, 'Ace', 11)
+      card2 = Card.new(:spades, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 11)
+      card4 = Card.new(:heart, '5', 5)
+      card5= Card.new(:heart, 'Queen', 12)
+      card6 = Card.new(:heart, '5', 5)
+      card7 = Card.new(:heart, '5', 5)
+      card8 = Card.new(:heart, '5', 5)
+
+      cards = [card1, card2, card5]
+      cards1 = [card3, card4, card6]
+      deck1 = Deck.new(cards)
+      deck2 = Deck.new(cards1)
+
+      player1 = Player.new("Megan", deck1)
+      player2 = Player.new("Aurora", deck2)
+      turn = Turn.new(player1, player2)
+
+      assert_equal turn.type, :war
 
     end
 
