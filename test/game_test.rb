0d65c15320b15d825bc99ced240f1c19e7069d53
Travis McKinstry <mckinstrytg@gmail.com>
2020-07-05 18:44:18 -0600

    Add test to check if Game class has 2 players

--- test/game_test.rb
+++ test/game_test.rb
@@ -24,8 +24,27 @@ class GameTest < Minitest::Test
       player2 = Player.new("Aurora", deck2)
       turn = Turn.new(player1, player2)
       game1 = Game.new
-      require "pry"; binding.pry
+      # require "pry"; binding.pry
       assert_instance_of Game, game1
+    end
+
+    def test_it_has_2_players
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spades, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 11)
+      card4 = Card.new(:heart, '5', 5)
+
+      cards = [card1, card2]
+      cards1 = [card3, card4]
+      deck1 = Deck.new(cards)
+      deck2 = Deck.new(cards1)
+
+      player1 = Player.new("Megan", deck1)
+      player2 = Player.new("Aurora", deck2)
+      turn = Turn.new(player1, player2)
+      game1 = Game.new
+      assert_equal turn.player1.name, "Megan"
+      assert_equal turn.player2.name, "Aurora"
     end
 
 
