9bf1b2db29f8b4667ae8e5632ea55df8cbe1ad68
Priya Power <priyajag72@gmail.com>
2020-07-05 20:39:04 -0500

    Created method for reading file line by line into an array.

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -2,7 +2,26 @@ require './lib/card'
 
 class CardGenerator
 
+  attr_accessor :filename, :cards
+
   def initialize(filename)
     @filename = filename
+    @cards = []
+  end
+
+  # Need to find something that can open the file
+  # THEN, it should read the first line, convert it to an array element
+  # THEN, add into array
+  # there is a class file
+  # You can open by file = File.open("filename")
+  # Read method 1 - .read
+  # method 1 hints - .split OR .readlines and .chomp
+  # Seems like best method
+  # Rubyguides.com example uses enumerator .map
+  def read_generator
+    file = File.open("./lib/deck_of_cards.txt")
+    file_data = file.readlines.map(&:chomp)
+    file.close
+    file_data
   end
 end
--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -11,5 +11,49 @@ class CardGeneratorTest < Minitest::Test
 
     assert_instance_of CardGenerator, card_gen
   end
-  
+
+
+  def test_it_can_read_my_file
+    filename = "deck_of_cards.txt"
+    card_gen = CardGenerator.new(filename)
+    assert_equal "deck_of_cards.txt", card_gen.filename
+    # Didn't work...
+    # Error: filename isn't a method...
+    # I've initialized it. GOT IT! I didn't let my file 'get' it!
+    # Fixed! Perfect.
+  end
+
+  def test_class_can_open_an_array_to_hold_txt_lines
+    filename = "deck_of_cards.txt"
+    card_gen = CardGenerator.new(filename)
+    assert_equal [], card_gen.cards
+    # Oops, didn't initialize or access cards!
+  end
+
+  # Have to research reading files
+  # This might be a while...
+  # Need to find something that can open the file
+  # THEN, it should read the first line, convert it to an array element
+  # THEN, add into array
+
+  def test_can_open_and_read_file
+    filename = "deck_of_cards.txt"
+    card_gen = CardGenerator.new(filename)
+    # assert_equal [], card_gen.read_generator
+  end
+
+  def test_random_lines
+    filename = "deck_of_cards.txt"
+    card_gen = CardGenerator.new(filename)
+    read_test = card_gen.read_generator
+    assert_equal ":club, 'Two', 2", read_test[0]
+    assert_equal ":diamond, 'Three', 3", read_test[14]
+    assert_equal ":heart, 'Four', 4", read_test[28]
+  end
+
+  # def test
+  #   filename = "deck_of_cards.txt"
+  #   card_gen = CardGenerator.new(filename)
+  # end
+  #
 end
