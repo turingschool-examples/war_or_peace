d76ae65b80f03d6ce74076147e6ba8d71692bc6f
Hope Gochnour <hope.gochnour@gmail.com>
2020-07-06 01:38:31 -0600

    Runner updated

--- lib/war_or_peace_runner.rb
+++ lib/war_or_peace_runner.rb
@@ -3,20 +3,6 @@ require "./lib/deck"
 require "./lib/player"
 require "./lib/turn"
 
-# Method notes:
-# Cards: nada
-# Decks: rank_of_card_at(array_loc) - specifies card at given index location
-#        high_ranking_cards - returns an array with only the cards that are Jack+
-#        percent_high_ranking - obvious
-#        remove_card - removes first card of array
-#        add_card(card_to_add) - adds specified card to end of array
-# Players: has_lost? - if player has 0 cards, player loses (IMPORTANT!)
-# Turns: type - determines round type
-#        winner - determines round winner based on card values
-#        pile_cards - removes the cards compared above and adds them to the spoils
-#        award_spoils - moves cards from spoils to winner's deck, or destroys them
-#        generate_cards - makes all of the cards (IMPORTANT)
-
 p "Welcome to War!"
 
 loop do
@@ -67,7 +53,20 @@ loop do
   turn.player1.has_lost?
   turn.player2.has_lost?
 
-  if ((turn.player1.has_lost? == true) || (turn.player2.has_lost? == true)) || ((turn.player1.has_lost? == true) && (turn.player2.has_lost? == true)) || turn_count == 1000000
+  if ((turn.player1.has_lost? == true) && (turn.player2.has_lost? == true))
+    p "DRAW"
+    break
+
+  elsif turn.player2.has_lost? == true
+    p "#{turn.player1.name} wins!"
+    break
+
+  elsif turn.player1.has_lost? == true
+    p "#{turn.player2.name} wins!"
+    break
+
+  elsif turn_count == 1000000
+    p "Turn count exceeded. War continues another day."
     break
   end
 end
