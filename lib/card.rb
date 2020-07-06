e31d21db18fd8abd2fc968580fe390106e38c283
Hope Gochnour <hope.gochnour@gmail.com>
2020-07-05 23:30:42 -0600

    Formatting update to lib/card.rb

--- lib/card.rb
+++ lib/card.rb
@@ -7,9 +7,3 @@ class Card
     @rank = rank
   end
 end
-#
-# deck_1 = Deck.new(standard_deck.shuffle)
-# deck_2 = Deck.new(standard_deck.shuffle)
-
-# player_1 = Player.new("Clarissa," Deck.new(standard_deck.shuffle))
-# player_2 = Player.new("Ambrose", deck_2)
