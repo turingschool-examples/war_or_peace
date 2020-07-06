5f5f969a76b7e93c65825e3ac0c729edad317f94
RyanDBarnett <conner2468@gmail.com>
2020-07-06 01:21:24 -0600

    Create war or peace runner file

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -1,39 +1,7 @@
-require './lib/card'
-require './lib/deck'
-require './lib/player'
-require './lib/turn'
+require './lib/card_generator'
+require './lib/game'
 
-def create_suit(suit)
-  suit_cards = []
-  values = {
-    1 => '1',
-    2 => '2',
-    3 => '3',
-    4 => '4',
-    5 => '5',
-    6 => '6',
-    7 => '7',
-    8 => '8',
-    9 => '9',
-    10 => '10',
-    11 => 'Jack',
-    12 => 'Queen',
-    13 => 'King',
-    14 => 'Ace'
-  }
-  rank = 2
-
-  while rank <= 14 do
-    p rank
-    suit_cards << Card.new(suit, values[rank], rank)
-    rank += 1
-  end
-
-  suit_cards
-end
-
-suits = ['heart', 'spade', 'diamond', 'club']
-
-cards = suits.reduce([]) { |cards, suit| cards.concat(create_suit(suit)) }
-
-p cards
+cards = Card_Generator.new('cards.txt').cards
+shuffled_cards = cards.shuffle
+game = Game.new(shuffled_cards)
+game.start
