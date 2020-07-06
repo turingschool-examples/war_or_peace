b42e6770210a18256a319ca253a9a580144ea55c
Travis McKinstry <mckinstrytg@gmail.com>
2020-07-05 18:48:05 -0600

    Add test to check if turn can discern what turn type it is

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -27,6 +27,25 @@ class TurnTest < Minitest::Test
       assert_instance_of Turn, turn
     end
 
+    def test_it_has_3_types
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spades, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 11)
+      card4 = Card.new(:heart, '5', 5)
+
+      cards = [card1, card2]
+      cards1 = [card3, card4]
+      deck1 = Deck.new(cards)
+      deck2 = Deck.new(cards1)
+
+      player1 = Player.new("Megan", deck1)
+      player2 = Player.new("Aurora", deck2)
+      turn = Turn.new(player1, player2)
+
+      assert_equal turn.type, :basic
+
+    end
+
 
 
   end
