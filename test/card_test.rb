712a2b2ed1ec2431a26f20db5f3d861e1c97117a
Cydnee Owens <cks410@yahoo.com>
2020-08-16 12:21:26 -0600

    Add iteration 1

--- test/card_test.rb
+++ test/card_test.rb
@@ -16,8 +16,21 @@ class CardTest < Minitest::Test
   def test_it_has_readable_attributes
     card = Card.new(:diamond, 'Queen', 12)
 
+  def test_has_a_suit
+    card = Card.new(:diamond, 'Queen', 12)
+
     assert_equal :diamond, card.suit
+  end
+
+  def test_has_a_value
+    card = Card.new(:diamond, 'Queen', 12)
+
     assert_equal 'Queen', card.value
+  end
+
+  def test_has_a_rank
+    card = Card.new(:diamond, 'Queen', 12)
+
     assert_equal 12, card.rank
   end
 end
