4041556b8be2f7cc71d1a1d5f84159dded4c80c3
John Kim <abcdefghijohnk@gmail.com>
2020-08-17 15:54:45 -0600

    combined add and remove card test, test pass

--- lib/deck.rb
+++ lib/deck.rb
@@ -22,4 +22,8 @@ class Deck
   def remove_card
     @cards.shift
   end
+
+  def add_card(card)
+    @cards.push(card)
+  end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -46,27 +46,22 @@ class DeckTest <  Minitest::Test
     assert_equal 66.67, deck.percent_high_ranking
   end
 
-  def test_it_can_remove_cards_and_check_high_cards
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-
-    deck.remove_card
-    assert_equal [card2, card3], deck.cards
-    assert_equal [card3], deck.high_ranking_cards
-    assert_equal 50.0, deck.percent_high_ranking
-  end
-
-  def test_it_can_add_cards
+  def test_it_can_remove_and_add_cards_and_check_high_cards
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
     card4 = Card.new(:club, '5', 5)
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
+    deck.remove_card
+
+    assert_equal [card2, card3], deck.cards
+    assert_equal [card3], deck.high_ranking_cards
+    assert_equal 50.0, deck.percent_high_ranking
 
     deck.add_card(card4)
+    assert_equal [card2, card3, card4], deck.cards
+    assert_equal [card3], deck.high_ranking_cards
+    assert_equal 33.33, deck.percent_high_ranking
   end
 end
