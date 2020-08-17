7215d28b90bfd8f4b3ff02e178889eb5dd63f7d0
John Kim <abcdefghijohnk@gmail.com>
2020-08-17 15:40:38 -0600

    create remove card test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -44,6 +44,16 @@ class DeckTest <  Minitest::Test
     deck = Deck.new(cards)
 
     assert_equal 66.67, deck.percent_high_ranking
+  end
 
+  def test_it_can_remove_cards
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    deck.remove_card
+    assert_equal [card2, card3], deck.cards
   end
 end
