3d2ae4d5c9621d59aca4a14eeeab3debc6d32db9
John Kim <abcdefghijohnk@gmail.com>
2020-08-17 15:19:33 -0600

    create rank of card method, test pass

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -14,4 +14,15 @@ class DeckTest <  Minitest::Test
     assert_instance_of Deck, deck
     assert_equal [card1, card2, card3], deck.cards
   end
+
+  def test_it_can_get_rank_of_specific_card
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal 12, deck.rank_of_card_at(0)
+    assert_equal 14, deck.rank_of_card_at(2)
+  end
 end
