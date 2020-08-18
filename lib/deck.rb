b712b193286b0fa5d319117c9959dac87e39dcd9
Carson Jardine <carsonjardine@gmail.com>
2020-08-17 20:37:38 -0600

    Create deck class and test and add several methods

---
+++ lib/deck.rb
@@ -0,0 +1,20 @@
+class Deck
+  attr_reader :cards
+
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def rank_of_card_at(index)
+    @cards[index].rank
+  end
+
+
+  def high_ranking_cards
+    @cards.find_all do |card|
+      card.rank >= 11
+    end
+
+  end
+
+end
---
+++ test/deck_test.rb
@@ -0,0 +1,37 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+
+class DeckTest < Minitest::Test
+
+  def setup
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @cards = [@card1, @card2, @card3]
+    @deck = Deck.new(@cards)
+  end
+
+  def test_it_exists
+    assert_instance_of Deck, @deck
+  end
+
+  def test_it_has_cards
+    assert_equal [@card1, @card2, @card3], @deck.cards
+  end
+
+  def test_rank_of_card_at
+    # skip
+    assert_equal 12, @deck.rank_of_card_at(0)
+    assert_equal 14, @deck.rank_of_card_at(2)
+  end
+
+  def test_high_ranking_cards
+    # skip
+    assert_equal [@card1, @card3], @deck.high_ranking_cards
+  end
+
+
+
+end
