77b3fe4ba4906c885acc73e73133e54238c52d04
Roberto Basulto <roberto.basulto085@gmail.com>
2020-08-17 20:40:29 -0400

    Finished add_card and its test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -66,6 +66,22 @@ class DeckTest < Minitest::Test
     deck.remove_card
 
     assert_equal [card2, card3], deck.cards
+    assert_equal 50.0, deck.percent_high_cards
   end
 
+  def test_should_add_another_card
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    card4 = Card.new(:club ,'5' ,5)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    deck.remove_card
+    deck.add_card(card4)
+
+    assert_equal [card2, card3, card4], deck.cards
+    assert_equal [card3], deck.high_ranking_cards
+    assert_equal 33.33, deck.percent_high_cards
+  end
 end
