cf1e7194d7cccf23b0969a1cb90da8f7db4e9b4f
Michael Foy <foymikek@gmail.com>
2020-08-18 17:05:32 -0600

    Add method psuedo code

--- lib/deck.rb
+++ lib/deck.rb
@@ -13,18 +13,28 @@ class Deck
   def high_ranking_cards
     # this method will return an array of cards in the deck that have a rank of
     # 11 or above (face cards and aces)
+    #  collector_array = []
+    # array.each do |card|
+    #   if card.rank >= 11
+          # collector_array << card
+    # end
   end
 
   def percent_high_ranking
     # this method will return the percentage of cards that a high ranking
+    # identify number of cards with high ranking
+    # divide no# of high ranking cards with total number of cards
+    # output the resulting percentage
   end
 
   def remove_card
     # this method will remove the top card form the deck
+    # array.pop
   end
 
   def add_card
     # this method will add one card to the bottem (end) of the deck
+    # array.insert(0, element)
   end
 
 end
