9f323e45a3cffda57c70b144be267745c4540f63
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-08-17 19:02:46 -0600

    Update'require ../lib/card'

--- test/card_test.rb
+++ test/card_test.rb
@@ -1,6 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/card'
+require '../lib/card'
 
 class CardTest < Minitest::Test
   def setup
@@ -15,7 +15,7 @@ class CardTest < Minitest::Test
 
   def test_it_has_readable_attributes
     card = Card.new(:diamond, 'Queen', 12)
-    
+
     assert_equal :diamond, card.suit
     assert_equal 'Queen', card.value
     assert_equal 12, card.rank
