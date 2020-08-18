973dc1691b474d5fe1d7d92995211bce5ce1c42c
Will Dunlap <dunlapww@gmail.com>
2020-08-18 15:15:51 -0600

    Fix decks in runner

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -2,8 +2,9 @@ require './lib/card'
 require './lib/deck'
 require './lib/player'
 require './lib/turn'
+require './lib/game'
 
-
+#variables to setup a standard deck of 52 cards
 card_values = %w(2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, King, Ace)
 card_ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 card_hash = card_values.zip(card_ranks).to_h
@@ -21,8 +22,15 @@ end
 full_deck.shuffle!
 
 #divide the deck into equal decks
-deck1 = full_deck.shift(26)
-deck2 = full_deck
+deck1 = Deck.new(full_deck.shift(26))
+deck2 = Deck.new(full_deck)
 
-p deck1.size
-p deck2.size
+#setup two players, each with a deck
+player1 = Player.new("Alex",deck1)
+player2 = Player.new("Ghengis",deck2)
+
+#setup a new game
+game = Game.new(player1,player2)
+
+#play the game
+game.start
