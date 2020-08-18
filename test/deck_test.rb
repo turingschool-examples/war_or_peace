ab74903c9b16381eacb97d62c67ae8f2a7b70aa7
Cydnee Owens <cks410@yahoo.com>
2020-08-18 08:00:33 -0600

    Add iteration 1

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -4,40 +4,42 @@ require './lib/deck'
 
 class DeckTest < Minitest::Test
   def setup
-    deck = Deck.new[cards]
+    cards = Deck.new[2..14]
   end
 
   def test_it_exists
-    deck = Deck.new[cards]
+    deck = Deck.new[2..14]
 
     assert_instance_of Deck, deck
   end
 
   def test_rank_of_card_at
-    deck = Deck.new([cards])
-    deck.index()
+    deck = Deck.new[2..14]
+    rank = []
+    deck.each do |num|
+      rank << num.index
   end
 
-  def high_ranking_cards
-    ranks = [11, 12, 13]
-    ranks.find_all do |rank|
+  def test_high_ranking_cards
+    deck = Deck.new[2..14]
+    deck.find_all do |rank|
       rank.count >= 11
   end
 
   def test_remove_card
-    deck = Deck.new[cards]
+    deck = Deck.new[2..14]
     deck.push
   end
 
   def test_add_card
-    deck = Deck.new[cards]
+    deck = Deck.new[2..14]
 
     assert deck.add_card
   end
 
 end
 
-p test_rank_of_card_at
-p high_ranking_cards
+p rank(0)
+p test_high_ranking_cards
 p test_remove_card
 p test_add_card
