9876505d3bd2a9067f64eb7492b1134e6d113adc
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-08-17 20:34:12 -0600

    Update first three tests

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,14 +5,35 @@ require '../lib/card'
 
 class DeckTest < Minitest::Test
 
-def setup
-  deck = Deck.new([cards])
-end
-
 def test_it_exists
-  deck = Deck.new()
+  card1 = Card.new(:diamond, 'Queen', 12)
+  card2 = Card.new(:spade, '3', 3)
+  card3 = Card.new(:heart, 'Ace', 14)
+  cards = [@card1, @card2, @card3]
+  deck = Deck.new(@cards)
 
   assert_instance_of Deck, deck
+end
+
+def test_it_has_cards
+  card1 = Card.new(:diamond, 'Queen', 12)
+  card2 = Card.new(:spade, '3', 3)
+  card3 = Card.new(:heart, 'Ace', 14)
+  cards = [@card1, @card2, @card3]
+  deck = Deck.new(@cards)
+
+  assert_equal [@card1, @card2, @card3], deck.cards
+end
+
+def test_it_can_find_rank
+  card1 = Card.new(:diamond, 'Queen', 12)
+  card2 = Card.new(:spade, '3', 3)
+  card3 = Card.new(:heart, 'Ace', 14)
+  cards = [@card1, @card2, @card3]
+  deck = Deck.new(@cards)
+
+  assert_equal 12, deck.rank_of_card_at(0)
+  assert_equal 14, deck.rank_of_card_at(2)
 end
 
 end
