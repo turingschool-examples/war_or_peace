c9d08dd49a33f72bb7169ee01607fb03ed81d99f
Cydnee Owens <cks410@yahoo.com>
2020-08-18 10:46:09 -0600

    Add iteration 1

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,8 +1,10 @@
 class Deck
-  attr_reader :deck
+  attr_reader :deck, :rank_of_card_at, :high_ranking_cards
 
   def initialize(deck)
     @deck = deck
+    @rank_of_card_at = rank_of_card_at
+    @high_ranking_cards = high_ranking_cards
 
   end
 end
