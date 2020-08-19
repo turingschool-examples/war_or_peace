4dadad026b2876f0f92ac6640f561235ff22b654
Eduardo Parra <helloeduardo@icloud.com>
2020-08-19 16:07:36 -0600

    Create turn test

---
+++ test/turn_test.rb
@@ -0,0 +1,12 @@
+require './lib/deck'
+require './lib/card'
+require './lib/player'
+require './lib/turn'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class TurnTest < Minitest::Test
+
+
+
+end
