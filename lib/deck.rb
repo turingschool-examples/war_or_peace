08d13a98b9e507582e90b093087ec49d835d6c73
Sean Steel <seanrsteel@gmail.com>
2020-08-19 06:28:12 -0600

    Add high ranking card method and test

--- lib/deck.rb
+++ lib/deck.rb
@@ -5,4 +5,15 @@ class Deck
     @cards = cards
 
   end
+
+  def high_ranking_cards
+    high_cards = Array.new
+    @cards.each do |card|
+      if card.rank >= 11
+        high_cards << card
+      end
+    end
+    high_cards
+  end
+
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -3,24 +3,31 @@ require './lib/card'
 require 'minitest/autorun'
 require 'minitest/pride'
 
-card1 = Card.new(:diamond, 'Queen', 12)
-card2 = Card.new(:spade, '3', 3)
-card3 = Card.new(:heart, 'Ace', 14)
-cards = [card1, card2, card3]
 
 class DeckTest < Minitest::Test
+  def setup
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @cards = [@card1, @card2, @card3]
+    @deck = Deck.new(@cards)
+  end
+
   def test_it_exists
-    deck = Deck.new(@cards)
 
-    assert_instance_of Deck, deck
+    assert_instance_of Deck, @deck
+  end
+
+  def test_has_attributes
+
+    assert_equal @cards, @deck.cards
+  end
+
+  def test_high_ranking_cards
+    high_ranking_cards = [@card1, @card3]
+
+    assert_equal high_ranking_cards, @deck.high_ranking_cards
   end
 
 
-  end
-
-
-
-
-
-
-#require 'pry'; binding.pry
+end
