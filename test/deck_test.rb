0e0c8094d40371fda38844f288d06a433fac231d
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-08-19 16:17:06 -0600

    Update formatting and indents

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,78 +5,78 @@ require './lib/card'
 
 class DeckTest < Minitest::Test
 
-def test_it_exists
-  card1 = Card.new(:diamond, 'Queen', 12)
-  card2 = Card.new(:spade, '3', 3)
-  card3 = Card.new(:heart, 'Ace', 14)
-  cards = [card1, card2, card3]
-  deck = Deck.new(cards)
+  def test_it_exists
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
 
-  assert_instance_of Deck, deck
-end
+    assert_instance_of Deck, deck
+  end
 
-def test_it_has_cards
-  card1 = Card.new(:diamond, 'Queen', 12)
-  card2 = Card.new(:spade, '3', 3)
-  card3 = Card.new(:heart, 'Ace', 14)
-  cards = [card1, card2, card3]
-  deck = Deck.new(cards)
+  def test_it_has_cards
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
 
-  assert_equal [card1, card2, card3], deck.cards
-end
+    assert_equal [card1, card2, card3], deck.cards
+  end
 
-def test_it_can_find_rank
-  card1 = Card.new(:diamond, 'Queen', 12)
-  card2 = Card.new(:spade, '3', 3)
-  card3 = Card.new(:heart, 'Ace', 14)
-  cards = [card1, card2, card3]
-  deck = Deck.new(cards)
+  def test_it_can_find_rank
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
 
-  assert_equal 12, deck.rank_of_card_at(0)
-  assert_equal 14, deck.rank_of_card_at(2)
-  assert_equal [card1, card2, card3], deck.cards
-end
+    assert_equal 12, deck.rank_of_card_at(0)
+    assert_equal 14, deck.rank_of_card_at(2)
+    assert_equal [card1, card2, card3], deck.cards
+  end
 
-def test_find_high_ranking_cards
-  card1 = Card.new(:diamond, 'Queen', 12)
-  card2 = Card.new(:spade, '3', 3)
-  card3 = Card.new(:heart, 'Ace', 14)
-  cards = [card1, card2, card3]
-  deck = Deck.new(cards)
+  def test_find_high_ranking_cards
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
 
 
-  assert_equal [card1, card3], deck.high_ranking_cards
-end
+    assert_equal [card1, card3], deck.high_ranking_cards
+  end
 
-def test_find_percent_high_ranking
-  card1 = Card.new(:diamond, 'Queen', 12)
-  card2 = Card.new(:spade, '3', 3)
-  card3 = Card.new(:heart, 'Ace', 14)
-  cards = [card1, card2, card3]
-  deck = Deck.new(cards)
+  def test_find_percent_high_ranking
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
 
-  assert_equal 66.67, deck.percent_high_ranking
-end
+    assert_equal 66.67, deck.percent_high_ranking
+  end
 
-def test_to_remove_and_add_cards
-  # skip
-  card1 = Card.new(:diamond, 'Queen', 12)
-  card2 = Card.new(:spade, '3', 3)
-  card3 = Card.new(:heart, 'Ace', 14)
-  cards = [card1, card2, card3]
-  deck = Deck.new(cards)
+  def test_to_remove_and_add_cards
+    # skip
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
 
-  assert_equal card1, deck.remove_card
-  assert_equal [card2, card3], deck.cards
-  assert_equal [card3], deck.high_ranking_cards
-  assert_equal 50.0, deck.percent_high_ranking
+    assert_equal card1, deck.remove_card
+    assert_equal [card2, card3], deck.cards
+    assert_equal [card3], deck.high_ranking_cards
+    assert_equal 50.0, deck.percent_high_ranking
 
-  #Adding cards test:
-  card4 = Card.new(:club, '5', 5)
+    #Adding cards test:
+    card4 = Card.new(:club, '5', 5)
 
-  assert_equal [card2, card3, card4], deck.add_card(card4)
-  assert_equal [card3], deck.high_ranking_cards
-  assert_equal 33.33, deck.percent_high_ranking
-end
+    assert_equal [card2, card3, card4], deck.add_card(card4)
+    assert_equal [card3], deck.high_ranking_cards
+    assert_equal 33.33, deck.percent_high_ranking
+  end
 
 end
