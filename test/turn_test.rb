62aafbc7da3b06dd1bd7e4c83a8de8751ff59175
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-08-19 17:35:15 -0600

    Create TurnTest file

---
+++ test/turn_test.rb
@@ -0,0 +1,10 @@
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+require 'minitest/test'
+require 'minitest/pride'
+
+class TurnTest < MiniTest::Test
+
+end
