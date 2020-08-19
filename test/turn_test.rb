d6795c81abd0d3a3d0f1d65a4f1e85642221b592
George Soderholm <georgesoderholm@gmail.com>
2020-08-19 14:56:29 -0600

    Add six test methods

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -160,30 +160,308 @@ class TurnTest < Minitest::Test
   end
 
   def test_it_can_have_a_winner_for_a_basic_turn
-    skip
+    # skip
+    cards1 = []
+    card1 = Card.new(:diamond, 'Ace', 14)
+    cards1 << card1
+    card2 = Card.new(:spade, '3', 3)
+    cards1 << card2
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards1 << card3
+    card4 = Card.new(:diamond, '9', 9)
+    cards1 << card4
+    deck1 = Deck.new(cards1)
+    player1 = Player.new("August", deck1)
+
+    cards2 = []
+    card1 = Card.new(:diamond, 'King', 13)
+    cards2 << card1
+    card2 = Card.new(:spade, '2', 2)
+    cards2 << card2
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards2 << card3
+    card4 = Card.new(:club, 'Jack', 11)
+    cards2 << card4
+    deck2 = Deck.new(cards2)
+    player2 = Player.new("Dale", deck2)
+    turn = Turn.new(player1, player2)
+
+    turn.type
+    turn.winner?
+
+    assert_equal "August", turn.winner?.name
   end
 
   def test_it_can_have_a_winner_for_a_war_turn
-    skip
+    # skip
+    cards1 = []
+    card1 = Card.new(:diamond, 'King', 13)
+    cards1 << card1
+    card2 = Card.new(:spade, '3', 3)
+    cards1 << card2
+    card3 = Card.new(:heart, '4', 4)
+    cards1 << card3
+    card4 = Card.new(:diamond, '9', 9)
+    cards1 << card4
+    deck1 = Deck.new(cards1)
+    player1 = Player.new("August", deck1)
+
+    cards2 = []
+    card1 = Card.new(:diamond, 'King', 13)
+    cards2 << card1
+    card2 = Card.new(:spade, '2', 2)
+    cards2 << card2
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards2 << card3
+    card4 = Card.new(:club, 'Jack', 11)
+    cards2 << card4
+    deck2 = Deck.new(cards2)
+    player2 = Player.new("Dale", deck2)
+
+    turn = Turn.new(player1, player2)
+    turn.type
+    turn.winner?
+
+    assert_equal "Dale", turn.winner?.name
   end
 
   def test_it_can_doesnt_have_a_winner_for_mutually_assured_destruction
-    skip
+    # skip
+    cards1 = []
+    card1 = Card.new(:diamond, 'King', 13)
+    cards1 << card1
+    card2 = Card.new(:spade, '3', 3)
+    cards1 << card2
+    card3 = Card.new(:heart, '6', 6)
+    cards1 << card3
+    card4 = Card.new(:diamond, '9', 9)
+    cards1 << card4
+    deck1 = Deck.new(cards1)
+    player1 = Player.new("August", deck1)
+
+    cards2 = []
+    card1 = Card.new(:diamond, 'King', 13)
+    cards2 << card1
+    card2 = Card.new(:spade, '2', 2)
+    cards2 << card2
+    card3 = Card.new(:heart, '6', 6)
+    cards2 << card3
+    card4 = Card.new(:club, 'Jack', 11)
+    cards2 << card4
+    deck2 = Deck.new(cards2)
+    player2 = Player.new("Dale", deck2)
+    turn = Turn.new(player1, player2)
+
+    turn.type
+    turn.winner?
+
+    assert_equal "No Winner", turn.winner?
   end
 
   def test_it_can_pile_cards_for_a_war_turn
-    skip
+    # skip
+    cards1 = []
+    card1 = Card.new(:diamond, 'King', 13)
+    cards1 << card1
+    card2 = Card.new(:spade, '3', 3)
+    cards1 << card2
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards1 << card3
+    card4 = Card.new(:diamond, '9', 9)
+    cards1 << card4
+    deck1 = Deck.new(cards1)
+    player1 = Player.new("August", deck1)
+
+    cards2 = []
+    card1 = Card.new(:diamond, 'King', 13)
+    cards2 << card1
+    card2 = Card.new(:spade, '2', 2)
+    cards2 << card2
+    card3 = Card.new(:heart, '6', 6)
+    cards2 << card3
+    card4 = Card.new(:club, 'Jack', 11)
+    cards2 << card4
+    deck2 = Deck.new(cards2)
+    player2 = Player.new("Dale", deck2)
+
+    turn = Turn.new(player1, player2)
+    turn.type
+    turn.winner?
+    turn.pile_cards
+
+    assert_equal 6, turn.spoils_of_war.length
   end
 
   def test_it_can_pile_cards_for_a_basic_turn
-    skip
+    # skip
+    cards1 = []
+    card1 = Card.new(:diamond, 'Ace', 14)
+    cards1 << card1
+    card2 = Card.new(:spade, '3', 3)
+    cards1 << card2
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards1 << card3
+    card4 = Card.new(:diamond, '9', 9)
+    cards1 << card4
+    deck1 = Deck.new(cards1)
+    player1 = Player.new("August", deck1)
+
+    cards2 = []
+    card1 = Card.new(:diamond, 'King', 13)
+    cards2 << card1
+    card2 = Card.new(:spade, '2', 2)
+    cards2 << card2
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards2 << card3
+    card4 = Card.new(:club, 'Jack', 11)
+    cards2 << card4
+    deck2 = Deck.new(cards2)
+    player2 = Player.new("Dale", deck2)
+    turn = Turn.new(player1, player2)
+
+    turn.type
+    turn.winner?
+    turn.pile_cards
+
+    assert_equal 2, turn.spoils_of_war.length
   end
 
   def test_it_can_remove_cards_from_each_player_in_mutually_assured_destruction_and_throw_those_away
-    skip
+    # skip
+    cards1 = []
+    card1 = Card.new(:diamond, 'King', 13)
+    cards1 << card1
+    card2 = Card.new(:spade, '3', 3)
+    cards1 << card2
+    card3 = Card.new(:heart, '6', 6)
+    cards1 << card3
+    card4 = Card.new(:diamond, '9', 9)
+    cards1 << card4
+    deck1 = Deck.new(cards1)
+    player1 = Player.new("August", deck1)
+
+    cards2 = []
+    card1 = Card.new(:diamond, 'King', 13)
+    cards2 << card1
+    card2 = Card.new(:spade, '2', 2)
+    cards2 << card2
+    card3 = Card.new(:heart, '6', 6)
+    cards2 << card3
+    card4 = Card.new(:club, 'Jack', 11)
+    cards2 << card4
+    deck2 = Deck.new(cards2)
+    player2 = Player.new("Dale", deck2)
+    turn = Turn.new(player1, player2)
+
+    turn.type
+    turn.winner?
+    turn.pile_cards
+
+    assert turn.spoils_of_war.empty?
   end
 
-  def test_it_can_award_the_spoils_to_the_winner_of_the_turn
-    skip
+  def test_it_can_award_the_spoils_to_the_winner_of_a_basic_turn
+    # skip
+    cards1 = []
+    card1 = Card.new(:diamond, 'Ace', 14)
+    cards1 << card1
+    card2 = Card.new(:spade, '3', 3)
+    cards1 << card2
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards1 << card3
+    card4 = Card.new(:diamond, '9', 9)
+    cards1 << card4
+    deck1 = Deck.new(cards1)
+    player1 = Player.new("August", deck1)
+
+    cards2 = []
+    card1 = Card.new(:diamond, 'King', 13)
+    cards2 << card1
+    card2 = Card.new(:spade, '2', 2)
+    cards2 << card2
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards2 << card3
+    card4 = Card.new(:club, 'Jack', 11)
+    cards2 << card4
+    deck2 = Deck.new(cards2)
+    player2 = Player.new("Dale", deck2)
+    turn = Turn.new(player1, player2)
+
+    turn.type
+    turn.winner?
+    turn.pile_cards
+    turn.award_spoils
+
+    assert_includes turn.winner.deck.cards, turn.spoils_of_war
+  end
+
+  def test_it_can_award_the_spoils_to_the_winner_of_a_war_turn
+    # skip
+    cards1 = []
+    card1 = Card.new(:diamond, 'King', 13)
+    cards1 << card1
+    card2 = Card.new(:spade, '3', 3)
+    cards1 << card2
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards1 << card3
+    card4 = Card.new(:diamond, '9', 9)
+    cards1 << card4
+    deck1 = Deck.new(cards1)
+    player1 = Player.new("August", deck1)
+
+    cards2 = []
+    card1 = Card.new(:diamond, 'King', 13)
+    cards2 << card1
+    card2 = Card.new(:spade, '2', 2)
+    cards2 << card2
+    card3 = Card.new(:heart, '6', 6)
+    cards2 << card3
+    card4 = Card.new(:club, 'Jack', 11)
+    cards2 << card4
+    deck2 = Deck.new(cards2)
+    player2 = Player.new("Dale", deck2)
+
+    turn = Turn.new(player1, player2)
+    turn.type
+    turn.winner?
+    turn.pile_cards
+    turn.award_spoils
+
+    assert_includes turn.winner.deck.cards, turn.spoils_of_war
+  end
+
+  def test_it_can_tosses_six_cards_after_a_mutally_assured_destruction_turn
+    # skip
+    cards1 = []
+    card1 = Card.new(:diamond, 'King', 13)
+    cards1 << card1
+    card2 = Card.new(:spade, '3', 3)
+    cards1 << card2
+    card3 = Card.new(:heart, '6', 6)
+    cards1 << card3
+    card4 = Card.new(:diamond, '9', 9)
+    cards1 << card4
+    deck1 = Deck.new(cards1)
+    player1 = Player.new("August", deck1)
+
+    cards2 = []
+    card1 = Card.new(:diamond, 'King', 13)
+    cards2 << card1
+    card2 = Card.new(:spade, '2', 2)
+    cards2 << card2
+    card3 = Card.new(:heart, '6', 6)
+    cards2 << card3
+    card4 = Card.new(:club, 'Jack', 11)
+    cards2 << card4
+    deck2 = Deck.new(cards2)
+    player2 = Player.new("Dale", deck2)
+    turn = Turn.new(player1, player2)
+
+    turn.type
+    turn.winner?
+    turn.pile_cards
+    turn.award_spoils
+
+    assert turn.spoils_of_war.empty?
   end
 end
