30632a68cf9c1f638eb46794a8e5b244d763f021
Cydnee Owens <cks410@yahoo.com>
2020-08-18 21:44:34 -0600

    Add iteration 1 deck

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -34,16 +34,6 @@ class DeckTest < Minitest::Test
 
     assert_equal 12, deck.rank_of_card_at(0)
     assert_equal 14, deck.rank_of_card_at(2)
-
-  end
-
-  def test_there_are_cards
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-
     assert_equal [card1, card2, card3], deck.cards
   end
 
@@ -57,26 +47,41 @@ class DeckTest < Minitest::Test
     assert_equal [card1, card3], deck.high_ranking_cards
   end
 
-    def test_percent_high_ranking
+  # def test_percent_high_ranking
+  #   card1 = Card.new(:diamond, 'Queen', 12)
+  #   card2 = Card.new(:spade, '3', 3)
+  #   card3 = Card.new(:heart, 'Ace', 14)
+  #   cards = [card1, card2, card3]
+  #   deck = Deck.new(cards)
+  #
+  #   assert_equal 66.67, deck.percent_high_ranking
+  # end
+
+  def test_remove_card
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
-    require "pry"; binding.pry
 
-    assert_equal 66.67, deck.percent_high_ranking
+    assert_equal [card2, card3], deck.remove_card
+    assert_equal [card2, card3], deck.cards
+    # assert_equal [card3], deck.high_ranking_cards
+    assert_equal 50.0, deck.percent_high_ranking
+  end
 
+  def test_add_card
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    card4 = Card.new(:club, '5', 5)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
 
-  # def test_remove_card
-  #   deck = Deck.new[2..14]
-  #   deck.push
-  #
-  # end
-  #
-  # def test_add_card(card)
-  #   deck = Deck.new[2..14]
-  #   @deck << card + 1
-  # end
+    assert_equal [card1, card2, card3, card4], deck.add_card(card4)
+    assert_equal [card1, card2, card3, card4], deck.cards
+    # assert_equal [card1, card3], deck.high_ranking_cards
+    assert_equal 33.33, deck.percent_high_ranking
+  end
 
 end
