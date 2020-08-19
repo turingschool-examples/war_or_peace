e4df217a9aab8a8c4cc1d09500aa11644be39406
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-08-19 16:16:02 -0600

    Update last test to include test for adding cards

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -58,7 +58,7 @@ def test_find_percent_high_ranking
   assert_equal 66.67, deck.percent_high_ranking
 end
 
-def test_to_remove_card
+def test_to_remove_and_add_cards
   # skip
   card1 = Card.new(:diamond, 'Queen', 12)
   card2 = Card.new(:spade, '3', 3)
@@ -70,6 +70,13 @@ def test_to_remove_card
   assert_equal [card2, card3], deck.cards
   assert_equal [card3], deck.high_ranking_cards
   assert_equal 50.0, deck.percent_high_ranking
+
+  #Adding cards test:
+  card4 = Card.new(:club, '5', 5)
+
+  assert_equal [card2, card3, card4], deck.add_card(card4)
+  assert_equal [card3], deck.high_ranking_cards
+  assert_equal 33.33, deck.percent_high_ranking
 end
 
 end
