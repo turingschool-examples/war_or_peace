6bb4e078490c872465a49547710b8e80e088f427
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-08-19 14:28:32 -0600

    Change assert on 70 to expect card1

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -34,15 +34,6 @@ def test_it_can_find_rank
 
   assert_equal 12, deck.rank_of_card_at(0)
   assert_equal 14, deck.rank_of_card_at(2)
-end
-
-def test_it_has_cards
-  card1 = Card.new(:diamond, 'Queen', 12)
-  card2 = Card.new(:spade, '3', 3)
-  card3 = Card.new(:heart, 'Ace', 14)
-  cards = [card1, card2, card3]
-  deck = Deck.new(cards)
-
   assert_equal [card1, card2, card3], deck.cards
 end
 
@@ -63,9 +54,24 @@ def test_find_percent_high_ranking
   card3 = Card.new(:heart, 'Ace', 14)
   cards = [card1, card2, card3]
   deck = Deck.new(cards)
-  require "pry"; binding.pry 
+  require "pry"; binding.pry
 
   assert_equal 66.67, deck.percent_high_ranking
+end
+
+def test_to_remove_card
+  skip
+  card1 = Card.new(:diamond, 'Queen', 12)
+  card2 = Card.new(:spade, '3', 3)
+  card3 = Card.new(:heart, 'Ace', 14)
+  cards = [card1, card2, card3]
+  deck = Deck.new(cards)
+
+  assert_equal [card1], deck.remove_card
+  assert_equal [card2, card3], deck.cards
+#skip
+  # assert_equal [card3], deck.high_ranking_cards
+  # assert_equal 50.0, deck.percent_high_ranking
 end
 
 end
