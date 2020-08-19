29ac56e84c4ac2aa15f13ed029819ee35aba4aec
George Soderholm <georgesoderholm@gmail.com>
2020-08-19 08:26:13 -0600

    Add one method: turn

---
+++ lib/turn.rb
@@ -0,0 +1,19 @@
+class Turn
+  attr_accessor :player1, :player2, :spoils_of_war
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @spoils_of_war = spoils_of_war
+  end
+
+  def type
+    if (@player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)) && (@player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2))
+      return :mutally_assured_destruction
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
+      :war
+    elsif @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
+      :basic
+    end
+  end
+
+end
