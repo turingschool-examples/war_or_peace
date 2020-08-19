c769cc414470fdc7c05256b65a83938e29cd2c73
Aidan Murray <aidanhansm@gmail.com>
2020-08-18 19:56:53 -0600

    Add testing cards to deck test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -12,4 +12,14 @@ class DeckTest < Minitest::Test
 
     assert_instance_of(Deck, deck)
   end
+
+  def test_cards
+    card = Card.new(:spade, 'Ten', 10)
+    cards = []
+    cards << card
+    deck = Deck.new(cards)
+
+    assert_equal(cards, deck.cards)
+    assert_equal(card, deck.cards[0])
+  end
 end
