5883248b13c2f3ab84e246d83d0c556f4b2ce501
Cydnee Owens <cks410@yahoo.com>
2020-08-18 19:36:44 -0600

    Add iteration 1

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,43 +1,82 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/deck'
+require './lib/card'
 
 
 class DeckTest < Minitest::Test
-  def setup
-    cards = Deck.new[2..14]
-  end
-
   def test_it_exists
-    deck = Deck.new[2..14]
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
 
     assert_instance_of Deck, deck
   end
 
+  def test_there_are_cards
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal [card1, card2, card3], deck.cards
+  end
+
   def test_rank_of_card_at
-    deck = Deck.new[2..14]
-    rank = []
-    deck.each do |num|
-      rank << num.index
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal 12, deck.rank_of_card_at(0)
+    assert_equal 14, deck.rank_of_card_at(2)
+
+  end
+
+  def test_there_are_cards
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal [card1, card2, card3], deck.cards
   end
 
   def test_high_ranking_cards
-    deck = Deck.new[2..14]
-    deck.find_all do |rank|
-      rank >= 11
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal [card1, card3], deck.high_ranking_cards
   end
 
-  def test_remove_card
-    deck = Deck.new[2..14]
-    deck.push
+    def test_percent_high_ranking
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    require "pry"; binding.pry
 
-    #or should remove_card be the method?
-  end
+    assert_equal 66.67, deck.percent_high_ranking
 
-  def test_add_card(card)
-    @deck << card
 
-    @deck + 1
-  end
+  # def test_remove_card
+  #   deck = Deck.new[2..14]
+  #   deck.push
+  #
+  # end
+  #
+  # def test_add_card(card)
+  #   deck = Deck.new[2..14]
+  #   @deck << card + 1
+  # end
 
 end
