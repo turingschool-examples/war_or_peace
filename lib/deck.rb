e45518c8345092bd8a7f248d9bfa7238a541b874
Cydnee Owens <cks410@yahoo.com>
2020-08-18 19:36:06 -0600

    Add iteration 1

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,10 +1,27 @@
 class Deck
-  attr_reader :deck
+  attr_reader :cards
 
-  def initialize(deck)
-    @deck = deck
-    @rank_of_card_at = rank_of_card_at
-    @high_ranking_cards = high_ranking_cards
+  def initialize(cards)
+    @cards = cards
 
   end
+
+  def rank_of_card_at(index)
+    @cards.at(index).rank
+  end
+
+  def high_ranking_cards
+    high_cards = []
+    cards.each do |card|
+      if card.rank >= 11
+        high_cards << card
+      end
+  end
+  p high_cards
+end
+
+  def percent_high_ranking
+    pecent = cards.high_ranking_cards.count / cards
+  end
+  p percent
 end
