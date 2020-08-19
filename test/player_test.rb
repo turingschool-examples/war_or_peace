c1ca580a71ec5bf1767a26fcb46dd729eb1d2939
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-08-19 17:26:48 -0600

    Update test to remove card

--- test/player_test.rb
+++ test/player_test.rb
@@ -35,4 +35,33 @@ class PlayerTest < MiniTest::Test
 
     assert_equal deck, player.deck
   end
+
+  def test_if_player_can_lose
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+
+    assert_equal false, player.has_lost?
+  end
+
+  def test_if_lose_remove_top_card
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+
+    assert_equal card1, player.deck.remove_card
+    assert_equal false, player.has_lost?
+
+    #Remove another card and check if lost
+    assert_equal card2, player.deck.remove_card
+    assert_equal false, player.has_lost?
+
+    #Remove the last card and check if lost
+    assert_equal card3, player.deck.remove_card
+    assert_equal true, player.has_lost?
+  end
 end
