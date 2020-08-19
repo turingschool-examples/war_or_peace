62a93e634530b5e87ad9d7f8492d70df6f8c7087
Connor Ferguson <cpfergus1@gmail.com>
2020-08-19 09:13:46 -0600

    Updated formatting for readability

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -15,22 +15,22 @@ class DeckTest <Minitest::Test
   end
 
   def Test_deck_exists
-  card1 = Card.new(:diamond, 'Queen', 12)
-  card2 = Card.new(:spade, '3', 3)
-  card3 = Card.new(:heart, 'Ace', 14)
-  cards = [card1, card2, card3]
-  deck = Deck.new(cards)
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
   assert_instance_of Deck, deck
   end
 
   def test_rank_of_cards
-  card1 = Card.new(:diamond, 'Queen', 12)
-  card2 = Card.new(:spade, '3', 3)
-  card3 = Card.new(:heart, 'Ace', 14)
-  cards = [card1, card2, card3]
-  deck = Deck.new(cards)
-  deck.rank_of_card(0)
-  assert_equal 12, deck.rank_of_card(0)
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    deck.rank_of_card(0)
+    assert_equal 12, deck.rank_of_card(0)
   end
 
   def test_high_rank_cards
@@ -43,12 +43,13 @@ class DeckTest <Minitest::Test
     deck.high_ranking_cards
     assert_equal highcards, deck.high_ranking_cards
   end
+
   def test_percent_high_rank
-  card1 = Card.new(:diamond, 'Queen', 12)
-  card2 = Card.new(:spade, '3', 3)
-  card3 = Card.new(:heart, 'Ace', 14)
-  cards = [card1, card2, card3]
-  deck = Deck.new(cards)
-  deck.percent_high_ranking
-  assert_in_delta 2/3 66.67
-end
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    deck.percent_high_ranking
+    assert_in_delta 2/3 66.67
+  end
