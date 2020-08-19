34d3ba2376895eeee6a24f4264fa547214306a4a
Kevin Cuadros <kev040798@gmail.com>
2020-08-19 18:10:58 -0400

    Update test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -38,6 +38,17 @@ class DeckTest < Minitest::Test
     assert_equal 14, deck.rank_of_card_at(2)
   end
 
+  def test_returns_nil_with_index
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    assert_nil deck.rank_of_card_at(5)
+  end
+
   def test_it_return_high_ranking_cards
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
