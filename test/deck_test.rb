36c07d9a87ebb6a4535e3b0029e86e18d4bfbf5e
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-08-19 16:07:52 -0600

    Add tests for rank test and update add new card test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -18,11 +18,14 @@ class DeckTest < Minitest::Test
   end
 
   def test_rank_of_card
-    card = Card.new(:diamond, 'Queen', 12)
-    cards = [card]
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
     deck = Deck.new(cards)
 
     assert_equal 12, deck.rank_of_card_at(0)
+    assert_equal 14, deck.rank_of_card_at(2)
   end
 
   def test_high_ranking_cards_returns_array_of_cards_with_rank_over_ten
@@ -35,7 +38,7 @@ class DeckTest < Minitest::Test
     assert_equal [card1, card3], deck.high_ranking_cards
   end
 
-  def test_percent_high_ranking_gives_total_percentage_of_high_ranking_cardskip
+  def test_percent_high_ranking_gives_total_percentage_of_high_ranking_card
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
@@ -60,10 +63,12 @@ class DeckTest < Minitest::Test
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
+    cards = [card1, card2]
     deck = Deck.new(cards)
 
-
+    assert_equal [card1, card2], deck.cards
+    assert_equal [card1, card2, card3], deck.add_card(card3)
+    assert_equal [card1, card2, card3], deck.cards
 
   end
 
