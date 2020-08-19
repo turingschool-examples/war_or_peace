8f95d75bf2a23c3e42ed1f6765a1fcb517d79191
Kevin Cuadros <kev040798@gmail.com>
2020-08-18 20:56:20 -0400

    Issue eighteen done

--- test/player_test.rb
+++ test/player_test.rb
@@ -37,4 +37,20 @@ class PlayerTest < Minitest::Test
 
     assert_equal deck, player.deck
   end
+
+  def test_player_lost
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+
+    player.deck.remove_card
+    player.deck.remove_card
+    player.deck.remove_card
+    player.deck
+
+    assert_equal true, player.has_lost?
+  end
 end
