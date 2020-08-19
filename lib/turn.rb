e45dfe1885cc5dfa776ad847009fe368b2c5c44d
Kevin Cuadros <kev040798@gmail.com>
2020-08-19 10:50:53 -0400

    Issue twenty-one done

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,7 +3,9 @@ require_relative 'deck'
 require_relative 'player'
 
 class Turn
+  attr_reader :player1, :player2
   def initialize(player1, player2)
-    
+    @player1 = player1
+    @player2 = player2
   end
 end
