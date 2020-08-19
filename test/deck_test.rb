650359f53cad366fa824f9b957ef27691a9dbdfc
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-08-18 19:37:29 -0600

    Add percent_high_ranking and find_high_ranking_cards tests

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require '../lib/deck'
-require '../lib/card'
+require './lib/deck'
+require './lib/card'
 
 class DeckTest < Minitest::Test
 
@@ -9,8 +9,8 @@ def test_it_exists
   card1 = Card.new(:diamond, 'Queen', 12)
   card2 = Card.new(:spade, '3', 3)
   card3 = Card.new(:heart, 'Ace', 14)
-  cards = [@card1, @card2, @card3]
-  deck = Deck.new(@cards)
+  cards = [card1, card2, card3]
+  deck = Deck.new(cards)
 
   assert_instance_of Deck, deck
 end
@@ -19,21 +19,53 @@ def test_it_has_cards
   card1 = Card.new(:diamond, 'Queen', 12)
   card2 = Card.new(:spade, '3', 3)
   card3 = Card.new(:heart, 'Ace', 14)
-  cards = [@card1, @card2, @card3]
-  deck = Deck.new(@cards)
+  cards = [card1, card2, card3]
+  deck = Deck.new(cards)
 
-  assert_equal [@card1, @card2, @card3], deck.cards
+  assert_equal [card1, card2, card3], deck.cards
 end
 
 def test_it_can_find_rank
   card1 = Card.new(:diamond, 'Queen', 12)
   card2 = Card.new(:spade, '3', 3)
   card3 = Card.new(:heart, 'Ace', 14)
-  cards = [@card1, @card2, @card3]
-  deck = Deck.new(@cards)
+  cards = [card1, card2, card3]
+  deck = Deck.new(cards)
 
   assert_equal 12, deck.rank_of_card_at(0)
   assert_equal 14, deck.rank_of_card_at(2)
+end
+
+def test_it_has_cards
+  card1 = Card.new(:diamond, 'Queen', 12)
+  card2 = Card.new(:spade, '3', 3)
+  card3 = Card.new(:heart, 'Ace', 14)
+  cards = [card1, card2, card3]
+  deck = Deck.new(cards)
+
+  assert_equal [card1, card2, card3], deck.cards
+end
+
+def test_find_high_ranking_cards
+  card1 = Card.new(:diamond, 'Queen', 12)
+  card2 = Card.new(:spade, '3', 3)
+  card3 = Card.new(:heart, 'Ace', 14)
+  cards = [card1, card2, card3]
+  deck = Deck.new(cards)
+
+
+  assert_equal [card1, card3], deck.high_ranking_cards
+end
+
+def test_find_percent_high_ranking
+  card1 = Card.new(:diamond, 'Queen', 12)
+  card2 = Card.new(:spade, '3', 3)
+  card3 = Card.new(:heart, 'Ace', 14)
+  cards = [card1, card2, card3]
+  deck = Deck.new(cards)
+  require "pry"; binding.pry 
+
+  assert_equal 66.67, deck.percent_high_ranking
 end
 
 end
