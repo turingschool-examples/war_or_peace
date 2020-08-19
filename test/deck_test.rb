6fd2a809f4ad886a3caf14656e327b7536919478
George Soderholm <georgesoderholm@gmail.com>
2020-08-18 19:45:50 -0600

    Add three more test methods

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -58,6 +58,7 @@ class DeckTest < Minitest::Test
   end
 
   def test_it_can_find_the_rank_of_a_particular_card
+    # skip
     cards = []
     card1 = Card.new(:diamond, 'Queen', 12)
     cards << card1
@@ -69,11 +70,12 @@ class DeckTest < Minitest::Test
     deck = Deck.new(cards)
 
     deck.rank_of_card_at(0)
-    
-    assert_equal 12, deck.cards.rank_of_card_at(0)
+
+    assert_equal 12, deck.rank_of_card_at(0)
   end
 
   def test_it_can_find_all_the_high_ranking_cards
+    # skip
     cards = []
     card1 = Card.new(:diamond, 'Queen', 12)
     cards << card1
@@ -83,57 +85,67 @@ class DeckTest < Minitest::Test
     cards << card3
     card4 = Card.new(:diamond, '9', 9)
     deck = Deck.new(cards)
+
+    deck.high_ranking_cards
+
+    assert_equal [card1, card3], deck.high_ranking_cards
   end
 
   def test_it_can_find_the_percentage_of_high_ranking_cards_in_the_deck
+    # skip
+    cards = []
+    card1 = Card.new(:diamond, '10', 10)
+    cards << card1
+    card2 = Card.new(:spade, '3', 3)
+    cards << card2
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards << card3
+    card4 = Card.new(:club, '7', 7)
+    cards << card4
+    deck = Deck.new(cards)
+
+    deck.percent_high_ranking
+
+    assert_equal 25.0, deck.percent_high_ranking
   end
 
   def test_it_can_remove_a_card_from_the_deck
+    # skip
+    cards = []
+    card1 = Card.new(:diamond, '10', 10)
+    cards << card1
+    card2 = Card.new(:spade, '3', 3)
+    cards << card2
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards << card3
+    card4 = Card.new(:club, '7', 7)
+    cards << card4
+    deck = Deck.new(cards)
+
+    deck.remove_card #first element of the array
+
+    assert_equal [card2, card3, card4], deck.cards
   end
 
   def test_it_can_add_a_card_to_the_deck
+    # skip
+    cards = []
+    card1 = Card.new(:diamond, '10', 10)
+    cards << card1
+    card2 = Card.new(:spade, '3', 3)
+    cards << card2
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards << card3
+    card4 = Card.new(:club, '7', 7)
+    cards << card4
+    deck = Deck.new(cards)
+
+    deck.percent_high_ranking
+    assert_equal 25.0, deck.percent_high_ranking
+    card5 = Card.new(:club, 'King', 13)
+
+    deck.add_card(card5)
+
+    assert_equal [card1, card2, card3, card4, card5], deck.cards
   end
-
-
 end
-
-# pry(main)> deck.rank_of_card_at(0)
-# #=> 12
-#
-# pry(main)> deck.rank_of_card_at(2)
-# #=> 14
-#
-# pry(main)> deck.cards
-# #=> [#<Card:0x007fbfd18490e8...>, #<Card:0x007fbfd19f4fa0...>, #<Card:0x007fbfd18555a0...>]
-#
-# pry(main)> deck.high_ranking_cards
-# #=> [#<Card:0x007fbfd18490e8...>, #<Card:0x007fbfd18555a0...>]
-#
-# pry(main)> deck.percent_high_ranking
-# #=> 66.67
-#
-# pry(main)> deck.remove_card
-# #=> #<Card:0x007fbfd18490e8 @rank=12, @suit=:diamond, @value="Queen">
-#
-# pry(main)> deck.cards
-# #=> [#<Card:0x007fbfd19f4fa0...>, #<Card:0x007fbfd18555a0...>]
-#
-# pry(main)> deck.high_ranking_cards
-# #=> [#<Card:0x007fbfd18555a0...>]
-#
-# pry(main)> deck.percent_high_ranking
-# #=> 50.0
-#
-# pry(main)> card4 = Card.new(:club, '5', 5)
-# #=> #<Card:0x007fbfd2963978 @rank=5, @suit=:club, @value="5">
-#
-# pry(main)> deck.add_card(card4)
-#
-# pry(main)> deck.cards
-# #=> [#<Card:0x007fbfd19f4fa0...>, #<Card:0x007fbfd18555a0...>, #<Card:0x007fbfd2963978...>]
-#
-# pry(main)> deck.high_ranking_cards
-# #=> [#<Card:0x007fbfd18555a0...>]
-#
-# pry(main)> deck.percent_high_ranking
-# #=> 33.33
