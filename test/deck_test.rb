ff4915b1c7d3f412ed2e620cd6c0ba3644402493
Eduardo Parra <helloeduardo@icloud.com>
2020-08-18 20:13:39 -0600

    Add test_it_exists

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,8 +1,22 @@
 require './lib/deck'
+require './lib/card'
 require 'minitest/autorun'
 require 'minitest/pride'
 
 class DeckTest < Minitest::Test
-  
+  def setup
+
+  end
+
+  def test_it_exists
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_instance_of Deck, deck
+  end
+
 
 end
