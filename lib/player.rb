6083a901d86833186dff9a0df218ef35d716c686
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-08-19 16:27:17 -0600

    Create and initialize player class and test

---
+++ lib/player.rb
@@ -0,0 +1,7 @@
+class Player
+  attr_reader :name
+  def initialize(name, deck)
+    @name = name
+  end
+  
+end
---
+++ test/player_test.rb
@@ -0,0 +1,30 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+
+class PlayerTest < MiniTest::Test
+
+  def test_it_exists
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+
+    assert_instance_of Player, player
+  end
+
+  def test_player_has_a_name
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+
+    assert_equal 'Clarisa', player.name
+  end
+
+
+end
