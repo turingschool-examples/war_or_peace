99b80a98e5c899661d5da6bffd25896b9943e55b
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-08-18 19:36:27 -0600

    Add high_ranking_cards method

--- lib/deck.rb
+++ lib/deck.rb
@@ -8,13 +8,25 @@ class Deck
   end
 
   def rank_of_card_at(index)
-    cards.take(index).rank
+    @cards.at(index).rank
   end
 
   def high_ranking_cards
+    #iterate over the deck to find rank above 11
+    #move cards rank above 11 into a new array
+    high_cards = []
+
+    cards.each do |card|
+      if card.rank >= 11
+        high_cards << card
+      end
+    end
+    p high_cards
   end
 
   def percent_high_ranking
+    percent = high_ranking_cards.count / cards
+    puts percent
   end
 
   def remove_card
