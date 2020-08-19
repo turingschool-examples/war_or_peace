419e6d906f2b45d8a1deef95b74df4fb61888b40
Aidan Murray <aidanhansm@gmail.com>
2020-08-18 20:10:55 -0600

    Add high ranking and add/remove tests to deck test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -22,4 +22,37 @@ class DeckTest < Minitest::Test
     assert_equal(cards, deck.cards)
     assert_equal(card, deck.cards[0])
   end
+
+  def test_high_ranking_methods
+    card1 = Card.new(:spade, 'Ten', 10)
+    card2 = Card.new(:heart, 'Three', 3)
+    card3 = Card.new(:club, 'Jack', 11)
+    cards = []
+    cards << card1
+    cards << card2
+    cards << card3
+    deck = Deck.new(cards)
+
+
+    assert_equal(card3, deck.high_ranking_cards[0])
+    assert_equal(1.0/3.0, deck.percent_high_ranking)
+  end
+
+  def test_add_remove
+    deck = Deck.new([])
+    card1 = Card.new(:spade, 'Ten', 10)
+    card2 = Card.new(:heart, 'Three', 3)
+
+    deck.add_card(card1)
+
+    assert_equal(card1, deck.cards[0])
+
+    deck.add_card(card2)
+
+    assert_equal(card1, deck.cards[0])
+
+    deck.remove_card
+
+    assert_equal(card2, deck.cards[0])
+  end
 end
