24c85ec7e4d0e906094fdd96f3317c175177d83c
Eduardo Parra <helloeduardo@icloud.com>
2020-08-18 20:33:19 -0600

    Add tests that cards exist and have ranks

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -18,5 +18,29 @@ class DeckTest < Minitest::Test
     assert_instance_of Deck, deck
   end
 
+  def test_it_has_cards
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_instance_of Card, deck.cards[0]
+    assert_instance_of Card, deck.cards[1]
+    assert_instance_of Card, deck.cards[2]
+  end
+
+  def test_its_cards_have_a_rank
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal 12, deck.rank_of_card_at(0)
+    assert_equal 3, deck.rank_of_card_at(1)
+    assert_equal 14, deck.rank_of_card_at(2)
+  end
+
 
 end
