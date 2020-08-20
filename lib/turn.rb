2215768e27a82481dc673d15d28f78f7221057b7
Eduardo Parra <helloeduardo@icloud.com>
2020-08-19 18:37:39 -0600

    Add winner method and tests

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,4 +7,32 @@ class Turn
     @spoils_of_war = []
   end
 
+  def type
+    if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+      :mutually_assured_destruction
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+      :war
+    else
+      :basic
+    end
+  end
+
+  def winner
+    if type == :basic
+      if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+        player1
+      else
+        player2
+      end
+    elsif type == :war
+      if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+        player1
+      else
+        player2
+      end
+    else
+      "No Winner"
+    end
+  end
+
 end
