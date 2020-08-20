f2e925d3b122f7a666cdb3a799ca1f12a4760c88
Michael Foy <foymikek@gmail.com>
2020-08-20 06:40:08 -0600

    Add passing test test_it_exists

--- test/card_test.rb
+++ test/card_test.rb
@@ -4,16 +4,15 @@ require './lib/card'
 
 class CardTest < Minitest::Test
   def test_it_exists
-    card = Card.new(:diamond, 'Queen', 12)
+    card = Card.new(:diamond, 'Queen', 11)
     assert_instance_of Card, card
   end
 
   def test_it_has_readable_attributes
-    card = Card.new(:diamond, 'Queen', 12)
+    card = Card.new(:diamond, 'Queen', 11)
 
     assert_equal :diamond, card.suit
     assert_equal 'Queen', card.value
-    assert_equal 12, card.rank
-    require "pry"; binding.pry
+    assert_equal 11, card.rank
   end
 end
