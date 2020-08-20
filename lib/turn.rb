54f914d472861d4f8565624e5cef07570d433496
Roberto Basulto <roberto.basulto085@gmail.com>
2020-08-20 15:09:03 -0400

    Made the proccessing for the war vertion in pile cards to its own method

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,22 +19,26 @@ class Turn
 
   def pile_cards
     if type == :basic
-      spoils_of_war_adding_basic_cards_and_delete_from_deck
+      spoils_of_war_adding_basic_type_cards_and_delete_from_deck
     elsif type == :war
-      @spoils_of_war << @player1.deck.cards[0..2]
-      @spoils_of_war << @player2.deck.cards[0..2]
-      @spoils_of_war.flatten!
+      spoils_of_war_adding_war_type_cards_and_delete_from_deck
       remove_3_cards_from_players
     elsif type == :mutually_assured_destruction
       remove_3_cards_from_players
     end
   end
 
-  def spoils_of_war_adding_basic_cards_and_delete_from_deck
+  def spoils_of_war_adding_basic_type_cards_and_delete_from_deck
     @spoils_of_war << @player1.deck.cards[0]
     @spoils_of_war << @player2.deck.cards[0]
     player1.deck.remove_card
     player2.deck.remove_card
+  end
+
+  def spoils_of_war_adding_war_type_cards_and_delete_from_deck
+    @spoils_of_war << @player1.deck.cards[0..2]
+    @spoils_of_war << @player2.deck.cards[0..2]
+    @spoils_of_war.flatten!
   end
 
   def remove_3_cards_from_players
