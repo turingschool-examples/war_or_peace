64062574027eb321bb8d5b7ec17554f4e1be3434
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-08-20 17:52:13 -0600

    Add turn has a winner test

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -90,7 +90,7 @@ class TurnTest < MiniTest::Test
     player4 = Player.new("Paul", deck4)
     turn2 = Turn.new(player3, player4)
     deck5 = Deck.new([card9, card10, card4, card12])
-    deck6 = Deck.new([card13, card14, card12, card16])
+    deck6 = Deck.new([card13, card14, card12, card4])
     player5 = Player.new("Sally", deck5)
     player6 = Player.new("Paul", deck6)
     turn3 = Turn.new(player5, player6)
@@ -114,8 +114,28 @@ class TurnTest < MiniTest::Test
     player1 = Player.new("Megan", deck1)
     player2 = Player.new("Aurora", deck2)
     turn = Turn.new(player1, player2)
+    card9 = Card.new(:spades, '8', 8)
+    card10 = Card.new(:spades, '7', 7)
+    card11 = Card.new(:heart, 'Queen', 12)
+    card12 = Card.new(:diamond, 'Jack', 11)
+    card13 = Card.new(:spades, '8', 8)
+    card14 = Card.new(:diamond, 'Queen', 12)
+    card15 = Card.new(:spades, 'Ace', 13)
+    card16 = Card.new(:diamond, '5', 5)
+    deck3 = Deck.new([card9, card10, card11, card12])
+    deck4 = Deck.new([card13, card14, card15, card16])
+    player3 = Player.new("Sally", deck3)
+    player4 = Player.new("Paul", deck4)
+    turn2 = Turn.new(player3, player4)
+    deck5 = Deck.new([card9, card10, card4, card12])
+    deck6 = Deck.new([card13, card14, card12, card4])
+    player5 = Player.new("Sally", deck5)
+    player6 = Player.new("Paul", deck6)
+    turn3 = Turn.new(player5, player6)
 
-    assert_equal player2, turn.winner
+    assert_equal player1, turn.winner
+    assert_equal player4, turn2.winner
+    assert_equal "No Winner", turn3.winner
   end
 
 end
