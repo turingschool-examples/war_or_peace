e1ba2ecddfd7acc4d44a68408fbe0ff55b28ad7e
Cydnee Owens <cks410@yahoo.com>
2020-08-19 20:55:03 -0600

    Add iteration 2 player

--- lib/player.rb
+++ lib/player.rb
@@ -1,17 +1,19 @@
 class Player
-  attr_reader :name, :deck
+  attr_accessor :name, :deck
 
   def initialize(name, deck)
     @name = name
     @deck = deck
   end
 
-  # def player_has_lost?
-  #   has_lost == false
-  #   @cards.shift
-  #   p "Keep Going!"
-  # end
-  #
+  # def can_player_lose?
+
+  def player_has_lost?
+    if player_has_lost == false
+    cards.shift
+    end
+  end
+
   # def remove_card_from_deck
   #   if has_lost == false
   #     @cards.shift
