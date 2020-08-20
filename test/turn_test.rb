ed7118023ca0bf3304b99b13a4f9a1bed43f474c
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-08-19 20:47:51 -0600

    Add test for player names

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -2,7 +2,7 @@ require './lib/card'
 require './lib/deck'
 require './lib/player'
 require './lib/turn'
-require 'minitest/test'
+require 'minitest/autorun'
 require 'minitest/pride'
 
 class TurnTest < MiniTest::Test
@@ -26,4 +26,23 @@ class TurnTest < MiniTest::Test
     assert_instance_of Turn, turn
   end
 
+  def test_players_have_names
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+
+    assert_equal "Megan", player1.name
+    assert_equal "Aurora", player2.name
+  end
 end
