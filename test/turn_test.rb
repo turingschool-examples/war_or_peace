f67b4936c8dc22f910867b2b93aaae02d6d0ec9c
Roberto Basulto <roberto.basulto085@gmail.com>
2020-08-20 15:39:33 -0400

    Removed redundant card objects i  missed

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -61,7 +61,7 @@ class TurnTest < Minitest::Test
     assert_equal :war, turn.type
   end
 
-  def test_return_the_type_for_MAD
+  def test_return_the_type_for_mutually_assured_destruction
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
     card3 = Card.new(:heart, '9', 9)
@@ -101,17 +101,9 @@ class TurnTest < Minitest::Test
   end
 
   def test_winner_for_mutually_assured_destruction
-    card1 = Card.new(:heart, 'Jack', 11)
-    card2 = Card.new(:heart, '10', 10)
-    card3 = Card.new(:heart, '9', 9)
-    card4 = Card.new(:diamond, 'Jack', 11)
-    card5 = Card.new(:heart, '8', 8)
     card6 = Card.new(:diamond, '8', 8)
-    card7 = Card.new(:heart, '3', 3)
-    card8 = Card.new(:diamond, '2', 2)
-
-    deck1 = Deck.new([card1, card2, card5, card8])
-    deck2 = Deck.new([card4, card3, card6, card7])
+    deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    deck2 = Deck.new([@card4, @card3, card6, @card7])
     player1 = Player.new("Megan", deck1)
     player2 = Player.new("Aurora", deck2)
     turn = Turn.new(player1, player2)
@@ -159,7 +151,7 @@ class TurnTest < Minitest::Test
     assert_equal player2_expected, turn.player2.deck.cards
   end
 
-  def test_pilecards_and_spoils_of_war_for_MAD
+  def test_pilecards_and_spoils_of_war_for_mutaully_assured_destruction
     card6 = Card.new(:diamond, '8', 8)
     deck1 = Deck.new([@card1, @card2, @card5, @card8])
     deck2 = Deck.new([@card4, @card3, card6, @card7])
