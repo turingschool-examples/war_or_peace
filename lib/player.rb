32890baca6421db6cc547ea3cf8c6b53c92f892d
todd-estes <elestes@gmail.com>
2020-08-19 18:31:43 -0600

    Create player class file

---
+++ lib/player.rb
@@ -0,0 +1,18 @@
+class Player
+  attr_reader :name, :deck
+    def initialize(name, deck = [])
+      @name = name
+      @deck = deck
+    end
+
+    def has_lost?
+      if self.deck.cards == []
+        true
+      else
+        false
+      end
+    end
+
+
+
+end
