021985ce66cfe9943c110ca01f250a779a2e1b06
Cydnee Owens <cks410@yahoo.com>
2020-08-19 20:55:36 -0600

    Add iteration 2 player

--- test/player_test.rb
+++ test/player_test.rb
@@ -25,33 +25,21 @@ class PlayerTest < Minitest::Test
     player = Player.new('Clarisa', deck)
 
     assert_equal "Clarisa", player.name
-    require "pry"; binding.pry
+
   end
 
-  # def player_has_a_deck
-  #   card1 = Card.new(:diamond, 'Queen', 12)
-  #   card2 = Card.new(:spade, '3', 3)
-  #   card3 = Card.new(:heart, 'Ace', 14)
-  #   cards = [card1, card2, card3]
-  #   deck = Deck.new(cards)
-  #   player = Player.new('Clarisa', deck)
-  #
-  #   assert_equal [card1, card2, card3], player.deck
-  #   require "pry"; binding.pry
-  # end
+  def player_has_lost?
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    player = Player.new('Clarisa', deck)
 
-  # def player_has_lost?
-  #   card1 = Card.new(:diamond, 'Queen', 12)
-  #   card2 = Card.new(:spade, '3', 3)
-  #   card3 = Card.new(:heart, 'Ace', 14)
-  #   cards = [card1, card2, card3]
-  #   deck = Deck.new(cards)
-  #   player = Player.new('Clarisa', deck)
-  #
-  #   assert_equal false, player.has_lost?
-  #
-  #   require "pry"; binding.pry
-  # end
+    assert_equal false, player.has_lost?
+
+    require "pry"; binding.pry
+  end
 
   # def remove_card_from_deck
   #   card1 = Card.new(:diamond, 'Queen', 12)
