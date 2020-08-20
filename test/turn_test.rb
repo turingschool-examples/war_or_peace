583507e7961efaad17fd3626c4c7c8809197b606
Jake Heft <jakeheft@gmail.com>
2020-08-19 18:11:16 -0600

    Create turn_test

---
+++ test/turn_test.rb
@@ -0,0 +1,13 @@
+require "minitest/autorun"
+require "minitest/pride"
+require './lib/card'
+require './lib/deck'
+require "./lib/player"
+require "./lib/turn"
+
+
+class TurnTest < Minitest::Test
+  def test_
+    
+  end
+end
