d7c3c525dd7fe3cd75076a682a8c0b8b49a6a98f
Jake Heft <jakeheft@gmail.com>
2020-08-19 22:12:16 -0600

    Add player 1&2 variables. Define type, winner, and pile_cards methods. Award_spoils method defined, but empty

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,56 @@
 class Turn
+  attr_reader :player1, :player2, :spoils_of_war
 
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @spoils_of_war = []
+  end
+
+  def type
+    card_rank0 = [@player1, @player2].map do |player|
+      # require "pry"; binding.pry
+      player.deck.rank_of_card_at(0)
+    end
+    if card_rank0[0] != card_rank0[1]
+      :basic
+    elsif card_rank0[0] == card_rank0[1]
+      card_rank2 = [@player1, @player2].map do |player|
+        player.deck.rank_of_card_at(2)
+      end
+      if card_rank2[0] != card_rank2[1]
+        :war
+      else
+        :mutually_assured_destruction
+      end
+    end
+  end
+
+  def winner
+    if self.type == :basic
+      card_rank0 = [@player1, @player2].sort_by do |player|
+        player.deck.rank_of_card_at(0)
+      end
+      card_rank0[-1]
+    elsif self.type == :war
+      card_rank2 = [@player1, @player2].sort_by do |player|
+        player.deck.rank_of_card_at(2)
+      end
+      card_rank2[-1]
+    elsif self.type == :mutually_assured_destruction
+      "No Winner"
+    else
+      "WINNER ERROR"
+    end
+  end
+
+  def pile_cards
+    @spoils_of_war = [@player1, @player2].map do |player|
+      player.deck.remove_card
+    end
+  end
+
+  def award_spoils(winner)
+    
+  end
 end
