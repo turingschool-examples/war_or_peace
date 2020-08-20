9853d1ecd8618245443357e996af52dd076eeb7d
Cydnee Owens <cks410@yahoo.com>
2020-08-19 18:51:08 -0600

    add iteration 2 player

--- test/player_test.rb
+++ test/player_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/deck'
 require './lib/card'
+require './lib/deck'
 require './lib/player'
 
 class PlayerTest < Minitest::Test
@@ -16,7 +16,7 @@ class PlayerTest < Minitest::Test
     assert_instance_of Player, player
   end
 
-  def test_has_a_name?
+  def test_player_has_a_name?
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
@@ -24,45 +24,50 @@ class PlayerTest < Minitest::Test
     deck = Deck.new(cards)
     player = Player.new('Clarisa', deck)
 
-    assert_equal Clarissa, player.name
+    assert_equal "Clarisa", player.name
+    require "pry"; binding.pry
   end
 
-  def player_has_a_deck?
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-    player = Player.new('Clarisa', deck)
+  # def player_has_a_deck
+  #   card1 = Card.new(:diamond, 'Queen', 12)
+  #   card2 = Card.new(:spade, '3', 3)
+  #   card3 = Card.new(:heart, 'Ace', 14)
+  #   cards = [card1, card2, card3]
+  #   deck = Deck.new(cards)
+  #   player = Player.new('Clarisa', deck)
+  #
+  #   assert_equal [card1, card2, card3], player.deck
+  #   require "pry"; binding.pry
+  # end
 
-    assert_equal [card1, card2, card3], player.deck
-  end
+  # def player_has_lost?
+  #   card1 = Card.new(:diamond, 'Queen', 12)
+  #   card2 = Card.new(:spade, '3', 3)
+  #   card3 = Card.new(:heart, 'Ace', 14)
+  #   cards = [card1, card2, card3]
+  #   deck = Deck.new(cards)
+  #   player = Player.new('Clarisa', deck)
+  #
+  #   assert_equal false, player.has_lost?
+  #
+  #   require "pry"; binding.pry
+  # end
 
-  def player_has_lost?
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-    player = Player.new('Clarisa', deck)
+  # def remove_card_from_deck
+  #   card1 = Card.new(:diamond, 'Queen', 12)
+  #   card2 = Card.new(:spade, '3', 3)
+  #   card3 = Card.new(:heart, 'Ace', 14)
+  #   cards = [card1, card2, card3]
+  #   deck = Deck.new(cards)
+  #   player = Player.new('Clarisa', deck)
+  #
+  #   assert_equal [Card2, Card3], player.deck.remove_card
+  #   assert_equal false, player.has_lost?
+  #   assert_equal [Card3], player.deck.remove_card
+  #   assert_equal false, player.has_lost?
+  #   assert_equal [], player.deck.remove_card
+  #   assert_equal true, player.has_lost?
+  #   assert_equal [], player.deck
+  # end
 
-    assert_equal false, player.has_lost?
-  end
-
-  def remove_card_from_deck
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-    player = Player.new('Clarisa', deck)
-
-    assert_equal [Card2, Card3], player.deck.remove_card
-    assert_equal false, player.has_lost?
-    assert_equal [Card3], player.deck.remove_card
-    assert_equal false, player.has_lost?
-    assert_equal [], player.deck.remove_card
-    assert_equal true, player.has_lost?
-    assert_equal [], player.deck
-  end
 end
