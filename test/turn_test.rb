548f4c9b1187af4bcee6a805500b084dd9e7c7a7
Cydnee Owens <cks410@yahoo.com>
2020-08-19 20:56:55 -0600

    Add iteration 2 turn

---
+++ test/turn_test.rb
@@ -0,0 +1,18 @@
+equire 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+
+class TurnTest < Minitest::Test
+  def test_it_exists
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    player = Player.new('Clarisa', deck)
+
+    assert_instance_of Player, player
+  end
