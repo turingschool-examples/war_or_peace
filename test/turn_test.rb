a55147f505b8f62161f93b5c8d98db3ffc43d720
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-08-20 20:22:51 -0600

    Add basic test setup for m.a.d. test

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -169,7 +169,23 @@ class TurnTest < MiniTest::Test
 
     #Test that spoils go to winner
     assert_equal [card8], player1.deck.cards
-    assert_equal [card7, card1, card4, card2, card3, card5, card6], player2.deck.cards 
+    assert_equal [card7, card1, card4, card2, card3, card5, card6], player2.deck.cards
   end
 
+  def test_to_pile_cards_and_get_spoils_mad
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, '8', 8)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+  end 
 end
