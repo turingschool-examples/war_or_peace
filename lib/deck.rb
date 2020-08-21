40ce5b44f71b55863aa8c1fe2065e08d706071c6
Brian Liu <brian.b.liu@gmail.com>
2020-08-20 20:49:55 -0600

    Create test and method for remove card

--- lib/deck.rb
+++ lib/deck.rb
@@ -11,11 +11,30 @@ class Deck
   def high_ranking_cards
     high_ranking_cards = []
     @cards.each do |card|
-      if card.rank > 11
+      if card.rank > 10
         high_ranking_cards << card
       end
     end
     high_ranking_cards
   end
+
+  def percent_high_ranking
+    # (high_ranking_cards / cards) * 100
+    percent = ((high_ranking_cards.size.to_f / cards.size.to_f) * 100).round(2)
+  end
+
+  def remove_card
+    cards.shift # WHY ISN'T THIS SHOWING THE REMOVED CARD IN PRY?????
+  end
+
+  def add_card
+  end
+
+
+  #def high_ranking_cards
+    #@cards.find_all do |card|
+      #card.rank > 11
+    #end
+  #end
 
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -12,7 +12,7 @@ class DeckTest < Minitest::Test
     card3 = Card.new(:heart, 'Ace', 14)
     cards = [card1, card2, card3]
 
-    deck =Deck.new(cards)
+    deck = Deck.new(cards)
 
     assert_instance_of Deck, deck
     assert_equal cards, deck.cards
@@ -32,9 +32,9 @@ class DeckTest < Minitest::Test
   end
 
   def test_high_ranking_cards
-    card1 = Card.new(:hearts, 'Queen', 12)
+    card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:spade, 'Ace', 14)
+    card3 = Card.new(:heart, 'Ace', 14)
     cards = [card1, card2, card3]
 
     deck = Deck.new(cards)
@@ -42,5 +42,27 @@ class DeckTest < Minitest::Test
     assert_equal [card1, card3], deck.high_ranking_cards
   end
 
+  def test_percent_high_ranking
+    card1 = Card.new(:hearts, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:spade, 'Ace', 14)
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    assert_equal 66.67, deck.percent_high_ranking
+  end
+
+  def test_remove_card
+    card1 = Card.new(:hearts, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:spade, 'Ace', 14)
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    assert_equal card1, deck.remove_card
+    assert_equal [card2, card3], deck.cards
+  end
 
 end
