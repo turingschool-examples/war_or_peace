fe1b3a2670b0bda4b40beabe016c3ae0deb9dbbc
todd-estes <elestes@gmail.com>
2020-08-21 08:06:59 -0600

    Update turn.rb

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,10 +4,12 @@ class Turn
       @player1 = player1
       @player2 = player2
       @spoils_of_war = []
+      @turn_winner
     end
 
     def type
-      if(self.player1.deck.rank_of_card_at(0) == self.player2.deck.rank_of_card_at(0)) && (self.player1.deck.rank_of_card_at(2) == self.player2.deck.rank_of_card_at(2))
+      if(self.player1.deck.rank_of_card_at(0) == self.player2.deck.rank_of_card_at(0)) &&
+        (self.player1.deck.rank_of_card_at(2) == self.player2.deck.rank_of_card_at(2))
         :mutually_assured_destruction
       elsif self.player1.deck.rank_of_card_at(0) == self.player2.deck.rank_of_card_at(0)
         :war
@@ -15,14 +17,15 @@ class Turn
         :basic
       end
     end
-
+# this has to be ran through each time to reset @turn_winner, otherwise award_spoils
+#will keep shoveling spoils_of_war into original @turn_winner
     def winner
-      if self.type == :basic && self.player1
-        #don't know what to return yet
-        return
-      elsif self.type == :war &&
-        #don't know what to return yet
-        return
+      if self.type == :basic && self.player1.deck.rank_of_card_at(0) > self.player2.deck.rank_of_card_at(0) ||
+        self.type == :war && self.player1.deck.rank_of_card_at(2) > self.player2.deck.rank_of_card_at(2)
+        @turn_winner = player1
+      elsif self.type == :basic && self.player2.deck.rank_of_card_at(0) > self.player1.deck.rank_of_card_at(0) ||
+      self.type == :war && self.player2.deck.rank_of_card_at(2) > self.player1.deck.rank_of_card_at(2)
+        @turn_winner = player2
       elsif self.type == :mutually_assured_destruction
         "No Winner"
       end
@@ -41,8 +44,13 @@ class Turn
       end
     end
 
-    def award_spoils(winner)
-      winner.deck << @spoils_of_war
+    def award_spoils
+      @turn_winner.deck.cards << @spoils_of_war.shift(3)
+      flatten_card_array
+    end
+
+    def flatten_card_array
+      @turn_winner.deck.cards.flatten!
     end
 
 end
