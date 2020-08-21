88f19ea2a198f42cab9a7d307f9e4add1115e7c7
Sean Steel <seanrsteel@gmail.com>
2020-08-21 08:18:31 -0600

    Add pile cards method to class and test

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,35 +3,65 @@ class Turn
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
+    @spoils_of_war = []
   end
 
   def type
-    if @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
-      return :mutually_assured_destruction
-    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
-      return :war
+    if mutually_assured_destruction?
+      :mutually_assured_destruction
+    elsif war?
+      :war
     else
-      return :basic
+      :basic
     end
   end
 
   def winner
-    if self.type == :basic
+    if type == :basic
       if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
         return @player1
       else
         @player2
       end
-    elsif self.type == :war
+    elsif type == :war
       if @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
         return @player1
       else
         @player2
       end
-    else self.type == :mutually_assured_destruction
+    else type == :mutually_assured_destruction
       "No Winner"
     end
   end
 
+  def pile_cards
+    if type == :basic
+      @spoils_of_war.push(@player1.deck.remove_card, @player2.deck.remove_card)
+    elsif type == :war
+      3.times do
+        @spoils_of_war << @player1.deck.remove_card
+        @spoils_of_war << @player2.deck.remove_card
+      end
+      @spoils_of_war
+    else type == :mutually_assured_destruction
+      3.times {@player1.deck.remove_card}
+      3.times {@player2.deck.remove_card}
+      @spoils_of_war
+    end
+  end
+
+#  def award_spoils
+
+#  end
+
+  private
+
+  def mutually_assured_destruction?
+    @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
+  end
+
+  def war?
+    @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
+  end
 
 end
