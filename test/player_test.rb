03b524e25c041d21a48a55ae23149f2f0177e8c3
Brian Liu <brian.b.liu@gmail.com>
2020-08-20 22:12:52 -0600

    Create bank of variables for card and deck.

--- test/player_test.rb
+++ test/player_test.rb
@@ -7,13 +7,18 @@ require './lib/player'
 
 class PlayerTest < Minitest::Test
 
-  def test_it_exists_and_has_attributes
+  def cards_deck
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
     cards = [card1, card2, card3]
     deck = Card.new(cards)
-    player = Player.new('Clarisa', deck)
+  end
+
+
+  def test_it_exists
+
+    player = Player.new('Clarisa', @deck)
 
     assert_instance_of Player, player
   end
