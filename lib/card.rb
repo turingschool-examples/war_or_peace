e1506d5a7c87271c7257032e50ec947636c983c5
Garrett Cottrell <garrett.cottrell2116@gmail.com>
2020-08-20 22:16:16 -0600

    Tested/Created Card class

---
+++ lib/card.rb
@@ -0,0 +1,19 @@
+class Card
+  def initialize(suit, value, rank)
+    @suit = suit
+    @rank = rank
+    @value = value
+  end
+
+  def suit
+    @suit
+  end
+
+  def value
+    @value
+  end
+
+  def rank
+    @rank
+  end
+end
