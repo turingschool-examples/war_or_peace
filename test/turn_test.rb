578ae8bb1d0a077106082918fe17a0ca55b86d88
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-08-20 19:54:15 -0600

    Add test for war type

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -135,9 +135,30 @@ class TurnTest < MiniTest::Test
     assert_equal [card4, card6, card7], player2.deck.cards
   end
 
+  def test_to_pile_cards_and_get_spoils_war
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
 
 
+    #Test to check type is war
+    assert_equal :war, turn.type
 
+    winner = turn.winner
 
+    #Test to check player2 is winner
+    assert_equal player2, turn.winner 
+  end
 
 end
