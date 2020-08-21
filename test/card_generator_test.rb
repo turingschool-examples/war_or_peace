47f5a149bb597170c259418c7171e22f96413fae
Kevin Cuadros <kev040798@gmail.com>
2020-08-20 20:30:50 -0400

    Created first test

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -5,7 +5,7 @@ require '../lib/card_generator'
 class CardGeneratorTest < Minitest::Test
   def test_it_exists
     filename = "cards.txt"
-    cards = CardGenerator.new(filename).cards
+    cards = CardGenerator.new(filename)
 
     assert_instance_of CardGenerator, cards
   end
