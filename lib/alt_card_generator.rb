b8842b83fecc111be8da822b1dec8d1bc5354424
Eugene Theriault <hybridbassist@gmail.com>
2020-08-20 18:54:11 -0700

    Change naming for clarity

--- lib/alt_card_generator.rb
+++ lib/alt_card_generator.rb
@@ -1,17 +1,17 @@
 class AltCardGenerator
-  attr_reader :cards, :ranks, :symbols
+  attr_reader :cards, :ranks, :suits
 
   def initialize
     @cards = []
     @ranks = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8,
               "9": 9, "10": 10, "Jack": 11, "Queen": 12, "King": 13, "Ace": 14}
-    @symbols = [:diamond, :club, :heart, :spade]
+    @suits = [:diamond, :club, :heart, :spade]
   end
 
   def generate_cards
-    @symbols.each do |symbol|
+    @suits.each do |suit|
       @ranks.each do |rank, value|
-        cards << Card.new(symbol, rank, value)
+        cards << Card.new(suit, rank, value)
       end
     end
   end
