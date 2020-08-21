5b908c33cc3c8604d49276736fb268b04859ed0d
Sean Steel <seanrsteel@gmail.com>
2020-08-21 15:03:23 -0600

    Create runner file and start class

---
+++ test/start_test.rb
@@ -0,0 +1,10 @@
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+require './lib/start'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class TurnTest < MiniTest::Test
+  
