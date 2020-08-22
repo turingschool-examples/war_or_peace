555b8a0a9412782ec9d118d998514aa357cedb3c
todd-estes <elestes@gmail.com>
2020-08-22 17:55:50 -0600

    Update war_or_peace_runner.rb

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -5,14 +5,14 @@ require  './lib/turn'
 require  './lib/game'
 
 
-turn_counter = 0
+turn_counter = 1
 #lay out card suits, values, and ranks
 suit = [:hearts, :spades, :diamonds, :clubs]
 value = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
 "Ten", "Jack", "Queen", "King", "Ace"]
 rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 #build deck *no idea how to build the deck*
-group = suit.flat_map {|suit| value.flat_map {|value| rank.flat_map {Card.new(suit, value, rank)}}
+cards1 = suit.flat_map {|suit| value.flat_map {|value| rank.flat_map {Card.new(suit, value, rank)}}
 #cards1 = deck build
 cards1.shuffle!
 #split the deck and create decks
@@ -31,34 +31,5 @@ game.start
 
 reply = gets.chomp.upcase
 if reply = "GO"
-  puts
-
-
-#game mechanics
-
-turn.type
-
-turn.winner
-
-turn.pile_cards
-
-turn.award_spoils(winner)
-
-p ""
-
-
-
-#start code
-def start
-
-
-
-
-turn_counter = 1
-
-  until player1.has_lost?  || player2.has_lost?
-   if
-
-   elseif turn_counter == 1000000
-   p "---- DRAW ----"
-  end
+  game.play
+end
