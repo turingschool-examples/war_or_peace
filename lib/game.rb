cda7a088bb343888b24abc637d88b4a733ee9505
todd-estes <elestes@gmail.com>
2020-08-22 17:52:54 -0600

    Update game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -3,22 +3,33 @@ class Game
   end
 
 
-
   def start
     p 'Welcome to War! (or Peace) This game will be played with 52 cards.'
     p 'The players today are Megan and Aurora.'
     p 'Type 'GO' to start the game!'
   end
 
- until player1.has_lost? == true || player2.has_lost? == true or
-   turn_counter == 1000000
-   if
-  p "Turn #{turn_counter}: #{turn.winner} "
-
-   #turn methods
-   turn_counter += 1
-   Turn 1: Megan won 2 cards
-   Turn 2: WAR - Aurora won 6 cards
-   Turn 3: *mutually assured destruction* 6 cards removed from play
-   if
-  p "Turn #{turn_counter}: #{turn.winner} "
+  def play
+    until player1.has_lost? == true || player2.has_lost? == true
+      if turn_counter == 1000000
+        p "---- DRAW ----"
+          break
+      if turn.type == :basic
+        winner = turn.winner
+        p "Turn #{turn_counter}: #{winner} has one 2 cards"
+        turn.pile_cards
+        turn.award_spoils
+        turn_counter += 1
+      elsif turn.type == :war
+        winner = turn.winner
+        p "Turn #{turn_counter}: WAR - #{winner} has won 6 cards"
+        turn.pile_cards
+        turn.award_spoils
+        turn_counter += 1
+      elsif self.type == :mutually_assured_destruction
+        p "*mutually assured destruction* 6 cards removed from play"
+        turn_counter += 1
+      elsif turn_
+      end
+  end
+end
