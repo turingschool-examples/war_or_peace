50dd8ed684a4da3c6d3a6a02057aa28aa294db78
Brian Liu <brian.b.liu@gmail.com>
2020-08-22 02:41:13 -0600

    Create standard deck and runner.

---
+++ war_or_peace.rb
@@ -0,0 +1,4 @@
+puts "Welcome to War! (or Peace) This game will be played with 52 cards."
+puts "The players today are Megan and Aurora."
+puts "Type 'GO' to start the game!"
+puts ------------------------------------------------------------------
