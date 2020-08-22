63ab20379b03bc7b76e379c6d8343b848f723b33
Eugene Theriault <hybridbassist@gmail.com>
2020-08-22 11:03:44 -0700

    Remove superfluous attributes and instance variables

--- lib/alt_card_generator.rb
+++ lib/alt_card_generator.rb
@@ -1,17 +1,17 @@
 class AltCardGenerator
-  attr_reader :cards, :ranks, :suits
+  attr_reader :cards
 
   def initialize
     @cards = []
-    @ranks = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8,
-              "9": 9, "10": 10, "Jack": 11, "Queen": 12, "King": 13, "Ace": 14}
-    @suits = [:diamond, :club, :heart, :spade]
     generate_cards
   end
 
   def generate_cards
-    @suits.each do |suit|
-      @ranks.each do |rank, value|
+    ranks = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8,
+              "9": 9, "10": 10, "Jack": 11, "Queen": 12, "King": 13, "Ace": 14}
+    suits = [:diamond, :club, :heart, :spade]
+    suits.each do |suit|
+      ranks.each do |rank, value|
         @cards << Card.new(suit, rank, value)
       end
     end
