8d8f1449b7fe76d22d0a7f452de90cb894e07f45
Eduardo Parra <helloeduardo@icloud.com>
2020-08-22 08:49:02 -0600

    Create game test

---
+++ test/game_test.rb
@@ -0,0 +1,11 @@
+require './lib/deck'
+require './lib/card'
+require './lib/player'
+require './lib/turn'
+require './lib/tgame'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class GameTest < Minitest::Test
+
+end
