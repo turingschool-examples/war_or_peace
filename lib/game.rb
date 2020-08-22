4b1355a03201987234ab4de869446277d3925a4d
Cydnee Owens <cks410@yahoo.com>
2020-08-22 15:12:41 -0600

    'Add until loop iteration 3'

--- lib/game.rb
+++ lib/game.rb
@@ -1,25 +1,24 @@
 require "pry"
 
 class Game
-  attr_reader :turn, :player1, :player2
-
-  def intialize(turn)
-    @turn = turn
-    @player1 = player1
-    @player2 = player2
-  end
+  # attr_reader :turn, :player1, :player2
+  #
+  # def intialize(turn)
+  #   @turn = turn
+  #   @player1 = player1
+  #   @player2 = player2
+  # end
 
   def start
     p "Welcome, to the game"
     p "Type GO to start"
-    user_input = gets.chomp.upcase
+    user_input = gets.chomp
     if user_input == "GO"
-    else
-      p 'That was not Go. '
-    end
-      until @cards == [] do
+      until @all_cards == [] do
         @turn = Turn.new(player1, player2)
       end
+    else
+      p 'Please enter GO'
     end
-
+  end
 end
