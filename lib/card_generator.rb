15c31d0a67663721f933c316242fd8438cf096fd
Eduardo Parra <helloeduardo@icloud.com>
2020-08-22 13:57:50 -0600

    Add initialize and tests

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,7 +1,9 @@
 class CardGenerator
+  attr_reader :filename, :cards
 
   def initialize(filename)
     @filename = filename
+    @cards = []
   end
 
 end
--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -1,10 +1,23 @@
 require './lib/deck'
 require './lib/card'
+require './lib/card_generator'
 require 'minitest/autorun'
 require 'minitest/pride'
 
 class CardGeneratorTest < Minitest::Test
 
-  
+  def test_it_exists
+    filename = "cards.txt"
+    card_generator = CardGenerator.new(filename)
+
+    assert_instance_of CardGenerator, card_generator
+  end
+
+  def test_it_has_a_filename
+    filename = "cards.txt"
+    card_generator = CardGenerator.new(filename)
+
+    assert_equal "cards.txt", card_generator.filename
+  end
 
 end
