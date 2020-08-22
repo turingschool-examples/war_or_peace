d3fec294fa38d318507d10c75e4bb01b37c074ae
Eduardo Parra <helloeduardo@icloud.com>
2020-08-22 14:25:07 -0600

    Add card generating functionality and tests

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -3,7 +3,24 @@ class CardGenerator
 
   def initialize(filename)
     @filename = filename
-    @cards = []
+    @cards = generate_cards
+  end
+
+  def generate_cards
+    generated_cards = []
+
+    card_strings = File.readlines(filename, chomp:true)
+    card_strings.each do |card_string|
+      card_data = card_string.split(", ")
+
+      suit = card_data[1].downcase.to_sym
+      value = card_data[0]
+      rank = card_data[2].to_i
+
+      generated_cards << Card.new(suit, value, rank)
+    end
+
+    generated_cards
   end
 
 end
--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -20,4 +20,24 @@ class CardGeneratorTest < Minitest::Test
     assert_equal "cards.txt", card_generator.filename
   end
 
+  def test_it_generates_cards
+    filename = "cards.txt"
+    card_generator = CardGenerator.new(filename)
+
+    #check total cards
+    assert_equal 52, card_generator.cards.count
+
+    #check first card
+    assert_instance_of Card, card_generator.cards.first
+    assert_equal "2", card_generator.cards.first.value
+    assert_equal 2, card_generator.cards.first.rank
+    assert_equal :heart, card_generator.cards.first.suit
+
+    #check last card
+    assert_instance_of Card, card_generator.cards.last
+    assert_equal "Ace", card_generator.cards.last.value
+    assert_equal 14, card_generator.cards.last.rank
+    assert_equal :diamond, card_generator.cards.last.suit
+  end
+
 end
