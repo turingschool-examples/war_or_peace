bd62ddfed8349f8ec904c06f359bb51feab62161
Brian Liu <brian.b.liu@gmail.com>
2020-08-21 22:36:13 -0600

    Create test and method award sspoils, also successful test for award spoils.

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,15 +40,23 @@ class Turn
   def pile_cards
     if type == :basic
       spoils_of_war << player1.deck.remove_card
-      spoils_of_war << player2.deck.remove_card # shovel top card from deck into spoils_of_war array for both players
+      spoils_of_war << player2.deck.remove_card
     elsif type == :war
       spoils_of_war << player1.deck.cards[0..2]
       spoils_of_war << player2.deck.cards[0..2]
-      spoils_of_war.flatten! # shovel top three cards from deck to the spoils_of_war array
+      spoils_of_war.flatten!
     elsif type == :mutually_assured_destruction
       player1.deck.cards.slice!(0, 3)
-      player2.deck.cards.slice!(0, 3) # remove top three cards from deck
+      player2.deck.cards.slice!(0, 3)
     end
   end # def pile_cards
+
+  def award_spoils(winner)
+    if winner = player1
+      player1.deck.cards << spoils_of_war
+    elsif winner = player2
+      player2.deck.cards << spoils_of_war
+    end
+  end
 
 end
