8cdd590d2a54ed3bc6dd554c5f446f95c4471851
Eduardo Parra <helloeduardo@icloud.com>
2020-08-22 09:42:04 -0600

    Add game loop steps and output text

--- lib/game.rb
+++ lib/game.rb
@@ -9,18 +9,42 @@ class Game
 
   def start
     game_loop
+    display_game_result
   end
 
   def game_loop
-    until over?
-      turn = Turn.new(player1, player2)
+    until game_over?
+      turn = Turn.new(player1, player2) #don't really need a new turn object every time. Pile_cards advances the turn.
       @turn_count += 1
       #do turn stuff
+      turn_winner = turn.winner
+      turn_type = turn.type
+      turn.pile_cards
+
+      if turn_type == :basic
+        p "Turn #{turn_count}: #{turn_winner.name} has won #{turn.spoils_of_war.count} cards"
+      elsif turn_type == :war
+        p "Turn #{turn_count}: WAR - #{turn_winner.name} has won #{turn.spoils_of_war.count} cards"
+      else #mutually_assured_destruction
+        p "Turn #{turn_count}: *mutually assured destruction* - 6 cards removed from play"
+      end
+
+      turn.award_spoils(turn_winner)
     end
   end
 
-  def over?
-    player1.has_lost? || player2.has_lost? || turn_count == 100
+  def game_over?
+    player1.has_lost? || player2.has_lost? || turn_count == 1000000
+  end
+
+  def display_game_result
+    if player1.has_lost?
+      puts "*~*~*~* #{player2.name} has won the game! *~*~*~*"
+    elsif player2.has_lost?
+      puts "*~*~*~* #{player1.name} has won the game! *~*~*~*"
+    else
+      puts "---- DRAW ----"
+    end
   end
 
 end
