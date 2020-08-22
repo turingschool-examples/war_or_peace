d24553be3bfba87dc81847107e9bf1fc8e6e50b5
Brian Liu <brian.b.liu@gmail.com>
2020-08-21 20:06:05 -0600

    Create test and method for pile cards.

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,7 +6,7 @@ class Turn
     @player1       = player1
     @player2       = player2
     @spoils_of_war = []
-  end
+  end # def initialize
 
   def type
     if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) &&
@@ -17,17 +17,17 @@ class Turn
     else
       return :basic
     end
-  end
+  end # def type
 
   def winner
     if type == :basic
-      if player1.deck.rank_of_card(0) > player2.deck.rank_of_card(0)
+      if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
         winner = player1
       else
         winner = player2
       end
     elsif type == :war
-      if player1.deck.rank_of_card(2) > player2.deck.rank_of_card(2)
+      if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
         winner = player1
       else
         winner = player2
@@ -35,7 +35,6 @@ class Turn
     elsif type == :mutually_assured_destruction
       return "No Winner"
     end
-
-  end
+  end # def winner
 
 end
