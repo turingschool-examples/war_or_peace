fc398fcf75aa09b2e445452da10f1e664dd47477
Brian Liu <brian.b.liu@gmail.com>
2020-08-21 19:35:07 -0600

    Fixed method winner variables.

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,7 +9,8 @@ class Turn
   end
 
   def type
-    if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+    if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) &&
+       player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
       return :war
     elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
       return :mutually_assured_destruction
@@ -19,19 +20,19 @@ class Turn
   end
 
   def winner
-    if turn.type == :basic
+    if type == :basic
       if player1.deck.rank_of_card(0) > player2.deck.rank_of_card(0)
-        return player1
+        winner = player1
       else
-        return player2
+        winner = player2
       end
-    elsif turn.type == :war
+    elsif type == :war
       if player1.deck.rank_of_card(2) > player2.deck.rank_of_card(2)
-        return player1
+        winner = player1
       else
-        return player2
+        winner = player2
       end
-    elsif turn.type == :mutually_assured_destruction
+    elsif type == :mutually_assured_destruction
       return "No Winner"
     end
 
