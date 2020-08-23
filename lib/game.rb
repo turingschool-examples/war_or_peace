254cf235213f824e13ecc2e138be5b801521dff9
todd-estes <elestes@gmail.com>
2020-08-23 10:06:52 -0600

    Update game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -18,20 +18,22 @@ class Game
     until turn.player1.has_lost? == true || turn.player2.has_lost? == true
       if @turn_counter == 1000000
         p "---- DRAW ----"
+        break
       elsif turn.type == :basic
         winner = turn.winner
-        p "Turn #{@turn_counter}: #{winner} has one 2 cards"
+        p "Turn #{@turn_counter}: #{winner.name} has won 2 cards"
         turn.pile_cards
         turn.award_spoils(winner)
         @turn_counter += 1
       elsif turn.type == :war
         winner = turn.winner
-        p "Turn #{@turn_counter}: WAR - #{winner} has won 6 cards"
+        p "Turn #{@turn_counter}: WAR - #{winner.name} has won 6 cards"
         turn.pile_cards
         turn.award_spoils(winner)
         @turn_counter += 1
       elsif self.type == :mutually_assured_destruction
         p "*mutually assured destruction* 6 cards removed from play"
+        turn.pile_cards
         @turn_counter += 1
       end
     end
--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -5,29 +5,54 @@ require  './lib/turn'
 require  './lib/game'
 
 
-turn_counter = 1
+
 #lay out card suits, values, and ranks
-suit = [:hearts, :spades, :diamonds, :clubs]
-value = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
-"Ten", "Jack", "Queen", "King", "Ace"]
-rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+# suit = [:hearts, :spades, :diamonds, :clubs]
+# value = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+# "Ten", "Jack", "Queen", "King", "Ace"]
+# rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 #build deck *no idea how to build the deck*
-cards1 = suit.flat_map {|suit| value.flat_map {|value| rank.flat_map {Card.new(suit, value, rank)}}
-#cards1 = deck build
+suites = [:hearts, :diamonds, :clubs, :spaids]
+card_values_ranks = {
+  "2" => 2,
+  "3" => 3,
+  "4" => 4,
+  "5" => 5,
+  "6" => 6,
+  "7" => 7,
+  "8" => 8,
+  "9" => 9,
+  "10" => 10,
+  "Jack" => 11,
+  "Queen" => 12,
+  "King" => 13,
+  "Ace" => 14,
+}
+
+cards1 =[]
+
+suites.each do |suite|
+  card_values_ranks.each do |value, rank|
+    cards1 << Card.new(suite, value, rank)
+  end
+end
+
+
 cards1.shuffle!
-#split the deck and create decks
+
 cards2 = cards1.shift(26)
 deck1 = Deck.new(cards1)
 deck2 = Deck.new(cards2)
-#create players
 player1 = Player.new("Megan", deck1)
 player2 = Player.new("Aurora", deck2)
-#create turn
 turn = Turn.new(player1, player2)
+# require "pry"; binding.pry
 
 game = Game.new(turn)
 
 game.start
+
+
 
 reply = gets.chomp.upcase
 if reply = "GO"
