3c8676a30170616fbf98fafb308107c44a3068dc
todd-estes <elestes@gmail.com>
2020-08-23 16:07:24 -0600

    Update card_test.rb

--- test/card_test.rb
+++ test/card_test.rb
@@ -15,7 +15,5 @@ class CardTest < Minitest::Test
     assert_equal :diamond, card.suit
     assert_equal 'Queen', card.value
     assert_equal 12, card.rank
-    require "pry"; binding.pry
-  
   end
 end
