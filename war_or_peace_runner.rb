5c2ca0c8d1b7dc073ca85c62dc4bfe8c8c4c1321
todd-estes <elestes@gmail.com>
2020-08-23 10:08:01 -0600

    Update war_or_peace_runner.rb

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -4,14 +4,6 @@ require './lib/player'
 require  './lib/turn'
 require  './lib/game'
 
-
-
-#lay out card suits, values, and ranks
-# suit = [:hearts, :spades, :diamonds, :clubs]
-# value = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
-# "Ten", "Jack", "Queen", "King", "Ace"]
-# rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-#build deck *no idea how to build the deck*
 suites = [:hearts, :diamonds, :clubs, :spaids]
 card_values_ranks = {
   "2" => 2,
@@ -46,13 +38,10 @@ deck2 = Deck.new(cards2)
 player1 = Player.new("Megan", deck1)
 player2 = Player.new("Aurora", deck2)
 turn = Turn.new(player1, player2)
-# require "pry"; binding.pry
 
 game = Game.new(turn)
 
 game.start
-
-
 
 reply = gets.chomp.upcase
 if reply = "GO"
