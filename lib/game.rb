71842e33ed729021b00e7503f94dd6056abd9dee
todd-estes <elestes@gmail.com>
2020-08-23 16:04:35 -0600

    Update game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -3,20 +3,19 @@ class Game
   def initialize(turn)
     @turn = turn
     @turn_counter = 1
+    # @game_winner = game_winner
   end
-
-
-
 
   def start
     p 'Welcome to War! (or Peace) This game will be played with 52 cards.'
     p 'The players today are Megan and Aurora.'
     p 'Type GO to start the game!'
+    p '------------------------------------------------------------------'
   end
 
   def play
     until turn.player1.has_lost? == true || turn.player2.has_lost? == true
-      if @turn_counter == 1000000
+      if @turn_counter == 1000001
         p "---- DRAW ----"
         break
       elsif turn.type == :basic
@@ -31,11 +30,21 @@ class Game
         turn.pile_cards
         turn.award_spoils(winner)
         @turn_counter += 1
-      elsif self.type == :mutually_assured_destruction
+      elsif turn.type == :mutually_assured_destruction
         p "*mutually assured destruction* 6 cards removed from play"
         turn.pile_cards
         @turn_counter += 1
       end
     end
+    winner_statement
   end
+
+  def winner_statement
+    if turn.player1.has_lost? == true
+      p "*~*~*~* #{turn.player2.name} has won the game! *~*~*~*"
+    elsif turn.player2.has_lost? == true
+      p "*~*~*~* #{turn.player1.name} has won the game! *~*~*~*"
+    end
+  end
+
 end
