3acf4c77ae095228be7eb5cafcb9916ba322f6b2
todd-estes <elestes@gmail.com>
2020-08-23 16:08:04 -0600

    Update deck_test.rb

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -4,30 +4,21 @@ require './lib/card'
 require './lib/deck'
 
 class DeckTest < Minitest::Test
-  def test_setup
-    # skip
-    card = Card.new(:diamond, 'Queen', 12)
-  end
+  def test_it_exists_and_has_an_attribute
 
-  def test_it_exists_and_has_attributes
-    # skip
-    # card = Card.new(:diamond, 'Queen', 12)
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
 
-        card1 = Card.new(:diamond, 'Queen', 12)
-        card2 = Card.new(:spade, '3', 3)
-        card3 = Card.new(:heart, 'Ace', 14)
-        cards = [card1, card2, card3]
-
-        deck = Deck.new(cards)
-        require "pry"; binding.pry
-        # require "pry"; binding.pry
+    deck = Deck.new(cards)
 
     assert_instance_of Deck, deck
     assert_equal cards, deck.cards
   end
 
   def test_rank_of_card_at
-    skip
+
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
@@ -36,24 +27,65 @@ class DeckTest < Minitest::Test
     deck = Deck.new(cards)
 
     assert_equal 12, deck.rank_of_card_at(0)
-    assert_equal 12, deck.rank_of_card_at(1)
-    assert_equal 12, deck.rank_of_card_at(2)
+    assert_equal 3, deck.rank_of_card_at(1)
+    assert_equal 14, deck.rank_of_card_at(2)
   end
 
   def test_high_ranking_cards
-    skip
+
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
     cards = [card1, card2, card3]
 
     deck = Deck.new(cards)
-    require "pry"; binding.pry
+
 
     assert_equal [card1, card3], deck.high_ranking_cards
     refute_equal [card1], deck.high_ranking_cards
   end
 
+  def test_percent_high_ranking
+
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
 
 
+    assert_equal 66.67, deck.percent_high_ranking
+  end
+
+  def test_remove_card
+
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+
+
+
+
+    assert_equal card1, deck.remove_card
+    assert_equal [card2, card3], deck.cards
+  end
+
+  def test_add_card
+
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    card4 = Card.new(:spade, "5", 5)
+
+    assert_equal [card1, card2, card3, card4], deck.add_card(card4)
+  end
 end
