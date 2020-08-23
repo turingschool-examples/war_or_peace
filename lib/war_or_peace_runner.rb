dba384945d2eac841ff5d3f47d57aebcdf21a3bf
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-08-23 11:54:33 -0600

    Move user input to runner file and add loop for user input

--- lib/war_or_peace_runner.rb
+++ lib/war_or_peace_runner.rb
@@ -1,4 +1,26 @@
 require './lib/game'
 
-game = Game.new
-game.start
+
+p "Welcome to War! (or Peace) This game will be played with 52 cards."
+p "The players today are Megan and Aurora."
+p "Type 'GO' to start the game!"
+p "------------------------------------------------------------------"
+
+answer = ""
+count = 0
+
+until answer == "GO"
+  answer = gets.chomp
+  count += 1
+
+  if answer == "GO"
+    game = Game.new
+    game.start
+  else
+    p "Please type 'GO' to begin the game!"
+  end
+  if count >= 10
+    p "It seems like it's not a good time to play War. Maybe another time!"
+    break
+  end
+end
--- test/war_or_peace_runner_test.rb
+++ test/war_or_peace_runner_test.rb
@@ -9,7 +9,12 @@ require './lib/war_or_peace_runner'
 
 class WarOrPeaceRunnerTest < MiniTest::Test
 
-  
+  def test_start_game_method_starts_new_game
+    game = Game.new
+
+    assert_equal expected, game.run
+  end
+
 
 
 end
