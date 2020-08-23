4cdbc8c08c1230c4d36d424c45db3ff4643b90a6
Jake Heft <jakeheft@gmail.com>
2020-08-22 22:44:09 -0600

    Tidy up and add notes for morning

--- lib/game.rb
+++ lib/game.rb
@@ -13,7 +13,7 @@ class Game
   end
 
   def game_ended
-    if (@player1.has_lost? && @player2.has_lost?) || @turn_count == 1000000  ### set back to million
+    if (@player1.has_lost? && @player2.has_lost?) || @turn_count == 1000000
       puts "---- DRAW ----"
     elsif @player1.has_lost? || @player2.has_lost?
       puts "*~*~*~* #{game_winner.name} has won the game! *~*~*~*"
@@ -21,7 +21,7 @@ class Game
   end
 
   def start
-    until game_winner || @turn_count == 1000000  ###set back to million
+    until game_winner || @turn_count == 1000000
       turn = Turn.new(player1, player2)
       p @player1.deck.cards.length
       p @player2.deck.cards.length
@@ -35,7 +35,15 @@ class Game
         winner = turn.winner
       end
       turn.pile_cards
-      puts "Turn #{@turn_count}: #{turn.type != :mutually_assured_destruction ? winner.name : "No Winner"} won #{turn.spoils_of_war.count} cards"
+      ### might want to put an if statement below instead of what current puts statement.
+      ###   It might be more readable and also you need to add 'WAR - ' in for that type
+      ### UPDATE: must use if statement. Right now on MAD, the winner from previous
+      ###   hand is brought up and "wins 0 cards". This should probably be put into
+      ###   a method and called here rather than piling up methods in start method.
+      ###   I took a screenshot to show what I'm talking about.
+      puts "Turn #{@turn_count}: #{turn.type != :mutually_assured_destruction ?
+        "#{winner.name} won #{turn.spoils_of_war.count} cards" : "*mutually assured
+        destruction* 6 cards removed from play"}"
       turn.award_spoils(winner)
     end
     game_ended
