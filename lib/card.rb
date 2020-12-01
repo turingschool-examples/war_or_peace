0036719be2498d2b61d250208e289c69cf8e5782
Kris Litman <duguy8@gmail.com>
2020-11-30 19:23:34 -0500

    Initial Commit

---
+++ lib/card.rb
@@ -0,0 +1,14 @@
+class Card
+  attr_reader :suit, :value, :rank
+  def initialize(suit, value, rank)
+    @suit = suit
+    @value = value
+    @rank = rank
+  end
+end
+
+card = Card.new(:heart, 'Jack', 11)
+card.suit
+card.value
+card.rank
+require "pry"; binding.pry
