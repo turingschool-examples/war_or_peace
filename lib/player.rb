c0e30ca4eeccd6faaa9568db0ad11fe9f588ea7d
Adam Bowers <adam.j.bowers@gmail.com>
2020-12-01 13:00:04 -0700

    Create lost? method on Player class and write lost? method test on Player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,12 +1,18 @@
 class Player
   attr_reader :name,
-              :deck,
-              :lost 
+              :deck
 
   def initialize(name, deck)
     @name = name
     @deck = deck
     @lost = false
+  end
+
+  def lost?
+    if @deck.cards.count == 0
+      @lost = true
+    end
+    return @lost
   end
 
 end
--- test/player_test.rb
+++ test/player_test.rb
@@ -45,6 +45,24 @@ class PlayerTest < Minitest::Test
 
     player = Player.new('Clarisa', deck)
 
-    assert_equal false, player.lost
+    assert_equal false, player.lost?
+  end
+
+  def test_it_can_change_lost?
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, "Ace", 14)
+
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    player = Player.new('Clarisa', deck)
+
+    3.times do
+      player.deck.remove_card
+    end
+
+    assert_equal true, player.lost?
   end
 end
