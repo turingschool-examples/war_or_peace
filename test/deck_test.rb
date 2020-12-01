f115a848c00f12f0c0908f7d0a9f8fe0b3d99285
Khoa Nguyen <KhoaNg323@hotmail.com>
2020-11-30 17:18:04 -0700

    Initial commit

---
+++ test/deck_test.rb
@@ -0,0 +1,27 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require '../lib/deck'
+
+class DeckTest < Minitest::Test
+
+  def high_ranking_card_at
+
+  end
+
+  def high_ranking_cards
+
+  end
+
+  def percent_high_ranking
+
+  end
+
+  def remove_card
+
+  end
+
+  def add_card
+    
+  end
+
+end
