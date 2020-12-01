578e5b5e81207ab9ab07b26e11b2ecfe1a9fd9a6
Joseph Budina <joebud3@gmail.com>
2020-12-01 10:56:10 -0700

    Add winner method

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,17 +11,42 @@ class Turn
 
   def type 
     if (
-      @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
+      @player1.deck.rank_of_card_at(0) != 
+      @player2.deck.rank_of_card_at(0)
     )
       :basic
     elsif (
-      @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
+      @player1.deck.rank_of_card_at(0) == 
+      @player2.deck.rank_of_card_at(0)
     )
       :war 
     elsif (
-      @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
+      @player1.deck.rank_of_card_at(0) == 
+      @player2.deck.rank_of_card_at(0) && 
+      @player1.deck.rank_of_card_at(2) == 
+      @player2.deck.rank_of_card_at(2)
     )
       :mutually_assured_destruction 
+    end
+  end
+
+  def winner
+    if type == :basic && (
+      @player1.deck.rank_of_card_at(0) > 
+      @player2.deck.rank_of_card_at(0)
+    )
+      @player1
+    elsif
+      @player2
+    elsif type == :war && (
+      @player1.deck.rank_of_card_at(2) > 
+      @player2.deck.rank_of_card_at(2)
+    )
+      @player1
+    elsif
+      @player2
+    elsif type == :mutually_assured_destruction
+      p "No Winner"
     end
   end
 end
