f9d76b27bbf2ce824b0119181258107146e14788
Alexa Morales <alexamsmyth@gmail.com>
2020-11-30 19:46:38 -0500

    Revise deck class file

---
+++ test/deck_test.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+
+class DeckTest < Minitest::Test
+  def setup
+    deck = Deck.new(cards)
+  end
+
+  def test_it_exists
+    card = Card.new(:diamond, 'Queen', 12)
+
+    assert_instance_of Card, card
+  end
+
+  def test_it_has_readable_attributes
+    card = Card.new(:diamond, 'Queen', 12)
+
+    assert_equal :diamond, card.suit
+    assert_equal 'Queen', card.value
+    assert_equal 12, card.rank
+  end
+end
