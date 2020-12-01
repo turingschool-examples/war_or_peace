ae8b61cac47ca3a09ac4aebf2b33e9a368d12462
Joseph Budina <joebud3@gmail.com>
2020-11-30 17:48:16 -0700

    Add cards test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -8,6 +8,7 @@ class DeckTest < Minitest::Test
     @card1 = Card.new(:diamond, 'Queen', 12)
     @card2 = Card.new(:spade, '3', 3)
     @card3 = Card.new(:heart, 'Ace', 14)
+    @card4 = Card.new(:club, '5', 5)
     @cards = [@card1, @card2, @card3]
     @deck = Deck.new(@cards)
   end
@@ -36,5 +37,14 @@ class DeckTest < Minitest::Test
     assert_equal [@card2,@card3], @deck.cards
     assert_equal [@card3], @deck.high_ranking_cards
     assert_equal 50.0, @deck.percent_high_ranking
+  end
+
+  def test_it_can_add_cards
+    @deck.remove_card
+    @deck.add_card(@card4)
+
+    assert_equal [@card2,@card3,@card4], @deck.cards
+    assert_equal [@card3], @deck.high_ranking_cards
+    assert_equal 33.33, @deck.percent_high_ranking
   end
 end
