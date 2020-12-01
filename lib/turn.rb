db9200af228374f8f37f80cba0c48b162801152e
Jenny Branham <branhamjenny@gmail.com>
2020-12-01 09:37:43 -0600

    Check in before class

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,5 +3,29 @@
    def initialize(player1, player2)
      @player1 = player1
      @player2 = player2
+     @spoils_of_war = []
+   end
+
+   #add cards in spoils_of_war to the winner of the turn
+   def award_spoils
+   end
+
+   #cards will be sent from player deck to spoils_of_war
+   def pile_cards
+   end
+
+   #basic, war, of mutally_assured_destruction
+   def type
+     #if rank_of_card_at(0) are not the same, then basic
+     #if rank_of_card_at(0) are the same, then war
+     #if rank_of_card_at(0) and (2) are the same,
+     #then mutally_assured_destruction
+   end
+
+   #determines winner of war
+   def winner
+     #if basic
+     #elsif war
+     #elsif mutally_assured_destruction
    end
  end
