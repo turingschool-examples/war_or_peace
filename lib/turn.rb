572a04a13414659fd82f0b0170f4372495898e8f
Jenny Branham <branhamjenny@gmail.com>
2020-12-01 13:42:53 -0600

    Work on turn type

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,7 +2,7 @@
    attr_reader  :player1,
                 :player2,
                 :spoils_of_war
-                
+
    def initialize(player1, player2)
      @player1 = player1
      @player2 = player2
@@ -20,9 +20,17 @@
    #basic, war, of mutally_assured_destruction
    def type
      #if rank_of_card_at(0) are not the same, then basic
-     #if rank_of_card_at(0) are the same, then war
-     #if rank_of_card_at(0) and (2) are the same,
-     #then mutally_assured_destruction
+    if player1.rank_of_card_at(0) != player2.rank_of_card_at(0)
+       :basic
+    #if rank_of_card_at(0) are the same, then war
+    elsif player1.rank_of_card_at(0) == player2.rank_of_card_at(0)
+      :war
+    #if rank_of_card_at(0) and (2) are the same,
+    #then mutally_assured_destruction
+  elsif player1.rank_of_card_at(0) == player2.rank_of_card_at(0) &&
+    player1.rank_of_card_at(2) == player2.rank_of_card_at(2)
+      :mutally_assured_destruction
+    end
    end
 
    #determines winner of war
