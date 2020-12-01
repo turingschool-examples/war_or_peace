a60434e12b8ccb09281bb719dde5d59312cc76ca
Adam Bowers <adam.j.bowers@gmail.com>
2020-12-01 07:47:11 -0700

    Write Deck class add_card method

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -75,4 +75,19 @@ class DeckTest < Minitest::Test
 
     assert_equal high_card, deck.remove_card
   end
+
+  def test_it_can_add_card
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, "Ace", 14)
+
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    card4 = Card.new(:club, "5", 5)
+    require "pry"; binding.pry
+    deck.add_card
+  end
+
 end
