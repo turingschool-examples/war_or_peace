8f1a64bc127cd297e42c69761da5737c59383563
Joseph Budina <joebud3@gmail.com>
2020-11-30 18:21:43 -0700

    Add has lost method

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,18 @@
 class Player 
   attr_reader :name,
               :deck
+
   def initialize(name, deck)
-    @name = name 
-    @deck = deck 
+    @name = name
+    @deck = deck
+    @has_lost = false
+  end
+
+  def has_lost?
+    if @deck.cards.empty?
+      @has_lost = true
+    else
+      @has_lost
+    end
   end
 end
