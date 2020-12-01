5e760ad757ae59175f7b97ac45397001fa7011da
Adam Bowers <adam.j.bowers@gmail.com>
2020-12-01 07:24:51 -0700

    Write Deck class remove_card method and write test for remove_card method

--- lib/deck.rb
+++ lib/deck.rb
@@ -10,6 +10,22 @@ class Deck
   end
 
   def high_ranking_cards
-    
+    high_ranking_cards = []
+    @cards.map do |card|
+      if card.rank > 10
+        high_ranking_cards << card
+      end
+    end
+  return high_ranking_cards
+  end
+
+  def percent_high_ranking
+    dividend = high_ranking_cards.count
+    divisor = @cards.count.to_f
+    percent_high_ranking = (dividend / divisor).round(4) * 100
+  end
+
+  def remove_card
+    @cards.shift 
   end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -45,6 +45,34 @@ class DeckTest < Minitest::Test
     cards = [card1, card2, card3]
 
     deck = Deck.new(cards)
-    require "pry"; binding.pry
+    high_ranking_cards = [card1, card3]
+
+    assert_equal high_ranking_cards, deck.high_ranking_cards
+  end
+
+  def test_it_can_calculate_percent_high_ranking
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, "Ace", 14)
+
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    assert_equal 66.67, deck.percent_high_ranking
+  end
+
+  def test_it_can_remove_high_card
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, "Ace", 14)
+
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    deck.remove_card
+
+    assert_equal cards.shift, deck.remove_card
   end
 end
