ca7c804cc3b8da1b66870bdb3e950ad545043aca
Harrison Blake <harrison.r.blake@gmail.com>
2020-12-01 19:52:49 -0700

    added deck does it exist test

---
+++ test/deck_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+
+class DeckTest < Minitest::Test
+  def test_it_exists
+    cards = []
+    card_1 = Card.new(:diamond, 'Queen', 12)
+    card_2 = Card.new(:heart, 'two', 2)
+    cards.push(card_1)
+    cards.push(card_2)
+    deck = Deck.new(cards)
+    assert_instance_of Deck, deck
+  end
+end
