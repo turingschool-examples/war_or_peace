fe00ba353fd1c6d7e868773e72dc158d256e266e
Alex Schwartz <aschwartz@detaso.com>
2020-12-01 22:20:18 -0600

    Add high_ranking_cards and test to deck

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,9 +5,10 @@ require './lib/card'
 
 class DeckTest < Minitest::Test
   def setup
-    card_1 = Card.new(:diamond, 'Queen', 12)
-    card_2 = Card.new(:heart, 'Jack', 11)
-    @cards = [card_1, card_2]
+    @card_1 = Card.new(:diamond, 'Queen', 12)
+    @card_2 = Card.new(:heart, 'Jack', 11)
+    @card_3 = Card.new(:club, 'Two', 2)
+    @cards = [@card_1, @card_2, @card_3]
 
     @deck = Deck.new(@cards)
   end
@@ -24,5 +25,11 @@ class DeckTest < Minitest::Test
     # TODO (alex schwartz): which one of these is the better Expected param for this test?
     # assert_equal 12, @deck.rank_of_card_at(1)
     assert_equal @deck.cards.first.rank, @deck.rank_of_card_at(0)
+  end
+
+  def test_high_ranking_cards
+    assert_equal 2, @deck.high_ranking_cards.length
+    assert_includes @deck.high_ranking_cards, @card_1
+    assert_includes @deck.high_ranking_cards, @card_2
   end
 end
