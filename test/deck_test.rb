c7f210b9a3e0ce75f30e870d0137c65b61bc71dc
Katy La Tour <klatour324@gmail.com>
2020-12-01 22:41:41 -0600

    Create test for high ranking cards

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -24,12 +24,22 @@ class DeckTest < Minitest::Test
 
   def test_it_returns_rank_if_card_found
     card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:heart, 'Three', 3)
-    card3 = Card.new(:spade, 'Ten', 10)
+    card2 = Card.new(:heart, '3', 3)
+    card3 = Card.new(:spade, '10', 10)
 
     deck = Deck.new([card1, card2, card3])
 
     assert_equal 12, deck.rank_of_card_at(0)
+  end
+
+  def test_it_returns_cards_with_a_rank_above_10
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:diamond, 'King', 13)
+    card3 = Card.new(:spade, '7', 7)
+
+    deck = Deck.new([card1, card2, card3])
+
+    assert_equal [card1, card2], deck.high_ranking_cards
   end
 
 end
