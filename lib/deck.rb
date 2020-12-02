d23123c920d523a471e82cb5a06628746ec92772
Trevor Suter <Trevorsuter@icloud.com>
2020-12-01 20:20:43 -0700

    first completed deck

---
+++ lib/deck.rb
@@ -0,0 +1,41 @@
+class Deck
+  attr_reader :cards
+
+  def initialize (cards)
+    @cards = cards
+  end
+
+  def rank_of_card_at(index)
+    if @cards[index].nil? == true
+      return 0
+    else @cards[index].rank
+    end
+  end
+
+
+
+  def high_ranking_cards
+    cards.select do |card|
+      card.rank >= 11
+    end
+  end
+
+  def percent_high_ranking
+      percentage = (high_ranking_cards.length.to_f / cards.length).round(2)
+      percentage * 100
+  end
+
+  def remove_card
+    cards.shift
+  end
+
+  def add_card(new_card)
+    cards.append(new_card)
+  end
+end
+
+card1 = Card.new(:diamond, 'Queen', 12)
+card2 = Card.new(:spade, '3', 3)
+card3 = Card.new(:heart, 'Ace', 14)
+
+cards = [card1, card2, card3]
