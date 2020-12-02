a000186415f9b69027799924b58f1df99c401f15
DougWelchons <doug.welchons@gmail.com>
2020-12-02 14:44:35 -0700

    Add additional Turn class methods. Iteration 2

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,18 +18,42 @@ class Turn
     player2.deck.cards.slice(index).rank
   end
 
+  def check_winner(index)
+    if player1.deck.cards.slice(index).rank > player2.deck.cards.slice(index).rank
+      @winner = @player1
+    else
+      @winner = @player2
+    end
+  end
+
   def type
     @type_of_turn = @type_class[[compaire(0), compaire(2)]]
   end
 
   def winner
     if @type_of_turn == :basic
-      @winner = @player2
+      check_winner(0)
     elsif @type_of_turn == :war
-      @winner = @player2
+      check_winner(2)
     else
       "No Winner"
     end
   end
 
+  def pile_cards
+    if @type_of_turn == :basic
+      player1.deck.remove_card
+      player2.deck.remove_card
+    elsif @type_of_turn == :war
+      3.times do
+        player1.deck.remove_card
+        player2.deck.remove_card
+      end
+    else
+      3.times do
+        player1.deck.remove_card
+        player2.deck.remove_card
+      end
+    end
+  end
 end
