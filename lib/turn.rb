c02953f12b453316e63e9272aefc28438e2f1a53
Joseph Budina <joebud3@gmail.com>
2020-12-01 19:39:42 -0700

    Add pile cards method

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,14 +32,14 @@ class Turn
 
   def winner
     if type == :basic && (
-      @player1.deck.rank_of_card_at(0) > 
+      @player1.deck.rank_of_card_at(0) >
       @player2.deck.rank_of_card_at(0)
     )
       @player1
     elsif
       @player2
     elsif type == :war && (
-      @player1.deck.rank_of_card_at(2) > 
+      @player1.deck.rank_of_card_at(2) >
       @player2.deck.rank_of_card_at(2)
     )
       @player1
@@ -47,6 +47,16 @@ class Turn
       @player2
     elsif type == :mutually_assured_destruction
       p "No Winner"
+    end
+  end
+
+  def pile_cards
+    if type == :basic
+      @spoils_of_war << @player1.deck.remove_card
+      @spoils_of_war << @player2.deck.remove_card
+    elsif type == :war 
+      @spoils_of_war << @player1.deck[0..2]
+      @spoils_of_war << @player2.deck[0..2]
     end
   end
 end
