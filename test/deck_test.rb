dcb9ed3752f13bb791290f4d99eddc5f9032c0ac
Harrison Blake <harrison.r.blake@gmail.com>
2020-12-02 01:00:55 -0700

    refactored deck_test: added setup and test_percent_high_ranking

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -4,23 +4,27 @@ require './lib/card'
 require './lib/deck'
 
 class DeckTest < Minitest::Test
-  def test_it_exists
+  def setup
     cards = []
     card_1 = Card.new(:diamond, 'Queen', 12)
     card_2 = Card.new(:heart, 'two', 2)
     cards.push(card_1)
     cards.push(card_2)
-    deck = Deck.new(cards)
-    assert_instance_of Deck, deck
+    @deck = Deck.new(cards)
+  end
+
+  def test_it_exists
+
+    assert_instance_of Deck, @deck
   end
 
   def test_high_ranking_cards
-    cards = []
-    card_1 = Card.new(:diamond, 'Queen', 12)
-    card_2 = Card.new(:heart, 'two', 2)
-    cards.push(card_1)
-    cards.push(card_2)
-    deck = Deck.new(cards)
-    assert_equal [card_1], deck.high_ranking_cards()
+
+    assert_equal 12, @deck.high_ranking_cards[0].rank
+  end
+
+  def test_percent_high_ranking
+
+    assert_equal 0.5, @deck.percent_high_ranking
   end
 end
