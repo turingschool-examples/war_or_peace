d7708b7f9e9f07a5ca9bff93ac7d4bb51610db5d
DougWelchons <doug.welchons@gmail.com>
2020-12-01 19:29:29 -0700

    Add changes to Player test. Iteration 2

--- test/player_test.rb
+++ test/player_test.rb
@@ -14,6 +14,7 @@ class PlayerTest < Minitest::Test
   end
 
   def test_player_has_a_name
+    skip
     deck = Deck.new([])
     player = Player.new('Clarisa', deck)
 
@@ -21,6 +22,7 @@ class PlayerTest < Minitest::Test
   end
 
   def test_player_has_a_deck
+    skip
     deck = Deck.new([])
     player = Player.new('Clarisa', deck)
 
@@ -28,29 +30,50 @@ class PlayerTest < Minitest::Test
   end
 
   def test_player_hasnt_lost_by_default
-    cards = []
-    deck = Deck.new(cards)
+    skip
+    deck = Deck.new([])
     player = Player.new('Clarisa', deck)
 
     refute player.has_lost?
   end
 
   def test_player_removes_cards
+    skip
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
     deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+    player.deck.remove_card
 
-    assert_???
+    refute_includes(player.deck.cards, card1)
+    assert_includes(player.deck.cards, card2)
+    assert_includes(player.deck.cards, card3)
   end
 
   def test_player_doesnt_lose_when_they_have_cards
+    skip
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+    player.deck.remove_card
 
+    refute_equal [], player.deck.cards
     refute player.has_lost?
   end
 
   def test_player_loses_when_they_have_no_cards
+    skip
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+    3.times { player.deck.remove_card }
 
+    assert_equal [], player.deck.cards
     assert player.has_lost?
   end
 
