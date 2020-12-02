10c31362700ebd6e94d89fece4264ef3fe10c343
DougWelchons <doug.welchons@gmail.com>
2020-12-01 22:01:00 -0700

    Add final tests to turn test file. Iteration 2.

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -146,8 +146,8 @@ class TurnTest < Minitest::Test
     turn.winner
     turn.pile_cards
 
-    asssert_equal [card2, card3, card4], turn.player1.deck.cards
-    asssert_equal [card6, card7, card8], turn.player2.deck.cards
+    assert_equal [card2, card3, card4], turn.player1.deck.cards
+    assert_equal [card6, card7, card8], turn.player2.deck.cards
   end
 
   def test_war_turn_removes_3_cards_from_each_palyer
@@ -200,16 +200,77 @@ class TurnTest < Minitest::Test
 
   def test_removed_cards_are_moved_to_spoils_of_war
     skip
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card3, card4])
+    deck2 = Deck.new([card5, card6, card7, card8])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
 
+    turn.type
+    turn.winner
+    turn.pile_cards
+
+    assert_equal [card2, card3, card4], turn.player1.deck.cards
+    assert_equal [card6, card7, card8], turn.player2.deck.cards
+    assert_equal [card1, card5], turn.spoils_of_war
   end
 
   def test_MAD_turns_dont_move_cards_to_spoils_of_war
     skip
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:heart, '8', 8)
+    card5 = Card.new(:diamond, 'Jack', 11)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:spade, '9', 9)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card3, card4])
+    deck2 = Deck.new([card5, card6, card7, card8])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
 
+    turn.type
+    turn.winner
+    turn.pile_cards
+
+    assert_equal [card4], turn.player1.deck.cards
+    assert_equal [card8], turn.player2.deck.cards
+    assert_equal [], turn.spoils_of_war
   end
 
   def test_spoils_of_war_go_to_the_winner
     skip
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card3, card4])
+    deck2 = Deck.new([card5, card6, card7, card8])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
 
+    turn.type
+    turn.winner
+    turn.pile_cards
+    turn.award_spoils
+
+    assert_equal [card2, card3, card4, card1, card5], turn.player1.deck.cards
+    assert_equal [card6, card7, card8], turn.player2.deck.cards
+    assert_equal [], turn.spoils_of_war
   end
 end
