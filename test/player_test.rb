a2f891a3b08adfa43a8a0a6fdba12e55b487e8d1
Katy La Tour <klatour324@gmail.com>
2020-12-02 09:43:36 -0600

    Create a test that shows a player exists with a deck

---
+++ test/player_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+
+class PlayerTest < Minitest::Test
+  def test_a_player_exists_with_a_deck_of_cards
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    deck = Deck.new([card1, card2, card3])
+    
+    player = Player.new('Clarissa', deck)
+
+    assert_instance_of Player, player
+  end
+end
