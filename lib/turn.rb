732677d66d7cf667638554b38374d2927bc5148e
Jenny Branham <branhamjenny@gmail.com>
2020-12-01 19:57:42 -0600

    Winner test and method

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,42 +1,62 @@
- class Turn
-   attr_reader  :player1,
-                :player2,
-                :spoils_of_war
+class Turn
+  attr_reader  :player1,
+  :player2,
+  :spoils_of_war
 
-   def initialize(player1, player2)
-     @player1 = player1
-     @player2 = player2
-     @spoils_of_war = []
-   end
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @spoils_of_war = []
+  end
 
-   #add cards in spoils_of_war to the winner of the turn
-   def award_spoils
-   end
+  #add cards in spoils_of_war to the winner of the turn
+  def award_spoils
+  end
 
-   #cards will be sent from player deck to spoils_of_war
-   def pile_cards
-   end
+  #cards will be sent from player deck to spoils_of_war
+  def pile_cards
+  end
 
-   #basic, war, of mutally_assured_destruction
-   def type
-     #if rank_of_card_at(0) are not the same, then basic
-    if player1.rank_of_card_at(0) != player2.rank_of_card_at(0)
-       :basic
-    #if rank_of_card_at(0) are the same, then war
-    elsif player1.rank_of_card_at(0) == player2.rank_of_card_at(0)
-      :war
+  #basic, war, of mutally_assured_destruction
+  def type
+    player_1_rank_0 = player1.deck.rank_of_card_at(0)
+    player_2_rank_0 = player2.deck.rank_of_card_at(0)
+    player_1_rank_2 = player1.deck.rank_of_card_at(2)
+    player_2_rank_2 = player2.deck.rank_of_card_at(2)
+
+    #if rank_of_card_at(0) are not the same, then basic
+    if player_1_rank_0 != player_2_rank_0
+      :basic
     #if rank_of_card_at(0) and (2) are the same,
     #then mutally_assured_destruction
-  elsif player1.rank_of_card_at(0) == player2.rank_of_card_at(0) &&
-    player1.rank_of_card_at(2) == player2.rank_of_card_at(2)
+    elsif player_1_rank_0 == player_2_rank_0 &&
+      player_1_rank_2 == player_2_rank_2
       :mutally_assured_destruction
+    #if rank_of_card_at(0) are the same, then war
+    elsif player_1_rank_0 == player_2_rank_0
+      :war
     end
-   end
+  end
 
-   #determines winner of war
-   def winner
-     #if basic
-     #elsif war
-     #elsif mutally_assured_destruction
-   end
- end
+  #determines winner of war
+  def winner
+    #if basic
+    if type ==  :basic
+      if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+        player1.name
+      else
+        player2.name
+      end
+    #elsif war
+    elsif type == :war
+      if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+        player1.name
+      else
+        player2.name
+      end
+    #elsif mutally_assured_destruction
+    elsif type == :mutally_assured_destruction
+      "No Winner"
+    end
+  end
+end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -12,16 +12,15 @@ class TurnTest < MiniTest::Test
     @card3 = Card.new(:heart, 'Ace', 14)
     @cards = [@card1, @card2, @card3]
     deck = Deck.new(@cards)
-    player1 = Player.new("Jenny", deck)
+    @player1 = Player.new("Jenny", deck)
 
     @card4 = Card.new(:diamond, '10', 10)
     @card5 = Card.new(:spade, '5', 5)
     @card6 = Card.new(:heart, 'King', 13)
     @cards2 = [@card4, @card5, @card6]
     deck2 = Deck.new(@cards2)
-    player2 = Player.new("Marisa", deck2)
-
-    turn = Turn.new(player1,player2)
+    @player2 = Player.new("Marisa", deck2)
+    turn = Turn.new(@player1, @player2)
   end
 
   def test_it_exists
@@ -36,10 +35,88 @@ class TurnTest < MiniTest::Test
   def test_sending_player_cards_to_spoil_of_war
   end
 
-  def test_type_of_turn
+  def test_basic_type_of_turn
+    turn = setup
+    assert_equal :basic, turn.type
   end
 
-  def test_winner_of_war
+  def test_war_type_of_turn
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @cards = [@card1, @card2, @card3]
+    deck = Deck.new(@cards)
+    player5 = Player.new("Jenny", deck)
+
+    @card4 = Card.new(:diamond, '10', 12)
+    @card5 = Card.new(:spade, '5', 5)
+    @card6 = Card.new(:heart, 'King', 13)
+    @cards2 = [@card4, @card5, @card6]
+    deck2 = Deck.new(@cards2)
+    player6 = Player.new("Marisa", deck2)
+
+    turn = Turn.new(player5,player6)
+    assert_equal :war, turn.type
   end
-  
+
+  def test_mutally_assured_destruction_type_of_turn
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @cards = [@card1, @card2, @card3]
+    deck = Deck.new(@cards)
+    player3 = Player.new("Jenny", deck)
+
+    @card4 = Card.new(:diamond, '10', 12)
+    @card5 = Card.new(:spade, '5', 5)
+    @card6 = Card.new(:heart, 'King', 14)
+    @cards2 = [@card4, @card5, @card6]
+    deck2 = Deck.new(@cards2)
+    player4 = Player.new("Marisa", deck2)
+
+    turn = Turn.new(player3,player4)
+    assert_equal :mutally_assured_destruction, turn.type
+  end
+
+  def test_basic_winner_of_war
+    turn = setup
+    assert_equal @player1.name, turn.winner
+  end
+
+  def test_mutally_assured_destruction_winner
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @cards = [@card1, @card2, @card3]
+    deck = Deck.new(@cards)
+    player3 = Player.new("Jenny", deck)
+
+    @card4 = Card.new(:diamond, '10', 12)
+    @card5 = Card.new(:spade, '5', 5)
+    @card6 = Card.new(:heart, 'King', 14)
+    @cards2 = [@card4, @card5, @card6]
+    deck2 = Deck.new(@cards2)
+    player4 = Player.new("Marisa", deck2)
+
+    turn = Turn.new(player3,player4)
+    assert_equal "No Winner", turn.winner
+  end
+  def test_war_winner
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'King', 13)
+    @cards = [@card1, @card2, @card3]
+    deck = Deck.new(@cards)
+    player5 = Player.new("Jenny", deck)
+
+    @card4 = Card.new(:diamond, '10', 12)
+    @card5 = Card.new(:spade, '5', 5)
+    @card6 = Card.new(:heart, 'Ace', 14)
+    @cards2 = [@card4, @card5, @card6]
+    deck2 = Deck.new(@cards2)
+    player6 = Player.new("Marisa", deck2)
+
+    turn = Turn.new(player5,player6)
+    assert_equal player6.name, turn.winner
+  end
 end
