a0a7bf820e1612fabb2f6061d913026f766bc031
Katy La Tour <klatour324@gmail.com>
2020-12-01 21:54:41 -0600

    Create tests for rank of card at method

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/card'
 require './lib/deck'
 
 class DeckTest < Minitest::Test
@@ -14,4 +15,21 @@ class DeckTest < Minitest::Test
 
     assert_equal [], deck.cards
   end
+
+  def test_it_returns_nil_for_rank_if_card_not_found
+    deck = Deck.new([])
+
+    assert_nil deck.rank_of_card_at(0)
+  end
+
+  def test_it_returns_rank_if_card_found
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:heart, 'Three', 3)
+    card3 = Card.new(:spade, 'Ten', 10)
+
+    deck = Deck.new([card1, card2, card3])
+
+    assert_equal 12, deck.rank_of_card_at(0)
+  end
+
 end
