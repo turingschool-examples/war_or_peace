80d37bac0b9cb24771fd5a334e869238758cb238
Trevor Suter <Trevorsuter@icloud.com>
2020-12-01 20:23:46 -0700

    first deck minus excess info

--- lib/deck.rb
+++ lib/deck.rb
@@ -33,9 +33,3 @@ class Deck
     cards.append(new_card)
   end
 end
-
-card1 = Card.new(:diamond, 'Queen', 12)
-card2 = Card.new(:spade, '3', 3)
-card3 = Card.new(:heart, 'Ace', 14)
-
-cards = [card1, card2, card3]
