0a72b61fcb690de1d5e9cac0620663e89b153531
Khoa Nguyen <KhoaNg323@hotmail.com>
2020-12-01 21:56:30 -0700

    Added initialize and has_lost? to player.rb and player_test.rb

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,14 @@
 class Player
+  attr_reader :name,
+              :deck
+  def initialize(name, deck)
+    @name = name
+    @deck = deck
+    @has_lost = false
+  end
 
-
-
+  def has_lost?
+   @has_lost 
+ end
 
 end
--- test/player_test.rb
+++ test/player_test.rb
@@ -1,8 +1,42 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/player'
+require '../lib/player'
+require '../lib/deck'
+require '../lib/card'
 
 class PlayerTest < Minitest::Test
-  def setup
+  # def setup
+  #   player = Player.new('Clarisa', deck)
+  # end
+
+  def test_it_exists
+    card1  = Card.new(:diamond, 'Queen', 12)
+    card2  = Card.new(:spade, '3', 3)
+    card3  = Card.new(:heart, 'Ace', 14)
+    cards  = [card1, card2, card3]
+    deck   = Deck.new(cards)
     player = Player.new('Clarisa', deck)
+
+    assert_equal 'Clarisa', player.name
   end
+
+  def test_has_lost?
+    card1  = Card.new(:diamond, 'Queen', 12)
+    card2  = Card.new(:spade, '3', 3)
+    card3  = Card.new(:heart, 'Ace', 14)
+    cards  = [card1, card2, card3]
+    deck   = Deck.new(cards)
+    player = Player.new('Clarisa', deck)
+
+
+    assert_equal false, player.has_lost?
+    player.deck.remove_card
+    assert_equal false, player.has_lost?
+    player.deck.remove_card
+    assert_equal false, player.has_lost?
+    player.deck.remove_card
+    assert_equal true, player.has_lost?
+
+  end
+
+end
