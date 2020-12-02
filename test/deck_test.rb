d346bf31a69d8eda058226500965cfefe7d13c84
Jesus Quezada-Guillen <maj3st1@gmail.com>
2020-12-01 21:19:51 -0600

    Add test to verify all methods work if removing a card.

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -49,5 +49,19 @@ class DeckTest < Minitest::Test
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
     assert_equal card1, deck.remove_card
+    assert_equal [card2, card3], deck.cards
   end
+
+  def test_it_can_tell_percentage_rank_after_card_removed
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    deck.remove_card
+    assert_equal [card3], deck.high_ranking_cards
+    assert_equal 50.0, deck.percent_high_ranking
+  end
+
+
 end
