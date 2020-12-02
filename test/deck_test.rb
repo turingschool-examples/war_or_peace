1f2d7cc574f63b2dad5fb378cd617ee9c23b4ed6
Jesus Quezada-Guillen <maj3st1@gmail.com>
2020-12-01 21:23:30 -0600

    Add test for add_card method of Deck class.

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -63,5 +63,15 @@ class DeckTest < Minitest::Test
     assert_equal 50.0, deck.percent_high_ranking
   end
 
+  def test_it_can_add_cards
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card2, card3]
+    deck = Deck.new(cards)
+    card4 = Card.new(:club, '5', 5)
+    deck.add_card(card4)
+    assert_equal [card2, card3, card4], deck.cards
+  end
+
 
 end
