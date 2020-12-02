4e7d112991ea8948f7619489206db85abc8ef03d
Alexa Morales <alexamsmyth@gmail.com>
2020-12-02 17:02:27 -0500

    Set up player_test.rn, Iteration 2

---
+++ test/player_test.rb
@@ -0,0 +1,28 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/deck'
+require './lib/player'
+require './lib/card'
+require 'pry'
+
+class PlayerTest < Minitest::Test
+
+def test_it_exists
+end
+
+
+def test_name
+  #player.name
+end
+
+def test_deck
+  #player.Deck
+end
+
+def test_has_lost?
+  #lose when deck empty, boolean
+end
+
+def test_deck.remove_card
+  #this shoudl already work from ./deck
+end
