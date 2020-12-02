db62112bec7498a79abd26e97355a6e9ec7af8f8
Katy La Tour <klatour324@gmail.com>
2020-12-02 00:12:20 -0600

    Add percent high ranking method

--- lib/deck.rb
+++ lib/deck.rb
@@ -14,6 +14,11 @@ class Deck
     cards.select { |card| card.rank > 10 }
   end
 
+  def percent_high_ranking
+    percentage = (high_ranking_cards.length.to_f / cards.length.to_f) * 100.00
+    percentage.round(2)
+  end
+
   def remove_card
     cards.shift
   end
