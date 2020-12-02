eb7fec0c907bf5e2042d13b66397d17a9f4d8755
Katy La Tour <klatour324@gmail.com>
2020-12-02 16:48:40 -0600

    Create test to see if player lost the game

--- test/player_test.rb
+++ test/player_test.rb
@@ -11,9 +11,21 @@ class PlayerTest < Minitest::Test
     card3 = Card.new(:heart, 'Ace', 14)
 
     deck = Deck.new([card1, card2, card3])
-    
+
     player = Player.new('Clarissa', deck)
 
     assert_instance_of Player, player
+  end
+
+  def test_if_player_has_lost_the_game
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    deck = Deck.new([card1, card2, card3])
+
+    player = Player.new('Clarissa', deck)
+
+    assert_equal false, player.has_lost?
   end
 end
