eeff155d21f3f6ced32044d71b3211754a62ca86
Angel Breaux <abreaux@detaso.com>
2020-12-02 09:59:26 -0600

    Refactor test files to use setup method properly

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,85 +5,46 @@ require './lib/deck'
 
 class DeckTest < Minitest::Test
   def setup
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @cards = [@card1, @card2, @card3]
+    @deck = Deck.new(@cards)
   end
 
   def test_it_exists
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-
-    assert_instance_of Card, card1
-    assert_instance_of Card, card2
-    assert_instance_of Card, card3
-    assert_instance_of Deck, deck
+    assert_instance_of Card, @card1
+    assert_instance_of Card, @card2
+    assert_instance_of Card, @card3
+    assert_instance_of Deck, @deck
   end
 
   def test_it_has_readable_attributes
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-
-    assert_equal [card1, card2, card3], deck.cards
+    assert_equal [@card1, @card2, @card3], @deck.cards
   end
 
   def test_rank_of_card_at
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-
-    assert_equal card1.rank, deck.rank_of_card_at(0)
+    assert_equal @card1.rank, @deck.rank_of_card_at(0)
   end
 
   def test_high_ranking_cards
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-
-    assert_equal [card1, card3], deck.high_ranking_cards
+    assert_equal [@card1, @card3], @deck.high_ranking_cards
   end
 
   def test_percent_high_ranking
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-
-    assert_equal 66.67, deck.percent_high_ranking
+    assert_equal 66.67, @deck.percent_high_ranking
   end
 
   def test_remove_card
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-    deck.remove_card
+    @deck.remove_card
 
-    assert_equal [card2, card3], deck.cards
+    assert_equal [@card2, @card3], @deck.cards
   end
 
   def test_add_card
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card2, card3]
-    deck = Deck.new(cards)
-    card1 = Card.new(:diamond, 'Queen', 12)
-    deck.add_card(card1)
+    card4 = Card.new(:club, '5', 5)
+    @deck.add_card(card4)
 
-    assert_equal [card2, card3, card1], deck.cards
+    assert_equal [@card1, @card2, @card3, card4], @deck.cards
   end
 end
--- test/player_test.rb
+++ test/player_test.rb
@@ -6,55 +6,34 @@ require './lib/player'
 
 class PlayerTest < Minitest::Test
   def setup
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-    player = Player.new('Clarisa', deck)
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @cards = [@card1, @card2, @card3]
+    @deck = Deck.new(@cards)
+    @player = Player.new('Clarisa', @deck)
   end
 
   def test_it_exists
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-    player = Player.new('Clarisa', deck)
-
-    assert_instance_of Card, card1
-    assert_instance_of Card, card2
-    assert_instance_of Card, card3
-    assert_instance_of Deck, deck
-    assert_instance_of Player, player
+    assert_instance_of Card, @card1
+    assert_instance_of Card, @card2
+    assert_instance_of Card, @card3
+    assert_instance_of Deck, @deck
+    assert_instance_of Player, @player
   end
 
   def test_it_has_readable_attributes
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-    player = Player.new('Clarisa', deck)
-
-    assert_equal "Clarisa", player.name
-    assert_equal deck, player.deck
+    assert_equal "Clarisa", @player.name
+    assert_equal @deck, @player.deck
   end
 
   def test_has_lost?
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-    player = Player.new('Clarisa', deck)
-
-    assert_equal false, player.has_lost?
+    assert_equal false, @player.has_lost?
 
     3.times do
-      player.deck.remove_card
+      @player.deck.remove_card
     end
-    assert_equal true, player.has_lost?
+    assert_equal true, @player.has_lost?
 
   end
 end
