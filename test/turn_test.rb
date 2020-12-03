edb07d6ad32e07802b983e601bbe748e3ef1ed23
Katy La Tour <klatour324@gmail.com>
2020-12-02 23:38:05 -0600

    Create tests that returns winner for war type and no winner for mutually assured destruction type

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -87,4 +87,37 @@ class TurnTest < Minitest::Test
     assert_equal player1, turn.winner
   end
 
+  def test_it_returns_winner_of_war_type_who_has_a_higher_rank_of_card_at_2
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:club, '10', 10)
+    card3 = Card.new(:spade, '4', 4)
+    card4 = Card.new(:heart, 'Jack', 11)
+
+    deck1 = Deck.new([card1, card2, card3])
+    deck2 = Deck.new([card1, card3, card4])
+
+    player1 = Player.new('Clarissa', deck1)
+    player2 = Player.new('Tracy', deck2)
+
+    turn = Turn.new(player1, player2)
+
+    assert_equal player2, turn.winner
+  end
+
+  def test_it_returns_no_winner_for_mutually_assured_destruction_type
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:club, '10', 10)
+    card3 = Card.new(:spade, '4', 4)
+    card4 = Card.new(:heart, 'Jack', 11)
+
+    deck1 = Deck.new([card1, card2, card3])
+    deck2 = Deck.new([card1, card4, card3])
+
+    player1 = Player.new('Clarissa', deck1)
+    player2 = Player.new('Tracy', deck2)
+
+    turn = Turn.new(player1, player2)
+
+    assert_equal "No Winner", turn.winner
+  end
 end
