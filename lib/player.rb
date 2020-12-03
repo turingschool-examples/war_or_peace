c5be040914ceddf3311f22a679d6930470629d78
Harrison Blake <harrison.r.blake@gmail.com>
2020-12-02 21:38:11 -0700

    Finished Player name method, test_name now passes in player_test

--- lib/player.rb
+++ lib/player.rb
@@ -2,9 +2,11 @@ require './lib/card'
 require './lib/deck'
 
 class Player
-  attr_reader :deck
+  attr_reader :name, :deck
 
-  def initialize(deck)
+  def initialize(name, deck)
     @deck = deck
+    @name = name
   end
+
 end
--- test/player_test.rb
+++ test/player_test.rb
@@ -5,14 +5,16 @@ require './lib/deck'
 require './lib/player'
 
 class PlayerTest < Minitest::Test
-  def test_it_exists
+  def setup
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
     deck = Deck.new([card1, card2, card3])
 
     @player = Player.new("Clarisa", deck)
+  end
 
+  def test_it_exists
     assert_instance_of Player, @player
   end
 
