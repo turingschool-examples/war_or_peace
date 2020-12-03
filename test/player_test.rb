ce41d85d598ff765038c83add031f849afd848f4
Jesus Quezada-Guillen <maj3st1@gmail.com>
2020-12-02 20:11:01 -0600

    Add testing for has_lost? method return true for Player class, all tests passing.

--- test/player_test.rb
+++ test/player_test.rb
@@ -32,4 +32,19 @@ class PlayerTest < Minitest::Test
     player = Player.new('Clarisa', deck)
     assert_equal false, player.has_lost?
   end
+
+  def test_it_can_lose_the_game
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+    player.deck.remove_card
+    assert_equal false, player.has_lost?
+    player.deck.remove_card
+    assert_equal false, player.has_lost?
+    player.deck.remove_card
+    assert player.has_lost?
+    assert_equal deck, player.deck
+  end
 end
