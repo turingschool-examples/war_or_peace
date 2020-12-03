90b9a02f245591c9cdbda8a7e418f5a40b18c2a4
Alex Schwartz <aschwartz@detaso.com>
2020-12-03 09:43:50 -0600

    Create type method for turn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,4 +8,34 @@ class Turn
     @player2 = player2
     @spoils_of_war = []
   end
+
+  def type
+    if mutually_assured_destruction?
+      :mutually_assured_destruction
+    elsif war?
+      :war
+    elsif basic?
+      :basic
+    else
+      # ??
+    end
+  end
+
+  private
+
+  def basic?
+    @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
+  end
+
+  def war?
+    @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
+  end
+
+  def mutually_assured_destruction?
+    if @player1.deck.cards.length > 2 && @player2.deck.cards.length > 2
+      @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
+    else
+      false
+    end
+  end
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -1,3 +1,8 @@
+# TODO (alex schwartz): pull out the card types in the turn tests to the setup method
+#   - replace the card[1..6] names w/ them
+#   - update any methods that use cardX or deck objs and make them use the new cards
+#     (may have to manually set up the deck in each test, which hopefully isn't a problem)
+#
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/deck'
@@ -7,16 +12,18 @@ require './lib/turn'
 
 class TurnTest < Minitest::Test
   def setup
-    card1 = Card.new(:heart, 'Jack', 11)
-    card2 = Card.new(:heart, '10', 10)
-    card3 = Card.new(:heart, '9', 9)
-    card4 = Card.new(:diamond, 'Jack', 11)
+    @card1 = Card.new(:heart, 'Jack', 11)
+    @card2 = Card.new(:heart, '10', 10)
+    @card3 = Card.new(:heart, '9', 9)
+    @card4 = Card.new(:diamond, 'Jack', 11)
+    @card5 = Card.new(:heart, '8', 8)
+    @card6 = Card.new(:diamond, 'Queen', 12)
 
-    @deck1 = Deck.new([card1, card2])
-    @deck2 = Deck.new([card3, card4])
+    deck1 = Deck.new([])
+    deck2 = Deck.new([])
 
-    @player1 = Player.new('Alex', @deck1)
-    @player2 = Player.new('Bob', @deck2)
+    @player1 = Player.new('Alex', deck1)
+    @player2 = Player.new('Bob', deck2)
 
     @turn = Turn.new(@player1, @player2)
   end
@@ -34,15 +41,55 @@ class TurnTest < Minitest::Test
     assert_equal [], @turn.spoils_of_war
   end
 
-  def test_has_lost_lose_condition
-    skip
-    @player.deck.cards.clear
-    assert_equal true, @player.has_lost?
+  def test_type_basic
+    # each player's card at index 0 is not the same
+    queen_of_hearts = Card.new(:heart, 'Queen', 12)
+    ten_of_diamonds = Card.new(:diamond, '10', 10)
+
+    margaret_deck = Deck.new([queen_of_hearts])
+    jose_deck = Deck.new([ten_of_diamonds])
+
+    margaret = Player.new('Margaret', margaret_deck)
+    jose = Player.new('Jose', jose_deck)
+
+    basic_turn = Turn.new(margaret, jose)
+
+    assert_equal :basic, basic_turn.type
   end
 
-  def test_has_lost_continue_condition
-    skip
-    # As of data setup, player has NOT lost
-    assert_equal false, @player.has_lost?
+  def test_type_war
+    # each player's card at index 0 is the same
+    queen_of_hearts = Card.new(:heart, 'Queen', 12)
+    queen_of_spades = Card.new(:spade, 'Queen', 12)
+
+    margaret_deck = Deck.new([queen_of_hearts])
+    jose_deck = Deck.new([queen_of_spades])
+
+    margaret = Player.new('Margaret', margaret_deck)
+    jose = Player.new('Jose', jose_deck)
+
+    war_turn = Turn.new(margaret, jose)
+
+    assert_equal :war, war_turn.type
+  end
+
+  def test_type_mutually_assured_destruction
+    # each player's card at index 0 is the same and their cards at index 2 are also the same
+    queen_of_hearts = Card.new(:heart, 'Queen', 12)
+    queen_of_diamonds = Card.new(:diamond, 'Queen', 12)
+    queen_of_spades = Card.new(:spade, 'Queen', 12)
+    queen_of_clubs = Card.new(:club, 'Queen', 12)
+    ten_of_diamonds = Card.new(:diamond, '10', 10)
+    five_of_clubs = Card.new(:club, '5', 5)
+
+    margaret_deck = Deck.new([queen_of_hearts, ten_of_diamonds, queen_of_diamonds])
+    jose_deck = Deck.new([queen_of_spades, five_of_clubs, queen_of_clubs])
+
+    margaret = Player.new('Margaret', margaret_deck)
+    jose = Player.new('Jose', jose_deck)
+
+    mutually_assured_destruction_turn = Turn.new(margaret, jose)
+
+    assert_equal :mutually_assured_destruction, mutually_assured_destruction_turn.type
   end
 end
