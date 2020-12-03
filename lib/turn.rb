331c782927448806149184bc5d666a1ab4a1957a
Alexa Morales <alexamsmyth@gmail.com>
2020-12-03 08:11:22 -0500

    Complete turn_type and pass teest_turn_type

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,10 +3,21 @@ class Turn
               :player2,
               :spoils_of_war
 
-  def initialize (player1, player2, spoils_of_war)
+  def initialize (player1, player2)
     @player1 = player1
     @player2 = player2
-    @spoils_of_war = spoils_of_war
+    @spoils_of_war = []
   end
 
+  def type
+    if @player1.deck.cards[0] != @player2.deck.cards[0]
+      :basic
+    else #@player1.deck[0] == @player2.deck[0]
+      if @player1.deck.cards[2] != @player2.deck.cards[2]
+        :war
+      else #@player1.deck[2] == @player2.deck[2]
+        :mutually_assured_destruction
+      end
+    end
+  end
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -8,35 +8,59 @@ require 'pry'
 
 class TurnTest < Minitest::Test
 
-card1 = Card.new(:heart, 'Jack', 11)pry(main)> card2 = Card.new(:heart, '10', 10)
-card3 = Card.new(:heart, '9', 9)
-card4 = Card.new(:diamond, 'Jack', 11)
-card5 = Card.new(:heart, '8', 8)
-card6 = Card.new(:diamond, 'Queen', 12)
-card7 = Card.new(:heart, '3', 3)
-card8 = Card.new(:diamond, '2', 2)
-deck1 = Deck.new([card1, card2, card5, card8])
-deck2 = Deck.new([card3, card4, card6, card7])
-player1 = Player.new("Megan", deck1)
-player2 = Player.new("Aurora", deck2)
+  def test_it_exists
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
 
-def test_it_exists
-  #:play1 :play2 :spoils_of_war
-end
+    turn = Turn.new(player1, player2)
+    assert_instance_of Turn, turn
+  end
 
-def test_type
-  #basic, #war, #mutually_assured_destruction
-end
 
-def test_winner
-  #return the winner of the turn
-end
+  def test_type
+    #Megan's cards
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card5 = Card.new(:heart, '8', 8)
+    card8 = Card.new(:diamond, '2', 2)
+    #Aurora's cards
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
 
-def test_pile_cards
-  #cards are sent from player's decks into spoils of war.
-end
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
 
-def test_award_spoils
-  #add cards in @spoils_of_war to the winner of each turn
+    test = turn.type
+
+    assert_equal :basic, test
+
+    #basic, #war, #mutually_assured_destruction
+  end
+
+  def test_winner
+    #return the winner of the turn
+  end
+
+  def test_pile_cards
+    #cards are sent from player's decks into spoils of war.
+  end
+
+  def test_award_spoils
+    #add cards in @spoils_of_war to the winner of each turn
+  end
 end
-def
