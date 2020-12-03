9417a46d709d1eab6713ff1b0591d96185ddab1d
Alexa Morales <alexamsmyth@gmail.com>
2020-12-03 12:46:22 -0500

    Complete and pass turn.winner, Iteration 2

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,13 +11,31 @@ class Turn
 
   def type
     if @player1.deck.cards[0].rank != @player2.deck.cards[0].rank
-      :basic
+        :basic
     else #@player1.deck[0] == @player2.deck[0]
       if @player1.deck.cards[2].rank != @player2.deck.cards[2].rank
         :war
       else #@player1.deck[2] == @player2.deck[2]
         :mutually_assured_destruction
       end
+    end
+  end
+
+  def winner
+    if type == :basic
+      if @player1.deck.cards[0].rank > @player2.deck.cards[0].rank
+        @player1
+      else
+        @player2
+      end
+    elsif type == :war
+      if @player1.deck.cards[2].rank > @player2.deck.cards[2].rank
+        @player1
+      else
+        @player2
+      end
+    else type == :mutually_assured_destruction
+      "No winner"
     end
   end
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -94,8 +94,68 @@ class TurnTest < Minitest::Test
     assert_equal :mutually_assured_destruction, test
   end
 
-  def test_winner
+  def test_winner_basic
+    #Megan's cards
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card5 = Card.new(:heart, '8', 8)
+    card8 = Card.new(:diamond, '2', 2)
+    #Aurora's cards
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    test = turn.winner
     #return the winner of the turn
+    assert_equal player1, test
+  end
+  def test_winner_war
+    #Megan's cards
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card5 = Card.new(:heart, '8', 8)
+    card8 = Card.new(:diamond, '2', 2)
+    #Aurora's cards
+    card3 = Card.new(:spade, 'Jack', 11)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+  def test_winner_mutually_assured_destruction
+    #Megan's cards
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card5 = Card.new(:heart, '8', 8)
+    card8 = Card.new(:diamond, '2', 2)
+    #Aurora's cards
+    card3 = Card.new(:spade, 'Jack', 11)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card6 = Card.new(:diamond, '8', 8)
+    card7 = Card.new(:heart, '3', 3)
+
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    test = turn.winner
+
+    #return the winner of the turn
+    assert_equal "No winner", test
   end
 
   def test_pile_cards
@@ -106,3 +166,4 @@ class TurnTest < Minitest::Test
     #add cards in @spoils_of_war to the winner of each turn
   end
 end
+end 
