3f15ae8ea01ebed12a716661605b7ee695a9540b
Kris Litman <duguy8@gmail.com>
2020-12-02 20:44:27 -0500

    Halfway through Iteration 2

--- test/player_test.rb
+++ test/player_test.rb
@@ -6,29 +6,69 @@ require './lib/player'
 
 class PlayerTest < Minitest::Test
   def test_there_can_be_a_player
+    # skip
     player = Player.new("Clarisa")
 
     assert_instance_of Player, player
   end
 
   def test_player_can_have_a_name
-    #skip
+    # skip
     player = Player.new("Clarisa")
 
     assert_equal "Clarisa", player.name
   end
 
-  def test_player_can_have_a_deck
-    #skip
-    player = Player.new("Clarisa")
+  def test_player_starts_with_a_deck
+    #want the player to have a deck
+    # skip
+    deck = Deck.new
+    player = Player.new("Clarisa", deck)
+    assert_instance_of Deck, player.deck
+  end
+
+  def test_can_add_cards_to_the_deck
 
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
-    deck = Deck.new
-    deck.add_card(card1)
-    deck.add_card(card2)
-    deck.add_card(card3)
-    assert_equal 3, player.deck.length
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+    assert_equal 3, player.deck.cards.length
+  end
+
+  def test_to_remove_a_card
+
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+    player.deck.remove_card
+    assert_equal 2, player.deck.cards.length
+  end
+
+  def test_if_you_have_lost
+
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+    player.deck.remove_card
+    refute player.has_lost?
+  end
+
+  def test_that_you_lose_with_no_cards_left
+
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+    player.deck.remove_card
+    player.deck.remove_card
+    player.deck.remove_card
+    assert player.has_lost?
   end
 end
