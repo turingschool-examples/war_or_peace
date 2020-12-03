d33da22612a65bf0cc5c5a362cd5d9fb0dc54153
Jesus Quezada-Guillen <maj3st1@gmail.com>
2020-12-02 22:54:42 -0600

    Add instance variables for attributes accordingly.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,8 +1,11 @@
 class Turn
-  attr_reader :player1, :player2
+  attr_reader :player1,
+              :player2,
+              :spoils_of_war
 
   def initialize(player1_obj, player2_obj)
     @player1 = player1_obj
     @player2 = player2_obj
+    @spoils_of_war = []
   end
 end
