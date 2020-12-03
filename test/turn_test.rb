d8ca8762e3cc3d09dd9372413a60ced648a05d57
Joseph Budina <joebud3@gmail.com>
2020-12-03 08:10:29 -0700

    Add war winner test

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -63,4 +63,27 @@ class TurnTest < Minitest::Test
 
     assert_equal :war, turn.type
   end
+  
+  def test_which_player_wins_turn
+    deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    deck2 = Deck.new([@card4, @card3, @card6, @card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    winner = turn.winner
+
+    assert_equal player2, winner
+  end
+
+  # def test_if_it_piles_cards_in_spoils_of_war_type_war
+  #   deck1 = Deck.new([@card1, @card2, @card5, @card8])
+  #   deck2 = Deck.new([@card4, @card3, @card6, @card7])
+  #   player1 = Player.new("Megan", deck1)
+  #   player2 = Player.new("Aurora", deck2)
+  #   turn = Turn.new(player1, player2)
+  #   winner = turn.winner
+
+  #   turn.pile_cards
+  #   assert_equal [@card1,@card3], @turn.spoils_of_war
+  # end
 end
