5bc6c175a76e7ffe299c370176a3f3c3a07f9427
Harrison Blake <harrison.r.blake@gmail.com>
2020-12-02 21:03:02 -0700

    Framed player class and test_it_exists passes for player_test

---
+++ test/player_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+
+class PlayerTest < Minitest::Test
+  def test_it_exists
+    player = Player.new
+
+    assert_instance_of Player, player
+  end
+end
