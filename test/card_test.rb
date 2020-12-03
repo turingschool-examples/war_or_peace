21f342bef7389ef955c601f9c046705406f16f6c
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2020-12-02 20:44:24 -0700

    Added deck class and deck test files

--- test/card_test.rb
+++ test/card_test.rb
@@ -16,8 +16,8 @@ class CardTest < Minitest::Test
   def test_it_has_readable_attributes
     card = Card.new(:diamond, 'Queen', 12)
 
-    assert_instance_of :diamond, card.suit
-    assert_instance_of 'Queen', card.value
-    assert_instance_of 12, card.rank
+    assert :diamond, card.suit
+    assert 'Queen', card.value
+    assert 12, card.rank
   end
 end
