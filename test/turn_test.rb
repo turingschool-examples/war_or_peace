4332accb6c9fd0896f37c2a673ec3843db115441
Katy La Tour <klatour324@gmail.com>
2020-12-02 18:18:20 -0600

    Create setup method and test method for readable attributes

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -6,7 +6,8 @@ require './lib/player'
 require './lib/turn'
 
 class TurnTest < Minitest::Test
-  def test_a_turn_exists
+
+  def setup
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
     card3 = Card.new(:heart, '9', 9)
@@ -19,11 +20,19 @@ class TurnTest < Minitest::Test
     deck1 = Deck.new([card1, card2, card5, card8])
     deck2 = Deck.new([card3, card4, card6, card7])
 
-    player1 = Player.new('Clarissa', deck1)
-    player2 = Player.new('Tracy', deck2)
+    @player1 = Player.new('Clarissa', deck1)
+    @player2 = Player.new('Tracy', deck2)
 
-    turn = Turn.new(player1, player2)
+    @turn = Turn.new(@player1, @player2)
+  end
 
-    assert_instance_of Turn, turn
+  def test_a_turn_exists
+    assert_instance_of Turn, @turn
+  end
+
+  def test_it_has_readable_attributes
+    assert_equal @player1, @turn.player1
+    assert_equal @player2, @turn.player2
+    assert_equal [], @turn.spoils_of_war
   end
 end
