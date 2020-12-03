7340dd6d5b1ce6e37db6306fc6d9e9d86a12def5
Joseph Budina <joebud3@gmail.com>
2020-12-02 21:04:06 -0700

    Rework code

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,44 +11,40 @@ class Turn
 
   def type 
     if (
-      @player1.deck.rank_of_card_at(0) != 
-      @player2.deck.rank_of_card_at(0)
-    )
-      :basic
-    elsif (
-      @player1.deck.rank_of_card_at(0) == 
-      @player2.deck.rank_of_card_at(0)
-    )
-      :war 
-    elsif (
       @player1.deck.rank_of_card_at(0) == 
       @player2.deck.rank_of_card_at(0) && 
       @player1.deck.rank_of_card_at(2) == 
       @player2.deck.rank_of_card_at(2)
     )
-      :mutually_assured_destruction 
+      :mutually_assured_destruction
+    elsif (
+      @player1.deck.rank_of_card_at(0) == 
+      @player2.deck.rank_of_card_at(0)
+    )
+      :war 
+    elsif(
+        @player1.deck.rank_of_card_at(0) != 
+        @player2.deck.rank_of_card_at(0)
+      )
+        :basic
     end
   end
 
   def winner
-    if type == :basic && (
-      @player1.deck.rank_of_card_at(0) >
-      @player2.deck.rank_of_card_at(0)
-    )
-      @player1
+    if type == :mutually_assured_destruction
+       return 'No Winner'
+    elsif type == :war && @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
+        @player1
     elsif
-      @player2
-    elsif type == :war && (
-      @player1.deck.rank_of_card_at(2) >
-      @player2.deck.rank_of_card_at(2)
-    )
-      @player1
-    elsif
-      @player2
-    elsif type == :mutually_assured_destruction
-      p "No Winner"
+        @player2
+      end
+    if type == :basic && @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
+        @player1
+    else
+        @player2
     end
   end
+  
 
   def pile_cards
     if type == :mutually_assured_destruction && winner == "No Winner"
