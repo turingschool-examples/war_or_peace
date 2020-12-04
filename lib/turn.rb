1147ec9f2f0c6f513b6462113d962463ab0c930a
Wil McCauley <wmccauley724@gmail.com>
2020-12-03 19:22:44 -0700

    Complete tests and methods through pile_cards

--- lib/turn.rb
+++ lib/turn.rb
@@ -25,4 +25,50 @@ class Turn
       :basic
     end
   end
+
+  def rank_get(player, index)
+    player.deck.rank_of_card_at(index)
+  end
+
+  def winner
+    if type == :basic
+      if rank_get(@player1, 0) > rank_get(@player2, 0)
+        @player1
+      else
+        @player2
+      end
+    elsif type == :war
+      if rank_get(@player1, 2) > rank_get(player2, 2)
+        @player1
+      else
+        @player2
+      end
+    elsif type == :mutually_assured_destruction
+      'No Winner'
+    end
+  end
+
+  #refactor to .map? Might not work
+  def pile_cards
+    players = [@player1, @player2]
+    if type == :basic
+      players.each do |player|
+        @spoils_of_war << player.deck.cards[0]
+        player.deck.remove_card
+      end
+    elsif type == :war
+      players.each do |player|
+        @spoils_of_war << player.deck.cards[0..2]
+        3.times do
+          player.deck.remove_card
+        end
+      end
+    elsif type == :mutually_assured_destruction
+      players.each do |player|
+        3.times do
+          player.deck.remove_card
+        end
+      end
+    end
+  end
 end
