d5cb29bfdbc7f44c113d50fdea709a07ca12b4a7
Arika Turing <ablenker@gmail.com>
2020-12-03 19:06:05 -0700

    Pass first turn test

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -7,6 +7,12 @@ require './lib/turn'
 
 class TurnTest < Minitest::Test
   def test_it_exists
-    
+    deck1 = Deck.new([])
+    deck2 = Deck.new([])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_instance_of Turn, turn
   end
 end
