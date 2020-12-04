b98d12043fdf3dd832edff787da981c2b26aa296
Caryn Keffer <carynkeffer@gmail.com>
2020-12-04 01:14:25 -0500

    Work on pile_cards method

---
+++ lib/turn.rb
@@ -0,0 +1,102 @@
+class Turn
+  attr_reader :player1,
+              :player2,
+              :spoils_of_war
+  def initialize(player1, player2)
+    @player1       = player1
+    @player2       = player2
+    @spoils_of_war = []
+  end
+
+  def card_rank(player, index)
+    player.deck.rank_of_card_at(index)
+  end
+
+  def type_basic?
+    if card_rank(player1, 0) != card_rank(player2, 0)
+       true
+    end
+  end
+
+  def type_mutually_assured?
+    if ((card_rank(player1, 0) == card_rank(player2, 0)) &&
+       (card_rank(player1, 2) == card_rank(player2, 2)))
+       true
+    end
+  end
+
+  def type_war?
+    if card_rank(player1, 0) == card_rank(player2, 0)
+       true
+    end
+  end
+
+  def type
+    if type_basic? == true
+      :basic
+    elsif type_mutually_assured? == true
+      :mutually_assured_destruction
+    else
+      :war
+    end
+  end
+
+  def compare_ranks_at_0
+    highest_rank_at_0 = []
+    highest_rank_at_0 << (card_rank(player1, 0))
+    highest_rank_at_0 << (card_rank(player2, 0))
+  end
+
+  def compare_ranks_at_2
+    highest_rank_at_2 = []
+    highest_rank_at_2 << (card_rank(player1, 2))
+    highest_rank_at_2 << (card_rank(player2, 2))
+  end
+
+  def winner_at_0
+    if (compare_ranks_at_0[0] > compare_ranks_at_0[1]) == true
+      player1
+    else (compare_ranks_at_0[0] < compare_ranks_at_0[1]) == true
+      player2
+    end
+  end
+
+  def winner_at_2
+    if (compare_ranks_at_2[0] > compare_ranks_at_2[1]) == true
+      player1
+    else (compare_ranks_at_2[0] < compare_ranks_at_2[1]) == true
+      player2
+    end
+  end
+
+  def winner
+    if (type == :basic)
+      winner_at_0
+    elsif (type == :war)
+      winner_at_2
+    else (type == :mutually_assured)
+      "No Winner"
+    end
+  end
+
+  def pile_cards
+    if (type == :basic)
+      spoils_of_war << player1.deck.remove_card
+      spoils_of_war << player2.deck.remove_card
+    elsif (type == :war)
+      spoils_of_war << player1.deck.remove_card
+      spoils_of_war << player2.deck.remove_card
+      spoils_of_war << player1.deck.remove_card
+      spoils_of_war << player2.deck.remove_card
+      spoils_of_war << player1.deck.remove_card
+      spoils_of_war << player2.deck.remove_card
+    else (type == :mutually_assured_destruction)
+      player1.deck.remove_card
+      player2.deck.remove_card
+      player1.deck.remove_card
+      player2.deck.remove_card
+      player1.deck.remove_card
+      player2.deck.remove_card
+    end
+  end
+end
---
+++ test/turn_test.rb
@@ -0,0 +1,265 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+
+class TurnTest < Minitest::Test
+  def test_it_exists_and_has_attributes
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+
+    assert_instance_of Turn, turn
+    assert_equal player1, turn.player1
+    assert_equal player2, turn.player2
+    assert_equal [], turn.spoils_of_war
+  end
+
+  def test_player_has_card_rank
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+
+    assert_equal 11, turn.card_rank(player1, 0)
+    assert_equal 8, turn.card_rank(player1, 2)
+  end
+
+  def test_turn_type_is_basic
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+
+    assert_equal true, turn.type_basic?
+    assert_equal :basic, turn.type
+  end
+
+  def test_turn_type_is_war
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+
+    assert_equal true, turn.type_war?
+    assert_equal :war, turn.type
+  end
+
+  def test_turn_type_is_mutually_assured
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, '8', 8)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+
+    assert_equal true, turn.type_mutually_assured?
+    assert_equal :mutually_assured_destruction, turn.type
+  end
+
+  def test_basic_turn_has_winner
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+    winner = turn.winner
+
+    assert_equal [11, 9], turn.compare_ranks_at_0
+    assert_equal player1, turn.winner_at_0
+    assert_equal player1, turn.winner
+  end
+
+  def test_war_type_has_winner
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+    winner = turn.winner
+
+    assert_equal [8, 12], turn.compare_ranks_at_2
+    assert_equal player2, turn.winner_at_2
+    assert_equal player2, turn.winner
+  end
+
+  def test_mutually_assured_type_has_winner
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, '8', 8)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+    winner = turn.winner
+
+    assert_equal "No Winner", turn.winner
+  end
+
+  def test_can_pile_cards_for_basic_type
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+    winner = turn.winner
+
+    turn.pile_cards
+
+    assert_equal [card1, card3], turn.spoils_of_war
+  end
+
+    def test_can_pile_cards_for_war_type
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:heart, '9', 9)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, 'Queen', 12)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card4, card3, card6, card7])
+
+      player1 = Player.new("Megan", deck1)
+      player2 = Player.new("Aurora", deck2)
+
+      turn = Turn.new(player1, player2)
+      winner = turn.winner
+
+      turn.pile_cards
+
+      assert_equal [card1, card4, card2, card3, card5, card6], turn.spoils_of_war
+    end
+
+    def test_can_pile_cards_for_mutually_assured_type
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:heart, '9', 9)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, '8', 8)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card4, card3, card6, card7])
+
+      player1 = Player.new("Megan", deck1)
+      player2 = Player.new("Aurora", deck2)
+
+      turn = Turn.new(player1, player2)
+      winner = turn.winner
+
+
+      turn.pile_cards
+
+      assert_equal [], turn.spoils_of_war
+      assert_equal deck1, player1.deck
+      assert_equal deck2, player2.deck
+    end
+end
