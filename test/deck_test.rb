b88cd12503074daca6fdbed4caef8693ca3d52cc
Angel Breaux <abreaux@detaso.com>
2020-12-03 20:13:59 -0600

    Refactor deck test file

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -16,12 +16,10 @@ class DeckTest < Minitest::Test
 
   def test_it_exists
     assert_instance_of Deck, @deck
-    assert_instance_of Deck, @deck2
   end
 
   def test_it_has_readable_attributes
     assert_equal [@card1, @card2, @card3], @deck.cards
-    assert_equal [@card2, @card3], @deck2.cards
   end
 
   def test_rank_of_card_at
@@ -31,7 +29,6 @@ class DeckTest < Minitest::Test
 
   def test_high_ranking_cards
     assert_equal [@card1, @card3], @deck.high_ranking_cards
-    assert_equal [@card3], @deck2.high_ranking_cards
   end
 
   def test_percent_high_ranking
@@ -41,18 +38,14 @@ class DeckTest < Minitest::Test
 
   def test_remove_card
     @deck.remove_card
-    @deck2.remove_card
 
     assert_equal [@card2, @card3], @deck.cards
-    assert_equal [@card3], @deck2.cards
   end
 
   def test_add_card
     card4 = Card.new(:club, '5', 5)
     @deck.add_card(card4)
-    @deck2.add_card(card4)
 
     assert_equal [@card1, @card2, @card3, card4], @deck.cards
-    assert_equal [@card2, @card3, card4], @deck2.cards
   end
 end
