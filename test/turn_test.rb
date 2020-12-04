c34210ba54bd3baa8628175e2753e77c77885396
Arika Turing <ablenker@gmail.com>
2020-12-03 18:48:23 -0700

    Start turn

---
+++ test/turn_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+
+class TurnTest < Minitest::Test
+  def test_it_exists
+    
+  end
+end
