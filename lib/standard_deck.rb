7c7d362c070fddd3eb3dd07718409b378ec281a0
Angel Breaux <abreaux@detaso.com>
2020-12-04 12:16:29 -0600

    Refactor standard_deck

--- lib/standard_deck.rb
+++ lib/standard_deck.rb
@@ -8,14 +8,16 @@ class StandardDeck
   def initialize
     @cards = []
     @suits = [:heart, :diamond, :spade, :club]
-    @values_ranks = {values: ['2','3','4','5','6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'],
+    @values_and_ranks = {values: ['2','3','4','5','6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'],
                     ranks: [2, 3, 4, 5, 6, 7 ,8, 9, 10, 11, 12, 13, 14]}
   end
 
   def create_standard_deck
     @suits.each do |suit|
       13.times do |num|
-        add_card(Card.new(suit.to_sym, @values_ranks[:values].at(num), @values_ranks[:ranks].at(num)))
+        value = @values_and_ranks[:values].at(num)
+        rank = @values_and_ranks[:ranks].at(num)
+        add_card(Card.new(suit.to_sym, value, rank))
       end
     end
   end
