fb6b77bb50f1c2eed19ee4f21aee0e93d42f5ec4
Trevor Suter <Trevorsuter@icloud.com>
2020-12-03 19:49:08 -0700

    add two decks and two players

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -5,7 +5,7 @@ require './lib/turn'
 
 suits = [:hearts, :diamonds, :spades, :clubs]
 
-value_with_rank = {'2': 2, '3': 3, '4': 4,'5': 5, '6': 6, '7': 7,
+value_with_rank = { '2': 2, '3': 3, '4': 4,'5': 5, '6': 6, '7': 7,
 '8': 8, '9': 9, '10': 10, 'Jack': 11, 'Queen': 12, 'King': 13,
 'Ace': 14 }
 
@@ -18,3 +18,16 @@ suits.map do |suit|
 end
 
 #p cards
+
+cards.shuffle!
+
+deck1 = cards.shift(26)
+deck2 = cards.shift(26)
+
+player1 = Player.new("Dwight", deck1)
+player2 = Player.new("Jim", deck2)
+
+p player1
+p player1.deck.count
+p player2
+p player2.deck.count
