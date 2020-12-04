b7015c877ece742bb0e38f9463fb5aa891a8152f
Jesus Quezada-Guillen <maj3st1@gmail.com>
2020-12-03 19:50:52 -0600

    Add winner method to Turn class for :basic turn, all test pass.

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,4 +14,13 @@ class Turn
       :basic
     end
   end
+
+  def winner
+    if type == :basic && @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
+      @player1
+    else
+      @player2
+    end
+
+  end
 end
