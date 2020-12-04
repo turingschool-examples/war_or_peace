462c97af985587d0b8c8fbd75cbbdf5567447038
Angel Breaux <abreaux@detaso.com>
2020-12-03 20:18:43 -0600

    Update runner

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -1,34 +1,5 @@
-require './lib/standard_deck'
-require './lib/deck'
-require './lib/player'
-require './lib/turn'
+require './lib/game'
 
-def split_deck
-  cards = StandardDeck.new
-  shuffled_deck = cards.create_standard_deck.shuffle
-  @deck1 = Deck.new(shuffled_deck[0..25])
-  @deck2 = Deck.new(shuffled_deck[26..52])
-end
+new_game = Game.new
 
-def create_two_players
-  split_deck
-  @player1 = Player.new("Angel", @deck1)
-  @player2 = Player.new("Chris", @deck2)
-end
-
-def take_turn
-  create_two_players
-  @turns << Turn.new(@player1, @player2)
-end
-
-def start
-  puts "Welcome to War! (or Peace) This game will be played with 52 cards."
-  puts "The players today are #{@player1.name} and #{@player2.name}."
-  puts "Type 'GO' to start the game!"
-  50.times do
-    print "-"
-  end
-end
-
-create_two_players
-start
+new_game.start
