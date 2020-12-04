99a64371d088c6052b212a6e5c5d1664eb749dab
Katy La Tour <klatour324@gmail.com>
2020-12-03 20:18:37 -0600

    Create test for pile cards method and war type of turn

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -200,4 +200,29 @@ class TurnTest < Minitest::Test
     assert_equal [card2, card5, card8], player1.deck.cards
     assert_equal [card4, card6, card7], player2.deck.cards
   end
+
+  def test_when_it_is_a_war_type_each_player_sends_three_cards_from_top_of_deck_into_spoils_pile
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:heart, '9', 9)
+
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+
+    player1 = Player.new('Clarissa', deck1)
+    player2 = Player.new('Tracy', deck2)
+
+    turn = Turn.new(player1, player2)
+
+    turn.pile_cards
+
+    assert_equal [card1, card4, card2, card3, card5, card6], turn.spoils_of_war
+    assert_equal [card8], player1.deck.cards
+    assert_equal [card7], player2.deck.cards
+  end
 end
