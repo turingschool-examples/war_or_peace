c6e31c3c77c5560c02f93f037f271db0cf219a13
Angel Breaux <abreaux@detaso.com>
2020-12-04 14:29:59 -0600

    Update turn to only call type method once

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,6 +10,10 @@ class Turn
   end
 
   def type
+    @type ||= find_type
+  end
+  
+  def find_type
     if @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
       :mutually_assured_destruction
     elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
