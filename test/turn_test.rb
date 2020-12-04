e8e609ebb95078806a68a6cade0eff5f65c0a2a3
Katy La Tour <klatour324@gmail.com>
2020-12-03 20:54:12 -0600

    Create test to remove top three cards from players decks for mutually assured destruction type

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -225,4 +225,29 @@ class TurnTest < Minitest::Test
     assert_equal [card8], player1.deck.cards
     assert_equal [card7], player2.deck.cards
   end
+
+  def test_when_it_is_mutually_assured_destruction_type_removes_top_three_cards_from_players_decks
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:heart, '9', 9)
+
+    deck1 = Deck.new([card1, card2, card8, card5])
+    deck2 = Deck.new([card4, card6, card3, card7])
+
+    player1 = Player.new('Clarissa', deck1)
+    player2 = Player.new('Tracy', deck2)
+
+    turn = Turn.new(player1, player2)
+
+    turn.pile_cards
+
+    assert_equal [], turn.spoils_of_war
+    assert_equal [card5], player1.deck.cards
+    assert_equal [card7], player2.deck.cards
+  end
 end
