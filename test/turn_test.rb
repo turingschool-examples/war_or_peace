2cd608d4a95eb1aea4829dd39866e31f5fbd1c50
Katy La Tour <klatour324@gmail.com>
2020-12-03 22:44:39 -0600

    Create test for award spoils for a basic type of turn

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -250,4 +250,36 @@ class TurnTest < Minitest::Test
     assert_equal [card5], player1.deck.cards
     assert_equal [card7], player2.deck.cards
   end
+
+  def test_it_will_award_spoils_cards_from_basic_turn_to_winner
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:heart, '9', 9)
+
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+
+    player1 = Player.new('Clarissa', deck1)
+    player2 = Player.new('Tracy', deck2)
+
+    turn = Turn.new(player1, player2)
+
+    turn.pile_cards
+
+    winner = turn.winner
+
+    turn.award_spoils(winner)
+
+    assert_equal [], turn.spoils_of_war
+    assert winner.deck.cards.include?(card4)
+    assert winner.deck.cards.include?(card6)
+    assert winner.deck.cards.include?(card7)
+    assert winner.deck.cards.include?(card1)
+    assert winner.deck.cards.include?(card3)
+  end
 end
