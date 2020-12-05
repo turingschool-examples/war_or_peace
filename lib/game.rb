7969aaff37f996d2efd7b4f00ac73cabd7e6d5c7
Alexa Morales <alexamsmyth@gmail.com>
2020-12-05 11:21:10 -0500

    Revise game class

--- lib/game.rb
+++ lib/game.rb
@@ -9,27 +9,31 @@ class Game
   end
 
   def welcome
-    p "Welcome to War! (or Peace) This game will be played with 52 cards.
-The players today are Megan and Aurora.
-Type 'GO' to start the game!
-------------------------------------------------------------------"
+    # p "Welcome to War! (or Peace) This game will be played with 52 cards.
+    #   The players today are Megan and Aurora.
+    #   Type 'GO' to start the game!
+    #   ------------------------------------------------------------------"
+    # p "=> (type 'GO' when you are ready for WAR!)"
+    # p gets.chomp
   end
+end 
 
-  def game_winner
-    if turns.player1.deck.cards  == [] && turns.player2.deck.cards == []
-      p "This is mathmatically near impossible, you have accomplished something rare! It is impressive, but everyone LOSES."
-    elsif turns.player1.deck.cards  == []
-      p "*~*~*~* Aurora has won the game! *~*~*~*"
-    elsif turns.player2.deck.cards == []
-      p "*~*~*~* Megan has won the game! *~*~*~*"
-    elsif
-      Turn.type
-      Turn.pile_cards
-      Turn.award_spoils(player)
-    end
-  end
+#   def winner
+#     # @turns.each do |turn|
+#     if turns.player1.deck.cards  == [] && turns.player2.deck.cards == []
+#       p "This is mathmatically near impossible, you have accomplished something rare! It is impressive, but everyone LOSES."
+#     elsif turns.player1.deck.cards  == []
+#       p "*~*~*~* Aurora has won the game! *~*~*~*"
+#     elsif turns.player2.deck.cards == []
+#       p "*~*~*~* Megan has won the game! *~*~*~*"
+#     # elsif
+#       # Turn.type
+#       # Turn.pile_cards
+#       # Turn.award_spoils(player)
+#     end
+#   end
+# end
 
-end
 
   # if    turn.player1.deck == []
       #   "*~*~*~* Aurora has won the game! *~*~*~*"
