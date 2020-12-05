5acb3de7b5491a1b1834a6474625a7caba42a64c
Wil McCauley <wmccauley724@gmail.com>
2020-12-05 12:26:40 -0700

    Continue work on deck logic and loops

--- lib/war_or_peace_runner.rb
+++ lib/war_or_peace_runner.rb
@@ -1,9 +1,11 @@
+require_relative 'card'
+
 
 game_deck = []
 
 suits = [:diamond, :heart, :spade, :club]
 ranks = [1,2,3,4,5,6,7,8,9,10,11,12,13]
-face_values = ['Jack', 'Queen', King]
+face_values = ['Jack', 'Queen', 'King']
 
 def normalize_value(rank)
   if rank == 1
@@ -11,15 +13,14 @@ def normalize_value(rank)
   elsif rank < 11
     rank.to_s
   else
-    rank 
-
+    require "pry"; binding.pry
+    face_values[rank - 11]
+  end
 end
 
 suits.each do |suit|
   ranks.each do |rank|
-
+    card = Card.new(suit, normalize_value(rank), rank)
+    game_deck << card
   end
 end
-
-
-# card = Card.new(suit, "#{value}", rank)
