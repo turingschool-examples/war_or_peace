e072ccb500ef548c1965c7808d94ce2975b34ebc
Alexa Morales <alexamsmyth@gmail.com>
2020-12-05 09:09:02 -0500

    Add play method to game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,36 @@
+require 'pry'
+
+
 class Game
   attr_reader :turns
 
   def initialize (turns)
     @turns = turns
   end
-end 
+
+
+
+  def play
+    if turns.player1.deck.cards  == []
+      p "*~*~*~* Aurora has won the game! *~*~*~*"
+    elsif turns.player2.deck.cards == []
+      p "*~*~*~* Megan has won the game! *~*~*~*"
+    elsif
+      Turn.type
+      Turn.pile_cards
+      Turn.award_spoils(player)
+    end
+  end
+
+end
+
+  # if    turn.player1.deck == []
+      #   "*~*~*~* Aurora has won the game! *~*~*~*"
+      # elsif turn.player2.deck == []
+      #   "*~*~*~* Aurora has won the game! *~*~*~*"
+      # # elsif
+      # #  determine type, determine winner, pile cards, award spoils, play another turn
+#       end
+#     end
+#   end
+# end
--- test/game_test.rb
+++ test/game_test.rb
@@ -25,5 +25,85 @@ class GameTest < Minitest::Test
 
     turn = Turn.new(player1, player2)
     game = Game.new(turn)
-    assert_instance_of Game, game 
+    assert_instance_of Game, game
   end
+
+
+  def test_play_continue
+    skip
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+    game = Game.new(turn)
+    assert_equal "Turn 1: Megan won 2 cards", game.play
+  end
+
+  def test_play_game_player1_won
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+    game = Game.new(turn)
+    assert_equal "*~*~*~* Aurora has won the game! *~*~*~*", game.play
+  end
+  def test_play_game_player2_won
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+    game = Game.new(turn)
+    assert_equal "*~*~*~* Megan has won the game! *~*~*~*", game.play
+  end
+
+  def test_play_game_continue_play
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+    game = Game.new(turn)
+    test = player1.deck
+    assert_equal [card2, card5, card8, card1, card3], test
+  end
+
+end
