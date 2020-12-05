f3bef98ea8f9bd42e1771c0f1d4eaa2b1bdd11fc
Diana Buffone <diana.pamelaet@gmail.com>
2020-12-05 00:38:20 -0500

    Add day 5 progress

--- lib/player.rb
+++ lib/player.rb
@@ -4,6 +4,13 @@ class Player
 
   def initialize(name, deck)
     @name = name
-    @deck = []
+    @deck = deck
+  end
+
+  def has_lost?
+    # cards.each do |card|
+    # if card.rank = 14
+    # return false
+    # end
   end
 end
--- test/player_test.rb
+++ test/player_test.rb
@@ -15,4 +15,15 @@ class PlayerTest < Minitest::Test
 
   assert_instance_of Player, player
   end
+
+  def test_has_player_lost
+
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+    
+    assert_equal false, player.has_lost?
+  end
 end
