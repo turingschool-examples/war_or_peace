f697371b47c168d1ddb100b3009fcce815c702a4
Wil McCauley <wmccauley724@gmail.com>
2020-12-05 12:05:30 -0700

    Begin building deck-build logic

--- lib/war_or_peace_runner.rb
+++ lib/war_or_peace_runner.rb
@@ -1,6 +1,25 @@
 
+game_deck = []
 
 suits = [:diamond, :heart, :spade, :club]
+ranks = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+face_values = ['Jack', 'Queen', King]
+
+def normalize_value(rank)
+  if rank == 1
+    'Ace'
+  elsif rank < 11
+    rank.to_s
+  else
+    rank 
+
+end
 
 suits.each do |suit|
-  
+  ranks.each do |rank|
+
+  end
+end
+
+
+# card = Card.new(suit, "#{value}", rank)
