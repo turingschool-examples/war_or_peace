bb096a602f7b2d6f29ea30b9d40fa16ad3adee55
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2020-12-05 16:36:57 -0700

    Test if player is there.

--- test/player_test.rb
+++ test/player_test.rb
@@ -5,3 +5,14 @@ require './lib/deck'
 require './lib/player'
 
 class PlayerTest < Minitest::Test
+    def test_player_is_there
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '5', 5)
+      card3 = Card.new(:club, '8', 8)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      player =Player.new('Clarissa', deck)
+
+      assert_instance_of Player, player
+    end
+end
