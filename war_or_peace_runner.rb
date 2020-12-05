29a93fd968713c1ee8d6544163bc97e674ae3f03
Alexa Morales <alexamsmyth@gmail.com>
2020-12-05 09:48:11 -0500

    Add 52card loop

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -1,6 +1,11 @@
+require './lib/deck'
+require './lib/player'
 require './lib/card'
+require './lib/turn'
+require './lib/game'
 require 'pry'
 
+#deck set up
 standard_deck = []
 suits = [:club, :diamond, :heart, :spade]
 
@@ -19,6 +24,21 @@ suits.map do |suit|
     end
   end
 end
-
-standard_deck
 shuffle_deck = standard_deck.shuffle
+p card_count = shuffle_deck.count / 2
+p shuffle_deck.slice(card_count)
+p shuffle_deck.slice(26)
+
+#game set up
+deck1 = Deck.new([card1, card2, card5, card8])
+deck2 = Deck.new([card3, card4, card6, card7])
+player1 = Player.new("Megan", deck1)
+player2 = Player.new("Aurora", deck2)
+
+turn = Turn.new(player1, player2)
+game = Game.new(turn)
+
+# game = Game.new(turns)
+p game.welcome
+
+gets chomp
