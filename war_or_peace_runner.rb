6277b6be9c284a8ccf576dc29578a2d79688c4e1
Alexa Morales <alexamsmyth@gmail.com>
2020-12-05 16:32:00 -0500

    Complete fully functioning war_game (not loop)

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -43,95 +43,37 @@ deck2 = Deck.new(cards2)
 player1 = Player.new("Megan", deck1)
 player2 = Player.new("Aurora", deck2)
   #Game set up
-turns = Turn.new(player1, player2)
-game = Game.new(turns)
+# turns = Turn.new(player1, player2)
+# game = Game.new(turns)
 
 # game.start
   turn_index = 0
-5.times.map do
-  turn_index +=1
-  winner = game.turns.winner
-  win_name = winner.name
-  turns.pile_cards
-  turns.award_spoils(winner)
-    if turns.type == :basic
-        puts "Turn #{turn_index}: #{win_name} won 2 cards: #{turns.spoils_of_war[0]}. Megan has #{turns.player1.deck.cards.length}. Auroa has #{turns.player2.deck.cards.length}."
-    elsif turns.type == :war
-        puts "Turn #{turn_index}: WAR - #{win_name} won 6 cards"
-    else type == :mutually_assured_destruction
-        puts "Turn #{turn_index}: *mutually assured destruction* 6 cards removed from play"
-    end
+# while turns.player1.deck.cards.empty? == true || turns.player2.deck.cards.empty? == false || turn_index < 10
+  500.times do
+    turn_index +=1
+    turns = Turn.new(player1, player2)
+    game = Game.new(turns)
+
+
+      if turns.type == :basic
+          winner = game.turns.winner
+        turns.pile_cards
+        turns.award_spoils(winner)
+            win_name = winner.name
+          puts "Turn #{turn_index}: #{win_name} won 2 cards. Megan has #{turns.player1.deck.cards.length}. Auroa has #{turns.player2.deck.cards.length}."
+      elsif turns.type == :war
+          winner = game.turns.winner
+        turns.pile_cards
+        turns.award_spoils(winner)
+            win_name = winner.name
+          puts "Turn #{turn_index}: WAR - #{win_name} won 6 cards. Megan has #{turns.player1.deck.cards.length}. Auroa has #{turns.player2.deck.cards.length}."
+      elsif turns.type == :mutually_assured_destruction
+        turns.pile_cards
+        # turns.award_spoils(winner)
+          puts "Turn #{turn_index}: *mutually assured destruction* 6 cards removed from play"
+      end
+    # end
 
     # while turn_index < 10 #|| game.turns.player1.deck.cards == [] || || game.turns.player2.deck.cards != []
     # end
   end
-
-
-  #   winner = game.turns.winner
-  #   win_name = winner.name
-  #   turns.award_spoils(winner)
-  #   turns = Turn.new(player1, player2)
-  #       if type == :basic
-  #         puts "#{win_name} won 2 cards"
-  #       elsif type == :war
-  #         puts "WAR - #{win_name} won 6 cards"
-  #       else type == :mutually_assured_destruction
-  #         puts "*mutually assured destruction* 6 cards removed from play"
-  #       end
-  #   until game.turns.player1.deck.cards != [] || game.turns.player2.deck.cards != []
-  #     break "GAME OVER"
-  #   end
-  # end
-
-
-
-
-#   if game.turns.player1.deck.cards  == [] && turn.player2.deck.cards == []
-#     p "This is mathmatically near impossible, you have accomplished something rare! It is impressive, but everyone LOSES."
-#   elsif game.turns.player1.deck.cards  == []
-#     p "*~*~*~* Aurora has won the game! *~*~*~*"
-#   elsif game.turns.player2.deck.cards == []
-#     p "*~*~*~* Megan has won the game! *~*~*~*"
-#   elsif
-#     p winner = turn.winner
-#     p turn.type
-#     p turn.pile_cards
-#     p turn.award_spoils(winner)
-#     p player1.deck.cards.length
-#     p player2.deck.cards.length
-#     turn = Turn.new(player1, player2)
-#   end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# game.each do |turn|
-#   turn.turn.winner = winner
-#   turn.turn.pile_cards
-#   turn.turn.award_spoils
