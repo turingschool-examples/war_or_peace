6b8680a1330dedecf84fb390742591dfc404283f
Gvieve <gnuebel84@yahoo.com>
2020-12-05 15:11:22 -0700

    Create Deck class

---
+++ lib/deck.rb
@@ -0,0 +1,34 @@
+# require './lib/card'
+
+class Deck
+  attr_reader :cards
+
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def add_card(card)
+    @cards << card
+  end
+
+  def rank_of_card_at(index)
+    cards[index].rank
+  end
+
+  def high_ranking_cards
+    high_ranking_cards =
+    cards.find_all do |card|
+    card.rank > 10
+    end
+  end
+
+  def percent_high_ranking
+    percent_high_ranking =
+    (((high_ranking_cards.count).to_f /
+    (cards.count).to_f)*100).round(2)
+  end
+
+  def remove_card
+    remove_card = cards.delete_at(0)
+  end
+end
