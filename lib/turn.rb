1ee38814c830061efb46c1b375d14e96b4a4aaf7
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2020-12-05 22:02:44 -0700

    Need to fix methods issues for deck and spoils

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,16 @@ class Turn
   def initialize (player1, player2)
     @player1 = player1
     @player2 = player2
+    @spoils_of_war = []
     end
 
+  def type
+    if player1.deck.cards[0].rank == player2.deck.cards[0].rank && player1.deck.cards[2].rank == player2.deck.card[2].rank
+      return  :mutually_assured_destruction
+    elsif player1.deck.cards[0].rank == player2.deck.cards[0].rank
+        return  :war
+    else
+        return  :basic
+    end
+  end
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -8,15 +8,60 @@ require './lib/turn'
 class TurnTest < Minitest::Test
 
   def test_turn_exists
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '5', 5)
-    card3 = Card.new(:club, '8', 8)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-    player1 = Player.new('Megan', deck)
-    player2 = Player.new('Aurora', deck)
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    cards1 = [card1, card2, card5, card8]
+    cards2 = [card3, card4, card6, card7]
+    deck1 = Deck.new(cards1)
+    deck2 = Deck.new(cards2)
+    player1 = Player.new('Megan', deck1)
+    player2 = Player.new('Aurora', deck2)
+    turn = Turn.new(player1, player2)
+    assert_instance_of Turn, turn
+  end
+
+  def test_if_has_spoils_of_war
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    cards1 = [card1, card2, card5, card8]
+    cards2 = [card3, card4, card6, card7]
+    deck1 = Deck.new(cards1)
+    deck2 = Deck.new(cards2)
+    player1 = Player.new('Megan', deck1)
+    player2 = Player.new('Aurora', deck2)
     turn = Turn.new(player1, player2)
 
-    assert_instance_of Turn, turn
+    assert_equal [], turn.spoils_of_war
+  end
+
+  def test_basic_turn
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    cards1 = [card1, card2, card5, card8]
+    cards2 = [card3, card4, card6, card7]
+    deck1 = Deck.new(cards1)
+    deck2 = Deck.new(cards2)
+    player1 = Player.new('Megan', deck1)
+    player2 = Player.new('Aurora', deck2)
+    turn = Turn.new(player1, player2)
+    assert_equal :basic, turn.type
   end
 end
