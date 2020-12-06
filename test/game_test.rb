bac3a904ceb21bb4b050fb9d094c2c65d04666ab
Wil McCauley <wmccauley724@gmail.com>
2020-12-05 17:17:24 -0700

    Add game_test file

---
+++ test/game_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/deck'
+require './lib/card'
+require './lib/player'
+require './lib/turn'
+require './lib/war_or_peace_runner'
+require './lib/game'
+
+class GameTest < Minitest::Test
+  
+end
