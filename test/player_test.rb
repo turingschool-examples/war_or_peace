75f77f31c6682b0743b9a134766ca9f256be9578
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2020-12-05 18:11:59 -0700

    Add player lost test and fix prior test

--- test/player_test.rb
+++ test/player_test.rb
@@ -15,4 +15,36 @@ class PlayerTest < Minitest::Test
 
       assert_instance_of Player, player
     end
+
+    def test_player_has_name
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '5', 5)
+      card3 = Card.new(:club, '8', 8)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      player =Player.new('Clarissa', deck)
+
+      assert_equal player.name, 'Clarissa'
+    end
+    def test_player_has_deck
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '5', 5)
+      card3 = Card.new(:club, '8', 8)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      player = Player.new('Clarissa', deck)
+
+      assert_includes(deck.cards, card1)
+    end
+
+    def test_has_player_lost
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '5', 5)
+      card3 = Card.new(:club, '8', 8)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      player = Player.new('Clarissa', deck)
+
+      assert_equal false, player.has_lost
+    end
 end
