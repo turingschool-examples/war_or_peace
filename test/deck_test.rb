23abf9ae1276470f28e40045bcec7d6ca1eb1c32
Katy La Tour <klatour324@gmail.com>
2020-12-06 15:45:43 -0600

    Refactor card attributes

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -25,8 +25,8 @@ class DeckTest < Minitest::Test
 
   def test_it_returns_rank_if_card_found
     card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:heart, '3', 3)
-    card3 = Card.new(:spade, '10', 10)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
 
     deck = Deck.new([card1, card2, card3])
 
@@ -34,19 +34,19 @@ class DeckTest < Minitest::Test
   end
 
   def test_it_returns_cards_with_a_rank_above_10
-    card1 = Card.new(:heart, 'Jack', 11)
-    card2 = Card.new(:diamond, 'King', 13)
-    card3 = Card.new(:spade, '7', 7)
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
 
     deck = Deck.new([card1, card2, card3])
 
-    assert_equal [card1, card2], deck.high_ranking_cards
+    assert_equal [card1, card3], deck.high_ranking_cards
   end
 
   def test_it_returns_the_percent_of_cards_with_a_rank_above_10
-    card1 = Card.new(:heart, 'Jack', 11)
-    card2 = Card.new(:diamond, 'King', 13)
-    card3 = Card.new(:spade, '7', 7)
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
 
     deck = Deck.new([card1, card2, card3])
 
@@ -54,9 +54,9 @@ class DeckTest < Minitest::Test
   end
 
   def test_remove_top_card_from_deck
-    card1 = Card.new(:heart, 'Jack', 11)
-    card2 = Card.new(:diamond, 'King', 13)
-    card3 = Card.new(:spade, '7', 7)
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
 
     deck = Deck.new([card1, card2, card3])
 
@@ -64,9 +64,9 @@ class DeckTest < Minitest::Test
   end
 
   def test_add_card_to_end_of_deck
-    card1 = Card.new(:heart, 'Jack', 11)
-    card2 = Card.new(:diamond, 'King', 13)
-    card3 = Card.new(:spade, '7', 7)
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
     card4 = Card.new(:club, '5', 5)
 
     deck = Deck.new([card1, card2, card3])
