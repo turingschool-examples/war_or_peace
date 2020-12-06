0c32aa2d680b17f5ca5a1158fe4129362e26d77b
Joseph Budina <joebud3@gmail.com>
2020-12-06 09:04:04 -0700

    Add turn and end result methods and class variable

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,15 +1,25 @@
 class Turn
+  @@turn_count = 0
   attr_reader :player1,
               :player2,
-              :spoils_of_war
+              :spoils_of_war,
+              :type,
+              :winner
 
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
     @spoils_of_war = []
+    @type = set_type
+    @winner = set_winner
+    @@turn_count += 1
   end
 
-  def type 
+  def self.turn_count
+    @@turn_count
+  end
+
+  def set_type
      if mutually_assured_destruction?
       :mutually_assured_destruction
     elsif (
@@ -25,28 +35,35 @@ class Turn
     end
   end
 
-  def mutually_assured_destruction? 
+  def mutually_assured_destruction?
     @player1.deck.rank_of_card_at(0) ==
     @player2.deck.rank_of_card_at(0) &&
     @player1.deck.rank_of_card_at(2) ==
     @player2.deck.rank_of_card_at(2)
   end
 
-  def winner
-    if type == :mutually_assured_destruction
-       return 'No Winner'
-    elsif type == :war && @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
+  def set_winner
+    if type == :basic 
+      if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
         @player1
-    elsif
+      else
         @player2
       end
-    if type == :basic && @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
+    elsif @player1.card_count < 3
+      @player2
+    elsif (@player2.card_count < 3)
+      @player1
+    elsif type == :war 
+      if @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
         @player1
-    else
+      else 
         @player2
+      end
+    else type == :mutually_assured_destruction 
+      return 'No Winner'
     end
   end
-  
+
 
   def pile_cards
     if type == :mutually_assured_destruction && winner == "No Winner"
@@ -66,8 +83,28 @@ class Turn
   end
 
   def award_spoils(winner)
-    @spoils_of_war.map do |card|
-      winner.deck.add_card(card)
+    unless :mutually_assured_destruction == type
+      @spoils_of_war.each do |card|
+        winner.deck.add_card(card)
+      end
+    end
+  end
+
+  def turn_result
+    if type == :basic
+      puts "Turn #{@@turn_count}: #{winner.name} won #{spoils_of_war.length} cards"
+    elsif type == :war 
+      puts "Turn #{@@turn_count}: WAR - Aurora won #{spoils_of_war.length} cards"
+    else
+      puts "Turn #{@@turn_count}: *mutually assured destruction* #{spoils_of_war.length} cards removed from play"
+    end
+  end
+
+  def end_result
+    if @@turn_count == 1_000_000
+      puts "--- Draw ---"
+    else
+      puts "*~*~*~* #{winner.name} has won the game! *~*~*~*"
     end
   end
 end
