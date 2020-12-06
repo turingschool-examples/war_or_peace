37fb53c796f7f47dbb8ae891bff9d63084921820
Katy La Tour <klatour324@gmail.com>
2020-12-06 13:20:03 -0600

    Add test for readable attributes for player1 and player2

--- test/game_test.rb
+++ test/game_test.rb
@@ -9,8 +9,35 @@ require './lib/game'
 
 class GameTest < Minitest::Test
   def test_it_exists
-    game = Game.new
+    card_generator = CardGenerator.new
+    card_generator.make_standard_deck
+
+    shuffle_cards = card_generator.full_card_deck.shuffle
+    deck1 = Deck.new(shuffle_cards.slice(1..26))
+    deck2 = Deck.new(shuffle_cards.slice(27..52))
+    
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    game = Game.new(player1, player2)
 
     assert_instance_of Game, game
+  end
+
+  def test_it_has_readable_attributes
+    card_generator = CardGenerator.new
+    card_generator.make_standard_deck
+
+    shuffle_cards = card_generator.full_card_deck.shuffle
+    deck1 = Deck.new(shuffle_cards.slice(1..26))
+    deck2 = Deck.new(shuffle_cards.slice(27..52))
+
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    game = Game.new(player1, player2)
+
+    assert_equal player1, game.player1
+    assert_equal player2, game.player2
   end
 end
