45a8fbf92cae108080c222533d6c31953d9c20b7
Gvieve <gnuebel84@yahoo.com>
2020-12-06 02:43:17 -0700

    Create method to pile cards

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,4 +18,38 @@ attr_reader :player1,
       :basic
     end
   end
+
+  def winner
+    if type == :basic
+      if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+        player1
+      else player2
+      end
+    elsif type == :war
+      if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+        player1
+      else player2
+      end
+    else "No Winner"
+    end
+  end
+
+  def pile_cards
+    if type == :mutually_assured_destruction
+      @player1.deck.remove_card
+      @player2.deck.remove_card
+      @player1.deck.remove_card
+      @player2.deck.remove_card
+      @player1.deck.remove_card
+      @player2.deck.remove_card
+    elsif type == :war
+      @spoils << player1.deck.remove_card
+      @spoils << player2.deck.remove_card
+      @spoils << player1.deck.remove_card
+      @spoils << player2.deck.remove_card
+    else type == :basic
+      @spoils_of_war << @player1.deck.remove_card
+      @spoils_of_war << @player2.deck.remove_card
+    end
+  end
 end
