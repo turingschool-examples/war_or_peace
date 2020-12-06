831c6ef1bbac9a2986f4c11402c37dee44e2254f
Katy La Tour <klatour324@gmail.com>
2020-12-06 15:08:25 -0600

    Add start method to Game class

--- lib/game.rb
+++ lib/game.rb
@@ -6,4 +6,19 @@ class Game
     @player1 = player1
     @player2 = player2
   end
+
+  def start
+    p"Welcome to War! (or Peace) This game will be played with 52 cards."
+    p "The players today are Megan and Aurora."
+    p "Type 'GO' to start the game!"
+    p "------------------------------------------------------------------"
+
+    input = gets.chomp.upcase
+
+    until input == 'GO'
+      p "Error! Invalid input. Please try again."
+      input = gets.chomp.upcase
+    end
+    play_turns
+  end
 end
