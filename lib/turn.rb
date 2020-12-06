328787042afe8e604d1df33636ae0721df6d2134
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2020-12-06 00:13:34 -0700

    Make sure types work properly.

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,11 +13,30 @@ class Turn
 
   def type
     if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
-      :mutually_assured_destruction
+          :mutually_assured_destruction
     elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
-      :war
+          :war
     else
-      :basic
+          :basic
     end
   end
+
+def winner
+  if type == :basic
+    if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+      player1
+    else player1.deck.rank_of_card_at(0) < player2.deck.rank_of_card_at(0)
+      player2
+    end
+  elsif type == :war
+    if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+      player1
+    else player1.deck.rank_of_card_at(2) < player2.deck.rank_of_card_at(2)
+      player2
+    end
+  elsif type == :mutually_assured_destruction
+    "No Winner"
+    end
+  end
+
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -64,4 +64,41 @@ class TurnTest < Minitest::Test
 
     assert_equal :basic, turn.type
   end
+
+  def test_war_turn
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:club, 'Jack', 11)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new('Megan', deck1)
+    player2 = Player.new('Aurora', deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal :war, turn.type
+  end
+
+  def test_mutually_assured_destruction
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:club, 'Jack', 11)
+    card4 = Card.new(:spade, '10', 10)
+    card5 = Card.new(:heart, 'Queen', 12)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new('Megan', deck1)
+    player2 = Player.new('Aurora', deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal :mutually_assured_destruction, turn.type
+  end
+
 end
