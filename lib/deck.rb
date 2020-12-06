7925d3c442cd4a4136460e9b16708b490869ee9b
Alex Schwartz <aschwartz@detaso.com>
2020-12-05 18:42:30 -0600

    Update deck to handle method return causing errors

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,7 +6,7 @@ class Deck
   end
 
   def rank_of_card_at(index)
-    (@cards.length > index) ? @cards[index].rank : nil
+    (@cards.length > index) ? @cards[index].rank : 0
   end
 
   def high_ranking_cards
