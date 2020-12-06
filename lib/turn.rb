fd63839f3336ae748c53321563a47cacb63aac1b
Diana Buffone <diana.pamelaet@gmail.com>
2020-12-06 15:36:34 -0500

    Define pile cards

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,4 +18,31 @@ class Turn
       :war
     end
   end
+
+  def winner
+    # require "pry"; binding.pry
+    if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+      player1
+    elsif player2.deck.rank_of_card_at(0) > player1.deck.rank_of_card_at(0)
+      player2
+    end
+  end
+
+  def pile_cards
+    require "pry"; binding.pry
+    if :basic
+      @spoils_of_war << player1.deck.cards[0] &&
+      @spoils_of_war << player2.deck.cards[0]
+    elsif :war
+      @spoils_of_war << player1.deck.cards[0..2] &&
+      @spoils_of_war << player2.deck.cards[0..2]
+    else :mutually_assured_destruction
+      3.times do player1.deck |card|
+        player1.deck.remove_card
+      end
+      3.times do player2.deck |card|
+        player2.deck.remove_card
+      end
+    end
+  end
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -41,4 +41,58 @@ class TurnTest < Minitest::Test
     # require "pry"; binding.pry
     assert_equal :basic, turn.type
   end
+
+  def test_it_can_determine_turn_winner
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal player1, turn.winner
+  end
+
+  def test_it_can_move_cards_to_pile
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal @spoils_of_war = [card1, card3], turn.pile_cards
+  end
+
+  def test_it_can_award_the_spoils
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal player1.deck, turn.award_spoils(winner)
+  end
 end
