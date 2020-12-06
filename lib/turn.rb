b376b19bba3bf8f4174c9b30614c4175cf42b19d
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2020-12-05 23:11:57 -0700

    Fix issues with Player class missing comma and finish type on turn.rb

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,8 @@
+require './lib/deck'
+require './lib/player'
 class Turn
-  attr_reader :player1
-              :player2
+  attr_reader :player1,
+              :player2,
               :spoils_of_war
 
   def initialize (player1, player2)
@@ -10,12 +12,12 @@ class Turn
     end
 
   def type
-    if player1.deck.cards[0].rank == player2.deck.cards[0].rank && player1.deck.cards[2].rank == player2.deck.card[2].rank
-      return  :mutually_assured_destruction
-    elsif player1.deck.cards[0].rank == player2.deck.cards[0].rank
-        return  :war
+    if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+      :mutually_assured_destruction
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+      :war
     else
-        return  :basic
+      :basic
     end
   end
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -16,17 +16,17 @@ class TurnTest < Minitest::Test
     card6 = Card.new(:diamond, 'Queen', 12)
     card7 = Card.new(:heart, '3', 3)
     card8 = Card.new(:diamond, '2', 2)
-    cards1 = [card1, card2, card5, card8]
-    cards2 = [card3, card4, card6, card7]
-    deck1 = Deck.new(cards1)
-    deck2 = Deck.new(cards2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
     player1 = Player.new('Megan', deck1)
     player2 = Player.new('Aurora', deck2)
     turn = Turn.new(player1, player2)
+
     assert_instance_of Turn, turn
   end
 
-  def test_if_has_spoils_of_war
+  def test_if_has_attributes
+
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
     card3 = Card.new(:heart, '9', 9)
@@ -35,18 +35,19 @@ class TurnTest < Minitest::Test
     card6 = Card.new(:diamond, 'Queen', 12)
     card7 = Card.new(:heart, '3', 3)
     card8 = Card.new(:diamond, '2', 2)
-    cards1 = [card1, card2, card5, card8]
-    cards2 = [card3, card4, card6, card7]
-    deck1 = Deck.new(cards1)
-    deck2 = Deck.new(cards2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
     player1 = Player.new('Megan', deck1)
     player2 = Player.new('Aurora', deck2)
     turn = Turn.new(player1, player2)
 
+    assert_equal player1, turn.player1
+    assert_equal player2, turn.player2
     assert_equal [], turn.spoils_of_war
   end
 
   def test_basic_turn
+
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
     card3 = Card.new(:heart, '9', 9)
@@ -55,13 +56,12 @@ class TurnTest < Minitest::Test
     card6 = Card.new(:diamond, 'Queen', 12)
     card7 = Card.new(:heart, '3', 3)
     card8 = Card.new(:diamond, '2', 2)
-    cards1 = [card1, card2, card5, card8]
-    cards2 = [card3, card4, card6, card7]
-    deck1 = Deck.new(cards1)
-    deck2 = Deck.new(cards2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
     player1 = Player.new('Megan', deck1)
     player2 = Player.new('Aurora', deck2)
     turn = Turn.new(player1, player2)
+
     assert_equal :basic, turn.type
   end
 end
