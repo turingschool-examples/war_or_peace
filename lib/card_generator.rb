5204b759dac666e4ca637ad5de1ac2a583db941e
Katy La Tour <klatour324@gmail.com>
2020-12-06 12:21:03 -0600

    Add initialize method and readers for suits ranks and full card deck instance variables

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,3 +1,11 @@
 class CardGenerator
-  
+  attr_reader :suits,
+              :ranks,
+              :full_card_deck
+
+  def initialize(suits, ranks)
+    @suits = suits
+    @ranks = ranks
+    @full_card_deck = []
+  end
 end
