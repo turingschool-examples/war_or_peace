ed0ecd62728b26e542199011b69edca84e6e4cf9
Diana Buffone <diana.pamelaet@gmail.com>
2020-12-05 20:15:51 -0500

    Create type class

---
+++ lib/turn.rb
@@ -0,0 +1,16 @@
+class Turn
+  attr_reader :player1,
+              :player2,
+              :spoils_of_war
+  def initialize(player1, player2)
+    @player1       = @player1
+    @player2       = @player2
+    @spoils_of_war = []
+  end
+
+  def type
+    if player1.deck.cards[0] != player2.deck.cards[0]
+      :basic
+    end
+  end
+end
