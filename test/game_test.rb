3b6900c788f024410c0ad80491cc3ee2e4e2f6ce
Wil McCauley <wmccauley724@gmail.com>
2020-12-06 15:35:51 -0700

    Remove unneccesary require header from game test

--- test/game_test.rb
+++ test/game_test.rb
@@ -4,7 +4,6 @@ require './lib/deck'
 require './lib/card'
 require './lib/player'
 require './lib/turn'
-# require './lib/war_or_peace_runner'
 require './lib/game'
 
 class GameTest < Minitest::Test
