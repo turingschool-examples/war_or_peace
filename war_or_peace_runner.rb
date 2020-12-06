2c730aaaefb9c5feb435547c9aecafad7370908d
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2020-12-06 15:16:45 -0700

    Get runner file working

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -3,3 +3,17 @@ require './lib/deck'
 require './lib/player'
 require './lib/turn'
 require './lib/game'
+
+random_cards = make_deck.shuffle
+random_cards1 = random_cards[0..25]
+random_cards2 = random_cards[26..51]
+deck1 = Deck.new(random_cards1)
+deck2 = Deck.new(random_cards2)
+player1 = Player.new('Megan', deck1)
+player2 = Player.new('Aurora', deck2)
+turn = Turn.new(player1, player2)
+new_game = Game.new(turn)
+
+p new_game.turn.player1.deck.cards.length
+
+new_game.start_message
