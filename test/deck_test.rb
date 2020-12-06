ce548329cd3ca1ab778a69bb04f2b380793ee92c
Andrew Smith <andrewpatrick138@gmail.com>
2020-12-06 13:18:11 -0800

    clean up methods

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -26,17 +26,14 @@ class DeckTest < Minitest::Test
     deck = build_deck
     assert_instance_of Deck, deck
   end
+
   def test_rank
     deck = build_deck
     assert_equal 12,deck.rank_of_card_at(0)
   end
+
   def test_high_cards
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-    high_cards = deck.high_ranking_cards
+    deck = build_deck
     assert_equal 2, high_cards.length
     assert_includes(high_cards,card1)
     assert_includes(high_cards,card3)
