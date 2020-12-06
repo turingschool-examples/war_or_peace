d285f8d184b9a16fd4ccd7852bec115541b221a4
Alexa Morales <alexamsmyth@gmail.com>
2020-12-06 14:11:10 -0500

    Create game.play method

--- lib/game.rb
+++ lib/game.rb
@@ -14,6 +14,63 @@ class Game
       Type 'GO' to start the game!
       ------------------------------------------------------------------"
     p "=> (type 'GO' when you are ready for WAR!)"
-    p gets.chomp
+    if gets.chomp != "GO"
+      p "TRY AGAIN (type GO):"
+      p gets.chomp
+    end
   end
+
+  def play
+    turn_index = 0
+    10000000.times do
+        turn_index +=1
+      if turn_index > 1000000
+        return p "---- DRAW ----"
+      else
+        if turns.type == :basic
+          if turns.player1.deck.cards.empty? == true
+            return print "*~*~*~* Aurora has won the game! *~*~*~*"
+          elsif turns.player2.deck.cards.empty? == true
+            return print "*~*~*~* Megan has won the game! *~*~*~*"
+          else
+            turns.winner
+            winner = turns.winner
+            turns.pile_cards
+            turns.award_spoils (winner)
+            win_name = winner.name
+            d1 = turns.player1.deck.cards.length
+            d2 = turns.player2.deck.cards.length
+            p "Turn #{turn_index}: #{win_name} won 2 cards. Megan has #{d1}. Aurora has #{d2}"
+          end
+        elsif turns.type == :war
+          if turns.player1.deck.cards.length < 3
+            return print "*~*~*~* Aurora has won the game! *~*~*~*"
+          elsif turns.player2.deck.cards.length < 3
+            return print "*~*~*~* Megan has won the game! *~*~*~*"
+          else
+            winner = turns.winner
+            turns.pile_cards
+            turns.award_spoils(winner)
+            win_name = winner.name
+            d1 = turns.player1.deck.cards.length
+            d2 = turns.player2.deck.cards.length
+            puts "Turn #{turn_index}: WAR - #{win_name} won 6 cards. Megan has #{d1}. Aurora has #{d2}"
+          end
+        else turns.type == :mutually_assured_destruction
+          if turns.player1.deck.cards.length < 3
+            return print "*~*~*~* Aurora has won the game! *~*~*~*"
+          elsif turns.player2.deck.cards.length < 3
+            return print "*~*~*~* Megan has won the game! *~*~*~*"
+          else
+            turns.winner
+            turns.pile_cards
+            d1 = turns.player1.deck.cards.length
+            d2 = turns.player2.deck.cards.length
+            puts "Turn #{turn_index}: *mutually assured destruction* 6 cards removed from play. Megan has #{d1}. Aurora has #{d2}"
+          end
+        end
+      end
+    end
+  end
+
 end
