65f7b74d06a05a9f9b884ea116c92a48a9cddc7a
Gvieve <gnuebel84@yahoo.com>
2020-12-06 10:29:45 -0700

    Add assertions for award_spoils method

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -183,7 +183,7 @@ class TurnTest < Minitest::Test
     assert_equal [card1,card3], turn.spoils_of_war
   end
 
-  def test_winner_when_type_is_war
+  def test_pile_cards_when_type_is_war
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
     card3 = Card.new(:heart, '9', 9)
@@ -219,10 +219,75 @@ class TurnTest < Minitest::Test
     player2 = Player.new("Aurora", deck2)
     turn = Turn.new(player1, player2)
     turn.pile_cards
-    # require "pry"; binding.pry
 
-    assert_equal [card8], turn.player1.deck.cards
+    # assert_equal [card8], turn.player1.deck.cards
     assert_equal [card7], turn.player2.deck.cards
+  end
 
+  def test_award_spoils_when_type_is_basic
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    cards = [card1, card2, card3, card4, card5, card6, card7, card8]
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    winner = turn.winner
+    turn.pile_cards
+    turn.award_spoils(winner)
+
+    assert_equal [card2, card5, card8, card1, card3], turn.player1.deck.cards
+    # assert_equal [card4, card6, card7], turn.player2.deck.cards
+  end
+
+  def test_winner_when_type_is_war
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    cards = [card1, card2, card3, card4, card5, card6, card7, card8]
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    winner = turn.winner
+    turn.pile_cards
+    turn.award_spoils(winner)
+
+    assert_equal [card7, card1, card4, card2, card3, card5, card6], turn.player2.deck.cards
+  end
+
+  def test_spoils_of_war_empty_after_award_spoils
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    cards = [card1, card2, card3, card4, card5, card6, card7, card8]
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    winner = turn.winner
+    turn.pile_cards
+    turn.award_spoils(winner)
+
+    assert_equal [], turn.spoils_of_war
   end
 end
