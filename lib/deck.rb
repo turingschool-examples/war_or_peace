6dcefc007ea3b97d18f3d2b529f3146c26044540
Alexa Morales <alexamsmyth@gmail.com>
2020-12-06 14:13:36 -0500

    Revise deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -12,20 +12,12 @@ class Deck
     @cards[i].rank
   end
 
-  def test
-      cards.class
-      cards.each do |card|
-        p card.class
-      end
-  end
-  
+
   def high_ranking_cards
     @cards.select {|card| card.rank >=12}
-
   end
 
   def percent_high_ranking
-    #return an array of cards in the deck rank >=11
     numer = cards.select {|card| card.rank >=12}.count
     dem = cards.count
     result = (numer.to_f / dem.to_f) * 100
@@ -34,12 +26,10 @@ class Deck
 
   def remove_card
     cards.delete_at(0)
-    # cards
-    #remove top card from deck
   end
 
   def add_card (new_card)
-    @cards << new_card#add card to end of Deck
+    @cards << new_card
   end
 
 end
