8c61a43d98c2035cfb9cf7238af695d8431e5798
Gvieve <gnuebel84@yahoo.com>
2020-12-06 13:41:15 -0700

    Fix formatting

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -14,7 +14,7 @@ class DeckTest < Minitest::Test
 
   assert_instance_of Deck, deck
   end
-
+  
   def test_cards_exists
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
