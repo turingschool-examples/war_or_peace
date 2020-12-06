cd43a14f8923127ca6f7d3d197ed3ab1090de86d
Kris Litman <duguy8@gmail.com>
2020-12-06 14:19:33 -0500

    Work on adding cards to generator

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,4 +1,12 @@
+
 class CardGenerator
-  def intialize
+  attr_reader :filename
+  def initialize(filename)
+    @file_name = filename
+  end
+
+  def read_files
+    # @filename = File.new "cards.txt"
+    # @filename = File.open("cards.txt", "r")
   end
 end
