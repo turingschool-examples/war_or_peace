7371e48764a20ff04c2556227d37484431aab65a
Jesus Quezada-Guillen <maj3st1@gmail.com>
2020-12-06 11:08:19 -0600

    Add test for pile_cards method for mutually_assured_distruction turn type.

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -206,4 +206,41 @@ class TurnTest < Minitest::Test
     turn = Turn.new(player1, player2)
     assert_equal :mutually_assured_destruction, turn.type
   end
+
+  def test_it_can_tell_no_winner
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, '8', 8)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    winner = turn.winner
+    message = "No Winner"
+    assert_equal message, winner
+  end
+
+  def test_it_removes_cards_for_mutually_assured_distruction_turn_type
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, '8', 8)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    turn.pile_cards
+    assert_equal [], turn.spoils_of_war
+  end
 end
