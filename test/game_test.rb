43f411d0fb3da259cec2a5497042ed13231d5438
Katy La Tour <klatour324@gmail.com>
2020-12-06 13:05:35 -0600

    Create test for instance of Game

---
+++ test/game_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+require './lib/card_generator'
+require './lib/game'
+
+class GameTest < Minitest::Test
+  def test_it_exists
+    game = Game.new
+
+    assert_instance_of Game, game
+  end
+end
