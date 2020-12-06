416b17ceb1bc6047f8228d922445e9a9069ff880
Katy La Tour <klatour324@gmail.com>
2020-12-06 12:45:01 -0600

    Initialize default parameters and make default methods for suits and ranks

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -3,9 +3,31 @@ class CardGenerator
               :ranks,
               :full_card_deck
 
-  def initialize(suits, ranks)
+  def initialize(suits = default_suits, ranks = default_ranks)
     @suits = suits
     @ranks = ranks
     @full_card_deck = []
+  end
+
+  def default_suits
+    [:heart, :spade, :club, :diamond]
+  end
+
+  def default_ranks
+    {
+      '2' => 2,
+      '3' => 3,
+      '4' => 4,
+      '5' => 5,
+      '6' => 6,
+      '7' => 7,
+      '8' => 8,
+      '9' => 9,
+      '10' => 10,
+      'Jack' => 11,
+      'Queen' => 12,
+      'King' => 13,
+      'Ace' => 14
+    }
   end
 end
