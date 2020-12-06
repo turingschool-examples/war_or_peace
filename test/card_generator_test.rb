a7a8e4cccaef57b2e0b4655b0c905fa52a2edb9e
Katy La Tour <klatour324@gmail.com>
2020-12-06 12:58:12 -0600

    Create test to make a standard deck of cards

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -80,4 +80,31 @@ class CardGeneratorTest < Minitest::Test
     assert_equal default_suits, card_generator.default_suits
     assert_equal default_ranks, card_generator.default_ranks
   end
+
+  def test_it_makes_a_standard_deck_of_cards
+    suits = [:heart, :spade, :club, :diamond]
+    ranks ={
+      '2' => 2,
+      '3' => 3,
+      '4' => 4,
+      '5' => 5,
+      '6' => 6,
+      '7' => 7,
+      '8' => 8,
+      '9' => 9,
+      '10' => 10,
+      'Jack' => 11,
+      'Queen' => 12,
+      'King' => 13,
+      'Ace' => 14
+    }
+
+    card_generator = CardGenerator.new(suits, ranks)
+
+    assert_equal true, card_generator.full_card_deck.empty?
+
+    card_generator.make_standard_deck
+
+    assert_equal 52, card_generator.full_card_deck.count
+  end
 end
