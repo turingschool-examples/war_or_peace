6fb5e53c52384d26046469bbe71fb15fc85ee1fa
Trevor Robinson <trevor.robi1254@gmail.com>
2020-12-06 17:45:44 -0700

    Fix turn bug

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,23 +16,28 @@ class Turn
   end
 
   def type
-    if player1.deck.cards[0].rank != player2.deck.cards[0].rank
+    # if player1.deck.cards.count < 3 || player2.deck.cards.count < 3
+    #   if  player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) != player2.deck.rank_of_card_at(2)
+    #     :draw
+    #   elsif player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
+    #     :basic
+    #   end
+    if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
       :basic
-    elsif player1.deck.cards[0].rank == player2.deck.cards[0].rank && player1.deck.cards[2].rank != player2.deck.cards[2].rank
+    elsif player1.deck.rank_of_card_at(2) != player2.deck.rank_of_card_at(2)
       :war
-    elsif
-      player1.deck.cards[0].rank == player2.deck.cards[0].rank && player1.deck.cards[2].rank == player2.deck.cards[2].rank
+    elsif player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
       :mutually_assured_destruction
     end
   end
 
   def winner
    if type == :basic
-     return player1 if player1.deck.cards[0].rank > player2.deck.cards[0].rank
-     return player2 if player1.deck.cards[0].rank < player2.deck.cards[0].rank
+     return player1 if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+     return player2 if player1.deck.rank_of_card_at(0) < player2.deck.rank_of_card_at(0)
    elsif type == :war
-     return player1 if player1.deck.cards[2].rank > player2.deck.cards[2].rank
-     return player2 if player1.deck.cards[2].rank < player2.deck.cards[2].rank
+     return player1 if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+     return player2 if player1.deck.rank_of_card_at(2) < player2.deck.rank_of_card_at(2)
    elsif type == :mutually_assured_destruction
      #binding.pry
      "No Winner"
@@ -49,13 +54,15 @@ class Turn
       @spoils_of_war << player2.deck.remove_card(3)
       @spoils_of_war.flatten!
     elsif type == :mutually_assured_destruction
-      player1.deck.cards.shift(3)
-      player2.deck.cards.shift(3)
+      player1.deck.remove_card(3)
+      player2.deck.remove_card(3)
     end
   end
 
   def award_spoils
-    unless type == :mutually_assured_destruction
+    # if player1.deck.cards.count < 3 || player2.deck.cards.count < 3
+    # end
+    unless type == :mutually_assured_destruction || type == :draw
       winner.deck.add_card(@spoils_of_war)
       winner.deck.cards.flatten!
     end
