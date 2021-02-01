2395b2fd969f0a756643bdac0d4d6176b44e650c
George Ho <george.h.w.ho@gmail.com>
2021-02-01 18:26:48 -0500

    Add deck testcase 2

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -14,4 +14,13 @@ class DeckTest < Minitest::Test
     assert_instance_of Deck, deck
   end
 
+  def test_it_has_readable_attributes
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal deck.cards, cards
+  end
 end
