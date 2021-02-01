2172bb4ab674b1da572d939e1a34f254dbcd6acc
George Ho <george.h.w.ho@gmail.com>
2021-02-01 18:26:12 -0500

    Add deck testcase 1

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,16 +5,13 @@ require './lib/deck'
 
 class DeckTest < Minitest::Test
   def test_it_exists
-    card = Card.new(:diamond, 'Queen', 12)
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
 
-    assert_instance_of Card, card
+    assert_instance_of Deck, deck
   end
 
-  def test_it_has_readable_attributes
-    card = Card.new(:diamond, 'Queen', 12)
-
-    assert_equal :diamond, card.suit
-    assert_equal 'Queen', card.value
-    assert_equal 12, card.rank
-  end
 end
