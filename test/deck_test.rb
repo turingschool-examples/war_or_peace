fbed06629a907e27914db4c2b8af2924b1b77c68
George Ho <george.h.w.ho@gmail.com>
2021-02-01 18:27:09 -0500

    Add deck testcase 3

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -23,4 +23,15 @@ class DeckTest < Minitest::Test
 
     assert_equal deck.cards, cards
   end
+
+  def test_find_rank_of_card_at_index_0
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal deck.rank_of_card_at(0), 12
+  end
+  
 end
