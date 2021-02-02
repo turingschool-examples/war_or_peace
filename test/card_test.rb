19198ff6d8ec836c53d73c106d66c5d70b17cade
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-02-01 19:23:33 -0600

    Add card class

--- test/card_test.rb
+++ test/card_test.rb
@@ -11,7 +11,7 @@ class CardTest < Minitest::Test
 
   def test_it_has_readable_attributes
     card = Card.new(:diamond, 'Queen', 12)
-    
+
     assert_equal :diamond, card.suit
     assert_equal 'Queen', card.value
     assert_equal 12, card.rank
