8623cbc4c08103ce24a267ec20d9f7dcd20865a4
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-02-02 09:34:03 -0600

    Add test for high_ranking_cards

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -23,4 +23,18 @@ class DeckTest < Minitest::Test
 
     assert_equal deck.rank_of_card_at(0), card1
   end
+
+  def test_high_ranking_cards
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal deck.high_ranking_cards, [card1, card3]
+  end
+
+  def test_percent_high_ranking_cards
+    
+  end
 end
