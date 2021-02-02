b8519bd5f865f014ff443b699ccff2af74278db6
Olivia Charles <ocharles92@gmail.com>
2021-02-02 13:52:42 -0500

    Writing methods for war turn, not all tests passing yet

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,36 +14,58 @@ class Turn
     if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) &&
        player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
         :mutually_assured_destruction
-    elsif player1.deck.rank_of_card_at(0) ==     player2.deck.rank_of_card_at(0)
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
         :war
     else player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
         :basic
     end
   end
 
-
-
   def basic_winner
     if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
       player1
-    else player1.deck.rank_of_card_at(0) <        player2.deck.rank_of_card_at(0)
+    else player1.deck.rank_of_card_at(0) < player2.deck.rank_of_card_at(2)
       player2
     end
   end
 
+  def war_winner
+    if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+      player1
+    else player1.deck.rank_of_card_at(2) < player2.deck.rank_of_card_at(2)
+      player2
+    end
+  end
 
   def winner
     if  type == :mutually_assured_destruction
       p "No Winner"
-    else  type == :basic
+    elsif type == :war
+      war_winner
+    else type == :basic
        basic_winner
     end
   end
 
   def pile_cards
-    if type == :basic
-    spoils_of_war << player1.deck.remove_card
-    spoils_of_war << player2.deck.remove_card
+    if type == :mutually_assured_destruction
+      player1.deck.remove_card
+      player1.deck.remove_card
+      player1.deck.remove_card
+      player2.deck.remove_card
+      player2.deck.remove_card
+      player2.deck.remove_card
+    elsif type == :war
+      @spoils_of_war << player1.deck.remove_card
+      @spoils_of_war << player1.deck.remove_card
+      @spoils_of_war << player1.deck.remove_card
+      @spoils_of_war << player2.deck.remove_card
+      @spoils_of_war << player2.deck.remove_card
+      @spoils_of_war << player2.deck.remove_card
+      @spoils_of_war << player2.deck.remove_card
+    else type == :basic
+      @spoils_of_war << player1.deck.remove_card
+      @spoils_of_war << player2.deck.remove_card
     end
   end
 
