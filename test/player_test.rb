8b3914347537be0d61903c073294087ff2c3a0d5
George Ho <george.h.w.ho@gmail.com>
2021-02-01 20:40:05 -0500

    Add more test

--- test/player_test.rb
+++ test/player_test.rb
@@ -30,6 +30,20 @@ class PlayerTest < MiniTest::Test
   end
 
   def test_it_has_lost?
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    player = Player.new('Clarisa', deck)
 
+    assert_equal player.has_lost?, false
+    player.deck.remove_card
+    assert_equal player.has_lost?, false
+    player.deck.remove_card
+    assert_equal player.has_lost?, false
+    player.deck.remove_card
+    assert_equal player.has_lost?, true
+    assert_equal player.deck, Deck.new()
   end
 end
