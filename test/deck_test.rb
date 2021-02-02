7ab2b42d724cf97f717c5b08e9437a82ebbcfc14
Ryan Stemmle <ryan.stemmle@gmail.com>
2021-02-01 22:44:56 -0500

    Add outline for deck tests

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -24,18 +24,43 @@ class DeckTest < Minitest::Test
   end
 
   def test_it_can_return_rank_at_index_pos
+    card1 = Card.new(:diamond, "Queen", 12)
+    card2 = Card.new(:spade, "3", 3)
+    card3 = Card.new(:heart, "Ace", 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    # require "pry"; binding.pry
+    assert_equal 12, deck.rank_of_card_at(0)
+  end
+
+  def test_it_can_return_rank_at_another_index_pos
+    card1 = Card.new(:diamond, "Queen", 12)
+    card2 = Card.new(:spade, "3", 3)
+    card3 = Card.new(:heart, "Ace", 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    # require "pry"; binding.pry
+    assert_equal 14, deck.rank_of_card_at(2)
   end
 
   def test_it_returns_high_ranking_cards
+    skip
+    assert_equal [card1, card2], deck.high_ranking_cards
   end
 
   def test_it_returns_percentage_of_high_ranking_cards
+    skip
+    assert_equal 50.0, deck.percent_high_ranking
   end
 
   def test_it_can_remove_top_card
+    skip
+    assert_equal 2, cards.length
   end
 
   def test_it_can_add_one_card_to_bottom
+    skip
+    assert_equal 3, cards.length
   end
 
 end
