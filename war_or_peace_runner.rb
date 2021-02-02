aad37926a6f0bf54bd3c36817075f349113ca880
George Ho <george.h.w.ho@gmail.com>
2021-02-02 10:27:41 -0500

    Add file generating and parsing

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -3,15 +3,18 @@ require './lib/deck'
 require './lib/player'
 require './lib/turn'
 require './lib/game'
+require './lib/card_generator'
 
-standard_deck = []
-suits = [:spades, :hearts, :diamonds, :clubs]
-ranks = %w{Ace 2 3 4 5 6 7 8 9 10 Jack Queen King}
-suits.each do |suit|
-  ranks.size.times do |i|
-    standard_deck << Card.new(suit, ranks[i], i+1)
-  end
-end
+# standard_deck = []
+# suits = [:spades, :hearts, :diamonds, :clubs]
+# ranks = %w{Ace 2 3 4 5 6 7 8 9 10 Jack Queen King}
+# suits.each do |suit|
+#   ranks.size.times do |i|
+#     standard_deck << Card.new(suit, ranks[i], i+1)
+#   end
+# end
+
+standard_deck = CardGenerator.new('cards').cards
 
 shuffled_deck = standard_deck.shuffle
 cards1 = shuffled_deck[0..25]
