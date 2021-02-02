cafd41ebe9106ad43434ee668853cea198c3a001
George Ho <george.h.w.ho@gmail.com>
2021-02-01 20:06:26 -0500

    Add player tests

--- test/player_test.rb
+++ test/player_test.rb
@@ -6,4 +6,30 @@ require './lib/player'
 
 class PlayerTest < MiniTest::Test
 
+  def test_it_exists
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    player = Player.new('Clarisa', deck)
+
+    assert_instance_of Player, player
+  end
+
+  def test_it_has_readable_attributes
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    player = Player.new('Clarisa', deck)
+
+    assert_equal player.name, 'Clarisa'
+    assert_equal player.deck, Deck.new(cards)
+  end
+
+  def test_it_has_lost?
+
+  end
 end
