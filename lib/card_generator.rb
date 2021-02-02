3d9bd45a46a97559457f9eaff2cd0d24405d9c2c
George Ho <george.h.w.ho@gmail.com>
2021-02-02 10:03:52 -0500

    Add card generator and tests

---
+++ lib/card_generator.rb
@@ -0,0 +1,39 @@
+class CardGenerator
+  attr_reader :filename
+  def initialize(f)
+    @filename = f
+
+  end
+
+  def generate_txt
+    path_to_file = "#{filename}.txt"
+
+    File.delete(path_to_file) if File.exist?(path_to_file)
+
+    suits = %w[Spades Hearts Diamonds Clubs]
+    ranks = %w{2 3 4 5 6 7 8 9 10 Jack Queen King Ace}
+    suits.each do |suit|
+      ranks.size.times do |i|
+        File.open(path_to_file, "a") { |f| f.write "#{ranks[i]}, #{suit}, #{i+2} \n" }
+      end
+    end
+  end
+
+  def cards
+    card_array = self.file_to_array
+    standard_deck = []
+    52.times {
+      rank = card_array.shift
+      suit = card_array.shift
+      value = card_array.shift.to_i
+      standard_deck << Card.new(suit, rank, value)
+    }
+    standard_deck
+  end
+
+  def file_to_array
+    path_to_file = "#{filename}.txt"
+    text = File.open(path_to_file).read.scan(/\w+/)
+  end
+
+end
---
+++ test/card_generator_test.rb
@@ -0,0 +1,33 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/card_generator'
+
+class CardGeneratorTest < Minitest::Test
+  def test_it_exists
+    cards = CardGenerator.new('cards')
+    assert_instance_of CardGenerator, cards
+  end
+
+  def test_it_can_generate_txt
+    cards = CardGenerator.new('test')
+    cards.generate_txt
+    real = File.open("cards.txt")
+    test = File.open("test.txt")
+    assert_equal real.read, test.read
+  end
+
+  def test_it_can_convert_txt_to_array
+    cards = CardGenerator.new('test')
+    text = cards.file_to_array
+    assert_equal text[0..2], ["2", "Spades", "2"]
+  end
+
+  def test_it_can_convert_arr_to_card
+    cards = CardGenerator.new('test')
+
+    assert_equal cards.cards.size, 52
+    assert_instance_of Card, cards.cards[0]
+  end
+
+end
