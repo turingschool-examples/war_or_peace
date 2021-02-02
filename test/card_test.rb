b1fedef84cb04fa65ca049c41038fed126bab857
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-02-01 18:36:08 -0700

    Initial commit; Card class and tests created - passing.

--- test/card_test.rb
+++ test/card_test.rb
@@ -3,17 +3,14 @@ require 'minitest/pride'
 require './lib/card'
 
 class CardTest < Minitest::Test
-  def test_it_exists
-    card = Card.new(:diamond, 'Queen', 12)
-
-    assert_instance_of Card, card
+  def setup
+    @card = Card.new(:heart, 'Jack', 11)
   end
 
-  def test_it_has_readable_attributes
-    card = Card.new(:diamond, 'Queen', 12)
-    
-    assert_equal :diamond, card.suit
-    assert_equal 'Queen', card.value
-    assert_equal 12, card.rank
+  def test_it_exists_and_has_attributes
+    assert_instance_of Card, @card
+    assert_equal :heart, @card.suit
+    assert_equal "Jack", @card.value
+    assert_equal 11, @card.rank
   end
 end
