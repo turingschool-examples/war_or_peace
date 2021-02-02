80639e81ac294507c3e1a027320fe1860fd6593c
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-02-01 20:03:52 -0600

    Add rank_of_card_at test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -13,4 +13,14 @@ class DeckTest < Minitest::Test
 
     assert_instance_of Deck, deck
   end
+
+  def test_rank_of_cards
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    assert_equal deck.rank_of_card_at(0), card1
+  end
 end
