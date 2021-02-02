31c94053487c553713592def9f1bf3dd3541a6e5
George Ho <george.h.w.ho@gmail.com>
2021-02-02 10:24:30 -0500

    Update test for value on empty index

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -51,7 +51,7 @@ class DeckTest < Minitest::Test
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
 
-    assert_nil deck.rank_of_card_at(3)
+    assert_equal deck.rank_of_card_at(3), 0
   end
 
   def test_find_high_ranking_cards
