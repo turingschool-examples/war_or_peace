d30a0a3ae95802242990eab96b9919498927754f
George Ho <george.h.w.ho@gmail.com>
2021-02-02 11:22:12 -0500

    Update draw conditions

--- lib/game.rb
+++ lib/game.rb
@@ -9,23 +9,21 @@ class Game
     turn_counter = 0
     while turn.player1.deck.cards.size > 0 && turn.player2.deck.cards.size > 0 && turn_counter < 1000000
       turn_counter += 1
+      the_winner = turn.winner
       if turn.type == :mutually_assured_destruction
         puts "Turn #{turn_counter}: *mutually assured destruction* 6 cards removed from play"
       elsif turn.type == :war
-        puts "Turn #{turn_counter}: WAR - #{turn.winner.name} won 6 cards"
+        puts "Turn #{turn_counter}: WAR - #{the_winner.name} won 6 cards"
       else
-        puts "Turn #{turn_counter}: #{turn.winner.name} won 2 cards"
+        puts "Turn #{turn_counter}: #{the_winner.name} won 2 cards"
       end
 
       turn.pile_cards
-      turn.award_spoils(turn.winner)
-      puts "player1's cards: #{turn.player1.deck.cards.size} || player2's cards: #{turn.player2.deck.cards.size}"
+      turn.award_spoils(the_winner)
+      # puts "player1's cards: #{turn.player1.deck.cards.size} || player2's cards: #{turn.player2.deck.cards.size}"
     end
-    self.result
-  end
 
-  def result
-    if turn.winner == 'No Winner'
+    if turn.winner == 'No Winner' || turn_counter == 1000000
       puts '---- DRAW ----'
     else
       puts "*~*~*~* #{turn.winner.name} has won the game! *~*~*~*"
