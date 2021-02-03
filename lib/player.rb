58414752e17d5bd4c0880bf8be8159901f129d6a
ignored-comment <info.konham@gmail.com>
2021-02-03 16:32:00 -0500

    Add .name method; passing test

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,6 @@
 class Player
-
+  attr_reader :name
+  
   def initialize(name, deck)
     @name = name
     @deck = deck
--- test/player_test.rb
+++ test/player_test.rb
@@ -5,7 +5,6 @@ require './lib/deck'
 require './lib/player'
 
 class PlayerTest < Minitest::Test
-
   def test_it_exists
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
@@ -16,4 +15,16 @@ class PlayerTest < Minitest::Test
 
     assert_instance_of Player, player
   end
+
+  def test_it_has_a_name
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+
+    assert_equal 'Clarisa', player.name
+  end
+
 end
