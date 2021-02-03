f9436ddc7a0b02ccfe910fe2dbf7dcb8ce7d10e2
Ryan Stemmle <ryan.stemmle@gmail.com>
2021-02-02 23:57:18 -0500

    Modify deck lass to accept *cards

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,18 @@
 class Turn
+  attr_reader :player1,
+              :player2,
+              :spoils_of_war
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @spoils_of_war = []
+  end
+
+  def players_names
+    [@player1.name, player2.name]
+  end
+
+
 
 end
