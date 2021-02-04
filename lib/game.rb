51a09cb788267ced5278c2b5a47794ae846eb243
George Ho <george.h.w.ho@gmail.com>
2021-02-04 14:52:07 -0500

    Refactor implemented turn changes

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,6 @@
 class Game
   attr_reader :turn
+
   def initialize(t)
     @turn = t
   end
@@ -7,12 +8,12 @@ class Game
   def start
     # game logic
     turn_counter = 0
-    while !turn.player1.has_lost? && !turn.player2.has_lost? && turn_counter < 1000000
+    while turn.valid_game? && turn_counter < 1000000
       turn_counter += 1
       the_winner = turn.winner
-      if turn.type == :mutually_assured_destruction
+      if turn.mutually_assured_destruction?
         puts "Turn #{turn_counter}: *mutually assured destruction* 6 cards removed from play"
-      elsif turn.type == :war
+      elsif turn.war?
         puts "Turn #{turn_counter}: WAR - #{the_winner.name} won 6 cards"
       else
         puts "Turn #{turn_counter}: #{the_winner.name} won 2 cards"
@@ -20,7 +21,7 @@ class Game
 
       turn.pile_cards
       turn.award_spoils(the_winner)
-      puts "player1's cards: #{turn.player1.deck.cards.size} || player2's cards: #{turn.player2.deck.cards.size}"
+      turn.player_stats
     end
 
     if turn.winner == 'No Winner' || turn_counter == 1000000
