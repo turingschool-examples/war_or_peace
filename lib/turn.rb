9add9b7fb14123641943fd71b866fa32baeb7b0d
ignored-comment <info.konham@gmail.com>
2021-02-03 19:55:57 -0500

    Add winner & return_bigger_winner method; passing tests

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,4 +6,38 @@ class Turn
     @player2 = player2
     @spoils_of_war = []
   end
+
+  def type
+    player1_at_0 = player1.deck.rank_of_card_at(0)
+    player1_at_2 = player1.deck.rank_of_card_at(2)
+    player2_at_0 = player2.deck.rank_of_card_at(0)
+    player2_at_2 = player2.deck.rank_of_card_at(2)
+
+    if player1_at_0 == player2_at_0 && player1_at_2 == player1_at_2
+      :mutually_assured_destruction
+    elsif player1_at_0 == player2_at_0
+      :war
+    else
+      :basic
+    end
+  end
+
+  def winner
+    if type == :mutually_assured_destruction
+      "No Winner"
+    elsif type == :war
+      return_bigger_winner(2)
+    elsif type == :basic
+      return_bigger_winner(0)
+    end
+  end
+
+  def return_bigger_winner(index)
+    if player1.deck.rank_of_card_at(index) > player2.rank_of_card_at(index)
+      player1
+    else
+      player2
+    end
+  end
+
 end
