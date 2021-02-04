7f0451c3123be52c214d146bdf09ca045103a764
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-02-04 06:20:54 -0600

    Edit has_lost? method

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,19 @@
 class Player
-  attr_reader :player_name
+  attr_reader :name
 
-  def initialize(player_name, deck)
-    @player_name = player_name
+
+  def initialize(name, deck)
+    @name = name
+    @has_lost = false
+    @deck = deck
   end
+
+  def has_lost?
+    @has_lost = deck.cards.empty?
+  end
+
+  def deck
+    @deck
+  end
+
 end
