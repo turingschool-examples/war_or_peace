776d68cfae842bce04244d566ef721d109bdd758
Ryan Stemmle <ryan.stemmle@gmail.com>
2021-02-04 15:53:51 -0500

    Test pile_cards in turn class

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,17 +8,47 @@ class Turn
     @player1 = player1
     @player2 = player2
     @spoils_of_war = []
-    @type = :basic
+    @type = [:basic, :war, :mutually_assured_destruction]
+  end
+
+  def pile_cards
+      if    turn_type == :mutually_assured_destruction
+      elsif turn_type == :war
+      else  turn_type == :basic
+    end
+  end
+
+  def turn_type
+    if    player1.deck.cards[0].rank == player2.deck.cards[0].rank &&
+          player1.deck.cards[2].rank == player2.deck.cards[2].rank
+      @type[2]
+    elsif player1.deck.cards[0].rank == player2.deck.cards[0].rank
+      @type[1]
+    else  player1.deck.cards[0].rank != player2.deck.cards[0].rank
+      @type[0]
+    end
   end
 
   def winner
-    if player1.deck.remove_card.rank > player2.deck.remove_card.rank
-      player1
+    if    turn_type == :mutually_assured_destruction
+      "No Winner"
+    elsif turn_type == :war
+      if player1.deck.cards[2].rank > player2.deck.cards[2].rank
+        player1
+      else
+        player2
+      end
+    elsif turn_type == :basic
+      if player1.deck.remove_card.rank > player2.deck.remove_card.rank
+        player1
+      else
+        player2
+      end
     end
   end
 
   def players_names
-    [player1.name, player2.name]
+    [@player1.name, @player2.name]
   end
 
 
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -7,14 +7,16 @@ require './lib/turn'
 
 class TurnTest < Minitest::Test
   def setup
-    @card1 = Card.new(:heart, "Jack", 11)
-    @card2 = Card.new(:heart, "10", 10)
-    @card3 = Card.new(:heart, "9", 9)
-    @card4 = Card.new(:diamond, "Jack", 11)
-    @card5 = Card.new(:heart, "8", 8)
-    @card6 = Card.new(:diamond, "Queen", 12)
-    @card7 = Card.new(:heart, "3", 3)
-    @card8 = Card.new(:diamond, "2", 2)
+    @card1 =  Card.new(:heart, "Jack", 11)
+    @card2 =  Card.new(:heart, "10", 10)
+    @card3 =  Card.new(:heart, "9", 9)
+    @card4 =  Card.new(:diamond, "Jack", 11)
+    @card5 =  Card.new(:heart, "8", 8)
+    @card6 =  Card.new(:diamond, "Queen", 12)
+    @card7 =  Card.new(:heart, "3", 3)
+    @card8 =  Card.new(:diamond, "2", 2)
+    @card9 =  Card.new(:spade, "10", 10)
+    @card10 = Card.new(:heart, "5", 5)
   end
 
   def test_it_exists
@@ -31,7 +33,7 @@ class TurnTest < Minitest::Test
     player1 = Player.new("Megan", deck1)
     player2 = Player.new("Aurora", deck2)
     turn = Turn.new(player1, player2)
-
+# require "pry"; binding.pry
     assert_instance_of Turn, turn
   end
 
@@ -71,40 +73,84 @@ class TurnTest < Minitest::Test
     assert_equal [], turn.spoils_of_war
   end
 
-  def test_it_is_basic_turn_by_default
-    @card1
-    @card2
-    @card3
-    @card4
-    @card5
-    @card6
-    @card7
-    @card8
+  def test_it_can_be_basic_turn
+
     deck1 = Deck.new(@card1, @card2, @card5, @card8)
     deck2 = Deck.new(@card3, @card4, @card6, @card7)
     player1 = Player.new("Megan", deck1)
     player2 = Player.new("Aurora", deck2)
     turn = Turn.new(player1, player2)
 
-    assert_equal :basic, turn.type
+    assert_equal :basic, turn.turn_type
   end
 
-  def test_player_with_higher_cards_is_winner
-    @card1
-    @card2
-    @card3
-    @card4
-    @card5
-    @card6
-    @card7
-    @card8
+  def test_it_can_be_war_type
+    # skip
+    deck1 = Deck.new(@card1, @card2, @card5, @card8)
+    deck2 = Deck.new(@card4, @card3, @card6, @card7)
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal :war, turn.turn_type
+  end
+
+  def test_it_can_be_mutually_assured_destruction_type
+    # skip
+
+    deck1 = Deck.new(@card1, @card5, @card2, @card8, @card10)
+    deck2 = Deck.new(@card4, @card3, @card9, @card6, @card7)
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal :mutually_assured_destruction, turn.turn_type
+  end
+
+  def test_winner_works_for_basic_turn
+    # skip
     deck1 = Deck.new(@card1, @card2, @card5, @card8)
     deck2 = Deck.new(@card3, @card4, @card6, @card7)
     player1 = Player.new("Megan", deck1)
     player2 = Player.new("Aurora", deck2)
     turn = Turn.new(player1, player2)
-# require "pry"; binding.pry
+
     assert_equal player1, turn.winner
+  end
+
+  def test_winner_works_for_war_turn
+    # skip
+    deck1 = Deck.new(@card1, @card2, @card5, @card8)
+    deck2 = Deck.new(@card4, @card3, @card6, @card7)
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal player2, turn.winner
+  end
+
+  def test_no_winner_works_for_mutually_assured_destruction
+    # skip
+    deck1 = Deck.new(@card1, @card5, @card2, @card8)
+    deck2 = Deck.new(@card4, @card3, @card9, @card7)
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    assert_equal "No Winner", turn.winner
+  end
+
+  def test_basic_play_results_two_new_cards_in_spoils
+    # skip
+    deck1 = Deck.new(@card1, @card5, @card2, @card8)
+    deck2 = Deck.new(@card4, @card3, @card9, @card7)
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    turn.pile_cards
+
+    assert_equal [@card1, @card4], turn.spoils_of_war
   end
 
 
