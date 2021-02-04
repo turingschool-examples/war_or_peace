1717b0bd104d95501a2004a1f93f1411adf92e3f
Klaudia Stewart <klaudiastewart@gmail.com>
2021-02-03 21:11:11 -0700

    add two tests to player

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,21 @@
 class Player
-  def initialize(deck)
+  attr_reader :name,
+              :deck
+  def initialize(name, deck = [])
+    @name = name
     @deck = deck
   end
+
+  # def has_lost?
+  #   if deck == []
+  #     return true
+  #   else
+  #     return false
+  #   end
+  # end
+
+  def has_lost?
+    @deck.cards == []
+  end
+  # Can use lines 9-15 or lines 17-19 for test_has_lost. Anytime a method has a '?', you can assume that is Boolean and will return t/f.
 end
--- test/player_test.rb
+++ test/player_test.rb
@@ -11,10 +11,24 @@ class PlayerTest < Minitest::Test
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
     cards = [card1, card2, card3]
-    deck = Deck.new([card1, card2, card3])
+    deck = Deck.new(cards)
     player = Player.new('Clarisa', deck)
 
     assert_instance_of Player, player
-    assert_equal cards, player.deck
+    assert_instance_of Deck, deck
+    assert_equal [card1, card2, card3], player.deck.cards
+    #player.deck.cards <- deck is an object and cards is an attribute of that deck in which cards have 3 instances
+  end
+
+  def test_has_lost
+  # skip
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    player = Player.new('Clarisa', deck)
+
+    assert_equal false, player.has_lost?
   end
 end
