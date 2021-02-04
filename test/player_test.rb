08a82c4242efc5ac7eb2789cc33a37c39e1e0c36
ignored-comment <info.konham@gmail.com>
2021-02-04 15:23:59 -0500

    Refactor assertions for readability

--- test/player_test.rb
+++ test/player_test.rb
@@ -5,70 +5,37 @@ require './lib/deck'
 require './lib/player'
 
 class PlayerTest < Minitest::Test
+  def setup
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+
+    @deck = Deck.new([@card1, @card2, @card3])
+    @player = Player.new('Clarisa', @deck)
+  end
+
   def test_it_exists
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-
-    deck = Deck.new([card1, card2, card3])
-    player = Player.new('Clarisa', deck)
-
-    assert_instance_of Player, player
+    assert_instance_of Player, @player
   end
 
   def test_it_has_a_name
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-
-    deck = Deck.new([card1, card2, card3])
-    player = Player.new('Clarisa', deck)
-
-    assert_equal 'Clarisa', player.name
+    assert_equal 'Clarisa', @player.name
   end
 
   def test_it_has_a_deck
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-
-    deck = Deck.new([card1, card2, card3])
-    player = Player.new('Clarisa', deck)
-
-    assert_equal deck, player.deck
+    assert_equal @deck, @player.deck
   end
 
   def test_player_has_not_lost_as_a_default
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-
-    deck = Deck.new([card1, card2, card3])
-    player = Player.new('Clarisa', deck)
-
-    assert_equal false, player.has_lost?
+    assert_equal false, @player.has_lost?
   end
 
   def test_removes_top_card_from_deck
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-
-    deck = Deck.new([card1, card2, card3])
-    player = Player.new('Clarisa', deck)
-
-    assert_equal card1, player.deck.remove_card
+    assert_equal @card1, @player.deck.remove_card
   end
 
   def test_player_loses_when_no_more_cards_in_deck
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-
-    deck = Deck.new([card1, card2, card3])
-    player = Player.new('Clarisa', deck)
-
-    3.times {|remove| player.deck.remove_card}
-    assert_equal true, player.has_lost?
+    3.times {|remove| @player.deck.remove_card}
+    assert_equal true, @player.has_lost?
   end
 end
