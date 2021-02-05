28f134bdade482651b6939cfca016df95e4d8271
ignored-comment <info.konham@gmail.com>
2021-02-05 15:07:25 -0500

    Create turn_message, final_message method

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,31 @@
 class Game
+  attr_reader :player1, :player2, :turn_number
 
-  def initialize(turn)
-    @turn = turn
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @turn_number = 1
   end
+
+  def turn_messages(turn_number, turn_type, turn_winner)
+    if turn_type == :mutually_assured_destruction
+      "Turn #{turn_number}: *mutually assured destruction* 6 cards removed from play"
+    elsif turn_type == :basic
+      "Turn #{turn_number}: #{turn_winner} won 2 cards"
+    elsif turn_type == :war
+      "Turn #{turn_number}: WAR - #{turn_winner} won 6 cards"
+    end
+  end
+
+  def final_message
+    if player1.has_lost?
+      "*~*~*~* #{turn.player2.winner} has won the game! *~*~*~*"
+    elsif player2.has_lost?
+      "*~*~*~* #{turn.player1.winner} has won the game! *~*~*~*"
+    else
+      "---- DRAW ----"
+    end
+  end
+
+
 end
