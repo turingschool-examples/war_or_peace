f6dd5064046a8e22dc4d0e8abafb70763bde4fd0
ignored-comment <info.konham@gmail.com>
2021-02-05 15:08:11 -0500

    Test readable_attributes; pass, shows_turn_messages; fail

--- test/game_test.rb
+++ test/game_test.rb
@@ -20,14 +20,39 @@ class GameTest < Minitest::Test
     @deck1 = Deck.new([@card1, @card2, @card5, @card8])
     @deck2 = Deck.new([@card3, @card4, @card6, @card7])
 
-    @player1 = Player.new("Megan", @deck1)
-    @player2 = Player.new("Aurora", @deck2)
+    @player1 = Player.new("Player 1", @deck1)
+    @player2 = Player.new("Player 2", @deck2)
 
-    @turn = Turn.new(@player1, @player2)
-    @game = Game.new(@turn)
+    @game = Game.new(@player1, @player2)
   end
 
   def test_it_exists
+    # skip
     assert_instance_of Game, @game
   end
+
+  def test_readable_attributes
+    # skip
+    assert_equal @player1, @game.player1
+    assert_equal @player2, @game.player2
+    assert_equal 1, @game.turn_number
+  end
+
+
+  def test_turn_shows_turn_messages
+    # skip
+    assert_equal "Turn 5: WAR - Player 1 won 6 cards", @game.turn_messages(5, :war, "Player 1")
+  end
+
+  def test_returns_draw
+    # skip
+
+    assert_equal "---- DRAW ----", @game.final_message
+  end
+
+  def test_returns_win
+    skip
+  end
+
+
 end
