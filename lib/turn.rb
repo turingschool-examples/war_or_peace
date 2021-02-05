b08fffbfb285bfafb14d457824b1e113a2721e70
Alex Klick <aklick12@gmail.com>
2021-02-05 13:03:18 -0700

    turn type ready for testing

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,5 +4,29 @@ class Turn
         @player2 = player2
         @player1 = player1
         @spoils_of_war = []
+        @type = ''
+    end
+    def type
+        if [@player1.deck.cards[-1],@player2.deck.cards[-1]].min < 2
+            return 'game over'
+        elsif @player1.deck.cards[-1] != @player2.deck.cards[-1]
+            @type = 'basic'
+        elsif [@player1.deck.cards[-1].length,@player2.deck.cards[-1].length].min < 4
+            if [@player1.deck.cards[-1].length,@player2.deck.cards[-1].length].min < 3
+                @type = 'gg'
+            elsif [@player1.deck.cards[-1].length,@player2.deck.cards[-1].length].min == 3
+                if @player1.deck.cards[-3] == @player2.deck.cards[-3]
+                    @type = 'gg'
+                else
+                    @type = 'war'
+                end
+            end
+        else 
+            if @player1.deck.cards[-3] == @player2.deck.cards[-3]
+                @type = 'mad'
+            else
+                @type = 'war'
+            end
+        end
     end
 end
