4ceabef1bd08550b6aeab84803e37d7d463a1b64
Klaudia Stewart <klaudiastewart@gmail.com>
2021-02-04 19:25:07 -0700

    fix basic and war turn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,7 +11,7 @@ class Turn
   def type
     if player1.deck.rank_of_card_at(0) !=  player2.deck.rank_of_card_at(0)
       :basic
-    elsif player1.deck.rank_of_card_at(0) !=  player2.deck.rank_of_card_at(0)
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
       :war
     else
       :mutually_assured_destruction
@@ -19,22 +19,22 @@ class Turn
   end
 
   def winner
-    if self.type == :basic && player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0) ||
-      self.type == :war && player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+    if type == :basic && player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0) ||
+      type == :war && player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
       player1
-    elsif self.type == :basic && player2.deck.rank_of_card_at(0) > player1.deck.rank_of_card_at(0) ||
-      self.type == :war && player2.deck.rank_of_card_at(2) > player1.deck.rank_of_card_at(2)
+    elsif type == :basic && player2.deck.rank_of_card_at(0) > player1.deck.rank_of_card_at(0) ||
+      type == :war && player2.deck.rank_of_card_at(2) > player1.deck.rank_of_card_at(2)
       player2
-    elsif self.type == :mutually_assured_destruction
+    elsif type == :mutually_assured_destruction
       'No Winner'
     end
   end
 
   def pile_cards
-    if self.type == :basic
+    if type == :basic
       @spoils_of_war.push(player1.deck.cards.shift)
       @spoils_of_war.push(player2.deck.cards.shift)
-    elsif self.type == :war
+    elsif type == :war
       @spoils_of_war.push(player1.deck.cards.shift(3))
       @spoils_of_war.push(player2.deck.cards.shift(3))
     elsif self.type == :mutually_assured_destruction
@@ -44,7 +44,11 @@ class Turn
   end
 
   def award_spoils(winner)
-    winner.deck.cards.push(@spoils_of_war.shift(3))
-    winner.deck.cards.flatten!
-  end
+     if type == :mutually_assured_destruction
+       return
+     end
+     winner.deck.cards << @spoils_of_war
+     winner.deck.cards == winner.deck.cards.flatten
+     @spoils_of_war.clear
+   end
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -52,7 +52,7 @@ class TurnTest < MiniTest::Test
     assert_equal [], turn.spoils_of_war
   end
 
-  def test_type_of_turn
+  def test_basic_turn
     # skip
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
@@ -70,36 +70,26 @@ class TurnTest < MiniTest::Test
     player2 = Player.new("Aurora", deck2)
 
     turn = Turn.new(player1, player2)
-
-    assert_equal :basic, turn.type
-  end
-
-  def test_if_winner
-    # skip
-    card1 = Card.new(:heart, 'Jack', 11)
-    card2 = Card.new(:heart, '10', 10)
-    card3 = Card.new(:heart, '9', 9)
-    card4 = Card.new(:diamond, 'Jack', 11)
-    card5 = Card.new(:heart, '8', 8)
-    card6 = Card.new(:diamond, 'Queen', 12)
-    card7 = Card.new(:heart, '3', 3)
-    card8 = Card.new(:diamond, '2', 2)
-
-    deck1 = Deck.new([card1, card2, card5, card8])
-    deck2 = Deck.new([card3, card4, card6, card7])
-
-    player1 = Player.new("Megan", deck1)
-    player2 = Player.new("Aurora", deck2)
-
-    turn = Turn.new(player1, player2)
-
     winner = turn.winner
+
+    assert_equal player1, turn.player1
+    assert_equal player2, turn.player2
+    assert_equal [], turn.spoils_of_war
+    assert_equal :basic, turn.type
+    assert_equal player1, turn.winner
 
     turn.pile_cards
-    assert_equal 2, turn.spoils_of_war.count
+
+    assert_equal [card1, card3], turn.spoils_of_war
+
+    turn.award_spoils(winner)
+
+    assert_equal [card2, card5, card8, card1, card3], turn.player1.deck.cards
+    #could I write, `assert_equal [deck1], turn.player1.deck` ?
+    assert_equal [card4, card6, card7], turn.player2.deck.cards
   end
 
-  def test_what_player_won
+  def test_war_turn
     # skip
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
@@ -111,18 +101,98 @@ class TurnTest < MiniTest::Test
     card8 = Card.new(:diamond, '2', 2)
 
     deck1 = Deck.new([card1, card2, card5, card8])
-    deck2 = Deck.new([card3, card4, card6, card7])
+    deck2 = Deck.new([card4, card3, card6, card7])
 
     player1 = Player.new("Megan", deck1)
     player2 = Player.new("Aurora", deck2)
 
     turn = Turn.new(player1, player2)
-
     winner = turn.winner
 
+    assert_equal :war, turn.type
+    assert_equal player2, turn.winner
+
+    turn.pile_cards
+
+    assert_equal [card1, card2, card5, card4, card3, card6], turn.spoils_of_war
+
     turn.award_spoils(winner)
-    assert_equal [], turn.spoils_of_war
-    assert_equal 4, player1.deck.cards.count
-    assert_equal [], player2.deck.cards
-  end
+
+    assert_equal [card8], turn.player1.deck.cards
+    assert_equal [card7, card1, card2, card5, card4, card3, card6], turn.player2.deck.cards
+  end 
 end
+#   def test_type_of_turn
+#     # skip
+#     card1 = Card.new(:heart, 'Jack', 11)
+#     card2 = Card.new(:heart, '10', 10)
+#     card3 = Card.new(:heart, '9', 9)
+#     card4 = Card.new(:diamond, 'Jack', 11)
+#     card5 = Card.new(:heart, '8', 8)
+#     card6 = Card.new(:diamond, 'Queen', 12)
+#     card7 = Card.new(:heart, '3', 3)
+#     card8 = Card.new(:diamond, '2', 2)
+#
+#     deck1 = Deck.new([card1, card2, card5, card8])
+#     deck2 = Deck.new([card3, card4, card6, card7])
+#
+#     player1 = Player.new("Megan", deck1)
+#     player2 = Player.new("Aurora", deck2)
+#
+#     turn = Turn.new(player1, player2)
+#
+#     assert_equal :basic, turn.type
+#   end
+#
+#   def test_if_winner
+#     # skip
+#     card1 = Card.new(:heart, 'Jack', 11)
+#     card2 = Card.new(:heart, '10', 10)
+#     card3 = Card.new(:heart, '9', 9)
+#     card4 = Card.new(:diamond, 'Jack', 11)
+#     card5 = Card.new(:heart, '8', 8)
+#     card6 = Card.new(:diamond, 'Queen', 12)
+#     card7 = Card.new(:heart, '3', 3)
+#     card8 = Card.new(:diamond, '2', 2)
+#
+#     deck1 = Deck.new([card1, card2, card5, card8])
+#     deck2 = Deck.new([card3, card4, card6, card7])
+#
+#     player1 = Player.new("Megan", deck1)
+#     player2 = Player.new("Aurora", deck2)
+#
+#     turn = Turn.new(player1, player2)
+#
+#     winner = turn.winner
+#
+#     turn.pile_cards
+#     assert_equal 2, turn.spoils_of_war.count
+#   end
+#
+#   def test_what_player_won
+#     # skip
+#     card1 = Card.new(:heart, 'Jack', 11)
+#     card2 = Card.new(:heart, '10', 10)
+#     card3 = Card.new(:heart, '9', 9)
+#     card4 = Card.new(:diamond, 'Jack', 11)
+#     card5 = Card.new(:heart, '8', 8)
+#     card6 = Card.new(:diamond, 'Queen', 12)
+#     card7 = Card.new(:heart, '3', 3)
+#     card8 = Card.new(:diamond, '2', 2)
+#
+#     deck1 = Deck.new([card1, card2, card5, card8])
+#     deck2 = Deck.new([card3, card4, card6, card7])
+#
+#     player1 = Player.new("Megan", deck1)
+#     player2 = Player.new("Aurora", deck2)
+#
+#     turn = Turn.new(player1, player2)
+#
+#     winner = turn.winner
+#
+#     turn.award_spoils(winner)
+#     assert_equal [], turn.spoils_of_war
+#     assert_equal 4, player1.deck.cards.count
+#     assert_equal [], player2.deck.cards
+#   end
+# end
