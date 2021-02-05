37e47d424f09af831129c89101dceb09096278b3
Olivia Charles <ocharles92@gmail.com>
2021-02-04 21:32:42 -0500

    running start

--- lib/game.rb
+++ lib/game.rb
@@ -17,18 +17,32 @@ class Game
   end
 
   def start
-    count = 0
-    while count < 100000000
-    count += 1
+    count = 1
+    while count < 100
     turn = Turn.new(@player1, @player2)
+    winner = turn.winner
     if turn.type == :mutually_assured_destruction
       p "Turn #{count} mutually assured destruction, Stalement
             6 cards have been completely destroyed"
+            turn.pile_cards
     elsif turn.type == :war
       p "Turn #{count}: #{turn.winner.name} won the battle and gains 6 cards."
+      turn.pile_cards
+      turn.award_spoils(winner)
     else turn.type == :basic
       p "Turn #{count}: #{turn.winner.name} won the skirmish and gains 2 cards."
     end
-  end
-  end
+    turn.pile_cards
+    turn.award_spoils(winner)
+
+    if turn.player1.has_lost?
+      p "*~*~*~*~ #{player2.name} has won the war. *~*~*~*~ "
+    else turn.player2.has_lost?
+      p  "*~*~*~*~ #{player1.name} has won the war. *~*~*~*~ "
+    end
+
+
+    count += 1
+   end
+ end
 end
