ccd26c1905b93adf230543931357c023939d4353
Olivia Charles <ocharles92@gmail.com>
2021-02-05 15:23:53 -0500

    Refactored to use a loop on the pile cards method, and made a conditional statement for winner

--- lib/turn.rb
+++ lib/turn.rb
@@ -24,7 +24,7 @@ class Turn
   def basic_winner
     if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
       player1
-    else player1.deck.rank_of_card_at(0) < player2.deck.rank_of_card_at(2)
+    else
       player2
     end
   end
@@ -32,7 +32,7 @@ class Turn
   def war_winner
     if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
       player1
-    else player1.deck.rank_of_card_at(2) < player2.deck.rank_of_card_at(2)
+    else
       player2
     end
   end
@@ -43,17 +43,21 @@ class Turn
     elsif type == :war
       war_winner
     else type == :basic
-       basic_winner
+      basic_winner
     end
   end
 
   def pile_cards
     if type == :mutually_assured_destruction
-      player1.deck.remove_card(3)
-      player2.deck.remove_card(3)
+      3.times do
+      player1.deck.remove_card
+      player2.deck.remove_card
+    end
     elsif type == :war
-      @spoils_of_war << player1.deck.remove_card(3)
-      @spoils_of_war << player2.deck.remove_card(3)
+      3.times do
+      @spoils_of_war << player1.deck.remove_card
+      @spoils_of_war << player2.deck.remove_card
+    end
     else type == :basic
       @spoils_of_war << player1.deck.remove_card
       @spoils_of_war << player2.deck.remove_card
@@ -61,6 +65,10 @@ class Turn
   end
 
   def award_spoils(winner)
-    winner.deck.add_card(@spoils_of_war)
+    if winner == player1
+      player1.deck.cards.push(@spoils_of_war)
+    else winner == player2
+      player2.deck.cards.push(@spoils_of_war)
+    end
   end
 end
