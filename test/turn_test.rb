1f9eeb407bff9fcdec0e56ec12a99990fd329ffe
Alex Klick <aklick12@gmail.com>
2021-02-05 12:35:09 -0700

    added test for attributes test_players

---
+++ test/turn_test.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/deck'
+require './lib/card'
+require './lib/player'
+require './lib/turn'
+
+class PlayerTest < Minitest::Test
+  def setup 
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @card4 = Card.new(:diamond, 'Jack', 11)
+    @card5 = Card.new(:heart, '8', 8)
+    @card6 = Card.new(:diamond, 'Queen', 12)
+    @deck1 = Deck.new([@card3, @card2, @card1])
+    @deck2 = Deck.new([@card6, @card5, @card4])
+    @player1 = Player.new("Alex",@deck1)
+    @player2 = Player.new("AK",@deck2)
+  end
+  def test_it_exists
+    assert_instance_of Player, @player
+  end
+  def test_lost
+    assert_equal @player.has_lost?, false
+    @player.deck.cards.clear
+    assert_equal @player.has_lost?, true
+  end
+end
