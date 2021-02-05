35b538c1fd387468460bf6d01b5fbeffadc1b336
Olivia Charles <ocharles92@gmail.com>
2021-02-05 15:23:17 -0500

    refactored to tell if rank of card at  nil to reply with 0

--- lib/deck.rb
+++ lib/deck.rb
@@ -5,8 +5,12 @@ class Deck
     @cards = cards
   end
 
-  def rank_of_card_at(cards)
-    @cards[cards].rank
+  def rank_of_card_at(index)
+    if @cards[index].nil?
+      0
+    else
+      @cards[index].rank
+    end
   end
 
   def high_ranking_cards
