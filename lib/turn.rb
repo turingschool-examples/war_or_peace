9fca94b0142b603a58173704be613facb3238079
Alex Klick <aklick12@gmail.com>
2021-02-05 23:26:14 -0700

    turn updated to pass test

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,49 +1,77 @@
 class Turn
-    attr_reader :spoils_of_war, :player1, :player2
+    attr_reader :spoils_of_war, :player1, :player2, :winner, :turn_type, :winner_of_game
     def initialize(player1, player2)
         @player2 = player2
         @player1 = player1
         @spoils_of_war = []
+        @turn_type = self.type
+        @winner = self.winner
+        @turn_count = 0
     end
     def type
-        if [@player1.deck.cards.length,@player2.deck.cards.length].min < 2
-            return 'game over'
+        if [@player1.deck.cards.length,@player2.deck.cards.length].min < 1
+            return @turn_type = 'game over'
         elsif @player1.deck.cards[-1].rank != @player2.deck.cards[-1].rank
-            return'basic'
+            return @turn_type = :basic
         elsif [@player1.deck.cards.length,@player2.deck.cards.length].min < 4
             if [@player1.deck.cards.length,@player2.deck.cards.length].min < 3
-                return 'gg'
+                return @turn_type = 'gg'
             elsif [@player1.deck.cards.length,@player2.deck.cards.length].min == 3
                 if @player1.deck.cards[-3].rank == @player2.deck.cards[-3].rank
-                    return 'gg'
+                    return @turn_type = 'gg'
                 else
-                    return 'war'
+                    return @turn_type = :war
                 end
             end
         else 
             if @player1.deck.cards[-3] == @player2.deck.cards[-3]
-                return 'mad'
+                return @turn_type = :mad
             else
-                return 'war'
+                return @turn_type = :war
             end
         end
     end
     def winner
-        if self.type == 'basic'
-            return @player1.deck.cards[-1].rank > @player2.deck.cards[-1].rank ? @player1.name : @player2.name
-        elsif self.type == 'war'
-            return @player1.deck.cards[-3].rank > @player2.deck.cards[-3].rank ? @player1.name : @player2.name
-        elsif self.type == 'mad'
-            return 'No Winner'
+        p @winner
+        if @turn_count == 1
+            return @winner_of_game
+        elsif @turn_type == :basic
+            return @winner_of_game = @player1.deck.cards[-1].rank > @player2.deck.cards[-1].rank ? @player1 : @player2
+        elsif @turn_type == :war
+            return @winner_of_game = @player1.deck.cards[-3].rank > @player2.deck.cards[-3].rank ? @player1 : @player2
+        else
+            return @winner_of_game = 'No Winner'
         end
     end
     def pile_cards
-        if self.winner != 'No Winner'
-            if self.type == 'war'
-                @spoils_of_war.concat([@player1.deck.cards.pop(3), @player2.deck.cards.pop(3)])
+        self.winner
+        @turn_count = 1
+        if @winner != 'No Winner'
+            if @turn_type == 'war'
+                @spoils_of_war.concat(@player1.deck.cards.pop(3) + @player2.deck.cards.pop(3))
+                p @spoils_of_war
+                return @spoils_of_war
             else
-                @spoils_of_war.concat([@player1.deck.cards.pop, @player2.deck.cards.pop])
+                @spoils_of_war.concat(@player1.deck.cards.pop(1) + @player2.deck.cards.pop(1))
+                return @spoils_of_war
             end
+        else
+            @player1.deck.cards.pop(3)
+            @player2.deck.cards.pop(3)
+            return 
+        end
+    end
+    def award_spoils 
+        self.winner
+        if @spoils_of_war != []
+            @spoils_of_war.concat(@winner_of_game.deck.cards)
+            @winner_of_game.deck.cards.clear
+            @winner_of_game.deck.cards.concat(@spoils_of_war)
+            @turn_count = 0
+            @spoils_of_war.clear
+            return 
+        else
+            return nil
         end
     end
 end
