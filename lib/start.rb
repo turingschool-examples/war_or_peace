c694fb975c7bbbdc3a97940613e939b8fb56d8f4
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-02-06 22:53:15 -0600

    Test to see if game runs - works now

--- lib/start.rb
+++ lib/start.rb
@@ -8,16 +8,18 @@ class Start
 
   def start_game
     if turn.type == :mutually_assured_destruction
-      turn.pile_cards
+      # turn.pile_cards
       puts "Turn #{@turn_count += 1}: *mutually assured destruction* 6 cards removed from play"
     elsif turn.type == :basic
-      turn.pile_cards
-      turn.award_spoils
+      # turn.pile_cards
+      # turn.award_spoils
       puts "Turn #{@turn_count += 1}: #{turn.winner.name} won 2 cards"
     elsif turn.type == :war
-      turn.pile_cards
-      turn.award_spoils
+      # turn.pile_cards
+      # turn.award_spoils
       puts "Turn #{@turn_count += 1}: WAR - #{turn.winner.name} won 6 cards"
     end
+    turn.pile_cards
+    turn.award_spoils
   end
 end
