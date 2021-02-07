b3c224bca1e89c832f46169cd20d3a7a47d8daac
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-02-07 14:52:17 -0600

    Try to fix bug in start_game method

--- lib/start.rb
+++ lib/start.rb
@@ -1,34 +1,37 @@
 class Start
-  attr_reader :turn, :turn_count
+  attr_reader :turn
 
   def initialize(turn)
     @turn = turn
     @turn_count = 0
   end
 
-  def start_game
-    if turn.type == :mutually_assured_destruction
-      # turn.pile_cards
-      puts "Turn #{@turn_count += 1}: *mutually assured destruction* 6 cards removed from play"
-    elsif turn.type == :basic
-      # turn.pile_cards
-      # turn.award_spoils
-      puts "Turn #{@turn_count += 1}: #{turn.winner.name} won 2 cards"
-    elsif turn.type == :war
-      # turn.pile_cards
-      # turn.award_spoils
-      puts "Turn #{@turn_count += 1}: WAR - #{turn.winner.name} won 6 cards"
-    end
-    turn.pile_cards
-    turn.award_spoils
+  def turn_count
+    @turn_count += 1
   end
+
+  def start_game
+    say_turn_winner
+    turn.award_spoils(turn.winner)
+  end
+
+  def say_turn_winner
+    if turn.type == :mutually_assured_destruction
+      p "Turn #{turn_count}: *mutually assured destruction* 6 cards removed from play"
+    elsif turn.type == :basic
+      p "Turn #{turn_count}: #{turn.winner.name} won 2 cards"
+    elsif turn.type == :war
+      p "Turn #{turn_count}: WAR - #{turn.winner.name} won 6 cards"
+    end
+  end
+
 
   def announce_winner
     if turn.player1.has_lost?
       puts "*~*~*~* #{turn.player2.name} has won the game! *~*~*~*"
     elsif turn.player2.has_lost?
       puts "*~*~*~* #{turn.player1.name} has won the game! *~*~*~*"
-    elsif @turn_count > 1000000
+    elsif @turn_count == 1000001
       "---- DRAW ----"
     end
   end
