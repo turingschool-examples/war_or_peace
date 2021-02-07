d0fd2188f1ad8d9b6056ce2e15855ff1513c3494
Joe Ray <jray100.jr@gmail.com>
2021-02-07 11:38:04 -0700

    Iteration 2

--- lib/turn.rb
+++ lib/turn.rb
@@ -38,34 +38,27 @@ class Turn
   end
 
   def pile_cards
-    if @type == :basic
-      @spoils_of_war.unshift(@player2.deck.cards[0])
-      @player2.deck.cards.delete_at(0)
-      @spoils_of_war.unshift(@player1.deck.cards[0])
-      @player1.deck.cards.delete_at(0)
-    elsif @type == :war
-      @spoils_of_war.unshift(@player1.deck.cards[0])
-      @spoils_of_war.unshift(@player1.deck.cards[0])
-      @spoils_of_war.unshift(@player1.deck.cards[0])
-      @player1.deck.cards.delete_at(0)
-      @player1.deck.cards.delete_at(0)
-      @player1.deck.cards.delete_at(0)
-      @spoils_of_war.unshift(@player2.deck.cards[0])
-      @spoils_of_war.unshift(@player2.deck.cards[0])
-      @spoils_of_war.unshift(@player2.deck.cards[0])
-      @player2.deck.cards.delete_at(0)
-      @player2.deck.cards.delete_at(0)
-      @player2.deck.cards.delete_at(0)
-    else @type == :mutually_assured_destruction
-      'No Winner'
+    if type == :mutually_assured_destruction
+      3.times do
+        @player1.deck.remove_card
+        @player2.deck.remove_card
       end
+    elsif type == :war
+      3.times do
+        @spoils_of_war << @player1.deck.remove_card
+        @spoils_of_war << @player2.deck.remove_card
+      end
+    else type == :basic
+      @spoils_of_war << @player1.deck.remove_card
+      @spoils_of_war << @player2.deck.remove_card
     end
+  end
 
     def award_spoils(winner)
       @spoils_of_war.each do |card|
       winner.deck.cards << card
     end
-      winner.deck.cards
+      @spoils_of_war = []
     end
 
 end
