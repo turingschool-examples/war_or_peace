e161d847bf81182d111770b1b5f89c93ee200fe2
kevn7n <>
2021-02-06 19:44:41 -0700

    Game.rb 1.0 2/6/21

---
+++ lib/game.rb
@@ -0,0 +1,63 @@
+class Game
+  attr_reader :player1, :player2
+
+  def initialize(player1,player2)
+    @player1 = player1
+    @player2 = player2
+  end
+
+  def start_a_war
+    line = 1
+    result = nil
+    # require "pry"; binding.pry
+    until player1.deck.cards.size == 3 || player2.deck.cards.size == 3
+      turn = Turn.new(player1, player2)
+        p player1.deck.cards.size
+        p player2.deck.cards.size
+      turn.type # result = turn.type
+      # p result
+      victor = turn.winner
+      turn.pile_cards
+      turn.spoils_of_war
+      turn.award_spoils(victor)
+        line +=1
+      break if line == 201
+    end
+    # until player1.has_lost? == true || player2.has_lost? == true
+    #   turn = Turn.new(player1, player2)
+    #   result = turn.type
+    #   if result == :mutually_assured_destruction
+    #     victor = "No Winner"
+    #   else
+    #     victor = turn.winner
+    #   end
+    #   turn.pile_cards
+    #   turn.award_spoils(victor)
+    #   if result == :mutually_assured_destruction
+    #     p "Turn #{line}: *mutually assured destruction* 6 cards removed from play"
+    #     # require "pry"; binding.pry
+    #   elsif result == :war
+    #     p "Turn #{line}: WAR - #{victor.name} won 6 cards"
+    #   else
+    #     p "Turn #{line}: #{victor.name} won 2 cards"
+    #   end
+    #   p player1.deck.cards.size
+    #   p player2.deck.cards.size
+    #   line +=1
+    #   break if line == 201
+    # end
+    end_the_war
+    # require "pry"; binding.pry
+  end
+
+  def end_the_war
+    if player1.has_lost? == true || player2.has_lost? == true
+      p "*~*~*~* #{victor.name} has won the game! *~*~*~*"
+    else
+      p "---- DRAW ----"
+    end
+    p "Player 1 cards: " + player1.deck.cards.size.to_s
+    p "Player 2 cards: " + player2.deck.cards.size.to_s
+  end
+
+end
--- test/war_or_peace_runner.rb
+++ test/war_or_peace_runner.rb
@@ -14,7 +14,7 @@ p "------------------------------------------------------------------"
 
 cue = "> "
 start = nil
-until (start == 'GO' || start == 'g')
+until (start == 'GO' || start == 'g') #make 'go'
     print cue
     start = gets.chomp
 end
@@ -33,11 +33,62 @@ deck2 = Deck.new([])
 
 player1 = Player.new('Meagan', deck1)
 player2 = Player.new('Aurora', deck2)
-turn = Turn.new(player1, player2)
+# turn = Turn.new(player1, player2)
 
 26.times do |index|
   player1.deck.cards << full_deck.shift
   player2.deck.cards << full_deck.shift
 end
 
-turn.start_a_war
+game = Game.new(player1,player2)
+game.start_a_war
+
+# def start_a_war
+#   line = 1
+#   result = nil
+#   # require "pry"; binding.pry
+#   until player1.has_lost? == true || player2.has_lost? == true
+#     turn = Turn.new(player1, player2)
+#     result = turn.type
+#     if result == :mutually_assured_destruction
+#       victor = "No Winner"
+#     else
+#       victor = turn.winner
+#     end
+#     turn.pile_cards
+#     turn.award_spoils(victor)
+#     if result == :mutually_assured_destruction
+#       p "Turn #{line}: *mutually assured destruction* 6 cards removed from play"
+#       # require "pry"; binding.pry
+#     elsif result == :war
+#       p "Turn #{line}: WAR - #{victor.name} won 6 cards"
+#     else
+#       p "Turn #{line}: #{victor.name} won 2 cards"
+#     end
+#     p player1.deck.cards.size
+#     p player2.deck.cards.size
+#     line +=1
+#     break if line == 1001
+#   end
+#   end_the_war
+# end
+#
+# def end_the_war
+#   if turn.player1.has_lost? == true || turn.player2.has_lost? == true
+#     p "*~*~*~* #{victor.name} has won the game! *~*~*~*"
+#   else
+#     p "---- DRAW ----"
+#   end
+#   p "Player 1 cards: " + player1.deck.cards.size.to_s
+#   p "Player 2 cards: " + player2.deck.cards.size.to_s
+# end
+
+# game = Game.new
+# player1.deck.cards << full_deck[index]
+# player2.deck.cards << full_deck[index+26]
+# turn.finish
+
+# values = %w{'2' '3' '4' '5' '6' '7' '8' '9' '10' 'Jack' 'Queen' 'King' 'Ace'}
+# suits = %w{:spades :hearts :diamonds :clubs}
+# ranks = %w{2 3 4 5 6 7 8 9 10 11 12 13 14}
+# 10.times {|n| full_deck << card=Card.new(:heart, "#{n}", n)}
