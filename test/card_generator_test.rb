74d1d167ae115a0089b3182ebb19f54670776de2
Ryan Stemmle <ryan.stemmle@gmail.com>
2021-02-07 14:02:49 -0500

    Refactor card gen test for array

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -7,7 +7,7 @@ require './lib/card'
 class CardGeneratorTest < Minitest::Test
 
   def test_it_exists
-    filename = "2, Heart, 2"
+    filename = ["2, Heart, 2"]
     box_cards = CardGenerator.new(filename)
 
     assert_instance_of CardGenerator, box_cards
@@ -15,31 +15,31 @@ class CardGeneratorTest < Minitest::Test
 
   def test_it_has_attributes
     # skip
-    filename = "2, Heart, 2"
+    filename = ["2, Heart, 2"]
     box_cards = CardGenerator.new(filename)
 
-    assert_equal [:heart, "2", 2], box_cards.box_cards
+    assert_equal :heart, box_cards.cards[0].suit
   end
 
   def test_a_string_can_create_rank_value_suit
-    skip
-    data = "2, Heart, 2"
+    # skip
+    data = ["2, Heart, 2"]
     card1 = CardGenerator.new(data)
 
-    assert_equal [:heart, "2", 2], card1.get_card_info(data)
+    assert_equal 2, card1.cards[0].rank
 
   end
 
   def test_a_string_can_create_rank_value_suit_with_another
-    skip
-    data = "King, Spade, 13"
+    # skip
+    data = ["King, Spade, 13"]
     card1 = CardGenerator.new(data)
 
-    assert_equal [:spade, "King", 13], card1.get_card_info(data)
+    assert_equal "King", card1.cards[0].value
   end
 
   def test_it_can_import_a_string_of_data
-    skip
+    # skip
     file = File.new('cards.txt')
     lines = File.readlines(file, chomp: true)
 
@@ -47,12 +47,34 @@ class CardGeneratorTest < Minitest::Test
   end
 
   def test_it_can_import_data_and_make_it_a_card
-    skip
+      # skip
     file = File.new('cards.txt')
     lines = File.readlines(file, chomp: true)
-    # card1 = CardGenerator.new(lines[0])
+    # require "pry"; binding.pry
+    card1 = CardGenerator.new([lines[0]])
 
-    assert_equal [:heart, "2", 2], card1 = CardGenerator.new(lines[0])
+    assert_equal Card, card1.cards[0].class
   end
+
+  def test_it_can_import_two_lines_and_convert
+    # skip
+    file = File.new('cards.txt')
+    lines = File.readlines(file, chomp: true)
+    card1 = CardGenerator.new([lines[0]])
+    card2 = CardGenerator.new([lines[1]])
+
+    assert_equal card2.cards, card2.cards
+  end
+
+  def test_see_what_happens_when_we_enter_all_data
+    # skip
+    file = File.new('cards.txt')
+    lines = File.readlines(file, chomp: true)
+
+    full_deck = CardGenerator.new(lines)
+
+    assert_equal "All the cards", full_deck.cards
+  end
+
 
 end
