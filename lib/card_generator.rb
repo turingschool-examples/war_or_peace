192ffc2998dcad10d8ca01181486cd94a7e43081
Ryan Stemmle <ryan.stemmle@gmail.com>
2021-02-07 14:00:38 -0500

    Move create card method to initialize

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,18 +1,23 @@
 class CardGenerator
-  attr_reader :box_cards
+  attr_reader :cards
 
   def initialize(data)
-    @box_cards = []
-    card_info = data.split(",")
-    #swap first two position
-    card_info[0], card_info[1] = card_info[1], card_info[0]
-    #make suit a symbol all lowercase
-    suit = card_info[0].strip.to_sym.downcase
-    #make value, its already an uppercase string
-    value = card_info[1]
-    #make rank an integer
-    rank = card_info[2].strip.to_i
-    @box_cards.push(suit, value, rank)
+    @cards = []
+    #Need to iterate over array and pass out a string
+    #array.each do |data| string do the code below...
+    data.each do |card_string|
+      card_info = card_string.split(",")
+      #swap first two position
+      card_info[0], card_info[1] = card_info[1], card_info[0]
+      #make suit a symbol all lowercase
+      suit = card_info[0].strip.to_sym.downcase
+      #make value, its already an uppercase string
+      value = card_info[1]
+      #make rank an integer
+      rank = card_info[2].strip.to_i
+      card = Card.new(suit, value, rank)
+      @cards << card
+    end 
   end
 
   # def get_card_info(data)
