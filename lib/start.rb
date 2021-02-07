dae58b8f1b0e9a3963fd890649f0e01c4d347f52
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-02-06 21:40:01 -0600

    Add start file to run game

---
+++ lib/start.rb
@@ -0,0 +1,23 @@
+class Start
+  attr_reader :turn, :turn_count
+
+  def initialize(turn)
+    @turn = turn
+    @turn_count = 0
+  end
+
+  def start_game
+    if turn.type == :mutually_assured_destruction
+      turn.pile_cards
+      puts "Turn #{@turn_count += 1}: *mutually assured destruction* 6 cards removed from play"
+    elsif turn.type == :basic
+      turn.pile_cards
+      turn.award_spoils
+      puts "Turn #{@turn_count += 1}: #{turn.winner.name} won 2 cards"
+    elsif turn.type == :war
+      turn.pile_cards
+      turn.award_spoils
+      puts "Turn #{@turn_count += 1}: WAR - #{turn.winner.name} won 6 cards"
+    end
+  end
+end
