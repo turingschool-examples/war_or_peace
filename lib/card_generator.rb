17212b786072a731ea7867d1c33590110a549399
Ryan Stemmle <ryan.stemmle@gmail.com>
2021-02-06 23:05:56 -0500

    Move method to initialize and add tests

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,14 +1,36 @@
 class CardGenerator
-  attr_reader :filename
+  attr_reader :box_cards
 
-  def initialize(filename)
-    @filename = filename
-    @card_array = []
-    # puts Dir.pwd
+  def initialize(data)
+    @box_cards = []
+    card_info = data.split(",")
+    #swap first two position
+    card_info[0], card_info[1] = card_info[1], card_info[0]
+    #make suit a symbol all lowercase
+    suit = card_info[0].strip.to_sym.downcase
+    #make value, its already an uppercase string
+    value = card_info[1]
+    #make rank an integer
+    rank = card_info[2].strip.to_i
+    @box_cards.push(suit, value, rank)
   end
 
-  def get_card_info(data)
-    data[0].split(",").sort
-  end
+  # def get_card_info(data)
+  #   # require "pry"; binding.pry
+  #   # 'a' is a temporary placeholder for array with data from card.txt file
+  #   card_info = data.split(",")
+  #   #swap first two position
+  #   card_info[0], card_info[1] = card_info[1], card_info[0]
+  #   #make suit a symbol all lowercase
+  #   suit = card_info[0].strip.to_sym.downcase
+  #   #make value, its already an uppercase string
+  #   value = card_info[1]
+  #   #make rank an integer
+  #   rank = card_info[2].strip.to_i
+  #   [suit, value, rank]
+  # end
 
 end
+
+
+# File.readlines(file) do |line| puts line.chop end
--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -1,31 +1,58 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card_generator'
+require './lib/card'
 
 
 class CardGeneratorTest < Minitest::Test
+
   def test_it_exists
-    filename = File.new('cards.txt')
+    filename = "2, Heart, 2"
     box_cards = CardGenerator.new(filename)
 
     assert_instance_of CardGenerator, box_cards
   end
 
   def test_it_has_attributes
-    filename = File.new('cards.txt')
+    # skip
+    filename = "2, Heart, 2"
     box_cards = CardGenerator.new(filename)
-# require "pry"; binding.pry
-    assert_equal filename, box_cards.filename
+
+    assert_equal [:heart, "2", 2], box_cards.box_cards
   end
 
   def test_a_string_can_create_rank_value_suit
-    data = ["2, Heart, 2"]
+    skip
+    data = "2, Heart, 2"
     card1 = CardGenerator.new(data)
-
 
     assert_equal [:heart, "2", 2], card1.get_card_info(data)
 
   end
 
+  def test_a_string_can_create_rank_value_suit_with_another
+    skip
+    data = "King, Spade, 13"
+    card1 = CardGenerator.new(data)
+
+    assert_equal [:spade, "King", 13], card1.get_card_info(data)
+  end
+
+  def test_it_can_import_a_string_of_data
+    skip
+    file = File.new('cards.txt')
+    lines = File.readlines(file, chomp: true)
+
+    assert_equal "2, Heart, 2", lines[0]
+  end
+
+  def test_it_can_import_data_and_make_it_a_card
+    skip
+    file = File.new('cards.txt')
+    lines = File.readlines(file, chomp: true)
+    # card1 = CardGenerator.new(lines[0])
+
+    assert_equal [:heart, "2", 2], card1 = CardGenerator.new(lines[0])
+  end
 
 end
