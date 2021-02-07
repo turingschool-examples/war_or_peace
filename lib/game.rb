7711019eafb6dfd08b8895174b73670f944bb304
Wyatt <wyattwicks@gmail.com>
2021-02-07 14:36:43 -0700

    Clean up play_loop method

--- lib/game.rb
+++ lib/game.rb
@@ -9,17 +9,17 @@ class Game
   end
 
   def start
-    player_names = get_players
+    player_names = @player_1, @player_2
+    #player_names = get_players
     create_players_and_decks(player_names)
     input = introduction
     play_loop if input == 'GO'
     p "that's not an option!" if input != 'GO'
-
   end
 
   def introduction
     puts "Welcome to War! (or Peace) This game will be played with 52 cards."
-    puts "The players today are #{@player_1.name} and #{@player_2.name}."
+    puts "The players today are Megan and Aurora."
     puts "Type 'GO' to start the game!"
     puts "-" * 65
     gets.chomp.upcase
@@ -31,45 +31,50 @@ class Game
     left,right = shuffle_deck.each_slice((shuffle_deck.size/2.0).round).to_a
     deck1.cards = left
     deck2 = Deck.new(right)
-
-    @player_1 = Player.new(player_names[0], deck1)
-    @player_2 = Player.new(player_names[1], deck2)
-
+    @player_1 = Player.new("Megan", deck1)
+    @player_2 = Player.new("Aurora", deck2)
   end
 
-
-  def get_players
-    puts "Welcome to War! (or Peace) This game will be played with 52 cards."
-    puts "Please Enter Player One's Name: "
-    @player_1 = gets.chomp.capitalize
-    p "------------------------------------------------------------------"
-    puts "Hello #{@player_1}! Welcome to War! (or Peace)"
-    puts "Please Enter Player Two's Name: "
-    @player_2 = gets.chomp.capitalize
-    p "------------------------------------------------------------------"
-    puts "Hello #{@player_2}! Welcome to War! (or Peace)"
-    return [@player_1, @player_2]
-  end
+  #Un-comment below code and change @player_1 & 2 names to add user input names.
+  # def get_players
+  #   puts "Welcome to War! (or Peace) This game will be played with 52 cards."
+  #   puts "Please Enter Player One's Name: "
+  #   @player_1 = gets.chomp.capitalize
+  #   p "------------------------------------------------------------------"
+  #   puts "Hello #{@player_1}! Welcome to War! (or Peace)"
+  #   puts "Please Enter Player Two's Name: "
+  #   @player_2 = gets.chomp.capitalize
+  #   p "------------------------------------------------------------------"
+  #   puts "Hello #{@player_2}! Welcome to War! (or Peace)"
+  #   return [@player_1, @player_2]
+  # end
 
   def play_loop
     n = 1
-    i = 0
-    z = 0
+    w = 0
+    m = 0
+    b = 0
     until @player_1.has_lost? == true || @player_2.has_lost? == true
-      break if n > 999999
+      #break if n > 1000000
       turn = Turn.new(@player_1, @player_2)
       type = turn.type
       winner = turn.winner
-      i += 1 if type == :war
-      z += 1 if type == :mutually_assured_destruction
+      b += 1 if type == :basic
+      w += 1 if type == :war
+      m += 1 if type == :mutually_assured_destruction
       turn.pile_cards
       turn.award_spoils(winner)
       state_winner(type, winner, n)
       n += 1
+      if n == 1000001
+        puts "---- DRAW ----"
+        break
+      end
 
     end
-    puts "#{i} WAR"
-    puts "#{z} MAD"
+    puts "#{b} BASIC turns"
+    puts "#{w} WAR turns"
+    puts "#{m} MAD turns"
   end
 
 
