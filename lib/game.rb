76247b18b89a4b8fc4c60bb23cce5aa25404910e
Kseniya <kseniya.kudelich@gmail.com>
2021-02-07 16:42:40 -0700

    Add turn.type call on turn type

--- lib/game.rb
+++ lib/game.rb
@@ -38,12 +38,12 @@ class Game
         turn_count += 1
 
 
-        if type == :mutually_assured_destruction
+        if turn.type == :mutually_assured_destruction
           puts "#{turn_count}: *mutually assured destruction* 6 cards removed from play"
-        elsif type == :war
+        elsif turn.type == :war
         #     # we remove all 3 cards from both decks and add all these cards to @spoils_of_war and the winner of that turn gets
             puts "Turn #{turn_count}: dis is WAR - #{winner.name} won 6 cards"
-        else type == :basic
+        else turn.type == :basic
               puts "Turn #{turn_count}: #{winner.name} won 2 cards"
         end
 
