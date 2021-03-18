44b361cb0a8d908459d2c398fc4ff8a06815acfb
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-18 11:40:28 -0700

    Create call_rank and high_ranking_card test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -27,4 +27,47 @@ class DeckTest < Minitest::Test
 
     assert_equal [@card1, @card2, @card3], @deck.cards
   end
+
+  def test_it_can_call_card_rank
+    @deck.add_cards(@card1)
+    @deck.add_cards(@card2)
+    @deck.add_cards(@card3)
+
+    assert_equal 12, @deck.rank_of_card_at(0)
+    assert_equal 14, @deck.rank_of_card_at(2)
+  end
+
+  def test_it_can_find_high_ranking_cards
+    @deck.add_cards(@card1)
+    @deck.add_cards(@card2)
+    @deck.add_cards(@card3)
+
+    assert_equal [@card1, @card3], @deck.high_ranking_cards
+  end
+
+  def test_it_can_give_percentage_of_high_ranking_cards
+    @deck.add_cards(@card1)
+    @deck.add_cards(@card2)
+    @deck.add_cards(@card3)
+
+    assert_equal 66.67, @deck.percent_high_ranking
+  end
 end
+
+#  deck.remove_card
+# #=> #<Card:0x007fbfd18490e8 @rank=12, @suit=:diamond, @value="Queen">
+#  deck.cards
+# #=> [#<Card:0x007fbfd19f4fa0...>, #<Card:0x007fbfd18555a0...>]
+#  deck.high_ranking_cards
+# #=> [#<Card:0x007fbfd18555a0...>]
+#  deck.percent_high_ranking
+# #=> 50.0
+#  card4 = Card.new(:club, '5', 5)
+# #=> #<Card:0x007fbfd2963978 @rank=5, @suit=:club, @value="5">
+#  deck.add_card(card4)
+#  deck.cards
+# #=> [#<Card:0x007fbfd19f4fa0...>, #<Card:0x007fbfd18555a0...>, #<Card:0x007fbfd2963978...>]
+#  deck.high_ranking_cards
+# #=> [#<Card:0x007fbfd18555a0...>]
+#  deck.percent_high_ranking
+# #=> 33.33
