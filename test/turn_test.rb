a7d2ab136cd6d82a0e3158e509ace514d52f5231
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-18 16:41:51 -0700

    Consolidate tests

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -8,19 +8,19 @@ require './lib/turn'
 class TurnTest < Minitest::Test
 
   def setup
-  @card1 = Card.new(:heart, 'Jack', 11)
-  @card2 = Card.new(:heart, '10', 10)
-  @card5 = Card.new(:heart, '8', 8)
-  @card8 = Card.new(:diamond, '2', 2)
-  @deck1 = Deck.new([@card1, @card2, @card5, @card8])
-  @player1 = Player.new("Megan", @deck1)
+  card1 = Card.new(:heart, 'Jack', 11)
+  card2 = Card.new(:heart, '10', 10)
+  card5 = Card.new(:heart, '8', 8)
+  card8 = Card.new(:diamond, '2', 2)
+  deck1 = Deck.new([card1, card2, card5, card8])
+  @player1 = Player.new("Megan", deck1)
 
-  @card3 = Card.new(:heart, '9', 9)
-  @card4 = Card.new(:diamond, 'Jack', 11)
-  @card6 = Card.new(:diamond, 'Queen', 12)
-  @card7 = Card.new(:heart, '3', 3)
-  @deck2 = Deck.new([@card3, @card4, @card6, @card7])
-  @player2 = Player.new("Aurora", @deck2)
+  card3 = Card.new(:heart, '9', 9)
+  card4 = Card.new(:diamond, 'Jack', 11)
+  card6 = Card.new(:diamond, 'Queen', 12)
+  card7 = Card.new(:heart, '3', 3)
+  deck2 = Deck.new([card3, card4, card6, card7])
+  @player2 = Player.new("Aurora", deck2)
 
   @turn = Turn.new(@player1, @player2)
   end
@@ -29,12 +29,9 @@ class TurnTest < Minitest::Test
     assert_instance_of Turn, @turn
   end
 
-  def test_player_can_take_a_turn
+  def test_it_has_readable_attributes
     assert_equal @player1, @turn.player1
     assert_equal @player2, @turn.player2
-  end
-
-  def test_it_starts_with_no_spoils_of_war
     assert_equal [], @turn.spoils_of_war
   end
 
