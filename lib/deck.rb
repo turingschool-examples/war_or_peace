6346309d277879c8924c20307089f41e87a7b903
Scott-Borecki <scottborecki@gmail.com>
2021-05-10 17:20:32 -0600

    Update deck.rb

---
+++ lib/deck.rb
@@ -0,0 +1,26 @@
+class Deck
+
+  attr_reader :cards
+
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def high_ranking_cards
+    high_ranking_cards = cards.select do |card|
+      card.rank > 10
+    end
+  end
+
+  def rank_of_card_at(index)
+    cards[index].rank
+  end
+
+  def percent_high_ranking
+    high_ranking_cards = cards.select do |card|
+      card.rank > 10
+    end
+    (high_ranking_cards.count.fdiv(cards.count) * 100).round(2)
+  end
+
+end
