4e882810d2c173ba2d98709e4af5612fd371cd9e
Scott-Borecki <scottborecki@gmail.com>
2021-05-10 16:10:30 -0600

    Update deck_spec.rb

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,1 +1,85 @@
 require './lib/deck'
+
+RSpec.describe Deck do
+  it "exists" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck).to be_an_instance_of(Deck)
+  end
+
+  it "has cards method" do
+    # An attr_reader to read the @cards attribute
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.cards).to eq(cards)
+  end
+
+  it "has high_ranking_cards method" do
+  # This method will return an array of cards in the deck that have a rank of 11 or above (face cards and aces)
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.high_ranking_cards).to eq([card1, card3])
+  end
+
+  it "has rank_of_card_at method" do
+  # This method will take one argument that represents the index location of a card to be used and will return the rank of that card
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.rank_of_card_at(0)).to eq(12)
+    expect(deck.rank_of_card_at(2)).to eq(14)
+  end
+
+  it "has percent_high_ranking method" do
+  # This method will return the percentage of cards that are high ranking
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.percent_high_ranking).to eq(66.67)
+  end
+
+  it "has remove_card method" do
+  # This method will remove the top card from the deck
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.remove_card).to eq(card1)
+    expect(deck.cards).to eq([card2, card3])
+  end
+
+  it "has add_card method" do
+  # This method will add one card to the bottom (end) of the deck
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    card4 = Card.new(:club, '5', 5)
+
+    deck.add_card(card4)
+
+    expect(deck.cards).to eq([card1, card2, card3, card4])
+  end
+
+end
