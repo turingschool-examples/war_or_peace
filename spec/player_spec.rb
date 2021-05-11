967567981336321de4f7b520db3b41b10e95eef9
KatherineMcConnell <mcconnell.katherine01@gmail.com>
2021-05-11 08:02:48 -0600

    Assert;pass initialize and attributes: name and deck

---
+++ spec/player_spec.rb
@@ -0,0 +1,29 @@
+require 'rspec'
+require_relative '../lib/card'
+require_relative '../lib/deck'
+require_relative '../lib/player'
+
+RSpec.describe Player do
+  describe '#initialize' do
+    it "exists" do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      deck = Deck.new([card1, card2, card3])
+      player = Player.new('Clarisa', deck)
+      expect(player).to be_an_instance_of(Player)
+    end
+  end
+
+  describe 'attributes' do
+    it "has readable attributes" do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      deck = Deck.new([card1, card2, card3])
+      player = Player.new('Clarisa', deck)
+      expect(player.name).to eq('Clarisa')
+      expect(player.deck).to eq(deck)
+    end
+  end
+end
