43f65e054b5d4d72505d725c4a384ac6961c175a
Scott-Borecki <scottborecki@gmail.com>
2021-05-12 12:58:21 -0600

    Refactor card_spec.rb

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,17 +1,23 @@
 require './lib/card'
 
 RSpec.describe Card do
-  it "exists" do
-    card = Card.new(:diamond, 'Queen', 12)
 
-    expect(card).to be_an_instance_of(Card)
+  describe 'Object Creation' do
+
+    before(:each) do
+      @card = Card.new(:diamond, 'Queen', 12)
+    end
+
+    it "exists" do
+      expect(@card).to be_an_instance_of(Card)
+    end
+
+    it "has readable attributes" do
+      expect(@card.suit).to eq(:diamond)
+      expect(@card.value).to eq('Queen')
+      expect(@card.rank).to eq(12)
+    end
+
   end
 
-  it "has readable attributes" do
-    card = Card.new(:diamond, 'Queen', 12)
-
-    expect(card.suit).to eq(:diamond)
-    expect(card.value).to eq('Queen')
-    expect(card.rank).to eq(12)
-  end
 end
