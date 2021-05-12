e26657246e4d9a8d63a2262a0326c31f9652d331
sami-p <sami.peterson14@gmail.com>
2021-05-12 08:53:58 -0500

    Add 2 more expects to #add_card spec

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -80,17 +80,18 @@ RSpec.describe Deck do
     end
 
     it '#add_card' do
-      card1 = Card.new(:diamond, 'Queen', 12)
       card2 = Card.new(:spade, '3', 3)
       card3 = Card.new(:heart, 'Ace', 14)
-      cards = [card1, card2, card3]
+      cards = [card2, card3]
 
       deck = Deck.new(cards)
 
       card4 = Card.new(:club, '5', 5)
       deck.add_card(card4)
 
-      expect(deck.cards).to eq([card1, card2, card3, card4])
+      expect(deck.cards).to eq([card2, card3, card4])
+      expect(deck.high_ranking_cards).to eq([card3])
+      expect(deck.percent_high_ranking).to eq(33.33)
     end
   end
 end
