2f39db19e524b5c44537ffb22f3f350f375867c7
Marla Schulz <marlaa.schulz@gmail.com>
2021-05-11 18:55:27 -0600

    Complete deck class and spec

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,19 +1,23 @@
+require 'pry'
+
 class Deck
   attr_reader :cards
   def initialize (cards)
     @cards = cards
+    @high_cards = []
   end
   def rank_of_card_at (index)
     @cards[index].rank
   end
   def high_ranking_cards
-    high_cards = @cards.select do |x|
+    @high_cards = @cards.select do |x|
+      #binding.pry
       x.rank >= 11
     end
-    return high_cards
   end
   def percent_high_ranking
-    (@cards.high_cards.length / @cards.length) * 100
+    #Sbinding.pry
+    ((@high_cards.length.to_f / @cards.length.to_f) * 100).round(2)
   end
   def remove_card
     @cards.shift
