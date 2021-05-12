4bc2d3bdaa389c0ce41bcf909dc6753af85cf9e9
sami-p <sami.peterson14@gmail.com>
2021-05-11 19:56:05 -0500

    Test #rank_of_card_at()

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -27,4 +27,18 @@ RSpec.describe Deck do
       expect(deck.cards).to eq([card1, card2, card3])
     end
   end
+
+  describe 'ranks of cards' do
+    it '#rank_of_card_at by index' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+
+      cards = [card1, card2, card3]
+
+      deck = Deck.new(cards)
+
+      expect(deck.rank_of_card_at(0)).to eq(12)
+    end
+  end
 end
