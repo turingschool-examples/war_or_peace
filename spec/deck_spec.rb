7687bb5ff66f5bc45c1c01b4e659ee6b98461530
sami-p <sami.peterson14@gmail.com>
2021-05-12 08:32:51 -0500

    Add #percent_high_ranking spec

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -54,5 +54,17 @@ RSpec.describe Deck do
       expect(deck.cards).to eq([card1, card2, card3])
       expect(deck.high_ranking_cards).to eq([card1, card3])
     end
+
+    it '#percent_high_ranking' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+
+      cards = [card1, card2, card3]
+
+      deck = Deck.new(cards)
+
+      expect(deck.percent_high_ranking).to eq(66.67)
+    end
   end
 end
