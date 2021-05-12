4fc401abd13ec8f11d93c07fc167122e4be0fb8d
Marla Schulz <marlaa.schulz@gmail.com>
2021-05-11 19:14:22 -0600

    Complete player class and spec

---
+++ lib/player.rb
@@ -0,0 +1,12 @@
+require 'pry'
+
+class Player
+  attr_reader :name, :deck
+  def initialize (name, deck)
+    @name = name
+    @deck = deck
+  end
+  def has_lost?
+    false
+  end
+end
---
+++ spec/player_spec.rb
@@ -0,0 +1,66 @@
+require_relative '../lib/card.rb'
+require_relative '../lib/deck.rb'
+require_relative '../lib/player.rb'
+
+RSpec.describe Player do
+  it 'has a name' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    player = Player.new('Clarisa', deck)
+
+    expect(player.name).to eq('Clarisa')
+  end
+
+  it 'has a deck' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    player = Player.new('Clarisa', deck)
+
+    expect(player.deck).to eq(deck)
+  end
+
+  it 'can lose' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    player = Player.new('Clarisa', deck)
+
+    expect(player.has_lost?).to be false
+  end
+
+  it 'can remove a card from the deck' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    player = Player.new('Clarisa', deck)
+
+    expect(player.deck.remove_card).to eq(card1)
+    expect(player.has_lost?).to be false
+
+    expect(player.deck.remove_card).to eq(card2)
+    expect(player.has_lost?).to be false
+
+    expect(player.deck.remove_card).to eq(card3)
+    expect(player.has_lost?).to be false
+
+    expect(player.deck).to eq(deck)
+  end
+end
