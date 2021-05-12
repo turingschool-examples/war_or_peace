18998daaf990db9645b4614b3265b13f36059445
Antonio King <antonio.j.king@gmail.com>
2021-05-11 22:05:31 -0600

    Setup Deck class

---
+++ lib/deck.rb
@@ -0,0 +1,28 @@
+class Deck
+attr_reader :cards 
+
+  def initialize(array_of_cards)
+    @arrray_of_cards = array_of_cards
+  end
+
+  def rank_of_card_at(num)
+
+  end
+
+  def high_ranking_cards
+
+  end
+
+  def percent_high_ranking
+
+  end
+
+  def remove_card
+
+  end
+
+  def add_card
+
+  end
+
+end
