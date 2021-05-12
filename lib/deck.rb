eba448c0b4285bae50ce2d776e8893c58a141972
Michael Abbott <michael@mjabbottdesign.com>
2021-05-11 19:54:53 -0700

    Add rank_of_card_at method

--- lib/deck.rb
+++ lib/deck.rb
@@ -3,4 +3,25 @@ class Deck
   def initialize(cards)
     @cards = cards
   end
+
+  def rank_of_card_at(index)
+    cards[index].rank
+    # this method will take one argument
+    # that represents the index location of
+    # a card to be used and will return the
+    # rank of that card
+  end
+
+  def high_ranking_cards
+  end
+
+  def percent_high_ranking
+  end
+
+  def remove_card
+  end
+
+  def add_card
+  end
+
 end
