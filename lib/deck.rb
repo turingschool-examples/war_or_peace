d8f8096a70d72785be647ffd099b72e003468c0d
Matt Toensing <matthew.toensing@gmail.com>
2021-05-11 20:17:04 -0600

    Add deck

---
+++ lib/deck.rb
@@ -0,0 +1,11 @@
+class Deck
+
+end
+# 
+# p deck.high_ranking_cards
+# p deck.percent_high_ranking
+# p deck.remove_card
+# p deck.cards
+# card4 = Card.new(:club, '5', 5)
+# deck.add_card(card4)
+# p deck.cards
---
+++ spec/deck_spec.rb
@@ -0,0 +1,19 @@
+require_relative 'spec_helper'
+
+RSpec.describe Deck do
+  before :each do
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @cards = [@card1, @card2, @card3]
+    @deck = Deck.new(@cards)
+  end
+
+  it "exists" do
+    expect(@deck).to be_an_instance_of(Deck)
+  end
+
+  it "has cards" do
+    expect(@deck.cards).to eq(@cards)
+  end
+end
