79a6104e305238bdacc11f9f15003007480cae96
Scott-Borecki <scottborecki@gmail.com>
2021-05-12 12:58:50 -0600

    Refactor player_spec.rb

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,40 +4,45 @@ require './lib/player'
 
 RSpec.describe Player do
 
-  it "exists" do
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    deck = Deck.new([card1, card2, card3])
-    player = Player.new('Clarisa', deck)
+  describe 'Object Creation' do
 
-    expect(player.name).to eq('Clarisa')
-    expect(player.deck).to eq(deck)
-  end
+    before(:each) do
+      @card1 = Card.new(:diamond, 'Queen', 12)
+      @card2 = Card.new(:spade, '3', 3)
+      @card3 = Card.new(:heart, 'Ace', 14)
+      @deck = Deck.new([@card1, @card2, @card3])
+      @player = Player.new('Clarisa', @deck)
+    end
 
-  it "has has_lost? method" do
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    deck = Deck.new([card1, card2, card3])
-    player = Player.new('Clarisa', deck)
-
-    expect(player.has_lost?).to be false
+    it "exists" do
+      expect(@player.name).to eq('Clarisa')
+      expect(@player.deck).to eq(@deck)
+    end
 
   end
 
-  it "has has_lost? method" do
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    deck = Deck.new([card1, card2, card3])
-    player = Player.new('Clarisa', deck)
+  describe 'Instance Methods' do
 
-    player.deck.remove_card
-    player.deck.remove_card
-    player.deck.remove_card
+    before(:each) do
+      @card1 = Card.new(:diamond, 'Queen', 12)
+      @card2 = Card.new(:spade, '3', 3)
+      @card3 = Card.new(:heart, 'Ace', 14)
+      @deck = Deck.new([@card1, @card2, @card3])
+      @player = Player.new('Clarisa', @deck)
+    end
 
-    expect(player.has_lost?).to be true
+    it "has has_lost? method" do
+      expect(@player.has_lost?).to be false
+    end
+
+    it "has has_lost? method" do
+      @player.deck.remove_card
+      @player.deck.remove_card
+      @player.deck.remove_card
+
+      expect(@player.has_lost?).to be true
+    end
+
   end
 
 end
