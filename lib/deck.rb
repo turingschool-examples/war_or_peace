1820c67b8bcddc85aca38466930900027472cc05
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-05-11 19:45:23 -0600

    Create rank_of_card_at method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,7 +1,14 @@
 class Deck
-  attr_reader :cards
+  attr_reader :cards,
+              :rank_of_card_at
+              :index
 
   def initialize(cards)
     @cards = cards
+  end
+
+  def rank_of_card_at(index)
+    @index = index
+    @rank_of_card_at = cards[index].rank
   end
 end
