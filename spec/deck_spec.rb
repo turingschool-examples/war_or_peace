cf2cbc7da4f7ddf34ec3d917808ac32761d8b2b8
Jacob Martinez <jacobmarcareer@gmail.com>
2021-05-13 14:10:15 -0600

    Add before do to tests to shorten

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,83 +3,50 @@ require './lib/deck'
 require './lib/card'
 
 RSpec.describe Deck do
-  it 'exists' do
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
+  before 'tests' do
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @cards = [@card1, @card2, @card3]
+    @deck = Deck.new(@cards)
+  end
 
-    expect(deck).to be_a(Deck)
+  it 'exists' do
+    expect(@deck).to be_a(Deck)
   end
 
   it 'has cards' do
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-
-    expect(deck.cards).to eq([card1, card2, card3])
+    expect(@deck.cards).to eq([@card1, @card2, @card3])
   end
 
   it 'checks rank of cards' do
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-
-    expect(deck.rank_of_card_at(0)).to eq(12)
-    expect(deck.rank_of_card_at(2)).to eq(14)
+    expect(@deck.rank_of_card_at(0)).to eq(12)
+    expect(@deck.rank_of_card_at(2)).to eq(14)
   end
 
   it 'checks for high ranking cards' do
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-
-    expect(deck.high_ranking_cards).to eq([card1, card3])
+    expect(@deck.high_ranking_cards).to eq([@card1, @card3])
   end
 
   it 'gives percentage of high ranking cards' do
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-
-    expect(deck.percent_high_ranking).to eq(66.67)
+    expect(@deck.percent_high_ranking).to eq(66.67)
   end
 
   it 'removes first card from deck' do
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-
-    expect(deck.remove_card).to eq(card1)
-    expect(deck.cards).to eq([card2, card3])
-    expect(deck.high_ranking_cards).to eq([card3])
-    expect(deck.percent_high_ranking).to eq(50.0)
+    expect(@deck.remove_card).to eq(@card1)
+    expect(@deck.cards).to eq([@card2, @card3])
+    expect(@deck.high_ranking_cards).to eq([@card3])
+    expect(@deck.percent_high_ranking).to eq(50.0)
   end
 
   it 'adds a card to deck' do
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-    deck.remove_card
+    @deck.remove_card
     card4 = Card.new(:club, '5', 5)
-    deck.add_card(card4)
+    @deck.add_card(card4)
 
     expect(card4).to be_a(Card)
-    expect(deck.cards).to eq([card2, card3, card4])
-    expect(deck.high_ranking_cards).to eq([card3])
-    expect(deck.percent_high_ranking).to eq(33.33)
+    expect(@deck.cards).to eq([@card2, @card3, card4])
+    expect(@deck.high_ranking_cards).to eq([@card3])
+    expect(@deck.percent_high_ranking).to eq(33.33)
   end
 end
