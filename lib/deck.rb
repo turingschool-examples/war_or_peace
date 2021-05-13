54931d271b7d1dee8c3ca15e3c291f139a613a80
Jacob Martinez <jacobmarcareer@gmail.com>
2021-05-13 13:43:58 -0600

    Refactor methods, make cleaner

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,7 +1,7 @@
 require './lib/card'
 
 class Deck
-  attr_reader :cards, :high_ranking_cards
+  attr_reader :cards
 
   def initialize(cards)
     @cards = cards
@@ -12,23 +12,23 @@ class Deck
   end
 
   def high_ranking_cards
-    @high_ranking_cards = @cards.select {|high_card| high_card.rank > 10}
+    @cards.select {|high_card| high_card.rank > 10}
   end
 
   def percent_high_ranking
     def high_cards
-      @cards.select {|high_card| high_card.rank > 10}.length.to_f
+      high_ranking_cards.length
     end
 
     def original_cards
       @cards.length
     end
 
-    ((high_cards / original_cards) * 100).round(2)
+    ((high_cards / original_cards.to_f) * 100).round(2)
   end
 
   def remove_card
-    @cards.slice!(0)
+    @cards.shift #.shift takes first item from array, returns item as value
   end
 
   def add_card(new_card)
