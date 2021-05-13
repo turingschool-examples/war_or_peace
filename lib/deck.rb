f0e88bb780dd5b13ce594d0ccfb5bea5ed789e08
Lee Hopper <hopper.lee.j@gmail.com>
2021-05-12 21:00:22 -0600

    Build high_ranking_cards method

--- lib/deck.rb
+++ lib/deck.rb
@@ -8,7 +8,17 @@ class Deck
   end
 
   def rank_of_cards_at(index)
-    @cards.at(index).rank 
+    @cards.at(index).rank
+  end
+
+  def high_ranking_cards
+    high_ranks = []
+    @cards.each do |card|
+      if card.rank >= 11
+        high_ranks << card
+      end
+    end
+    high_ranks
   end
 
 end
