a962ca857720d6ebddb57a27e86de24fb5261c0a
Matt Toensing <matthew.toensing@gmail.com>
2021-05-12 20:56:38 -0600

    Include tests

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe Deck do
     expect(@deck.cards).to eq(@cards)
   end
 
-  it "it can return card value at given index" do
+  it "can return card value at given index" do
     expect(@deck.rank_of_card_at(0)).to eq(12)
     expect(@deck.rank_of_card_at(2)).to eq(14)
   end
@@ -26,41 +26,18 @@ RSpec.describe Deck do
     expect(@deck.high_ranking_cards).to eq([@card1, @card3])
   end
 
+  it "can show the percentage of high cards" do
+    expect(@deck.percent_high_ranking).to eq(66.7)
+  end
+
+  it "can remove a card from the deck" do
+    expect(@deck.remove_card).to eq(@card1)
+    expect(@deck.cards).to eq([@card2, @card3])
+    expect(@deck.percent_high_ranking).to eq(50)
+  end
+
   it "can add cards" do
     card4 = Card.new(:club, '5', 5)
     expect(@deck.add_card(card4)).to eq(@cards)
   end
 end
-
-
-# pry(main)> deck.high_ranking_cards
-# #=> [#<Card:0x007fbfd18490e8...>, #<Card:0x007fbfd18555a0...>]
-#
-# pry(main)> deck.percent_high_ranking
-# #=> 66.67
-#
-# pry(main)> deck.remove_card
-# #=> #<Card:0x007fbfd18490e8 @rank=12, @suit=:diamond, @value="Queen">
-#
-# pry(main)> deck.cards
-# #=> [#<Card:0x007fbfd19f4fa0...>, #<Card:0x007fbfd18555a0...>]
-#
-# pry(main)> deck.high_ranking_cards
-# #=> [#<Card:0x007fbfd18555a0...>]
-#
-# pry(main)> deck.percent_high_ranking
-# #=> 50.0
-#
-# pry(main)> card4 = Card.new(:club, '5', 5)
-# #=> #<Card:0x007fbfd2963978 @rank=5, @suit=:club, @value="5">
-#
-# pry(main)> deck.add_card(card4)
-#
-# pry(main)> deck.cards
-# #=> [#<Card:0x007fbfd19f4fa0...>, #<Card:0x007fbfd18555a0...>, #<Card:0x007fbfd2963978...>]
-#
-# pry(main)> deck.high_ranking_cards
-# #=> [#<Card:0x007fbfd18555a0...>]
-#
-# pry(main)> deck.percent_high_ranking
-# #=> 33.33
