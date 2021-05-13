eccff473d4d34536607103dba86956025dc1b662
danembb <dbrophy720@gmail.com>
2021-05-12 22:50:48 -0600

    In progress create rank_of_card_at method

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,4 +4,11 @@ class Deck
   def initialize(cards)
     @cards = cards
   end
+
+  def rank_of_card_at(rank)
+    @cards.each do |card|
+      rank == card.rank
+      rank_of_card_at(rank)
+    end
+  end
 end
