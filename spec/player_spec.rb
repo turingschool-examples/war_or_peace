d6f574bd4fa3f45fba8d771a17b6d3e6f6cdc806
Matt Toensing <matthew.toensing@gmail.com>
2021-05-12 20:57:30 -0600

    Include player tests

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,14 +1,40 @@
-require 'rspec'
-require '../lib/card'
-require '../lib/deck'
-require '../lib/player'
+require_relative 'spec_helper'
 
 RSpec.describe Player do
-  it "text" do
-
+  before :each do
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @deck = Deck.new([@card1, @card2, @card3])
+    @player = Player.new('Clarisa', @deck)
   end
 
-  it "text" do
+  it "exists" do
+    expect(@player).to be_an_instance_of(Player)
+  end
 
+  it "has a name" do
+    expect(@player.name).to eq('Clarisa')
+  end
+
+  it "has a deck of cards" do
+    expect(@player.deck).to eq(@deck)
+  end
+
+  it "has not lost yet" do
+    expect(@player.has_lost?).to be false
+  end
+
+  it "can lose a round" do
+    @player.deck.remove_card
+    expect(@player.has_lost?).to be false
+
+    @player.deck.remove_card
+    expect(@player.has_lost?).to be false
+
+    @player.deck.remove_card
+    expect(@player.has_lost?).to be true
+
+    expect(@player.deck.cards).to eq([])
   end
 end
