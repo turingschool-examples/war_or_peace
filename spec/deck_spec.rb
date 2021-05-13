8fd70a5c98ddd80e38ff464b05e3f2483eb43d5b
Lee Hopper <hopper.lee.j@gmail.com>
2021-05-13 13:55:43 -0600

    Change deck arguments formatting

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -13,8 +13,7 @@ describe Deck do
       card1 = Card.new(:diamond, 'Queen', 12)
       card2 = Card.new(:spade, '3', 3)
       card3 = Card.new(:heart, 'Ace', 14)
-      cards = [card1, card2, card3]
-      deck = Deck.new(cards)
+      deck = Deck.new([card1, card2, card3])
 
       expect(deck.cards).to eq([card1, card2, card3])
     end
@@ -25,8 +24,7 @@ describe Deck do
       card1 = Card.new(:diamond, 'Queen', 12)
       card2 = Card.new(:spade, '3', 3)
       card3 = Card.new(:heart, 'Ace', 14)
-      cards = [card1, card2, card3]
-      deck = Deck.new(cards)
+      deck = Deck.new([card1, card2, card3])
 
       expect(deck.rank_of_card_at(0)).to eq(12)
       expect(deck.rank_of_card_at(2)).to eq(14)
@@ -36,8 +34,7 @@ describe Deck do
       card1 = Card.new(:diamond, 'Queen', 12)
       card2 = Card.new(:spade, '3', 3)
       card3 = Card.new(:heart, 'Ace', 14)
-      cards = [card1, card2, card3]
-      deck = Deck.new(cards)
+      deck = Deck.new([card1, card2, card3])
 
       expect(deck.high_ranking_cards).to eq([card1, card3])
     end
@@ -46,8 +43,7 @@ describe Deck do
       card1 = Card.new(:diamond, 'Queen', 12)
       card2 = Card.new(:spade, '3', 3)
       card3 = Card.new(:heart, 'Ace', 14)
-      cards = [card1, card2, card3]
-      deck = Deck.new(cards)
+      deck = Deck.new([card1, card2, card3])
 
       expect(deck.percent_high_ranking).to eq(66.67)
     end
@@ -56,8 +52,7 @@ describe Deck do
       card1 = Card.new(:diamond, 'Queen', 12)
       card2 = Card.new(:spade, '3', 3)
       card3 = Card.new(:heart, 'Ace', 14)
-      cards = [card1, card2, card3]
-      deck = Deck.new(cards)
+      deck = Deck.new([card1, card2, card3])
       deck.remove_card
 
       expect(deck.cards).to eq([card2, card3])
@@ -67,8 +62,7 @@ describe Deck do
       card1 = Card.new(:diamond, 'Queen', 12)
       card2 = Card.new(:spade, '3', 3)
       card3 = Card.new(:heart, 'Ace', 14)
-      cards = [card1, card2, card3]
-      deck = Deck.new(cards)
+      deck = Deck.new([card1, card2, card3])
 
       card4 = Card.new(:club, '5', 5)
       deck.add_card(card4)
