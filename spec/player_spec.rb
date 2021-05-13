6fa5cc644fb0f8541a9f4afa29e96083d22c4daa
Lee Hopper <hopper.lee.j@gmail.com>
2021-05-13 14:42:42 -0600

    Build tests for has_lost method

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -20,4 +20,28 @@ describe Player do
       expect(player.deck).to eq(deck)
     end
   end
+
+  context 'Methods' do
+    it 'Return false player has lost?' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      deck = Deck.new([card1, card2, card3])
+      player = Player.new('Clarisa', deck)
+
+      expect(player.has_lost?).to be(false)
+    end
+
+    it 'Return true player has lost?' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      deck = Deck.new([card1, card2])
+      player = Player.new('Clarisa', deck)
+
+      player.deck.remove_card
+      player.deck.remove_card
+
+      expect(player.has_lost?).to be(true)
+    end
+  end
 end
