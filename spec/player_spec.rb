eff2aa27f9441036e0452fe33d2cecc7f70683f6
Jacob Martinez <jacobmarcareer@gmail.com>
2021-05-13 14:25:42 -0600

    Make player has lost test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,33 +4,27 @@ require './lib/deck'
 require './lib/player'
 
 RSpec.describe Player do
+  before do
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @deck = Deck.new([@card1, @card2, @card3])
+    @player = Player.new('Clarisa', @deck)
+  end
+
   it 'exists' do
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    deck = Deck.new([card1, card2, card3])
-    player = Player.new('Clarisa', deck)
-
-    expect(player).to be_a(Player)
+    expect(@player).to be_a(Player)
   end
 
-  it "has name" do
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    deck = Deck.new([card1, card2, card3])
-    player = Player.new('Clarisa', deck)
-
-    expect(player.name).to eq('Clarisa')
+  it 'has name' do
+    expect(@player.name).to eq('Clarisa')
   end
 
-  it "has deck" do
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    deck = Deck.new([card1, card2, card3])
-    player = Player.new('Clarisa', deck)
+  it 'has deck' do
+    expect(@player.deck).to eq(@deck)
+  end
 
-    expect(player.deck).to eq(deck)
+  it 'has lost?' do
+    expect(@player.has_lost?).to eq(false)
   end
 end
