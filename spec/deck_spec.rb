95ad4a2b72a53034c0ddb82bcb056647ef5a0c90
Jacob Martinez <jacobmarcareer@gmail.com>
2021-05-12 22:22:32 -0600

    Make add card test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -64,7 +64,7 @@ RSpec.describe Deck do
     expect(deck.high_ranking_cards).to eq([card1, card3])
   end
 
-  it "gives percentage of high ranking cards" do
+  it 'gives percentage of high ranking cards' do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
@@ -74,7 +74,7 @@ RSpec.describe Deck do
     expect(deck.percent_high_ranking).to eq(66.67)
   end
 
-  it "removes first card from deck" do
+  it 'removes first card from deck' do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
@@ -85,5 +85,21 @@ RSpec.describe Deck do
     expect(deck.cards).to eq([card2, card3])
     expect(deck.high_ranking_cards).to eq([card3])
     expect(deck.percent_high_ranking).to eq(50.0)
+  end
+
+  it 'adds a card to deck' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    deck.remove_card
+    card4 = Card.new(:club, '5', 5)
+    deck.add_card(card4)
+
+    expect(card4).to be_a(Card)
+    expect(deck.cards).to eq([card2, card3, card4])
+    expect(deck.high_ranking_cards).to eq(card3)
+    expect(deck.percent_high_ranking).to eq(33.33)
   end
 end
