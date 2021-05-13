8104d6c66976b7d42f3455846bc5a377b196e1e1
Lee Hopper <hopper.lee.j@gmail.com>
2021-05-12 20:26:02 -0600

    Build initialize method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,13 +1,9 @@
+require './lib/card'
+
 class Deck
   attr_reader :cards
 
   def initialize(cards)
     @cards = cards
-  end
-
-  def rank_of_card_at(index)
-    for @cards.at(index)
-      return @rank
-    end
   end
 end
