e07ae2ad915a45035757351a5b576d2c3677e972
Sawaez Amin <sawaezamin@gmail.com>
2021-05-13 13:55:49 -0600

    Make deck spec changes

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,22 +1,21 @@
-require 'rspec'
 require './lib/card'
 require './lib/deck'
 
 
 RSpec.describe Deck do
-  it "exists" do
-    deck = Deck.new()
+  describe '#initialize' do
 
-    card1 = Card.new(:diamond, "Queen", 12)
-    card2 = Card.new(:spade, "3", 3)
-    card3 = Card.new(:heart, "Ace", 14)
-    cards = [card1, card2, card3]
+    it 'is a deck of cards' do
+      deck_of_cards = Deck.new
 
-    deck = Deck.new(cards)
-    expect
+      expect(deck_of_cards).to_be_an_instance_of(Deck)
+
+      card1 = Card.new(:diamond, "Queen", 12)
+      card2 = Card.new(:spade, "3", 3)
+      card3 = Card.new(:heart, "Ace", 14)
+      cards = [card1, card2, card3]
+
+    deck_of_cards = Deck.new(cards)
 
   end
-
-  it "has readable attributes" do
-
-  end
+end
