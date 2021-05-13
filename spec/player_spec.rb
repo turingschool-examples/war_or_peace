16d077e3ff2280257baee7e7fefb8cb2a74c2e64
sami-p <sami.peterson14@gmail.com>
2021-05-13 18:35:30 -0500

    Add second #has_lost? spec test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -36,5 +36,38 @@ RSpec.describe Player do
 
       expect(player.deck).to eq(deck)
     end
+
+    it 'defaults #has_lost? to false' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      deck = Deck.new([card1, card2, card3])
+
+      player = Player.new('Clarisa', deck)
+
+      expect(player.has_lost?).to eq(false)
+    end
+
+    it 'can tell when a player #has_lost' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      deck = Deck.new([card1, card2, card3])
+
+      player = Player.new('Clarisa', deck)
+
+      player.deck.remove_card
+      expect(player.deck.cards).to eq([card2, card3])
+      expect(player.has_lost?).to eq(false)
+
+
+      player.deck.remove_card
+      expect(player.deck.cards).to eq([card3])
+      expect(player.has_lost?).to eq(false)
+
+      player.deck.remove_card
+      expect(player.deck.cards).to eq([])
+      expect(player.has_lost?).to eq(true)
+    end
   end
 end
