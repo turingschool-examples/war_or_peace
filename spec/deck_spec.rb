fddffa031037782b6f9f8001380fed8d425005f0
Jacob Martinez <jacobmarcareer@gmail.com>
2021-05-12 22:08:09 -0600

    Add extra expects to remove card test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -83,5 +83,7 @@ RSpec.describe Deck do
 
     expect(deck.remove_card).to eq(card1)
     expect(deck.cards).to eq([card2, card3])
+    expect(deck.high_ranking_cards).to eq([card3])
+    expect(deck.percent_high_ranking).to eq(50.0)
   end
 end
