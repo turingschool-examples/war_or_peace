1442b646217d29acd0c5185d2f5c1a201f4f4d9c
Michael Abbott <michael@mjabbottdesign.com>
2021-05-12 20:22:11 -0700

    Add #high_ranking_cards test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -6,6 +6,7 @@ describe Deck do
   context 'attributes' do
     it 'is a deck' do
       deck = Deck.new([])
+
       expect(deck).to be_an_instance_of(Deck)
     end
 
@@ -15,6 +16,7 @@ describe Deck do
       card3 = Card.new(:heart, 'Ace', 14)
       cards = [card1, card2, card3]
       deck = Deck.new(cards)
+
       expect(deck.cards).to eq([card1, card2, card3])
     end
   end
@@ -26,10 +28,20 @@ describe Deck do
       card3 = Card.new(:heart, 'Ace', 14)
       cards = [card1, card2, card3]
       deck = Deck.new(cards)
+
       expect(deck.rank_of_card_at(0)).to eq(12)
       expect(deck.rank_of_card_at(1)).to eq(3)
       expect(deck.rank_of_card_at(2)).to eq(14)
     end
-  end
 
+    it 'returns high ranking cards' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+
+      expect(deck.high_ranking_cards).to eq([card1, card3])
+    end
+  end
 end
