774baf40127a0111891af7de2051f2f11beb458c
Brett Jones <bhjones4545@gmail.com>
2021-05-13 14:08:43 -0600

    add deck creation

---
+++ spec/deck_spec.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require './lib/card'
+require './lib/deck'
+
+Rspec.describe Deck do
+  deck :each do
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [@card1, @card2, @card3]
+
+    @deck = Deck.new[(@cards)]
+  end
+
+  it 'the deck exists' do
+    expect(deck).to be_an_instance_of(Deck)
+  end
+
+  it 'obtains cards' do
+    expect(deck.cards).to eq(@cards)
+  end
+
+end
