dc6830bfd3a3c9d75f1ff9d263a920268eef51e8
Jacob Martinez <jacobmarcareer@gmail.com>
2021-05-12 19:14:29 -0600

    Add Deck method test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -21,3 +21,25 @@ RSpec.describe Card do
     expect(card3).to be_a(Card)
   end
 end
+
+RSpec.describe Deck do
+  it 'exists' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck).to be_a(Deck)
+  end
+
+  it 'has cards' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.cards).to eq([card1, card2, card3])
+  end
+end
