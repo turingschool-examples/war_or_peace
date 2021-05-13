4f3832283d396927a8986b6738fe593524650952
Lee Hopper <hopper.lee.j@gmail.com>
2021-05-12 20:48:47 -0600

    Build test for rank_of_card_at method

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -19,4 +19,17 @@ describe Deck do
       expect(deck.cards).to eq [card1, card2, card3]
     end
   end
+
+  context 'Methods' do
+    it 'returns high ranking cards' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+
+      expect(deck.rank_of_cards_at(0)).to eq(12)
+      expect(deck.rank_of_cards_at(2)).to eq(14)
+    end
+  end
 end
