31eda16ce423052a2e23592f0895dece2703afbe
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-05-12 22:18:36 -0400

    Define card class, pass rspec tests

--- lib/card.rb
+++ lib/card.rb
@@ -1,2 +1,11 @@
 class Card
+  attr_accessor :suit, :rank, :value
+  def initialize(suit, value, rank)
+    @suit = suit
+    @rank = rank
+    @value = value
+  end
+
 end
+
+card = Card.new(:diamond, "Queen", 12)
