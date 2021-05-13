d71cd8490ff00824628d2941d2dc005316803a83
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-05-13 14:43:17 -0600

    Clean up deck_spec.rb

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -10,13 +10,12 @@ describe Deck do
     @cards = [@card1, @card2, @card3]
     @deck = Deck.new(@cards)
   end
-  it "exists" do
 
+  it "exists" do
     expect(@deck).to be_an_instance_of(Deck)
   end
 
   it "has readable attributes" do
-
     expect(@deck.cards).to match_array([
       have_attributes(suit: :diamond, value: "Queen", rank: 12),
       have_attributes(suit: :spade, value: "3", rank: 3),
@@ -25,13 +24,11 @@ describe Deck do
   end
 
   it "can output rank of card at certian index" do
-
     expect(@deck.rank_of_card_at(0)).to eq(12)
     expect(@deck.rank_of_card_at(2)).to eq(14)
   end
 
   it "can output array of high ranking cards" do
-
     expect(@deck.high_ranking_cards).to match_array([
     have_attributes(suit: :diamond, value: "Queen", rank: 12),
     have_attributes(suit: :heart, value: "Ace", rank: 14),
@@ -39,13 +36,11 @@ describe Deck do
   end
 
   it "can output the percent of high ranking cards" do
-
     expect(@deck.percent_high_ranking).to eq(66.67)
   end
 
   it "can output array of cards after removing a card" do
     @deck.remove_card
-
     expect(@deck.cards).to match_array([
       have_attributes(suit: :spade, value: "3", rank: 3),
       have_attributes(suit: :heart, value: "Ace", rank: 14),
@@ -56,12 +51,11 @@ describe Deck do
     @deck.remove_card
     card4 = Card.new(:club, '5', 5)
     @deck.add_card(card4)
-
     expect(@deck.cards).to match_array([
       have_attributes(suit: :spade, value: "3", rank: 3),
       have_attributes(suit: :heart, value: "Ace", rank: 14),
       have_attributes(suit: :club, value: "5", rank: 5),
       ])
   end
-
+  
 end
