2f3f51b1303329308a799615eb0465b0767a35bb
Lee Hopper <hopper.lee.j@gmail.com>
2021-05-13 22:14:40 -0600

    Build winner method

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,25 +2,48 @@ require './lib/player'
 require './lib/deck'
 
 class Turn
-  attr_reader :player_1, :player_2, :spoils_of_war
+  attr_reader :player1, :player2, :spoils_of_war
 
-  def initialize(player_1, player_2)
-    @player_1 = player_1
-    @player_2 = player_2
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
     @spoils_of_war = []
   end
 
   def type
-    p1_c1 = @player_1.deck.rank_of_card_at(0)
-    p2_c1 = @player_2.deck.rank_of_card_at(0)
-    p1_c2 = @player_1.deck.rank_of_card_at(2)
-    p2_c2 = @player_2.deck.rank_of_card_at(2)
-    if p1_c1 != p2_c1
+    @p1_c1 = @player1.deck.rank_of_card_at(0)
+    @p2_c1 = @player2.deck.rank_of_card_at(0)
+    @p1_c2 = @player1.deck.rank_of_card_at(2)
+    @p2_c2 = @player2.deck.rank_of_card_at(2)
+    if @p1_c1 != @p2_c1
       :basic
-    elsif p1_c1 == p2_c1 && p1_c2 != p2_c2
+    elsif @p1_c1 == @p2_c1 && @p1_c2 != @p2_c2
       :war
-    elsif p1_c1 == p2_c1 && p1_c2 == p2_c2
+    elsif @p1_c1 == @p2_c1 && @p1_c2 == @p2_c2
       :mutually_assured_destruction
     end
+  end
+
+  def winner
+    if type == :basic
+      result = @p1_c1 > @p2_c1
+    elsif type == :war
+      result = @p1_c2 > @p2_c2
+    else type == :mutually_assured_destruction
+      result = nil
+    end
+
+    if result == true
+      @player1
+    elsif result == false
+      @player2
+    else
+      'No Winner'
+    end
+  end
+
+  def pile_cards
+    if type == :basic
+      
   end
 end
