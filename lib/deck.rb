c05ae6fee7404b76304630a6cf2a2cddfa702093
Blake Dempsey <bdempsey864@gmail.com>
2021-05-13 18:19:22 -0600

    Added rank of card at

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,3 +1,18 @@
 class Deck
-  
+  attr_reader :cards
+  def initialize(cards)
+    # @rank = rank
+    # @suit = suit
+    # @value = value
+    @cards = cards
+  end
+
+  def rank_of_card_at(index)
+    @cards[index].rank
+  end
+
+  def high_ranking_cards
+    @cards.rank
+  end
+
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,10 +1,41 @@
 require 'rspec'
 require './lib/deck'
 
-RSpec describe Deck do
+RSpec.describe Deck do
   it 'exists'do
-    deck = Deck.new()
+    deck = Deck.new([])
 
-    expect(deck)to be_an_instance_of(Deck)
+    expect(deck).to be_an_instance_of(Deck)
+  end
+
+  it 'adds new cards' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.cards).to eq(cards)
+  end
+
+  it "can return rank of card using index position" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.rank_of_card_at(0)).to eq(12)
+    expect(deck.rank_of_card_at(2)).to eq(14)
+  end
+
+  it "cards that are highly ranked" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.percent_high_ranking(cards)).to eq(66.67)
   end
 end
