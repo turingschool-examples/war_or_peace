a055c7436637c7315370c6c6be76c7d2f45590cf
Lee Hopper <hopper.lee.j@gmail.com>
2021-05-13 20:34:44 -0600

    Build type method

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,5 @@
 require './lib/player'
+require './lib/deck'
 
 class Turn
   attr_reader :player_1, :player_2, :spoils_of_war
@@ -7,5 +8,19 @@ class Turn
     @player_1 = player_1
     @player_2 = player_2
     @spoils_of_war = []
+  end
+
+  def type
+    p1_c1 = @player_1.deck.rank_of_card_at(0)
+    p2_c1 = @player_2.deck.rank_of_card_at(0)
+    p1_c2 = @player_1.deck.rank_of_card_at(2)
+    p2_c2 = @player_2.deck.rank_of_card_at(2)
+    if p1_c1 != p2_c1
+      :basic
+    elsif p1_c1 == p2_c1 && p1_c2 != p2_c2
+      :war
+    elsif p1_c1 == p2_c1 && p1_c2 == p2_c2
+      :mutually_assured_destruction
+    end
   end
 end
