29efe69858020c74bf0fb377e824c84287a4a1ec
Caroline Tan <carolinectan3@gmail.com>
2021-05-14 11:19:37 -0400

    Identify methods of Player class

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,11 @@
 class Turn
+  attr_reader :basic, :war, :mutually_assured_destruction
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+  end
+
   def type
   end
 
