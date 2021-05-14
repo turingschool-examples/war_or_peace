e65b046cf87d5a05090dd3adf83e96404a1c82c3
Antonio King <antonio.j.king@gmail.com>
2021-05-13 21:57:34 -0600

    Create array of 52 cards

--- lib/card.rb
+++ lib/card.rb
@@ -61,6 +61,8 @@ card46 = Card.new(:clover, '8', 8)
 card47 = Card.new(:clover, '9', 9)
 card48 = Card.new(:clover, '10', 10)
 card49 = Card.new(:clover, 'Jack', 11)
-card51 = Card.new(:clover, 'Queen', 12)
-card52 = Card.new(:clover, 'King', 13)
-card53 = Card.new(:clover, 'Ace', 14)
+card50 = Card.new(:clover, 'Queen', 12)
+card51 = Card.new(:clover, 'King', 13)
+card52 = Card.new(:clover, 'Ace', 14)
+
+cards = [card1, card2, card3, card4, card4, card5, card6, card7, card8, card9, card10, card11, card12, card13, card14, card15, card16, card17, card18, card19, card20, card21, card22, card23, card24, card25, card26, card27, card28, card29, card30, card31, card32, card33, card34, card35, card36, card37, card38, card39, card40, card41, card42, card43, card44, card45, card46, card47, card48, card49, card50, card51, card52] 
