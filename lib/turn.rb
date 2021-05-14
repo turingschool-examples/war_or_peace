2cde3f935e27ce94a28e5262dff92cd0d7f87016
Michael Abbott <michael@mjabbottdesign.com>
2021-05-14 11:20:43 -0700

    Add #pile_cards, #award_spoils and #pile_cards test

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,4 +35,18 @@ class Turn
     end
   end
 
+  def pile_cards
+    if type == :basic
+      spoils_of_war << player1.deck.remove_card && spoils_of_war << player2.deck.remove_card
+    elsif type == :war
+      3.times { spoils_of_war << player1.deck.remove_card && spoils_of_war << player2.deck.remove_card }
+    elsif type == :mutually_assured_destruction
+      3.times { player1.deck.remove_card && player2.deck.remove_card }
+    end
+  end
+
+  def award_spoils(winner)
+    winner.deck.cards << spoils_of_war
+  end
+
 end
