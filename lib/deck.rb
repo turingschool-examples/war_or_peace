5864fbed5185446a758128d4d1c309c4b6a9aaee
Scott-Borecki <scottborecki@gmail.com>
2021-05-14 07:57:57 -0600

    Refactor deck.rb comments

--- lib/deck.rb
+++ lib/deck.rb
@@ -7,7 +7,7 @@ class Deck
   end
 
   def high_ranking_cards
-    # This method will return an array of cards in the deck that have a rank
+    # This method returns an array of cards in the deck that have a rank
     # of 11 or above (face cards and aces)
     high_ranking_cards = @cards.select do |card|
       card.rank > 10
@@ -15,26 +15,23 @@ class Deck
   end
 
   def rank_of_card_at(index)
-    # This method will take one argument that represents the index location of
-    # a card to be used and will return the rank of that card
+    # This method takes one argument that represents the index location of
+    # a card to be used and returns the rank of that card
     @cards[index].rank
   end
 
   def percent_high_ranking
-    # This method will return the percentage of cards that are high ranking
-    # high_ranking_cards = @cards.select do |card|
-    #   card.rank > 10
-    # end
+    # This method returns the percentage of cards that are high ranking
     (high_ranking_cards.count.fdiv(@cards.count) * 100).round(2)
   end
 
   def remove_card
-    # This method will remove the top card from the deck
+    # This method removes the top card from the deck
     @cards.delete_at(0)
   end
 
   def add_card(new_card)
-    # This method will add one card to the bottom (end) of the deck
+    # This method adds one card to the bottom (end) of the deck
     @cards.insert(-1, new_card)
   end
 
