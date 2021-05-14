ef3b2d3b070f3b6b48ebec0edba1cb943f722e57
Caroline Tan <carolinectan3@gmail.com>
2021-05-14 13:45:38 -0400

    Predict methods

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,5 @@
 class Turn
-  attr_reader :basic, :war, :mutually_assured_destruction
+  attr_reader :basic, :war, :spoils_of_war#, :mutually_assured_destruction
 
   def initialize(player1, player2)
     @player1 = player1
@@ -8,6 +8,9 @@ class Turn
 
   def type
   end
+
+
+
 
   def winner
   end
