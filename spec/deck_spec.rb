354878b681590663a4c7eee39270a7573911603e
Lee Hopper <hopper.lee.j@gmail.com>
2021-05-13 19:18:11 -0600

    Move class initialization to top of code block

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,71 +3,48 @@ require './lib/deck'
 require './lib/card'
 
 describe Deck do
+  card1 = Card.new(:diamond, 'Queen', 12)
+  card2 = Card.new(:spade, '3', 3)
+  card3 = Card.new(:heart, 'Ace', 14)
+  deck = Deck.new([card1, card2, card3])
+
   context 'Attributes' do
     it 'is a deck' do
-      deck = Deck.new([])
       expect(deck).to be_an_instance_of(Deck)
     end
 
     it 'has cards' do
-      card1 = Card.new(:diamond, 'Queen', 12)
-      card2 = Card.new(:spade, '3', 3)
-      card3 = Card.new(:heart, 'Ace', 14)
-      deck = Deck.new([card1, card2, card3])
-
       expect(deck.cards).to eq([card1, card2, card3])
     end
   end
 
   context 'Methods' do
     it 'returns card rank by index position' do
-      card1 = Card.new(:diamond, 'Queen', 12)
-      card2 = Card.new(:spade, '3', 3)
-      card3 = Card.new(:heart, 'Ace', 14)
-      deck = Deck.new([card1, card2, card3])
-
       expect(deck.rank_of_card_at(0)).to eq(12)
       expect(deck.rank_of_card_at(2)).to eq(14)
     end
 
     it 'returns high ranking cards' do
-      card1 = Card.new(:diamond, 'Queen', 12)
-      card2 = Card.new(:spade, '3', 3)
-      card3 = Card.new(:heart, 'Ace', 14)
-      deck = Deck.new([card1, card2, card3])
-
       expect(deck.high_ranking_cards).to eq([card1, card3])
     end
 
     it 'returns percent high ranking cards' do
-      card1 = Card.new(:diamond, 'Queen', 12)
-      card2 = Card.new(:spade, '3', 3)
-      card3 = Card.new(:heart, 'Ace', 14)
-      deck = Deck.new([card1, card2, card3])
-
       expect(deck.percent_high_ranking).to eq(66.67)
     end
 
     it 'remove card from top of deck' do
-      card1 = Card.new(:diamond, 'Queen', 12)
-      card2 = Card.new(:spade, '3', 3)
-      card3 = Card.new(:heart, 'Ace', 14)
-      deck = Deck.new([card1, card2, card3])
-      deck.remove_card
+      new_deck = Deck.new([card1, card2, card3])
+      new_deck.remove_card
 
-      expect(deck.cards).to eq([card2, card3])
+      expect(new_deck.cards).to eq([card2, card3])
     end
 
     it 'add card to bottom of deck' do
-      card1 = Card.new(:diamond, 'Queen', 12)
-      card2 = Card.new(:spade, '3', 3)
-      card3 = Card.new(:heart, 'Ace', 14)
-      deck = Deck.new([card1, card2, card3])
-
+      new_deck = Deck.new([card1, card2, card3])
       card4 = Card.new(:club, '5', 5)
-      deck.add_card(card4)
 
-      expect(deck.cards).to eq([card1, card2, card3, card4])
+      new_deck.add_card(card4)
+      expect(new_deck.cards).to eq([card1, card2, card3, card4])
     end
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,19 +3,18 @@ require './lib/deck'
 require './lib/player'
 
 describe Player do
+  card1 = Card.new(:diamond, 'Queen', 12)
+  card2 = Card.new(:spade, '3', 3)
+  card3 = Card.new(:heart, 'Ace', 14)
+  deck = Deck.new([card1, card2, card3])
+  player = Player.new('Clarisa', deck)
+
   context 'Attributes' do
     it 'is a player' do
-      player = Player.new('name', 'deck')
       expect(player).to be_an_instance_of(Player)
     end
 
     it 'has a name and deck' do
-      card1 = Card.new(:diamond, 'Queen', 12)
-      card2 = Card.new(:spade, '3', 3)
-      card3 = Card.new(:heart, 'Ace', 14)
-      deck = Deck.new([card1, card2, card3])
-      player = Player.new('Clarisa', deck)
-
       expect(player.name).to eq('Clarisa')
       expect(player.deck).to eq(deck)
     end
@@ -23,25 +22,15 @@ describe Player do
 
   context 'Methods' do
     it 'Return false player has lost?' do
-      card1 = Card.new(:diamond, 'Queen', 12)
-      card2 = Card.new(:spade, '3', 3)
-      card3 = Card.new(:heart, 'Ace', 14)
-      deck = Deck.new([card1, card2, card3])
-      player = Player.new('Clarisa', deck)
-
       expect(player.has_lost?).to be(false)
     end
 
     it 'Return true player has lost?' do
-      card1 = Card.new(:diamond, 'Queen', 12)
-      card2 = Card.new(:spade, '3', 3)
-      deck = Deck.new([card1, card2])
-      player = Player.new('Clarisa', deck)
-
-      player.deck.remove_card
-      player.deck.remove_card
-
-      expect(player.has_lost?).to be(true)
+      new_player = Player.new('Clarisa', deck)
+      new_player.deck.remove_card
+      new_player.deck.remove_card
+      new_player.deck.remove_card
+      expect(new_player.has_lost?).to be(true)
     end
   end
 end
