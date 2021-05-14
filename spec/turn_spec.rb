24efe249da8bbef15b461cc77ea95e8f13e9ce63
Lee Hopper <hopper.lee.j@gmail.com>
2021-05-13 22:14:21 -0600

    Build winner method tests

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -21,18 +21,20 @@ describe Turn do
 
   context 'Attribues' do
     it 'is a turn' do
+      
       expect(turn).to be_an_instance_of(Turn)
     end
 
     it 'returns arguments as attributes' do
-      expect(turn.player_1).to eq(player1)
-      expect(turn.player_2).to eq(player2)
+      expect(turn.player1).to eq(player1)
+      expect(turn.player2).to eq(player2)
       expect(turn.spoils_of_war).to eq([])
     end
   end
 
-  context 'Methods' do
+  context 'type method' do
     it 'returns type of turn :basic' do
+
       expect(turn.type).to eq(:basic)
     end
     it 'returns type of turn :war' do
@@ -50,6 +52,41 @@ describe Turn do
       new_player2 = Player.new("Aurora", new_deck2)
       new_turn = Turn.new(new_player1, new_player2)
       expect(new_turn.type).to eq(:mutually_assured_destruction)
+    end
+  end
+
+  context 'winner method' do
+    it 'return :basic type winner' do
+
+      expect(turn.winner).to eq(player1)
+    end
+
+    it 'return :war type winner' do
+      new_deck1 = Deck.new([card1, card2, card5, card8])
+      new_deck2 = Deck.new([card1, card4, card6, card7])
+      new_player1 = Player.new("Megan", new_deck1)
+      new_player2 = Player.new("Aurora", new_deck2)
+      new_turn = Turn.new(new_player1, new_player2)
+
+      expect(new_turn.winner).to eq(new_player2)
+    end
+
+    it 'return :mutually_assured_destruction type winner' do
+      new_deck1 = Deck.new([card1, card2, card5, card8])
+      new_deck2 = Deck.new([card1, card4, card5, card7])
+      new_player1 = Player.new("Megan", new_deck1)
+      new_player2 = Player.new("Aurora", new_deck2)
+      new_turn = Turn.new(new_player1, new_player2)
+
+      expect(new_turn.winner).to eq('No Winner')
+    end
+  end
+
+  context 'pile_cards method' do
+    it 'add cards to spoils for :basic turn' do
+      turn.pile_cards
+
+      expect(turn.spoils_of_war).to eq([card1, card3])
     end
   end
 end
