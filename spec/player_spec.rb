05e9b9be543f75dd570ed07a7559fbfb0a00101e
Lee Hopper <hopper.lee.j@gmail.com>
2021-05-14 16:55:14 -0600

    Change setup to before each loop

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,34 +3,33 @@ require './lib/deck'
 require './lib/player'
 
 describe Player do
-  card1 = Card.new(:diamond, 'Queen', 12)
-  card2 = Card.new(:spade, '3', 3)
-  card3 = Card.new(:heart, 'Ace', 14)
-  deck = Deck.new([card1, card2, card3])
-  player = Player.new('Clarisa', deck)
+  before(:each) do
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @deck = Deck.new([@card1, @card2, @card3])
+    @player = Player.new('Clarisa', @deck)
+  end
 
   context 'Attributes' do
-    it 'is a player' do
-      expect(player).to be_an_instance_of(Player)
-    end
-
-    it 'has a name and deck' do
-      expect(player.name).to eq('Clarisa')
-      expect(player.deck).to eq(deck)
+    it 'is a player with assigned instance variables' do
+      expect(@player).to be_an_instance_of(Player)
+      expect(@player.name).to eq('Clarisa')
+      expect(@player.deck).to eq(@deck)
     end
   end
 
   context 'Methods' do
     it 'Return false player has lost?' do
-      expect(player.has_lost?).to be(false)
+      expect(@player.has_lost?).to be(false)
     end
 
     it 'Return true player has lost?' do
-      new_player = Player.new('Clarisa', deck)
-      new_player.deck.remove_card
-      new_player.deck.remove_card
-      new_player.deck.remove_card
-      expect(new_player.has_lost?).to be(true)
+      @player = Player.new('Clarisa', @deck)
+      @player.deck.remove_card
+      @player.deck.remove_card
+      @player.deck.remove_card
+      expect(@player.has_lost?).to be(true)
     end
   end
 end
