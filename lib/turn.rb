3c49c2ba3223227c4754bdd1e715a1f7678f422c
Scott-Borecki <scottborecki@gmail.com>
2021-05-14 08:01:06 -0600

    Refactor turn.rb comments

--- lib/turn.rb
+++ lib/turn.rb
@@ -20,43 +20,43 @@ attr_reader :player1, :player2, :spoils_of_war
   end
 
   def winner
-    # this method will determine the winner of the turn.
-      # if the turn has a type of :basic, it will return whichever player has
-      # a higher rank_of_card_at(0)
+    # this method determines the winner of the turn.
     if type == :basic
+      # if the turn has a type of :basic, it returns whichever player has
+      # a higher rank_of_card_at(0)
       if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
         @winner = player1
       else
         @winner = player2
       end
-      # if the turn has a type of :war the winner will be whichever player has
-      # a higher rank_of_card_at(2)
     elsif type == :war
+      # if the turn has a type of :war, the winner is whichever player has
+      # a higher rank_of_card_at(2)
       if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
         @winner = player1
       else
         @winner = player2
       end
-      # if the turn has a type of :mutually_assured_destruction the method
-      # will return No Winner.
     else
+      # if the turn has a type of :mutually_assured_destruction, the method
+      # returns No Winner.
       return "No Winner"
     end
   end
 
   def pile_cards
-  #when this method is called, cards will be sent from the players' decks
+  #when this method is called, cards are sent from the players' decks
   # into the @spoils_of_war based on these rules:
-    # for a :basic turn, each player will send one card (the top card) to
-    # the spoils pile
     if type == :basic
+      # for a :basic turn, each player sends one card (the top card) to
+      # the spoils pile
       @spoils_of_war << @player1.deck.cards[0]
       @player1.deck.remove_card
       @spoils_of_war << @player2.deck.cards[0]
       @player2.deck.remove_card
-    # for a :war turn, each player will send three cards (the top three
-    # cards) to the spoils pile
     elsif type == :war
+      # for a :war turn, each player sends three cards (the top three
+      # cards) to the spoils pile
       @spoils_of_war << @player1.deck.cards[0]
       @spoils_of_war << @player1.deck.cards[1]
       @spoils_of_war << @player1.deck.cards[2]
@@ -69,11 +69,11 @@ attr_reader :player1, :player2, :spoils_of_war
       @player2.deck.remove_card
       @player2.deck.remove_card
       @player2.deck.remove_card
-    # for a :mutually_assured_destruction turn, each player will remove
-    # three cards from play (the top three cards in their deck). These
-    # cards are not sent to the spoils pile, they are simply removed from
-    # each players’ deck.
     elsif type == :mutually_assured_destruction
+      # for a :mutually_assured_destruction turn, each player removes
+      # three cards from play (the top three cards in their deck). These
+      # cards are not sent to the spoils pile, they are simply removed from
+      # each players’ deck.
       @player1.deck.remove_card
       @player1.deck.remove_card
       @player1.deck.remove_card
@@ -86,7 +86,7 @@ attr_reader :player1, :player2, :spoils_of_war
   end
 
   def award_spoils
-    # this method will add each of the cards in the @spoils_of_war array to
+    # this method adds each of the cards in the @spoils_of_war array to
     # the winner of the turn.
     @spoils_of_war.each do |spoils|
       @winner.deck.add_card(spoils)
