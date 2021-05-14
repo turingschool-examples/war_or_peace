6225f642a581c1a7bf20721f2491cef93159acda
Michael Abbott <michael@mjabbottdesign.com>
2021-05-13 21:16:20 -0700

    Add #winner, update #type and #type test

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,18 +8,31 @@ class Turn
   end
 
   def type
-    basic = :basic
-    war = :war
-    mutually_assured_destruction = :mutually_assured_destruction
+    # basic = :basic
+    # war = :war
+    # mutually_assured_destruction = :mutually_assured_destruction
 
     if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
-      basic
+      :basic
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+      :mutually_assured_destruction
     elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
-      war
-    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card(2) == player2.deck.rank_of_card(2)
-      mutually_assured_destruction
+      :war
     end
   end
 
+  def winner
+    if type == :basic && player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+      player1
+    elsif type == :basic && player1.deck.rank_of_card_at(0) < player2.deck.rank_of_card_at(0)
+      player2
+    elsif type == :war && player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+      player1
+    elsif type == :war && player1.deck.rank_of_card_at(2) < player2.deck.rank_of_card_at(2)
+      player2
+    elsif type == :mutually_assured_destruction
+      "No Winner"
+    end
+  end
 
 end
