a8f3548590b31d32abf263813cee2c8b0d942341
Amanda McGuire <aruwart001@gmail.com>
2021-05-14 17:51:44 -0600

    inital commit

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -15,4 +15,18 @@ RSpec.describe Card do
     expect(card.value).to eq('Queen')
     expect(card.rank).to eq(12)
   end
+
+  it "has a new card" do
+    card= Card.new(:heart, 'Jack', 11)
+
+    expect(card).to be_an_instance_of(Card)
+  end
+
+  it "has different readable attributes" do
+    card = Card.new(:heart, 'Jack', 11)
+
+    expect(card.suit).to eq(:heart)
+    expect(card.value).to eq('Jack')
+    expect(card.rank).to eq(11)
+  end
 end
