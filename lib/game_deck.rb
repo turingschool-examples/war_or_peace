0794b07361128bb38f4225216c4b67338ac62f55
Lee Hopper <hopper.lee.j@gmail.com>
2021-05-15 00:10:30 -0600

    Build gamedeck method and pass tests

--- lib/game_deck.rb
+++ lib/game_deck.rb
@@ -1,17 +1,21 @@
 require './lib/deck'
-require './lib/player'
 
 class GameDeck
+  attr_reader :deck
+
+  def initialize
+  end
 
   def build_game_deck
     suits = [:heart, :diamond, :spade, :club]
-    values = ['2', '3', '3', '5', '6', '7', '8', '9', 'Jack', 'Queen', 'King', 'Ace']
+    values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
     ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+    @deck = Deck.new([])
 
     suits.each do |suit|
-      ranks.length.times do |rank|
-        deck << Card.new(suit, values[rank], rank)
+      ranks.each do |rank|
+        @deck.cards << Card.new(suit, values[rank - 2], rank)
       end
     end
   end
-end 
+end
