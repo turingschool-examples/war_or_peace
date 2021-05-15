e43bdd6d0e38a817e90b33e06500792d59c01c31
Lee Hopper <hopper.lee.j@gmail.com>
2021-05-14 23:02:11 -0600

    Change percent high ranking method syntax

--- lib/game_deck.rb
+++ lib/game_deck.rb
@@ -1,1 +1,17 @@
 require './lib/deck'
+require './lib/player'
+
+class GameDeck
+
+  def build_game_deck
+    suits = [:heart, :diamond, :spade, :club]
+    values = ['2', '3', '3', '5', '6', '7', '8', '9', 'Jack', 'Queen', 'King', 'Ace']
+    ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+    suits.each do |suit|
+      ranks.length.times do |rank|
+        deck << Card.new(suit, values[rank], rank)
+      end
+    end
+  end
+end 
