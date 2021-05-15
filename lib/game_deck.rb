46b0eb9d5bc97bb1ab58e982c543f8bc2500d70f
Lee Hopper <hopper.lee.j@gmail.com>
2021-05-15 00:24:55 -0600

    Move deck instance variable to initialize method

--- lib/game_deck.rb
+++ lib/game_deck.rb
@@ -4,13 +4,13 @@ class GameDeck
   attr_reader :deck
 
   def initialize
+    @deck = Deck.new([])
   end
 
   def build_game_deck
     suits = [:heart, :diamond, :spade, :club]
     values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
     ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-    @deck = Deck.new([])
 
     suits.each do |suit|
       ranks.each do |rank|
