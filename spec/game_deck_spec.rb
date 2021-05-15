5f916bed17c4638d7689f0c6acc4c35dee0020e9
Lee Hopper <hopper.lee.j@gmail.com>
2021-05-15 16:38:52 -0600

    Modify test to test if class output can be assigned to Deck class

--- spec/game_deck_spec.rb
+++ spec/game_deck_spec.rb
@@ -10,16 +10,16 @@ describe GameDeck do
       deck1.build_game_deck
 
       expect(deck1).to be_an_instance_of(GameDeck)
-      expect(deck1.deck.cards.length).to eq(52)
-      expect(deck1.deck.cards.first.rank).to eq(2)
-      expect(deck1.deck.cards.first.value).to eq('2')
-      expect(deck1.deck.cards.first.suit).to eq(:heart)
-      expect(deck1.deck.cards.last.rank).to eq(14)
-      expect(deck1.deck.cards.last.value).to eq('Ace')
-      expect(deck1.deck.cards.last.suit).to eq(:club)
+      expect(deck1.game_deck.length).to eq(52)
+      expect(deck1.game_deck.first.rank).to eq(2)
+      expect(deck1.game_deck.first.value).to eq('2')
+      expect(deck1.game_deck.first.suit).to eq(:heart)
+      expect(deck1.game_deck.last.rank).to eq(14)
+      expect(deck1.game_deck.last.value).to eq('Ace')
+      expect(deck1.game_deck.last.suit).to eq(:club)
     end
 
-    it 'is a deck' do
+    it 'Can be used by Deck' do
       gamedeck = GameDeck.new
       gamedeck = gamedeck.build_game_deck
       deck = Deck.new(gamedeck)
