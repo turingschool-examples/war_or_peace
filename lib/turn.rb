3904ee18d39531adf9d93d4b11cfd7977783a34e
Carina Sweet <carina.sweet06@gmail.com>
2021-05-15 15:11:20 -0600

    Added winner, pile_cards, and award_spoils methods & tests

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,10 +9,51 @@ class Turn
     @spoils_of_war = []
   end
 
-  class Type
+  def type
+    if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
+      :basic
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
+      :war
+    else @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
+     :mutually_assured_destruction
+    end
+   end
 
-    def initialize(:basic, :war, :mutually_assured_destruction)
+   def winner
+     if type == :basic
+       if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
+         @player1
+       elsif @player1.deck.rank_of_card_at(0) < @player2.deck.rank_of_card_at(0)
+         @player2
+       end
+     elsif type == :war
+       if @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
+         @player1
+       elsif @player1.deck.rank_of_card_at(2) < @player2.deck.rank_of_card_at(2)
+         @player2
+       end
+     elsif type == :mutually_assured_destruction
+       "No Winner"
+     end
+  end
 
+  def pile_cards
+    if type == :basic
+      @spoils_of_war << @player1.deck.remove_card
+      @spoils_of_war << @player2.deck.remove_card
+    elsif type == :war
+      @spoils_of_war << @player1.deck(0..2)
+      @spoils_of_war << @player2.deck(0..2)
+    elsif type == :mutually_assured_destruction
+# need to finish
+    end
+  end
+
+  def award_spoils(winner)
+    if winner == @player1
+      @player1.deck << @spoils_of_war
+    elsif winner == @player2
+      @player2.deck << @spoils_of_war
     end
   end
 
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -5,33 +5,57 @@ require './lib/player'
 require './lib/turn'
 
 RSpec.describe Turn do
+  context 'type :basic' do
+    before :each do
+      @card1 = Card.new(:heart, 'Jack', 11)
+      @card2 = Card.new(:heart, '10', 10)
+      @card3 = Card.new(:heart, '9', 9)
+      @card4 = Card.new(:diamond, 'Jack', 11)
+      @card5 = Card.new(:heart, '8', 8)
+      @card6 = Card.new(:diamond, 'Queen', 12)
+      @card7 = Card.new(:heart, '3', 3)
+      @card8 = Card.new(:diamond, '2', 2)
 
-  before :each do
-    @card1 = Card.new(:heart, 'Jack', 11)
-    @card3 = Card.new(:heart, '9', 9)
-    @card4 = Card.new(:diamond, 'Jack', 11)
-    @card5 = Card.new(:heart, '8', 8)
-    @card6 = Card.new(:diamond, 'Queen', 12)
-    @card7 = Card.new(:heart, '3', 3)
-    @card8 = Card.new(:diamond, '2', 2)
+      @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+      @deck2 = Deck.new([@card3, @card4, @card6, @card7])
 
-    @deck1 = Deck.new([@card1, @card2, @card5, @card8])
-    @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+      @player1 = Player.new("Meagn", @deck1)
+      @player2 = Player.new("Aurora", @deck2)
 
-    @player1 = Player.new("Meagn", @deck1)
-    @player2 = Player.new("Aurora", @deck2)
+      @turn = Turn.new(@player1, @player2)
+    end
 
-    @turn = Turn.new(@player1, @player2)
+    it "has a turn" do
+      expect(@turn).to be_an_instance_of(Turn)
+    end
+
+    it "has an attr reader" do
+      expect(@turn.player1).to eq(@player1)
+      expect(@turn.player2).to eq(@player2)
+      expect(@turn.spoils_of_war).to eq([])
+    end
+
+    it "has a type" do
+      expect(@turn.type).to eq(:basic)
+    end
+
+    it "has a winner" do
+      expect(@turn.winner).to eq(@player1)
+    end
+
+    it "piles cards to spoils of war" do
+      @turn.pile_cards
+
+      expect(@turn.spoils_of_war).to eq([@card1, @card3])
+    end
+
+    it "awards spoils to winnder" do
+      @turn.pile_cards
+      @turn.spoils_of_war
+      @turn.award_spoils(@turn.winner)
+
+      expect(@turn.player1.deck).to eq([@card1, @card2, @card5, @card8, @card1, @card3])
+      expect(@turn.player2.deck).to eq([@card4, @card6, @card7])
+    end
   end
-
-  it "has a turn" do
-    expect(@turn).to be_an_instance_of(Turn)
-  end
-
-  it "has an attr reader" do
-    expect(@turn.player1).to eq(@player1)
-    expect(@turn.player2).to eq(@player2)
-    expect(@turn.spoils_of_war).to eq([])
-  end
-
 end
