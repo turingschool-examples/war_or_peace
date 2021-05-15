8103b85f87ed30b507865de55090fd30395d9dc1
sami-p <sami.peterson14@gmail.com>
2021-05-14 20:01:38 -0500

    Test :war #award_spoils; modify :basic #award_spoils test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -161,8 +161,8 @@ RSpec.describe Turn do
       turn.pile_cards
       turn.award_spoils(winner)
 
-      expect(player1.deck.cards).to eq([card2, card5, card8, card1, card3])
-      expect(player2.deck.cards).to eq([card4, card6, card7])
+      expect(player1.deck).to eq(deck1)
+      expect(player2.deck).to eq(deck2)
     end
   end
 
@@ -227,10 +227,37 @@ RSpec.describe Turn do
       player2 = Player.new("Aurora", deck2)
 
       turn = Turn.new(player1, player2)
-      
+
       turn.pile_cards
 
       expect(turn.spoils_of_war).to eq([card1, card2, card5, card4, card3, card6])
+    end
+
+    it '#award_spoils to winner' do
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:heart, '9', 9)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, 'Queen', 12)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card4, card3, card6, card7])
+
+      player1 = Player.new("Megan", deck1)
+      player2 = Player.new("Aurora", deck2)
+
+      turn = Turn.new(player1, player2)
+
+      winner = turn.winner
+
+      turn.pile_cards
+      turn.award_spoils(winner)
+
+      expect(player1.deck).to eq(deck1)
+      expect(player2.deck).to eq(deck2)
     end
   end
 end
