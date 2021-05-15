7470f1cb5ea487c6aca4fc1d5e948ee38a89124f
Antonio King <antonio.j.king@gmail.com>
2021-05-15 10:45:58 -0600

    Complete rank_of_card_at method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,28 +1,39 @@
 class Deck
-attr_reader :cards 
+  attr_reader :cards
 
-  def initialize(array_of_cards)
-    @arrray_of_cards = array_of_cards
+  # take one argument - an array of cards
+  def initialize(cards)
+    @cards = cards
   end
 
-  def rank_of_card_at(num)
-
+  # take one argument that represents the index location of a card to be used and return the rank of that card
+  def rank_of_card_at(index)
+    # @cards = cards = [card1, card2, card3] = Card.new("diamond", 'Queen', '12') = (suit, value, rank)
+    @cards[index].rank
+    # => rank_of_cards(0) returns 12, (1) returns 3, (2) returns 14
   end
 
-  def high_ranking_cards
+  # return an array of cards in the deck that have a rank of 11 or above (face cards or aces)
+  # def high_ranking_cards(card)
+  #   'Jack' = 11
+  #   'Queen' = 12
+  #   'King' = 13
+  #   'Ace' = 14
+  # end
 
+  # return the percentage of cards that are high ranking
+  # def percent_high_ranking
+  #   16 / 52
+  # end
+
+  # remove top card from the deck
+  def remove_card(card)
+  #   @cards -= card
   end
 
-  def percent_high_ranking
-
-  end
-
-  def remove_card
-
-  end
-
-  def add_card
-
+  # add one card to bottom of the deck
+  def add_card(card)
+    @cards << card
   end
 
 end
---
+++ spec/deck_spec.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+require './lib/deck'
+
+describe Deck do
+  it "exists" do
+    deck = Deck.new('cards')
+
+    expect(deck).to be_an_instance_of (Deck)
+  end
+
+  it "has 52 cards" do
+    deck = Deck.new()
+
+    expect(deck).to eq
+  end
+end
