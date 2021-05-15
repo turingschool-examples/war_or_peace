71881b1ab7097f8d9b089bc8ef82b40e8a5cbe6c
Antonio King <antonio.j.king@gmail.com>
2021-05-15 13:53:57 -0600

    Add high_ranking_cards test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,5 +1,6 @@
 require 'rspec'
 require './lib/deck'
+require './lib/deck'
 
 describe Deck do
   it "exists" do
@@ -13,5 +14,29 @@ describe Deck do
     deck = Deck.new(cards)
 
     expect(deck.cards).to eq(["card1", "card2", "card3"])
+  end
+
+  it "returns the rank of the card" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    expect(deck.rank_of_card_at(0)).to eq(12)
+  end
+
+  it "lists high ranking cards in deck" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    expect(deck.high_ranking_cards).to eq([card1, card3])
   end
 end
