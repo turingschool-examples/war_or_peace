6d6c6cf7ff57c90538db7496450d93424bff1053
Scott-Borecki <scottborecki@gmail.com>
2021-05-14 20:17:38 -0600

    Refactor turn.rb for low card counts

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,17 +12,19 @@ attr_reader :player1, :player2, :spoils_of_war
   def type
     if player1.deck.cards.length > 2 && player2.deck.cards.length > 2
 
-      if player1.deck.rank_of_card_at(0) ==   player2.deck.rank_of_card_at(0) &&
-        player1.deck.rank_of_card_at(2) ==  player2.deck.rank_of_card_at(2)
+      if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) &&
+        player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
         :mutually_assured_destruction
-      elsif player1.deck.rank_of_card_at(0) ==  player2.deck.rank_of_card_at(0)
+      elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
         :war
       else :basic
       end
 
-    else :basic # if either player has 2 or less cards, only a basic turn is
-      # called
-
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+      # This is considered a draw and the top card is put on the back
+      # of the deck
+      :draw
+    else :basic
     end
 
   end
@@ -53,34 +55,54 @@ attr_reader :player1, :player2, :spoils_of_war
   end
 
   def pile_cards
-  #when this method is called, cards are sent from the players' decks
-  # into the @spoils_of_war based on these rules:
+  # when this method is called, cards are sent from the players' decks
+  # into the @spoils_of_war:
     if type == :basic
-      # for a :basic turn, each player sends one card (the top card) to
-      # the spoils pile
-      @spoils_of_war << @player1.deck.cards[0]
-      @player1.deck.remove_card
-      @spoils_of_war << @player2.deck.cards[0]
-      @player2.deck.remove_card
+      pile_basic
     elsif type == :war
-      # for a :war turn, each player sends three cards (the top three
-      # cards) to the spoils pile
-      @player1.deck.cards[0..2].each { |card| @spoils_of_war << card }
-      3.times { @player1.deck.remove_card }
-      @player2.deck.cards[0..2].each { |card| @spoils_of_war << card }
-      3.times { @player2.deck.remove_card }
+      pile_war
     elsif type == :mutually_assured_destruction
-      # for a :mutually_assured_destruction turn, each player removes
-      # three cards from play (the top three cards in their deck). These
-      # cards are not sent to the spoils pile, they are simply removed from
-      # each players’ deck.
-      3.times { @player1.deck.remove_card }
-      3.times { @player2.deck.remove_card }
+      pile_mutually_assured_destruction
+    elsif type == :draw # shuffle deck otherwise will often end in draw.
+      # What fun is that?
+      pile_draw
     else
       p "Whoops! Something went wrong here!"
     end
   end
 
+  def pile_basic
+    # for a :basic turn, each player sends one card (the top card) to
+    # the spoils pile
+    @spoils_of_war << @player1.deck.cards[0]
+    @player1.deck.remove_card
+    @spoils_of_war << @player2.deck.cards[0]
+    @player2.deck.remove_card
+  end
+
+  def pile_war
+    # for a :war turn, each player sends three cards (the top three
+    # cards) to the spoils pile
+    @player1.deck.cards[0..2].each { |card| @spoils_of_war << card }
+    3.times { @player1.deck.remove_card }
+    @player2.deck.cards[0..2].each { |card| @spoils_of_war << card }
+    3.times { @player2.deck.remove_card }
+  end
+
+  def pile_mutually_assured_destruction
+    # for a :mutually_assured_destruction turn, each player removes
+    # three cards from play (the top three cards in their deck). These
+    # cards are not sent to the spoils pile, they are simply removed from
+    # each players’ deck.
+    3.times { @player1.deck.remove_card }
+    3.times { @player2.deck.remove_card }
+  end
+
+  def pile_draw
+    @player1.deck.cards.shuffle! # alternatively, could use #rotate method
+    @player2.deck.cards.shuffle! # alternatively, could use #rotate method
+  end
+  
   def award_spoils
     # this method adds each of the cards in the @spoils_of_war array to
     # the winner of the turn.
