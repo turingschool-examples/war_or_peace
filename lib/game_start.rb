17161686d3acd388f40a90d21edfda26c012d5d0
Elliot Olbright <elliotolbright@gmail.com>
2021-05-15 11:24:03 -0600

    add war_or_peace, start game start file

---
+++ lib/game_start.rb
@@ -0,0 +1,13 @@
+class Game
+    attr_reader :start
+    def initialize
+        
+    def start
+        p 'Welcome to War! (or Peace) This game will be played with 52 cards.
+        The players today are Megan and Aurora.
+        Type 'GO' to start the game!'
+        
+
+
+    end 
+end 
