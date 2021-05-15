3442a5ff5f4696653a568f764f8a385d2061164a
Matt Toensing <matthew.toensing@gmail.com>
2021-05-15 15:09:57 -0600

    Update and save work

--- lib/game.rb
+++ lib/game.rb
@@ -9,42 +9,44 @@ class Game
   def start
     # while both player cards are more than 0 and turn count is less than 1,000,000
     turn_count = 0
-    while @turn.player1.deck.cards.length >= 0 && @turn.player1.deck.cards.length >= 0 && turn_count <= 1000000
+    while @turn.player1.deck.cards.length >= 0 && @turn.player1.deck.cards.length >= 0 && turn_count < 1000000 do
       turn_count += 1
 
       if @turn.type == :basic
-        winner = @turn.winner
+        winner = turn.winner
         @turn.pile_cards
         @turn.award_spoils(winner)
 
         p "Turn #{turn_count}: #{winner.name} won 2 cards"
       elsif @turn.type == :war
-        winner = @turn.winner
+        winner = turn.winner
         @turn.pile_cards
         @turn.award_spoils(winner)
 
         p "Turn #{turn_count}: #{winner.name} won 6 cards"
       else
-        winner = @turn.winner
+        winner = turn.winner
         @turn.pile_cards
         @turn.award_spoils(winner)
 
         p "Turn #{turn_count}: *mutually assured destruction* 6 cards removed from play"
+        break 
       end
 
-      if @turn.player1.deck.cards.length == 0 || @turn.player1.deck.cards.length == 0
-        end_game
-      elsif turn_count == 1000000
-        p ' ---- DRAW ----'
-      end
+      break if @turn.player1.deck.cards.empty?  || @turn.player2.deck.cards.empty?
+      # elsif turn_count == 1000000
+      # p ' ---- DRAW ----'
     end
+    end_game
   end
+end
 
-  def end_game
-    if @turn.player1.has_lost? == true
-      p "#{@turn.player2.name} Won"
-    else @turn.player2.has_lost? == true
-      p "#{@turn.player1.name} Won"
-    end
+def end_game
+  if @turn.player1.has_lost? == true
+    p "*~*~*~* #{@turn.player2.name} has won the game! *~*~*~*"
+  elsif @turn.player2.has_lost? == true
+    p "*~*~*~* #{@turn.player1.name} has won the game! *~*~*~*"
+  else
+    p '---- DRAW ----'
   end
 end
