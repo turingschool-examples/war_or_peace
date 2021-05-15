4910a9d55aa32010c7ea3438092f84e5848ab692
Scott-Borecki <scottborecki@gmail.com>
2021-05-14 18:13:07 -0600

    Refactor #type

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,13 +10,21 @@ attr_reader :player1, :player2, :spoils_of_war
   end
 
   def type
-    if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) &&
-       player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
-      :mutually_assured_destruction
-    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
-      :war
-    else :basic
+    if player1.deck.cards.length > 2 && player2.deck.cards.length > 2
+
+      if player1.deck.rank_of_card_at(0) ==   player2.deck.rank_of_card_at(0) &&
+        player1.deck.rank_of_card_at(2) ==  player2.deck.rank_of_card_at(2)
+        :mutually_assured_destruction
+      elsif player1.deck.rank_of_card_at(0) ==  player2.deck.rank_of_card_at(0)
+        :war
+      else :basic
+      end
+
+    else :basic # if either player has 2 or less cards, only a basic turn is
+      # called
+
     end
+
   end
 
   def winner
