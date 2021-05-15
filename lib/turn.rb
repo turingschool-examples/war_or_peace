80cd598276a112ac67976feab706559e7888dcee
Matt Kragen <mattdkragen@gmail.com>
2021-05-14 19:22:53 -0700

    Add type method and test for :basic

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,4 +8,39 @@ class Turn
         @player2 = player2
         @spoils_of_war = []
     end
+
+    def type
+        if @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+            :mutually_assured_destruction
+        elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
+            :war
+        else
+            :basic
+        end
+    end
+
+    def winner
+        case type
+        when :basic
+            player1.deck[0] > player2.deck[0] ? player1 : player2
+        when :war
+            player1.deck[2] > player2.deck[2] ? player1 : player2
+        else
+            'No Winner'
+        end
+    end
+
+    def pile_cards
+        case type
+        when :basic
+            spoils_of_war << player1.deck[0]
+            spoils_of_war << player2.deck[0]
+        when :war
+            spoils_of_war << player1.deck[0..2]
+            spoils_of_war << player2.deck[0..2]
+        else
+            player1.deck.remove_card * 3
+            player2.deck.remove_card * 3
+        end
+    end
 end
