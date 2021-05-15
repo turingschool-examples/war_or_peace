28eb836361610be675706fa711d9235f28c4f9ee
Scott-Borecki <scottborecki@gmail.com>
2021-05-14 20:17:58 -0600

    Refactor game.rb for low card counts

--- lib/game.rb
+++ lib/game.rb
@@ -45,12 +45,14 @@ class Game
           p "Turn #{count}: WAR - #{turn_winner} won #{spoils_count} cards"
         elsif turn_type == :mutually_assured_destruction
           p "Turn #{count}: *mutually assured destruction* 6 cards removed from play"
+        elsif turn_type == :draw
+          p "Turn #{count}: The cards are getting low!  This was a draw! Please hold while I shuffle both decks!"
         else
           p "There's been a very terrible error!"
         end
 
-        p player1.deck.cards.count # checking card count
-        p player2.deck.cards.count # checking card count
+        # p player1.deck.cards.count # checking card count
+        # p player2.deck.cards.count # checking card count
 
         if player1.has_lost?
           p "*~*~*~* #{player2.name} has won the game! *~*~*~*"
@@ -132,7 +134,7 @@ class Game
 
     @full_deck = [card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11, card12, card13, card14, card15, card16, card17, card18, card19, card20, card21, card22, card23, card24, card25, card26, card27, card28, card29, card30, card31, card32, card33, card34, card35, card36, card37, card38, card39, card40, card41, card42, card43, card44, card45, card46, card47, card48, card49, card50, card51, card52]
 
-    @full_deck.shuffle!.shuffle!.shuffle!
+    @full_deck.shuffle!.shuffle!.shuffle! # three times a charm!
 
   end
 
