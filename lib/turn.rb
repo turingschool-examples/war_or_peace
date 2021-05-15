e0cb978ee0945e90fe9c4558da056bc1572df6e8
Jacob Martinez <jacobmarcareer@gmail.com>
2021-05-14 23:47:27 -0600

    Add winner method

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,4 +16,24 @@ class Turn
       return :war
     end
   end
+
+  def winner
+    if self.type == :basic
+      if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
+        return @player1
+      elsif @player1.deck.rank_of_card_at(0) < @player2.deck.rank_of_card_at(0)
+        return @player2
+      end
+
+    elsif self.type == :war
+      if @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
+        return @player1
+      elsif @player1.deck.rank_of_card_at(2) < @player2.deck.rank_of_card_at(2)
+        return @player2
+      end
+
+    elsif self.type == :mutually_assured_destruction
+      return 'No Winner'
+    end
+  end
 end
