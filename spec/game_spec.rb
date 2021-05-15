516e7c68d54058f96b2ae14daaa09562a4242b4e
Scott-Borecki <scottborecki@gmail.com>
2021-05-14 18:13:46 -0600

    Add #initialize_deck test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -34,29 +34,28 @@ RSpec.describe Game do
 
   describe 'Instance Methods' do
 
-    before(:each) do
-      @card1 = Card.new(:heart, 'Jack', 11)
-      @card2 = Card.new(:heart, '10', 10)
-      @card3 = Card.new(:heart, '9', 9)
-      @card4 = Card.new(:diamond, 'Jack', 11)
-      @card5 = Card.new(:heart, '8', 8)
-      @card6 = Card.new(:diamond, 'Queen', 12)
-      @card7 = Card.new(:heart, '3', 3)
-      @card8 = Card.new(:diamond, '2', 2)
-      @deck1 = Deck.new([@card1, @card2, @card5, @card8])
-      @deck2 = Deck.new([@card3, @card4, @card6, @card7])
-      @player1 = Player.new("Megan", @deck1)
-      @player2 = Player.new("Aurora", @deck2)
-      @turn = Turn.new(@player1, @player2)
+    # before(:each) do
+    #   @card1 = Card.new(:heart, 'Jack', 11)
+    #   @card2 = Card.new(:heart, '10', 10)
+    #   @card3 = Card.new(:heart, '9', 9)
+    #   @card4 = Card.new(:diamond, 'Jack', 11)
+    #   @card5 = Card.new(:heart, '8', 8)
+    #   @card6 = Card.new(:diamond, 'Queen', 12)
+    #   @card7 = Card.new(:heart, '3', 3)
+    #   @card8 = Card.new(:diamond, '2', 2)
+    #   @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    #   @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+    #   @player1 = Player.new("Megan", @deck1)
+    #   @player2 = Player.new("Aurora", @deck2)
+    #   @turn = Turn.new(@player1, @player2)
+    #
+    #   @game = Game.new
+    # end
 
-      @game = Game.new
-    end
-
-    xit '#start' do
-      expect(@game.start).to eq("Welcome to War! (or Peace) This game will be played with 52 cards.
-The players today are Megan and Aurora.
-Type 'GO' to start the game!
-------------------------------------------------------------------")
+    it '#initialize_deck' do
+      game = Game.new
+      game.initialize_deck
+      expect(game.full_deck.count).to eq(52)
     end
 
   end
