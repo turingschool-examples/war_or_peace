dbd0a8545920d826b4b768a7db385f00c5005a7b
Jessica Jones <daddehjones@gmail.com>
2021-05-16 18:28:41 -0400

    add turn type

---
+++ spec/turn.rb
@@ -0,0 +1,34 @@
+class Turn
+  attr_reader :player1, :player2, :spoils_of_war
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @spoils_of_war = []
+  end
+
+  def type
+    player1_first_card = @player1.deck.rank_of_card_at(0)
+    player2_first_card = @player2.deck.rank_of_card_at(0)
+    player1_third_card = @player1.deck.rank_of_card_at(2)
+    player2_third_card = @player2.deck.rank_of_card_at(2)
+    if player1_first_card == player2_first_card && player1_third_card == player2_third_card
+      return :mutually_assured_destruction
+    elsif player1_first_card == player2_first_card
+      return :war
+    else
+      return :basic
+  end
+
+    def winner
+      if type == :basic
+        if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
+          return @player1
+
+
+
+
+
+
+
+  end
