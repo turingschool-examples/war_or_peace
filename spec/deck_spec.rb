c6152d1b07bcfd9f772d73dda01c979e255edbad
Sawaez Amin <sawaezamin@gmail.com>
2021-05-16 15:03:47 -0600

    Make changes to add_ card

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -72,7 +72,6 @@ RSpec.describe Deck do
     end
 
     it 'can add cards' do
-      card1 = Card.new(:diamond, "Queen", 12)
       card2 = Card.new(:spade, "3", 3)
       card3 = Card.new(:heart, "Ace", 14)
       cards = [card1, card2, card3]
