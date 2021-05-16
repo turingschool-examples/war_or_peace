3af6960051ab3ecb59a1aec03717acd77b8d9326
Matt Kragen <mattdkragen@gmail.com>
2021-05-15 17:03:55 -0700

    Create .start method and requirements for spec

---
+++ lib/game.rb
@@ -0,0 +1,14 @@
+class = Game
+    def initialize
+    end
+
+    def start
+        puts 'Welcome to War! (or Peace) This game will be played with 52 cards.'
+        puts 'The players today are Megan and Aurora.'
+        puts 'Type 'GO' to start the game!'
+        puts '------------------------------------------------------------------'
+        print '> '
+        response = gets.chomp
+        if response == GO
+            
+    end
---
+++ spec/game_spec.rb
@@ -0,0 +1,9 @@
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+require './lib/game'
+
+RSpec.describe Game do
+    it 'exists' do
+        
