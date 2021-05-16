6659bf17d544dff7c22df7091e674f992c5e342e
KatherineMcConnell <mcconnell.katherine01@gmail.com>
2021-05-16 17:54:32 -0600

    Restructure file organization & attempt I4

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,2 +1,12 @@
+handle = file.open(ARGV[0], "r")
+
+incoming_text = handle.read
+
+writer = file.open(ARGV[1],'w')
+
+handle.write(cards.txt)
+
+writer.close
+
  filename = "cards.txt"
 require './lib/card_generator'
