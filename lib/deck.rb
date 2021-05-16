4304cbf5e379773f6647269fccd68547d551ad39
Alexandra Kaudy <alexkaudy@gmail.com>
2021-05-16 11:14:53 -0600

    add pseudocode to describe deck_spec goals

--- lib/deck.rb
+++ lib/deck.rb
@@ -9,24 +9,31 @@ class Deck
 
   def rank_of_card_at(i)
     # represents an index location of a card to be used
+    ranks = ['Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King','Ace']
   end
 
   def high_ranking_cards
     # returns array of cards in the deck that have a rank of 11 or above
+    high_ranking_cards = ['Jack', 'Queen', 'King', 'Ace']
   end
 
   def percent_high_ranking
     # returns the percentage of cards that are high ranking
+    high_ranking_cards.length / ranks.length * 100
   end
 
   def remove_card
     # remove the top card from the deck
+    cards.shift
   end
 
-  def add_card
+  def add_card(card)
     # add one card to the bottom of the deck. the bottom of the deck is the end of the deck.
+    @cards << card
   end
 end
+
+# Scratch work:
 
 #   @faces.each do |face|
 #     if face.class == Integer
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,6 +1,19 @@
 require './lib/deck'
+require './lib/card'
 require 'rspec'
 
 describe Deck do
+ # test that initialize returns an array of cards
 
+ # test that the attr_reader reads the @cards attribute
+
+ # test that the rank_of_card_at method takes one argument at an index location
+ # and returns the rank of the card
+
+ # test that the percentage of high ranking cards is as expected within the deck
+ # This should be 30.7 percent... I think... 13 cards, 4 high ranking cards
+
+ # test to ensure remove_card removes the top card from the deck
+
+ # test ot ensure that add_card adds one card to he bottom aka end of the deck
 end
