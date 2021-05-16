afdac03cc0c99cb4cbb2b0f050d1aa733bf15ea7
Alexandra Kaudy <alexkaudy@gmail.com>
2021-05-16 09:37:12 -0600

    Add pseudocode for methods to card class file

--- lib/card.rb
+++ lib/card.rb
@@ -12,19 +12,27 @@ class Card
   end
 
   def suit
+    # four possible suits - diamond, heart, spade, club
   end
 
   def value
+    # points teach card is worth based upon rank
+    # ranges from 2-14
   end
 
   def rank
+    # otherwise known as the face of the card
+    # ranks 2-10 are numbers
+    # ranks 11-14 are face cards aka high ranking cards
   end
 end
+
+
+# Scratch work:
 # define the methods listed above
 
 # close... but choose an element from each array
 # also assign the proper value
-# Couldn't this be done in the deck, though?
   # def suit
   #   suit = [:diamond, :heart, :spade, :club]
   # end
