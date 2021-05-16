d580f234c79a3ccf6705782b5c74be4e9ba8d3ad
Ted Staros <tstaros23@gmail.com>
2021-05-16 15:45:52 -0600

    created rank_of_card_at method

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -11,28 +11,55 @@ RSpec.describe Deck do
 
     cards = [card1, card2, card3]
 
-    cards = Deck.new(cards)
-    expect(cards).to be_an_instance_of(Deck)
+    deck = Deck.new(cards)
+    expect(deck).to be_an_instance_of(Deck)
   end
 
-  it "create cards" do
+  it "shows the cards in the deck" do
 
-
-  end
-
-
-  it "Card array" do
-
-
-    expect(@cards)
-
-  end
-
-  xit "cards array" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
 
     cards = [card1, card2, card3]
 
-    expect(deck.cards).to eq([card1, card2, card3])
-    end
+    deck = Deck.new(cards)
+    expect(deck.cards).to eq(cards)
+
+
+  end
+
+  it "find rank of card" do
+
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+
+      cards = [card1, card2, card3]
+
+      deck = Deck.new(cards)
+      expect(deck.rank_of_card_at(0)).to eq(12)
+      expect(deck.rank_of_card_at(2)).to eq(14)
+
+  end
+
+  it "find high ranking cards" do
+
+
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+
+        cards = [card1, card2, card3]
+
+        deck = Deck.new(cards)
+
+        expect(deck.high_ranking_cards) 
+
+
+
+
+
+  end
 
 end
