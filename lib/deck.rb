abc61324a12a3f179681c897b17e1891645185ca
Ted Staros <tstaros23@gmail.com>
2021-05-15 18:24:45 -0600

    Add cards to Deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,11 +2,10 @@ class Deck
   attr_reader :cards
 
   def initialize(cards)
-    @cards = cards
-    
+    @cards = []
   end
 
-  def rank_of_card_at(index)
+
 
 
 
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -2,32 +2,37 @@ require './lib/card'
 require './lib/deck'
 
 
-Rspec.describe Deck do
+RSpec.describe Deck do
   it "exists" do
+
     card1 = Card.new(:diamond, 'Queen', 12)
-
-    expect(card1).to be_an_instance_of(Deck)
-  end
-
-Rspec.describe Deck do
-  it "exists" do
     card2 = Card.new(:spade, '3', 3)
-
-    expect(card2).to be_an_instance_of(Deck)
-  end
-
-Rspec.describe Deck do
-  it "exists" do
     card3 = Card.new(:heart, 'Ace', 14)
-
-    expect(card3).to be_an_instance_of(Deck)
-  end
-
-Rspec.describe Deck do
-  it "cards array" do
 
     cards = [card1, card2, card3]
 
-    expect(deck.cards).to eq()
+    cards = Deck.new(cards)
+    expect(cards).to be_an_instance_of(Deck)
+  end
+
+  it "create cards" do
+
+
+  end
+
+
+  it "Card array" do
+
+
+    expect(@cards)
+
+  end
+
+  xit "cards array" do
+
+    cards = [card1, card2, card3]
+
+    expect(deck.cards).to eq([card1, card2, card3])
+    end
 
 end
