fce427628940378d7181dd7795142c0e1cf6c11a
Alexandra Kaudy <alexkaudy@gmail.com>
2021-05-16 10:22:21 -0600

    add notes re: failed rspec test

--- lib/card.rb
+++ lib/card.rb
@@ -10,27 +10,8 @@ class Card
     @value = value
     @rank = rank
   end
-
-  def suit
-    # four possible suits - diamond, heart, spade, club
-    # failing second test in card_spec.rb  - returns an array of all possible suits,
-    # but we want only one of hte suits to be chosen
-    suits = [:diamond, :heart, :spade, :club]
-  end
-
-  def value
-    # points teach card is worth based upon rank
-    # ranges from 2-14
-    values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-  end
-
-  def rank
-    # otherwise known as the face of the card
-    # ranks 2-10 are numbers
-    # ranks 11-14 are face cards aka high ranking cards
-    ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
-  end
 end
+
 
 
 # Scratch work:
@@ -49,3 +30,25 @@ end
   # def rank
   #   rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
   # end
+
+
+# More Scratch work - probably intended to run in the Deck
+# def suit
+#   # four possible suits - diamond, heart, spade, club
+#   # failing second test in card_spec.rb  - returns an array of all possible suits,
+#   # but we want only one of the suits to be chosen
+#   # suits = [:diamond, :heart, :spade, :club]
+# end
+#
+# def value
+#   # points teach card is worth based upon rank
+#   # ranges from 2-14
+#   # values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+# end
+#
+# def rank
+#   # otherwise known as the face of the card
+#   # ranks 2-10 are numbers
+#   # ranks 11-14 are face cards aka high ranking cards
+#   # ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+# end
