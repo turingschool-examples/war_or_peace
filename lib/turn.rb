3a3bb39d71634a585799eccf2fac82acb063e3f3
Jacob Martinez <jacobmarcareer@gmail.com>
2021-05-16 12:30:05 -0600

    Finish adding methods

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,7 @@
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+
 class Turn
   attr_reader :player1, :player2, :spoils_of_war
 
@@ -35,5 +39,29 @@ class Turn
     elsif self.type == :mutually_assured_destruction
       return 'No Winner'
     end
+  end
+
+  def pile_cards
+    if self.type == :basic
+      @spoils_of_war << player1.deck.remove_card
+      @spoils_of_war << player2.deck.remove_card
+    elsif self.type == :war
+      3.times do
+        @spoils_of_war << player1.deck.remove_card
+        @spoils_of_war << player2.deck.remove_card
+      end
+    else
+      3.time do
+        player1.deck.remove_card
+        player2.deck.remove_card
+      end
+    end
+  end
+
+  def award_spoils
+    @spoils_of_war.each do |card|
+      winner.deck.add_card(card)
+    end
+    @spoils_of_war = []
   end
 end
