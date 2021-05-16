eab3cc0d56d78e34039bf902f176ea4648a1fe8d
Brett Jones <bhjones4545@gmail.com>
2021-05-16 13:28:37 -0600

    player class completed

--- lib/player.rb
+++ lib/player.rb
@@ -2,10 +2,16 @@ class Player
 
 attr_reader :name,
             :deck
-            
+
   def initialize(name, deck)
     @name = name
-    @@deck = deck
+    @deck = deck
+  end
+
+#this determines when the player loses
+#could this piece of code work?(found online) ask instructor
+  def lost?
+    @deck.cards.length == 0
   end
 
 end
