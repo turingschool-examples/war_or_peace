a19836e7b74abce244d03d6e1bff78f4e76500ae
Alexandra Kaudy <alexkaudy@gmail.com>
2021-05-16 16:29:03 -0600

    resolve failed tests in deck, one test still fails

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,38 +4,70 @@ class Deck
 
   def initialize(cards)
     # returns an array of cards
-    @cards = []
+    @cards = cards
   end
 
   def rank_of_card_at(index)
     # represents an index location of a card to be used
-    rank = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King','Ace']
+    rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
     rank.fetch(index)
-
   end
 
   def high_ranking_cards
     # returns array of cards in the deck that have a rank of 11 or above
-    high_ranking_cards = ['Jack', 'Queen', 'King', 'Ace']
+    high_ranking_cards = [11, 12, 13, 14]
   end
 
   def percent_high_ranking
-    # returns the percentage of cards that are high ranking
-    high_ranking_cards.count / rank.count * 100.0
+    # returns the percentage of cards that are high ranking in the deck
+    # remember the deck refers to the cards in a player's hand, not the whole deck
+    # as it would br IRL - this isn't all 52 cards
+    # percent_high_ranking = (high_ranking_cards.length) / (cards.length) * (100)
+    # nope. repseudocode this...
+
+    # cards in hand with high rank divided by card in hand total
+    # How many cards are in the deck?
+    # set the length equal to a variable to make calculation easier in the future
+    # @cards << cards
+    # b = @cards.length
+    # Are there any high ranking cards in the deck?
+    # Keep in mind cards with a value greater than 11 are considered high ranking
+
+    # create an empty array for the high rnking (high value) cards
+    high_value_cards = []
+    # put the high cards in the deck (referred to as deck) into the array
+    # find cards in hand with high rank and put into another array
+    # Calculate the number of elements in this array (call it high_value_cards
+    @cards.each do |card|
+      if rank == [11, 12, 13, 14]
+        high_value_cards << card
+      end
+
+      high_value_cards.length
+    end
+
+    # Calculate the number of elements in this array and set equal to a variable to make calculation easier
+    # a = high_value_cards.length
+
+    # divide a by b and multiply the resulting number by 100.0 to get a number reprewsenting a percentage
+    # round to one decimal place for accuracy. Or not...
+    # (a / b)
+
+    percent_high_ranking = (high_value_cards.length).div(@cards.length)
   end
 
   def remove_card
     # remove the top card from the deck
-    cards.shift
+    @cards.shift
   end
 
   def add_card(card)
     # add one card to the bottom of the deck. the bottom of the deck is the end of the deck.
-    @cards << Card.new(card)
+    cards << @cards.shift
   end
 end
 
-# Scratch work:
+# Scratch work: nope nope nope  - I need to try another approach
 
 #   @faces.each do |face|
 #     if face.class == Integer
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -25,8 +25,8 @@ describe Deck do
    card3 = Card.new(:heart, 'Ace',14)
    cards = [card1, card2, card3]
    deck = Deck.new(cards)
-   
-   expect(deck.rank_of_card_at(2)).to eq('4')
+
+   expect(deck.rank_of_card_at(2)).to eq(4)
  end
 
  # test that the percentage of high ranking cards is as expected within the deck
@@ -34,11 +34,33 @@ describe Deck do
  # I think... 52 cards, 12 high ranking cards
  # I think the deck class refers to the cards in hand, in this case.
  # That is confusing...
-xit "percentage of ranking cards" do
+ it "percentage of ranking cards" do
+  card1 = Card.new(:diamond, 'Queen', 12)
+  card2 = Card.new(:spade, '3', 3)
+  card3 = Card.new(:heart, 'Ace',14)
+  cards = [card1, card2, card3]
+  deck = Deck.new(cards)
 
-  expect(deck.percent_high_ranking).to eq()
-end
+  expect(deck.percent_high_ranking).to eq(66)
+ end
  # test to ensure remove_card removes the top card from the deck
+ it "removes card from the top of the deck" do
+  card1 = Card.new(:diamond, 'Queen', 12)
+  card2 = Card.new(:spade, '3', 3)
+  card3 = Card.new(:heart, 'Ace',14)
+  cards = [card1, card2, card3]
+  deck = Deck.new(cards)
 
+  expect(deck.remove_card).to eq(card1)
+ end
  # test ot ensure that add_card adds one card to he bottom aka end of the deck
+ it "adds card to bottom of the deck" do
+   card1 = Card.new(:diamond, 'Queen', 12)
+   card2 = Card.new(:spade, '3', 3)
+   card3 = Card.new(:heart, 'Ace',14)
+   cards = [card1, card2, card3]
+   deck = Deck.new(cards)
+
+   expect(deck.add_card(card1)).to eq([card2, card3, card1])
+ end
 end
