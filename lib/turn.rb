9be843cb601c4749ba5e6c5bae50fbb7df21bc09
Alexandra Kaudy <alexkaudy@gmail.com>
2021-05-16 17:52:51 -0600

    Add pseudocode to methods in turn class file

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,4 +8,25 @@ class Turn
     @player2 = player2
   end
 
+  def type
+    :basic
+    # whichever player has a higher rank_of_card_at(0)
+    :war
+    # whichever player has a higher rank_of_card_at(2)
+    :mutually_assured_destruction
+    # neither player has a higher rank_of_card_at(2)
+    return "No winner"
+  end
+
+  def pile_cards
+    # for :basic turns, each player's top card is sent to the spoils pile
+    # for :war turns, each player sends top 3 cards to the spoils pile
+    # for :mutually_assured_destruction turns, each player removes cards from play
+      # these cards are removed from play, not to be placed in the spoils pile
+  end
+
+  def award_spoils
+     winner = []
+     winner << spoils_of_war
+  end
 end
