62a471175a49e421c0f88b4b94ef17b7c3a604de
Lee Hopper <hopper.lee.j@gmail.com>
2021-05-15 18:22:19 -0600

    Add start method initialize from deck object

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -1,7 +1,13 @@
-require './lib/deck'
-require './lib/player'
+require './lib/turn'
+require './lib/game_deck'
 
-deck1 = Deck.new(cards1)
-deck2 = Deck.new(cards2)
+deck = GameDeck.new
+deck.build_game_deck
+deck1 = Deck.new(deck.game_deck)
+deck2 = Deck.new(deck.game_deck)
+deck1.shuffle
+deck2.shuffle
 player1 = Player.new('Lee', deck1)
 player2 = Player.new('Natalie', deck2)
+turn = Turn.new(player1, player2)
+turn.start
