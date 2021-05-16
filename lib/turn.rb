cc5bc353ed322ffb5c1976aeb843c0f715bc5d27
Miriam Aceves <miriammerari02@gmail.com>
2021-05-16 16:55:03 -0400

    Complete type method

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,18 +5,35 @@ class Turn
     @player1 = player1
     @player2 = player2
     @spoils_of_war = []
+    @type = type
+  end
+
+  def type
+    if p1_card.deck.rank_of_card_at(0) != p2_card.deck.rank_of_card_at(0)
+      @type = :basic
+    elsif p1_card.deck.rank_of_card_at(0) == p2_card.deck.rank_of_card_at(0)
+      @type = :war
+    elsif p1_card.deck.rank_of_card_at(0) == p2_card.deck.rank_of_card_at(0) && p1_card.deck.rank_of_card_at(2) == p2_card.deck.rank_of_card_at(2)
+      @type = :mutually_assured_destruction
+    end
   end
 
   def winner
-    p1_card = @player1.deck.rank_of_card_at(0)
-    p2_card = @player2.deck.rank_of_card_at(0)
+    p1_card = @player1.deck.rank_of_card_at()
+    p2_card = @player2.deck.rank_of_card_at()
 
     if p1_card > p2_card
-
-      return @player1
+     return @player1
     elsif p2_card > p1_card
-
       return @player2
     end
+
+    def pile_cards
+    end
+
+    def award_spoils
+      turn.winner << @spoils_of_war
+    end
+
   end
 end
