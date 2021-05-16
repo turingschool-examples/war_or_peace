d6952ca200252bef897b5926393b48c48e7bf3b6
Jason Knoll <jason.p.knoll@gmail.com>
2021-05-16 03:14:41 -0700

    Finish iteration 2

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,7 +9,6 @@ class Turn
     @player1 = player1
     @player2 = player2
     @spoils_of_war = []
-    @pile_cards = []
   end
 
   # Determines the type of turn that will take place
@@ -17,14 +16,14 @@ class Turn
   def type
      if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
        :basic
-     elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(0)
+     elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
        :mutually_assured_destruction
      elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
        :war
      end
    end
 
-
+   # game mechanic that determines the winner
    def winner
      if type == :basic
        if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
@@ -39,16 +38,34 @@ class Turn
          @player2
        end
      elsif type == :mutually_assured_destruction
+       puts "No Winner"
      end
    end
 
+   # when this method is called, cards will be sent from the players’ decks into the @spoils_of_war based on these rules
    def pile_cards
+     if type == :basic
+       self.spoils_of_war << @player1.deck.remove_card
+       self.spoils_of_war << @player2.deck.remove_card
+     elsif type == :war
+      3.times do
+        self.spoils_of_war <<  @player1.deck.remove_card
+        self.spoils_of_war <<  @player2.deck.remove_card
+      end
+     elsif type == :mutually_assured_destruction
+       3.times do
+         @player1.deck.remove_card
+         @player2.deck.remove_card
+       end
+     end
    end
 
-   def spoils_of_war
+   # This method will add each of the cards in the @spoils_of_war array to the winner of the turn
+   # .map
+   def award_spoils(winner)
+     @spoils_of_war.each do |card|
+       winner.deck.cards << card
+     end
+     @spoils_of_war = []
    end
-
-   def award_spoils
-   end
-
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -47,11 +47,75 @@ RSpec.describe Turn do
     expect(@turn.winner).to eq(@player1)
   end
 
-  xit 'Spoils of war works' do
+  it 'Pile cards?' do
     @turn.winner
-    expect(@turn.spoils_of_war).to eq([@card3])
-
+    expect(@player1.deck).to eq(@deck1)
+    expect(@player2.deck).to eq(@deck2)
+    @turn.pile_cards
+    expect(@turn.spoils_of_war).to eq([@card1, @card3])
   end
 
+  it 'Sends won cards to winning player' do
+    winner = @turn.winner
+    @turn.pile_cards
+    @turn.award_spoils(winner)
+    expect(@player1.deck.cards).to eq([@card2, @card5, @card8, @card1, @card3])
+  end
+end
+describe 'Turn type war' do
+  before(:each) do
+    @card1 = Card.new(:heart, 'Jack', 11)
+    @card2 = Card.new(:heart, 'Ten', 10)
+    @card3 = Card.new(:heart, 'Nine', 9)
+    @card4 = Card.new(:diamond, 'Jack', 11)
+    @card5 = Card.new(:heart, 'Eight', 8)
+    @card6 = Card.new(:diamond, 'Queen', 12)
+    @card7 = Card.new(:heart, 'Three', 3)
+    @card8 = Card.new(:diamond, 'Two', 2)
+    @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    @deck2 = Deck.new([@card4, @card3, @card6, @card7])
+    @player1 = Player.new("Meegan", @deck1)
+    @player2 = Player.new("André", @deck2)
+    @turn = Turn.new(@player1, @player2)
+  end
 
+  it 'Should be War turn' do
+    expect(@turn.type).to eq(:war)
+  end
+
+  it 'Give cards to winner :War' do
+    winner = @turn.winner
+    @turn.pile_cards
+    @turn.award_spoils(winner)
+    expect(@player2.deck.cards).to eq([@card7, @card1, @card4, @card2, @card3, @card5, @card6])
+  end
+end
+describe 'Turn type mutually_assured_destruction' do
+  before(:each) do
+    @card1 = Card.new(:heart, 'Jack', 11)
+    @card2 = Card.new(:heart, 'Ten', 10)
+    @card3 = Card.new(:heart, 'Nine', 9)
+    @card4 = Card.new(:diamond, 'Jack', 11)
+    @card5 = Card.new(:heart, 'Eight', 8)
+    @card6 = Card.new(:diamond, 'Eight', 8)
+    @card7 = Card.new(:heart, 'Three', 3)
+    @card8 = Card.new(:diamond, 'Two', 2)
+    @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    @deck2 = Deck.new([@card4, @card3, @card6, @card7])
+    @player1 = Player.new("Meegan", @deck1)
+    @player2 = Player.new("André", @deck2)
+    @turn = Turn.new(@player1, @player2)
+  end
+
+  it 'Should be War turn' do
+    expect(@turn.type).to eq(:mutually_assured_destruction)
+  end
+
+  it 'Players left with just one card' do
+    winner = @turn.winner
+    @turn.pile_cards
+    @turn.award_spoils(winner)
+    expect(@player1.deck.cards).to eq([@card8])
+    expect(@player2.deck.cards).to eq([@card7])
+  end
 end
