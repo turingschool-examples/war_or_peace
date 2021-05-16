050f47b170a77711b26d1b768b7af0edf3bca415
Blake Dempsey <bdempsey864@gmail.com>
2021-05-16 15:24:21 -0600

    Added winner types

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,13 +1,30 @@
 class Turn
-  attr_reader :player1, :player2, :spoils_of_war
- def initialize(player1, player2, spoils_of_war)
+  attr_reader :player1, :player2, :spoils_of_war, :winner
+ def initialize(player1, player2)
    @player1 = player1
    @player2 = player2
-   @spoils_of_war = spoils_of_war
+   @spoils_of_war = []
+   @winner = nil
  end
 
- # def basic
- #   @player1 = player1
- #   @player2 = player2
- # end
+ def type
+  if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
+    if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
+      @winner = @player1
+    else
+      @winner = @player2
+    end
+    :basic
+  elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
+    @winner = "No Winner"
+    :mutually_assured_destruction
+  else
+    if @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
+      @winner = @player1
+    else
+      @winner = @player2
+    end
+    :war
+  end
+ end
 end
