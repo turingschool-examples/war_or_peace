1e0396b26da9c1e4b7556c1cab732287dbf850fd
Brett Jones <bhjones4545@gmail.com>
2021-05-16 17:19:41 -0600

    fixed card mistakes

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,54 +11,58 @@ class Turn
   end
 
   def type
-    if player1.deck.cards.length > 2 && player2.deck.cards.length > 2
-
-      if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
-        && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
-        :mutually_assured_destruction
-      elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
-        && player1.deck.rank_of_card_at(2) != player2.deck.rank_of_card_at(2)
-      else
-        :basic
-      end
+    if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
+      :basic
+    elsif @player1.deck.rank_of_card_at(2) != @player2.deck.rank_of_card_at(2)
+      :war
+    elsif @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
+      :mutually_assured_destruction
     end
   end
 
   def winner
     if type == :basic
       if @winner = player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
-      @player1
-      else
-      @player2
+        @winner = player1
+      else @winner = player1.deck.rank_of_card_at(0) < player2.deck.rank_of_card_at(0)
+        @winner = player2
       end
     elsif type == :war
       if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
         @player1
-      else
+      else player1.deck.rank_of_card_at(2) < player2.deck.rank_of_card_at(2)
         @player2
       end
-    elsif type == mutually_assured_destruction
+    elsif type == :mutually_assured_destruction
       "No Winner"
     end
   end
 
   def pile_cards
     if type == :basic
-      @spoils_of_war << @player1.deck.remove_card
-      @spoils_of_war << @player2.deck.remove_card
+      @spoils_of_war << @player1.deck.cards[0]
+      @player1.deck.remove_card
+      @spoils_of_war << @player2.deck.cards[0]
+      @player2.deck.remove_card
     elsif type == :war
-      3.times {@spoils_of_war << @player1.deck.remove_card}
-      3.times {@spoils_of_war << @player2.deck.remove_card}
-    else
+      @player1.deck.cards[0..2].each do |card|
+        @spoils_of_war << @card
+      end
+      3.times {@player1.deck.remove_card}
+      @player2.deck.cards[0..2].each do |card|
+        @spoils_of_war << @card
+      end
+      3.times {@player2.deck.remove_card}
+    elsif type == :mutually_assured_destruction
       #mutually_assured_destruction
       3.times {@player1.deck.remove_card}
       3.times {@player2.deck.remove_card}
     end
   end
 
-  def award_spoils(winner)
-    @spoils_of_war.each do |card|
-      winner.deck.add_card(card)
+  def award_spoils
+    @spoils_of_war.each do |spoils|
+      @winner.deck.add_card(spoils)
     end
   end
 
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -49,7 +49,6 @@ describe 'basic turn type' do
     @player1 = Player.new("Megan", @deck1)
     @player2 = Player.new("Aurora", @deck2)
     @turn = Turn.new(@player1, @player2)
-    @winner = turn.winner
   end
 
   it 'is basic turn type' do
@@ -67,14 +66,14 @@ describe 'basic turn type' do
 
   it 'award winner with spoils_of_war' do
     @turn.pile_cards
-    @turn.award_spoils(winner)
+    @turn.award_spoils
     expect(@player1.deck.cards).to eq([@card2, @card5, @card8, @card1, @card3])
     expect(@player2.deck.cards).to eq([@card4, @card6, @card7])
   end
 
 end
 
-descirbe 'War turn type' do
+describe 'War turn type' do
 
   before :each do
     @card1 = Card.new(:heart, 'Jack', 11)
@@ -86,14 +85,13 @@ descirbe 'War turn type' do
     @card7 = Card.new(:heart, '3', 3)
     @card8 = Card.new(:diamond, '2', 2)
     @deck1 = Deck.new([@card1, @card2, @card5, @card8])
-    @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+    @deck2 = Deck.new([@card4, @card3, @card6, @card7])
     @player1 = Player.new("Megan", @deck1)
     @player2 = Player.new("Aurora", @deck2)
     @turn = Turn.new(@player1, @player2)
-    @winner = turn.winner
   end
 
-  it 'is war turn type' do
+  it 'turn type' do
     expect(@turn.type).to eq(:war)
   end
 
@@ -103,7 +101,7 @@ descirbe 'War turn type' do
 
   it 'pile_cards for war type' do
     @turn.pile_cards
-    expect(@turn.spoils_of_war).to eq([@card1, @card2, @card5, @card3, @card4, @card5])
+    expect(@turn.spoils_of_war).to eq([@card1, @card2, @card5, @card4, @card3, @card5])
     expect(@player1.deck).to eq(@card8)
     expect(@player2.deck).to eq(@card7)
   end
@@ -117,7 +115,7 @@ descirbe 'War turn type' do
 
 end
 
-descirbe 'mutually_assured_destruction type' do
+describe 'mutually_assured_destruction type' do
 
   before :each do
     @card1 = Card.new(:heart, 'Jack', 11)
@@ -125,19 +123,18 @@ descirbe 'mutually_assured_destruction type' do
     @card3 = Card.new(:heart, '9', 9)
     @card4 = Card.new(:diamond, 'Jack', 11)
     @card5 = Card.new(:heart, '8', 8)
-    @card6 = Card.new(:diamond, 'Queen', 12)
+    @card6 = Card.new(:diamond, '8', 8)
     @card7 = Card.new(:heart, '3', 3)
     @card8 = Card.new(:diamond, '2', 2)
     @deck1 = Deck.new([@card1, @card2, @card5, @card8])
-    @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+    @deck2 = Deck.new([@card4, @card3, @card6, @card7])
     @player1 = Player.new("Megan", @deck1)
     @player2 = Player.new("Aurora", @deck2)
     @turn = Turn.new(@player1, @player2)
-    @winner = turn.winner
   end
 
   it 'is mutually_assured_destruction type' do
-    expect(@turn.type).to eq(:Mutually_assured_destruction)
+    expect(@turn.type).to eq(:mutually_assured_destruction)
   end
 
   it 'has mutually_assured_destruction winner' do
@@ -155,7 +152,7 @@ descirbe 'mutually_assured_destruction type' do
     @turn.pile_cards
     @turn.award_spoils
     expect(@player1.deck.cards).to eq([@card8])
-    expect(@player2.deck.cards).to eq([@card9])
+    expect(@player2.deck.cards).to eq([@card7])
   end
 
 end
