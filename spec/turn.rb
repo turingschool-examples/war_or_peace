f0d32d4abf3ccb8dfb5bde537ac4f0253e3f0094
Jessica Jones <daddehjones@gmail.com>
2021-05-16 19:43:16 -0400

    add spoils and piles

--- spec/turn.rb
+++ spec/turn.rb
@@ -18,30 +18,39 @@ class Turn
       return :war
     else
       return :basic
+    end
   end
 
-    def winner
-      if type == :basic
-        if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
-          return @player1
-        else
-          return @player2
-        end
-      if type == :war
-        if @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
-          return @player1
-        else
-          return @player2
-        end
-      if type == :mutually_assured_destruction
-        return "No Winner"
+  def winner
+    if type == :basic
+      if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
+        return @player1
+      else
+        return @player2
       end
-      
 
+    elsif type == :war
+      if @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
+        return @player1
+      else
+        return @player2
+      end
 
+    else type == :mutually_assured_destruction
+      return "No Winner"
+    end
 
+    def pile_cards
+      if type == :basic
+        @spoils_of_war = ([card1, card3])
+      elsif type == :war
+        @spoils_of_war = ([card1, card2, card5, card3, card4, card6])
+      elsif type == :mutually_assured_destruction
+        @spoils_of_war = []
+      end
 
-
-
-
+    def award_spoils(winner)
+    end
   end
+
+end
