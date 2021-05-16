985c4a1814822f4dc044d41c0b77603baaa81f36
Brett Jones <bhjones4545@gmail.com>
2021-05-16 14:36:21 -0600

    war type tests finished

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,8 +1,8 @@
 require 'rspec'
 require './lib/card'
 require './lib/deck'
-require '.lib/player'
-require '.lib/turn'
+require './lib/player'
+require './lib/turn'
 
 RSpec.describe Turn do
 
@@ -49,6 +49,7 @@ describe 'basic turn type' do
     @player1 = Player.new("Megan", @deck1)
     @player2 = Player.new("Aurora", @deck2)
     @turn = Turn.new(@player1, @player2)
+    @winner = turn.winner
   end
 
   it 'is basic turn type' do
@@ -59,16 +60,59 @@ describe 'basic turn type' do
     expect(@turn.winner).to eq(@player1)
   end
 
-  it 'pile cards for basic type' do
+  it 'pile_cards for basic type' do
     @turn.pile_cards
     expect(@turn.spoils_of_war).to eq([@card1, @card3])
   end
 
   it 'award winner with spoils_of_war' do
     @turn.pile_cards
-    @turn.award_spoils
+    @turn.award_spoils(winner)
     expect(@player1.deck.cards).to eq([@card2, @card5, @card8, @card1, @card3])
     expect(@player2.deck.cards).to eq([@card4, @card6, @card7])
+  end
+
+end
+
+descirbe 'War turn type' do
+
+  before :each do
+    @card1 = Card.new(:heart, 'Jack', 11)
+    @card2 = Card.new(:heart, '10', 10)
+    @card3 = Card.new(:heart, '9', 9)
+    @card4 = Card.new(:diamond, 'Jack', 11)
+    @card5 = Card.new(:heart, '8', 8)
+    @card6 = Card.new(:diamond, 'Queen', 12)
+    @card7 = Card.new(:heart, '3', 3)
+    @card8 = Card.new(:diamond, '2' 2)
+    @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+    @player1 = Player.new("Megan", @deck1)
+    @player2 = Player.new("Aurora", @deck2)
+    @turn = Turn.new(@player1, @player2)
+    @winner = turn.winner
+  end
+
+  it 'is war turn type' do
+    expect(@turn.type).to eq(:war)
+  end
+
+  it 'winner of war type' do
+    expect(@turn.winner).to eq(@player2)
+  end
+
+  it 'pile_cards for war type' do
+    @turn.pile_cards
+    expect(@turn.spoils_of_war).to eq([@card1, @card2, @card5, @card3, @card4, @card5])
+    expect(@player1.deck).to eq(@card8)
+    expect(@player2.deck).to eq(@card7)
+  end
+
+  it 'award winner with spoils_of_war for war type' do
+    @turn.pile_cards
+    @turn.award_spoils
+    expect(@player1.deck.cards).to eq([@card8])
+    expect(@player2.deck.cards).to eq([@card7, @card1, @card2, @card5, @card4, @card3, @card6])
   end
 
 end
