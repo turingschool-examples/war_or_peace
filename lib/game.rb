ecbb5664aa996a395eab375b67b5cec49e7e1b58
Matt Kragen <mattdkragen@gmail.com>
2021-05-16 11:49:36 -0700

    Refactor welcome method

--- lib/game.rb
+++ lib/game.rb
@@ -6,17 +6,18 @@ class Game
     end
 
     def welcome
-        puts "Welcome to War! (or Peace) This game will be played with 52 cards."
-        puts "Welcome to War! (or Peace) This game will be played with 52 cards."
-        puts "The players today are Megan and Aurora."
-        puts "Type 'GO' to start the game!"
-        puts '------------------------------------------------------------------'
+        p "Welcome to War! (or Peace) This game will be played with 52 cards."
+        p "Welcome to War! (or Peace) This game will be played with 52 cards."
+        p "Welcome to War! (or Peace) This game will be played with 52 cards."
+        p "The players today are Megan and Aurora."
+        p "Type 'GO' to start the game!"
+        p '------------------------------------------------------------------'
         print '> '
         response = gets.chomp
         if response != GO
-            puts "I see... Perhaps another time, then."
+            p "I see... Perhaps another time, then."
         else
-            puts "Then let's begin!"
+            p "Then let's begin!"
         end
     end
 
