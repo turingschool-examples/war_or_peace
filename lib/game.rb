39e87d9988786aa20b7af47977e7e2d876ecc8bf
Matt Toensing <matthew.toensing@gmail.com>
2021-05-16 16:58:25 -0600

    Player win count feature added

--- lib/game.rb
+++ lib/game.rb
@@ -7,35 +7,43 @@ class Game
   end
 
   def start
-    # while both player cards are more than 0 and turn count is less than 1,000,000
+    p "Welcome to War! (or Peace) This game will be played with 52 cards. The players today are #{@turn.player1.name} and #{@turn.player2.name}. Type 'GO' to start the game!"
+
+    play_game = gets.chomp
+    if play_game.downcase == 'go'
+      self.run_game
+    else
+      p 'Some other time maybe'
+    end
+  end
+
+  def run_game
+    # While both player's cards are more than 0 and turn count is less than 1,000,000
     turn_count = 0
-    while @turn.player1.deck.cards.length >= 0 && @turn.player1.deck.cards.length >= 0 && turn_count < 1000000 do
+    while @turn.player1.deck.cards.length > 0 && @turn.player2.deck.cards.length > 0 && turn_count < 1000000 do
       turn_count += 1
 
       if @turn.type == :basic
-        winner = turn.winner
+        winner = @turn.winner
         @turn.pile_cards
         @turn.award_spoils(winner)
 
         p "Turn #{turn_count}: #{winner.name} won 2 cards"
+        break if @turn.player1.has_lost?  || @turn.player2.has_lost?
       elsif @turn.type == :war
-        winner = turn.winner
+        winner = @turn.winner
         @turn.pile_cards
         @turn.award_spoils(winner)
 
-        p "Turn #{turn_count}: #{winner.name} won 6 cards"
+        p "Turn #{turn_count}: WAR - #{winner.name} won 6 cards"
+        break if @turn.player1.has_lost?  || @turn.player2.has_lost?
       else
-        winner = turn.winner
         @turn.pile_cards
-        @turn.award_spoils(winner)
 
         p "Turn #{turn_count}: *mutually assured destruction* 6 cards removed from play"
-        break 
       end
 
-      break if @turn.player1.deck.cards.empty?  || @turn.player2.deck.cards.empty?
-      # elsif turn_count == 1000000
-      # p ' ---- DRAW ----'
+      break if @turn.player1.has_lost?  || @turn.player2.has_lost?
     end
     end_game
   end
@@ -43,9 +51,9 @@ end
 
 def end_game
   if @turn.player1.has_lost? == true
-    p "*~*~*~* #{@turn.player2.name} has won the game! *~*~*~*"
+    p "*~*~*~* #{@turn.player2.name} has won the game! *~*~*~* ---- #{@turn.player2.name} won #{@turn.player2_win_count} hands and #{@turn.player1.name} won #{@turn.player1_win_count} hands"
   elsif @turn.player2.has_lost? == true
-    p "*~*~*~* #{@turn.player1.name} has won the game! *~*~*~*"
+    p "*~*~*~* #{@turn.player1.name} has won the game! *~*~*~* ---- #{@turn.player1.name} won #{@turn.player1_win_count} hands and #{@turn.player2.name} won #{@turn.player2_win_count} hands"
   else
     p '---- DRAW ----'
   end
--- lib/turn.rb
+++ lib/turn.rb
@@ -1,11 +1,13 @@
 
 class Turn
-  attr_reader :player1, :player2, :spoils_of_war
+  attr_reader :player1, :player2, :spoils_of_war, :player1_win_count, :player2_win_count
 
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
     @spoils_of_war = []
+    @player1_win_count = 0
+    @player2_win_count = 0
   end
 
   def type
@@ -19,8 +21,10 @@ class Turn
   end
 
   def winner
+    # Case statement
     case type
     when :basic
+       # Ternary operator
       first_card(@player1) > first_card(@player2) ? @player1 : @player2
     when :war
       third_card(@player1) > third_card(@player2) ? @player1 : @player2
@@ -30,32 +34,32 @@ class Turn
   end
 
   def pile_cards
-    if type == :basic
-      @spoils_of_war << player1.deck.cards.shift
-      @spoils_of_war << player2.deck.cards.shift
-    elsif type == :war
-      @spoils_of_war.concat(player1.deck.cards.shift(3))
-      @spoils_of_war.concat(player2.deck.cards.shift(3))
-    elsif type == :mutually_assured_destruction
+    case type
+    when :basic
+      @spoils_of_war << @player1.deck.remove_card
+      @spoils_of_war << @player2.deck.remove_card
+    when :war
+      @spoils_of_war.concat(@player1.deck.cards.shift(3))
+      @spoils_of_war.concat(@player2.deck.cards.shift(3))
+    when :mutually_assured_destruction
       @player1.deck.cards.shift(3)
       @player2.deck.cards.shift(3)
     end
   end
 
   def award_spoils(winner)
-    # require "pry"; binding.pry
     if winner == @player1
       @player1.deck.cards.concat(@spoils_of_war)
-      @spoils_of_war = []
+      @player1_win_count += 1
     elsif winner == @player2
       @player2.deck.cards.concat(@spoils_of_war)
-      @spoils_of_war = []
+      @player2_win_count += 1
     end
+    @spoils_of_war = []
   end
 
   # Helper Methods
   def first_card(player)
-    # require "pry"; binding.pry
     player.deck.rank_of_card_at(0)
   end
 
