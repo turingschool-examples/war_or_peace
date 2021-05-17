fdda971f5e1a4b8fdf4c3d382dcfb525ed4714ab
danembb <dbrophy720@gmail.com>
2021-05-17 09:42:23 -0600

    Create winner method for war turn

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -41,11 +41,28 @@ RSpec.describe do
       expect(turn.player1).to eq(player1)
       expect(turn.player2).to eq(player2)
       expect(turn.spoils_of_war).to eq([])
-      expect(turn.type).to eq(:basic)
     end
   end
 
   describe '#methods' do
+    it '#can have a basic turn' do
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:heart, '9', 9)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, 'Queen', 12)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card3, card4, card6, card7])
+      player1 = Player.new("Megan", deck1)
+      player2 = Player.new("Aurora", deck2)
+      turn = Turn.new(player1, player2)
+
+      expect(turn.type).to eq(:basic)
+    end
+
     it '#can determine a winner for a turn' do
       card1 = Card.new(:heart, 'Jack', 11)
       card2 = Card.new(:heart, '10', 10)
@@ -106,5 +123,79 @@ RSpec.describe do
       expect(player1.deck).to eq(winner.deck)
       expect(player2.deck).to eq(deck2)
     end
+
+    it '#can have a war turn' do
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:heart, '9', 9)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, 'Queen', 12)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card4, card3, card6, card7])
+      player1 = Player.new("Megan", deck1)
+      player2 = Player.new("Aurora", deck2)
+      turn = Turn.new(player1, player2)
+
+      expect(turn.type).to eq(:war)
+    end
+
+    it '#can determine a winner for a turn' do
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:heart, '9', 9)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, 'Queen', 12)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card4, card3, card6, card7])
+      player1 = Player.new('Megan', deck1)
+      player2 = Player.new('Aurora', deck2)
+      turn = Turn.new(player1, player2)
+      winner = turn.winner
+
+      expect(turn.winner).to eq(player2)
+    end
+
+    it '#can pile cards into spoils of war' do
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:heart, '9', 9)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, 'Queen', 12)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card4, card3, card6, card7])
+      player1 = Player.new('Megan', deck1)
+      player2 = Player.new('Aurora', deck2)
+      turn = Turn.new(player1, player2)
+      winner = turn.winner
+      turn.pile_cards
+
+      expect(turn.spoils_of_war).to eq([card1, card2, card3, card4, card5, card6])
+    end
+    # it '#can have a mutually assured destruction turn' do
+    #   card1 = Card.new(:heart, 'Jack', 11)
+    #   card2 = Card.new(:heart, '10', 10)
+    #   card3 = Card.new(:heart, '9', 9)
+    #   card4 = Card.new(:diamond, 'Jack', 11)
+    #   card5 = Card.new(:heart, '8', 8)
+    #   card6 = Card.new(:diamond, 'Queen', 12)
+    #   card7 = Card.new(:heart, '3', 3)
+    #   card8 = Card.new(:diamond, '2', 2)
+    #   deck1 = Deck.new([card1, card2, card5, card8])
+    #   deck2 = Deck.new([card3, card4, card6, card7])
+    #   player1 = Player.new("Megan", deck1)
+    #   player2 = Player.new("Aurora", deck2)
+    #   turn = Turn.new(player1, player2)
+    #
+    #   expect(turn.type).to eq(:mutually_assured_destruction)
+    # end
   end
 end
