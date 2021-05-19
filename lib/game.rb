a50fb9759a6617a92cccb5f8d1aa1c6526fcfb5a
Matt Kragen <mattdkragen@gmail.com>
2021-05-18 21:57:05 -0700

    Remove silly printing mistake

--- lib/game.rb
+++ lib/game.rb
@@ -7,8 +7,6 @@ class Game
 
     def welcome
         p "Welcome to War! (or Peace) This game will be played with 52 cards."
-        p "Welcome to War! (or Peace) This game will be played with 52 cards."
-        p "Welcome to War! (or Peace) This game will be played with 52 cards."
         p "The players today are Megan and Aurora."
         p "Type 'GO' to start the game!"
         p '------------------------------------------------------------------'
