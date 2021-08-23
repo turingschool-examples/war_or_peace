eae832ce76f98eb0c95d1603cd0dcd5ae68a0e96
Justina Hortizuela <justinahortizuela@gmail.com>
2021-08-23 14:55:14 -0700

    Initial commit

---
+++ lib/card.rb
@@ -0,0 +1,20 @@
+class Deck
+  attr_reader :cards
+  def initialize([])
+    @cards = cards
+  end
+
+  def rank_of_card_at()
+  end
+
+  def high_ranking_cards()
+  end
+
+  def percent_high_ranking
+  end
+
+  def remove_card
+  end
+
+  def add_card
+  end
