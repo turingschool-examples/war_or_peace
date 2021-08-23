b8f11c5ceaab738c75e133e184f64b64ce2439fd
Sam Devine <samuel.will.devine@gmail.com>
2021-08-23 15:42:52 -0600

    Add empty methods to Deck class

---
+++ lib/deck.rb
@@ -0,0 +1,24 @@
+class Deck
+
+  attr_reader :cards
+
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def rank_of_card_at(index) #returns the rank of a card in the @cards array
+  end
+
+  def high_ranking_cards #return an array of cards in the deckwith rank 11 or above
+  end
+
+  def percent_high_ranking #return the percentage of cards that are high ranking
+  end
+
+  def remove_card #removes the top (beginning) card from the deck
+  end
+
+  def add_card #adds one card to the bottom (end) of the deck
+  end
+
+end
