d73d42fa7066a1224cf7cf81f24045e3428f97dd
Chaz Simons <chaz.simons@gmail.com>
2021-08-23 16:31:59 -0700

    Add deck class to card.rb

--- lib/card.rb
+++ lib/card.rb
@@ -19,3 +19,33 @@ class Card
     @rank
   end
 end
+
+class Deck
+  attr_reader
+
+  def initialize(cards)
+    @deck = [cards]
+  end
+
+  def rank_of_card_at(cut)
+    cards.index_at(cut)
+  end
+
+  def high_ranking_cards
+    face_cards = @cards.select {|rank| rank >= 11}
+  end
+
+  def percent_high_ranking
+    percentage = (face_cards / @cards.length) * 100
+    return percentage
+  end
+
+  def remove_card
+    @cards.pop(0)
+  end
+
+  def add_card
+    @cards.unshift(-1)
+  end
+
+end
