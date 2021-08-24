878ead7162cad289162f2b65e75e4f4a857d2989
Andrew Smith <c13andrew.smith@gmail.com>
2021-08-23 18:58:05 -0700

    Add player_spec

---
+++ spec/player_spec.rb
@@ -0,0 +1,57 @@
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require 'rspec'
+require 'pry'
+
+RSpec.describe Deck do
+  it 'exists' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    player = Player.new('Clarisa', deck)
+    expect(player).to be_instance_of(Player)
+  end
+
+  it 'has a name' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    player = Player.new('Clarisa', deck)
+    expect(player.name).to eq('Clarisa')
+  end
+
+  it 'has a deck' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    player = Player.new('Clarisa', deck)
+    expect(player.deck).to eq(deck)
+
+  end
+
+  it 'reports if Player lost' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    player = Player.new('Clarisa', deck)
+    expect(player.has_lost?).to be false
+
+    player.deck.remove_card
+    expect(player.has_lost?).to be false
+
+    player.deck.remove_card
+    player.deck.remove_card
+    expect(player.has_lost?).to be true
+
+  end
+
+end
