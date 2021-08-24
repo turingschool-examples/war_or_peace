fd5b38368837c70f2597d931b8e2558205576fc0
Alex McConnell <alexmmcconnell@gmail.com>
2021-08-24 16:12:18 -0600

    Add deck.rb testing file

---
+++ spec/deck_spec.rb
@@ -0,0 +1,52 @@
+require 'rspec'
+require './lib/card'
+require './lib/deck'
+
+describe Deck do
+  describe '#initialize' do
+    xit 'creates an array equal to the cards variable' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      expect(deck.cards).to eq(cards)
+    end
+    xit 'creates an array of cards' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      expect(deck.cards).to be_a(Array)
+    end
+  end
+  describe '#rank_of_card_at' do
+    xit 'gives us the rank of the chosen card' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      expect(deck.rank_of_card_at(0)).to eq(12)
+    end
+    xit 'gives us the rank of the chosen card' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      expect(deck.rank_of_card_at(2)).to eq(14)
+    end
+  end
+  describe '#high_ranking_cards' do
+    it 'gives us an array of cards that rank 11 or above' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      expect(deck.high_ranking_cards).to eq([card1, card3])
+    end
+  end
+end
