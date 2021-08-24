c343c579649763b5b4a71f0cc6f5b914f15177a3
Didi Dodson <mdodson55@gmail.com>
2021-08-23 19:41:09 -0600

    Add cards code

--- lib/card.rb
+++ lib/card.rb
@@ -1,2 +1,19 @@
 class Card
-end  
+  def initialize(suit, value, rank)
+    @suit = suit
+    @value = value
+    @rank = rank
+  end
+
+  def suit
+    @suit
+  end
+
+  def value
+    @value
+  end
+
+  def rank
+    @rank
+  end
+end
