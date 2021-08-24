6d79549c976cfae77470447d15ed66572dbe4932
Karan Mehta <karanm645@gmail.com>
2021-08-23 22:56:01 -0500

    "Build dec_spec tests"

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -7,13 +7,12 @@ RSpec.describe Deck do
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
     cards = [card1, card2, card3]
-
     deck = Deck.new(cards)
+
     expect(deck).to be_an_instance_of(Deck)
-    expect(deck.cards).to eq([card1, card2, card3])
   end
 
-  it "has card rankings" do
+  it "has cards, ranks them, calculates %, and removes cards" do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
@@ -24,5 +23,24 @@ RSpec.describe Deck do
 
     expect(deck.rank_of_card_at(0)).to eq(12)
     expect(deck.rank_of_card_at(2)).to eq(14)
+    expect(deck.high_ranking_cards).to eq([card1, card3])
+    expect(deck.percent_high_ranking).to eq(66.67)
+    expect(deck.remove_card).to eq(card1)
+  end
+
+  it "adds a new card" do
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    card4 = Card.new(:club, '5', 5)
+    cards = [card2, card3]
+
+    deck = Deck.new(cards)
+    expect(deck.cards).to eq([card2, card3])
+
+    deck.add_card(card4)
+
+    expect(deck.cards).to eq([card2, card3, card4])
+    expect(deck.high_ranking_cards).to eq([card3])
+    expect(deck.percent_high_ranking).to eq(33.33)
   end
 end
