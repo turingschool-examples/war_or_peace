0599b9842d8660c59cbe92569ab4930ab92a40f9
Alex McConnell <alexmmcconnell@gmail.com>
2021-08-24 17:03:48 -0600

    Add test for percent high ranking method

--- lib/deck.rb
+++ lib/deck.rb
@@ -9,13 +9,25 @@ class Deck
     cards[position.to_i].rank
   end
   def high_ranking_cards
-    high_rank_cards = []
+    high_card_array = []
     cards.each do |card|
       if card.rank >= 11
-        high_rank_cards << card
-      else
+        high_card_array << card
       end
     end
+    return high_card_array
+  end
+  def percent_high_ranking # there's probably a cleaner way to do this...
+    high_card_array = []
+    cards.each do |card|
+      if card.rank >= 11
+        high_card_array << card
+      end
+    end
+    number_of_high_rank_cards = high_card_array.count
+    number_of_cards = cards.count
+    percent_high_rank = number_of_high_rank_cards.to_f / number_of_cards.to_f * 100
+    return percent_high_rank.round(2)
   end
 end
 
