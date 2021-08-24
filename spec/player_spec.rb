766cbc9de19176e720fb0ae5d4ed07602bde2bcb
Karan Mehta <karanm645@gmail.com>
2021-08-23 23:35:07 -0500

    "Add player test"

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -13,5 +13,44 @@ RSpec.describe Player do
     expect(player).to be_an_instance_of(Player)
   end
 
-  
+  it "has a name" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+
+    expect(player.name).to eq("Clarisa")
+  end
+
+  it "has a deck" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+
+    expect(player.deck).to eq(deck)
+  end
+
+  it "determines if player has lost" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+
+    expect(player.has_lost?).to eq(false)
+    expect(player.deck.remove_card).to eq(card1)
+
+    expect(player.has_lost?).to eq(false)
+    expect(player.deck.remove_card).to eq(card2)
+
+    expect(player.has_lost?).to eq(false)
+    expect(player.deck.remove_card).to eq(card3)
+
+    expect(player.has_lost?).to eq(true)
+
+    expect(player.deck).to eq(deck)
+  end
 end
