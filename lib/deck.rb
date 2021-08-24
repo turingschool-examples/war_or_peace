3db94b4328ccf054c996da2cff31caecbbbce47e
John Napier <jcee.napier@gmail.com>
2021-08-23 21:51:45 -0600

    Update deck methods

---
+++ lib/deck.rb
@@ -0,0 +1,25 @@
+class Deck
+  attr_reader :cards
+
+  @cards = cards
+
+  def initialize[]
+  end
+
+  def rank_of_card_at()
+    return rank_of_card_at
+  end
+
+  def high_ranking_cards
+  end
+
+  def percent_high_ranking
+  end
+
+  def remove_card
+  end
+
+  def add_card
+  end
+
+  
