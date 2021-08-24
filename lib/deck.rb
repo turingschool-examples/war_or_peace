8db73802989a697ee6eef8488e2ddbfbc4ddf301
Karan Mehta <karanm645@gmail.com>
2021-08-23 22:56:34 -0500

    "Complete card file"

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,9 +6,24 @@ class Deck
   end
 
   def rank_of_card_at(ranking)
-    @cards.each do |card|
-      card.rank
-# require "pry"; binding.pry
+    @cards[ranking].rank
+  end
+
+  def high_ranking_cards
+    cards.select do |card|
+      card.rank >= 11
     end
+  end
+
+  def percent_high_ranking
+    (high_ranking_cards.count.to_f / @cards.count * 100).round(2)
+  end
+
+  def remove_card
+    @cards.shift
+  end
+
+  def add_card(new_card)
+    @cards << new_card
   end
 end
