cb5b65374bf2bcc34fa93c437bc3f80a45d6daec
Gregory Fischer <gwhoisj@gmail.com>
2021-08-24 07:32:00 -0400

    Card working

--- lib/card.rb
+++ lib/card.rb
@@ -1,6 +1,10 @@
 class Card
-  def initialize(suite_parameter, value_parameter, rank_parameter)
-    @suite    = suite_parameter
+  attr_reader :suit,
+              :value,
+              :rank
+
+  def initialize(suit_parameter, value_parameter, rank_parameter)
+    @suit     = suit_parameter
     @value    = value_parameter
     @rank     = rank_parameter
   end
