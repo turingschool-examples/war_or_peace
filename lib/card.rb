5b3e260f2dc6f2d604cf974021e527d00067b009
Gregory Fischer <gwhoisj@gmail.com>
2021-08-24 07:21:56 -0400

    Add instance variables

---
+++ lib/card.rb
@@ -0,0 +1,8 @@
+class Card
+  def initialize(suite_parameter, value_parameter, rank_parameter)
+    @suite    = suite_parameter
+    @value    = value_parameter
+    @rank     = rank_parameter
+  end
+
+end
