c8fde5ba96052387664dd093f7db3b577a4bc0f5
Alex McConnell <alexmmcconnell@gmail.com>
2021-08-24 19:12:01 -0600

    Temporarily disabled all Player class tests

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,7 +5,7 @@ require './lib/player'
 
 describe Player do
   describe '#initialize' do
-    it 'player name is Clarisa' do
+    xit 'player name is Clarisa' do
       card1 = Card.new(:diamond, 'Queen', 12)
       card2 = Card.new(:spade, '3', 3)
       card3 = Card.new(:heart, 'Ace', 14)
@@ -13,7 +13,7 @@ describe Player do
       player = Player.new('Clarisa', deck)
       expect(player.name).to eq('Clarisa')
     end
-    it 'player deck has card1, card2, and card3' do
+    xit 'player deck has card1, card2, and card3' do
       card1 = Card.new(:diamond, 'Queen', 12)
       card2 = Card.new(:spade, '3', 3)
       card3 = Card.new(:heart, 'Ace', 14)
@@ -23,7 +23,7 @@ describe Player do
     end
   end
   describe "#has_lost?" do
-    it 'player has not lost' do
+    xit 'player has not lost' do
       card1 = Card.new(:diamond, 'Queen', 12)
       card2 = Card.new(:spade, '3', 3)
       card3 = Card.new(:heart, 'Ace', 14)
@@ -31,7 +31,7 @@ describe Player do
       player = Player.new('Clarisa', deck)
       expect(player.has_lost?).to eq(false)
     end
-    it 'player still has two cards; has not lost' do
+    xit 'player still has two cards; has not lost' do
       card1 = Card.new(:diamond, 'Queen', 12)
       card2 = Card.new(:spade, '3', 3)
       card3 = Card.new(:heart, 'Ace', 14)
@@ -40,7 +40,7 @@ describe Player do
       player.deck.remove_card
       expect(player.has_lost?).to eq(false)
     end
-    it 'player still has one card; has not lost' do
+    xit 'player still has one card; has not lost' do
       card1 = Card.new(:diamond, 'Queen', 12)
       card2 = Card.new(:spade, '3', 3)
       card3 = Card.new(:heart, 'Ace', 14)
@@ -50,7 +50,7 @@ describe Player do
       player.deck.remove_card
       expect(player.has_lost?).to eq(false)
     end
-    it 'player has no cards; has lost' do
+    xit 'player has no cards; has lost' do
       card1 = Card.new(:diamond, 'Queen', 12)
       card2 = Card.new(:spade, '3', 3)
       card3 = Card.new(:heart, 'Ace', 14)
