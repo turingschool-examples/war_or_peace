a9a79eae47d7fd344a4f93c7af9aafc3d72f652b
Alex McConnell <alexmmcconnell@gmail.com>
2021-08-24 18:33:20 -0600

    Add tests for has_lost? method

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -22,4 +22,44 @@ describe Player do
       expect(player.deck).to eq(deck)
     end
   end
+  describe "#has_lost?" do
+    it 'player has not lost' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      deck = Deck.new([card1, card2, card3])
+      player = Player.new('Clarisa', deck)
+      expect(player.has_lost?).to eq(false)
+    end
+    it 'player still has two cards; has not lost' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      deck = Deck.new([card1, card2, card3])
+      player = Player.new('Clarisa', deck)
+      player.deck.remove_card
+      expect(player.has_lost?).to eq(false)
+    end
+    it 'player still has one card; has not lost' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      deck = Deck.new([card1, card2, card3])
+      player = Player.new('Clarisa', deck)
+      player.deck.remove_card
+      player.deck.remove_card
+      expect(player.has_lost?).to eq(false)
+    end
+    it 'player has no cards; has lost' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      deck = Deck.new([card1, card2, card3])
+      player = Player.new('Clarisa', deck)
+      player.deck.remove_card
+      player.deck.remove_card
+      player.deck.remove_card
+      expect(player.has_lost?).to eq(true)
+    end
+  end
 end
