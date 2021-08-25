2f6e6efa04ed52e4d4113fea37bda8d4d40c5dde
Philip Medlin <phmedlin@gmail.com>
2021-08-24 22:08:11 -0600

    Create deck_spec.rb

---
+++ spec/deck_spec.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require './lib/card'
+
+RSpec.describe Deck do
+#  it "exists" do
+    #card = Card.new(:diamond, 'Queen', 12)
+
+  #  expect(card).to be_an_instance_of(Card)
+#  end
+
+#  it "has readable attributes" do
+#    card = Card.new(:diamond, 'Queen', 12)
+
+#    expect(card.suit).to eq(:diamond)
+#    expect(card.value).to eq('Queen')
+#    expect(card.rank).to eq(12)
+#  end
+end
+#describe Student do
+#  describe '#initialize' do
+#    it 'is an instance of student' do
+#      student = Student.new('Penelope')
+#      expect(student).to be_a Student
+#    end
