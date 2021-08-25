3b3ab565b68eb4cc6927e1bf569e0124d12fc7f6
Alex McConnell <alexmmcconnell@gmail.com>
2021-08-24 19:22:34 -0600

    Add method for type of turn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,5 +7,15 @@ class Turn
     @player1 = player1
     @player2 = player2
     @spoils_of_war = []
+    @turn = ''
+  end
+  def type
+    if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
+      turn = :basic
+    elsif player1.deck.rank_of_card_at(2) != player2.deck.rank_of_card_at(2)
+      turn = :war
+    else
+      turn = :mutually_assured_destruction
+    end
   end
 end
