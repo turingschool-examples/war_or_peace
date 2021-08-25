b08be8dd946a8cece23394af7e4eb57d58313aa8
Micha Bernhard <mib70@pitt.edu>
2021-08-24 19:59:18 -0600

    Add high_ranking_cards test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -26,4 +26,18 @@ describe Deck do
       expect(deck.rank_of_card_at(0)).to eq(12)
     end
   end
+  describe '#high_ranking_cards' do
+    it 'starts as an empty array' do
+      high_rank_cards = []
+      expect(high_rank_cards).to be_empty
+    end
+    it 'returns an array with high ranking cards' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      expect(deck.high_ranking_cards).to be_an Array
+    end
+  end
 end
