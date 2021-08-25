25fc0567b6460c3acd54195face7af5baee2cdeb
Micha Bernhard <mib70@pitt.edu>
2021-08-24 19:59:52 -0600

    Add high_ranking_cards method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,24 +1,26 @@
 require './lib/card.rb'
 
 class Deck
-  attr_reader :cards
+  attr_reader :cards, :high_rank_cards
 
   def initialize(cards)
     @cards = cards
-    #@high_rank_cards = []
+    @high_rank_cards = []
   end
 
   def rank_of_card_at(index)
     return @cards[index].rank
   end
-=begin
+
   def high_ranking_cards
-    if @rank >= 11
-      @high_rank_cards.append(card)
+    @cards.each do |card|
+      if card.rank >= 11
+        @high_rank_cards << (card)
+      end
     end
     return @high_rank_cards
   end
-
+=begin
   def percent_high_ranking
     percent = (@high_rank_cards / @cards.length) * 100
     return percent
