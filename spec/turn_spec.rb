88413cdc26c6ff90339b809df3d0e974c66f3ef8
Sam Devine <samuel.will.devine@gmail.com>
2021-08-24 18:16:51 -0600

    Build test functionality for basic turns

---
+++ spec/turn_spec.rb
@@ -0,0 +1,72 @@
+require 'rspec'
+require './lib/card.rb'
+require './lib/deck.rb'
+require './lib/player.rb'
+require './lib/turn.rb'
+
+decscribe Turn do
+
+  before :each do
+    @card1 = Card.new(:heart, 'Jack', 11)
+    @card2 = Card.new(:heart, '10', 10)
+    @card3 = Card.new(:heart, '9', 9)
+    @card4 = Card.new(:diamond, 'Jack', 11)
+    @card5 = Card.new(:heart, '8', 8)
+    @card6 = Card.new(:diamond, 'Queen', 12)
+    @card7 = Card.new(:heart, '3', 3)
+    @card8 = Card.new(:diamond, '2', 2)
+
+    @deck1 = Deck.new([card1, card2, card5, card8])
+    @deck2 = Deck.new([card3, card4, card6, card7])
+
+    @player1 = Player.new('Megan', deck1)
+    @player2 = Player.new('Aurora', deck2)
+
+    turn = Turn.new(player1, player2)
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@turn).to be_an_instance_of(Turn)
+    end
+
+    it 'has readable attributes' do
+      expect(@turn.player1).to eq('Megan')
+      expect(@turn.player2).to eq('Aurora')
+      expect(@turn.spoils_of_war).to eq([])
+    end
+  end
+
+
+  describe '#basic turn' do
+
+    it 'is a basic turn' do
+      expect(@turn.type).to eq(:basic)
+    end
+
+    it 'has a winner' do
+      expect(@turn.winner).to eq('Megan')
+    end
+
+    it "moves both 'played' cards to the spoils_of_war" do
+      expect(@turn.spoils_of_war).to eq([])
+
+      @turn.pile_cards
+      expect(@turn.spoils_of_war).to include(@card1)
+      expect(@turn.spoils_of_War).to include(@card3)
+      expect(@turn.player1.deck).to eq([@card2, @card5, @card8])
+      expect(@turn.player2.deck).to eq([@card4, @card6, @card7])
+    end
+
+    it "awards the spoils to the winner" do
+      winner = @turn.winner
+      @turn.pile_cards
+      @turn.award_spoils(winner)
+
+      expect(winner.deck).to include(@card1)
+      expect(winner.deck).to include(@card3)
+      expect(@player1.deck.size).to eq(4)
+      expect(@player2.deck.size).to eq(2)
+    end
+  end
+end
