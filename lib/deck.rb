341bc5f9bf1aab7f2351e7f40fb022cb89c9f121
Cole Henson <colehenson95@gmail.com>
2021-08-25 17:27:03 -0600

    Add testable deck methods

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,15 +2,50 @@ class Deck
   attr_reader :cards
 
   def initialize(cards)
-    @cards = cards
+    @cards = Card
   end
+
+  def rank_of_card_at
+    cards.each do |card|
+      p card.rank
+    end
+  end
+
+  def high_ranking_cards
+    cards.each do |card|
+      if card.rank >= 11
+        p card
+      end
+    end
+
+def percent_high_ranking
+
+high_count = 0
+
+  cards.each do |card|
+    if card.rank >= 11
+      high_count += 1
+    end
+
+  p 100 * (high_count.to_f/(cards.length))
+end
+
+def remove_card
+  card.shift
+end
+
+def add_card
+  cards.push(card4)
+end
+
 end
 
 
 card1 = Card.new(:diamond, 'Queen', 12)
 card2=  Card.new(:spade, '3', 3)
 card3 = Card.new(:heart, 'Ace', 14)
+card4 = Card.new(:club, '8', 8)
 
-cards = ['card1', 'card2', 'card3' ]
+cards = ['card1', 'card2', 'card3']
 
 deck = Deck.new(cards)
