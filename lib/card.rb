c0cb91062b280c4ea85e8c77dc79d9a5ec1928f4
Stephanie Helm <stephaniemhelm@gmail.com>
2021-08-24 22:16:10 -0600

    add diamond cards

--- lib/card.rb
+++ lib/card.rb
@@ -24,5 +24,16 @@ card = Card.new(:heart, 'Queen', 12)
 card = Card.new(:heart, 'King', 13)
 card = Card.new(:heart, 'Ace', 14)
 
-
-require "pry" ; binding.pry
+card = Card.new(:diamond, '2', 2)
+card = Card.new(:diamond, '3', 3)
+card = Card.new(:diamond, '4', 4)
+card = Card.new(:diamond, '5', 5)
+card = Card.new(:diamond, '6', 6)
+card = Card.new(:diamond, '7', 7)
+card = Card.new(:diamond, '8', 8)
+card = Card.new(:diamond, '9', 9)
+card = Card.new(:diamond, '10', 10)
+card = Card.new(:diamond, 'Jack', 11)
+card = Card.new(:diamond, 'Queen', 12)
+card = Card.new(:diamond, 'King', 13)
+card = Card.new(:diamond, 'Ace', 14)
