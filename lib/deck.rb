8ea36703d1a49fb7deade2fe067135b466e25cb0
Philip Medlin <phmedlin@gmail.com>
2021-08-26 13:54:43 -0600

    Completed methods in deck.rb

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,6 +1,5 @@
 class Deck
   attr_reader :cards
-              :high_cards
 
   def initialize(cards)
     @cards = cards
@@ -11,27 +10,24 @@ class Deck
   end
 
   def high_ranking_cards
-    high_cards = []
-    cards.each do |card|
-      if card.rank >= 11
-        high_cards << card
-      end
+    cards.find_all do |card|
+      card.rank >= 11
     end
-    high_cards
   end
 
   def percent_high_ranking
-    (high_ranking_cards.count / cards.count) * 100
-    #this method will return the percentage of cards that are high ranking
+    high_cards = high_ranking_cards
+    #((high_cards.count) / (cards.count)) * 100
+    x = high_cards.count
+    y = cards.count
+    x.to_f / y.to_f * 100.0
   end
 
   def remove_card
     cards.delete_at(0)
-    #this method will remove the top card from the deck
   end
 
   def add_card(card)
     cards.append(card)
-    #this method will add one card to the bottom (end) of the deck
   end
 end
