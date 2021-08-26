d91dfe529d42a4d57bdd9c7a65d771365fd74ded
Matthew Kimball <mekimba6@gmail.com>
2021-08-26 12:59:12 -0600

    minor code cleanup

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,16 +6,16 @@ class Deck
   end
 
   def rank_of_card_at(pos)
-    @cards[pos].rank
+    cards[pos].rank
   end
 
   def percent_high_ranking
-    (high_ranking_cards.count / @cards.count.to_f * 100).round(2)
+    (high_ranking_cards.count / cards.count.to_f * 100).round(2)
   end
 
   def high_ranking_cards
     high_rank = []
-    @cards.each do |card|
+    cards.each do |card|
       if card.rank >= 11
         high_rank << card
       end
@@ -24,10 +24,10 @@ class Deck
   end
 
   def remove_card
-    @cards.shift
+    cards.shift
   end
 
   def add_card(card)
-    @cards << card
+    cards << card
   end
 end
