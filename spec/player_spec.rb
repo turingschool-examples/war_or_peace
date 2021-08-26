1bd3a1d555e1628d8301ea12b40e3192a3ca3903
Jacob Yarborough <jayarborough95@gmail.com>
2021-08-26 17:04:51 -0600

    Add initialize instance method test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,3 +2,19 @@ require 'rspec'
 require './lib/deck'
 require './lib/card'
 require './lib/player'
+
+describe Player do
+
+  describe "#initialize" do
+    it "is an instance of Player" do
+      card1 = Card.new(:spade, '3', 3)
+      card2 = Card.new(:diamonds, 'king', 13)
+      cards = [card1, card2]
+      deck1 = Deck.new(cards)
+      player1 = Player.new("Jacob", deck1)
+
+      expect(player1).to be_an_instance_of(Player)
+    end
+  end
+
+end
