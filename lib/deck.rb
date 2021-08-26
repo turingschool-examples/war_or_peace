9a12fbefc41b91f36671b31de7551ec4d6fbc10e
Jacob Yarborough <jayarborough95@gmail.com>
2021-08-25 19:41:04 -0600

    Add high_ranking_cards instance method

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,4 +6,14 @@ class Deck
   def rank_of_card_at(index_num)
     return @cards[index_num][2]
   end
+
+  def high_ranking_cards
+    new_array = []
+    @cards.each do |card|
+      if card[2] >= 11
+        new_array.push(card)
+      end
+    end
+    p new_array
+  end
 end
