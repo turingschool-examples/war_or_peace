35c74a1c662649c80d3c23b7fc745954d12bcf17
Micha Bernhard <mib70@pitt.edu>
2021-08-25 18:10:58 -0600

    Created tests for the initialize method

---
+++ spec/player_spec.rb
@@ -0,0 +1,35 @@
+require 'rspec'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+
+describe Player do
+  describe '#initialize' do
+    it 'has a name' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      deck = Deck.new([card1, card2, card3])
+      player = Player.new('Clarisa', deck)
+      expect(player.name).to be_a String
+    end
+    it 'has a deck' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      deck = Deck.new([card1, card2, card3])
+      player = Player.new('Clarisa', deck)
+      expect(player.deck).to be_a Deck
+    end
+  end
+  describe '#has_lost?' do
+    it "will return a Boolean value" do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      deck = Deck.new([card1, card2, card3])
+      player = Player.new('Clarisa', deck)
+      expect(player.has_lost?).to be false
+    end
+  end
+end
