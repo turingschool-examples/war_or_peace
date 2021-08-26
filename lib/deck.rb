e73f4b52237a68cc641b4979611cbe7fc963e7de
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-08-25 18:42:27 -0600

    Create deck class methods

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,4 +1,26 @@
-require './lib/deck'
+require 'rspec'
 
 class Deck
-  
+  attr_reader :cards
+
+  def initialize
+    @cards = []
+  end
+
+  def rank_of_card_at()
+  end
+
+  def high_ranking_cards
+  end
+
+  def percent_high_ranking
+  end
+
+  def remove_card
+  end
+
+  def add_card
+  end
+end
+
+deck = Deck.new
