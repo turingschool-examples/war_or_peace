7e6faf06dab099a78dbe8512277e58c6573a6f4d
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-08-26 13:09:09 -0600

    Add high_ranking_cards method and test

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,32 +4,19 @@ class Deck
     @cards = cards
   end
 
-#Method takes one argument representing index location of a card to be used and will return that rank of cards.
+  #Method will take one argument that represents the index location of a card to be used and will return the rank of that card.
   def rank_of_card_at(index)
     @cards[index].rank
   end
 
 
+  #Method will return an array of cards in the deck that have a rank of 11 or above (faces and aces)
+  def high_ranking_cards
 
-#Method will return an array of cards in the deck that have a rank of 11 or above (faces and aces)
-  # def high_ranking_cards
-  #   #need to reassign ace and face cards to integers
-  #   if Card.rank => 11, 12, 'Ace', 'Jack', 'Queen', 'King'
-  #   return new_array << []
-  # end
+    high_rank = []
 
-#Method will return the percentage of cards that are high ranking
-  # def percent_high_ranking
-  #   (  /total)* 100
-  # end
-
-#Method will return top card from the deck.
-  # def remove_card
-  #
-  # end
-#Method will add one card to the bottom (end) of the deck.
-  # def add_card
-  #
-  # end
-
+    cards.find_all do |card|
+      card.rank >= 11
+    end
+  end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,3 +1,41 @@
+
 require 'rspec'
-require './lib/card'
 require './lib/deck'
+require './lib/card'
+require 'pry'
+
+RSpec.describe Deck do
+  it "exists" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    expect(deck).to be_an_instance_of(Deck)
+  end
+
+  it "can find the rank of card at index" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    expect(deck.rank_of_card_at(0)).to eq(12)
+    expect(deck.rank_of_card_at(2)).to eq(14)
+  end
+
+  it "put high ranking cards in an array" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    expect(deck.high_ranking_cards).to eq([card1,card3])
+  end
+end
