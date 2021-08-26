ccfe7404fbfcf383d6e1acd2123ca13e638dc89f
Cole Henson <colehenson95@gmail.com>
2021-08-26 14:29:41 -0600

    Add readability test

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,7 +2,7 @@ class Deck
   attr_reader :cards
 
   def initialize(cards)
-    @cards = Card
+    @cards = cards
   end
 
   def rank_of_card_at
@@ -12,40 +12,38 @@ class Deck
   end
 
   def high_ranking_cards
+    cards.find_all do |card|
+      card.rank >= 11
+    end
+  end
+
+  def percent_high_ranking
+
+  high_count = 0
+
     cards.each do |card|
       if card.rank >= 11
-        p card
+        high_count += 1
       end
     end
+    p 100 * (high_count.to_f/(cards.length))
+  end
 
-def percent_high_ranking
+  def remove_card
+    card.shift
+  end
 
-high_count = 0
-
-  cards.each do |card|
-    if card.rank >= 11
-      high_count += 1
-    end
-
-  p 100 * (high_count.to_f/(cards.length))
-end
-
-def remove_card
-  card.shift
-end
-
-def add_card
-  cards.push(card4)
-end
-
+  def add_card
+    cards.push(card4)
+  end
 end
 
 
-card1 = Card.new(:diamond, 'Queen', 12)
-card2=  Card.new(:spade, '3', 3)
-card3 = Card.new(:heart, 'Ace', 14)
-card4 = Card.new(:club, '8', 8)
-
-cards = ['card1', 'card2', 'card3']
-
-deck = Deck.new(cards)
+# card1 = Card.new(:diamond, 'Queen', 12)
+# card2=  Card.new(:spade, '3', 3)
+# card3 = Card.new(:heart, 'Ace', 14)
+# card4 = Card.new(:club, '8', 8)
+#
+# cards = [card1, card2, card3]
+#
+# deck = Deck.new(cards)
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -5,8 +5,28 @@ require './lib/card'
 describe Deck do
   describe '#initialize' do
     it 'is an an instance of deck' do
-      deck = Deck.new('deck_1')
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2=  Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      card4 = Card.new(:club, '8', 8)
+
+      cards = ['card1', 'card2', 'card3']
+
+      deck = Deck.new(cards)
+
       expect(deck).to be_a Deck
+    end
+    it "has readable attributes" do
+
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2=  Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      card4 = Card.new(:club, '8', 8)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+
+      expect(cards).to be_an Array
+      expect(cards[0]).to be_a Card
     end
   end
 end
