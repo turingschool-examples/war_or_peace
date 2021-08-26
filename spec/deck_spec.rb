1797bbde39cf440839c4c515d5dda3ca76f46aa6
Justina Hortizuela <justinahortizuela@gmail.com>
2021-08-25 17:55:25 -0700

    add card 4

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -33,8 +33,19 @@ describe Deck do
       cards = [card1, card2, card3]
       deck = Deck.new(cards)
       expect(deck.high_ranking_cards). to be_an Array
+      expect(deck.high_ranking_cards).to eq [card1,card3]
     end
   end
+  describe "#percent_high_ranking" do
+    it 'return the percentage of high ranking cards' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      expect(deck.percentage_high_ranking).to eq 66.67
+  end
+end
 
   describe "#remove_card" do
     it 'can remove a card from top of deck' do
@@ -43,7 +54,22 @@ describe Deck do
       card3 = Card.new(:heart, 'Ace', 14)
       cards = [card1, card2, card3]
       deck = Deck.new(cards)
-      expect(deck.remove_card).to eq 
+      expect(deck.cards).to eq [card1, card2, card3]
+      expect(deck.remove_card).to eq card1
+      expect(deck.cards).to eq [card2, card3]
   end
 end
+  describe "#add_card" do
+    it 'can remove a card from top of deck' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      card4 = Card.new(:club, '5', 5)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      expect(deck.cards). to eq [card1, card2, card3]
+      expect(deck.add_card(card3)).to eq [card1, card2, card3, card4]
+  end
+end
+
 end
