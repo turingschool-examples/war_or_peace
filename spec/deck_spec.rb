3f51ad0571374e385e9c0deb91267cb35294fb86
Jacob Yarborough <jayarborough95@gmail.com>
2021-08-26 14:20:23 -0600

    Update rank_of_cards_at instance method

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -27,9 +27,7 @@ describe Deck do
     context "given 0"
       it "returns the 3rd item in the first item in the array" do
         card1 = Card.new(:spade, '3', 3)
-        card2 = Card.new(:diamond, 'Queen', 12)
-        card3 = Card.new(:club, '5', 5)
-        cards = [card1, card2, card3]
+        cards = [card1]
         deck = Deck.new(cards)
         expect(deck.rank_of_card_at(0)).to eq(3)
       end
