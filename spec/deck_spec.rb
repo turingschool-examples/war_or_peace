315f3ae5f077591e1d951a05d357c31c662cabb6
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-08-25 18:43:35 -0600

    Create first deck test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -2,9 +2,9 @@ require 'rspec'
 require './lib/deck.rb'
 
 Rspec.describe Deck do
-  it "exists" do
-    deck = Deck.new[]
-
-    expect(deck).to be_empty[] 
+  describe '#initialize' do
+    it "exists" do
+      deck = Deck.new
+    expect(deck).to be_empty
   end
 end
