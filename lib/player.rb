64ad1c5ae0db85282bde5adcc226c506ad7abb96
Cole Henson <colehenson95@gmail.com>
2021-08-26 17:54:06 -0600

    Add player class

---
+++ lib/player.rb
@@ -0,0 +1,18 @@
+class Player
+  attr_reader :name,
+  :deck,
+
+  def initialize(name, deck)
+    @name = @name
+    @deck = deck
+  end
+
+  def has_lost
+    if deck.length == 0
+      return true
+    else
+      return false
+    end
+  end
+
+  
