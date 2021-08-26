59c63e43fce627071d78453b5419dd8b24b9e32d
Alex McConnell <alexmmcconnell@gmail.com>
2021-08-25 18:30:01 -0600

    Finalize winner method

--- lib/turn.rb
+++ lib/turn.rb
@@ -21,20 +21,16 @@ class Turn
   end
 
   def winner
-    if turn = :basic
-      if player1.deck.rank_of_card_at(0) >= player2.deck.rank_of_card_at(0)
-        return player1
-      else
-        return player2
-      end
-    elsif turn = :war
-      if player1.deck.rank_of_card_at(2) >= player2.deck.rank_of_card_at(2)
-        return player1
-      else
-        return player2
-      end
+    if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+      return player1
+    elsif player1.deck.rank_of_card_at(0) < player2.deck.rank_of_card_at(0)
+      return player2
+    elsif player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+      return player1
+    elsif player1.deck.rank_of_card_at(2) < player2.deck.rank_of_card_at(2)
+      return player2
     else
-      winner = "No Winner"
+      return "No Winner"
     end
   end
 end
