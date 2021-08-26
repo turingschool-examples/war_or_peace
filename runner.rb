897ee5947b29c9537d08524c7cd988936b24fded
Micha Bernhard <mib70@pitt.edu>
2021-08-25 18:11:41 -0600

    Make runner file require deck file

--- runner.rb
+++ runner.rb
@@ -1,6 +1,7 @@
 require 'pry'
 require './lib/card.rb'
 require './lib/deck.rb'
+require './lib/player'
 
 card1 = Card.new(:diamond, 'Queen', 12)
 card2 = Card.new(:spade, '3', 3)
@@ -8,5 +9,6 @@ card3 = Card.new(:heart, 'Ace', 14)
 card4 = Card.new(:club, '5', 5)
 cards = [card1, card2, card3]
 deck = Deck.new(cards)
+player = Player.new('Clarisa', deck)
 
 binding.pry
