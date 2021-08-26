034ddbc872f7721164d02aeee2ef37baee7b2aec
Alex McConnell <alexmmcconnell@gmail.com>
2021-08-25 18:29:33 -0600

    Add tests for winner method

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -22,6 +22,7 @@ describe Turn do
       turn = Turn.new(player1, player2)
       expect(turn.player1).to eq(player1)
     end
+
     it 'player2 is Aurora with deck2' do
       card1 = Card.new(:heart, 'Jack', 11)
       card2 = Card.new(:heart, '10', 10)
@@ -38,6 +39,7 @@ describe Turn do
       turn = Turn.new(player1, player2)
       expect(turn.player2).to eq(player2)
     end
+
     it 'spoils_of_war is an empty array' do
       card1 = Card.new(:heart, 'Jack', 11)
       card2 = Card.new(:heart, '10', 10)
@@ -55,6 +57,7 @@ describe Turn do
       expect(turn.spoils_of_war).to eq([])
     end
   end
+
   describe '#type' do
     it 'this is a basic type of turn' do
       card1 = Card.new(:heart, 'Jack', 11)
@@ -72,6 +75,7 @@ describe Turn do
       turn = Turn.new(player1, player2)
       expect(turn.type).to eq(:basic)
     end
+
     it 'this is a turn with a war' do
       card1 = Card.new(:heart, 'Jack', 11)
       card2 = Card.new(:heart, '10', 10)
@@ -88,6 +92,7 @@ describe Turn do
       turn = Turn.new(player1, player2)
       expect(turn.type).to eq(:war)
     end
+
     it 'this is a turn with mutually assured destruction' do
       card1 = Card.new(:heart, 'Jack', 11)
       card2 = Card.new(:heart, '10', 10)
@@ -105,6 +110,57 @@ describe Turn do
       expect(turn.type).to eq(:mutually_assured_destruction)
     end
   end
-end
 
-# NEXT STEPS: I MUST complete my necessary tests for the winner method before continuing work.
+  describe '#winner' do
+    it 'basic turn determines Megan as winner' do
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:heart, '9', 9)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, 'Queen', 12)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card3, card4, card6, card7])
+      player1 = Player.new("Megan", deck1)
+      player2 = Player.new("Aurora", deck2)
+      turn = Turn.new(player1, player2)
+      expect(turn.winner).to eq(player1)
+    end
+
+    it 'war turn determines Aurora as winner' do
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:heart, '9', 9)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, 'Queen', 12)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card4, card3, card6, card7])
+      player1 = Player.new("Megan", deck1)
+      player2 = Player.new("Aurora", deck2)
+      turn = Turn.new(player1, player2)
+      expect(turn.winner).to eq(player2)
+    end
+
+    it 'mutually assured destruction determines no winner' do
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:heart, '9', 9)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, '8', 8)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card4, card3, card6, card7])
+      player1 = Player.new("Megan", deck1)
+      player2 = Player.new("Aurora", deck2)
+      turn = Turn.new(player1, player2)
+      expect(turn.winner).to eq("No Winner")
+    end
+  end
+end
