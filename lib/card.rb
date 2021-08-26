9e5673851447452f257d48203d634ded09eb8ab3
Mallory Vining <mallorygallery12@gmail.com>
2021-08-25 20:21:28 -0600

    Add to card file

--- lib/card.rb
+++ lib/card.rb
@@ -1,2 +1,26 @@
+require './lib/card'
+
+class Card
+  attr_reader :suit,
+              :rank,
+              :value
+  def initialize(:heart, 'Jack', 11)
+    @suit = suit
+    @rank = rank
+    @value = value
+  end
+
+  def rank
+  end
+
+  def suit
+  end
+
+  def value
+  end
+
+end
+card = Card.new(:heart, 'Jack', 11)
+
 class Deck
 end
