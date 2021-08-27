f2c56b5d92b0dbf392bb259157a71ac3ffa8a780
Didi Dodson <mdodson55@gmail.com>
2021-08-27 11:50:55 -0600

    Feat: add has_lost method

--- lib/player.rb
+++ lib/player.rb
@@ -1,26 +1,15 @@
 class Player
-  attr_reader :name, :deck
+  attr_reader :name, :deck, :cards
 
   #initialize
   def initialize(name, deck)
     @name = name
     @deck = deck
+    @cards = cards
   end
 
-  #name method called on player
-
-
-
-  #deck method called on player
-
-
-
   #has_lost? method called on player
-
-
-
-  #deck.remove_card method called on player
-
-
-
+  def has_lost?
+    @deck.cards == []
+  end
 end
