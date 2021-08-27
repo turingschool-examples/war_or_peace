63e60577a4ec452762c0e40cc30c3331568af3f5
Didi Dodson <mdodson55@gmail.com>
2021-08-27 11:51:30 -0600

    Test: add has_lost test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -15,7 +15,18 @@ RSpec.describe Player do
       player = Player.new('Clarisa', deck)
       expect(deck.cards).to eq(cards)
       expect(player).to be_a(Player)
+      expect(player.name).to eq('Clarisa')
     end
-
   end
-end
+
+  describe "#has_lost?"
+    it 'cards count greater than 0' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      player = Player.new('Clarisa', deck)
+      expect(player.has_lost?).to eq(false)
+    end
+  end
