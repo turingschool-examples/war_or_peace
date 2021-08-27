34070cbda8c9971e29caa1ec82e6c8f9602bcf60
Philip Medlin <phmedlin@gmail.com>
2021-08-27 12:46:41 -0600

    docs modify pry_prompts file

--- pry_prompts.rb
+++ pry_prompts.rb
@@ -1,3 +1,5 @@
+#Deck prompts
+
 require './lib/card'
 require './lib/deck'
 card1 = Card.new(:diamond, 'Queen', 12)
@@ -5,3 +7,14 @@ card2 = Card.new(:spade, '3', 3)
 card3 = Card.new(:heart, 'Ace', 14)
 cards = [card1, card2, card3]
 deck = Deck.new(cards)
+
+#Player prompts
+
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+card1 = Card.new(:diamond, 'Queen', 12)
+card2 = Card.new(:spade, '3', 3)
+card3 = Card.new(:heart, 'Ace', 14)
+deck = Deck.new([card1, card2, card3])
+player = Player.new('Clarissa', deck)
