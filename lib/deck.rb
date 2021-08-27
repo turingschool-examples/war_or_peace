71daef4e862bb38b64ddb2fcb92e89bd28f6075e
John Napier <jcee.napier@gmail.com>
2021-08-27 11:54:48 -0600

    Feature: rank_of_card_at method added

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,8 +2,24 @@
 
 class Deck
   attr_reader :cards
+
   def initialize(cards)
-    @cards = []
+    @cards = cards
+    # @rank_of_card_at = rank
+    # @high_ranking_cards = []
+    # @percent_high_ranking = percent
+    # @remove_card = remove_car
+    # @add_card = add_card
   end
 
+
+  def rank_of_card_at(index)
+   cards[index].rank
+  end
 end
+#   def high_ranking_cards
+#     cards.map do |card|
+#       if rank >= 10
+#
+#   end
+# end
