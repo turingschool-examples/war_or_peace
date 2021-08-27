177a7726383b4a1ee860ce8ccb0b7086c6a487f1
John Napier <jcee.napier@gmail.com>
2021-08-27 12:19:40 -0600

    Function: added high_ranking_cards function

--- lib/deck.rb
+++ lib/deck.rb
@@ -5,21 +5,17 @@ class Deck
 
   def initialize(cards)
     @cards = cards
-    # @rank_of_card_at = rank
-    # @high_ranking_cards = []
-    # @percent_high_ranking = percent
-    # @remove_card = remove_car
-    # @add_card = add_card
   end
 
 
   def rank_of_card_at(index)
    cards[index].rank
   end
+
+  def high_ranking_cards
+    @cards = cards
+    cards.find_all do |card|
+      card.rank >= 11
+    end
+  end
 end
-#   def high_ranking_cards
-#     cards.map do |card|
-#       if rank >= 10
-#
-#   end
-# end
