d794ee2266e41202f87825902cd7ed979ca81fd9
Andrew Smith <c13andrew.smith@gmail.com>
2021-08-26 18:22:34 -0700

    Add turn.rb

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Turn
   attr_reader :player1, :player2, :spoils_of_war
 
@@ -8,52 +10,58 @@ class Turn
   end
 
   def type
-    if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
-      return :basic
-    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+
+    if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
+      return :mutually_assured_destruction
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
       return :war
     else
-      return :mutually_assured_destruction
+      return :basic  #lazy forcing
     end
+
   end
 
   def winner
-    if self.type == :basic
-      if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+    case self.type
+    when  :basic
+      if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
         return player1
       else
         return player2
       end
-    elsif self.type == :war
-      if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
-        return player1
+    when :war
+      if @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
+        return @player1
       else
-        return player2
+        return @player2
       end
-    else
-      return "No winner"
+    when :mutually_assured_destruction
+      return "No Winner"
     end
   end
 
+
+
   def pile_cards
     if self.type == :basic
-        @spoils_of_war.push(player1.deck.remove_card)
-        @spoils_of_war.push(player2.deck.remove_card)
+        @spoils_of_war.push(@player1.deck.remove_card)
+        @spoils_of_war.push(@player2.deck.remove_card)
     elsif self.type == :war
-        @spoils_of_war.push(player1.deck.remove_card)
-        @spoils_of_war.push(player1.deck.remove_card)
-        @spoils_of_war.push(player1.deck.remove_card)
-        @spoils_of_war.push(player2.deck.remove_card)
-        @spoils_of_war.push(player2.deck.remove_card)
-        @spoils_of_war.push(player2.deck.remove_card)
+        @spoils_of_war.push(@player1.deck.remove_card)
+        @spoils_of_war.push(@player1.deck.remove_card)
+        @spoils_of_war.push(@player1.deck.remove_card)
+        @spoils_of_war.push(@player2.deck.remove_card)
+        @spoils_of_war.push(@player2.deck.remove_card)
+        @spoils_of_war.push(@player2.deck.remove_card)
     else
-      player1.deck.remove_card
-      player2.deck.remove_card
-      player1.deck.remove_card
-      player2.deck.remove_card
-      player1.deck.remove_card
-      player2.deck.remove_card
+        @player1.deck.remove_card
+        @player2.deck.remove_card
+        @player1.deck.remove_card
+        @player2.deck.remove_card
+        @player1.deck.remove_card
+        @player2.deck.remove_card
     end
+
   end
 
   # add spoils to the bottom of winner's deck
