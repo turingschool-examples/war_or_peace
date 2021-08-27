80bdebaf6525ff367ba11b9c55180f0895ab0bb6
Philip Medlin <phmedlin@gmail.com>
2021-08-27 12:46:04 -0600

    Feat add methods and acommpanying test to player

---
+++ lib/player.rb
@@ -0,0 +1,21 @@
+#require './lib/card'
+#require './lib/deck'
+
+class Player
+  attr_reader :deck,
+              :name
+
+  def initialize(name, deck)
+    @name = name
+    @deck = deck
+  end
+
+  def name
+    @name
+  end
+
+  def has_lost?
+    if
+
+  #def
+end
