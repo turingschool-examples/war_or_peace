c5a98c478562cd2e0c101192e95d446f3a712bbc
Sam Devine <samuel.will.devine@gmail.com>
2021-08-27 12:42:34 -0600

    Refactor Turn class to reference methods instead of instance vars

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,50 +8,48 @@ class Turn
     @player1        = player1
     @player2        = player2
     @spoils_of_war  = []
-    @type           = :default
-    @winner
   end
 
   def type
     if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
-      @type = :basic
+      :basic
     elsif @player1.deck.rank_of_card_at(2) != @player2.deck.rank_of_card_at(2)
-      @type = :war
+      :war
     else
-      @type = :mutually_assured_destruction
+      :mutually_assured_destruction
     end
   end
 
   def winner
-    if @type == :basic
+    if type == :basic
       if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
-        @winner = @player1
+        @player1
       else
-        @winner = @player2
+        @player2
       end
-    elsif @type == :war
+    elsif type == :war
       if @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
-        @winner = @player1
+        @player1
       else
-        @winner = @player2
+        @player2
       end
-    elsif @type == :mutually_assured_destruction
-      @winner = 'No Winner'
+    elsif type == :mutually_assured_destruction
+      'No Winner'
     end
   end
 
   def pile_cards
-    if @type == :basic
+    if type == :basic
       @spoils_of_war << @player1.deck.remove_card
       @spoils_of_war << @player2.deck.remove_card
-    elsif @type == :war
+    elsif type == :war
       3.times do
         @spoils_of_war << @player1.deck.remove_card
       end
       3.times do
         @spoils_of_war << @player2.deck.remove_card
       end
-    elsif @type == :mutually_assured_destruction
+    elsif type == :mutually_assured_destruction
       3.times do
         @player1.deck.remove_card
       end
@@ -62,9 +60,9 @@ class Turn
   end
 
   def award_spoils(winner)
-    if @type != :mutually_assured_destruction
-      while @spoils_of_war.length > 0
-        winner.deck.cards << @spoils_of_war.delete_at(0)
+    if type != :mutually_assured_destruction
+      @spoils_of_war.size.times do
+        winner.deck.cards << @spoils_of_war.shift
       end
     end
   end
