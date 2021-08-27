0c3ac20a45b6e83de697dc2fb3417b9e02c0fdc6
Jacob Yarborough <jayarborough95@gmail.com>
2021-08-26 22:05:27 -0600

    Add attr_reader test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -24,4 +24,22 @@ describe Turn do
     end
   end
 
+  describe "attr_reader" do
+    it "has readable attributes" do
+      card1 = Card.new(:spade, '3', 3)
+      card2 = Card.new(:diamonds, 'king', 13)
+      card3 = Card.new(:clubs, '8', 8)
+      card4 = Card.new(:spades, '5', 5)
+      cards1 = [card1, card2]
+      cards2 = [card3, card4]
+      deck1 = Deck.new(cards1)
+      deck2 = Deck.new(cards2)
+      player1 = Player.new("Jacob", deck1)
+      player2 = Player.new("Bacoj", deck2)
+      turn1 = Turn.new(player1, player2)
+
+      expect(turn1.player1).to eq(player1)
+    end
+  end
+
 end
