5ee2e89337845fa14fd890f53e8af0fedeb5a6d2
Andrew Patterson <andrew.patterson747@gmail.com>
2021-08-26 21:16:02 -0500

    Complete Player class

---
+++ lib/player.rb
@@ -0,0 +1,16 @@
+class Player
+  attr_reader :name, :deck, :game_lost
+
+  def initialize(name, deck)
+    @name = name
+    @deck = deck
+    @game_lost = false
+  end
+
+  def has_lost?
+    if deck.cards.count == 0
+      @game_lost = true
+    else @game_lost
+    end
+  end
+end
