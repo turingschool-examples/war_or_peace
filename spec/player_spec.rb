c58c8eba9eed7541312bdd72a1b07ff010ebcb2b
Cole Henson <colehenson95@gmail.com>
2021-08-26 19:26:00 -0600

    Add initial player tests

---
+++ spec/player_spec.rb
@@ -0,0 +1,22 @@
+require 'rspec'
+require './lib/player'
+require './lib/deck'
+require './lib/card'
+
+describe Player do
+
+  describe '#initialize' do
+    it 'is an instance of player' do
+      player = Player.new('Clarisa','deck_1')
+
+      expect(player).to be_a Player
+    end
+    it 'has readable attributes' do
+
+      player = Player.new('Clarisa','deck_1')
+
+      expect(name).to be_a String
+      expect(deck).to be_a Deck
+    end
+  end
+end
