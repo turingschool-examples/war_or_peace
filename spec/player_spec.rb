7a0133778485e397003526d18b0ff820ab56ac90
Sam Devine <samuel.will.devine@gmail.com>
2021-08-27 12:08:34 -0600

    Refactor player spec

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,32 +4,30 @@ require './lib/deck'
 require './lib/player'
 
 RSpec.describe Player do
-  it "has a name" do
-    player = Player.new('Clarisa', [])
+  before :each do
+    @card1 = Card.new(:diamond, 'Q', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'A', 14)
+    @deck = Deck.new([@card1, @card2, @card3])
+    @player = Player.new('Clarisa', @deck)
+  end
 
-    expect(player.name).to eq('Clarisa')
+  it "has a name" do
+    expect(@player.name).to eq('Clarisa')
   end
 
   it "has a deck" do
-    deck = Deck.new([])
-    player = Player.new('Clarisa', deck)
-
-    expect(player.deck).to be_an_instance_of(Deck)
+    expect(@player.deck).to eq(@deck)
   end
 
   it "knows if it has lost" do
+    expect(@player.has_lost?).to eq(false)
 
-    card1 = Card.new(:diamond, 'Q', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'A', 14)
-    deck = Deck.new([card1, card2, card3])
-    player = Player.new('Clarisa', deck)
+    @player.deck.remove_card
+    @player.deck.remove_card
+    expect(@player.has_lost?).to eq(false)
 
-    expect(player.has_lost?).to eq(false)
-    player.deck.remove_card
-    player.deck.remove_card
-    expect(player.has_lost?).to eq(false)
-    player.deck.remove_card
-    expect(player.has_lost?).to eq(true)
+    @player.deck.remove_card
+    expect(@player.has_lost?).to eq(true)
   end
 end
