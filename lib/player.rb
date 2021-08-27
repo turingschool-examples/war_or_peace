952120dd02812a6e6986ba4bae4becece4a1f30e
Justina Hortizuela <justinahortizuela@gmail.com>
2021-08-26 18:19:43 -0700

    add has lost

---
+++ lib/player.rb
@@ -0,0 +1,20 @@
+class Player
+  attr_reader :name,
+              :deck
+  def initialize(name, deck)
+    @name = name
+    @deck = deck
+  end
+
+  def deck
+    @deck
+  end
+
+  def has_lost?
+    if deck.cards.count == 0
+      true
+    else deck.cards.count > 0
+      false
+    end
+  end
+end
