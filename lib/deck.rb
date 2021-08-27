6ad6f12024f71c7e9d58edec230066876bb075a5
Eldridge Turambi <eturambi95@gmail.com>
2021-08-27 10:01:05 -0600

    test: add test for rank_of_cards and high_ranking_cards

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,8 +6,21 @@ class Deck
   end
 
   def rank_of_cards_at(index_location)
-    @rank = rank
-    return rank(index_location)
+    @cards.at(index_location).rank
   end
 
-  
+  def high_ranking_cards
+    high_rank_cards = []
+    cards.each do |card|
+      if card.rank >= 11
+        high_rank_cards << card
+      end
+    end
+    high_rank_cards#require "pry"; binding.pry
+  end
+
+  def percent_high_ranking
+
+    percent_high_rank = ((high_ranking_cards.count.to_f / cards.count.to_f) * 100).round(2)
+  end
+end
