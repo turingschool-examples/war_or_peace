062c453682d45fffa94ea2cc1a3bfcac66aef1cc
Alex McConnell <alexmmcconnell@gmail.com>
2021-08-28 08:26:59 -0600

    Add dev notes for card class

--- lib/card.rb
+++ lib/card.rb
@@ -1,9 +1,9 @@
 class Card
-  attr_reader :suit,
+  attr_reader :suit, # Allows for reading the suit, value, and rank of a card
               :value,
               :rank
 
-  def initialize(suit, value, rank)
+  def initialize(suit, value, rank) # Creates a card with a suit, value, and rank
     @suit = suit
     @value = value
     @rank = rank
