9ae292c166d275a0738f429626d6100fa984860d
Stephanie Helm <stephaniemhelm@gmail.com>
2021-08-28 09:15:41 -0600

    feat: add winner method

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,8 @@
 class Turn
   attr_reader   :player1,
                 :player2,
-                :spoils_of_war
+                :spoils_of_war,
+                :type
 
   def initialize(player1, player2)
     @player1        = player1
@@ -12,14 +13,34 @@ class Turn
   def type
     if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) &&
       player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
-      return :mutually_assured_destruction
+      @type = :mutually_assured_destruction
     elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
-      return :war
-    else player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
-    return :basic
+      @type = :war
+    else
+      @type = :basic
     end
+    return @type
   end
 
   def winner
+    if @type == :basic
+      if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+        return player1
+      else
+        return player2
+      end
+    end
 
+    if @type == :war
+      if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+        return player1
+      else
+        return player2
+      end
+    end
+
+    if @type == :mutually_assured_destruction
+      return "No winner"
+    end
+  end
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -29,22 +29,121 @@ RSpec.describe Player do
     expect(player2.deck).to eq(deck2)
   end
 
-  xit 'type of turn' do
+  it 'type of turn :basic' do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:heart, '2', 2)
     card3 = Card.new(:spade, 'Ace', 14)
-    card4 = Card.new(:club, '5', 5)
-    cards = [card1, card2, card3, card4]
-    deck1 = Deck.new([card1, card2, card3, card4])
+    cards = [card1, card2, card3]
+    deck1 = Deck.new([card1, card2, card3])
     player1 = Player.new('Megan', deck1)
 
-    card5 = Card.new(:spade, '6', 6)
-    card6 = Card.new(:club, 'Jack', 11)
-    card7 = Card.new(:diamond, '10', 10)
-    card8 = Card.new(:heart, '8', 8)
-    cards = [card5, card6, card7, card8]
-    deck2 = Deck.new([card5, card6, card7, card8])
+    card4 = Card.new(:spade, '6', 6)
+    card5 = Card.new(:club, 'Jack', 11)
+    card6 = Card.new(:diamond, '10', 10)
+    cards = [card4, card5, card6]
+    deck2 = Deck.new([card4, card5, card6])
     player2 = Player.new('Aurora', deck2)
-    expect(turn.type).to eq(:basic)
+
+    turn1 = Turn.new(player1, player2)
+    expect(turn1.type).to eq(:basic)
   end
+
+  it 'type of turn :war' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:heart, '2', 2)
+    card3 = Card.new(:spade, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck1 = Deck.new([card1, card2, card3])
+    player1 = Player.new('Megan', deck1)
+
+    card4 = Card.new(:spade, 'Queen', 12)
+    card5 = Card.new(:club, 'Jack', 11)
+    card6 = Card.new(:diamond, '10', 10)
+    cards = [card4, card5, card6]
+    deck2 = Deck.new([card4, card5, card6])
+    player2 = Player.new('Aurora', deck2)
+
+    turn2 = Turn.new(player1, player2)
+    expect(turn2.type).to eq(:war)
+  end
+
+  it 'type of turn :mutually assured destruction' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:heart, '2', 2)
+    card3 = Card.new(:spade, '10', 10)
+    cards = [card1, card2, card3]
+    deck1 = Deck.new([card1, card2, card3])
+    player1 = Player.new('Megan', deck1)
+
+    card4 = Card.new(:spade, 'Queen', 12)
+    card5 = Card.new(:club, 'Jack', 11)
+    card6 = Card.new(:diamond, '10', 10)
+    cards = [card4, card5, card6]
+    deck2 = Deck.new([card4, card5, card6])
+    player2 = Player.new('Aurora', deck2)
+
+    turn3 = Turn.new(player1, player2)
+    expect(turn3.type).to eq(:mutually_assured_destruction)
+  end
+
+  it 'winner :basic' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:heart, '2', 2)
+    card3 = Card.new(:spade, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck1 = Deck.new([card1, card2, card3])
+    player1 = Player.new('Megan', deck1)
+
+    card4 = Card.new(:spade, '6', 6)
+    card5 = Card.new(:club, 'Jack', 11)
+    card6 = Card.new(:diamond, '10', 10)
+    cards = [card4, card5, card6]
+    deck2 = Deck.new([card4, card5, card6])
+    player2 = Player.new('Aurora', deck2)
+
+    turn1 = Turn.new(player1, player2)
+    expect(turn1.type).to eq(:basic)
+    expect(turn1.winner).to eq(player1)
+  end
+
+  it 'winner :war' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:heart, '2', 2)
+    card3 = Card.new(:spade, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck1 = Deck.new([card1, card2, card3])
+    player1 = Player.new('Megan', deck1)
+
+    card4 = Card.new(:spade, 'Queen', 12)
+    card5 = Card.new(:club, 'Jack', 11)
+    card6 = Card.new(:diamond, '10', 10)
+    cards = [card4, card5, card6]
+    deck2 = Deck.new([card4, card5, card6])
+    player2 = Player.new('Aurora', deck2)
+
+    turn2 = Turn.new(player1, player2)
+    expect(turn2.type).to eq(:war)
+    expect(turn2.winner).to eq(player1)
+  end
+
+  it 'winner :mutually assured destruction' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:heart, '2', 2)
+    card3 = Card.new(:spade, '10', 10)
+    cards = [card1, card2, card3]
+    deck1 = Deck.new([card1, card2, card3])
+    player1 = Player.new('Megan', deck1)
+
+    card4 = Card.new(:spade, 'Queen', 12)
+    card5 = Card.new(:club, 'Jack', 11)
+    card6 = Card.new(:diamond, '10', 10)
+    cards = [card4, card5, card6]
+    deck2 = Deck.new([card4, card5, card6])
+    player2 = Player.new('Aurora', deck2)
+
+    turn3 = Turn.new(player1, player2)
+    expect(turn3.type).to eq(:mutually_assured_destruction)
+    expect(turn3.winner).to eq("No winner")
+  end
+
 end
