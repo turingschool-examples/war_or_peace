573d15bacad0fe16d8976bb35b47e296a4062971
Alex McConnell <alexmmcconnell@gmail.com>
2021-08-28 08:32:36 -0600

    Refactor - Add dev notes for war_or_peace_runner

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -6,27 +6,27 @@ require './lib/card_generator'
 
 filename = "cards.txt"
 
-cards = CardGenerator.new(filename).cards
+cards = CardGenerator.new(filename).cards # generates a deck from cards.txt
 
-deck1 = Deck.new(cards.shuffle![0..25])
+deck1 = Deck.new(cards.shuffle![0..25]) # setup of players, decks, and turn 1
 deck2 = Deck.new(cards.shuffle![26..51])
 player1 = Player.new("Megan", deck1)
 player2 = Player.new("Aurora", deck2)
 turn = Turn.new(player1, player2)
 
-lets_go = 'no'
+lets_go = 'no' # setup for user input to begin the game
 
-while lets_go != 'GO'
+while lets_go != 'GO' # can accept any input; only progresses once it has recieved 'GO' as input
 p "Welcome to War! (or Peace) This game will be played with 52 cards."
 p "The players today are Megan and Aurora."
 p "Type 'GO' to start the game!"
 p "------------------------------------------------------------------"
 
-lets_go = gets.chomp.to_s
+lets_go = gets.chomp.to_s # converts odd inputs to strings; also a failsafe for certain inputs
   if lets_go == 'GO'
   else
     puts "Invalid input."
   end
 end
 
-turn.start
+turn.start # starts the game with the start method defined in ''./lib/turn'
