680f1d35b09bd0ade6c25f1fea6976be5b3d3a03
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-08-27 21:42:37 -0600

    Refactor deck test file

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,10 +1,26 @@
 require 'rspec'
 require './lib/deck.rb'
 
-Rspec.describe Deck do
+RSpec.describe Deck do
   describe '#initialize' do
-    it "exists" do
-      deck = Deck.new
-    expect(deck).to be_empty
+    it "is an instance of Deck" do
+      deck1 = Deck.new()
+
+      expect(deck1).to be_a Deck
+    end
+
+    it 'is an array' do
+      deck1 = Deck.new()
+
+      expect(@cards).to be(nil)
+    end
   end
+
+  # describe '#high_ranking_cards' do
+  #   it "returns and array of cards" do
+  #     high_ranking_cards = @cards.map do |card|
+  #
+  #     expect(high_ranking_cards).to be_a array
+  #   end
+  # end
 end
