351a64c6d47b519b5863e1936025dcc5578008c3
Karan Mehta <karanm645@gmail.com>
2021-08-28 16:31:44 -0500

    "Space out"

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -23,8 +23,11 @@ RSpec.describe Deck do
 
     expect(deck.rank_of_card_at(0)).to eq(12)
     expect(deck.rank_of_card_at(2)).to eq(14)
+
     expect(deck.high_ranking_cards).to eq([card1, card3])
+    
     expect(deck.percent_high_ranking).to eq(66.67)
+
     expect(deck.remove_card).to eq(card1)
   end
 
