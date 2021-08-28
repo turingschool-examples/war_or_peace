e55b9e2f42936cf43c3023315e8234ee703f1b55
Alex McConnell <alexmmcconnell@gmail.com>
2021-08-27 18:55:57 -0600

    Test - Add tests for CardGenerator class

---
+++ spec/card_generator_spec.rb
@@ -0,0 +1,39 @@
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+require './lib/card_generator'
+# require './cards.txt'
+
+describe CardGenerator do
+  describe '#initialize' do
+    it 'generates a filename equal to cards.txt' do
+      filename = "cards.txt"
+      cards = CardGenerator.new(filename)
+
+      expect(cards.filename).to eq("cards.txt")
+    end
+
+    it 'generates an empty array' do
+      filename = "cards.txt"
+      cards = CardGenerator.new(filename)
+
+      expect(cards.card_array).to eq([])
+    end
+  end
+  describe '#cards' do
+    it 'generates an array of cards' do
+      filename = "cards.txt"
+      cards = CardGenerator.new(filename).cards
+
+
+      expect(cards).to be_a(Array)
+    end
+    it 'generates a 52-object array' do
+      filename = "cards.txt"
+      cards = CardGenerator.new(filename).cards
+
+      expect(cards.length).to eq(52)
+    end
+  end
+end
