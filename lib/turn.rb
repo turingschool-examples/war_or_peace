78e1f527347b2bdf364ebdf822635b776fa0e514
Nathan Brown <nathanbrown@Nathans-MacBook-Pro.local>
2021-08-27 23:50:37 -0600

    Fix bug with spoils going to loser of turn, create runner file

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,7 +3,8 @@ require './lib/player'
 class Turn
   attr_accessor :player1,
                 :player2,
-                :spoils_of_war
+                :spoils_of_war,
+                :winner
 
   def initialize(player1, player2)
     @player1 = player1
@@ -12,15 +13,15 @@ class Turn
   end
 
   def type
-    player1_rank0 = player1.deck.rank_of_card_at(0)
-    player2_rank0 = player2.deck.rank_of_card_at(0)
+    @player1_rank0 = @player1.deck.rank_of_card_at(0)
+    @player2_rank0 = @player2.deck.rank_of_card_at(0)
 
-    player1_rank2 = player1.deck.rank_of_card_at(2)
-    player2_rank2 = player2.deck.rank_of_card_at(2)
+    @player1_rank2 = @player1.deck.rank_of_card_at(2)
+    @player2_rank2 = @player2.deck.rank_of_card_at(2)
 
-    if player1_rank0 == player2_rank0 && player1_rank2 == player2_rank2
+    if @player1_rank0 == @player2_rank0 && @player1_rank2 == @player2_rank2
       return :mutually_assured_destruction
-    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
       return :war
     else
       return :basic
@@ -31,44 +32,50 @@ class Turn
     if type == :mutually_assured_destruction
       return 'No Winner'
     elsif type == :war
-      if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
-        return player1
+      if @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
+        return @player1
       else
-        return player2
+        return @player2
       end
     elsif type == :basic
-      if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
-        return player1
-      else
-        return player2
+      if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
+        @player1
+      elsif @player1.deck.rank_of_card_at(0) < @player2.deck.rank_of_card_at(0)
+        @player2
       end
     end
   end
+
   def pile_cards
+    @winner = winner
   if type == :mutually_assured_destruction
-    player1.deck.cards.slice!(0..2)
-    player2.deck.cards.slice!(0..2)
+    @player1.deck.cards.slice!(0..2)
+    @player2.deck.cards.slice!(0..2)
   elsif type == :war
-      @spoils_of_war += player1.deck.cards.values_at(0..2)
-      player1.deck.cards.slice!(0..2)
+      @spoils_of_war += @player1.deck.cards.values_at(0..2)
+      @player1.deck.cards.slice!(0..2)
 
-      @spoils_of_war += player2.deck.cards.values_at(0..2)
-      player2.deck.cards.slice!(0..2)
+      @spoils_of_war += @player2.deck.cards.values_at(0..2)
+      @player2.deck.cards.slice!(0..2)
   elsif type == :basic
-      @spoils_of_war << player1.deck.cards[0]
-      player1.deck.cards.slice!(0)
+      @spoils_of_war << @player1.deck.cards[0]
+      @player1.deck.cards.slice!(0)
 
 
-      @spoils_of_war << player2.deck.cards[0]
-      player2.deck.cards.slice!(0)
+      @spoils_of_war << @player2.deck.cards[0]
+      @player2.deck.cards.slice!(0)
     end
   end
 
   def award_spoils
-    if winner == player1
-      player1.deck.cards << @spoils_of_war
+    if @winner == @player1
+      @spoils_of_war.each do |spoil|
+        @player1.deck.cards << spoil
+      end
     else
-      player2.deck.cards << @spoils_of_war
+      @spoils_of_war.each do |spoil|
+        @player2.deck.cards << spoil
+      end
   end
 end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -31,7 +31,18 @@ describe Deck do
     expect(deck.cards[0].suit).to eq :diamond
   end
 end
-  # describe '#remove_card' do
-  #   it 'removes card as expected'
-  # end
+  describe "#rank_of_card_at" do
+    it "returns the rank of card at given index" do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      card4 = Card.new(:club, '5', 5)
+      cards = [card1, card2, card3, card4]
+      deck = Deck.new(cards)
+
+      expect(deck.rank_of_card_at(0)).to eq(12)
+      expect(deck.rank_of_card_at(2)).to eq(14)
+    end
+
+  end
 end
