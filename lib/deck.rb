9c6ea73c9cec6d2b44d3261ad1f74e35b749f0f4
Mallory Vining <mallorygallery12@gmail.com>
2021-08-27 19:41:35 -0600

    feat:add methods to deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -9,4 +9,14 @@ class Deck
   def rank_of_card_at(index)
     cards[index].rank
   end
+
+  def high_ranking_cards
+    high_ranking_cards = []
+    cards.each do |card|
+      if rank > 11 card == high_ranking_cards
+      else 
+    end
+    # each card is looked at to see if it's high ranking.
+    # If it is it goes into high_ranking_cards array
+  end
 end
