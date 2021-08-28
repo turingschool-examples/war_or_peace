a285f621f44cfc11d77057c3880152db6f25f65c
Alex McConnell <alexmmcconnell@gmail.com>
2021-08-28 08:26:18 -0600

    Refactor - Add dev notes for deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,15 +1,15 @@
 class Deck
-  attr_reader :cards
+  attr_reader :cards # allows reading of individual cards
 
-  def initialize(card_array)
+  def initialize(card_array) # creates an array of cards
     @cards = card_array
   end
 
-  def rank_of_card_at(position)
+  def rank_of_card_at(position) # determines the rank of a given card
     @cards[position.to_i].rank
   end
 
-  def high_ranking_cards
+  def high_ranking_cards # returns all high ranking cards (Jacks or higher)
     high_card_array = []
     cards.each do |card|
       if card.rank >= 11
@@ -19,18 +19,18 @@ class Deck
     high_card_array
   end
 
-  def percent_high_ranking
+  def percent_high_ranking # returns what percentage of cards are high ranking
     number_of_high_rank_cards = high_ranking_cards.count
     number_of_cards = cards.count
     percent_high_rank = number_of_high_rank_cards.to_f / number_of_cards.to_f * 100
     percent_high_rank.round(2)
   end
 
-  def remove_card
+  def remove_card # removes the top (first) card from a deck
     cards.shift
   end
 
-  def add_card(card)
+  def add_card(card) # adds a card to the bottom (end) of a deck
     @cards << card
   end
 end
