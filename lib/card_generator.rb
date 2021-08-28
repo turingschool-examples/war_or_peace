ec15b0c533d0256e8d92018a7cd0334864732636
Nathan Brown <nathanbrown@Nathans-MacBook-Pro.local>
2021-08-28 03:26:31 -0600

    Feat: Add card_generator file and rspec

---
+++ lib/card_generator.rb
@@ -0,0 +1,33 @@
+require './lib/turn'
+
+class CardGenerator
+  attr_reader :filename,
+              :cards
+
+  def initialize(filename)
+    @filename = filename
+    @cards = []
+  end
+
+  def deck_object_creator
+    full_deck = []
+    File.foreach(@filename) do |line|
+    full_deck << line.strip.split("\n")
+    end
+    full_deck_2 = []
+    full_deck.each do |card|
+      full_deck_2 << card[0].split(", ")
+    end
+    full_deck_3 = []
+    full_deck_2.each do |array|
+      suit = array[1].downcase.to_sym
+      value = array[0]
+      rank = array[2].to_i
+      full_deck_3 << Card.new(suit, value, rank)
+    end
+    full_deck_3.each do |card|
+      @cards << card
+    end
+  end
+
+end
