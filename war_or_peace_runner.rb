9f754a917d89b903b2c64fbcec6eb93aceefe69b
Nathan Brown <nathanbrown@Nathans-MacBook-Pro.local>
2021-08-28 00:33:51 -0600

    Add Game class to runner file

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -1,68 +1,87 @@
 require './lib/turn'
+class Game
+  attr_reader :full_deck,
+              :deck1,
+              :deck2
+
+  def initialize
+    @full_deck = [
+    Card.new(:heart, 'Ace', 14),
+    Card.new(:heart, 'King', 13),
+    Card.new(:heart, 'Queen', 12),
+    Card.new(:heart, 'Jack', 11),
+    Card.new(:heart, '10', 10),
+    Card.new(:heart, '9', 9),
+    Card.new(:heart, '8', 8),
+    Card.new(:heart, '7', 7),
+    Card.new(:heart, '6', 6),
+    Card.new(:heart, '5', 5),
+    Card.new(:heart, '4', 4),
+    Card.new(:heart, '3', 3),
+    Card.new(:heart, '2', 2),
+    Card.new(:diamond, 'Ace', 14),
+    Card.new(:diamond, 'King', 13),
+    Card.new(:diamond, 'Queen', 12),
+    Card.new(:diamond, 'Jack', 11),
+    Card.new(:diamond, '10', 10),
+    Card.new(:diamond, '9', 9),
+    Card.new(:diamond, '8', 8),
+    Card.new(:diamond, '7', 7),
+    Card.new(:diamond, '6', 6),
+    Card.new(:diamond, '5', 5),
+    Card.new(:diamond, '4', 4),
+    Card.new(:diamond, '3', 3),
+    Card.new(:diamond, '2', 2),
+    Card.new(:spade, 'Ace', 14),
+    Card.new(:spade, 'King', 13),
+    Card.new(:spade, 'Queen', 12),
+    Card.new(:spade, 'Jack', 11),
+    Card.new(:spade, '10', 10),
+    Card.new(:spade, '9', 9),
+    Card.new(:spade, '8', 8),
+    Card.new(:spade, '7', 7),
+    Card.new(:spade, '6', 6),
+    Card.new(:spade, '5', 5),
+    Card.new(:spade, '4', 4),
+    Card.new(:spade, '3', 3),
+    Card.new(:spade, '2', 2),
+    Card.new(:club, 'Ace', 14),
+    Card.new(:club, 'King', 13),
+    Card.new(:club, 'Queen', 12),
+    Card.new(:club, 'Jack', 11),
+    Card.new(:club, '10', 10),
+    Card.new(:club, '9', 9),
+    Card.new(:club, '8', 8),
+    Card.new(:club, '7', 7),
+    Card.new(:club, '6', 6),
+    Card.new(:club, '5', 5),
+    Card.new(:club, '4', 4),
+    Card.new(:club, '3', 3),
+    Card.new(:club, '2', 2)
+  ]
+end
+
+  def split_into_two_shuffled_decks
+    shuffled_deck = game.full_deck.sample(52)
+    @deck1 = shuffled_deck[0..25]
+    @deck2 = shuffled_deck[26..51]
+  end
+
+  def start
+
+  end
+end
+
+game = Game.new
 
 
-#create 52 card deck
 
-full_deck = [
-  Card.new(:heart, 'Ace', 14),
-  Card.new(:heart, 'King', 13),
-  Card.new(:heart, 'Queen', 12),
-  Card.new(:heart, 'Jack', 11),
-  Card.new(:heart, '10', 10),
-  Card.new(:heart, '9', 9),
-  Card.new(:heart, '8', 8),
-  Card.new(:heart, '7', 7),
-  Card.new(:heart, '6', 6),
-  Card.new(:heart, '5', 5),
-  Card.new(:heart, '4', 4),
-  Card.new(:heart, '3', 3),
-  Card.new(:heart, '2', 2),
-  Card.new(:diamond, 'Ace', 14),
-  Card.new(:diamond, 'King', 13),
-  Card.new(:diamond, 'Queen', 12),
-  Card.new(:diamond, 'Jack', 11),
-  Card.new(:diamond, '10', 10),
-  Card.new(:diamond, '9', 9),
-  Card.new(:diamond, '8', 8),
-  Card.new(:diamond, '7', 7),
-  Card.new(:diamond, '6', 6),
-  Card.new(:diamond, '5', 5),
-  Card.new(:diamond, '4', 4),
-  Card.new(:diamond, '3', 3),
-  Card.new(:diamond, '2', 2),
-  Card.new(:spade, 'Ace', 14),
-  Card.new(:spade, 'King', 13),
-  Card.new(:spade, 'Queen', 12),
-  Card.new(:spade, 'Jack', 11),
-  Card.new(:spade, '10', 10),
-  Card.new(:spade, '9', 9),
-  Card.new(:spade, '8', 8),
-  Card.new(:spade, '7', 7),
-  Card.new(:spade, '6', 6),
-  Card.new(:spade, '5', 5),
-  Card.new(:spade, '4', 4),
-  Card.new(:spade, '3', 3),
-  Card.new(:spade, '2', 2),
-  Card.new(:club, 'Ace', 14),
-  Card.new(:club, 'King', 13),
-  Card.new(:club, 'Queen', 12),
-  Card.new(:club, 'Jack', 11),
-  Card.new(:club, '10', 10),
-  Card.new(:club, '9', 9),
-  Card.new(:club, '8', 8),
-  Card.new(:club, '7', 7),
-  Card.new(:club, '6', 6),
-  Card.new(:club, '5', 5),
-  Card.new(:club, '4', 4),
-  Card.new(:club, '3', 3),
-  Card.new(:club, '2', 2)
-]
-
-#separate cards into two decks
-
-shuffled_deck = full_deck.sample(52)
-deck1 = shuffled_deck[0..25]
-deck2 = shuffled_deck[26..51]
-puts deck1.count
-puts deck2.count
 #create two players and assign them decks
+
+player1 = Player.new('Megan', deck1)
+player2 = Player.new('Aurora', deck2)
+
+p "Welcome to War! (or Peace) This game will be played with 52 cards.
+The players today are Megan and Aurora.
+Type 'GO' to start the game!
+------------------------------------------------------------------"
