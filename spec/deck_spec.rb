bc4dc8576249ff6a2dc58cc3409a638021ff8cb3
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-08-28 11:25:50 -0600

    Refactored deck_spec file

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,28 +1,76 @@
 require 'rspec'
 require './lib/deck.rb'
+require './lib/card.rb'
 
 RSpec.describe Deck do
-  describe '#initialize' do
-    it "is an instance of Deck" do
-      deck1 = Deck.new()
+# Test whether or not new object belogs to class
+  it "exists" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
 
-      expect(deck1).to be_a Deck
-    end
-
-    it 'is an array' do
-      deck1 = Deck.new()
-
-      expect(@cards).to be_an_instance_of(Array)
-    end
+    expect(deck).to be_a(Deck)
   end
 
-  describe '#high_ranking_cards' do
-    it "returns and array of cards" do
-      high_ranking_cards = @cards.map do |card|
-        @card >= 11
-      end
+  it 'has attributes' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
 
-      expect(high_ranking_cards).to be_a array
-    end
+    expect(deck.cards).to eq(cards)
   end
+
+  # describe "#rank_of_card_at" do
+  #   xit "returns rank of card at index location" do
+  #     rank_of_card(2)
+  #     # Needs to iterate over @cards array, find the :rank of the
+  #     # card, and return said rank.
+  #   end
+  # end
+  #
+  #
+  # describe '#high_ranking_cards' do
+  #   xit "returns an array of cards" do
+  #
+  #     high_ranking_cards = @cards.map do |card|
+  #       @card >= 11
+  #     end
+  #
+  #     expect(high_ranking_cards).to be_an_instance of(Array)
+  #   end
+  # end
+  #
+  # describe '#percent_high_ranking' do
+  #   xit "returns percentage of cards in deck that are >= 11" do
+  #
+  #   end
+  # end
+  #
+  # describe '#remove_card' do
+  #   it "removes a card from the top/front of the cards array" do
+  #     card1 = Card.new('diamond', 'Queen', 12)
+  #     card2 = Card.new('spade', '3', 3)
+  #
+  #     @cards = [card1, card2]
+  #
+  #     @cards.shift
+  #
+  #     expect(@cards).to match_array([card2])
+  #
+  #   end
+  # end
+  #
+  # describe '#add_card' do
+  #   it "add's a card to the end/bottom of the cards array" do
+  #     @cards = []
+  #     card1 = Card.new('diamond', 'Queen', 12)
+  #     @cards.push(card1)
+  #
+  #     expect(@cards).to include(card1)
+  #   end
+  # end
 end
