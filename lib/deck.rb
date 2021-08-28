7fb08527dadadf55fe6a70e41b3c5ac55ebbce3e
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-08-28 14:41:05 -0600

    Removed superfulous code

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,35 +1,16 @@
+require './lib/card.rb'
+
 class Deck
   attr_reader :cards
 
   def initialize(cards)
     @cards = cards
-    require 'pry'; binding.pry
+    #require 'pry'; binding.pry
   end
-  # def rank_of_card_at(index)
-  # end
-  #
-  # def high_ranking_cards
-  # end
-  #
-  #   # high_ranking_cards = []
-  #   #
-  #   # @cards.each do |card|
-  #   #   if @rank >= 11
-  #   #     high_ranking_cards << card
-  #   #   end
-  #   # end
-  #
-  #
-  #
-  #
-  # def percent_high_ranking
-  # end
-  #
-  # def remove_card
-  #   @cards.shift
-  # end
-  #
-  # def add_card
-  #   @cards.push
-  # end
+
+  def add_card(card)
+    @cards << card
+# require 'pry';  binding.pry
+  end
 end
+require 'pry'; binding.pry
