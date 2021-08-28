2393b76ab4987365278109e48eeb3eee4d00e38b
Andrew Smith <c13andrew.smith@gmail.com>
2021-08-27 21:11:42 -0700

    refactor: migrate functionality to game.rb

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -2,6 +2,8 @@ require './lib/card'
 require './lib/deck'
 require './lib/player'
 require './lib/turn'
+require './lib/game'
+require './lib/card_generator'
 require 'rspec'
 require 'pry'
 
@@ -26,59 +28,59 @@ player1 = Player.new("Megan", deck2)
 player2 = Player.new("Aurora", deck1)
 turn = Turn.new(player1, player2)
 
-class Game
+puts "Welcome to War! (or Peace) This game will be played with 52 cards."
+puts "The players today are #{turn.player1.name} and #{turn.player2.name}."
+puts "Type 'GO' to start the game!"
+puts "------------------------------------------------------------------"
 
-  def initialize(turn)
-    @turn = turn
-  end
+prompt = gets.chomp
 
-  def start
-      puts "Welcome to War! (or Peace) This game will be played with 52 cards."
-      puts "The players today are #{@turn.player1.name} and #{@turn.player2.name}."
-      puts "Type 'GO' to start the game!"
-      puts "------------------------------------------------------------------"
-
-      prompt = gets.chomp
-
-      while prompt != "GO"
-        puts "Type 'GO' to start the game!"
-        puts "------------------------------------------------------------------"
-        prompt = gets.chomp
-      end
-
-      count = 1
-      #binding.pry
-      while (!@turn.player1.has_lost?) && (!@turn.player2.has_lost?) && (count <= 1000000)
-          #binding.pry
-          if @turn.type == :basic
-            puts "Turn #{count}: #{@turn.winner.name} won 2 cards"
-            winner = @turn.winner
-            @turn.pile_cards
-            @turn.award_spoils(winner)
-          elsif @turn.type == :war
-            puts "Turn #{count}: WAR - #{@turn.winner.name} won 6 cards"
-            winner = @turn.winner
-            @turn.pile_cards
-            @turn.award_spoils(winner)
-          elsif @turn.type == :mutually_assured_destruction
-            puts "Turn #{count}: *mutually assured destruction* 6 cards removed from play"
-            @turn.pile_cards
-          end
-
-          count += 1
-
-      end
-
-      if @turn.player1.has_lost?
-        puts "*~*~*~* #{@turn.player2.name} has won the game! *~*~*~*"
-      elsif @turn.player2.has_lost?
-        puts "*~*~*~* #{@turn.player1.name} has won the game! *~*~*~*"
-      else
-        puts "---- DRAW ----"
-      end
-
-  end
+while prompt != "GO"
+  puts "Type 'GO' to start the game!"
+  puts "------------------------------------------------------------------"
+  prompt = gets.chomp
 end
 
 game = Game.new(turn)
 game.start
+
+# class Game
+#
+#   def initialize(turn)
+#     @turn = turn
+#   end
+#
+#   def start
+#       count = 1
+#       #binding.pry
+#       while (!@turn.player1.has_lost?) && (!@turn.player2.has_lost?) && (count <= 1000000)
+#           #binding.pry
+#           if @turn.type == :basic
+#             puts "Turn #{count}: #{@turn.winner.name} won 2 cards"
+#             winner = @turn.winner
+#             @turn.pile_cards
+#             @turn.award_spoils(winner)
+#           elsif @turn.type == :war
+#             puts "Turn #{count}: WAR - #{@turn.winner.name} won 6 cards"
+#             winner = @turn.winner
+#             @turn.pile_cards
+#             @turn.award_spoils(winner)
+#           elsif @turn.type == :mutually_assured_destruction
+#             puts "Turn #{count}: *mutually assured destruction* 6 cards removed from play"
+#             @turn.pile_cards
+#           end
+#
+#           count += 1
+#
+#       end
+#
+#       if @turn.player1.has_lost?
+#         puts "*~*~*~* #{@turn.player2.name} has won the game! *~*~*~*"
+#       elsif @turn.player2.has_lost?
+#         puts "*~*~*~* #{@turn.player1.name} has won the game! *~*~*~*"
+#       else
+#         puts "---- DRAW ----"
+#       end
+#
+#   end
+# end
