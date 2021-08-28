a573ca21929e39918f44dbd8472c58eb93cf7e1e
Alex McConnell <alexmmcconnell@gmail.com>
2021-08-27 18:52:45 -0600

    Refactor - Updated type method with sad path

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,12 +10,14 @@ class Turn
   end
 
   def type
-    if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
-      @turn = :basic
-    elsif player1.deck.rank_of_card_at(2) != player2.deck.rank_of_card_at(2)
-      @turn = :war
-    else
+    if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
       @turn = :mutually_assured_destruction
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+      @turn = :war
+    elsif
+      @turn = :basic
+    else
+      puts "Error: unable to determine type"
     end
   end
 
@@ -41,13 +43,10 @@ class Turn
       player1.deck.remove_card
       player2.deck.remove_card
     elsif @turn == :war
-        @spoils_of_war << player1.deck.cards[0]
-        @spoils_of_war << player1.deck.cards[1]
-        @spoils_of_war << player1.deck.cards[2]
-        @spoils_of_war << player2.deck.cards[0]
-        @spoils_of_war << player2.deck.cards[1]
-        @spoils_of_war << player2.deck.cards[2]
+      if player1.deck.cards.count < 3
       3.times do
+        @spoils_of_war << player1.deck.cards[0]
+        @spoils_of_war << player2.deck.cards[0]
         player1.deck.remove_card
         player2.deck.remove_card
       end
@@ -60,38 +59,37 @@ class Turn
     end
   end
 
-  def award_spoils
+  def award_spoils(winner)
+    pile_cards
     if winner == player1
-      pile_cards
       while @spoils_of_war != []
         player1.deck.add_card(@spoils_of_war[0])
         @spoils_of_war.shift
       end
     elsif winner == player2
-      pile_cards
       while @spoils_of_war != []
         player2.deck.add_card(@spoils_of_war[0])
         @spoils_of_war.shift
       end
     else
-      pile_cards
     end
   end
 
   def start
     turn_count = 0
 
-    until player1.has_lost? == true || player2.has_lost? == true || turn_count == 1000000
+    until player1.has_lost? == true || player2.has_lost? == true || turn_count == 10000
       turn_count += 1
       type
       if @turn == :basic
         p "Turn #{turn_count}: #{winner.name} won 2 cards"
       elsif @turn == :war
-        p "Turn #{turn_count}: #{winner.name} won 6 cards"
+        p "Turn #{turn_count}: WAR - #{winner.name} won 6 cards"
       else @turn == :mutually_assured_destruction
         p "Turn #{turn_count}: *mutually assured destruction* 6 cards removed from play"
       end
-      award_spoils
+      award_spoils(winner)
+      turn = Turn.new(player1, player2)
     end
 
     if player2.has_lost? == true
@@ -101,6 +99,7 @@ class Turn
     else
       p "Draw"
     end
+
     game_end = true
     return game_end
   end
