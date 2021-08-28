fa79e368a268cded3cca3338ad68ab852d1a8805
Alex McConnell <alexmmcconnell@gmail.com>
2021-08-28 08:24:45 -0600

    Refactor - Add dev notes for player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,13 +1,13 @@
 class Player
-  attr_reader :name,
+  attr_reader :name, # allows for reading the player name and deck
               :deck
 
-  def initialize(name, deck)
+  def initialize(name, deck) # creates a player with a name and chosen deck
     @name = name
     @deck = deck
   end
 
-  def has_lost?
+  def has_lost? # determines if a player has lost
     deck.cards == []
   end
 end
