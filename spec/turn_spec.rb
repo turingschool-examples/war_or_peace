0264c4c6a4bb39c80bc4dba5d316de67384d40b3
Andrew Patterson <andrew.patterson747@gmail.com>
2021-08-28 18:00:46 -0500

    Complete Iteration 2, add tests for pile_cards and award_spoils

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -193,20 +193,73 @@ RSpec.describe Turn do
 
     turn.pile_cards
 
-    results [card2, card5, card8]
-    expect(turn.player1.deck).to eq(results)
+    expect(player1.deck).to eq(deck1)
+    expect(player2.deck).to eq(deck2)
+
+    results = [card1, card3]
+    expect(turn.spoils_of_war).to eq(results)
+
+    turn.award_spoils(winner)
+
+    expect(player1.deck).to eq(deck1)
+    expect(player2.deck).to eq(deck2)
+  end
+
+  it 'has piles in war' do
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card1, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    winner = turn.winner
+
+    turn.pile_cards
+
+    expect(player1.deck).to eq(deck1)
+    expect(player2.deck).to eq(deck2)
+
+    results = [card1, card1, card2, card4, card5, card6]
+    expect(turn.spoils_of_war).to eq(results)
+
+    turn.award_spoils(winner)
+
+    expect(player1.deck).to eq(deck1)
+    expect(player2.deck).to eq(deck2)
+  end
+
+  it 'has piles in mutually_assured_destruction' do
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card1, card8])
+    deck2 = Deck.new([card1, card4, card4, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    winner = turn.winner
+
+    turn.pile_cards
+
+    expect(player1.deck).to eq(deck1)
+    expect(player2.deck).to eq(deck2)
+
+    results = []
+    expect(turn.spoils_of_war).to eq(results)
+
+    expect(player1.deck).to eq(deck1)
+    expect(player2.deck).to eq(deck2)
   end
 end
-
-
-# turn.pile_cards
-#
-# turn.spoils_of_war
-# #=> [#<Card:0x007fa3edaa0df0 @rank=11, @suit=:heart, @value="Jack">, #<Card:0x007fa3ed98d9b8 @rank=9, @suit=:heart, @value="9">]
-#
-# turn.award_spoils(winner)
-#
-# player1.deck
-# #=> #<Deck:0x007fa3eda472c8 @cards=[#<Card:0x007fa3eda519a8...>, #<Card:0x007fa3edb263d8...>, #<Card:0x007fa3eda89308...>, #<Card:0x007fa3edaa0df0...>, #<Card:0x007fa3ed98d9b8...>]>
-# player2.deck
-# #=> #<Deck:0x007fa3ee11ee48 @cards=[#<Card:0x007fa3ee14ef80...>, #<Card:0x007fa3eda3e1f0...>, #<Card:0x007fa3edad1cc0...>]>
