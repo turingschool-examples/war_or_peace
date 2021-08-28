076c378c057a3ef1db87c39a7c05768f3b131ef5
John Napier <jcee.napier@gmail.com>
2021-08-27 22:38:45 -0600

    Feat: complete iteration 1

--- lib/card.rb
+++ lib/card.rb
@@ -1,14 +1,8 @@
 class Card
   attr_reader :suit, :value, :rank
   def initialize(suit, value, rank)
-  @suit = suit
-  @value = value
-  @rank = rank
+    @suit = suit
+    @value = value
+    @rank = rank
   end
-
 end
-
-
-card = Card.new(:diamond, "Queen", 12)
-card = Card.new(:spade, "3", 3)
-card = Card.new(:heart, "Ace", 14)
