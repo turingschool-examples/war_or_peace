29144fdb91aee44a70f594a73296a8235906c2f8
Jacob Yarborough <jayarborough95@gmail.com>
2021-08-27 20:33:04 -0600

    Refactor initialize method

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,21 +5,20 @@ class Turn
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
+    @c1_0 = @player1.deck.rank_of_card_at(0)
+    @c1_1 = @player1.deck.rank_of_card_at(1)
+    @c1_2 = @player1.deck.rank_of_card_at(2)
+    @c2_0 = @player2.deck.rank_of_card_at(0)
+    @c2_1 = @player2.deck.rank_of_card_at(1)
+    @c2_2 = @player2.deck.rank_of_card_at(2)
   end
 
   def type
-    c1_0 = @player1.deck.rank_of_card_at(0)
-    c1_1 = @player1.deck.rank_of_card_at(1)
-    c1_2 = @player1.deck.rank_of_card_at(2)
-    c2_0 = @player2.deck.rank_of_card_at(0)
-    c2_1 = @player2.deck.rank_of_card_at(1)
-    c2_2 = @player2.deck.rank_of_card_at(1)
-
-    if (c1_0 == c2_0) && (c1_2 == c2_2)
+    if (@c1_0 == @c2_0) && (@c1_2 == @c2_2)
       :mutually_assured_destruction
-    elsif c1_0 == c2_0
+    elsif @c1_0 == @c2_0
       :war
-    else c1_0 != c2_0
+    else @c1_0 != @c2_0
       :basic
     end
   end
