fa9e04ea62a0c967774cb4639684cf35d76cf450
Andrew Patterson <andrew.patterson747@gmail.com>
2021-08-28 00:38:19 -0500

    Complete type and winner method

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,8 +15,35 @@ class Turn
       :war
 
     elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) &&
+
           player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
       :mutually_assured_destruction
+    end
+  end
+
+  def winner
+    if type == :basic
+      if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+        winner = player1
+      else winner = player2
+      end
+
+    elsif type == :war
+      if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+        winner = player1
+      else winner = player2
+      end
+
+    elsif type == :mutually_assured_destruction
+      winner = "No Winner"
+    end
+  end
+
+  def pile_cards
+    if type == :basic
+      @player1.deck.each do |cards|
+        cards.drop(0)
+      end
     end
   end
 end
