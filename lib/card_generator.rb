02722a61ed7ab0e51fb0f4690f62901657de369b
Alex McConnell <alexmmcconnell@gmail.com>
2021-08-28 08:28:29 -0600

    Refactor - Add dev notes for card_generator

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,18 +1,18 @@
 class CardGenerator
-  attr_reader :card_array,
+  attr_reader :card_array, # allows for reading the card array and filename
               :filename
 
-  def initialize(filename)
+  def initialize(filename) # creates a cardgenerator with a file name and an empty card array
     @card_array = []
     @filename = filename
   end
 
-  def cards
+  def cards # generates an initial array of cards, where each item in the array is an array with the card data
     File.readlines(@filename, chomp: true).each do |card|
       @card_array << card.split(',')
     end
 
-    @card_array.map do |card|
+    @card_array.map do |card| # converts the card array to individual interactable cards and outputs the resulting array
       card = Card.new(card[0].to_sym, card[1], card[2].to_i)
     end
   end
