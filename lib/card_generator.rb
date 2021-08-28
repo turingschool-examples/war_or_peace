355f4ddaa3d36bc0032ea5c98db5742bfa46ce86
Andrew Smith <c13andrew.smith@gmail.com>
2021-08-28 11:04:16 -0700

    Add #cards

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -6,7 +6,23 @@ class CardGenerator
   end
 
   def cards
-    file = File.open("users.txt")
+    file = File.open(@filename)
+    # creates an array of Strings by line
+    file_data = file.readlines.map(&:chomp)
+    file.close
+
+    cards = []
+
+    #iterate through each index
+    file_data.each do |line|
+      #split into array of 3 Strings
+      card_data = line.split(', ')
+      # create Card object
+      cards << Card.new(card_data[1].downcase.to_sym, card_data[0], card_data[2].to_i)
+    end
+
+    return cards
+
   end
 
 end
