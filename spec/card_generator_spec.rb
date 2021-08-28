681be669acf1b1cf8970782341f014e8f236dc54
Andrew Smith <c13andrew.smith@gmail.com>
2021-08-27 21:25:15 -0700

    Add test shells

---
+++ spec/card_generator_spec.rb
@@ -0,0 +1,33 @@
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+require './lib/game'
+require './lib/card_generator'
+
+require 'rspec'
+require 'pry'
+
+Rspec.describe CardGenerator do
+
+  it 'exists' do
+
+  end
+
+  it 'takes a filename as an argument' do
+  end
+
+  describe '#cards' do
+
+    it 'reads filename.txt' do
+
+    end
+
+    it 'outputs an array of Card objects' do
+
+    end
+
+
+  end
+
+end
