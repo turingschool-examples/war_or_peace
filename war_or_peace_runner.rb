6ce407d78cfb68aa3a1865f2a2ea7361608ff971
Jacob Yarborough <jayarborough95@gmail.com>
2021-08-28 16:49:08 -0600

    Hardcode 52 playing cards

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -59,6 +59,34 @@ card_50 = Card.new(:spades, '4', 4)
 card_51 = Card.new(:spades, '3', 3)
 card_52 = Card.new(:spades, '2', 2)
 
+start_deck = [card_1, card_2, card_3,
+              card_4, card_5, card_6,
+              card_7, card_8, card_9,
+              card_10, card_11, card_12,
+              card_13, card_14, card_15,
+              card_16, card_17, card_18,
+              card_19, card_20, card_21,
+              card_22, card_23, card_24,
+              card_25, card_26, card_27,
+              card_28, card_29, card_30,
+              card_31, card_32, card_33,
+              card_34, card_35, card_36,
+              card_37, card_38, card_39,
+              card_40, card_41, card_42,
+              card_43, card_44, card_45,
+              card_46, card_47, card_48,
+              card_49, card_50, card_51,
+              card_52
+             ]
+
+
+
+
+
+
+
+
+
 
 #Place the 52 card objects into an array
 #Randomly sort the array
