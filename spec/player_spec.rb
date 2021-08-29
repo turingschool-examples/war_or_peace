ec7f2a238b47ddaa352612169878c2528445da3c
Craig Cardwell <craigcardwell@Craigs-MacBook-Pro.local>
2021-08-29 13:57:55 -0600

    test: Add test for player name and player deck

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -15,5 +15,26 @@ RSpec.describe Deck do
 
     expect(player).to be_an_instance_of(Player)
   end
-  
+
+  it "can return player name" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    player = Player.new('Clarisa', deck)
+
+    expect(player.name).to eq('Clarisa')
+  end
+
+  it "can return player deck" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    player = Player.new('Clarisa', deck)
+
+    expect(player.deck).to eq(deck)
+  end
 end
