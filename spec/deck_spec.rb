b9e5b435984b5c979e02bc534b6d797f0eda98cd
Paul Leonard <paulpaulleonard@gmail.com>
2021-08-29 15:22:24 -0400

    Add remove_card Test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -45,11 +45,23 @@ RSpec.describe Deck do
 
     expect(deck.high_ranking_cards).to eq([card1, card3])
   end
+
+  it "can remove a card" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    expect(deck.cards).to eq([card1, card2, card3])
+    deck.remove_card(card1)
+
+    expect(deck).to eq([card2, card3])
+  end
 end
 
 # pry(main)> deck.high_ranking_cards
 # #=> [#<Card:0x007fbfd18490e8...>, #<Card:0x007fbfd18555a0...>]
-# 
+#
 # pry(main)> deck.percent_high_ranking
 # #=> 66.67
 #
