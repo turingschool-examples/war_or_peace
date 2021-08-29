58650feed209f0b7c90451cf4fb1e62223921b00
Didi Dodson <mdodson55@gmail.com>
2021-08-29 16:10:37 -0600

    Test: add club suit

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -15,33 +15,32 @@ card13 = Card.new(:heart, 'Ace', 14)
 card14 = Card.new(:diamond, '2', 2)
 card15 = Card.new(:diamond, '3', 3)
 card16 = Card.new(:diamond, '4', 4)
-card17= Card.new(:diamond, '5', 5)
-card18= Card.new(:diamond, '6', 6)
-card19= Card.new(:diamond, '7', 7)
-card20= Card.new(:diamond, '8', 8)
-card21= Card.new(:diamond, '9', 9)
-card22= Card.new(:diamond, '10', 10)
-card23= Card.new(:diamond, 'Jack', 11)
-card24= Card.new(:diamond, 'Queen', 12)
-card25= Card.new(:diamond, 'King', 13)
-card26= Card.new(:diamond, 'Ace', 14)
-card27
-card28
-card29
-card30
-card31
-card32
-card33
-card34
-card35
-card36
-card37
-card38
-card39
+card17 = Card.new(:diamond, '5', 5)
+card18 = Card.new(:diamond, '6', 6)
+card19 = Card.new(:diamond, '7', 7)
+card20 = Card.new(:diamond, '8', 8)
+card21 = Card.new(:diamond, '9', 9)
+card22 = Card.new(:diamond, '10', 10)
+card23 = Card.new(:diamond, 'Jack', 11)
+card24 = Card.new(:diamond, 'Queen', 12)
+card25 = Card.new(:diamond, 'King', 13)
+card26 = Card.new(:diamond, 'Ace', 14)
+card27 = Card.new(:club, '2', 2)
+card28 = Card.new(:club, '3', 3)
+card29 = Card.new(:club, '4', 4)
+card30 = Card.new(:club, '5', 5)
+card31 = Card.new(:club, '6', 6)
+card32 = Card.new(:club, '7', 7)
+card33 = Card.new(:club, '8', 8)
+card34 = Card.new(:club, '9', 9)
+card35 = Card.new(:club, '10', 10)
+card36 = Card.new(:club, 'Jack', 11)
+card37 = Card.new(:club, 'Queen', 12)
+card38 = Card.new(:club, 'King', 13)
+card39 = Card.new(:club, 'Ace', 14)
 card40
 card41
 card42
-
 card43
 card44
 card45
