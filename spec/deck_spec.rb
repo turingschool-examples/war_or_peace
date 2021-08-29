5c2ceece8778b75e79f36f9e81a61a0c5cd28e73
Andrew Smith <c13andrew.smith@gmail.com>
2021-08-29 10:02:19 -0700

    Remove commented out code

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe Deck do
     card3 = Card.new(:heart, 'Ace', 14)
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
-    #binding.pry
+    
     expect(deck.high_ranking_cards).to eq([card1, card3])
 
   end
