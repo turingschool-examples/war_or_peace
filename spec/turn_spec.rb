3e377941ca97547f2b830e216361776275dba7b0
Craig Cardwell <craigcardwell@Craigs-MacBook-Pro.local>
2021-08-29 17:52:46 -0600

    test: Add test to confirm turn type

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe Turn do
     expect(turn).to be_an_instance_of(Turn)
   end
 
-  it "can confirm type" do
+  it "can confirm type basic" do
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
     card3 = Card.new(:heart, '9', 9)
@@ -43,5 +43,44 @@ RSpec.describe Turn do
 
     #require "pry"; binding.pry
     expect(turn.type).to eq(:basic)
+  end
+
+  it "can confirm type war" do
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    spoils_of_war = []
+    turn = Turn.new(player1, player2)
+
+    #require "pry"; binding.pry
+    expect(turn.type).to eq(:war)
+  end
+  it "can confirm type mutually_assured_destruction" do
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 12)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    spoils_of_war = []
+    turn = Turn.new(player1, player2)
+
+    #require "pry"; binding.pry
+    expect(turn.type).to eq(:mutually_assured_destruction)
   end
 end
