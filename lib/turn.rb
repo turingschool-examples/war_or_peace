8d1b3cc12d571a065eace3934d0a88a6d842cb9c
Philip Medlin <phmedlin@gmail.com>
2021-08-28 19:40:39 -0600

    Feat add start method to turn class

--- lib/turn.rb
+++ lib/turn.rb
@@ -70,31 +70,9 @@ class Turn
     #end
   end
 
-
-
+  def start(player1, player2)
+    puts "Welcome to War! (or Peace) This game will be played with 52 cards.
+  The players today are #{player1.name} and #{player2.name}.
+  Type 'GO' to start the game!"
+  end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
