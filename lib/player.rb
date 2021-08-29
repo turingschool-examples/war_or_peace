6fa574af2c2756101db265ec161a2eddd886ac5d
Chris Hewitt <agop5134@gmail.com>
2021-08-29 15:51:57 -0600

    Final upload and add message

--- lib/player.rb
+++ lib/player.rb
@@ -8,4 +8,11 @@ class Player
 
 def has_lost?
   end
-end 
+end
+
+#comments: although my project will definitely fall well below expectations,
+#a lot really clicked for me late Saturday night and I understand
+#the purpose of testing and pry much more clearly now. Thanks for looking
+#at it and I'm excited to apply more time and knowledge to the next project,
+#now that I can apply tests to my code and understand the structure of these
+#projects more fully.  
