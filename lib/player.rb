a84d45f431c208bb5bad2f003c87b90d34ae3e26
Paul Leonard <paulpaulleonard@gmail.com>
2021-08-29 17:18:25 -0400

    Add has_lost method

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,18 @@
 class Player
-  attr_accessor :deck
+  attr_accessor :deck,
+                :name
 
-  def initialize
+  def initialize(name, deck)
+    @name = name
+    @deck = deck
+  end
+
+  def has_lost?
+
+    if @deck.cards.count >= 1
+      return false
+    else
+      return true
+    end
   end
 end
