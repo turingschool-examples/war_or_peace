b8a32312b7a57b8cadb5eb6e0f95769bd8d77a25
Craig Cardwell <craigcardwell@Craigs-MacBook-Pro.local>
2021-08-29 15:07:35 -0600

    test: Add test to confirm if player has lost method working

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -37,4 +37,16 @@ RSpec.describe Deck do
 
     expect(player.deck).to eq(deck)
   end
+
+  it "can show if player has lost" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    player = Player.new('Clarisa', deck)
+
+    require "pry"; binding.pry
+    expect(player.has_lost).to eq(false)
+  end
 end
