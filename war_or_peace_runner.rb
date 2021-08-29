5f1050e8c87e28a4be001e75a12cf737535a24a0
Didi Dodson <mdodson55@gmail.com>
2021-08-29 16:07:38 -0600

    Test: add heart and diamonds

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -1,14 +1,62 @@
 
-card1_1 = Card.new(:heart, '2', 2)
-card1_2 = Card.new(:heart, '9', 9)
-card1_3 = Card.new(:heart, 'Ace', 14)
-card1_4 = Card.new(:heart, '5', 5)
-card2_1 = Card.new(:spade, 'Jack', 11)
-card2_2 = Card.new(:spade, '3', 3)
-card2_3 = Card.new(:spade, '8', 8)
-card2_4 = Card.new(:spade, 'Queen', 12)
-cards1 = ([card1_1, card1_2, card1_3, card1_4])
-cards2 = ([card2_1, card2_2, card2_3, card2_4])
+card1 = Card.new(:heart, '2', 2)
+card2 = Card.new(:heart, '3', 3)
+card3 = Card.new(:heart, '4', 4)
+card4 = Card.new(:heart, '5', 5)
+card5 = Card.new(:heart, '6', 6)
+card6 = Card.new(:heart, '7', 7)
+card7 = Card.new(:heart, '8', 8)
+card8 = Card.new(:heart, '9', 9)
+card9 = Card.new(:heart, '10', 10)
+card10 = Card.new(:heart, 'Jack', 11)
+card11 = Card.new(:heart, 'Queen', 12)
+card12 = Card.new(:heart, 'King', 13)
+card13 = Card.new(:heart, 'Ace', 14)
+card14 = Card.new(:diamond, '2', 2)
+card15 = Card.new(:diamond, '3', 3)
+card16 = Card.new(:diamond, '4', 4)
+card17= Card.new(:diamond, '5', 5)
+card18= Card.new(:diamond, '6', 6)
+card19= Card.new(:diamond, '7', 7)
+card20= Card.new(:diamond, '8', 8)
+card21= Card.new(:diamond, '9', 9)
+card22= Card.new(:diamond, '10', 10)
+card23= Card.new(:diamond, 'Jack', 11)
+card24= Card.new(:diamond, 'Queen', 12)
+card25= Card.new(:diamond, 'King', 13)
+card26= Card.new(:diamond, 'Ace', 14)
+card27
+card28
+card29
+card30
+card31
+card32
+card33
+card34
+card35
+card36
+card37
+card38
+card39
+card40
+card41
+card42
+
+card43
+card44
+card45
+card46
+card47
+card48
+card49
+card50
+card51
+card52
+
+
+
+cards9 = ([)
+cards10 = ([)
 
 deck1 = Deck.new(cards1)
 deck2 = Deck.new(cards2)
