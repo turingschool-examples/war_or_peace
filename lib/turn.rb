ef8c8b07ada568a625d9a6dcab60c93d29ffaa9a
Micha Bernhard <mib70@pitt.edu>
2021-08-29 16:21:05 -0600

    Fix end of game error

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require './lib/card'
 require './lib/deck'
 require './lib/player'
@@ -5,47 +6,42 @@ require './lib/player'
 class Turn
   attr_reader :player1,
               :player2,
-              :spoils_of_war,
-              :turn_type,
-              :winner
+              :spoils_of_war
 
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
     @spoils_of_war = []
-    @turn_type = turn_type
-    @winner = winner
   end
 
   def type
-    if player1.deck.cards[0].rank != player2.deck.cards[0].rank
-      turn_type = :basic
-    elsif player1.deck.cards[0].rank == player2.deck.cards[0].rank && player1.deck.cards[2].rank == player2.deck.cards[2].rank
-      turn_type = :mutually_assured_destruction
-    elsif player1.deck.cards[0].rank == player2.deck.cards[0].rank
-      turn_type = :war
+    if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
+      :basic
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+      :war
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) &&
+            player1.rank_of_card_at(2) == player2.rank_of_card_at(2)
+      :mutually_assured_destruction
     end
-    # turn_type
   end
 
   def winner
     if type == :mutually_assured_destruction
-      return "No Winner"
     end
-
     if type == :basic
-      player1.deck.cards[0].rank > player2.deck.cards[0].rank
-      return player1
-    else
-      return player2
+      if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+        return player1
+      elsif player2.deck.rank_of_card_at(0) > player1.deck.rank_of_card_at(0)
+        return player2
+      end
     end
-
+    # check_deck
     if type == :war
-      #maybe try player1.rank_of_card_at
-      player1.deck.cards[2].rank > player2.deck.cards[2].rank
-      return player1
-    else
-      return player2
+      if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+        return player1
+      elsif player2.deck.rank_of_card_at(2) > player1.deck.rank_of_card_at(2)
+        return player2
+      end
     end
   end
 
@@ -71,9 +67,87 @@ class Turn
   end
 
   def award_spoils(winner)
-    (winner.deck.cards << @spoils_of_war).flatten
+    pile_cards
+    if winner == player1
+      while @spoils_of_war != [] do
+        player1.deck.cards << @spoils_of_war.shift
+      end
+    elsif winner == player2
+      while @spoils_of_war != [] do
+        player2.deck.cards << @spoils_of_war.shift
+      end
+    end
   end
 
+  # def check_deck
+  #   if player1.deck.cards.count < 3  && type == :war
+  #     # binding.pry
+  #     # player1.has_lost? = true
+  #     # puts "*~*~*~* #{player2.name} has won the game! *~*~*~*"
+  #     turn_type = :basic
+  #     bad_card = Card.new(:heart, "Card", 1)
+  #     player1.deck.cards.clear
+  #     player1.deck.cards.insert(0, bad_card)
+  #   elsif player2.deck.cards.count < 3 && type == :war
+  #     # binding.pry
+  #
+  #     # player2.has_lost? = true
+  #     # puts "*~*~*~* #{player1.name} has won the game! *~*~*~*"
+  #     turn_type = :basic
+  #     bad_card = Card.new(:heart, "Card", 1)
+  #     player2.deck.cards.clear
+  #     player2.deck.cards.insert(0, bad_card)
+  #   elsif player1.deck.cards.count < 3 && type == :mutually_assured_destruction
+  #     # binding.pry
+  #
+  #     # player1.has_lost? = true
+  #     # puts "*~*~*~* #{player2.name} has won the game! *~*~*~*"
+  #     turn_type = :basic
+  #     bad_card = Card.new(:heart, "Card", 1)
+  #     player1.deck.cards.clear
+  #     player1.deck.cards.insert(0, bad_card)
+  #   elsif player2.deck.cards.count < 3 && type == :mutually_assured_destruction
+  #     # binding.pry
+  #     # player2.has_lost? = true
+  #     # puts "*~*~*~* #{player1.name} has won the game! *~*~*~*"
+  #     turn_type = :basic
+  #     bad_card = Card.new(:heart, "Card", 1)
+  #     player2.deck.cards.clear
+  #     player2.deck.cards.insert(0, bad_card)
+  #   end
+  # end
+  def loop
+    count = 0
+    until player1.has_lost? == true || player2.has_lost? == true do
+
+      puts player1.deck.cards.count
+      puts player2.deck.cards.count
+      if winner == nil
+        puts "MAD"
+      else
+        winner_name = winner.name
+      end
+
+      if type == :basic
+        puts "Turn #{count}: turn type is #{type}, #{winner_name} won 2 cards"
+      elsif type == :war
+        puts "Turn #{count}: WAR - #{winner_name} won 6 cards"
+      elsif type == :mutually_assured_destruction
+        puts "Turn #{count}: *mutually assured destruction* 6 cards removed from play"
+      end
+      award_spoils(winner)
+
+      if count >= 1000000
+        puts "Draw"
+        break
+      elsif player1.has_lost? == true
+        puts "*~*~*~* #{player2.name} has won the game! *~*~*~*"
+      elsif player2.has_lost? == true
+        puts "*~*~*~* #{player1.name} has won the game! *~*~*~*"
+      end
+      count += 1
+    end
+  end
   def start
     puts "Welcome to War! (or Peace) This game will be played with 52 cards.
 The players today are #{player1.name} and #{player2.name}.
@@ -81,21 +155,9 @@ Type 'GO' to start the game!
 ---------------------------------------------------------------------"
 
     input = gets.chomp
-    count = 0
+
     if input == "GO"
-      until player1.has_lost? == true || player2.has_lost? == true do
-        count += 1
-        pile_cards
-        winner
-        award_spoils(winner)
-        if type == :basic
-          puts "Turn #{count}: #{winner.name} won 2 cards"
-        elsif type == :war
-          puts "Turn #{count}: WAR - #{winner.name} won 6 cards"
-        elsif type == :mutually_assured_destruction
-          puts "Turn #{count}: *mutually_assured_destruction* 6 cards removed from play"
-        end
-      end
+    loop
     else puts "You entered something other than 'GO'"
     end
   end
