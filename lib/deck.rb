fa2a4c3123d83d4123e32569544f3e0235059e81
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-08-28 19:17:43 -0600

    Feat- add lib/deck.rb

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,12 +1,12 @@
 class Deck
   attr_reader :cards
-  def initialize (cards)
+  def initialize(cards)
     @cards = cards
   end
 
   #Method will take one argument that represents the index location of a card to be used and will return the rank of that card.
   def rank_of_card_at(index)
-    @cards[index].rank
+    cards[index].rank
   end
 
 
@@ -20,12 +20,16 @@ class Deck
 
   #Method will return the percentage of cards that are high ranking.
   def percent_high_ranking
-
     ((high_ranking_cards.length.to_f / @cards.count) * 100).round(2)
-
   end
+
   #Method will remove the top card from deck.
   def remove_card
-    @cards.push[0]
+    @cards.shift
+  end
+
+  #Method adds card 4
+  def add_card(card)
+    @cards << card
   end
 end
