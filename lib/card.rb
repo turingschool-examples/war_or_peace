59b7db18a482f7eb189ea30163764ecf259a214f
Micha Bernhard <mib70@pitt.edu>
2021-08-29 16:17:25 -0600

    Refactor: Delete unneeded spaces

--- lib/card.rb
+++ lib/card.rb
@@ -6,9 +6,4 @@ attr_reader :suit, :value, :rank
     @value = value
     @rank = rank
   end
-
 end
-
-#card1 = Card.new(:diamond, 'Queen', 12)
-#card2 = Card.new(:spade, '3', 3)
-#card3 = Card.new(:heart, 'Ace', 14)
