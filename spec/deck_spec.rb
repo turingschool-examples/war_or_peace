61931373d3134c40b128abedf0fac1fad39de663
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-08-28 19:16:44 -0600

    Feat- add deck_spec.rb

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -28,7 +28,7 @@ RSpec.describe Deck do
     expect(deck.rank_of_card_at(2)).to eq(14)
   end
 
-  it "put high ranking cards in an array" do
+  it "it can calculate percent high ranking" do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
@@ -36,21 +36,12 @@ RSpec.describe Deck do
 
     deck = Deck.new(cards)
 
+    expect(deck.cards).to eq([card1, card2, card3])
     expect(deck.high_ranking_cards).to eq([card1,card3])
-  end
-
-  it "calculates percent high_ranking_cards" do
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-
-    deck = Deck.new(cards)
-
     expect(deck.percent_high_ranking).to eq(66.67)
   end
 
-  it "removes a card" do
+  it "it can remove a card" do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
@@ -59,6 +50,27 @@ RSpec.describe Deck do
     deck = Deck.new(cards)
 
     expect(deck.remove_card).to eq(card1)
+    expect(cards).to eq([card2, card3])
+    expect(deck.high_ranking_cards).to eq([card3])
+    expect(deck.percent_high_ranking).to eq(50.00)
+  end
+
+
+  it "can add a card" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    card4 = Card.new(:club, '5', 5)
+
+    deck.remove_card
+    deck.add_card(card4)
+    expect(deck.cards).to eq([card2, card3, card4])
+    expect(deck.high_ranking_cards).to eq([card3])
+    expect(deck.percent_high_ranking).to eq(33.33)
   end
 
 end
