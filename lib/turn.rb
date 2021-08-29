77eb93cd0f864648033e9cb231699fef2055e91f
Didi Dodson <mdodson55@gmail.com>
2021-08-29 10:53:56 -0600

    Fix: update to run

--- lib/turn.rb
+++ lib/turn.rb
@@ -49,7 +49,6 @@ class Turn
     end
   end
 
-
   # define pile of cards method
   def pile_cards
 
@@ -60,32 +59,35 @@ class Turn
       @player2.deck.cards.delete_at(0)
       @player2.deck.cards.delete_at(1)
       @player2.deck.cards.delete_at(2)
+        return @spoils_of_war
 
 
     elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
       @spoils_of_war << @player1.deck.cards[0..2]
       @spoils_of_war << @player2.deck.cards[0..2]
+      @player1.deck.cards.delete_at(0)
+      @player1.deck.cards.delete_at(1)
+      @player1.deck.cards.delete_at(2)
+      @player2.deck.cards.delete_at(0)
+      @player2.deck.cards.delete_at(1)
+      @player2.deck.cards.delete_at(2)
+        return @spoils_of_war
 
 
     elsif @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
       @spoils_of_war << @player1.deck.cards[0]
       @spoils_of_war << @player2.deck.cards[0]
+      @player1.deck.cards.delete_at(0)
+      @player2.deck.cards.delete_at(0)
+        return @spoils_of_war
     end
   end
-
-  def 
 
   #define award_spoils(winner) method
   def award_spoils(winner)
     if winner == @player1
-      @player1.deck.cards.delete_at(0)
-      @player2.deck.cards.delete_at(0)
       @player1.deck.cards.push(@spoils_of_war)
-
-    end
-
-    if winner == @player2
-
+    elsif winner == @player2
       @player2.deck.cards.push(@spoils_of_war)
     end
   end
