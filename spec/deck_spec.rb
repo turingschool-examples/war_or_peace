a32d3ad58a6a88896871e9e5986769f1dea996b7
Craig Cardwell <craigcardwell@Craigs-MacBook-Pro.local>
2021-08-28 21:28:32 -0600

    test: Add test code for high ranking card and percent of high cards

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -29,11 +29,39 @@ RSpec.describe Deck do
     card3 = Card.new(:heart, 'Ace', 14)
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
-    deck.cards
 
     expect(deck.rank_of_card_at(0)).to be(12)
     expect(deck.rank_of_card_at(2)).to be(14)
+  end
 
+  it "can create an array of high ranking cards" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
 
+    #expect(deck.high_ranking_cards).to be_a []
+    expect(deck.high_ranking_cards).to eq([card1, card3])
+  end
+
+  it "can identify percent of high rank cards" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.percent_high_ranking).to eq(66.67)
+  end
+
+  it "can identify percent of high rank cards" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.remove_card)to be([card1])
   end
 end
