c7f6e8a47323d61c9580e13fc2c90dec28e54b29
Andrew Patterson <andrew.patterson747@gmail.com>
2021-08-29 18:57:45 -0500

    Fix error deleting a card in a test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -47,7 +47,7 @@ RSpec.describe Turn do
     card2 = Card.new(:heart, '10', 10)
     card3 = Card.new(:heart, '9', 9)
     card4 = Card.new(:diamond, 'Jack', 11)
-    card5 = Card.new(:heart, '8', 8)
+    card5 = Card.new(:heart, '8', )
     card6 = Card.new(:diamond, 'Queen', 12)
     card7 = Card.new(:heart, '3', 3)
     card8 = Card.new(:diamond, '2', 2)
