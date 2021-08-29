02c5fa9b1add971baec0b4209912a3d30421b909
Karan Mehta <karanm645@gmail.com>
2021-08-28 21:19:26 -0500

    "Finish pile_cards & award_spoils method"

--- lib/turn.rb
+++ lib/turn.rb
@@ -43,6 +43,25 @@ class Turn
   end
 
   def pile_cards
+    if type == :basic
+      @spoils_of_war << @player1.deck.remove_card
+      @spoils_of_war << @player2.deck.remove_card
+    elsif type == :war
+      3.times do
+        @spoils_of_war << @player1.deck.remove_card
+        @spoils_of_war << @player2.deck.remove_card
+      end
+    else
+      3.times do
+        @player1.deck.remove_card
+        @player2.deck.remove_card
+      end
+    end
+  end
 
+  def award_spoils
+    @spoils_of_war.each do |spoils|
+      @winner.deck.add_card(spoils)
+    end
   end
 end
