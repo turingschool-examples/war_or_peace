c661bf8f9ee5afc072e6535d35b70e6166f653cf
Nathan Brown <nathanbrown@Nathans-MacBook-Pro.local>
2021-08-29 02:46:02 -0600

    Add before each statements to tests to save space

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,30 +3,24 @@ require './lib/deck'
 
 describe Deck do
   describe "#initialize" do
+    before(:each) do
+      @card1 = Card.new(:diamond, 'Queen', 12)
+      @card2 = Card.new(:spade, '3', 3)
+      @card3 = Card.new(:heart, 'Ace', 14)
+      @card4 = Card.new(:club, '5', 5)
+    end
   it 'exists' do
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    card4 = Card.new(:club, '5', 5)
-    cards = [card1, card2, card3, card4]
+    cards = [@card1, @card2, @card3, @card4]
     deck = Deck.new(cards)
     expect(deck).to be_a(Deck)
   end
   it "contains an array" do
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    card4 = Card.new(:club, '5', 5)
-    cards = [card1, card2, card3, card4]
+    cards = [@card1, @card2, @card3, @card4]
     deck = Deck.new(cards)
     expect(deck.cards).to be_an Array
   end
   it "has individual card attributes inside the array" do
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    card4 = Card.new(:club, '5', 5)
-    cards = [card1, card2, card3, card4]
+    cards = [@card1, @card2, @card3, @card4]
     deck = Deck.new(cards)
     expect(deck.cards[0].suit).to eq :diamond
   end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,43 +3,35 @@ require './lib/player'
 
 describe Player do
   describe '#initialize' do
-    it 'stores name variable correctly' do
-      card1 = Card.new(:diamond, 'Queen', 12)
-      card2 = Card.new(:spade, '3', 3)
-      cards = [card1, card2]
-      deck = Deck.new(cards)
-      player = Player.new('Clarisa', deck)
-
-      expect(player.name).to eq('Clarisa')
+    before(:each) do
+      @card1 = Card.new(:diamond, 'Queen', 12)
+      @card2 = Card.new(:spade, '3', 3)
+      @cards = [@card1, @card2]
+      @deck = Deck.new(@cards)
+      @player = Player.new('Clarisa', @deck)
     end
-    it 'stores deck variable correctly' do
-      card1 = Card.new(:diamond, 'Queen', 12)
-      card2 = Card.new(:spade, '3', 3)
-      cards = [card1, card2]
-      deck = Deck.new(cards)
-      player = Player.new('Clarisa', deck)
-
-      expect(player.deck).to be_a(Deck)
+    it 'stores name variable correctly' do
+      expect(@player.name).to eq('Clarisa')
+    end
+    it 'stores deck variable as a Deck object' do
+      expect(@player.deck).to be_a(Deck)
     end
   end
   describe '#has_lost?' do
+    before(:each) do
+      @card1 = Card.new(:diamond, 'Queen', 12)
+      @card2 = Card.new(:spade, '3', 3)
+      @cards = [@card1, @card2]
+      @deck = Deck.new(@cards)
+      @player = Player.new('Clarisa', @deck)
+    end
     it 'does not create false positives on having lost' do
-      card1 = Card.new(:diamond, 'Queen', 12)
-      card2 = Card.new(:spade, '3', 3)
-      cards = [card1, card2]
-      deck = Deck.new(cards)
-      player = Player.new('Clarisa', deck)
-      expect(player.has_lost?).to eq(false)
+      expect(@player.has_lost?).to eq(false)
     end
     it 'correctly identifies player has lost' do
-      card1 = Card.new(:diamond, 'Queen', 12)
-      card2 = Card.new(:spade, '3', 3)
-      cards = [card1, card2]
-      deck = Deck.new(cards)
-      player = Player.new('Clarisa', deck)
-      player.deck.remove_card
-      player.deck.remove_card
-      expect(player.has_lost?).to eq(true)
+      @player.deck.remove_card
+      @player.deck.remove_card
+      expect(@player.has_lost?).to eq(true)
     end
   end
 
