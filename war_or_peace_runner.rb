3a597402fa6afd169dfcb97f6f76648ebfd2d6df
Andrew Smith <c13andrew.smith@gmail.com>
2021-08-29 10:06:15 -0700

    Remove commented out code

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -20,7 +20,7 @@ require 'pry'
 #     end
 # end
 
-card_generator = CardGenerator.new('./lib/cards.txt')
+card_generator = CardGenerator.new('cards.txt')
 cards = card_generator.cards
 
 cards.shuffle!
@@ -46,44 +46,3 @@ end
 
 game = Game.new(turn)
 game.start
-
-# class Game
-#
-#   def initialize(turn)
-#     @turn = turn
-#   end
-#
-#   def start
-#       count = 1
-#       #binding.pry
-#       while (!@turn.player1.has_lost?) && (!@turn.player2.has_lost?) && (count <= 1000000)
-#           #binding.pry
-#           if @turn.type == :basic
-#             puts "Turn #{count}: #{@turn.winner.name} won 2 cards"
-#             winner = @turn.winner
-#             @turn.pile_cards
-#             @turn.award_spoils(winner)
-#           elsif @turn.type == :war
-#             puts "Turn #{count}: WAR - #{@turn.winner.name} won 6 cards"
-#             winner = @turn.winner
-#             @turn.pile_cards
-#             @turn.award_spoils(winner)
-#           elsif @turn.type == :mutually_assured_destruction
-#             puts "Turn #{count}: *mutually assured destruction* 6 cards removed from play"
-#             @turn.pile_cards
-#           end
-#
-#           count += 1
-#
-#       end
-#
-#       if @turn.player1.has_lost?
-#         puts "*~*~*~* #{@turn.player2.name} has won the game! *~*~*~*"
-#       elsif @turn.player2.has_lost?
-#         puts "*~*~*~* #{@turn.player1.name} has won the game! *~*~*~*"
-#       else
-#         puts "---- DRAW ----"
-#       end
-#
-#   end
-# end
