b1a967d996823be8ffae48f685c97ceb0988261c
Will Dunlap <dunlapww@gmail.com>
2021-09-29 13:15:20 -0600

    pass it can get card rank

--- lib/card.rb
+++ lib/card.rb
@@ -1,8 +1,9 @@
 class Card
-    attr_reader :suit, :value, :rank
-    def initialize(suit, value, rank)
-        @suit = suit
-        @value = value
-        @rank = rank
-    end
+  attr_reader :suit, :value, :rank
+
+  def initialize(suit, value, rank)
+    @suit = suit
+    @value = value
+    @rank = rank
+  end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -4,13 +4,12 @@ require './lib/card'
 
 RSpec.describe Deck do
   before :each do
-    card1 = Card.new(:diamond, 'Jack', 11 )
-    card2 = Card.new(:heart, 'Eight', 8 )
+    card1 = Card.new(:diamond, 'Jack', 11)
+    card2 = Card.new(:heart, 'Eight', 8)
     @cards = [card1, card2]
   end
 
-  it "exists" do
-    cards = []
+  it 'exists' do
     deck = Deck.new(@cards)
 
     expect(deck).to be_an_instance_of(Deck)
@@ -23,9 +22,7 @@ RSpec.describe Deck do
 
   it 'can return rank of card at' do
     deck = Deck.new(@cards)
-    expect(deck.rank_of_card_at(0).to eq(@cards[0].rank))
-    expect(deck.rank_of_card_at(1).to eq(@cards[1].rank))
+    expect(deck.rank_of_card_at(0)).to eq(@cards[0].rank)
+    expect(deck.rank_of_card_at(1)).to eq(@cards[1].rank)
   end
-
-  
 end
