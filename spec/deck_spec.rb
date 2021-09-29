4e2aef5f62218703b1a2c10c4e166ce918cde718
Will Dunlap <dunlapww@gmail.com>
2021-09-29 10:58:28 -0600

    pass deck has cards and can read them

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,10 +3,22 @@ require './lib/deck'
 require './lib/card'
 
 RSpec.describe Deck do
+  before :each do
+    card1 = Card.new(:diamond, 'Jack', 11 )
+    card2 = Card.new(:heart, 'Eight', 8 )
+    @cards = [card1, card2]
+  end
+
   it "exists" do
-    deck = Deck.new(cards)
+    cards = []
+    deck = Deck.new(@cards)
 
     expect(deck).to be_an_instance_of(Deck)
+  end
+
+  it 'can read cards' do
+    deck = Deck.new(@cards)
+    expect(deck.cards).to eq(@cards)
   end
 
   
