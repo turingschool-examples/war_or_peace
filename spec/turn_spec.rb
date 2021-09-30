ee652c756980a69dc954cd7a077a33ffbe16482c
Will Dunlap <dunlapww@gmail.com>
2021-09-30 08:24:01 -0600

    test it has attributes

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -16,4 +16,11 @@ RSpec.describe Player do
     turn = Turn.new(@player1, @player2)
     expect(turn).to be_a Turn
   end
+  
+  it 'has attributes' do
+    turn = Turn.new(@player1, @player2)
+    expect(turn.player1).to be_a Player
+    expect(turn.player2).to be_a Player
+    expect(turn.spoils_of_war).to be_a Array
+  end
 end
