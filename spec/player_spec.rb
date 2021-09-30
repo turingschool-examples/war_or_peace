942da7c6ac41864f5f3980d8fd4241596e354965
Will Dunlap <dunlapww@gmail.com>
2021-09-29 22:05:09 -0600

    test has player lost?

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -19,4 +19,19 @@ RSpec.describe Player do
     expect(player.deck).to be_a(Deck)
     expect(player.deck.cards.size).to eq(52)
   end
+  
+  it 'has_lost' do
+    card1 = Card.new(:diamond, "Two", 2)
+    card2 = Card.new(:diamond, "Three", 3)
+    card3 = Card.new(:diamond, "Four", 4)
+    cards = [card1, card2, card3]
+    player = Player.new("Sally", Deck.new(cards))
+    expect(player).has_lost?.to eq(false)
+    player.deck.remove_card
+    expect(player).has_lost?.to eq(false)
+    player.deck.remove_card
+    expect(player).has_lost?.to eq(false)
+    player.deck.remove_card
+    expect(player).has_lost?.to eq(true)
+  end
 end
