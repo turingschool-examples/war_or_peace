573c0e3936c43f9300a2b57575f01a1c4a83b2f0
Zachary Imbriaco <zachary@imbriaco.com>
2022-02-01 12:21:37 -0500

    Reverted initialize to @ values.

--- lib/card.rb
+++ lib/card.rb
@@ -1,6 +1,7 @@
-class Card()
+class Card
     attr_accessor :suit, :name, :value
-    def initialize(s, n, v)
+    
+    def initialize(suit, name, value)
       @suit = s
       @name = n
       @value = v
