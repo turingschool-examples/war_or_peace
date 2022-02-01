a13467258e9602aecb26b44d8ae0930c6fcce0f0
Zachary Imbriaco <zachary@imbriaco.com>
2022-02-01 12:53:46 -0500

    Added comments to card.rb.

--- lib/card.rb
+++ lib/card.rb
@@ -1,10 +1,13 @@
+# This file creates a Card class with suit, name, and value attributes, to be used for the other parts of War.
 class Card
+    # Attribute Reader for all values.
     attr_reader :suit, :name, :value
-    
+    # Initializes values in a new card.
     def initialize(suit, name, value)
       @suit = suit
       @name = name
       @value = value
     end
+
 end
 
