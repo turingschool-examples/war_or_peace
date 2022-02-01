99376b1ae87bc885885fc95e2ad13470d706d15f
James Harkins <jh.harkins@protonmail.com>
2022-02-01 08:50:31 -0700

    Update deck.rb

--- lib/deck.rb
+++ lib/deck.rb
@@ -3,6 +3,19 @@ class Deck
   attr_reader :cards
   def initialize(cards)
     @cards = cards
+    @high_ranking_cards = []
+  end
+
+  def rank_of_card_at(index)
+    @cards[index].rank
+  end
+
+  def high_ranking_cards
+    @cards.each do |card|
+      if card.rank >= 11
+        @high_ranking_cards << card
+      end
+    end
   end
 
 end
