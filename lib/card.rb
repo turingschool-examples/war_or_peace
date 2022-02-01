e1ef69148e56ba867f5a99d757faaba74fc598c2
Zachary Imbriaco <zachary@imbriaco.com>
2022-02-01 12:17:28 -0500

    Added attr_accessor with all attributes to Card.

--- lib/card.rb
+++ lib/card.rb
@@ -1,7 +1,8 @@
 class Card()
-    def initialize(suit, name, value)
-      @suit = suit
-      @name = name
-      @value = value
+    attr_accessor :suit, :name, :value
+    def initialize(s, n, v)
+      @suit = s
+      @name = n
+      @value = v
     end
 end
