918674f14f9aad883041520c1e0dbf9841e44dd9
reuben <creubend@gmail.com>
2022-02-02 07:21:05 -0700

    Add runner.rb

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,4 +1,15 @@
 class Deck
+  attr_reader :cards
+  
+  
   def initialize(deck)
     @deck = []
   end
+  
+  def rank_of_card_at[] 
+    returns card.rank
+  end
+  
+  def high_ranking_cards
+    if rank >= 11
+      high_rank = []
