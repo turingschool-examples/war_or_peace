63b435a1442be34c42a30f5bafd1766ac3ea4484
Zachary Imbriaco <zachary@imbriaco.com>
2022-02-02 16:31:30 -0500

    Add high_ranking_cards method.

--- lib/deck.rb
+++ lib/deck.rb
@@ -8,4 +8,14 @@ class Deck
     def rank_of_card_at(index)
         return cards[index].rank
     end
+
+    def high_ranking_cards()
+        high_rank = []
+        cards.each do |card|
+            if card.rank >= 11
+                high_rank << card
+            end
+        end
+        return high_rank
+    end
 end
