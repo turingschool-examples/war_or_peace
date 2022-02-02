2138afaee20834a6a4770c8a7c5fd1182458aba0
Johnny Bowman <johntbowman.production@gmail.com>
2022-02-01 23:02:07 -0700

    Pass first card test

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -8,11 +8,11 @@ RSpec.describe Card do
     expect(card).to be_an_instance_of(Card)
   end
 
-  it "has readable attributes" do
-    card = Card.new(:diamond, 'Queen', 12)
-
-    expect(card.suit).to eq(:diamond)
-    expect(card.value).to eq('Queen')
-    expect(card.rank).to eq(12)
-  end
+  # it "has readable attributes" do
+  #   card = Card.new(:diamond, 'Queen', 12)
+  #
+  #   expect(card.suit).to eq(:diamond)
+  #   expect(card.value).to eq('Queen')
+  #   expect(card.rank).to eq(12)
+  # end
 end
