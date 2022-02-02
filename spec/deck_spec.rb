0e2671991e25797e46ca401304ef87c4e8a56026
James Harkins <jh.harkins@protonmail.com>
2022-02-01 20:08:28 -0700

    Pass all deck_spec.rb tests

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -63,4 +63,39 @@ RSpec.describe Deck do
       expect(deck.percent_high_ranking).to eq(66.7)
     end
   end
+
+  describe '#remove_card' do
+    it "can remove cards from the deck" do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+
+      cards = [card1, card2, card3]
+
+      deck = Deck.new(cards)
+
+      deck.remove_card
+      expect(deck.cards).to eq([card2, card3])
+    end
+  end
+
+  describe '#add_card' do
+    it "can add cards to the deck" do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+
+      cards = [card1, card2, card3]
+
+      deck = Deck.new(cards)
+
+      deck.remove_card
+      expect(deck.cards).to eq([card2, card3])
+
+      card4 = Card.new(:club, '5', 5)
+
+      deck.add_card(card4)
+      expect(deck.cards).to eq([card2, card3, card4])
+    end
+  end
 end
