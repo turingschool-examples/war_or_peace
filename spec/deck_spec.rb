208ae9456e182e69ebb1dd7720cb8f49cb96e05a
Adrian Campos <adriancampos2992@yahoo.com>
2022-02-02 18:26:45 -0800

    add deck class, pass deck class exists test

---
+++ spec/deck_spec.rb
@@ -0,0 +1,12 @@
+require './lib/card'
+require './lib/deck'
+
+
+RSpec.describe Deck do
+  it 'deck class exists' do
+  card1 = Card.new(:diamond, 'Queen', 12)
+  card2 = Card.new(:spade, '3', 3)
+  card3 = Card.new(:heart, 'Ace', 14)
+  cards = [card1, card2, card3]
+  end
+end
