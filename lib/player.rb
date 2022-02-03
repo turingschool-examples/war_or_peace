44bfbfd23e1fe3f3ad2a51e3dc6f6f7ce41cc043
reuben <creubend@gmail.com>
2022-02-03 13:15:45 -0700

    Create player class

---
+++ lib/player.rb
@@ -0,0 +1,14 @@
+require "./lib/card"
+require "./lib/deck"
+require "./lib/player"
+
+
+
+class Player
+  def initialize("name", deck)
+    @name = name
+    @deck = deck
+  end
+  
+  def has_lost?
+    if 
