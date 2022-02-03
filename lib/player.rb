eb9e1c23fb6adedbb4fe42217191da848a4bdf98
Chris_K <ckjolhede@gmail.com>
2022-02-02 20:53:29 -0700

    feature: #type & #winner working

--- lib/player.rb
+++ lib/player.rb
@@ -5,9 +5,6 @@ require 'pry'
 class Player
   attr_reader :name, :deck
 
-  # def initialize(name, deck_name)
-  #   @deck_name = deck_name
-  #   @name = name
   def initialize(name, deck)
     @deck = deck
     @name = name
---
+++ lib/turn.rb
@@ -0,0 +1,56 @@
+require './lib/player'
+require './lib/deck'
+require './lib/card'
+require 'pry'
+
+class Turn
+  attr_reader :player1, :player2, :spoils_of_war, :winner
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @spoils_of_war = []
+  end
+
+  def type
+    if @player1.deck.cards[0].rank != @player2.deck.cards[0].rank
+      :basic
+    elsif @player1.deck.cards[2] != @player2.deck.cards[2]
+      :war
+    else
+      self.mutually_assured_destruction
+      :mutually_assured_destruction
+    end
+  end
+
+  def winner
+     if @player1.deck.cards[0].rank > @player2.deck.cards[0].rank
+       @winner = @player1
+     elsif @player1.deck.cards[0].rank < @player2.deck.cards[0].rank
+       @winner = @player2
+     else
+       "No Winner"
+     end
+  end
+
+  def pile_cards
+    if @turn.type == :basic
+      @spoils_of_war << @player1.deck.cards.shift
+      @spoils_of_war << @player2.deck.cards.shift
+    elsif @turn.type == :war
+      3.times do
+        @spoils_of_war << @player1.deck.cards.shift
+        @spoils_of_war << @player2.deck.cards.shift
+      end
+    else
+      3.times do
+        @player1.deck.cards.shift
+        @player2.deck.cards.shift
+      end
+    end
+  end
+
+  def award_spoils(winner)
+    @winner.deck.cards << @spoils_of_war
+  end
+end
