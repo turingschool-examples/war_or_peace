60b78ef675ae1d3911b4b8d085ab5df71e3ec387
Sandisz Thieme <sandiszthieme@gmail.com>
2022-02-02 23:22:12 -0700

    Add a second turn to the turn test file

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -5,7 +5,7 @@ require './lib/turn'
 require 'pry'
 
 RSpec.describe Turn do
-  context 'Turn' do
+  context 'Turn one' do
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
     card3 = Card.new(:heart, '9', 9)
@@ -47,6 +47,40 @@ RSpec.describe Turn do
       turn.pile_cards
       turn.award_spoils
       expect(player1.deck.cards).to include(card3)
+      expect(turn.spoils_of_war).to eq([])
+    end
+  end
+
+  context 'Turn Two' do
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+
+    it 'can determine what type of turn you are taking' do
+      expect(turn.type).to eq(:war)
+    end
+
+    it 'can determine a winner' do
+      expect(turn.winner).to eq(player2)
+    end
+
+    it 'can give the winner the spoils of war' do
+      turn.pile_cards
+      turn.award_spoils
+      expect(player2.deck.cards).to include(card5)
       expect(turn.spoils_of_war).to eq([])
     end
   end
