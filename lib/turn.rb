465d27de5379ff1344285d5860c0f606b92bee1a
James Harkins <jh.harkins@protonmail.com>
2022-02-02 20:24:26 -0700

    Pass turn_spec.rb tests for initialize and type methods

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,6 +9,66 @@ class Turn
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
+    @spoils_of_war = []
+  end
+
+  def type
+    @type = :type
+    if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
+      @type = :basic
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) != @player2.deck.rank_of_card_at(2)
+      @type = :war
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
+      @type = :mutually_assured_destruction
+    end
+    return @type
+  end
+
+  def winner
+    if @type == :basic && @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
+      @winner = @player1
+      @winner
+    elsif @type == :basic && @player1.deck.rank_of_card_at(0) < @player2.deck.rank_of_card_at(0)
+      @winner = @player2
+      @winner
+    elsif @type == :war && @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
+      @winner = @player1
+      @winner
+    elsif @type == :war && @player1.deck.rank_of_card_at(2) < @player2.deck.rank_of_card_at(2)
+      @winner = @player2
+      @winner
+    elsif @type == :mutually_assured_destruction
+      p "No Winner."
+    end
+  end
+
+  def pile_cards
+    if @type == :basic
+      @spoils_of_war << @player1.deck.remove_card
+      @spiols_of_war << @player2.deck.remove_card
+    elsif @type == :war
+      3.times do
+        @spoils_of_war << @player1.deck.remove_card
+        @spoils_of_war << @player2.deck.remove_card
+      end
+    elsif @type == :mutually_assured_destruction
+      3.times do
+        @player1.deck.remove_card
+        @player2.deck.remove_card
+      end
+    end
+  end
+
+  def award_spoils
+    if @winner == @player1
+      @spoils_of_war.each do |card|
+        @player1.deck.add_card(card)
+      end
+    elsif @winner == @player2
+      @spoils_of_war.each do |card|
+        @player2.deck.add_card(card)
+      end
+    end
   end
 
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -6,6 +6,116 @@ require './lib/turn'
 require 'pry'
 
 RSpec.describe Turn do
-  
+  describe '#initialize' do
+    it 'has two players' do
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:heart, '9', 9)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, 'Queen', 12)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card3, card4, card6, card7])
+
+      player1 = Player.new('Robot 1', deck1)
+      player2 = Player.new('Robot 2', deck2)
+
+      turn = Turn.new(player1, player2)
+
+      expect(turn.player1).to eq(player1)
+      expect(turn.player2).to eq(player2)
+    end
+  end
+
+  describe '#initialize' do
+    it 'starts out without any spoils of war' do
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:heart, '9', 9)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, 'Queen', 12)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card3, card4, card6, card7])
+
+      player1 = Player.new('Robot 1', deck1)
+      player2 = Player.new('Robot 2', deck2)
+
+      turn = Turn.new(player1, player2)
+      expect(turn.spoils_of_war).to eq([])
+    end
+  end
+
+  describe '#type' do
+    it 'can determine the turn type :basic' do
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:heart, '9', 9)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, 'Queen', 12)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card3, card4, card6, card7])
+
+      player1 = Player.new('Robot 1', deck1)
+      player2 = Player.new('Robot 2', deck2)
+
+      turn = Turn.new(player1, player2)
+      expect(turn.type).to eq(:basic)
+    end
+  end
+
+  describe '#type' do
+    it 'can determine the turn type :war' do
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:spade, 'Jack', 11)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, 'Queen', 12)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card3, card4, card6, card7])
+
+      player1 = Player.new('Robot 1', deck1)
+      player2 = Player.new('Robot 2', deck2)
+
+      turn = Turn.new(player1, player2)
+      expect(turn.type).to eq(:war)
+    end
+  end
+
+  describe '#type' do
+    it 'can determine the turn type :mutually_assured_destruction' do
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:spade, 'Jack', 11)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, '8', 8)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card3, card4, card6, card7])
+
+      player1 = Player.new('Robot 1', deck1)
+      player2 = Player.new('Robot 2', deck2)
+
+      turn = Turn.new(player1, player2)
+      expect(turn.type).to eq(:mutually_assured_destruction)
+    end
+  end
 
 end
