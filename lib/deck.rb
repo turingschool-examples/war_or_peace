20d46fcc732276a9d97d80a27920b0d1cf91bec4
alex randolph <alexgeorgerandolph@gmail.com>
2022-02-03 13:40:59 -0700

    Fix percent_high_ranking Method, Add remove_card and add_card

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,7 +1,7 @@
 require './card'
 
 class Deck
-  attr_reader :cards, :rank_of_card_at, :high_ranking_cards, :percent_high_ranking
+  attr_reader :cards, :rank_of_card_at, :high_ranking_cards, :percent_high_ranking, :remove_card, :add_card
 
   def initialize(cards)
     @cards = cards
@@ -16,19 +16,34 @@ class Deck
   end
 
   def high_ranking_cards
+    face_cards = []
     @cards.each do |card|
-      if card.rank < 11
-        @cards.delete(card)
+      if card.rank >= 11
+        face_cards << card# @cards.delete(card)
       end
     end
+    return face_cards
   end
 
   def percent_high_ranking
     high_ranking_cards
-    @percent_high_ranking = high_ranking_cards.size / 2
-      number = high_ranking_cards.size / 3.to_f * 100.to_f
-        return number.round(2)
+    total_cards = @cards
+    total = high_ranking_cards.count / total_cards.count.to_f * 100
+      return total.round(2)
+
   end
+
+  def remove_card
+    @cards.shift #could i shovel the removed card into a new array here?
+  end
+
+   def add_card(new_card)
+     @cards << new_card
+   end
+
+
+
+
 end
 # require 'pry'; binding.pry
 
@@ -38,3 +53,10 @@ end
 #   high = @high_ranking_cards
 #   total = cards.count
 # end
+
+#def percent_high_ranking
+#high_ranking_cards.size
+# return @cards.length.to_f
+# @percent_high_ranking = high_ranking_cards.size / 2
+#   number = high_ranking_cards.size / cards.length
+#     return
--- lib/runner.rb
+++ lib/runner.rb
@@ -8,11 +8,13 @@ card3 = Card.new(:heart, 'Ace', 14)
 cards = [card1, card2, card3]
 
 deck = Deck.new(cards)
-
+# card4 = Card.new(:club, '5', 5)
+#
+# deck.add_card(card4)
 #
 require 'pry'; binding.pry
-p deck.rank_of_card_at(2)
-#
+# p deck.rank_of_card_at(2)
+p "fml"
 # p deck.cards
 # p card1
 # p cards
