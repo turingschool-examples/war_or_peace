dac8105aec74885274ed905d08983aa118d4ec5a
Alex Psitos <apsitos08@gmail.com>
2022-02-03 15:41:47 -0500

    Add inputs and pry to runner.rb

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,2 +1,12 @@
 require_relative 'card'
 require_relative 'deck'
+
+card1 = Card.new(:diamond, 'Queen', 12)
+card2 = Card.new(:spade, '3', 3)
+card3 = Card.new(:heart, 'Ace', 14)
+
+cards = [card1, card2, card3]
+
+deck = Deck.new(cards)
+
+require 'pry'; binding.pry 
