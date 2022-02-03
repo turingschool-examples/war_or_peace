e99274d1cd8be735e08381fe05f6de0822cd0ae0
Cory Bethune <corybbethune@gmail.com>
2022-02-03 15:47:21 -0700

    Move deck test to deck_spec from card_spec

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,6 +1,7 @@
 require 'rspec'
 require './lib/card'
-require './lib/deck'
+require 'pry'
+
 
 RSpec.describe Card do
   it "exists" do
@@ -16,54 +17,4 @@ RSpec.describe Card do
     expect(card.value).to eq('Queen')
     expect(card.rank).to eq(12)
   end
-end
-
-
-card1 = Card.new(:diamond, 'Queen', 12)
-card2 = Card.new(:spade, '3', 3)
-card3 = Card.new(:heart, 'Ace', 14)
-card_array = [card1, card2, card3]
-deck2 = Deck.new(card_array)
-
-RSpec.describe Deck do
-  it 'exists' do
-    deck = Deck.new(card_array)
-
-    expect(deck).to be_an_instance_of(Deck)
-  end
-
-  it 'gets the card rank' do
-    expect(deck2.rank_of_card_at(0)).to eq(12)
-  end
-
-  it 'shows the high ranking cards' do
-    expect(deck2.high_ranking_cards).to eq([card1, card3])
-  end
-
-  it 'calcs percentage of high ranking cards' do
-    expect(deck2.percent_high_ranking).to eq(66.67)
-  end
-
-  it 'removes a card' do
-    test = deck2.remove_card
-    expect(test.rank).to eq(@rank=12)
-    expect(test.suit).to eq(@suit=:diamond)
-    expect(test.value).to eq(@value="Queen")
-  end
-
-  it 'adds a card' do
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    card_array = [card1, card2, card3]
-    deck2 = Deck.new(card_array)
-    card4 = Card.new(:club, '5', 5)
-    deck2.add_card(card4)
-
-    expect(deck2.cards).to eq([card1, card2, card3, card4])
-  end
-
-
-
-
 end
