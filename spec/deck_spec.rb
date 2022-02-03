46c62f013d807fca4f558a27d919d259ce0bf6e9
reuben <creubend@gmail.com>
2022-02-02 21:11:20 -0700

    Start add a card  test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -79,16 +79,20 @@ RSpec.describe Deck do
     # binding.pry
     expect(deck.remove_card).to eq(card1)
   end 
-  it "removes a card" do 
+  
+  
+  it "add a card" do 
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
+    card4 = Card.new(:club, '5', 5)
     
     cards = [card1, card2, card3]
     
     deck = Deck.new(cards)
+    
     # binding.pry
-    expect(deck.remove_card).to eq(card1)
+    expect(deck.add_card).to include?(deck[card4])
   end 
   
   
