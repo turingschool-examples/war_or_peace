68563a46b618e9cd310cd4263cd260c719e547f5
Chris Romero <chrisleeromero1@gmail.com>
2022-02-03 15:11:13 -0700

    Refactor player test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -13,6 +13,14 @@ RSpec.describe Player do
     expect(player).to be_an_instance_of(Player)
   end
 
+  it 'has a name' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+    expect(player.name).to eql('Clarisa')
+  end
 
 
 end
