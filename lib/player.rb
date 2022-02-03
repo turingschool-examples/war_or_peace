56b78a391baa6f846e30db31ccbb9974deb8d6f2
James Harkins <jh.harkins@protonmail.com>
2022-02-02 19:07:35 -0700

    Pass player_spec.rb tests and interaction pattern

--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,19 @@
 class Player
 
+  attr_reader :name,
+              :deck
+
+  def initialize(name, deck)
+    @name = name
+    @deck = deck
+  end
+
+  def has_lost?
+    if deck.cards == []
+      true
+    else
+      false
+    end
+  end
+
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,4 +4,72 @@ require './lib/deck'
 require './lib/player'
 
 RSpec.describe Deck do
+  describe '#initialize' do
+    it 'exists' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+
+      cards = [card1, card2, card3]
+
+      deck = Deck.new(cards)
+
+      player = Player.new('Jimbo', deck)
+      expect(player).to be_an_instance_of(Player)
+    end
+  end
+
+  describe '#initialize' do
+    it 'has a name' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+
+      cards = [card1, card2, card3]
+
+      deck = Deck.new(cards)
+
+      player = Player.new('Jimbo', deck)
+      expect(player.name).to eq('Jimbo')
+    end
+  end
+
+  describe '#initialize' do
+    it 'has a deck that can be analyzed' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+
+      cards = [card1, card2, card3]
+
+      deck = Deck.new(cards)
+
+      player = Player.new('Jimbo', deck)
+      expect(player.deck).to eq(deck)
+    end
+  end
+
+  describe '#has_lost?' do
+    it 'can determine if a player has lost' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+
+      cards = [card1, card2, card3]
+
+      deck = Deck.new(cards)
+
+      player = Player.new('Jimbo', deck)
+      expect(deck.cards).to eq([card1, card2, card3])
+      expect(player.has_lost?).to eq(false)
+
+      player.deck.remove_card
+      player.deck.remove_card
+      player.deck.remove_card
+      require 'pry'; binding.pry
+      expect(deck.cards).to eq([])
+      expect(player.has_lost?).to eq(true)
+    end
+  end
+
 end
