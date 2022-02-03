f4d3e241896c90c2bdb97b53f9cf31816834670a
Andrew Speth <speth.andrew@yahoo.com>
2022-02-03 15:56:39 -0700

    Add test - Turn has two players

---
+++ lib/turn.rb
@@ -0,0 +1,19 @@
+class Turn
+  attr_reader :player1, :player2, :spoils_of_war
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @spoils_of_war = []
+  end
+
+  def type
+    if @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
+      :mutually_assured_destruction
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
+      :war
+    else
+      :basic
+    end
+  end
+end
