a673bb94ce401f2e98ab0167cf5b3962451a35e5
Rin Fabian <pfabianschool@gmail.com>
2022-02-03 00:02:34 -0600

    wrote tests for deck.rb

---
+++ spec/deck_spec.rb
@@ -0,0 +1,73 @@
+require 'rspec'
+require './lib/card'
+require './lib/deck'
+
+RSpec.describe Card do
+  it 'can add cards to a deck' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.cards).to eq([card1, card2, card3])
+  end
+
+  xit 'can check the rank of cards' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.rank_of_card_at(0)).to eq(12)
+    expect(deck.rank_of_card_at(2)).to eq(14)
+  end
+
+  xit 'can detect ranks of 11 or higher' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.high_ranking_cards).to eq([card1, card2])
+  end
+
+  xit 'can detect the percent of high rank cards' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.percent_high_ranking).to eq(66.6)
+  end
+
+  xit 'can add cards to deck' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    card4 = Card.new(:club, '5', 5)
+
+    deck.add_card(card4)
+    expect(deck.cards).to eq([card1, card2, card3, card4])
+    expect(deck.high_ranking_cards).to eq(2)
+    expect(deck.percent_high_ranking).to eq(50)
+  end
+
+  xit 'can remove cards from the deck' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    deck.remove_card
+    expect(deck.cards).to eq([card1, card2])
+    expect(deck.high_ranking_cards).to eq(1)
+    expect(deck.percent_high_ranking).to eq(50)
+  end
+end
