8a2176406346c7150b654046a883a36112c1f237
Alex Psitos <apsitos08@gmail.com>
2022-02-03 16:17:15 -0500

    Add high ranking cards method

--- lib/deck.rb
+++ lib/deck.rb
@@ -7,5 +7,18 @@ class Deck
   end
 
   def rank_of_card_at(rank)
-    @rank_of_card_at = cards.fetch
+    @rank_of_card_at = cards.fetch(rank)
+    @rank_of_card_at.rank
+    #Implied return of specific rank value
+  end
+
+  def high_ranking_cards
+    face_cards = []
+    @cards.each do |card|
+      if card.rank >= 11
+        face_cards << card
+      end
+    end
+    return face_cards
+  end 
 end
