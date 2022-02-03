9084c34c31eabdf2f4242ab4cb4d97d97fb3ae9e
Chris Romero <chrisleeromero1@gmail.com>
2022-02-03 00:16:08 -0700

    Refactor remove card method and update initial deck tests

--- lib/deck.rb
+++ lib/deck.rb
@@ -3,40 +3,42 @@ require 'pry'
 
 class Deck
   attr_reader :cards, :cards_above_eleven
-
+# create the deck object template with an argument
   def initialize(cards)
     @cards = cards
     @cards_above_eleven = []
 
   end
-
+# determine the rank of a card by looking at the index value of the cards array
   def rank_of_card_at(card_at)
-    @cards[card_at]
+    card = @cards[card_at]
+    card.rank
   end
-
+# return the card objects of all the high ranking cards and store them in the cards_above_eleven instance variable
+# Question: this is somehow duplicating the cards, why isn't it stopping at one iteration?
+#  - It seems connected to the incorrect percentage totals I get when 'prying' the interaction pattern.
   def high_ranking_cards
     @cards.each do |card|
-      if card >= 11
+      if card.rank >= 11
          @cards_above_eleven << card
       end
     end
     @cards_above_eleven
-
   end
-
+# calculate the percent of high ranking cards in the deck
   def percent_high_ranking
     high_ranking_cards
-    ((@cards_above_eleven.count.to_f / @cards.count) * 100).round(2)
+    high_cards_float = (@cards_above_eleven.count.to_f / @cards.count)
+    high_cards_float = high_cards_float * 100
+    high_cards_float.round(2)
   end
-
+# remove a card from the first position (index zero) of the cards array
   def remove_card
     @cards.delete_at(0)
     @cards
   end
-
+# add a card to the end of the cards array
   def add_card(card)
-    @cards.push(card.rank)
-    binding.pry
+    @cards.push(card)
   end
-
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,45 +3,59 @@ require './lib/deck'
 
 RSpec.describe Deck do
   it "exists" do
-    deck = Deck.new([1,2,3])
-
+    card1 = Card.new(:club, '5', 5)
+    card2 = Card.new(:heart, '3', 3)
+    deck = Deck.new([card1, card2])
     expect(deck).to be_an_instance_of(Deck)
   end
 
   it "has 'cards' as a readable attribute" do
-    deck = Deck.new([1,2,3])
-
-    expect(deck.cards).to eq([1,2,3])
+    card1 = Card.new(:club, '5', 5)
+    deck = Deck.new([card1])
+    expect(deck.cards).to eq([card1])
   end
 
   it "returns the rank of a card" do
-    deck = Deck.new([1,2,3])
-
-    expect(deck.rank_of_card_at(0)).to eq(1)
+    card1 = Card.new(:club, '5', 5)
+    card2 = Card.new(:heart, '3', 3)
+    deck = Deck.new([card1, card2])
+    expect(deck.rank_of_card_at(0)).to eq(5)
   end
 
   it "returns an array of high ranking cards (11 or higher)" do
-
-    deck = Deck.new([1,3,6,9,12,13,14])
-    expect(deck.high_ranking_cards).to eq([12,13,14])
+    card1 = Card.new(:club, 'Ace', 14)
+    card2 = Card.new(:heart, 'Queen', 12)
+    card3 = Card.new(:club, 'Jack', 11)
+    card4 = Card.new(:heart, '3', 3)
+    deck = Deck.new([card1, card2, card3, card4])
+    expect(deck.high_ranking_cards).to eq([card1, card2, card3])
   end
 
   it "returns the percent of high ranking cards" do
 
-    deck = Deck.new([1,12,15])
+    card1 = Card.new(:club, 'Ace', 14)
+    card2 = Card.new(:heart, 'Queen', 12)
+    card3 = Card.new(:heart, '3', 3)
+    deck = Deck.new([card1, card2, card3])
     expect(deck.percent_high_ranking).to eq(66.67)
   end
 
   it "removes the first card from the deck" do
-
-    deck = Deck.new([1,12,15])
-    expect(deck.remove_card).to eq([12,15])
+    card1 = Card.new(:club, 'Ace', 14)
+    card2 = Card.new(:heart, 'Queen', 12)
+    card3 = Card.new(:club, 'Jack', 11)
+    card4 = Card.new(:heart, '3', 3)
+    deck = Deck.new([card1, card2, card3, card4])
+    expect(deck.remove_card).to eq([card2, card3, card4])
   end
 
   it "adds a card to the end of the deck" do
-    deck = Deck.new([1,12,15])
-    card4 = Card.new(:club, '5', 5)
-    expect(deck.add_card(card4)).to eq([1, 12, 15, 5])
+    card1 = Card.new(:club, 'Ace', 14)
+    card2 = Card.new(:heart, 'Queen', 12)
+    card3 = Card.new(:heart, '3', 3)
+    deck = Deck.new([card1, card2, card3])
+    card4 = Card.new(:heart, 'King', 13)
+    expect(deck.add_card(card4)).to eq([card1, card2, card3, card4])
   end
 
 end
