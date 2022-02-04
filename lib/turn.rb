06933287c0570f39fa7f7ba72600f1ce16572750
Cory Bethune <corybbethune@gmail.com>
2022-02-03 20:49:01 -0700

    Passed 4th turn class test

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,11 +16,14 @@ class Turn
     if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
       return :basic
 
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
+      return :mutually_assured_destruction
+
     elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
       return :war
 
     else
-      return :mutually_assured_destruction
+      
     end
 
 
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -47,5 +47,49 @@ RSpec.describe Turn do
     expect(turn.type).to eq(:basic)
   end
 
+  it "tells the match type of war" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    card_array1 = [card1, card2, card3]
+    deck1 = Deck.new(card_array1)
+    player1 = Player.new('Clarisa', deck1)
+
+    card4 = Card.new(:club, '5', 12)
+    card5 = Card.new(:spade, '10', 10)
+    card6 = Card.new(:heart, 'Jack', 11)
+    card_array2 = [card4, card5, card6]
+    deck2 = Deck.new(card_array2)
+    player2 = Player.new('Cory', deck2)
+
+    turn = Turn.new(player1, player2)
+
+    expect(turn.type).to eq(:war)
+  end
+
+  it "tells the match type of mutually_assured_destruction" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    card_array1 = [card1, card2, card3]
+    deck1 = Deck.new(card_array1)
+    player1 = Player.new('Clarisa', deck1)
+
+    card4 = Card.new(:club, '5', 12)
+    card5 = Card.new(:spade, '10', 10)
+    card6 = Card.new(:heart, 'Jack', 14)
+    card_array2 = [card4, card5, card6]
+    deck2 = Deck.new(card_array2)
+    player2 = Player.new('Cory', deck2)
+
+    turn = Turn.new(player1, player2)
+
+    expect(turn.type).to eq(:mutually_assured_destruction)
+  end
+
+  it  do
+  end
+
+
 
 end
