a72e11ca106ca1f0bc6d9fd17beaa057369cd526
reuben <creubend@gmail.com>
2022-02-04 08:17:13 -0700

    Create type method

---
+++ lib/turn.rb
@@ -0,0 +1,34 @@
+require 'pry'
+require 'rspec'
+require './lib/deck.rb'
+require './lib/card.rb'
+require './lib/player.rb'
+
+
+class Turn
+  attr_reader :player1, :player2, :spoils_of_war
+  
+  
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @spoils_of_war = []
+  end
+  
+  def type
+    if 
+      (player1.deck.rank_of_card_at(0)) == (player2.deck.rank_of_card_at(0))
+      :war
+    elsif 
+      player1.rank_of_card_at(0) == player2.rank_of_card_at(0) &&
+      player1.rank_of_card_at(2) == player2.rank_of_card_at(2)
+      :mutually_assured_destruction
+    else
+      :basic
+    end
+  end
+  
+
+  
+  
+end
