5d544b9a9e5253f25245a191118fa95d11e2d35c
reuben <creubend@gmail.com>
2022-02-04 12:12:49 -0700

    Add tests for war and MAD scenerios

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,12 +17,12 @@ class Turn
   
   def type
     if 
-      (player1.deck.rank_of_card_at(0)) == (player2.deck.rank_of_card_at(0))
-      :war
-    elsif 
       player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) &&
       player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
       :mutually_assured_destruction
+    elsif 
+      (player1.deck.rank_of_card_at(0)) == (player2.deck.rank_of_card_at(0))
+      :war
     else
       :basic
     end
@@ -44,7 +44,7 @@ class Turn
         player2
       end 
     else type == :mutually_assured_destruction
-      no_winner
+      "KaBoom!!!!"
     end  
         
     
