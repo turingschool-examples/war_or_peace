9b979539f664eca63a546755d57aea7f3e9d7739
Cory Bethune <corybbethune@gmail.com>
2022-02-03 20:36:40 -0700

    Passed 2nd turn class test

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,12 +5,27 @@ require 'pry'
 
 class Turn
 
-  attr_reader :player2, :player2
+  attr_reader :player1, :player2, :spoils_of_war
 
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
   end
+
+  def type
+    if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
+      return :basic
+
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
+      return :war
+
+    else
+      return :mutually_assured_destruction
+    end
+
+
+  end
+
 
 
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -22,10 +22,30 @@ RSpec.describe Turn do
   deck2 = Deck.new(card_array2)
   player2 = Player.new('Cory', deck2)
 
-  expect(turn1 = Turn.new(player1, player2)).to be_an_instance_of(Turn)
+  turn = Turn.new(player1, player2)
+
+  expect(turn).to be_an_instance_of(Turn)
   end
 
+  it "tells the match type of basic" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    card_array1 = [card1, card2, card3]
+    deck1 = Deck.new(card_array1)
+    player1 = Player.new('Clarisa', deck1)
 
+    card4 = Card.new(:club, '5', 5)
+    card5 = Card.new(:spade, '10', 10)
+    card6 = Card.new(:heart, 'Jack', 11)
+    card_array2 = [card4, card5, card6]
+    deck2 = Deck.new(card_array2)
+    player2 = Player.new('Cory', deck2)
+
+    turn = Turn.new(player1, player2)
+
+    expect(turn.type).to eq(:basic)
+  end
 
 
 end
