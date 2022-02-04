4fe96f7f8aa3e26a6ce713ffd18d85691772b924
Alex Psitos <apsitos08@gmail.com>
2022-02-04 14:04:13 -0500

    write turn type method and specs

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,23 @@
 require './lib/card'
 require './lib/deck'
 require './lib/player'
+
+class Turn
+  attr_reader :player1, :player2, :spoils_of_war
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @spoils_of_war = []
+  end
+
+def type
+  if @player1.deck.cards[0].rank !=
+    @player2.deck.cards[0].rank
+    :basic
+  elsif @player1.deck.cards[2].rank !=
+    @player2.deck.cards[2].rank
+    :war
+  else
+    :mutually_assured_destruction
+  end
+end 
