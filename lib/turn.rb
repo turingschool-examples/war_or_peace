ad48aadf1a1358fef93dbd64df68075502cfd7ec
Will Medders <wmedders@gmail.com>
2022-02-03 23:20:45 -0500

    Turn can declare a winner

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,15 +4,24 @@ class Turn
     @player1 = player1
     @player2 = player2
     @spoils_of_war = []
+
   end
 
   def type
-    if @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
-      return :mutally_assured_destruction
-    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
-     return :war
+    if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+      :mutally_assured_destruction
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+     :war
    else
      :basic
-   end
+    end
+  end
+
+  def winner
+    if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0) == true
+      return player1
+    elsif player1.deck.rank_of_card_at(0) < player2.deck.rank_of_card_at(0) == true
+      return player2
+    end
   end
 end
