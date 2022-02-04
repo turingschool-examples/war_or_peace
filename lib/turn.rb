b9f55d1cdc5df345202b7a5f210cdfa6e4976860
Cory Bethune <corybbethune@gmail.com>
2022-02-03 19:38:24 -0700

    Passed 1st turn class test

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,11 +1,16 @@
 require './lib/deck'
 require './lib/card'
 require './lib/player'
-require 
+require 'pry'
 
 class Turn
 
+  attr_reader :player2, :player2
 
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+  end
 
 
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -2,3 +2,30 @@ require 'rspec'
 require './lib/deck'
 require './lib/card'
 require './lib/player'
+require './lib/turn'
+require 'pry'
+
+
+RSpec.describe Turn do
+  it 'exists' do
+  card1 = Card.new(:diamond, 'Queen', 12)
+  card2 = Card.new(:spade, '3', 3)
+  card3 = Card.new(:heart, 'Ace', 14)
+  card_array1 = [card1, card2, card3]
+  deck1 = Deck.new(card_array1)
+  player1 = Player.new('Clarisa', deck1)
+
+  card4 = Card.new(:club, '5', 5)
+  card5 = Card.new(:spade, '10', 10)
+  card6 = Card.new(:heart, 'Jack', 11)
+  card_array2 = [card4, card5, card6]
+  deck2 = Deck.new(card_array2)
+  player2 = Player.new('Cory', deck2)
+
+  expect(turn1 = Turn.new(player1, player2)).to be_an_instance_of(Turn)
+  end
+
+
+
+
+end
