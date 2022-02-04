909ace1b1c4fd9c17dbba83dd5f88328f6314e44
Joseph Senecal <jasenecal2@gmail.com>
2022-02-04 09:27:07 -0700

    Add winner method to Turn class

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,15 +3,14 @@ class Turn
   def initialize(player1, player2)
     #do stuff[
     @spoils_of_war = []
-    @type = :basic
     @player1 = player1
     @player2 = player2
   end
 
   def type()
-    if player1.deck.cards[0].rank != player2.deck.cards[0].rank
+    if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
       @type = :basic
-    elsif player1.deck.cards[0].rank == player2.deck.cards[0].rank && player1.deck.cards[2].rank == player2.deck.cards[2].rank
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
       @type = :mutually_assured_destruction
     else
       @type = :war
@@ -19,7 +18,21 @@ class Turn
   end
 
   def winner ()
-    #determines who won the turn
+    if type() == :basic
+      if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
+        return @player1
+      else
+        return @player2
+      end
+    elsif type() == :war
+      if @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
+        return @player1
+      else
+        return @player2
+      end
+    else
+      return "No Winner"
+    end
   end
 
   def pile_cards ()
