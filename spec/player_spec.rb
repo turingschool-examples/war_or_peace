a42be236901c46251344deefcaaa686df953ac05
Adrian Campos <adriancampos2992@yahoo.com>
2022-02-03 19:10:59 -0800

    player deck test passes

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -21,6 +21,18 @@ RSpec.describe Player do
   expect(player.name).to eq("Clarisa")
   end
 
+  it 'player deck exists' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+  expect(player.deck).to eq(deck)
+  require "pry"; binding.pry
+  end
+
+
+
 
 
 
