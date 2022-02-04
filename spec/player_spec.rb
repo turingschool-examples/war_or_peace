f94535ea982534e2471dd728dc1850dba57f7f27
Alex Psitos <apsitos08@gmail.com>
2022-02-04 12:29:15 -0500

    Finish player spec

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -53,4 +53,24 @@ RSpec.describe Player do
     player = Player.new('Clarisa', deck)
     expect(player.has_lost?).to eq(false)
    end
+
+   it "can remove a card from the deck" do
+     card1 = Card.new(:diamond, 'Queen', 12)
+     card2 = Card.new(:spade, '3', 3)
+     card3 = Card.new(:heart, 'Ace', 14)
+
+     cards = [card1, card2, card3]
+     deck = Deck.new(cards)
+
+     player = Player.new('Clarisa', deck)
+     player.deck.remove_card
+     expect(player.has_lost?).to eq(false)
+     player.deck.remove_card
+     expect(player.has_lost?).to eq(false)
+     player.deck.remove_card
+     expect(player.has_lost?).to eq(true)
+
+   end
+
+
 end
