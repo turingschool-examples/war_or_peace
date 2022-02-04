c76631abeb0e61f467ec1c2cff57504d720fb02a
Cory Bethune <corybbethune@gmail.com>
2022-02-03 17:43:22 -0700

    Passed 2nd player class test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -15,11 +15,21 @@ RSpec.describe Player do
     deck = Deck.new(card_array)
 
 
-    player = Player.new('Cory', deck)
+    player = Player.new('Clarisa', deck)
     expect(player).to be_an_instance_of(Player)
   end
 
+  it 'gets the name attribute' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    card_array = [card1, card2, card3]
+    deck = Deck.new(card_array)
 
+
+    player = Player.new('Clarisa', deck)
+    expect(player.name).to eq('Clarisa')
+  end
 
 
 
