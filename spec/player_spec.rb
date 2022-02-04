f6f5e4c36915c5c4ffb7421854195dbf7c43aa49
Cory Bethune <corybbethune@gmail.com>
2022-02-03 17:46:54 -0700

    Passed 3rd player class test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -31,7 +31,17 @@ RSpec.describe Player do
     expect(player.name).to eq('Clarisa')
   end
 
+  it 'gets the deck attribute' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    card_array = [card1, card2, card3]
+    deck = Deck.new(card_array)
 
+
+    player = Player.new('Clarisa', deck)
+    expect(player.deck).to eq(deck)
+  end
 
 
 end
