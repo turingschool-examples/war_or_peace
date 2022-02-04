5f19e22c573beff138e5a78d098f096bc8ceea6e
Brylan Gannon <brylan.gannon112@gmail.com>
2022-02-03 21:13:57 -0700

    Add high_ranking_cards method

--- lib/deck.rb
+++ lib/deck.rb
@@ -11,4 +11,14 @@ class Deck
     return cards[index].rank
   end
 
+  def high_ranking_cards
+    high_ranks = []
+
+    cards.each do |card|
+      high_ranks << card if card.rank >= 11
+    end
+
+    return high_ranks
+  end
+
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -30,4 +30,18 @@ describe Deck do
       expect(deck.rank_of_card_at(0)).to eq(12)
     end
   end
+
+  describe 'high_ranking_cards' do
+    it 'returns an array of high rank cards' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+
+      cards = [card1, card2, card3]
+
+      deck = Deck.new(cards)
+
+      expect(deck.high_ranking_cards).to eq([card1, card3])
+    end
+  end
 end
