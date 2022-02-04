3c21126c742bd06afafea0d9498aa0b8c21b8c6b
Drew Proebstel <d.proebstel@gmail.com>
2022-02-04 10:15:31 -0800

    add itteration 1

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,18 +1,43 @@
 require 'pry'
-
 class Deck
+
+
 
   attr_reader :cards
 
-  def initialize(cards)
-    @cards = []
+  def initialize(cards = [])
+    @cards = cards
   end
 
-  #def cards
-  #  @cards = card
-  #end
 
   def rank_of_card_at(location)
-    return cards(location)
+    return cards[location].rank
   end
+
+  def high_ranking_cards
+    high_ranking_card_array = []
+    cards.each do |card|
+
+      if card.rank > 10
+
+        high_ranking_card_array.push(card)
+      end
+
+    end
+    high_ranking_card_array
+  end
+
+
+  def percent_high_ranking
+    (high_ranking_cards.length.to_f / cards.length.to_f).round(4) * 100
+  end
+
+  def remove_card
+    @remove_card = cards.shift
+  end
+
+  def add_card(card)
+    @cards << card
+  end
+
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,2 +1,48 @@
 require 'rspec'
 require './lib/deck'
+require './lib/card'
+require 'pry'
+
+RSpec.describe Deck do
+  it "exists" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    expect(deck).to be_an_instance_of(Deck)
+    expect(deck.cards).to eq cards
+    expect(deck.high_ranking_cards).to eq [card1, card3]
+  end
+
+  it "percent" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    expect(deck.percent_high_ranking).to eq(66.67)
+  end
+
+  it "remove" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    deck.remove_card
+    expect(deck.cards).to eq [card2, card3]
+  end
+
+  it "add_card" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    card4 = Card.new(:club, '5', 5)
+    deck.add_card(card4)
+    expect(deck.cards).to eq [card1, card2, card3, card4]
+  end
+
+end
