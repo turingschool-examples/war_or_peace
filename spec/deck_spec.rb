be046a95d58253f2a90b40df71fae22792af4ee6
Brylan Gannon <brylan.gannon112@gmail.com>
2022-02-03 21:09:32 -0700

    Add rank_of_card_at method

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -16,4 +16,18 @@ describe Deck do
       expect(deck.cards).to eq([card1, card2, card3])
     end
   end
+
+  describe '#rank_of_card_at' do
+    it 'returns the rank of a card at the given index' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+
+      cards = [card1, card2, card3]
+
+      deck = Deck.new(cards)
+
+      expect(deck.rank_of_card_at(0)).to eq(12)
+    end
+  end
 end
