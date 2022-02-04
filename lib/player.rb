26442679c5c61bd99d591e7f761eb9ecc1117e53
Cory Bethune <corybbethune@gmail.com>
2022-02-03 17:37:11 -0700

    Passed 1st player test

---
+++ lib/player.rb
@@ -0,0 +1,12 @@
+class Player
+attr_reader :name, :deck
+
+  def initialize(player_name, deck)
+    @name = player_name
+    @deck = deck
+  end
+
+
+
+
+end
---
+++ spec/player_spec.rb
@@ -0,0 +1,27 @@
+require 'rspec'
+require './lib/deck'
+require './lib/card'
+require './lib/player'
+
+
+
+
+RSpec.describe Player do
+  it 'exists' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    card_array = [card1, card2, card3]
+    deck = Deck.new(card_array)
+
+
+    player = Player.new('Cory', deck)
+    expect(player).to be_an_instance_of(Player)
+  end
+
+
+
+
+
+
+end
