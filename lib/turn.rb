1a50f8ea0fcaf0ac308d56136717c290f3b2b7f5
reuben <creubend@gmail.com>
2022-02-04 09:05:56 -0700

    Add test and method winner

--- lib/turn.rb
+++ lib/turn.rb
@@ -28,6 +28,25 @@ class Turn
     end
   end
   
+  def winner 
+    if type == :basic 
+      if(player1.deck.rank_of_card_at(0)) > (player2.deck.rank_of_card_at(0))
+        player1
+      else 
+        player2
+      end 
+      
+      
+    else type == :war
+      if(player1.deck.rank_of_card_at(2)) > (player2.deck.rank_of_card_at(2))
+        player1
+      else 
+        player2
+      end 
+    end  
+        
+    
+  end
 
   
   
