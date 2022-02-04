64bb490c2cce16c5e1de30b780806b982e068758
Joseph Senecal <jasenecal2@gmail.com>
2022-02-04 10:55:04 -0700

    Add tests for award_spoils Turn method

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -226,9 +226,84 @@ RSpec.describe 'exists' do
 
       turn = Turn.new(player1, player2)
       turn.pile_cards
+
       expect(turn.spoils_of_war).to eq([])
       expect(turn.player1.deck.cards).to eq([card8])
       expect(turn.player2.deck.cards).to eq([card7])
+    end
+  end
+
+  describe "has a method 'award_spoils' to add each card in the spoils_of_war pile to the winner's hand" do
+    it 'awards for :basic turn type' do
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:heart, '9', 9)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, 'Queen', 12)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card3, card4, card6, card7])
+
+      player1 = Player.new("Megan", deck1)
+      player2 = Player.new("Aurora", deck2)
+
+      turn = Turn.new(player1, player2)
+      turn.pile_cards
+      turn.award_spoils
+
+      expect(player1.deck.cards).to eq([card2, card5, card8, card1, card3])
+      expect(player2.deck.cards).to eq([card4, card6, card7])
+    end
+
+    it 'awards for :war type turn' do
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:heart, '9', 9)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, 'Queen', 12)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card4, card3, card6, card7]) #sneaky.... very sneaky
+
+      player1 = Player.new("Megan", deck1)
+      player2 = Player.new("Aurora", deck2)
+
+      turn = Turn.new(player1, player2)
+      turn.pile_cards
+      turn.award_spoils
+
+      expect(player1.deck.cards).to eq([card8])
+      expect(player2.deck.cards).to eq([card7, card1, card2, card5, card4, card3, card6])
+    end
+
+    it 'awards no cards for MAD' do
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:heart, '9', 9)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, '8', 8)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card4, card3, card6, card7])
+
+      player1 = Player.new("Megan", deck1)
+      player2 = Player.new("Aurora", deck2)
+
+      turn = Turn.new(player1, player2)
+      turn.pile_cards
+      turn.award_spoils
+
+      expect(player1.deck.cards).to eq([card8])
+      expect(player2.deck.cards).to eq([card7])
     end
   end
 end
