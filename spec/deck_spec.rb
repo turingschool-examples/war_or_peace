9972d858a959130e2a2759fb1734c8536bcbed9a
Joseph Downs <jos.w.downs@gmail.com>
2022-02-04 09:57:29 -0700

    Add player.rb

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -18,11 +18,18 @@ RSpec.describe Deck do
     expect(deck.rank_of_card_at(2)).to eq(14)
     expect(deck.cards).to eq(cards)
     expect(deck.high_ranking_cards).to eq([card1, card3])
-    expect(deck.percent_hicgh_ranking).to eq(66.67)
+    expect(deck.percent_high_ranking).to eq(66.67)
     expect(deck.remove_card).to eq(card1)
-    # expect(deck.cards).to eq([card2, card3])
-    # expect(deck.high_ranking_cards).to eq(card3)
-    # expect(deck.percent_high_ranking). to eq(1/2)
+    expect(deck.cards).to eq([card2, card3])
+    expect(deck.high_ranking_cards).to eq([card3])
+    expect(deck.percent_high_ranking).to eq(50.00)
+
+    card4 = Card.new(:club, "5", 5)
+
+    expect(deck.add_card(card4)).to eq([card2, card3, card4])
+    expect(deck.cards).to eq([card2, card3, card4])
+    expect(deck.high_ranking_cards).to eq([card3])
+    expect(deck.percent_high_ranking).to eq(33.33)
   end
 
 end
---
+++ spec/turn_spec.rb
@@ -0,0 +1,5 @@
+require "rspec"
+require "./lib/card"
+require "./lib/deck"
+require "./lib/player"
+require "./lib/turn"
