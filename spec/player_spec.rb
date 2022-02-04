ba58176648141b11cfe4c311b09c1ef49b63e3d8
Adrian Campos <adriancampos2992@yahoo.com>
2022-02-03 19:08:10 -0800

    add player name test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,13 +3,29 @@ require './lib/deck'
 require './lib/player'
 
 RSpec.describe Player do
-  it 'player class exists' do 
+  it 'player class exists' do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
     deck = Deck.new([card1, card2, card3])
     player = Player.new('Clarisa', deck)
   expect(player).to be_an_instance_of(Player)
-
   end
+
+  it 'player name exists' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+  expect(player.name).to eq("Clarisa")
+  end
+
+
+
+
+
+
+
+
 end
