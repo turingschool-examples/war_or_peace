28bc10a0b6a9343ad60904a84484e29d57059303
Sandisz Thieme <sandiszthieme@gmail.com>
2022-02-04 11:33:12 -0700

    Add ability for user to start the game

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -7,6 +7,14 @@ require 'pry'
 
 
 go = Gameplay.new
+puts "Welcome to War! (or Peace) This game will be played with 52 cards."
+puts "The players today are Morgan and Aurora."
+puts "Type 'GO' to starrt the game!"
+puts "------------------------------------------------------------------"
 
-go.start
-# puts player2.deck
+ready = gets.chomp
+if ready == 'go' || ready == 'GO' || ready == 'Go'
+  go.start
+else
+  puts "You can't just enter #{ready} and expect something to happen."
+end
