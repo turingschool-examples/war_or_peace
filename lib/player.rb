5a9a9f62164dc10786410372d773e08e0aea87d2
Phillip Kamps <phillip.kamps@gmail.com>
2022-02-03 18:18:55 -0700

    Add player class

---
+++ lib/player.rb
@@ -0,0 +1,6 @@
+class Player
+  def initialize(name, deck)
+    @name = name
+  end
+
+end
---
+++ spec/player_spec.rb
@@ -0,0 +1,18 @@
+require 'rspec'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+
+card1 = Card.new(:diamond, 'Queen', 12)
+card2 = Card.new(:spade, '3', 3)
+card3 = Card.new(:heart, 'Ace', 14)
+cards = [card1, card2, card3]
+deck = Deck.new(cards)
+player = Player.new('Clarisa', deck)
+
+RSpec.describe Player do
+  it "creates new player" do
+    expect(player).to be_an_instance_of(Player)
+  end
+  
+end
