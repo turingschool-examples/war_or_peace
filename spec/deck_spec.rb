4446b9b79321d1195b88c06a3a8345ddb677d888
Luke Pascale <lpluke5@gmail.com>
2022-02-05 15:26:02 -0700

    reformat: Change order of testing for deck class.

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -39,18 +39,6 @@ RSpec.describe Deck do
       expect(deck.percent_high_ranking).to eq(66.67)
     end
 
-    it 'can add cards to a deck' do
-      card1 = Card.new(:diamond, 'Queen', 12)
-      card2 = Card.new(:spade, '3', 3)
-      card3 = Card.new(:heart, 'Ace', 14)
-      cards = [card1, card2, card3]
-      deck = Deck.new(cards)
-      card4 = Card.new(:club, '5', 5)
-      deck.add_card(card4)
-
-      expect(deck.cards).to eq([card1, card2, card3, card4])
-    end
-
     it 'can remove cards' do
       card1 = Card.new(:diamond, 'Queen', 12)
       card2 = Card.new(:spade, '3', 3)
@@ -62,6 +50,17 @@ RSpec.describe Deck do
       expect(deck.cards).to eq([card2, card3])
       expect(deck.high_ranking_cards).to eq([card3])
       expect(deck.percent_high_ranking).to eq(50.0)
+    end
+
+    it 'can add cards to a deck' do
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card2, card3]
+      deck = Deck.new(cards)
+      card4 = Card.new(:club, '5', 5)
+      deck.add_card(card4)
+
+      expect(deck.cards).to eq([card2, card3, card4])
     end
   end
 end
