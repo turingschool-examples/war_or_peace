64f6825bd0c649d239f2a3228b815079b991758e
Brylan Gannon <brylan.gannon112@gmail.com>
2022-02-05 10:07:46 -0700

    Complete iteration 4

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,12 +1,15 @@
 require './lib/card'
+require 'pry'
 
 class CardGenerator
   attr_accessor :cards
+  
   def initialize(filename)
     @cards = []
     card_count = 0
-    File.foreach(filename) do |line|
-      card_count += 1
-      Card.new(line(0).to_sym, line(1).to_s, line(2))
   end
+
 end
+
+
+binding.pry
