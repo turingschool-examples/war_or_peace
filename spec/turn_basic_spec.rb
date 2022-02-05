d529d4eb959e996eba724c1d641c70b00d4b1465
Alex Psitos <apsitos08@gmail.com>
2022-02-05 10:51:14 -0500

    Add MAD type turn to turn spec

--- spec/turn_basic_spec.rb
+++ spec/turn_basic_spec.rb
@@ -74,6 +74,34 @@ RSpec.describe Turn do
     it 'awards spoils to the winner' do
       expect(turn.winner.deck.cards).to eq(player2.deck.cards)
     end
+  end
 
+  context "mutually_assured_destruction" do
+    let(:card1) {Card.new(:heart, 'Jack', 11)}
+    let(:card2) {Card.new(:heart, '10', 10)}
+    let(:card3) {Card.new(:heart, '9', 9)}
+    let(:card4) {Card.new(:diamond, 'Jack', 11)}
+    let(:card5) {Card.new(:heart, '8', 8)}
+    let(:card6) {Card.new(:diamond, '8', 8)}
+    let(:card7) {Card.new(:heart, '3', 3)}
+    let(:card8) {Card.new(:diamond, '2', 2)}
+    let(:deck1) {Deck.new([card1, card2, card5, card8])}
+    let(:deck2) {Deck.new([card4, card3, card6, card7])}
+    let(:player1) {Player.new("Megan", deck1)}
+    let(:player2) {Player.new("Aurora", deck2)}
+    let(:spoils_of_war) {[]}
+    let(:turn) {Turn.new(player1, player2)}
+
+    it 'returns the appropriate Turn type' do
+      expect(turn.type).to eq(:mutually_assured_destruction)
+    end
+
+    it "determines the winner" do
+      expect(turn.winner).to eq("No Winner")
+    end
+
+    it "piles the used cards" do
+      expect(turn.spoils_of_war).to eq(spoils_of_war)
+    end
   end
 end
