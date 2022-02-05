567f350ed8451124d6dc0168b446583e513bc19b
Luke Pascale <lpluke5@gmail.com>
2022-02-05 01:02:53 -0700

    feat: Add rank_of_card_at method.

--- lib/deck.rb
+++ lib/deck.rb
@@ -3,4 +3,8 @@ class Deck
   def initialize(cards)
     @cards = cards
   end
+
+  def rank_of_card_at(location)
+    cards[location].rank
+  end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,4 +1,5 @@
 require 'rspec'
+require 'pry'
 require './lib/card'
 require './lib/deck'
 
@@ -12,6 +13,17 @@ RSpec.describe Deck do
       deck = Deck.new(cards)
 
       expect(deck).to be_a(Deck)
+      expect(deck.cards).to eq([card1, card2, card3])
+    end
+
+    it 'can read specific card ranks' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+
+      expect(deck.rank_of_card_at(0)).to eq(12)
     end
   end
 end
