42da2574ab9bffcbd3a2b085da815c6ef093e694
Joseph Senecal <jasenecal2@gmail.com>
2022-02-06 14:09:05 -0700

    Increase input tolerances

--- runner.rb
+++ runner.rb
@@ -2,6 +2,7 @@ require './lib/card.rb'
 require './lib/deck.rb'
 require './lib/player.rb'
 require './lib/turn.rb'
+require './lib/card_generator.rb'
 require 'pry'
 
 #Initialization - Setup up two players with two *RANDOM* decks of 26 cards.
@@ -14,8 +15,9 @@ require 'pry'
 
 
 def lets_play()
-  full_deck = [Card.new(:heart, '2', 2), Card.new(:heart, '3', 3), Card.new(:heart, '4', 4), Card.new(:heart, '5', 5), Card.new(:heart, '6', 6), Card.new(:heart, '7', 7), Card.new(:heart, '8', 8), Card.new(:heart, '9', 9), Card.new(:heart, '10', 10), Card.new(:heart, 'Jack', 11), Card.new(:heart, 'Queen', 12), Card.new(:heart, 'King', 13), Card.new(:heart, 'Ace', 14), Card.new(:diamond, '2', 2), Card.new(:diamond, '3', 3), Card.new(:diamond, '4', 4), Card.new(:diamond, '5', 5), Card.new(:diamond, '6', 6), Card.new(:diamond, '7', 7), Card.new(:diamond, '8', 8), Card.new(:heart, '9', 9), Card.new(:diamond, '10', 10), Card.new(:diamond, 'Jack', 11), Card.new(:diamond, 'Queen', 12), Card.new(:diamond, 'King', 13), Card.new(:diamond, 'Ace', 14), Card.new(:club, '2', 2), Card.new(:club, '3', 3), Card.new(:club, '4', 4), Card.new(:club, '5', 5), Card.new(:club, '6', 6), Card.new(:club, '7', 7), Card.new(:club, '8', 8), Card.new(:club, '9', 9), Card.new(:club, '10', 10), Card.new(:club, 'Jack', 11), Card.new(:club, 'Queen', 12), Card.new(:club, 'King', 13), Card.new(:club, 'Ace', 14), Card.new(:spade, '2', 2), Card.new(:spade, '3', 3), Card.new(:spade, '4', 4), Card.new(:spade, '5', 5), Card.new(:spade, '6', 6), Card.new(:spade, '7', 7), Card.new(:spade, '8', 8), Card.new(:spade, '9', 9), Card.new(:spade, '10', 10), Card.new(:spade, 'Jack', 11), Card.new(:spade, 'Queen', 12), Card.new(:spade, 'King', 13), Card.new(:spade, 'Ace', 14),]
+  #full_deck = [Card.new(:heart, '2', 2), Card.new(:heart, '3', 3), Card.new(:heart, '4', 4), Card.new(:heart, '5', 5), Card.new(:heart, '6', 6), Card.new(:heart, '7', 7), Card.new(:heart, '8', 8), Card.new(:heart, '9', 9), Card.new(:heart, '10', 10), Card.new(:heart, 'Jack', 11), Card.new(:heart, 'Queen', 12), Card.new(:heart, 'King', 13), Card.new(:heart, 'Ace', 14), Card.new(:diamond, '2', 2), Card.new(:diamond, '3', 3), Card.new(:diamond, '4', 4), Card.new(:diamond, '5', 5), Card.new(:diamond, '6', 6), Card.new(:diamond, '7', 7), Card.new(:diamond, '8', 8), Card.new(:heart, '9', 9), Card.new(:diamond, '10', 10), Card.new(:diamond, 'Jack', 11), Card.new(:diamond, 'Queen', 12), Card.new(:diamond, 'King', 13), Card.new(:diamond, 'Ace', 14), Card.new(:club, '2', 2), Card.new(:club, '3', 3), Card.new(:club, '4', 4), Card.new(:club, '5', 5), Card.new(:club, '6', 6), Card.new(:club, '7', 7), Card.new(:club, '8', 8), Card.new(:club, '9', 9), Card.new(:club, '10', 10), Card.new(:club, 'Jack', 11), Card.new(:club, 'Queen', 12), Card.new(:club, 'King', 13), Card.new(:club, 'Ace', 14), Card.new(:spade, '2', 2), Card.new(:spade, '3', 3), Card.new(:spade, '4', 4), Card.new(:spade, '5', 5), Card.new(:spade, '6', 6), Card.new(:spade, '7', 7), Card.new(:spade, '8', 8), Card.new(:spade, '9', 9), Card.new(:spade, '10', 10), Card.new(:spade, 'Jack', 11), Card.new(:spade, 'Queen', 12), Card.new(:spade, 'King', 13), Card.new(:spade, 'Ace', 14),]
 
+  full_deck = get_cards
   shuffled_deck = full_deck.shuffle #creates a randomly shuffled array of card objets
   player1_deck = Deck.new(shuffled_deck[0..25]) #player1 takes top half
   player2_deck = Deck.new(shuffled_deck[26..51]) #player2 takes bottom half
@@ -44,25 +46,25 @@ def lets_play()
     player1 = turn.player1
     player2 = turn.player2
     # binding.pry
-    p "Megan has #{player1.deck.cards.length} cards left"
-    p "Aurora has #{player2.deck.cards.length} cards left"
+    #p "Megan has #{player1.deck.cards.length} cards left"
+    #p "Aurora has #{player2.deck.cards.length} cards left"
   end
   return "DRAW".center(5, "-")
+  # binding.pry
 end
 #YEAH FLOW CONTROL
 p "Welcome to War! (or Peace) This game will be played with 52 cards."
 p "The players today are Megan and Aurora."
 p "Type 'GO' to start the game!" #I'm sensing a GO method...
-
 input = gets.chomp
 loop do
-  if input == "GO"
+  if input == "GO" || "go" || "Go" || 'gO'
     p lets_play()
     break
   elsif input == "exit"
     break
   else
-    p "Invalid input"
+    p "Sorry, I didn't understand. Type 'GO' to continue, or 'exit' to leave the game"
     input = gets.chomp
   end
 end
