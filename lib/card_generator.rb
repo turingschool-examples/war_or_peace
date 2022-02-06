927e6c6b97cc0d092af336eba0fc25b9236ae9db
Joseph Senecal <jasenecal2@gmail.com>
2022-02-06 10:46:22 -0700

    Add code comments

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -2,10 +2,10 @@ require './lib/card.rb'
 require 'pry'
 
 cards_for_deck = File.read("lib/cards.txt") #creates a string object of card info separated by /n new lines.
-cards_for_deck = cards_for_deck.lines
-element = 0
-cards_for_deck.each do |line|
-  cards_for_deck[element] = line.strip
-  element += 1
+cards_for_deck = cards_for_deck.lines #partitions string into array elements, 1 element for each line in the file
+element = 0 #initialize counter to access current element in cards_for_deck
+cards_for_deck.each do |line| #for each element,
+  cards_for_deck[element] = line.chomp #chomp the new line character
+  element += 1 #increment counter
 end
-p cards_for_deck
+# p cards_for_deck #double check
