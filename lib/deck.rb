f4c0ef676fa8b1eb886c5e3ae85badbf76ac83cc
phdd0 <paul.haddick@outlook.com>
2022-02-06 15:27:31 -0700

    Provide Deck attr_reader and instantiate first test deck with card array

--- lib/deck.rb
+++ lib/deck.rb
@@ -5,3 +5,13 @@ card2 = Card.new(:spade, '3', 3)
 card3 = Card.new(:heart, 'Ace', 14)
 
 cards = [ card1, card2, card3 ]
+
+    class Deck
+      attr_reader :cards
+      def initialize(c)
+        @cards = c
+      end
+    end
+
+
+deck = Deck.new(cards)
