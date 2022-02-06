9b0da15623f0fa0655520f4d3197c2d753b54338
Andrew Speth <speth.andrew@yahoo.com>
2022-02-05 17:18:31 -0700

    Remove commented/unused code, fix formatting/indenting

--- lib/war_or_peace_runner.rb
+++ lib/war_or_peace_runner.rb
@@ -54,55 +54,25 @@ end
 turn_count = 0
 
 loop do
+  turn_count += 1
+  turn.pile_cards
 
-
-
-  # if player1.has_lost? || player2.has_lost?
-
-  # else
-
-    turn_count += 1
-    turn.pile_cards
-    # require 'pry'; binding.pry
-    # break if turn_count > 1000000
-    # break if player1.has_lost? || player2.has_lost?
-
-    # if turn.type == :player1_loses || turn.type == :player2_loses
-    #   require 'pry'; binding.pry
-    #   puts "*~*~*~* #{turn.winner.name} has won the game! *~*~*~*"
-    # elsif turn.type == :draw || turn_count > 1000000
-    #   puts "---- DRAW ----"
-
-
-    if turn.type == :basic
-      puts "Turn #{turn_count}: #{turn.winner.name} won #{turn.spoils_of_war.count} cards"
-    elsif turn.type == :war
-      puts "Turn #{turn_count}: WAR - #{turn.winner.name} won #{turn.spoils_of_war.count} cards"
-    elsif turn.type == :mutually_assured_destruction
-      puts "Turn #{turn_count}: *mutually assured destruction* 6 cards removed from play"
-    elsif turn.type == :player1_loses
-      # player1.has_lost? == true
-      player1.deck.cards.clear
-      puts "*~*~*~* #{turn.winner.name} has won the game! *~*~*~*"
-    elsif turn.type == :player2_loses
-      # player2.has_lost? == true
-      player2.deck.cards.clear
-      puts "*~*~*~* #{turn.winner.name} has won the game! *~*~*~*"
-    elsif turn.type == :draw || turn_count > 1000000
-      puts "---- DRAW ----"
-    end
-
-    break if player1.has_lost? || player2.has_lost? || turn.type == :draw || turn_count > 1000000
-    turn.award_spoils(turn.winner)
+  if turn.type == :basic
+    puts "Turn #{turn_count}: #{turn.winner.name} won #{turn.spoils_of_war.count} cards"
+  elsif turn.type == :war
+    puts "Turn #{turn_count}: WAR - #{turn.winner.name} won #{turn.spoils_of_war.count} cards"
+  elsif turn.type == :mutually_assured_destruction
+    puts "Turn #{turn_count}: *mutually assured destruction* 6 cards removed from play"
+  elsif turn.type == :player1_loses
+    player1.deck.cards.clear
+    puts "*~*~*~* #{turn.winner.name} has won the game! *~*~*~*"
+  elsif turn.type == :player2_loses
+    player2.deck.cards.clear
+    puts "*~*~*~* #{turn.winner.name} has won the game! *~*~*~*"
+  elsif turn.type == :draw || turn_count > 1000000
+    puts "---- DRAW ----"
   end
 
-
-# Error happens every once in a while
-
-# Traceback (most recent call last):
-#         4: from ./lib/war_or_peace_runner.rb:55:in `<main>'
-#         3: from ./lib/war_or_peace_runner.rb:55:in `loop'
-#         2: from ./lib/war_or_peace_runner.rb:67:in `block in <main>'
-#         1: from /Users/andrewspeth/turing/1module/projects/war_or_peace/lib/turn.rb:12:in `type'
-# /Users/andrewspeth/turing/1module/projects/war_or_peace/lib/deck.rb:9:in `rank_of_card_at': undefined method `rank' for nil:NilClass
-# (NoMethodError)
+  break if player1.has_lost? || player2.has_lost? || turn.type == :draw || turn_count > 1000000
+  turn.award_spoils(turn.winner)
+end
