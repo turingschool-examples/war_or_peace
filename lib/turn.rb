d94d6430a4e2b6c8c4b43fcb891044eb9a7935d1
Adrian Campos <adriancampos2992@yahoo.com>
2022-02-06 15:11:04 -0800

    start def pile cards method

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,4 +8,40 @@ class Turn
     @spoils_of_war = []
   end
 
-end
+  def type
+     if @player1.deck.cards[0].rank != @player2.deck.cards[0].rank
+       return :basic
+     elsif
+       @player1.deck.cards[0].rank == @player2.deck.cards[0].rank
+       return :war
+     else
+       @player1.deck.cards[0].rank == @player2.deck.cards[0].rank && @player1.deck.cards[2].rank == @player2.deck.cards[2].rank
+       return :mutually_assured_destruction
+     end
+   end
+
+    def winner
+      if  @player1.deck.cards[0].rank > @player2.deck.cards[0].rank
+        return @player1
+      elsif @player1.deck.cards[0].rank < @player2.deck.cards[0].rank
+        return @player2
+      elsif @player1.deck.cards[2].rank > @player2.deck.cards[2].rank
+        return @player1
+      elsif @player1.deck.cards[2].rank < @player2.deck.cards[2].rank
+        return @player2
+      else
+        "Mutually Assured Destruction, No winner."
+      end
+    end
+
+    def pile_cards
+      if type 
+
+    end
+
+    # def spoils_of_war
+    #
+    # end
+
+
+  end
