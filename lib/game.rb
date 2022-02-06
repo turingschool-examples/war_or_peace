7d9978d01b4e69a3f67e3b5e0ef46d2e187a6c47
Phillip Kamps <phillip.kamps@gmail.com>
2022-02-06 14:31:22 -0700

    Add counts turn num test + play game method

--- lib/game.rb
+++ lib/game.rb
@@ -8,23 +8,21 @@ class Game
    @turn = Turn.new(@player1, @player2)
   end
 
-  def start_game
+  def start_game(x)
     @turn.type
     @turn.winner
     @turn.pile_cards
     spoils = @turn.spoils_of_war.count
     winner = @turn.winner
     @turn.award_spoils(winner)
-    puts "Turn 1: #{@turn.winner.name} won #{spoils} cards"
+    puts "Turn #{x}: #{@turn.winner.name} won #{spoils} cards"
+  end
 
-
-
-
-    # if turn.counter < 10000000
-    #
-    # Turn.new
-    # else
-    #   puts "1M turns reached. It's a draw."
-    # end
+  def play_game
+    1000000.times do |turn_num|
+      start_game(turn_num)
+        # break if turn_num > 1000000
+      puts turn_num
+    end
   end
 end
