e3fd31ff5a2c0c3c3c215e8f978620fcb297b341
Luke Pascale <lpluke5@gmail.com>
2022-02-05 19:51:16 -0700

    test: Test mutually assured destruction output.

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,10 +11,10 @@ class Turn
   def type
     if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
       :basic
-    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
-      :war
     elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
       :mutually_assured_destruction
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+      :war
     end
   end
 
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -26,7 +26,7 @@ RSpec.describe Turn do
       expect(turn.player1).to eq(player1)
       expect(turn.player2).to eq(player2)
       expect(turn.spoils_of_war).to eq([])
-      expect(turn.type).to be(:basic)
+      expect(turn.type).to eq(:basic)
     end
 
     it 'can tell who won' do
@@ -105,11 +105,29 @@ RSpec.describe Turn do
       turn = Turn.new(player1, player2)
       winner = turn.winner
 
-      expect(turn.type).to be(:war)
+      expect(turn.type).to eq(:war)
       turn.pile_cards
       turn.award_spoils(winner)
       expect(player1.deck).to eq(deck1)
       expect(player2.deck).to eq(deck2)
+    end
+
+    it 'can identify mutually assured destruction' do
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:heart, '9', 9)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, '8', 8)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card4, card3, card6, card7])
+      player1 = Player.new("Megan", deck1)
+      player2 = Player.new("Aurora", deck2)
+      turn = Turn.new(player1, player2)
+      
+      expect(turn.type).to eq(:mutually_assured_destruction)
     end
   end
 end
