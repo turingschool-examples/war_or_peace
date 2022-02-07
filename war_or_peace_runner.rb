2afa46eddadf932d627c78efddd269d8d60be0e0
Drew Proebstel <d.proebstel@gmail.com>
2022-02-06 21:10:51 -0800

    fix mutual assured destruction

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -64,21 +64,19 @@ card52 = Card.new(:diamond, '2', 2)
 
 complete_deck = [card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11, card12, card13, card14, card15, card16, card17, card18, card19, card20, card21, card22, card23, card24, card25, card26, card27, card28, card29, card30, card31, card32, card33, card34, card35, card36, card37, card38, card39, card40, card41, card42, card43, card44, card45, card46, card47, card48, card49, card50, card51, card52]
 random_deck = complete_deck.shuffle
-#deck1 = Deck.new([card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11, card12, card13, card14, card15, card16, card17, card18, card19, card20, card21, card22, card23, card24, card25, card26])
-#deck2 = Deck.new([card27, card28, card29, card30, card31, card32, card33, card34, card35, card36, card37, card38, card39, card40, card41, card42, card43, card44, card45, card46, card47, card48, card49, card50, card51, card52])
 deck1 = Deck.new(random_deck[0..25])
 deck2 = Deck.new(random_deck[26..51])
 player1 = Player.new("Megan", deck1)
 player2 = Player.new("Aurora", deck2)
 
-p 'Welcome to War! (or Peace) This game will be played with 52 cards.'
+p "Welcome to War! (or Peace) This game will be played with 52 cards."
 p 'The players today are Megan and Aurora.'
 p "Type 'GO' to start the game!"
 p '------------------------------------------------------------------'
 
-p ''
-p ''
-p ''
+p ""
+p ""
+p ""
 turn = Turn.new(player1, player2)
 @turn_counter = 0
 while turn.player1.has_lost? == false && turn.player2.has_lost? == false && @turn_counter < 1000001
@@ -87,13 +85,14 @@ while turn.player1.has_lost? == false && turn.player2.has_lost? == false && @tur
   if turn.type == 'basic'
     p  "#{turn.winner.name} won 2 cards"
   elsif turn.type == 'war'
-    p "WAR - #{turn.winner.name} won 2 cards"
+    p "WAR - #{turn.winner.name} won 6 cards"
   else
     p "*mutually assured destruction* 6 cards removed from play"
   end
   turn.pile_cards
-  if turn.winner == player1 || turn.winner == player2
+  if winner == player1 || winner == player2
     turn.award_spoils(winner)
   end
   @turn_counter += 1
 end
+p "game done"
