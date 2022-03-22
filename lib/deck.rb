c66ef1c34eb2d8502f234ab7529ab1d4a89b7f36
Bryce Wein <bryce.wein@gmail.com>
2022-03-22 14:55:37 -0400

    Create method for returning high ranking cards.

--- lib/deck.rb
+++ lib/deck.rb
@@ -8,4 +8,8 @@ class Deck
   def rank_of_card_at(index)
     @cards[index].rank
   end
+
+  def high_ranking_cards
+    @cards.select { |card| card.rank >= 11 }
+  end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -4,12 +4,12 @@ require './lib/card'
 
 RSpec.describe Deck do
   before(:each) do
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, 'Queen', 12)
-    card3 = Card.new(:spade, '3', 3)
-    card4 = Card.new(:heart, 'Ace', 14)
-    sample_cards = [card1, card2, card3, card4]
-    @deck = Deck.new(sample_cards)
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, 'Queen', 12)
+    @card3 = Card.new(:spade, '3', 3)
+    @card4 = Card.new(:heart, 'Ace', 14)
+    @sample_cards = [@card1, @card2, @card3, @card4]
+    @deck = Deck.new(@sample_cards)
   end
 
   describe 'Test' do
@@ -20,8 +20,8 @@ RSpec.describe Deck do
       expect(@deck.rank_of_card_at(0)).to eq(12)
       expect(@deck.rank_of_card_at(3)).to eq(14)
     end
-    if 'it returns high ranking cards' do
-      expect(@deck.high_ranking_cards).to eq()
+    it 'it returns high ranking cards' do
+      expect(@deck.high_ranking_cards).to eq([@card1, @card2, @card4])
     end
   end
 end
