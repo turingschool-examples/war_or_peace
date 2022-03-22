fe3d3a5689d9b884712674fd1a49c3a7fca627cc
Andrew King <dev.andrewking@gmail.com>
2022-03-22 13:56:50 -0500

    add card.rb

---
+++ lib/card.rb
@@ -0,0 +1,11 @@
+class Card
+  attr_reader :suit,
+              :rank,
+              :value
+              
+  def initialize(suit, rank, value)
+    @suit = suit
+    @rank = rank
+    @value = value
+  end
+end
