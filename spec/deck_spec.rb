cdf942f928fbc61658194883d07819f47b09b3e1
Sarah Hall <swatson1994@yahoo.com>
2022-03-22 18:59:50 -0400

    Add test for cards in deck, and make pass

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,15 +3,26 @@ require './lib/card'
 require './lib/deck'
 
 describe Deck do
-  it 'exists'do
-  card1 = Card.new(:diamond, 'Queen', 12)
-  card2 = Card.new(:spade, '3', 3)
-  card3 = Card.new(:heart, 'Ace', 14)
 
-  cards = [card1, card2, card3]
+  it 'exists' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
 
-  deck = Deck.new(cards)
+    deck = Deck.new(cards)
     expect(deck).to be_an_instance_of(Deck)
+  end
+
+  it 'has cards' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+    expect(deck.cards).to eq(cards)
   end
 
 end
