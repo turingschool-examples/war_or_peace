926c72bbae1f0210ba90d3122e9b3056f8f77a6f
Bryce Wein <bryce.wein@gmail.com>
2022-03-22 15:33:47 -0400

    Create method for returning percentage of high ranking cards in deck.

--- lib/deck.rb
+++ lib/deck.rb
@@ -12,4 +12,8 @@ class Deck
   def high_ranking_cards
     @cards.select { |card| card.rank >= 11 }
   end
+
+  def percent_high_ranking
+    (high_ranking_cards.length.to_f / @cards.length) * 100
+  end
 end
