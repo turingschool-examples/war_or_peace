cc13e4cbcb8f1b38e857543f97833fe960dfec54
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-03-22 19:13:56 -0400

    Initialize deck class

---
+++ lib/deck.rb
@@ -0,0 +1,22 @@
+class Deck
+  attr_reader :cards
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def rank_of_card_at(index)
+  end
+
+  def high_ranking_cards
+  end
+
+  def percent_high_ranking
+  end
+
+  def remove_card
+  end
+
+  def add_card
+  end
+
+end
