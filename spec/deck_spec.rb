39d3fd5a80813068493ba194051576ad77dea445
“Brad <hutch13531@gmail.com>
2022-03-22 16:52:37 -0400

    Test- it exist

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,6 +3,13 @@ require './lib/deck'
 
 RSpec.describe Deck do
   it "exist" do
-  deck.new(cards)
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
   expect(deck).to be_an_instance_of(Deck)
+  end
+
+
 end
