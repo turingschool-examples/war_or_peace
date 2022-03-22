7678df27f5f21dc4a9a22e754b40fd07a49d133a
Sarah Hall <swatson1994@yahoo.com>
2022-03-22 18:48:23 -0400

    Refactor card class

--- lib/card.rb
+++ lib/card.rb
@@ -1,21 +1,22 @@
 class Card
-
+    attr_reader :suit, :value, :rank
   def initialize(suit, value, rank)
     @suit = suit
     @value = value
     @rank = rank
   end
+  
 
-  def suit
-    @suit
-  end
-
-  def value
-    @value
-  end
-
-  def rank
-    @rank
-  end
+  # def suit
+  #   @suit
+  # end
+  #
+  # def value
+  #   @value
+  # end
+  #
+  # def rank
+  #   @rank
+  # end
 
 end
