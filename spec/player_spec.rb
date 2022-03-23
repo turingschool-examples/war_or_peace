45b89b79a6d158b7266446224fc9c5d94917f62b
Jim Riddle <jimriddle0@gmail.com>
2022-03-23 16:09:36 -0500

    Change test file for player spec to use a before each command

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,16 +4,33 @@ require './lib/card'
 require './lib/player'
 
 RSpec.describe Player do
+  before(:each) do
+
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @cards = [card1, card2, card3]
+
+    @deck = Deck.new(cards)
+
+    @player = Player.new('Clarisa', deck)
+
+
+  end
+
+describe 'Test' do
+
   it "exists" do
 
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
 
-    deck = Deck.new(cards)
-
-    player = Player.new('Clarisa', deck)
+    # card1 = Card.new(:diamond, 'Queen', 12)
+    # card2 = Card.new(:spade, '3', 3)
+    # card3 = Card.new(:heart, 'Ace', 14)
+    # cards = [card1, card2, card3]
+    #
+    # deck = Deck.new(cards)
+    #
+    # player = Player.new('Clarisa', deck)
 
     expect(player).to be_an_instance_of(Player)
     # require 'pry'; binding.pry
@@ -44,5 +61,5 @@ RSpec.describe Player do
     # require 'pry'; binding.pry
   end
 
-
+ end
 end
