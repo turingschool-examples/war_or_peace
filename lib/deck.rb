e634d2997c6de06df64267c38cabb081aab516a9
Jim Riddle <jimriddle0@gmail.com>
2022-03-22 20:42:45 -0500

    Add rank of card at and high ranking cards with working tests

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,7 +1,29 @@
 class Deck
+
   attr_reader :cards
 
   def initialize(card)
     @cards = card
   end
+
+  def rank_of_card_at(location)
+    return cards[location].rank
+  end
+
+  def high_ranking_cards
+    #step through each element of my cards array
+    #and return a new array with only the cards that contain values >= 11
+    high_cards = []
+    cards.each do |card|
+      if card.rank >= 11
+        # require 'pry'; binding.pry
+        high_cards.push card
+      end
+    end
+    return high_cards
+  end
+
+
+
+
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -23,6 +23,43 @@ RSpec.describe Deck do
 
     deck = Deck.new(cards)
     expect(deck.cards).to eq([card1, card2, card3])
+    # require 'pry'; binding.pry
+  end
+
+  it "returns rank of card at" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    expect(deck.rank_of_card_at(0)).to eq(12)
+    expect(deck.rank_of_card_at(2)).to eq(14)
+    #require 'pry'; binding.pry
+  end
+  it "checks percentage of high ranking cards" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    expect(deck.high_ranking_cards).to eq([card1, card3])
+    # require 'pry'; binding.pry
+  end
+
+  it "returns the percentage of high ranking cards" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    expect(deck.percent_high_ranking).to eq(66.67)
     require 'pry'; binding.pry
   end
+
 end
