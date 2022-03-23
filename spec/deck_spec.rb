5600c44dde2b6cbe7c35b6bb2fa527dfe8b3c913
“Brad <hutch13531@gmail.com>
2022-03-23 18:48:36 -0400

    Test- remove card

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -44,13 +44,24 @@ RSpec.describe Deck do
     expect(deck.high_ranking_cards).to eq([card1, card3])
   end
 
-  it "has percent_high_ranking method" do
+  it "can determine the percent_high_ranking cards" do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
     expect(deck.percent_high_ranking).to eq(66.67)
+  end
+
+  it "can remove_card cards" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.remove_card).to eq(card1)
+    expect(deck.cards).to eq([card2, card3])
   end
 
 end
