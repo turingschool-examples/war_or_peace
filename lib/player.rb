e5fc3888d2ab4ac3f85b9f8e4a82a9a1ec7c2ce2
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-03-23 09:33:53 -0400

    Initialize player class and add attributes

--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,22 @@
 class Player
+  attr_reader :name, :deck
+  def initialize(name, deck)
+    @name = name
+    @deck = deck
+  end
 
+#   def has_lost?
+#     if cards == 0
+#       true
+#     else
+#       false
+#   end
 end
+
+
+
+
+
+# def remove_card
+#   @lost -= 1
+# end
