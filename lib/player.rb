369598e6011080bee3a25fa5be29987c57ff775f
tjhaines-cap <tjhaines@eckerd.edu>
2022-03-23 15:07:46 -0600

    Test attribute reader for player

--- lib/player.rb
+++ lib/player.rb
@@ -3,6 +3,8 @@ require './lib/deck'
 
 class Player
 
+  attr_reader :name, :deck
+
   #Initialize takes a string and a deck argument
   def initialize(name, deck)
     @name = name
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -17,5 +17,17 @@ describe Player do
     expect(player1).to be_an_instance_of(Player)
   end
 
-  
+  it 'can access attributes' do
+    card1 = Card.new(:diamond, "Queen", 12)
+    card2 = Card.new(:spade, "3", 3)
+    card3 = Card.new(:heart, "Ace", 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    player1 = Player.new("Clarisa", deck)
+
+    expect(player1.name).to eq("Clarisa")
+  end
+
+
 end
