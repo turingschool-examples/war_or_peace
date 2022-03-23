5477ff4d07233e74743c16bd80a2e03f46fb8221
Clay Ash <claycourtneyash@gmail.com>
2022-03-23 12:45:10 -0600

    Make rank_of_card_at pass

--- lib/card.rb
+++ lib/card.rb
@@ -7,4 +7,8 @@ class Card
   end
 end
 
-card = Card.new(:diamond, "Queen", 12)
+card = Card.new(:heart, 'Jack', 11)
+card1 = Card.new(:diamond, "Queen", 12)
+card2 = Card.new(:spade, '3', 3)
+card3 = Card.new(:heart, 'Ace', 14)
+#card4 = Card.new(:club, '5', 5)
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -3,15 +3,15 @@ require './lib/card'
 
 RSpec.describe Card do
   it "exists" do
-    card = Card.new(:diamond, 'Queen', 12)
-    expect(card).to be_an_instance_of(Card)
+    card1 = Card.new(:diamond, 'Queen', 12)
+    expect(card1).to be_an_instance_of(Card)
   end
 
   it "has readable attributes" do
-    card = Card.new(:diamond, 'Queen', 12)
+    card1 = Card.new(:diamond, 'Queen', 12)
 
-    expect(card.suit).to eq(:diamond)
-    expect(card.value).to eq('Queen')
-    expect(card.rank).to eq(12)
+    expect(card1.suit).to eq(:diamond)
+    expect(card1.value).to eq('Queen')
+    expect(card1.rank).to eq(12)
   end
 end
