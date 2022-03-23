649074d371f5970425532b7bf8a699317e90a938
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-03-22 20:29:24 -0400

    Add percent_high_ranking method

--- lib/deck.rb
+++ lib/deck.rb
@@ -9,12 +9,13 @@ class Deck
   end
 
   def high_ranking_cards
-    @cards.select do |card|
+  high_ranking_cards =  @cards.select do |card|
       card.rank >= 11
       end
   end
 
   def percent_high_ranking
+    (high_ranking_cards.length.to_f/cards.length.to_f * 100).round(2)
   end
 
   def remove_card
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -44,5 +44,14 @@ RSpec.describe Deck do
       expect(deck.high_ranking_cards).to eq([card1, card3])
     end
 
+    it "returns the percentage of high ranking cards" do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
 
-end
+      expect(deck.percent_high_ranking).to eq(66.67)
+    end
+
+  end
