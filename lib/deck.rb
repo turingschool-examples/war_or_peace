c14120e71035c66744221a9371437fc1dc2f87c8
tjhaines-cap <tjhaines@eckerd.edu>
2022-03-22 23:38:12 -0600

    Add and test high_ranking_cards_at in deck.rb

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,3 +1,5 @@
+require './lib/card'
+
 class Deck
 
   attr_reader :cards
@@ -7,12 +9,29 @@ class Deck
     @cards = cards
   end
 
+  #takes an integer as its parameter, returns the rank of the card at that index
   def rank_of_card_at(index)
     rank = @cards[index].rank
     return rank
   end
 
-  
-
+  def high_ranking_cards
+    high_cards = []
+    @cards.each do |card|
+      rank = card.rank
+      if card.rank >=11
+        high_cards << rank
+      end
+    end
+    return high_cards
+  end
 
 end
+
+# card1 = Card.new(:hearts, "Ace", 14)
+# card2 = Card.new(:hearts, "Two", 2)
+# cards = [card1, card2]
+#
+# deck = Deck.new(cards)
+#
+# high = deck.high_ranking_cards
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -4,14 +4,23 @@ require './lib/deck'
 
 RSpec.describe Deck do
   it "exists" do
-    card1 = Card.new(:hearts, "Ace", 1)
+    card1 = Card.new(:hearts, "Ace", 14)
     card2 = Card.new(:hearts, "Two", 2)
     cards = [card1, card2]
 
     deck = Deck.new(cards)
 
     expect(deck).to be_an_instance_of(Deck)
-
   end
-  
+
+  it "has high ranking cards at" do
+    card1 = Card.new(:hearts, "Ace", 14)
+    card2 = Card.new(:hearts, "Two", 2)
+    cards = [card1, card2]
+
+    deck = Deck.new(cards)
+
+    expect(deck.high_ranking_cards).to eq([14])
+  end
+
 end
