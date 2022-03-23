f8b8d58760e70e62d108890847d3d61160c5d844
“Brad <hutch13531@gmail.com>
2022-03-23 19:02:08 -0400

    Test-add card

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -64,4 +64,17 @@ RSpec.describe Deck do
     expect(deck.cards).to eq([card2, card3])
   end
 
+  it "can add_card to deck" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    card4 = Card.new(:club, '5', 5)
+    deck.add_card(card4)
+
+    expect(deck.cards).to eq([card1, card2, card3, card4])
+  end
+
 end
