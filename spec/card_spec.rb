a34c9250a51fb82d633ebd202ba8773b6f6a428a
Eric Espindola <eric_e_2012@hotmail.com>
2022-03-23 00:40:24 -0700

    add creates deck test

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -16,4 +16,12 @@ RSpec.describe Card do
     expect(card.value).to eq('Queen')
     expect(card.rank).to eq(12)
   end
+  it "creates deck" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    expect(deck).to be_an_instance_of(Deck)
+  end
 end
