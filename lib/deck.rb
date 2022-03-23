bf4d9b1ec2d211f627f38b18b49079e3a94085de
“Brad <hutch13531@gmail.com>
2022-03-22 22:10:08 -0400

    Test- high ranking cards

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,4 +6,10 @@ class Deck
     @cards = cards
   end
 
+  def high_ranking_cards
+    high_ranking_cards = @cards.select do |card|
+      card.rank > 10
+    end
+  end
+
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -19,7 +19,18 @@ RSpec.describe Deck do
     card3 = Card.new(:heart, 'Ace', 14)
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
+
     expect(deck.cards).to eq(cards)
+  end
+
+  it "can determine high_ranking_cards" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.high_ranking_cards).to eq([card1, card3])
   end
 
 end
