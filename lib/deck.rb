f92b8f775963ab3fbe687cd441ae61b5760b06bc
Colin Reinhart <colinbreinhartgmail.com>
2022-03-23 17:49:54 -0600

    Building out files

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,34 +1,15 @@
-require './card.rb'
-require 'pry'
+require './lib/card.rb'
+# require './spec/deck_spec.rb'
 
 class Deck
-  attr_reader :cards, :suite, :value, :rank
+  attr_reader :cards
 
   def initialize(cards)
-    @cards = []
+    @cards = cards
   end
 
-  def cards_in_deck
-    @cards << Card.new
+  def rank_of_card_at(index)
+    @cards[index].rank
   end
-
-  # def rank_of_card_at(0)
-  #   # @cards.map { |card| }
-  # end
-
-  # def high_ranking_cards([])
-  # end
-  #
-  # def percent_of_high_ranking_cards
-  #
-  # end
-  #
-  # def remove_card
-  #
-  # end
-  #
-  # def add_card
-  #
-  # end
 
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,20 +1,27 @@
 require 'rspec'
+require './lib/card'
 require './lib/deck'
-require 'pry'
 
 RSpec.describe Deck do
   it "exists" do
-    deck = Deck.new
-
+    deck = Deck.new([])
     expect(deck).to be_an_instance_of(Deck)
   end
 
   xit 'gives card at index 0' do
-    deck = Deck.new
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14) 
-    expect(deck.card[0].rank).to eq(12)
+    deck = Deck.new([])
+    card = Card.new(:diamond, 'Queen', 12)
+    card = Card.new(:spade, '3', 3)
+    card = Card.new(:heart, 'Ace', 14)
+    expect(card.rank_of_card_at).to eq(12)
+  end
+
+  xit 'are high ranking cards' do
+    deck = Deck.new([])
+    card = Card.new(:diamond, 'Queen', 12)
+    card = Card.new(:spade, '3', 3)
+    card = Card.new(:heart, 'Ace', 14)
+    expect(high_ranking_cards).to eq() #Not sure what to make it equal to yet 3/23
   end
 
 end
