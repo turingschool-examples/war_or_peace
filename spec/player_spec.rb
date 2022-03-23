a0f29a819e1ca0fc442059cc5fc12d5389efd4c2
Bryce Wein <bryce.wein@gmail.com>
2022-03-22 20:17:52 -0400

    Make initial player and test files

---
+++ spec/player_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+require './lib/player'
+require './lib/card'
+require './lib/deck'
+
+describe Player do
+  before(:each) do
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @deck = Deck.new([@card1, @card2, @card3])
+    @player = Player.new('Clarisa', @deck)
+  end
+
+  describe 'Test' do
+    it 'exists' do
+      expect(@player).to be_instance_of(Player)
+    end
+  end
+end
