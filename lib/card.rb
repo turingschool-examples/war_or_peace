1385d66cdf92cc1c37f9fa220ed0ba076245e586
Clay Ash <claycourtneyash@gmail.com>
2022-03-22 18:23:37 -0600

    Initial commit

---
+++ lib/card.rb
@@ -0,0 +1,10 @@
+class Card
+  attr_reader :suit, :value, :rank
+  def initialize(suit, value, rank)
+    @suit = suit
+    @value = value
+    @rank = rank
+  end
+end
+
+card = Card.new(:diamond, "Queen", 12)
