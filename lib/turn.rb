4ad1b3184b6f4b81422ae374bea0dec4de47172e
Andrew King <dev.andrewking@gmail.com>
2022-03-24 15:52:26 -0500

    add turn class with initialize method

---
+++ lib/turn.rb
@@ -0,0 +1,14 @@
+class Turn
+  require 'rspec'
+  require './lib/card'
+  require './lib/deck'
+  require './lib/player'
+  require 'pry'
+  attr_reader :player1,
+              :player2,
+              :spoils_of_war
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+  end
+end
