ee4ca52d6493cac45e40e4fc52bd79b83109a75b
Colin Reinhart <colinbreinhartgmail.com>
2022-03-24 14:19:27 -0600

    Add high ranking and percent with passing tests

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,6 +1,5 @@
 require './lib/card.rb'
-# require './spec/deck_spec.rb'
-
+require 'pry'
 class Deck
   attr_reader :cards
 
@@ -11,5 +10,24 @@ class Deck
   def rank_of_card_at(index)
     @cards[index].rank
   end
+
+  def high_ranking_cards
+    @high_ranking_cards = []
+    @cards.each do |card|
+      if card.rank > 10
+        @high_ranking_cards << card
+      end
+    end
+    return @high_ranking_cards
+
+    def percent_high_ranking
+      @percent_high_ranking = ((@high_ranking_cards.legnth / @cards.count) * 100).to_i
+    end
+    return @percent_high_ranking
+  end
+
+  # def remove_card
+  #   @card.pop
+  # end
 
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,27 +1,42 @@
 require 'rspec'
 require './lib/card'
 require './lib/deck'
+require 'pry'
 
-RSpec.describe Deck do
-  it "exists" do
-    deck = Deck.new([])
-    expect(deck).to be_an_instance_of(Deck)
-  end
+describe Deck do
 
-  xit 'gives card at index 0' do
-    deck = Deck.new([])
-    card = Card.new(:diamond, 'Queen', 12)
-    card = Card.new(:spade, '3', 3)
-    card = Card.new(:heart, 'Ace', 14)
-    expect(card.rank_of_card_at).to eq(12)
-  end
+    xit 'exists' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      expect(card1).to be_an_instance_of(Card)
+      expect(deck).to be_an_instance_of(Deck)
+    end
 
-  xit 'are high ranking cards' do
-    deck = Deck.new([])
-    card = Card.new(:diamond, 'Queen', 12)
-    card = Card.new(:spade, '3', 3)
-    card = Card.new(:heart, 'Ace', 14)
-    expect(high_ranking_cards).to eq() #Not sure what to make it equal to yet 3/23
-  end
+    xit 'rank of card at' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      expect(deck.rank_of_card_at(0)).to eq (12)
+    end
 
+    xit 'are high ranking cards' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      deck = Deck.new([card1, card2, card3])
+      expect(deck.high_ranking_cards).to eq([card1, card3]) #Not sure what to make it equal to yet 3/23
+    end
+
+    it 'percent of high ranking cards' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      deck = Deck.new([card1, card2, card3])
+      expect(deck.percent_high_ranking).to eq (66)
+    end
 end
