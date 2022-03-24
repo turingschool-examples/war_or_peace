fc230a17c2b26c36a17fa6a3aeab78929ef19827
Zach Prince <zachary.s.prince@outlook.com>
2022-03-24 07:32:44 -0400

    Add name test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -16,7 +16,19 @@ describe Player do
       end
     end
 
-    describe 'has_lost?' do
+    describe '#name' do
+        it 'has a name' do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+        cards = [card1, card2, card3]
+        deck = Deck.new(cards)
+        player = Player.new('Zach', deck)
+        expect(player.name).to eq 'Zach'
+        end
+    end
+
+    describe '#has_lost?' do
         it 'player has lost' do
         card1 = Card.new(:diamond, 'Queen', 12)
         card2 = Card.new(:spade, '3', 3)
@@ -30,5 +42,5 @@ describe Player do
         expect(player.has_lost?).to eq true
         end
     end
-    
+
 end
