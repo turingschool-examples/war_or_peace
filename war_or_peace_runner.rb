95c9983da6b21979ee26e564115694a7ef07832a
Bryce Wein <bryce.wein@gmail.com>
2022-03-23 20:13:18 -0400

    Add full intro and 'go' command

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -69,9 +69,14 @@ player2 = Player.new('Aurora', deck2)
 
 puts 'Welcome to War! (or Peace) This game will be played with 52 cards.'
 puts 'The players today are Megan and Aurora.'
-puts "Type 'GO' to start the game!"
-p '------------------------------------------------------------------'
-print '>'
+magic_word = 'GO'
+user_input = nil
+
+until magic_word == user_input
+  puts "Type 'GO' to start the game!"
+  print '>'
+  user_input = gets.chomp.upcase
+end
 
 game = Game.new
 game.start(player1, player2)
