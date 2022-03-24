469a26720b96115abe7f661ad03da11c6644f5f5
Stirling Hostetter <stirhoss@icloud.com>
2022-03-24 00:34:53 -0500

    Finish iteration 1

--- lib/deck.rb
+++ lib/deck.rb
@@ -11,4 +11,28 @@ class Deck
   def rank_of_card_at(index)
     @cards[index].rank
   end
+#binding.pry
+  def high_ranking_cards
+    high_cards = []
+    cards.each do |card|
+      if card.rank >= 11
+        high_cards << card
+      end
+    end
+    high_cards
+  end
+
+  def percent_high_ranking
+    percent = (high_ranking_cards.count.to_f / cards.count.to_f) * 100
+    percent.round(2)
+  end
+
+  def remove_card
+    cards.shift
+  end
+
+  def add_card(card)
+    cards << card
+  end
+
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,7 +3,6 @@ require './lib/card'
 require './lib/deck'
 
 describe Deck do
-
   it 'exists' do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
@@ -13,8 +12,10 @@ describe Deck do
 
     expect(deck).to be_an_instance_of(Deck)
   end
+end
 
-  it '#cards returns arrary of cards' do
+describe '#cards' do
+  it 'returns arrary of cards' do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
@@ -23,8 +24,10 @@ describe Deck do
 
     expect(deck.cards).to eq(cards)
   end
+end
 
-  it '#rank_of_card_at tells the rank of card' do
+describe '#rank_card_at' do
+  it 'tells the rank of card' do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
@@ -33,5 +36,86 @@ describe Deck do
 
     expect(deck.rank_of_card_at(0)).to eq(12)
     expect(deck.rank_of_card_at(2)).to eq(14)
+  end
+end
+
+describe '#high_ranking_cards' do
+  it 'will return all cards in deck with rank >= 11' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.high_ranking_cards).to eq([card1, card3])
+  end
+end
+
+describe '#percent_high_ranking' do
+  it 'will return a float of the percent of cards that have a rank >= 11' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.percent_high_ranking).to eq 66.67
+  end
+end
+
+describe '#remove_card' do
+  it 'will remove first card from deck (draw)' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.remove_card).to eq(card1)
+  end
+end
+
+describe 'method check' do
+  it 'Checks to make sure other methods still function' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    deck.remove_card
+
+    expect(deck.high_ranking_cards).to eq [card3]
+    expect(deck.percent_high_ranking).to eq(50.00)
+  end
+end
+
+describe '#add_card' do
+  it 'will add individual cards to deck' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    card4 = Card.new(:club, '5', 5)
+    deck.remove_card
+    deck.add_card(card4)
+
+    expect(deck.cards).to eq([card2, card3, card4])
+  end
+end
+
+describe 'method check' do
+  it 'Checks to make sure other methods still function'do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    card4 = Card.new(:club, '5', 5)
+    deck.remove_card
+    deck.add_card(card4)
+
+    expect(deck.high_ranking_cards).to eq [card3]
+    expect(deck.percent_high_ranking).to eq(33.33)
   end
 end
