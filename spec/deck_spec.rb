c075c15a8a16d0e3bdc0496724cb7cd9903391f6
Zach Prince <zachary.s.prince@outlook.com>
2022-03-24 15:46:56 -0400

    Add percentage_high_ranking test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -65,5 +65,17 @@ describe Deck do
     end
   end
 
+  describe '#percentage_high_ranking' do
+    it 'tells percentage of high rank cards' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      card4 = Card.new(:heart, '4', 4)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      
+    end
+  end
+
 end
 
