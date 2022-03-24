ae96e3ab37c869c6c757bf90dea65bb598bde25a
Clay Ash <claycourtneyash@gmail.com>
2022-03-24 13:19:18 -0600

    Make high_ranking test work

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -4,21 +4,49 @@ require './lib/card'
 
 RSpec.describe Deck do
   it "exists" do
-    deck = Deck.new(@cards)
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
     expect(deck).to be_an_instance_of(Deck)
   end
 
-describe "cards" do
+
   it 'can read the cards' do
-    deck = Deck.new(@cards)
-    expect(deck.cards).to eq(@cards)
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    expect(deck.cards).to eq(cards)
   end
 
-#   describe '#rank_of_card_at' do
-#   it "returns rank of card" do
-#     deck = Deck.new(@cards)
-#     expect(deck.rank_of_card_at).to eq(index)
-#    end
-#   end
+
+  describe '#rank_of_card_at' do
+    it "returns rank of card" do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      # require "pry"; binding.pry 
+
+      expect(deck.rank_of_card_at(0)).to eq(12)
+      expect(deck.rank_of_card_at(2)).to eq(14)
+    end
   end
+
+
+  describe '#high_ranking_cards' do
+    it "returns cards ranking 11 and above" do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      expect(deck.high_ranking_cards).to eq([card1, card3])
+
+   end
+ end
 end
