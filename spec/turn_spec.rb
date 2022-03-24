d98e7a6dfddbaeb86810e6b99a9f7c1ea7a251dd
Zach Prince <zachary.s.prince@outlook.com>
2022-03-24 15:43:37 -0400

    Add turn test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,2 +1,14 @@
 require './lib/turn'
 require 'pry'
+
+describe Turn do
+    describe '#initialize' do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+        cards = [card1, card2, card3]
+        deck = Deck.new(cards)
+        player = Player.new('Zach', deck)
+        
+    end
+end
