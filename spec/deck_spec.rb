59191ed63d1f572306ca305a6bf03f2b45a1f6d3
Andrew King <dev.andrewking@gmail.com>
2022-03-24 14:58:15 -0500

    add high_ranking_cards test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -11,19 +11,27 @@ RSpec.describe Deck do
     @cards = [@card, @card2, @card3]
     @deck = Deck.new(@cards)
   end
-  it "exists" do
-    card = Card.new(:diamond, 'Queen', 12)
-    deck = Deck.new(card)
 
-    expect(deck).to be_an_instance_of(Deck)
+  it "exists" do
+    expect(@deck).to be_an_instance_of(Deck)
   end
 
   it "has readable attributes" do
-    card = Card.new(:diamond, 'Queen', 12)
-    deck = Deck.new(card)
+    # @card = Card.new(:diamond, 'Queen', 12)
+    # @card2 = Card.new(:spade, '3', 3)
+    # @card3 = Card.new(:heart, 'Ace', 14)
+    # @cards = [@card, @card2, @card3]
+    # @deck = Deck.new(@cards)
 
-    expect(deck.card.suit).to eq(:diamond)
-    expect(card.value).to eq('Queen')
-    expect(card.rank).to eq(12)
+    # expect(@deck.rank_of_card_at(0)).to eq('Queen')
+    expect(@deck.rank_of_card_at(0)).to eq(12)
+  end
+
+  it "lists all high ranking cards" do
+    expect(@deck.high_ranking_cards.length).to eq(2)
+  end
+
+  it "gives percentage of high ranking cards" do
+    expect(@deck.percent_high_ranking).to eq(66.6)
   end
 end
