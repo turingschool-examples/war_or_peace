57f270797a44a32bff49f6bbcc5e349c61c56918
Bryce Wein <bryce.wein@gmail.com>
2022-03-23 22:13:28 -0400

    Add final test of card generator

--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -8,8 +8,13 @@ describe 'CardGenerator' do
     expect(cards).to be_a CardGenerator
   end
   it 'produces an array of 52 cards' do
-    filename = './lib/cards.txt'
+    filename = './cards.txt'
     cards = CardGenerator.new(filename).cards
     expect(cards.length).to eq(52)
+  end
+  it 'only contains instances of the card class' do
+    filename = './cards.txt'
+    cards = CardGenerator.new(filename).cards
+    expect(cards.all? { |e| e.instance_of?(Card) }).to eq(true)
   end
 end
