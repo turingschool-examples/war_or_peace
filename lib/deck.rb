d3a175866fe66208e663d28f76ea26d39cdc2c47
Luke Swenson <LukeSwenson95@outlook.com>
2022-03-24 09:01:09 -0600

    added 2 tests and updated runner file

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,3 +1,10 @@
-class Deck
 
+class Deck
+  attr_reader :cards
+  def initialize(cards)
+    @cards = cards
+  end
+  def rank_of_card_at(index)
+  
+  end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,2 +1,31 @@
+require 'rspec'
 require './lib/card'
 require './lib/deck'
+
+describe Deck do
+it "exists" do
+card1 = Card.new(:diamond, 'Queen', 12)
+card2 = Card.new(:spade, '3', 3)
+card3 = Card.new(:heart, 'Ace', 14)
+cards = [card1, card2, card3]
+  deck = Deck.new(cards)
+expect(deck).to be_a(Deck)
+end
+it "has cards" do
+  card1 = Card.new(:diamond, 'Queen', 12)
+  card2 = Card.new(:spade, '3', 3)
+  card3 = Card.new(:heart, 'Ace', 14)
+  cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    expect(deck.cards).to eq([card1, card2, card3])
+end
+ it "finds rank of card at given index" do
+   card1 = Card.new(:diamond, 'Queen', 12)
+   card2 = Card.new(:spade, '3', 3)
+   card3 = Card.new(:heart, 'Ace', 14)
+   cards = [card1, card2, card3]
+     deck = Deck.new(cards)
+     expect(deck.rank_of_card_at(0)).to eq(12)
+     expect(deck.rank_of_card_at(0)).to eq(14)
+ end
+end
