9bac3b3e27053334f247c22c31e599345da794b9
Stirling Hostetter <stirhoss@icloud.com>
2022-03-24 14:40:56 -0500

    Complete player object

--- lib/player.rb
+++ lib/player.rb
@@ -3,12 +3,17 @@ require './lib/card'
 require './lib/deck'
 
 class Player
-  attr_reader :card,
-              :deck
+  attr_reader :cards,
+              :deck,
+              :name
 
   def initialize(name, deck)
     @name = name
     @deck = deck
+  end
+
+  def has_lost?
+    deck.cards.count == 0
   end
 
 
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -13,3 +13,81 @@ describe Player do
     expect(player).to be_an_instance_of(Player)
   end
 end
+
+describe '#name' do
+  it 'reports the players name' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+
+    expect(player.name).to eq 'Clarisa'
+  end
+end
+
+describe '#deck' do
+  it 'returns all of the cards that are in deck' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+
+    expect(player.deck).to eq(deck)
+  end
+end
+
+describe '#has_lost?' do
+  it 'returns boolean if deck.count = 0' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+
+    expect(player.has_lost?).to eq false
+  end
+end
+
+describe 'method test' do
+  it 'ensures that player has access to remove_card' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+
+    expect(player.deck.remove_card).to eq card1
+  end
+end
+
+describe 'method test' do
+  it 'removes cards until #has_lost? returns true' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+    player.deck.remove_card
+    player.deck.remove_card
+    player.deck.remove_card
+
+    expect(player.has_lost?).to eq true
+  end
+end
+
+describe 'deck empty' do
+  it 'reports that there are not cards in deck' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+    player.deck.remove_card
+    player.deck.remove_card
+    player.deck.remove_card
+
+    expect(player.deck.cards.count).to eq 0
+  end
+end
