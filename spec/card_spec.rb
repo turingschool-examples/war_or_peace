c2602bfade08174e42aba6e7cbefbc9bdd673260
Saba Bhamidipati <saba.bhamidipati@gmail.com>
2022-03-25 05:26:07 -0400

    Create card and pass all tests

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -38,6 +38,10 @@ RSpec.describe Card do
   end
 
   it "is an array" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
     cards = [card1, card2, card3]
 
     expect(cards).to eq([card1, card2, card3])
