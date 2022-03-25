c1d489ddfa30103f27d210527221572df2a5603f
Wesley Garcia <wes@alltradestech.com>
2022-03-25 11:12:49 -0700

    Update player class and spec files with completed tests

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,11 @@
 class Player
-  attr_reader :name
+  attr_reader :name, :deck
   def initialize (name, deck)
     @name = name
+    @deck = deck
+  end
+
+  def has_lost?
+    deck.cards.count == 0
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -13,4 +13,46 @@ RSpec.describe Player do
     player = Player.new('Clarisa', deck)
     expect(player).to be_an_instance_of(Player)
   end
+
+  it "has a name" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+    expect(player.name).to eq("Clarisa")
+  end
+
+  it "has a deck" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+    expect(player.deck).to eq(deck)
+  end
+
+  it "determines if game is lost" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+    expect(player.has_lost?).to eq(false)
+    player.deck.remove_card
+    expect(player.has_lost?).to eq(false)
+    player.deck.remove_card
+    expect(player.has_lost?).to eq(false)
+    player.deck.remove_card
+    expect(player.has_lost?).to eq(true)
+  end
+
+  it "can remove cards" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+    expect(player.deck.remove_card).to eq(card1)
+  end
 end
