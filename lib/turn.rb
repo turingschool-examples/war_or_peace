60b9d9811dee410512983302659a6fc1f2858bee
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-03-25 11:50:46 -0600

    Add types to turn in iteration 3

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,11 +8,23 @@ class Turn
         @player1 = player1
         @player2 = player2
         @spoils_of_war = []
+        @type = type
     end
 
     def type
        return :basic if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0) 
+       return :war if @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
+       return :mutually_assured_destruction if :war == true && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at  
     end
-        
+
+    def winner
+        if @type = :basic
+            if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
+                @player1
+            else
+                @player2
+            end
+        end   
+    end       
 
 end
