b29fc4523cffa5dcb9d64dc8957a142f09675f08
Lucas Colwell <colwellc37@gmail.com>
2022-03-25 15:34:56 -0600

    Do has lost and won

--- lib/player.rb
+++ lib/player.rb
@@ -1,21 +1,29 @@
 require 'pry'
+require './lib/deck'
 
 class Player
   attr_reader :name, :deck
-  # 
-  # def initialize(name, deck)
-  #   @name = name
-  #   @deck = deck
-  # end
-  #
-  # def has_lost?
-  #   if @deck.cards.count > 0
+
+  def initialize(name, deck)
+    @name = name
+    @deck = deck
+  end
+
+  def has_lost?
+    if @deck.cards.count > 0
+      false
+    else
+      true
+    end
+  end
+
+  # def has_won?
+  #   if @deck.cards.count == 0
   #     false
   #   else
-  #     @deck.cards.count <=0
   #     true
   #   end
-  # end
+  end
 
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -21,22 +21,56 @@ describe Player do
 
   end
 
-  # it "has a deck" do
-  #   card1 = Card.new(:diamond, 'Queen', 12)
-  #   card2 = Card.new(:spade, '3', 3)
-  #   card3 = Card.new(:heart, 'Ace', 14)
-  #
-  #   cards = [card1, card2, card3]
-  #
-  #   deck = Deck.new(cards)
-  #   player1 = ["Clarisa", deck]
-  #
-  #   expect
-  # end
+  it "has a deck" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
 
-  # xit ""
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+    name = "Clarisa"
+    player1 = Player.new(name, deck)
+
+    expect(player1.deck).to be_an_instance_of(Deck)
+  end
+
+  it "checks to see if player lost" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+    name = "Clarisa"
+    player1 = Player.new(name, deck)
+
+    expect(player1.has_lost?).to eq(false)
 
 
+  end
+
+  it "checks to see if player won" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+    name = "Clarisa"
+
+    # 3.times do 
+    #   deck = deck.remove_card
+    #   return deck
+    # end
+    player1 = Player.new(name, deck)
+
+
+    expect(player1.has_lost?).to eq(false)
+
+  end
 
 
 end
