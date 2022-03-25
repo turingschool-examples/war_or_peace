78c80deba3653761ec79489a3fd817b52ee591f6
Luke Swenson <LukeSwenson95@outlook.com>
2022-03-24 19:45:33 -0600

    added test to check cards

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -59,4 +59,13 @@ it "remove top card from deck" do
   deck = Deck.new(cards)
   expect(deck.remove_card).to eq(card1)
 end
+
+it "checks deck of cards after removing card" do
+  card1 = Card.new(:diamond, 'Queen', 12)
+  card2 = Card.new(:spade, '3', 3)
+  card3 = Card.new(:heart, 'Ace', 14)
+  cards = [card1, card2, card3]
+  deck = Deck.new(cards)
+  expect(deck.cards).to eq(cards)
+end
 end
