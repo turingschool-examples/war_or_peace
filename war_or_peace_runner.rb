c1f827ae93b8ba2955e7b957279086d55615205f
Andrew King <dev.andrewking@gmail.com>
2022-03-25 12:10:36 -0500

    rename runner file and add start prompt

---
+++ war_or_peace_runner.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require 'pry'
+start_word = 'GO'
+puts "Welcome to War! (or Peace) This game will be played with 52 cards." + "\n"\
+"The players today are Megan and Aurora.\n" + "\n"\
+"Type '#{start_word}' to start the game!\n" + "\n"\
+"------------------------------------------------------------------" + "\n"
