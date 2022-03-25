6aa7b0b05b9cf497cb5e7f29934a768e352c5f8f
Lucas Colwell <colwellc37@gmail.com>
2022-03-25 12:19:50 -0600

    Add test 1

--- lib/player.rb
+++ lib/player.rb
@@ -1,24 +1,21 @@
-require '.lib/deck'
-require '.lib/pry'
+require 'pry'
 
 class Player
   attr_reader :name, :deck
-
-  def initialize(name, deck)
-    @name = name
-    @deck = deck
-  end
+  # 
+  # def initialize(name, deck)
+  #   @name = name
+  #   @deck = deck
+  # end
   #
   # def has_lost?
-  #   if deck.cards.count >= 1
+  #   if @deck.cards.count > 0
   #     false
   #   else
+  #     @deck.cards.count <=0
   #     true
   #   end
   # end
-  #
-  # def remove_card
-  #   deck.cards.shift
-  # end
+
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,14 +1,41 @@
 require 'rspec'
+require './lib/player'
 require './lib/card'
 require './lib/deck'
-require './lib/player'
 require 'pry'
 
-Rspec.describe Player do
+describe Player do
   it "exists" do
-    player1 = ""
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+    name = "Clarisa"
+    player1 = Player.new(name, deck)
+
+
+    expect(player1).to be_an_instance_of(Player)
 
   end
+
+  # it "has a deck" do
+  #   card1 = Card.new(:diamond, 'Queen', 12)
+  #   card2 = Card.new(:spade, '3', 3)
+  #   card3 = Card.new(:heart, 'Ace', 14)
+  #
+  #   cards = [card1, card2, card3]
+  #
+  #   deck = Deck.new(cards)
+  #   player1 = ["Clarisa", deck]
+  #
+  #   expect
+  # end
+
+  # xit ""
+
 
 
 
