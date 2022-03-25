6f06f330e06831c8dd087e851a569c4db4bf594b
Zachary Hazelwood <zhaze001@gmail.com>
2022-03-25 10:37:09 -0600

    Add deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,3 +1,39 @@
+require './lib/card'
+
 class Deck
-  
+  attr_reader :cards
+
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def add_card(card)
+    @cards << card
+  end
+
+  def remove_card
+    cards.shift
+  end
+
+  def rank_of_card_at(index)
+    cards[index].rank
+    # cards[index].each do |card|
+    #   puts card.rank
+    # end
+  end
+
+  def high_ranking_card
+    high_rank_cards = []
+    cards.each do |card|
+      if card.rank >= 11
+        high_rank_cards << card
+      end
+    end
+    return high_rank_cards
+  end
+
+  def percent_high_ranking
+    percent = high_ranking_card.count.to_f / @cards.count.to_f * 100
+    percent.round(2)
+  end
 end
---
+++ spec/deck_spec.rb
@@ -0,0 +1,74 @@
+require 'rspec'
+# require '.lib/card'
+require './lib/deck'
+
+describe Deck do
+  it "exists" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+    expect(deck).to be_an_instance_of(Deck)
+  end
+
+  it "checks rank" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+    expect(deck.rank_of_card_at(1)).to eq(3)
+  end
+
+  it "checks high rank" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+    expect(deck.high_ranking_card).to eq([card1, card3])
+  end
+
+  it "can remove cards" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+    expect(deck.remove_card).to eq(card1)
+    expect(deck.cards).to eq([card2, card3])
+  end
+
+  it "can add cards" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    card4 = Card.new(:club, '5', 5)
+    deck.add_card(card4)
+    expect(deck.cards).to eq([card1, card2, card3, card4])
+  end
+
+  it "checks percent of high ranking cards" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+    expect(deck.percent_high_ranking).to eq(66.67)
+  end
+end
