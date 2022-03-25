0742c85d9bc20a6ac64905ddda4379581cdbfe29
Andrew King <dev.andrewking@gmail.com>
2022-03-25 12:36:52 -0500

    add start case

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -2,9 +2,26 @@ require 'rspec'
 require './lib/card'
 require './lib/deck'
 require './lib/player'
+require './lib/turn'
 require 'pry'
 start_word = 'GO'
+user_input = ' '
+
 puts "Welcome to War! (or Peace) This game will be played with 52 cards." + "\n"\
-"The players today are Megan and Aurora.\n" + "\n"\
-"Type '#{start_word}' to start the game!\n" + "\n"\
+"The players today are Megan and Aurora." + "\n"\
+"Type '#{start_word}' to start the game!" + "\n"\
 "------------------------------------------------------------------" + "\n"
+
+until user_input.upcase == start_word do
+
+  user_input = gets.chomp
+
+  if(user_input.upcase == start_word)
+    @turn.start
+  elsif(user_input.upcase == "EXIT")
+    break
+  elsif(user_input.upcase != start_word)
+    puts "Invalid input."
+  end
+
+end
