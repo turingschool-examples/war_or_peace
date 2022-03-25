cd3e9b101be8d8260c067464218478fc3d59368e
Clay Ash <claycourtneyash@gmail.com>
2022-03-25 17:31:40 -0600

    Make Pile Cards method and test pass

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,7 +3,7 @@ class Turn
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
-    @spoils_of_war = spoils_of_war
+    @spoils_of_war = []
   end
 
   def type
@@ -31,17 +31,26 @@ class Turn
         return player2
       end
     else type == :mutually_assured_destruction
-      # 3.times do
-      #   player1.deck.shift
-      #   player2.deck.shift
-      # end
       return "No Winner"
     end
   end
 
-  # def pile_cards
-  #
-  # end
+  def pile_cards
+    if type == :basic
+      @spoils_of_war << player1.deck.remove_card
+      @spoils_of_war << player2.deck.remove_card
+    elsif type == :war
+      3.times do
+        @spoils_of_war << player1.deck.remove_card
+        @spoils_of_war << player2.deck.remove_card
+      end
+    else type == :mutually_assured_destruction
+      3.times do
+        player1.deck.remove_card
+        player2.deck.remove_card
+      end
+    end
+  end
   #
   # def award_spoils(winner)
   # end
