29a170b8d12089f6cc9a27576dbc30b9d28c093d
Zach Prince <zachary.s.prince@outlook.com>
2022-03-25 14:59:31 -0400

    Add winner and pile_cards method

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,33 +4,58 @@ class Turn
 
     attr_reader :player1, :player2, :spoils_of_war
 
-    def initialize (player1, player2, basic, war, mutually_assured_destruction)
+    def initialize (player1, player2) #basic, war, mutually_assured_destruction)
         @player1 = player1
         @player2 = player2
-        @basic = player1.deck.rank_of_card_at(0) != player2.decl.rank_of_card_at(0)
-        @war = player1.deck.rank_of_card_at(0) == player2.decl.rank_of_card_at(0)
-        @mutually_assured_destruction = player1.deck.rank_of_card_at(0, 2) != player2.decl.rank_of_card_at(0, 2)
+        @spoils_of_war = []
     end
 
-    def type(type)
-        if type = player1.deck.rank_of_card_at(0) != player2.decl.rank_of_card_at(0)
-            @basic
-        elsif player1.deck.rank_of_card_at(0) == player2.decl.rank_of_card_at(0)
-            @war
-        elsif player1.deck.rank_of_card_at(0, 2) != player2.decl.rank_of_card_at(0, 2)
-            @mutually_assured_destruction
+    def type
+        if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
+            :basic
+        elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+            :war
+        elsif player1.deck.rank_of_card_at(0, 2) != player2.deck.rank_of_card_at(0, 2)
+            :mutually_assured_destruction
         end
     end
 
     def winner
-        if 
-
-
-
+        type = type() #this is necessary to establish type in this method
+        if type == :basic && player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+            player1
+        elsif type == :basic && player1.deck.rank_of_card_at(0) < player2.deck.rank_of_card_at(0)
+            player2
+        elsif type == :war && player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+            player1
+        elsif type == :war && player1.deck.rank_of_card_at(2) < player2.deck.rank_of_card_at(2)
+            player2
+        elsif type == :mutually_assured_destruction && player1.deck.rank_of_card_at(0, 2) < player2.deck.rank_of_card_at(0, 2)
+            "No Winner"
+        end
     end
 
     def pile_cards
+        type = type() #this is necessary to establish type in this method
+        if type == :basic 
+            @spoils_of_war << player1.deck.remove_card && @spoils_of_war << player2.deck.remove_card
+        elsif type == :war
+            3.times @spoils_of_war << player1.deck.remove_card && @spoils_of_war << player2.deck.remove_card
+        elsif type == :mutually_assured_destruction
+            3.times {player1.deck.cards.shift} && 3.times {player1.deckcards.shift}
+        end
     end
+
+    def award_spoils
+        if winner == player1
+            player1.deck << @spoils_of_war
+        elsif winner == player2
+            player2.deck << @spoils_of_war
+        end
+
+
+    end
+
 
 
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,14 +1,81 @@
 require './lib/turn'
 require 'pry'
 
+
+
+
 describe Turn do
+
+   
+
     describe '#initialize' do
+        it 'is a turn' do
         card1 = Card.new(:diamond, 'Queen', 12)
         card2 = Card.new(:spade, '3', 3)
         card3 = Card.new(:heart, 'Ace', 14)
         cards = [card1, card2, card3]
         deck = Deck.new(cards)
         player = Player.new('Zach', deck)
-        
+        end
     end
+
+
+    describe '#type' do
+        it 'tells card type' do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+        card4 = Card.new(:diamond, 'King', 13)
+        card5 = Card.new(:spade, '7', 7)
+        card6 = Card.new(:heart, 'Jack', 11)
+        cards1 = [card1, card3, card5]
+        cards2 = [card2, card4, card6]
+        deck1 = Deck.new(cards1)
+        deck2 = Deck.new(cards2)
+        player1 = Player.new('Zach', deck1)
+        player2 = Player.new('Notzach', deck2)
+        turn = Turn.new(player1, player2)
+        expect(turn.type).to eq :basic
+        end
+    end
+
+    describe '#winner' do
+        it 'tells the winner' do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+        card4 = Card.new(:diamond, 'King', 13)
+        card5 = Card.new(:spade, '7', 7)
+        card6 = Card.new(:heart, 'Jack', 11)
+        cards1 = [card1, card3, card5]
+        cards2 = [card2, card4, card6]
+        deck1 = Deck.new(cards1)
+        deck2 = Deck.new(cards2)
+        player1 = Player.new('Zach', deck1)
+        player2 = Player.new('Notzach', deck2)
+        turn = Turn.new(player1, player2)
+        expect(turn.winner).to eq player1
+        end
+    end
+
+    describe '#pile_cards' do
+        it "Piles the cards" do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+        card4 = Card.new(:diamond, 'King', 13)
+        card5 = Card.new(:spade, '7', 7)
+        card6 = Card.new(:heart, 'Jack', 11)
+        cards1 = [card1, card3, card5]
+        cards2 = [card2, card4, card6]
+        deck1 = Deck.new(cards1)
+        deck2 = Deck.new(cards2)
+        player1 = Player.new('Zach', deck1)
+        player2 = Player.new('Notzach', deck2)
+        turn = Turn.new(player1, player2)
+        expect(turn.pile_cards).to eq player1.deck.count(2) && player2.deck.count(2)
+        end
+    end
+
 end
+
