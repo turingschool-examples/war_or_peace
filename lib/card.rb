21e5dba02465a1c1cab26f04f66f3da6237724ef
Clay Ash <claycourtneyash@gmail.com>
2022-03-25 11:15:51 -0600

    Clean up card class and test

--- lib/card.rb
+++ lib/card.rb
@@ -7,9 +7,3 @@ class Card
     @rank = rank
   end
 end
-
-card = Card.new(:heart, 'Jack', 11)
-card1 = Card.new(:diamond, "Queen", 12)
-card2 = Card.new(:spade, '3', 3)
-card3 = Card.new(:heart, 'Ace', 14)
-card4 = Card.new(:club, '5', 5)
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -4,13 +4,21 @@ require './lib/card'
 RSpec.describe Card do
   it "exists" do
     card = Card.new(:heart, 'Jack', 11)
-    expect(card1).to be_an_instance_of(Card)
+    card1 = Card.new(:diamond, "Queen", 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    card4 = Card.new(:club, '5', 5)
+    expect(card).to be_an_instance_of(Card)
   end
 
   it "has readable attributes" do
     card = Card.new(:heart, 'Jack', 11)
-    expect(card1.suit).to eq(:heart)
-    expect(card1.value).to eq('Jack')
-    expect(card1.rank).to eq(11)
+    card1 = Card.new(:diamond, "Queen", 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    card4 = Card.new(:club, '5', 5)
+    expect(card.suit).to eq(:heart)
+    expect(card.value).to eq('Jack')
+    expect(card.rank).to eq(11)
   end
 end
