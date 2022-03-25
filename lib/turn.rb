29bd4ddcd25e2e0e17d3c6f3015666e739aa327a
Andrew King <dev.andrewking@gmail.com>
2022-03-24 19:20:35 -0500

    add turn.rb

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,4 +13,17 @@ class Turn
     @player2 = player2
     @spoils_of_war = []
   end
+
+  def type
+    if(@player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0))
+      return :basic
+    elsif(@player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
+       && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2))
+       return :mutually_assured_destruction
+     else
+       return :war
+  end
+
+
+
 end
