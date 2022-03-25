4915911ff89827e6b85ba96303851a03a8cfb6ba
Lucas Colwell <colwellc37@gmail.com>
2022-03-25 10:37:47 -0600

    Improve flow

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -68,11 +68,12 @@ describe Deck do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
-    card4 = Card.new(:club, "5", 5)
 
     cards = [card1, card2, card3]
   #
     deck = Deck.new(cards)
+    card4 = Card.new(:club, "5", 5)
+
     expect(deck.add_card(card4)).to eq([card1, card2, card3, card4])
   end
 
