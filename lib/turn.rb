e8f3842f32901eb25cff6c3e00271f72852ba2da
Zach Prince <zachary.s.prince@outlook.com>
2022-03-24 21:22:59 -0400

    Add type method

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,6 +12,16 @@ class Turn
         @mutually_assured_destruction = player1.deck.rank_of_card_at(0, 2) != player2.decl.rank_of_card_at(0, 2)
     end
 
+    def type(type)
+        if type = player1.deck.rank_of_card_at(0) != player2.decl.rank_of_card_at(0)
+            @basic
+        elsif player1.deck.rank_of_card_at(0) == player2.decl.rank_of_card_at(0)
+            @war
+        elsif player1.deck.rank_of_card_at(0, 2) != player2.decl.rank_of_card_at(0, 2)
+            @mutually_assured_destruction
+        end
+    end
+
     def winner
         if 
 
