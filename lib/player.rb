19aa119dcf31969bcf25d2dbda9dfc6cca56cd26
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-03-24 18:11:33 -0600

    Passed first test iteration two

--- lib/player.rb
+++ lib/player.rb
@@ -2,4 +2,8 @@ require './lib/card'
 require './lib/deck'
 
 class Player 
+    # attr_reader :name
+    # def initialize(name)
+    #     @name = name
+    # end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,20 +4,24 @@ require './lib/player'
 
 
 RSpec.describe Player do
-card1 = Card.new(:diamond, 'Queen', 12)
-card2 = Card.new(:spade, '3', 3)
-card3 = Card.new(:heart, 'Ace', 14)
-deck = Deck.new([card1, card2, card3])
-player = Player.new('Clarisa', deck)
-
-it 'has a name' do
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    deck = Deck.new([card1, card2, card3])
-    player = Player.new('Clarisa', deck)
+    it 'exists' do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+        deck = Deck.new([card1, card2, card3])
+        player = Player.new
     
-    expect(player.name).to eq('Clarisa')
-    expect(player.deck).to eq(deck)
-end
+        expect(player).to be_an_instance_of(Player)
+    end
+
+    # it 'has a name' do
+    #     card1 = Card.new(:diamond, 'Queen', 12)
+    #     card2 = Card.new(:spade, '3', 3)
+    #     card3 = Card.new(:heart, 'Ace', 14)
+    #     deck = Deck.new([card1, card2, card3])
+    #     player = Player.new('Clarisa', deck)
+
+    #     expect(player.name).to eq('Clarisa')
+    #     expect(player.deck).to eq(deck)
+    # end
 end
