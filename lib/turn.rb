4b80b13e0e9a904f1ee661278c8f64baa32122f2
Andrew King <dev.andrewking@gmail.com>
2022-03-24 21:46:26 -0500

    add pile_cards method

--- lib/turn.rb
+++ lib/turn.rb
@@ -45,6 +45,22 @@ class Turn
     end
   end
 
-
-
+  def pile_cards(type)
+    if(type == :basic)
+      @spoils_of_war << @player1.deck.remove_card
+      @spoils_of_war << @player2.deck.remove_card
+    elsif(type == :war)
+      3.times do
+        @spoils_of_war << @player1.deck.remove_card
+        @spoils_of_war << @player2.deck.remove_card
+      end
+    else
+      3.times do
+        @player1.deck.remove_card
+        @player2.deck.remove_card
+      end
+    end
   end
+
+
+end
