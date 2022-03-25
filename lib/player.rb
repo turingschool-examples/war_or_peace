90763e8b0614e00ad07f8374bc7db71fe689da94
Sage Skaff <skaffsage@gmail.com>
2022-03-25 11:11:22 -0700

    Update player.rb and player_spec.rb

--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,16 @@
 class Player
-  
+  attr_reader :deck,
+              :name
+  def initialize(name, deck)
+    @name = name
+    @deck = deck
+  end
+
+  def has_lost?
+    if deck.cards.count >= 1
+      false
+    else
+      true
+    end
+  end
 end
