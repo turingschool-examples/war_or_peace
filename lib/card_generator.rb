6618253f4d82574cdec2a0b5e9aa6bea42180894
Andrew King <dev.andrewking@gmail.com>
2022-03-25 16:44:06 -0500

    have generator turn .txt file into array, then into card objects

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -7,18 +7,12 @@ class Card_Generator
   def initialize(file_name)
     @cards = []
     File.foreach(file_name) do |line|
-    card_array = line.lines(',')
-
-    value = card_array[0].delete(',').to_i
-    suit = card_array[1].delete(',')
-    rank = card_array[2].delete(',')
-
-    @cards << Card.new(suit, value, rank)
+      line.chomp!
+      card_array = line.lines(',')
+      value = card_array[0].delete(',').delete(' ').to_i
+      suit = card_array[1].delete(',').delete(' ')
+      rank = card_array[2].delete(',').delete(' ')
+      @cards << Card.new(suit, value, rank)
+    end
   end
-
-
-  end
-
-
-
 end
