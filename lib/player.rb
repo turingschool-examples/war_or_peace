d9596d08c3807a1de53321f1c2404ffb108b169a
Colin Reinhart <colinbreinhartgmail.com>
2022-03-24 19:59:04 -0600

    Add player files

---
+++ lib/player.rb
@@ -0,0 +1,18 @@
+require './lib/card'
+require './lib/deck'
+class Player
+  attr_reader :name, :deck
+
+  def initialize (name, deck)
+    @name = name
+    @deck = deck
+  end
+
+  def has_lost?
+    if deck.cards.length > 0
+      return false
+    else
+      return true
+    end
+  end
+end
