ad1d40129481da6ca06f11a681338b6c3da97428
Luke Swenson <LukeSwenson95@outlook.com>
2022-03-25 11:05:20 -0600

    Created player class and new test

---
+++ lib/player.rb
@@ -0,0 +1,8 @@
+class Player
+  attr_reader :player, :deck
+ def initialize(player, deck)
+   @player = player
+   @deck = deck
+ end
+
+end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,6 +1,7 @@
 require 'rspec'
 require './lib/card'
 require './lib/deck'
+require './lib/player'
 
 describe Deck do
 it "exists" do
@@ -103,4 +104,18 @@ it "creates new card and adds it" do
   card4 = Card.new(:club, '5', 5)
 expect(deck.add_card(card4)).to eq([card1, card2, card3, card4])
 end
+
+describe Player do
+  it "player exists" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    card4 = Card.new(:club, '5', 5)
+    player = Player.new('Clarisa', deck)
+    expect(player).to be_a(Player)
+  end
+end
+
 end
