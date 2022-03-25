998794bada4a25edcb0fdb688b695d8667caa007
Zachary Hazelwood <zhaze001@gmail.com>
2022-03-25 15:19:55 -0600

    Add has cards test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -12,6 +12,17 @@ describe Player do
     expect(player.name).to eq('Clarisa')
   end
 
+  it "has a deck" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    player = Player.new('Clarisa', deck)
+    expect(player.deck).to be_an_instance_of(Deck)
+  end
+
   it "check for a loss" do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
