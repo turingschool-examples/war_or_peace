c62fe8da337ca4f9cf547bef92fc55c6dc9bd791
Andrew King <dev.andrewking@gmail.com>
2022-03-24 19:52:30 -0500

    add basic, war, and MAD tests

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -6,24 +6,52 @@ require './lib/turn'
 require 'pry'
 
 RSpec.describe Turn do
-  before(:each ) do
-    @card1 = Card.new(:heart, 'Jack', 11)
-    @card2 = Card.new(:heart, '10', 10)
-    @card3 = Card.new(:heart, '9', 9)
-    @card4 = Card.new(:diamond, 'Jack', 11)
-    @card5 = Card.new(:heart, '8', 8)
-    @card6 = Card.new(:diamond, 'Queen', 12)
-    @card7 = Card.new(:heart, '3', 3)
-    @card8 = Card.new(:diamond, '2', 2)
+
+    before(:each ) do
+      @card1 = Card.new(:heart, 'Jack', 11)
+      @card2 = Card.new(:heart, '10', 10)
+      @card3 = Card.new(:heart, '9', 9)
+      @card4 = Card.new(:diamond, 'Jack', 11)
+      @card5 = Card.new(:heart, '8', 8)
+      @card6 = Card.new(:diamond, 'Queen', 12)
+      @card7 = Card.new(:heart, '2', 2)
+      @card8 = Card.new(:diamond, '2', 2)
+    end
+
+  it "exists" do
     @deck1 = Deck.new([@card1, @card2, @card5, @card8])
     @deck2 = Deck.new([@card3, @card4, @card6, @card7])
     @player1 = Player.new("Megan", @deck1)
     @player2 = Player.new("Aurora", @deck2)
     @turn = Turn.new(@player1, @player2)
+    expect(@turn).to be_an_instance_of(Turn)
   end
 
-  it "exists" do
-    expect(@turn).to be_an_instance_of(Turn)
+  it "type will be basic if first cards are not the same" do
+    @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+    @player1 = Player.new("Megan", @deck1)
+    @player2 = Player.new("Aurora", @deck2)
+    @turn = Turn.new(@player1, @player2)
+    expect(@turn.type).to eq(:basic)
+  end
+
+  it "type will be war if first cards are the same" do
+    @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    @deck2 = Deck.new([@card4, @card3, @card6, @card7])
+    @player1 = Player.new("Megan", @deck1)
+    @player2 = Player.new("Aurora", @deck2)
+    @turn = Turn.new(@player1, @player2)
+    expect(@turn.type).to eq(:war)
+  end
+
+  it "type will be war if first cards are the same" do
+    @deck1 = Deck.new([@card1, @card2, @card8, @card5])
+    @deck2 = Deck.new([@card4, @card3, @card7, @card6])
+    @player1 = Player.new("Megan", @deck1)
+    @player2 = Player.new("Aurora", @deck2)
+    @turn = Turn.new(@player1, @player2)
+    expect(@turn.type).to eq(:mutually_assured_destruction)
   end
 
 end
