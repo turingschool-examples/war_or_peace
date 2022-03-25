5c8c2331905cc74344b5a51cc6b6a86791543a5a
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-03-25 10:48:18 -0600

    Add parameters for :basic in iteration 2

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,10 +3,16 @@ require './lib/deck'
 require './lib/player'
 
 class Turn
-    attr_reader :player_1, :player_2 #:spoils_of_war
-    def initialize(player1, player2) #spoils_of_war)
-        @player_1 = player1
-        @player_2 = player2
-        # @spoils_of_war = spoils_of_war
+    attr_reader :player1, :player2, :spoils_of_war
+    def initialize(player1, player2)
+        @player1 = player1
+        @player2 = player2
+        @spoils_of_war = []
     end
+
+    def type
+       return :basic if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0) 
+    end
+        
+
 end
