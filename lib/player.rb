4ad65a7ef948ba56da17c8463a6c78801ad26823
Zachary Hazelwood <zhaze001@gmail.com>
2022-03-25 11:19:55 -0600

    Add Player test

--- lib/player.rb
+++ lib/player.rb
@@ -1,1 +1,18 @@
 require './lib/deck'
+
+class Player
+  attr_reader :name, :deck
+
+  def initialize(name, deck)
+    @name = name
+    @deck = deck
+  end
+
+  def has_lost?
+    if deck.cards.count >= 1
+      false
+    else
+      true
+    end
+  end
+end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,1 +1,14 @@
 require './lib/player'
+
+describe Player do
+  it "has players" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    player = Player.new('Clarisa', deck)
+    expect(player.name).to eq('Clarisa')
+  end
+end
