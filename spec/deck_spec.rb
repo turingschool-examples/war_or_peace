c7dac77b1e1f1f5614ff07eb7fe706f7f6e565af
Luke Swenson <LukeSwenson95@outlook.com>
2022-03-25 10:35:13 -0600

    Added new test to create card

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -83,4 +83,23 @@ it "high ranking cards" do
 
   expect(deck.high_ranking_cards).to eq([card1, card3])
 end
+
+it "checks percentage of high ranking cards" do
+  card1 = Card.new(:diamond, 'Queen', 12)
+  card2 = Card.new(:spade, '3', 3)
+  card3 = Card.new(:heart, 'Ace', 14)
+  cards = [card1, card2, card3]
+  deck = Deck.new(cards)
+
+  expect(deck.percent_high_ranking).to eq(66.67)
+end
+
+it "creates new card" do
+  card1 = Card.new(:diamond, 'Queen', 12)
+  card2 = Card.new(:spade, '3', 3)
+  card3 = Card.new(:heart, 'Ace', 14)
+  cards = [card1, card2, card3]
+  deck = Deck.new(cards)
+  card4 = Card.new(:club, '5', 5)
+end
 end
