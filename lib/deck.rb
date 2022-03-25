22e322596e090910f5f9817d2bb3512bd506fab5
Sage Skaff <skaffsage@gmail.com>
2022-03-24 18:47:17 -0700

    Update deck.rb and deck_spec.rb

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,12 +1,29 @@
 class Deck
   attr_reader :cards
-
-  def initialize(deck)
-    @deck = deck
-    @cards = []
+  def initialize(cards)
+    @cards = cards
   end
-  
-  def add_card(card)
-    @cards << card
+
+  def rank_of_card_at(index)
+    @cards[index].rank
+  end
+
+  def high_ranking_cards
+    high_rank = []
+    @cards.each do |card|
+      if card.rank >= 11
+        high_rank << card
+      end
+    end
+    return high_rank
+  end
+
+  def percent_high_ranking
+    decimal = high_ranking_cards.count.to_f / @cards.count
+    percent = (decimal * 100).round(2)
   end
 end
+
+
+#  def add_card(cards)
+#    @cards << cards
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,23 +1,60 @@
 require 'rspec'
+require './lib/card'
 require './lib/deck'
 
 RSpec.describe Deck do
   it 'exists' do
-    deck = Deck.new(cards)
-
-    expect(deck).to be_an_instance_of(Deck)
-  end
-
-  it 'has readable attributes'
-    deck = Deck.new(cards)
-
-    expect(deck.cards).to eq([])
-  end
-
-  it 'can add cards' do
-    deck = Deck.new(cards)
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
-    deck.add_card(ca)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    expect(deck).to be_an_instance_of(Deck)
   end
+
+  it 'has readable attributes' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    expect(deck.cards).to eq([card1, card2, card3])
+  end
+
+  it 'can return rank of cards' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    expect(deck.rank_of_card_at(0)).to eq(12)
+    expect(deck.rank_of_card_at(2)).to eq(14)
+  end
+
+  it 'can return high ranking cards' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    expect(deck.high_ranking_cards).to eq([card1, card3])
+  end
+
+  it 'can return percent of high ranking cards' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    expect(deck.percent_high_ranking).to eq(66.67)
+  end
+
+  xit 'remove the top card from the deck' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    expect(deck.remove_card).to eq([card1])
+  end
+end
