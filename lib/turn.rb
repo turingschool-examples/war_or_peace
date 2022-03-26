c12fe783ecf8c0953d3219944d465c473d4ccf7d
Caden Jarrett <caden.jarrett99@gmail.com>
2022-03-25 18:39:16 -0600

    added test for pile_cards

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,41 +17,51 @@ class Turn
       @type = :basic
     elsif (player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0))
       @type = :war
-    else (player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)) && (player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2))
+    elsif (player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)) && (player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2))
       @type = :mutually_assured_destruction
     end
   end
 
   def winner
-    if @type == :basic
-      if (player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0))
-        @winner = player1
-      elsif (player1.deck.rank_of_card_at(0) < player2.deck.rank_of_card_at(0))
-        @winner = player2
+    if type == :basic
+      if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+        player1
+      elsif player1.deck.rank_of_card_at(0) < player2.deck.rank_of_card_at(0)
+        player2
       end
-    elsif @type == :war
+    elsif type == :war
       if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
-        @winner = player1
+        player1
       elsif player1.deck.rank_of_card_at(2) < player2.deck.rank_of_card_at(2)
-        @winner = player2
+        player2
       end
-    else
-      @winner = 'No winner'
-    end
-
-    def pile_cards
-      if @type = :basic
-        player1.deck(0) << spoils_of_war
-        player2.deck(0) << spoils_of_war
-      elsif @type = :war
-        player1.deck(0,1,2) << spoils_of_war
-        player2.deck(0,1,2) << spoils_of_war
-      else
-        player1.deck(0,1,2) << 
-        player2.deck(0,1,2) <<
-
-
+    elsif type == :mutually_assured_destruction
+      'No winner'
     end
   end
+
+  def pile_cards
+    if type == :basic
+      spoils_of_war << player1.deck.cards.shift
+      spoils_of_war << player2.deck.cards.shift
+    elsif type == :war
+      spoils_of_war << player1.deck.cards.shift(0, 1, 2)
+      spoils_of_war << player2.deck.cards.shift(0, 1, 2)
+    elsif type == :mutually_assured_destruction
+      player1.deck.cards.shift(0, 1, 2)
+      player2.deck.cards.shift(0, 1, 2)
+    end
+  end
+
+  # def award_spoils(winner)
+  #   require "pry";binding.pry
+  #   if winner == player1
+  #     player1.deck.cards << spoils_of_war
+  #   elsif
+  #     player2.deck.cards << spoils_of_war
+  #   elsif 'No Winner'
+  #
+  #   end
+  # end
 
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -47,7 +47,7 @@ RSpec.describe Turn do
       expect(turn.type).to eq(:basic)
     end
 
-    it "can determine a winner" do
+    it "can determine a winner of a turn type" do
       card1 = Card.new(:heart, 'Jack', 11)
       card2 = Card.new(:heart, '10', 10)
       card3 = Card.new(:heart, '9', 9)
@@ -61,9 +61,55 @@ RSpec.describe Turn do
       player1 = Player.new("Megan", deck1)
       player2 = Player.new("Aurora", deck2)
       turn = Turn.new(player1, player2)
+      winner = turn.winner
 
       expect(turn.type).to eq(:basic)
       expect(turn.winner).to eq(player1)
+    end
+
+    it "can pile cards together from two decks" do
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:heart, '9', 9)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, 'Queen', 12)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card3, card4, card6, card7])
+      player1 = Player.new("Megan", deck1)
+      player2 = Player.new("Aurora", deck2)
+      turn = Turn.new(player1, player2)
+      winner = turn.winner
+
+      expect(turn.type).to eq(:basic)
+      turn.pile_cards
+      expect(turn.player1.deck.cards).to eq([card2, card5, card8])
+      expect(turn.spoils_of_war).to eq([card1, card3])
+    end
+
+    xit "award spoils" do
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:heart, '9', 9)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, 'Queen', 12)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card3, card4, card6, card7])
+      player1 = Player.new("Megan", deck1)
+      player2 = Player.new("Aurora", deck2)
+      turn = Turn.new(player1, player2)
+      winner = turn.winner
+
+      expect(turn.type).to eq(:basic)
+      turn.pile_cards
+      turn.award_spoils(winner)
+      # require "pry";binding.pry
+      expect(turn.player1.deck.cards).to eq([card2, card5, card8, card1, card3,])
     end
   end
 end
