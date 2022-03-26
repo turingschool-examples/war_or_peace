7f1d7b2988541d6a6b0f59c41dc078f61d13420b
Lucas Colwell <colwellc37@gmail.com>
2022-03-26 00:03:27 -0600

    Add winner test

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,7 @@
+require './lib/card'
+require './lib/deck'
 require './lib/player'
+require 'pry'
 
 class Turn
   attr_reader :player1, :player2, :spoils_of_war
@@ -8,15 +11,58 @@ class Turn
     @player2 = player2
     @spoils_of_war = []
   end
-#
-# def type
-#   @basic = basic
-#   @war = war
-#   @mutually_assured_destruction = mutually_assured_destruction
-# end
-#
-# def basic?
-#   if player1.deck(0) != player2.deck(0)
-#     true
-#   end
+
+  def type
+    # @basic = basic   unsure why i added these...
+    # @war = war
+    # @mutually_assured_destruction = mutually_assured_destruction
+    if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
+      :basic
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+      :mutually_assured_destruction
+    else player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+      :war
+    end
+  end
+
+  def winner
+    if type == :basic
+      if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+        player1
+      elsif player1.deck.rank_of_card_at(0) < player2.deck.rank_of_card_at(0)
+        player2
+      end
+
+    elsif type == :war
+      if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+        player1
+      elsif player1.deck.rank_of_card_at(2) < player2.deck.rank_of_card_at(2)
+        player2
+      end
+
+    elsif type == :mutually_assured_destruction
+      'No winner'
+    end
+  end
+  #
+  # def pile_cards
+  #   if @type == :basic
+  #     @spoils_of_war << player1.deck.cards.shift
+  #     @spoils_of_war << player2.deck.cards.shift
+  #   elsif @type == :war
+  #     @spoils_of_war << player1.deck.cards.shift(0, 1, 2)
+  #     @spoils_of_war << player2.deck.cards.shift(0, 1, 2)
+  #   else
+  #     player1.deck.cards.shift(0, 1, 2)
+  #     player2.deck.cards.shift(0, 1, 2)
+  #   end
+  # end
+  #
+  # def award_spoils(winner)
+  #   if winner == player1
+  #     player1.deck.cards << spoils_of_war #.join can be used?
+  #   else winner == player2
+  #     player2.deck.cards << spoils_of_war
+  #   end
+  # end
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,9 +1,214 @@
+require 'rspec'
+require './lib/card'
+require './lib/deck'
 require './lib/player'
+require './lib/turn'
 
 describe Turn do
-  it "" do
+  it "exists" do
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+
+    expect(turn).to be_an_instance_of(Turn)
+  end
+
+  it "Can check if turn is basic" do
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+
+
+    expect(turn.type).to eq(:basic)
 
   end
+
+  it "Can check if turn is a war" do
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+
+
+    expect(turn.type).to eq(:war)
+
+  end
+
+
+  it "Can check for mutual destruction" do
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, '8', 8)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+
+    expect(turn.type).to eq(:mutually_assured_destruction)
+  end
+
+
+  it "Can determine the winner" do
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+
+    turn = Turn.new(player1, player2)
+
+    expect(turn.winner).to eq(player1)
+
+    # redefine variables to test a war scenario
+    # deck 2 array changes here
+
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    expect(turn.winner).to eq(player2)
+
+    # redefine variables to test a mutual_destr scenario
+    # card 6 changes here
+
+    card6 = Card.new(:diamond, '8', 8)
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+
+    expect(turn.winner).to eq('No winner')
+  end
+  #
+  # xit "can tell what turn it is" do
+  #   card1 = Card.new(:heart, 'Jack', 11)
+  #   card2 = Card.new(:heart, '10', 10)
+  #   card3 = Card.new(:heart, '9', 9)
+  #   card4 = Card.new(:diamond, 'Jack', 11)
+  #   card5 = Card.new(:heart, '8', 8)
+  #   card6 = Card.new(:diamond, 'Queen', 12)
+  #   card7 = Card.new(:heart, '3', 3)
+  #   card8 = Card.new(:diamond, '2', 2)
+  #
+  #   deck1 = Deck.new([card1, card2, card5, card8])
+  #   deck2 = Deck.new([card3, card4, card6, card7])
+  #
+  #   player1 = Player.new("Megan", deck1)
+  #   player2 = Player.new("Aurora", deck2)
+  #
+  #   turn = Turn.new(player1, player2)
+  #
+  #   expect(turn.type).to eq(:basic)
+  #
+  # end
+  #
+  #
+  #
+  #
+  #
+  #
+  # xit "Can pile cards" do
+  #   card1 = Card.new(:heart, 'Jack', 11)
+  #   card2 = Card.new(:heart, '10', 10)
+  #   card3 = Card.new(:heart, '9', 9)
+  #   card4 = Card.new(:diamond, 'Jack', 11)
+  #   card5 = Card.new(:heart, '8', 8)
+  #   card6 = Card.new(:diamond, 'Queen', 12)
+  #   card7 = Card.new(:heart, '3', 3)
+  #   card8 = Card.new(:diamond, '2', 2)
+  #
+  #   deck1 = Deck.new([card1, card2, card5, card8])
+  #   deck2 = Deck.new([card3, card4, card6, card7])
+  #
+  #   player1 = Player.new("Megan", deck1)
+  #   player2 = Player.new("Aurora", deck2)
+  #
+  #   turn = Turn.new(player1, player2)
+  #   expect(turn.type).to eq(:basic)
+  #   # turn.pile_cards
+  #   #
+  # end
+  #
+  # xit "can award spoils" do
+  #   card1 = Card.new(:heart, 'Jack', 11)
+  #   card2 = Card.new(:heart, '10', 10)
+  #   card3 = Card.new(:heart, '9', 9)
+  #   card4 = Card.new(:diamond, 'Jack', 11)
+  #   card5 = Card.new(:heart, '8', 8)
+  #   card6 = Card.new(:diamond, 'Queen', 12)
+  #   card7 = Card.new(:heart, '3', 3)
+  #   card8 = Card.new(:diamond, '2', 2)
+  #
+  #   deck1 = Deck.new([card1, card2, card5, card8])
+  #   deck2 = Deck.new([card3, card4, card6, card7])
+  #
+  #   player1 = Player.new("Megan", deck1)
+  #   player2 = Player.new("Aurora", deck2)
+  #
+  #   turn = Turn.new(player1, player2)
+  #   winner = turn.winner
+  #
+  #   expect(turn.type).to eq(:basic)
+  #   turn.pile_cards
+  #   turn.award_spoils(winner)
+  #
+  #   expect(turn.player1.deck.cards).to eq([card2, card5, card8, card1, card3])
+  # end
 
 
 end
