0ee12a4a3f4d5a54f01c0f47c9da2f71163db544
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-03-26 12:17:44 -0600

    Finish :destruction turn

--- spec/destruction_spec.rb
+++ spec/destruction_spec.rb
@@ -79,5 +79,48 @@ RSpec.describe Turn do
             
             expect(turn.winner).to eq("No Winner")
         end 
+
+        it 'piles the cards' do
+            card1 = Card.new(:heart, 'Jack', 11)
+            card2 = Card.new(:heart, '10', 10)  
+            card3 = Card.new(:heart, '9', 9)
+            card4 = Card.new(:diamond, 'Jack', 11) 
+            card5 = Card.new(:heart, '8', 8)
+            card6 = Card.new(:diamond, '8', 8)
+            card7 = Card.new(:heart, '3', 3) 
+            card8 = Card.new(:diamond, '2', 2)
+            deck1 = Deck.new([card1, card2, card5, card8])
+            deck2 = Deck.new([card4, card3, card6, card7])
+            player1 = Player.new("Megan", deck1)  
+            player2 = Player.new("Aurora", deck2)
+            turn = Turn.new(player1, player2)
+
+            turn.pile_cards
+            expect(player1.deck.cards).to eq([card8])
+            expect(player2.deck.cards).to eq([card7])
+        end
+
+        it 'awards spoils' do
+            card1 = Card.new(:heart, 'Jack', 11)
+            card2 = Card.new(:heart, '10', 10)  
+            card3 = Card.new(:heart, '9', 9)
+            card4 = Card.new(:diamond, 'Jack', 11) 
+            card5 = Card.new(:heart, '8', 8)
+            card6 = Card.new(:diamond, '8', 8)
+            card7 = Card.new(:heart, '3', 3) 
+            card8 = Card.new(:diamond, '2', 2)
+            deck1 = Deck.new([card1, card2, card5, card8])
+            deck2 = Deck.new([card4, card3, card6, card7])
+            player1 = Player.new("Megan", deck1)  
+            player2 = Player.new("Aurora", deck2)
+            turn = Turn.new(player1, player2)
+            winner = turn.winner
+            
+            turn.pile_cards
+            turn.award_spoils(winner)
+            expect(player1.deck.cards).to eq([card8])
+            expect(player2.deck.cards).to eq([card7])
+        end 
+
     end
 end
