84fd6f149c27f6cb1abe28fcaaffd83df09e9869
Luke Swenson <LukeSwenson95@outlook.com>
2022-03-26 16:40:27 -0600

    fixed issue with undefined method pile_cards

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -10,6 +10,7 @@ describe Turn do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
     deck = Deck.new(cards)
     card4 = Card.new(:club, '5', 5)
     card5 = Card.new(:heart, '8', 8)
@@ -28,6 +29,7 @@ describe Turn do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
     deck = Deck.new(cards)
     card4 = Card.new(:club, '5', 5)
     card5 = Card.new(:heart, '8', 8)
@@ -47,6 +49,7 @@ describe Turn do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
     deck = Deck.new(cards)
     card4 = Card.new(:club, '5', 5)
     card5 = Card.new(:heart, '8', 8)
@@ -81,7 +84,7 @@ describe Turn do
     expect(turn.winner).to eq(player1)
   end
 
-  it "define p" do
+  it "recieves spoils of war" do
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
     card3 = Card.new(:heart, '9', 9)
@@ -100,4 +103,24 @@ describe Turn do
     expect(turn.winner).to eq(player1)
     expect(turn.pile_cards).to eq([card1, card3])
   end
+
+  # it "determine if war pile works" do
+  #   card1 = Card.new(:heart, 'Jack', 11)
+  #   card2 = Card.new(:heart, '10', 10)
+  #   card3 = Card.new(:heart, '9', 9)
+  #   card4 = Card.new(:diamond, 'Jack', 11)
+  #   card5 = Card.new(:heart, '8', 8)
+  #   card6 = Card.new(:diamond, 'Queen', 12)
+  #   card7 = Card.new(:heart, '3', 3)
+  #   card8 = Card.new(:diamond, '2', 2)
+  #   deck1 = Deck.new([card1, card2, card5, card8])
+  #   deck2 = Deck.new([card3, card4, card6, card7])
+  #   player1 = Player.new("Megan", deck1)
+  #   player2 = Player.new("Aurora", deck2)
+  #   turn = Turn.new(player1, player2)
+  #
+  #   expect(turn.type).to eq(:basic)
+  #   expect(turn.winner).to eq(player1)
+  #   expect(turn.pile_cards).to eq([card1, card3])
+end
 end
