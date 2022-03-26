c9be67d91613ee03007faf57c03c0a104e902104
Lucas Colwell <colwellc37@gmail.com>
2022-03-26 17:08:49 -0600

    Start WOP runner prompt

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -1,1 +1,23 @@
+require 'rspec'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
 require './lib/game'
+
+puts "Enter name of player 1"
+player1 = gets.chomp
+puts "Enter name of player 2"
+player2 = gets.chomp
+
+p "Welcome to War (or Peace) This game will be played with 52 cards.
+The players today are #{player1} and #{player2}.
+Type 'GO' to start the game!"
+
+action = gets.chomp
+
+if action.upcase == "GO"
+  p "Game will start (when coded)"
+else
+  p "wrong word "
+end
