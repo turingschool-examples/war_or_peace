561b2e80cbedd1b144da3e2ef5586fdfaf1c7946
Lucas Colwell <colwellc37@gmail.com>
2022-03-27 01:37:54 -0600

    Finish full deck, start shuffle

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,13 +7,10 @@ require './lib/game'
 
 describe Game do
   it "exists" do
-    game = Game.new('Megan', 'Aurora')
+    game = Game.new
 
     expect(game).to be_an_instance_of(Game)
   end
-
-      # returns same cards with different codes.
-      # DO NOT WASTE ANY MORE TIME UNTIL EXPLAINED
 
   it "can make an accurate deck of 52 cards" do
     card1 = Card.new(:diamond, "Ace", 14)
@@ -73,13 +70,13 @@ describe Game do
     card52 = Card.new(:spade, "King", 13)
 
     full_deck = Deck.new([card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11, card12, card13, card14, card15, card16, card17, card18, card19, card20, card21, card22, card23, card24, card25, card26, card27, card28, card29, card30, card31, card32, card33, card34, card35, card36, card37, card38, card39, card40, card41, card42, card43, card44, card45, card46, card47, card48, card49, card50, card51, card52])
-    game = Game.new('Megan', 'Aurora')
+    game = Game.new
 
-    expect(game.full_deck).to eq(game.make_new_deck)
+    expect(game.full_deck.cards).to eq(game.make_new_deck.cards)
 
   end
 
-  it "Can shuffle a deck" do
+  xit "Can shuffle a deck" do
     card1 = Card.new(:diamond, "Ace", 14)
     card2 = Card.new(:diamond, "2", 2)
     card3 = Card.new(:diamond, "3", 3)
@@ -137,10 +134,12 @@ describe Game do
     card52 = Card.new(:spade, "King", 13)
 
     full_deck = Deck.new([card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11, card12, card13, card14, card15, card16, card17, card18, card19, card20, card21, card22, card23, card24, card25, card26, card27, card28, card29, card30, card31, card32, card33, card34, card35, card36, card37, card38, card39, card40, card41, card42, card43, card44, card45, card46, card47, card48, card49, card50, card51, card52])
-    game = Game.new('Megan', 'Aurora')
+    game = Game.new
+
+    puts game.shuffle_deck
 
       #this is probably not the way, look up to not_eq ???
-    expect(game.full_deck).not_to eq(game.shuffle_deck)
+    # expect(game.full_deck).not_to eq(game.shuffle_deck)
   end
 
 
