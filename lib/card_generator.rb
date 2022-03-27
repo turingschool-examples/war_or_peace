89bc465370120a59fa6fd3e55a9e98504d6fa665
Andrew King <dev.andrewking@gmail.com>
2022-03-27 15:10:29 -0500

    fix issue with extra spaces and wrong variable being turned into int

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -9,9 +9,9 @@ class Card_Generator
     File.foreach(file_name) do |line|
       line.chomp!
       card_array = line.lines(',')
-      value = card_array[0].delete(',').delete(' ').to_i
+      value = card_array[0].delete(',').delete(' ')
       suit = card_array[1].delete(',').delete(' ')
-      rank = card_array[2].delete(',').delete(' ')
+      rank = card_array[2].delete(',').delete(' ').to_i
       @cards << Card.new(suit, value, rank)
     end
   end
