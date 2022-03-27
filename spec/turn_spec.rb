ec8a69cc08220db9996ebad597eb868ca75c503d
Shirley DeCesari <shirley.decesari@gmail.com>
2022-03-26 22:06:27 -0700

    Add winner test war and mad

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -76,8 +76,8 @@ RSpec.describe Turn do
     player2 = Player.new("Aurora", deck2)
     turn = Turn.new(player1, player2)
 
-      expect(turn.type).to eq(:mutually_assured_destruction)
-      expect(turn.winner).to eq("No Winner")
+    expect(turn.type).to eq(:mutually_assured_destruction)
+    expect(turn.winner).to eq("No Winner")
   end
 
   it 'has a pile for basic' do
@@ -158,7 +158,52 @@ RSpec.describe Turn do
 
     turn.pile_cards
     turn.award_spoils(winner)
+    # binding.pry
     expect(player1.deck.cards).to eq([card2, card5, card8, card1, card3])
+  end
 
+  it 'will add cards to the winner of war' do
+
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    winner = turn.winner
+
+    turn.pile_cards
+    turn.award_spoils(winner)
+    # binding.pry #use binding.pry then player1 to see what cards they have
+    expect(player1.deck.cards).to eq([card8, card1, card4, card2, card3, card5, card6])
+  end
+
+  it 'will add cards to the winner of mutually_assured_destruction' do
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, '8', 8)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card4, card3, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    winner = turn.winner
+
+    turn.pile_cards
+    turn.award_spoils(winner)
+    # binding.pry
+    expect(player1.deck.cards).to eq([card8])
   end
 end
