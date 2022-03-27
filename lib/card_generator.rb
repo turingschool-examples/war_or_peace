ef107c7df03dccee9f570bea2717a8ee3fa8c739
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-03-27 18:55:24 -0400

    Continue to work on cards.txt

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,12 +1,22 @@
 class CardGenerator
 
+attr_reader :cards
+
   def initialize(file)
     @file = file
+    @cards = []
   end
 
   def cards
     File.open("#{@file}").readlines
     file_data = File.readlines("#{@file}", chomp: true)
+
+    file_data.each do |card|
+    card_data = file_data.split(",")
+    value = card_data[0]
+    suit = card_data[1].to_sym
+    rank = card_data[2].to_i
+    end
   end
 
 end
