b218174bc50e9fdec7b731ad8dbe5f4764368b4f
Andrew King <dev.andrewking@gmail.com>
2022-03-27 15:11:29 -0500

    add decsribe to tests

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -28,21 +28,29 @@ RSpec.describe Deck do
     expect(@deck.rank_of_card_at(0)).to eq(12)
   end
 
+describe "high_ranking_cards" do
   it "lists all high ranking cards" do
     expect(@deck.high_ranking_cards.length).to eq(2)
   end
+end
 
+describe "#percent_high_ranking" do
   it "gives percentage of high ranking cards" do
     expect(@deck.percent_high_ranking).to eq(66.67)
   end
+end
 
+describe "#remove_card" do
   it "removes card from top of deck" do
     expect(@deck.remove_card).to eq(@card)
   end
+end
 
+describe "#add_card" do
   it "adds card to bottom of deck" do
     @deck.add_card(@card4)
     expect(@deck.cards.length).to eq(4)
   end
+end
 
 end
