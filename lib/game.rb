8ff0ee4566d94929bc4ce00d52855253a0a9b632
Shirley DeCesari <shirley.decesari@gmail.com>
2022-03-27 15:04:00 -0700

    Move start method to game file

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,15 @@
 require '.lib/turn'
 class Game
-  attr_reader :deck
-  def  initialize
+  attr_reader :deck 
+  def initialize
 
   end
 
-  def start_game
-
+  def start
+    p "Welcome to War! (or peace) This game will be played with 52 cards."
+    p "The players today are #{player1.name} and #{player2.name}."
+    p "Type 'GO' to start the game!"
+    p "_" * 10
   end
+
 end
