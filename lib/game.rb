20864b5f6898fba01aecc7ce884dcc3b32c50f59
Matt Deming <vballmatt44@gmail.com>
2022-03-26 23:03:30 -0600

    create game.rb and game_spec.rb and add 52 cards

---
+++ lib/game.rb
@@ -0,0 +1,23 @@
+require 'rspec'
+require './lib/card.rb'
+require './lib/deck.rb'
+require './lib/player.rb'
+require './lib/turn.rb'
+require './lib/game.rb'
+require 'pry'
+
+
+class Game
+attr_reader :full_deck
+def initialize
+  @full_deck = deck
+end
+
+
+
+
+
+  def start
+
+  end
+end
