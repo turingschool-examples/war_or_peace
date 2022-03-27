9bb16cf75bfc1a888a6fc3a33084512985d27480
Andrew King <dev.andrewking@gmail.com>
2022-03-27 15:20:48 -0500

    fix shuffle array method implementation

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -11,9 +11,8 @@ RSpec.describe Game do
   before(:each) do
     file_name = './lib/cards.txt'
     generator = Card_Generator.new(file_name)
-    generator.cards.shuffle
-    @deck1 = Deck.new([generator.cards[0..25]])
-    @deck2 = Deck.new(generator.cards[26..52])
+    @deck1 = Deck.new(generator.cards[0..25].shuffle)
+    @deck2 = Deck.new(generator.cards[26..52].shuffle)
     @player1 = Player.new("Megan", @deck1)
     @player2 = Player.new("Aurora", @deck2)
   end
@@ -23,5 +22,13 @@ RSpec.describe Game do
     expect(game).to be_an_instance_of(Game)
   end
 
-  
+describe "#start" do
+  it "start should run until player_lost? is fulfilled" do
+    game = Game.new(@player1, @player2)
+    game.start
+    expect(@player1.deck.cards.length).to eq(0)
+  end
+end
+
+
 end
