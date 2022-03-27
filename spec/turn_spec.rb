19866e29215bab6fd9f037691a043013abc00100
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-03-26 18:19:30 -0700

    tested pile_cards basic

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,6 +3,7 @@ require './lib/card'
 require './lib/deck'
 require './lib/player'
 require './lib/turn'
+# require 'pry'; binding.pry
 
 RSpec.describe Turn do
   it "exists" do
@@ -23,7 +24,7 @@ RSpec.describe Turn do
     expect(turn).to be_an_instance_of(Turn)
   end
 
-  it "has a turn type basic" do
+  it "has a turn type basic and winner" do
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
     card3 = Card.new(:heart, '9', 9)
@@ -42,7 +43,7 @@ RSpec.describe Turn do
     expect(turn.winner).to eq(player1)
   end
 
-  it "has a turn type war" do
+  it "has a turn type war and winner" do
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
     card3 = Card.new(:heart, '9', 9)
@@ -61,7 +62,7 @@ RSpec.describe Turn do
     expect(turn.winner).to eq(player2)
   end
 
-  it "has a turn type mutually_assured_destruction" do
+  it "has a turn type mutually_assured_destruction and no winner" do
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
     card3 = Card.new(:heart, '9', 9)
@@ -79,4 +80,25 @@ RSpec.describe Turn do
     expect(turn.type).to eq(:mutually_assured_destruction)
     expect(turn.winner).to eq("No Winner")
   end
+
+  it "has a pile_cards for basic" do
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    turn.pile_cards
+
+    expect(turn.spoils_of_war).to eq([card1, card3])
+  end
+
+
 end
