66906995f802638c6b7641910d573deaef211725
Andrew King <dev.andrewking@gmail.com>
2022-03-27 15:53:09 -0500

    add failsafe for when both players don't have enough cards to evaluate war

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,16 +15,24 @@ class Turn
   end
 
   def type
-    if(@player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0))
-      return :basic
 
-    elsif((@player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)) &&
-       (@player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)))
-       return :mutually_assured_destruction
+    if(@player1.deck.cards.length <= 2 && @player2.deck.cards.length <= 2)
+      puts "---- DRAW ----"
+      exit(true)
 
-     else
-       return :war
-     end
+    else
+
+      if(@player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0))
+        return :basic
+
+      elsif((@player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)) &&
+        (@player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)))
+        return :mutually_assured_destruction
+
+      else
+        return :war
+      end
+    end
   end
 
   def winner(type)
