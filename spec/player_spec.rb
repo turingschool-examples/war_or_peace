dc819a8f6d50e1ad0e118dccb79d464d6d0c3044
Krisetn Nestler <kristen.nestler@gmail.com>
2022-08-17 21:22:31 -0400

    add player class and fix spec

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -11,13 +11,17 @@ RSpec.describe do
     @player = Player.new('Clarisa', @deck)
   end
 
+  it "exists" do 
+    expect(@player).to be_an_instance_of(Player)
+  end
+
   it 'has attributes' do 
     expect(@player.name).to eq("Clarisa")
-    expect(@player.deck).to eq([@card1, @card2, @card3])
+    expect(@player.deck).to be_an_instance_of(Deck)
   end
     
   it 'can identify if a card has been removed and a player has lost' do   
-    expect(@player.has_lost?).to be false
+    expect(@player.has_lost?).to eq(false)
     @player.deck.remove_card
     expect(@player.has_lost?).to eq(false)
     @player.deck.remove_card
@@ -27,6 +31,11 @@ RSpec.describe do
   end 
 
   it "can tell how many cards are in a players deck" do 
-    expect(@player.deck).to eq([])
+    @player.deck.remove_card
+    @player.deck.remove_card
+    expect(@player.deck.cards).to eq(@cards=[@card3])
+    @player.deck.remove_card
+    expect(@player.deck.cards).to eq(@cards=[])
   end 
+
 end 
