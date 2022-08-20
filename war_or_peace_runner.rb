d746d7fb370a6722114ffe66ff323566783e0ba8
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-08-20 12:26:56 -0600

    Comment old game loop and insert new game loop

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -36,37 +36,21 @@ game.start(player1, player2)
 
 counter = 1
 
-turn = Turn.new(player1, player2)
+# turn = Turn.new(player1, player2)
 until counter == 10000
 
-  case turn.type
-  when :mutually_assured_destruction
-    puts "Turn #{counter}: *mutually assured destruction* 6 cards removed from play"
-  when :war
-    puts "Turn #{counter}: WAR - #{turn.winner.name} won 6 cards"
-  when :basic
-    puts "Turn #{counter}: #{turn.winner.name} won 2 cards"
-  else
-    puts "this type #{turn.type} DID NOT MATCH"
-  end
+  # case turn.type
+  # when :mutually_assured_destruction
+  #   puts "Turn #{counter}: *mutually assured destruction* 6 cards removed from play"
+  # when :war
+  #   puts "Turn #{counter}: WAR - #{turn.winner.name} won 6 cards"
+  # when :basic
+  #   puts "Turn #{counter}: #{turn.winner.name} won 2 cards"
+  # else
+  #   puts "this type #{turn.type} DID NOT MATCH"
+  # end
 
-  turn.award_spoils(turn.pile_cards)
-
-  # HOW CAN SPOILS BE AWARDED TO THE WINNER OF THE TURN PRIOR TO PILING CARDS?
-
-  break if player1.has_lost? || player2.has_lost?
-
-  counter += 1
-end
-
-if player1.has_lost?
-  puts "*~*~*~* #{player2.name} has won the game! *~*~*~*"
-elsif player2.has_lost?
-  puts "*~*~*~* #{player1.name} has won the game! *~*~*~*"
-elsif counter == 1_000_000
-  puts '---- DRAW ----'
-end
-
+  # turn.award_spoils(turn.pile_cards)
 until counter == 1_000_000
   turn = Turn.new(player1, player2)
   case turn.winner
@@ -99,3 +83,18 @@ until counter == 1_000_000
   
   counter += 1
 end 
+  
+
+  break if player1.has_lost? || player2.has_lost?
+
+  counter += 1
+end
+
+if player1.has_lost?
+  puts "*~*~*~* #{player2.name} has won the game! *~*~*~*"
+elsif player2.has_lost?
+  puts "*~*~*~* #{player1.name} has won the game! *~*~*~*"
+elsif counter == 1_000_000
+  puts '---- DRAW ----'
+end
+
