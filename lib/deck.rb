1d20afe39a6eaba1bf26c7f0c19cfd8c96a07ee6
Eli Fuchsman <efuchsman@gmail.com>
2022-08-23 10:10:31 -0600

    Define method for finding high ranking cards

--- lib/deck.rb
+++ lib/deck.rb
@@ -3,18 +3,34 @@ attr_reader :cards
 
   def initialize(cards = [])
   @cards = cards
+  @high_rank = false
+  @high_ranking_cards =[]
+
   end
 
-  def cards?
+  def sim_deck
   @cards
   end
 
-  def new_card(card)
+  def add_card(card)
     @cards << card
+    if card.rank >= 11
+      @high_rank = true
+      @high_ranking_cards << card
+    else
+      false
+    end
+
   end
 
   def rank_of_card_at(card)
     card.rank
+
   end
+
+  def high_ranking_cards
+    @high_ranking_cards
+  end
+
 
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -19,15 +19,15 @@ RSpec.describe Card do
 
   it "has no cards when created" do
     deck = Deck.new
-    expect(deck.cards?).to be_empty
+    expect(deck.sim_deck).to be_empty
   end
 
    it "can have a card in a deck" do
       deck = Deck.new
       card = Card.new(:heart, 'Jack', 11)
 
-      deck.new_card(card)
-      expect(deck.cards?.count).to eq (1)
+      deck.add_card(card)
+      expect(deck.sim_deck.count).to eq (1)
    end
 
    it "can have multiple cars in a deck" do
@@ -36,11 +36,11 @@ RSpec.describe Card do
     card_2 = Card.new(:spade, '3', 3)
     card_3 = Card.new(:heart, 'Ace', 14)
 
-    deck.new_card(card_1)
-    deck.new_card(card_2)
-    deck.new_card(card_3)
+    deck.add_card(card_1)
+    deck.add_card(card_2)
+    deck.add_card(card_3)
 
-    expect(deck.cards?.count).to eq(3)
+    expect(deck.sim_deck.count).to eq(3)
    end
 
    it 'can call rank' do
@@ -49,9 +49,9 @@ RSpec.describe Card do
     card_2 = Card.new(:spade, '3', 3)
     card_3 = Card.new(:heart, 'Ace', 14)
 
-    deck.new_card(card_1)
-    deck.new_card(card_2)
-    deck.new_card(card_3)
+    deck.add_card(card_1)
+    deck.add_card(card_2)
+    deck.add_card(card_3)
 
     deck.rank_of_card_at(card_1)
     deck.rank_of_card_at(card_3)
@@ -61,6 +61,25 @@ RSpec.describe Card do
 
    end
 
+  it 'can check for high ranking cards' do
+    deck = Deck.new
+    card_1 = Card.new(:diamond, 'Queen', 12)
+    card_2 = Card.new(:spade, '3', 3)
+    card_3 = Card.new(:heart, 'Ace', 14)
+    card_4 = Card.new(:club, 'King', 13)
+    card_5 = Card.new(:diamond, '7', 7)
+    card_6 = Card.new(:heart, '8', 8)
 
+    deck.add_card(card_1)
+    deck.add_card(card_2)
+    deck.add_card(card_3)
+    deck.add_card(card_4)
+    deck.add_card(card_5)
+    deck.add_card(card_6)
+
+    expect(deck.sim_deck.count).to eq(6)
+    expect(deck.high_ranking_cards.count).to eq(3)
+    expect(deck.high_ranking_cards).to eq([card_1, card_3, card_4])
+  end
 
 end
