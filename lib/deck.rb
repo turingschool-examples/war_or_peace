26531625a4fd0452289163d15f2fe0f35216a3f6
Ryan Loughry <thecoderanger5280@icloud.com>
2022-08-23 18:24:57 -0600

    Finish high card method

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,8 +2,17 @@ class Deck
   attr_reader :cards
   def initialize(cards)
     @cards = cards
+    @high_cards = []
   end
   def rank_of_card_at(index)
     @cards[index].rank
+  end
+  def high_ranking_cards
+    @cards.each do |card|
+      if(card.rank > 10)
+        @high_cards << card
+      end
+    end
+    @high_cards
   end
 end
