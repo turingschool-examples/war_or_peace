97265b0ba05194b34f0ed3fe7ee09dd8b48de439
Amanda Ross <amikaross@gmail.com>
2022-08-24 14:34:24 -0600

    Update turn.rb to pass #initialize tests

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,3 +2,14 @@ require "./card.rb"
 require "./deck.rb"
 require "./player.rb"
 
+class Turn 
+  attr_reader :player1,
+              :player2,
+              :spoils_of_war 
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @spoils_of_war = []
+  end
+end 
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -2,30 +2,41 @@ require "rspec"
 require "./lib/turn"
 
 RSpec.describe Turn do
-  before(:each) do 
-    card1 = Card.new(:heart, "Jack", 11)
-    card2 = Card.new(:heart, "10", 10)
-    card3 = Card.new(:heart, "9", 9)
-    card4 = Card.new(:diamond, "Jack", 11)
-    card5 = Card.new(:heart, "8", 8)
-    card6 = Card.new(:diamond, "Queen", 12)
-    card7 = Card.new(:heart, "3", 3)
-    card8 = Card.new(:diamond, "2", 2)
-    deck1 = Deck.new([card1, card2, card5, card8])
-    deck2 = Deck.new([card3, card4, card6, card7])
-    player1 = Player.new("Megan", deck1)
-    player2 = Player.new("Aurora", deck2)
-    turn = Turn.new(player1, player2)
-  end
-  
   describe "#initialize" do
     it "exists" do 
+      card1 = Card.new(:heart, "Jack", 11)
+      card2 = Card.new(:heart, "10", 10)
+      card3 = Card.new(:heart, "9", 9)
+      card4 = Card.new(:diamond, "Jack", 11)
+      card5 = Card.new(:heart, "8", 8)
+      card6 = Card.new(:diamond, "Queen", 12)
+      card7 = Card.new(:heart, "3", 3)
+      card8 = Card.new(:diamond, "2", 2)
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card3, card4, card6, card7])
+      player1 = Player.new("Megan", deck1)
+      player2 = Player.new("Aurora", deck2)
+      turn = Turn.new(player1, player2)
       expect(turn).to be_a Turn 
     end
 
     it "has readable attributes" do
+      card1 = Card.new(:heart, "Jack", 11)
+      card2 = Card.new(:heart, "10", 10)
+      card3 = Card.new(:heart, "9", 9)
+      card4 = Card.new(:diamond, "Jack", 11)
+      card5 = Card.new(:heart, "8", 8)
+      card6 = Card.new(:diamond, "Queen", 12)
+      card7 = Card.new(:heart, "3", 3)
+      card8 = Card.new(:diamond, "2", 2)
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card3, card4, card6, card7])
+      player1 = Player.new("Megan", deck1)
+      player2 = Player.new("Aurora", deck2)
+      turn = Turn.new(player1, player2)
       expect(turn.player1).to eq player1
       expect(turn.player2).to eq player2
+      expect(turn.spoils_of_war).to eq []
     end
   end 
 end 
