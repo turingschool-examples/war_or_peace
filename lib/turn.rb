447a642255d4e0858ef2a6db46b610975e47d56a
Eli Fuchsman <efuchsman@gmail.com>
2022-08-24 11:36:59 -0600

    Create pile cards method

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,12 +3,13 @@ require 'card.rb'
 require 'player.rb'
 
 class Turn
- attr_reader :player_1, :player_2, :spoils_of_war
+ attr_reader :player_1, :player_2, :spoils_of_war, :pile_cards_arr
 
  def initialize(player_1 , player_2)
     @player_1 = player_1
     @player_2 = player_2
     @spoils_of_war_arr = []
+    @pile_cards_arr = []
   end
 
   def spoils_of_war
@@ -30,6 +31,15 @@ class Turn
       player_1
     elsif type == :basic && player_2.deck[0].rank > player_1.deck[0].rank
     player_2
+    end
+  end
+
+  def pile_cards
+    if type == :basic
+      @spoils_of_war_arr << player_1.deck[0]
+      @spoils_of_war_arr << player_2.deck[0]
+      player_1.deck.shift
+      player_2.deck.shift
     end
   end
 
