82dc9f6e344ef2718e0d963297c3377b389d7116
Rich Kaht <adamselene1776@protonmail.com>
2022-08-23 20:58:42 -0600

    Add deck methods and additional deck tests

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,3 +1,11 @@
 class Deck
+  attr_reader :cards
   
+  def initialize(cards = [])
+    @cards = cards
+  end
+  
+  def rank_of_card_at(index_position)
+    cards[index_position].rank
+  end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Deck do
     expect(deck).to be_an_instance_of(Deck)
   end
   
-  xit 'has an array of cards' do
+  it 'has an array of cards' do
     deck = Deck.new
     expect(deck.cards).to eq([])
   end
@@ -20,9 +20,85 @@ RSpec.describe Deck do
     card3 = Card.new(:diamond, '8', 8)
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
-    
+
     expect(deck.rank_of_card_at(0)).to eq(14)
     expect(deck.rank_of_card_at(2)).to eq(8)
+  end
     
+  it 'has a method to return face cards and aces' do
+    card1 = Card.new(:spade, 'Ace', 14)
+    card2 = Card.new(:heart, '3', 3)
+    card3 = Card.new(:diamond, 'Queen', 12)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    
+    expect(deck.high_ranking_cards).to be([card0, card2])
+  end
+  
+  it 'has a method to return the percentage of high 
+  ranking cards' do
+    card1 = Card.new(:spade, 'Ace', 14)
+    card2 = Card.new(:heart, '3', 3)
+    card3 = Card.new(:diamond, 'Queen', 12)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    
+    expect(deck.percent_high_ranking).to eq(2/3)
+  end
+  it 'has a method to remove one card from the top of the deck' do
+    card1 = Card.new(:spade, 'Ace', 14)
+    card2 = Card.new(:heart, '3', 3)
+    card3 = Card.new(:diamond, 'Queen', 12)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    
+    expect(deck.remove_card).to eq(card1)
+    expect(deck.cards).to eq(cards.shift)
+  end
+  
+  it 'has a method to add one card to the bottom of the deck' do
+    card1 = Card.new(:spade, 'Ace', 14)
+    card2 = Card.new(:heart, '3', 3)
+    card3 = Card.new(:diamond, 'Queen', 12)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    
+    card4 = Card.new(:club, '7', 7)
+    deck.add_card(card4)
+    
+    expect(cards[3]).to eq(card4)
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
