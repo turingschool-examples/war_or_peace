feb45d3e7f8641e74b2c98acb28602fb075a7a9e
Will Wang <willy.j.wang@gmail,com>
2022-08-24 18:57:44 -0500

    Write deck class remove_card method test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -55,4 +55,15 @@ RSpec.describe Deck do
       expect(deck.percent_high_ranking).to eq(66.67)
     end
   end
+
+  describe '#remove_card' do
+    it "can remove top card" do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      expect(deck.remove_card).to eq(deck[0])
+    end
+  end
 end
