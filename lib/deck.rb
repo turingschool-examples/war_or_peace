04b424ec2f13bd44d8054072596888fa18653827
Bryan Flanagan <bflanagan78@yahoo.com>
2022-08-24 16:56:31 -0600

    Add methods to deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,6 +1,15 @@
 class Deck
-  attr_reader :cards
-  def initialize(cards)
+  attr_reader :cards, :rank_of_card_at, :high_ranking_cards, :percent_high_ranking
+  def initialize(cards, rank_of_card_at, high_ranking_cards, percent_high_ranking)
     @cards = cards
+    @rank_of_card_at = rank_of_card_at
+    @high_ranking_cards = high_ranking_cards
+    @percent_high_ranking = percent_high_ranking
   end
+
+  def rank_of_card_at
+    @card(2)
+    if @card(2) >= 11
+  end
+
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -9,4 +9,19 @@ RSpec.describe Deck do
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
   end
+
+  it "#has a card rank" do
+    deck.rank_of_card_at(0)
+    expect
+  end
+
+  it "#has high ranking cards" do
+    deck.high_ranking_cards
+    expect(@high_ranking_cards).to eq (card1, card3)
+  end
+
+  it "#has a percentage of high ranking cards" do
+    deck.percent_high_ranking
+    expect(@percent_high_ranking).to eq 66.67%
+  end
 end
