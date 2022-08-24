1238686a3910f79dc7ad9f27577e6cbb5429aaf8
Eli Fuchsman <efuchsman@gmail.com>
2022-08-23 23:24:37 -0600

    Complete intialize

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,10 +3,22 @@ require 'card.rb'
 require 'player.rb'
 
 class Turn
- attr_reader :player , :type
-  def initialize(player, type)
-    @player = player.name
-    @type = type
+ attr_reader :player_1, :player_2, :spoils_of_war
+
+ def initialize(player_1 , player_2)
+    @player_1 = player_1
+    @player_2 = player_2
+    @spoils_of_war = []
+
+
+  end
+
+  def type
+
+
+
+
+
   end
 
 
