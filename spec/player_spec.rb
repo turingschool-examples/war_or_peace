add47e8f9c7e09f4e2693b2f2d0b6cfb5aeab83e
Amanda Ross <amikaross@gmail.com>
2022-08-24 10:26:44 -0600

    Update player_spec to include #has_lost test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -25,4 +25,23 @@ RSpec.describe Player do
       expect(player.deck).to eq deck
     end
   end
+
+  describe "#has_lost" do 
+    it "Returns correct boolean based on players deck" do 
+      card1 = Card.new(:diamond, "Queen", 12)
+      card2 = Card.new(:spade, "3", 3)
+      card3 = Card.new(:heart, "Ace", 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      player = Player.new("Clarisa", deck)
+
+      expect(player.has_lost?).to eq false
+      player.deck.remove_card
+      expect(player.has_lost?).to eq false
+      player.deck.remove_card
+      expect(player.has_lost?).to eq false
+      player.deck.remove_card
+      expect(player.has_lost?).to eq true
+    end
+  end
 end
