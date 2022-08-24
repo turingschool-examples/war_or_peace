bdd97984e84206844d65f8f2bba6af1a635b5286
Eli Fuchsman <efuchsman@gmail.com>
2022-08-24 00:00:01 -0600

    Complete type method boolean

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,17 +8,21 @@ class Turn
  def initialize(player_1 , player_2)
     @player_1 = player_1
     @player_2 = player_2
-    @spoils_of_war = []
+    @spoils_of_war_arr = []
+  end
 
-
+  def spoils_of_war
+    @spoils_of_war_arr
   end
 
   def type
-
-
-
-
-
+    if player_1.deck[0].rank != player_2.deck[0].rank
+    :basic
+    elsif player_1.deck[0].rank == player_2.deck[0].rank
+      :war
+    elsif player_1.deck[0].rank == player_2.deck[0].rank && player_1.deck[2].rank == player_2.deck[2].rank
+      :mutually_assured_destruction
+    end
   end
 
 
