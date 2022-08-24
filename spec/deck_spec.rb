2a9eba2f08ce5a3b479602275508fb5ae4b9434b
Will Wang <willy.j.wang@gmail,com>
2022-08-24 17:51:50 -0500

    Write initialize and rank_of_card_at tests for deck class

---
+++ spec/deck_spec.rb
@@ -0,0 +1,36 @@
+require 'rspec'
+require './lib/card'
+require './lib/deck'
+
+RSpec.describe Deck do
+  describe '#initialize' do
+    it "exists" do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      expect(deck).to be_an_instance_of(Deck)
+    end
+    it "has readable cards attribute" do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      expect(deck.cards).to eq(cards)
+    end
+  end
+
+  describe '#rank_of_card_at' do
+    it "can read card rank at given position" do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      expect(deck.rank_of_card_at(0)).to eq(12)
+      expect(deck.rank_of_card_at(2)).to eq(14)
+    end
+  end
+end
