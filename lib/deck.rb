c7aff596b1749580c7d6d5a77b35f44360654922
Darby Smith <newman.darby2@gmail.com>
2022-08-24 20:58:49 -0400

    Add rank card method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,11 +1,11 @@
 class Deck 
   attr_reader :cards 
 
-  def initialize
-    @cards = []
+  def initialize(cards)
+    @cards = cards
   end 
 
-  def rank_of_card_at(card_name)
-    card_name.rank
+  def rank_of_card_at(index)
+    @cards[index].rank
   end 
 end 
