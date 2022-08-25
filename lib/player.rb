391ad866f8cfd13226eac5993d23a3d628903b6f
Ryan Loughry <thecoderanger5280@icloud.com>
2022-08-24 19:30:09 -0600

    finish third test player class

--- lib/player.rb
+++ lib/player.rb
@@ -5,4 +5,12 @@ class Player
     @name = name
     @deck = deck
   end
+
+  def has_lost?
+    if(@deck.cards.length == 0)
+      true
+    else
+      false
+    end
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -28,4 +28,22 @@ RSpec.describe Player do
       expect(player.deck).to eq(deck)
     end
   end
+  describe '#has_lost' do
+    it 'returns if the player has lost' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      player = Player.new('Ryan', deck)
+
+      player.deck.remove_card
+      expect(player.has_lost?).to eq(false)
+      player.deck.remove_card
+      expect(player.has_lost?).to eq(false)
+      player.deck.remove_card
+      expect(player.has_lost?).to eq(true)
+
+    end
+  end
 end
