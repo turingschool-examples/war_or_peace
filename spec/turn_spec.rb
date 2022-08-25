0a30337f96ce5be8bcf83a4c68cc1ca2965631c3
Will Wang <willy.j.wang@gmail,com>
2022-08-25 18:06:57 -0500

    Create turn class and test file

---
+++ spec/turn_spec.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+
+RSpec.describe Turn do
+  describe '#initialize' do
+    it 'exists' do
+
+    end
+  end
+end
