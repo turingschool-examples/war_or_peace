e2284f583159026aab0be082862087a1da99b4c3
Ryan Loughry <thecoderanger5280@icloud.com>
2022-08-24 19:22:38 -0600

    finish second test of player class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -15,5 +15,17 @@ RSpec.describe Player do
 
       expect(player).to be_an_instance_of(Player)
     end
+
+    it 'has attributes' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      player = Player.new('Ryan', deck)
+
+      expect(player.name).to eq('Ryan')
+      expect(player.deck).to eq(deck)
+    end
   end
 end
