f736dd3315aa373fd1170f7d2022045a8cbe7f70
Darby Smith <newman.darby2@gmail.com>
2022-08-24 21:11:25 -0400

    Test rank_of_card_at method

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -9,29 +9,33 @@ RSpec.describe Deck do
     @card3 = Card.new(:heart, 'Ace', 14)
     @cards = [@card1, @card2, @card3]
   end
-  
-  it 'is an instance of deck' do 
-    deck_1 = Deck.new(@cards)
-    expect(deck_1).to be_instance_of Deck
+  describe '#initialize' do 
+    it 'is an instance of deck' do 
+      deck_1 = Deck.new(@cards)
+      expect(deck_1).to be_instance_of Deck
+    end 
+
+    it 'has cards' do 
+      deck_1 = Deck.new(@cards)
+      expect(deck_1.cards).to eq([@card1, @card2, @card3]) 
+    end 
   end 
-  
-  it 'has cards' do 
-    deck_1 = Deck.new(@cards)
-    expect(deck_1.cards).to eq([@card1, @card2, @card3]) 
+
+  describe '#rank_of_card_at' do 
+    it 'returns the rank of that card' do 
+      deck_1 = Deck.new(@cards)
+      
+      expect(deck_1.rank_of_card_at(0)).to eq(12)
+      expect(deck_1.rank_of_card_at(2)).to eq(14)
+    end      
   end 
 
-  xit 'return the rank of that card' do 
-    deck_1 = Deck.new(cards)
-     
-    expect(deck.rank_of_card_at(0)).to eq(12)
-    expect(deck.rank_of_card_at(2)).to eq(14)
-  end      
+  describe '#high_ranking_card' do
+    it 'returns an array of high ranking cards' do 
+      deck_1 = Deck.new(@cards)
 
-  # xit 'returns an array of high ranking cards' do 
-  #   deck_1 = Deck.new(["card1", "card2"])
-
-  # end 
-
+    end 
+  end 
   # xit 'returns percentage cards of high ranking card' do
   #   deck_1 = Deck.new(["card1", "card2"])
   #   deck_1.rank_of_card_at(0)
