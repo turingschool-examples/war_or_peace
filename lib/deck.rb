4be5a0a0f6b2ec4c31c3a7ea14c12a725efd2ecd
Emily Port <emilylport@gmail.com>
2022-08-25 16:27:05 -0600

    add cards as empty array, maybe this is wrong

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,12 +1,30 @@
 class Deck 
-  attr_reader :cards 
+  attr_reader :cards, :add_card, :new_card, :index_location, :rank_of_card_at
   def initialize(cards)
-    @cards = []
+    @cards = [] 
+    @new_card = Card.new(:suit, :value, :rank) 
      
   end 
 
-  def rank_of_card_at(index_location)
-
+  def add_card(new_card)
+    @cards << new_card
   end 
 
+  def index_location
+    @cards.find_index 
+  end 
+
+  def rank_of_card_at(index_location)
+    @cards[index_location].rank
+  end 
+
+  def high_ranking_cards
+  end 
+
+  def remove_card
+    #use .shift method 
+  end 
+ 
 end 
+
+# require 'pry'; binding.pry 
