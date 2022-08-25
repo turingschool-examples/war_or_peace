c23db841f83c20ed086e11341e592806c20d839f
Sean Culliton <smculliton@gmail.com>
2022-08-24 18:24:52 -0600

    Update @cards to call generate method

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -5,15 +5,16 @@ class CardGenerator
     attr_reader :file, :cards
     def initialize(filename)
         @file = File.open("lib/#{filename}")
-        @cards = []
+        @cards = generate_deck
     end 
 
     def generate_deck
+        cards = []
         @file.each do |line|
              card = line.split(',').map { |item| item.strip }
              card[2] = card[2].to_i
-             @cards << Card.new(card[1],card[0],card[2])
+             cards << Card.new(card[1],card[0],card[2])
         end
-        @cards
+        cards
     end 
 end 
