f61a81e3f26cb37aa6a0a93a3f66d171fb68322b
Darby Smith <newman.darby2@gmail.com>
2022-08-24 20:56:57 -0400

    Add setup for each method

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,21 +1,30 @@
 require 'rspec'
 require './lib/deck'
+require './lib/card'
 
 RSpec.describe Deck do 
+  before (:each) do 
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3) 
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @cards = [@card1, @card2, @card3]
+  end
+  
   it 'is an instance of deck' do 
-    deck_1 = Deck.new
+    deck_1 = Deck.new(@cards)
     expect(deck_1).to be_instance_of Deck
   end 
   
   it 'has cards' do 
-    deck_1 = Deck.new
-    expect(deck_1.cards).to eq([]) 
+    deck_1 = Deck.new(@cards)
+    expect(deck_1.cards).to eq([@card1, @card2, @card3]) 
   end 
 
-  it 'represents the index location of a card' do 
-    deck_1 = Deck.new
-    deck_1.rank_of_card_at("card1") 
-    expect()
+  xit 'return the rank of that card' do 
+    deck_1 = Deck.new(cards)
+     
+    expect(deck.rank_of_card_at(0)).to eq(12)
+    expect(deck.rank_of_card_at(2)).to eq(14)
   end      
 
   # xit 'returns an array of high ranking cards' do 
