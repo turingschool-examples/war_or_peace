8521dccf50eecb3a4372e88780aca7f11572876f
Sean Culliton <smculliton@gmail.com>
2022-08-25 15:37:07 -0600

    Cleans player and player_spec

--- lib/player.rb
+++ lib/player.rb
@@ -1,16 +1,12 @@
 require 'pry'
-require_relative './card'
 require_relative './deck'
-
-
 
 class Player
     attr_reader :deck, :name
 
     def initialize(name, deck)
         @name = name
-        @deck = deck
-        # @has_lost = false  
+        @deck = deck 
     end 
 
     def has_lost?
@@ -18,15 +14,3 @@ class Player
         false
     end 
 end 
-# card1 = Card.new(:diamond, 'Queen', 12)
-# card2 = Card.new(:spade, '3', 3)
-# card3 = Card.new(:heart, 'Ace', 14)
-# deck = Deck.new([card1, card2, card3])
-# player = Player.new('Clarisa', deck)
-
-
-
-# player.deck.remove_card
-# player.deck.remove_card
-# player.deck.remove_card
-# binding.pry
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,38 +2,44 @@ require 'rspec'
 require '../lib/player'
 
 RSpec.describe Player do 
+    before(:each) do 
+        @card1 = Card.new(:diamond, 'Queen', 12)
+        @card2 = Card.new(:spade, '3', 3)
+        @card3 = Card.new(:heart, 'Ace', 14)
+        @deck = Deck.new([@card1, @card2, @card3])
+        @player = Player.new('Clarisa', @deck)
+    end 
+
+
     describe '#initialize' do 
         it 'exists and has attributes' do 
-            card1 = Card.new(:diamond, 'Queen', 12)
-            card2 = Card.new(:spade, '3', 3)
-            card3 = Card.new(:heart, 'Ace', 14)
-            deck = Deck.new([card1, card2, card3])
-            player = Player.new('Clarisa', deck)
+            expect(@player).to be_an_instance_of(Player)
+        end 
 
-            expect(player).to be_an_instance_of(Player)
-            expect(player.name).to eq('Clarisa')
-            expect(player.deck).to eq(deck)
-            expect(player.has_lost?).to eq(false)
+        it 'has a name' do 
+            expect(@player.name).to eq('Clarisa')
+        end 
+
+        it 'has a deck' do 
+            expect(@player.deck).to be_a Deck
+            expect(@player.deck).to eq(@deck)
         end 
     end 
+
     describe '#has_lost?' do 
         it 'has lost when no more cards left in deck' do 
-            card1 = Card.new(:diamond, 'Queen', 12)
-            card2 = Card.new(:spade, '3', 3)
-            card3 = Card.new(:heart, 'Ace', 14)
-            deck = Deck.new([card1, card2, card3])
-            player = Player.new('Clarisa', deck)
+            expect(@player.has_lost?).to eq(false)
 
-            expect(player.deck).to eq(deck)
-            expect(player.has_lost?).to eq(false)
+            @player.deck.remove_card
+            expect(@player.has_lost?).to eq(false)
 
-            player.deck.remove_card
-            expect(player.has_lost?).to eq(false)
-            player.deck.remove_card
-            expect(player.has_lost?).to eq(false)
-            player.deck.remove_card
-            expect(player.has_lost?).to eq(true)
-            expect(player.deck.cards).to eq([])
+            @player.deck.remove_card
+            expect(@player.has_lost?).to eq(false)
+
+            @player.deck.remove_card
+            expect(@player.has_lost?).to eq(true)
+
+            expect(@player.deck.cards).to eq([])
         end 
     end
 end
