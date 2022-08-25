b34246be96799a1fc5a94cbd40b1a15999cf41da
Bryan Keener <bkeener7@gmail.com>
2022-08-25 13:13:52 -0600

    Add high_ranking_cards method

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,7 +4,11 @@ class Deck
         @cards = cards
     end
 
-    def rank_of_card_at(rank)
-        cards[rank].rank
+    def rank_of_card_at(card_rank)
+        cards[card_rank].rank
+    end
+
+    def high_ranking_cards
+        cards.select{ |high_rank| high_rank.rank > 10}.map{ |high_rank| high_rank }
     end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -35,4 +35,16 @@ RSpec.describe Deck do
         expect(deck.rank_of_card_at(2)).to eq(14)
     end
 
+    it 'has high ranking cards' do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+
+        deck = Deck.new([card1, card2, card3])
+        expect(deck.high_ranking_cards).to eq([card1, card3])
+
+    end
+
+
+
 end
