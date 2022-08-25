9a9b410d8e31d1d1ae7561ce5f9626e2726d0f5a
Max MacGillivray <mary.macgillivray@gmail.com>
2022-08-24 21:38:18 -0700

    Add test for creating cards within deck_spec.rb

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,18 +1,18 @@
 class Deck
-  attr_reader @cards
+  # attr_reader @cards
   def initialize(cards)
     @cards = []
   end
-  def rank_of_card_at
-  end
-  def high_ranking_cards
-  end
-  def percent_high_ranking
-  end
-  def remove_card
-  end
-  def add_card
-  end
+  # def rank_of_card_at
+  # end
+  # def high_ranking_cards
+  # end
+  # def percent_high_ranking
+  # end
+  # def remove_card
+  # end
+  # def add_card
+  # end
 end
   
 
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,9 +1,18 @@
 require 'rspec'
+require './lib/deck'
 require './lib/card'
 require 'pry'
 
 RSpec.describe Deck do
   it 'exists' do
     card = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3) 
+    card3 = Card.new(:heart, 'Ace', 14)
+    expect(card).to be_an_instance_of(Card)
   end
+  
 end
+
+# card = Card.new(:diamond, 'Queen', 12)
+# card2 = Card.new(:spade, '3', 3) 
+# card3 = Card.new(:heart, 'Ace', 14)
