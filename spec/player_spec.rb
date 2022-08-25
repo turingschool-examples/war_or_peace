640495781a054e576206ce5fd8aebce79db1b490
Rich Kaht <adamselene1776@protonmail.com>
2022-08-24 18:35:36 -0600

    Add player class and refactor player tests

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,33 +3,56 @@ require './lib/deck'
 require './lib/player'
 
 RSpec.describe Player do
-  before(:each) do
-    card1 = Card.new(:spade, 'Ace', 14)
-    card2 = Card.new(:heart, '3', 3)
-    card3 = Card.new(:diamond, '8', 8)
-    cards = [card1, card2, card3]
-    deck = deck.new(cards)
-    player = Player.new('John', deck)
-  end
+  describe 'initialize' do
+  
+    it "is a instance of its class" do
+      card1 = Card.new(:spade, 'Ace', 14)
+      card2 = Card.new(:heart, '3', 3)
+      card3 = Card.new(:diamond, '8', 8)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      player = Player.new('John', deck)
+      
+      expect(player).to be_an_instance_of(Player)
+    end
     
-  it "is a instance of its class" do
-    expect(player).to be_an_instance_of(Player)
+    it "has a name" do
+      card1 = Card.new(:spade, 'Ace', 14)
+      card2 = Card.new(:heart, '3', 3)
+      card3 = Card.new(:diamond, '8', 8)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      player = Player.new('John', deck)
+      
+      expect(player.name).to eq('John')
+    end
+    
+    it "is initialized with a deck" do
+      card1 = Card.new(:spade, 'Ace', 14)
+      card2 = Card.new(:heart, '3', 3)
+      card3 = Card.new(:diamond, '8', 8)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      player = Player.new('John', deck)
+      
+      expect(player.deck).to eq(deck)
+    end
   end
-  
-  it "has a name" do
-    expect(player.name).to eq('John')
-  end
-  
-  it "is initialized with a deck" do
-    expect(player.deck).to eq(deck)
-  end
-
-  it "#has_lost? method returns boolean value for 
-  zero or non-zero number of cards in deck" do
-    expect(player.has_lost?).to eq(false)
-    deck.remove_card
-    deck.remove_card
-    deck.remove_card
-    expect(player.has_lost?).to eq(true)
+  describe 'behaviors' do
+    it "#has_lost? method returns boolean value for 
+    zero or non-zero number of cards in deck" do
+      card1 = Card.new(:spade, 'Ace', 14)
+      card2 = Card.new(:heart, '3', 3)
+      card3 = Card.new(:diamond, '8', 8)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      player = Player.new('John', deck)
+      
+      expect(player.has_lost?).to eq(false)
+      deck.remove_card
+      deck.remove_card
+      deck.remove_card
+      expect(player.has_lost?).to eq(true)
+    end
   end
 end
