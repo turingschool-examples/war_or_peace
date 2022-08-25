ad2326334d1ee220a7c12bcac74683ee928a1f59
Darby Smith <newman.darby2@gmail.com>
2022-08-25 15:31:19 -0400

    Describe initialize method tests

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -9,6 +9,25 @@ RSpec.describe Player do
     @card1 = Card.new(:diamond, 'Queen', 12)
     @card2 = Card.new(:spade, '3', 3) 
     @card3 = Card.new(:heart, 'Ace', 14)
-    @deck = Deck.new([@ard1, @card2, @card3])
+    @deck = Deck.new([@card1, @card2, @card3])
   end
-end 
+
+  describe '#initialize' do 
+    it 'is an instance of player' do 
+      player = Player.new('Clarisa', @deck)
+      expect(player).to be_instance_of Player
+    end 
+
+    it 'has a name' do 
+      player = Player.new('Clarisa', @deck)
+      expect(player.name).to eq('Clarisa')
+    end 
+
+    it 'can have a different name' do 
+      player2 = Player.new('John', @deck)
+      expect(player2.name).to eq('John')
+    end 
+  end 
+
+
+end  
