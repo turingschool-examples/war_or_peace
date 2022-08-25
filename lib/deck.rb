22c16314f975f49171d5416bd12a7d39fcfdd672
Darby Smith <newman.darby2@gmail.com>
2022-08-24 21:24:49 -0400

    Complete high_ranking_card test

--- lib/deck.rb
+++ lib/deck.rb
@@ -8,4 +8,15 @@ class Deck
   def rank_of_card_at(index)
     @cards[index].rank
   end 
+
+  def high_ranking_card
+    high_cards = []
+    @cards.each do |card| 
+      if card.rank >= 11
+        high_cards << card
+      end 
+    end
+    high_cards
+  end
+
 end 
