2755c679e124eef5b319dda64d06ca050884f96e
Sean Culliton <smculliton@gmail.com>
2022-08-24 18:08:40 -0600

    Condense card_generator and spec

--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -3,23 +3,16 @@ require './lib/card_generator'
 
 RSpec.describe CardGenerator do 
     it 'opens a file' do 
-        card_gen = CardGenerator.new
-        expect(card_gen.file).to be_a(File)
-    end 
-
-    it 'returns an array of args for Card.new' do
-        card_gen = CardGenerator.new
-        expect(card_gen.generate_array).to be_a(Array)
-        expect(card_gen.card_arguments[0]).to be_a(Array)
-        expect(card_gen.card_arguments[0][0]).to eq('2')
-        expect(card_gen.card_arguments[0][1]).to eq('Heart')
-        expect(card_gen.card_arguments[0][2]).to eq(2)
+        filename = 'cards.txt'
+        cards = CardGenerator.new(filename)
+        expect(cards.file).to be_a(File)
     end 
 
     it 'makes a deck of cards' do 
-        card_gen = CardGenerator.new
-        expect(card_gen.generate_deck).to be_a(Array)
-        expect(card_gen.cards[0]).to be_an_instance_of(Card)
-        expect(card_gen.cards[0].suit).to eq('Heart')
+        filename = 'cards.txt'
+        cards = CardGenerator.new(filename)
+        expect(cards.generate_deck).to be_a(Array)
+        expect(cards.cards[0]).to be_an_instance_of(Card)
+        expect(cards.cards[0].suit).to eq('Heart')
     end 
 end 
