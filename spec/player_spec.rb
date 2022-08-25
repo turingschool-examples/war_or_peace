ce6b794b7bda8de2a7b72d1859e0a21fe835ff2a
Will Wang <willy.j.wang@gmail,com>
2022-08-25 15:19:18 -0500

    Create player class and test file

---
+++ spec/player_spec.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+
+RSpec.describe Deck do
+  describe '#initialize' do
+    it "exists" do
+      
+    end
+  end
+end
