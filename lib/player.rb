3233d991ba3ba874775d6cab03125ee66c2dc61f
William Lampke <gravitybangers@gmail.com>
2022-08-25 17:56:37 -0400

    add player file

---
+++ lib/player.rb
@@ -0,0 +1,24 @@
+class Player
+    attr_reader :name, :deck
+    def initialize(name, deck)
+        @name = name
+        @deck = deck        
+    end
+    def has_lost?
+        if deck.cards.length == 0
+        return true
+        else
+        return false
+        end
+    end    
+    def remove_card
+        deck.cards.shift
+    end
+
+
+
+
+
+
+
+end
