e0ce5756ce29b520eb772633353be9242849909e
Mostafa Sakr <msakr3@gmail.com>
2022-08-25 14:11:46 -0600

    Add test for the two dynamic attributes player name and deck in spec file as well as matching code in player.rb file to make test pass

--- lib/player.rb
+++ lib/player.rb
@@ -7,12 +7,12 @@ class Player
     attr_reader :player_name,
                 :deck
 
+
     def initialize(player_name, deck)
 
         @player_name = player_name
         @deck = deck
 
-    def
+    end
 
 end
-
