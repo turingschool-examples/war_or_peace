7f8d634f8b53cede644ae0ed2b905f57adc0ddc5
William Lampke <gravitybangers@gmail.com>
2022-08-25 16:09:33 -0400

    add deck file

---
+++ lib/deck.rb
@@ -0,0 +1,34 @@
+class Deck
+    attr_reader :cards
+    
+    def initialize(cards)
+        @cards = cards
+
+    end    
+
+    def rank_of_card_at(index)
+        return @cards[index].rank
+    end
+    
+    def high_ranking_cards
+        high_rank = []
+        @cards.each do | card |
+            if card.rank >= 11
+                high_rank << card
+            end    
+        end
+        return high_rank    
+    end
+    
+    def percent_high_ranking
+        return high_ranking_cards().length.to_f / @cards.length.to_f
+    end
+
+    def remove_card
+        @cards.shift
+    end
+
+    def add_card(new)
+        @cards << new
+    end
+end
