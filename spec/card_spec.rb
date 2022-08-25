11c5a95cd71db6128d2d8aa0d5bcb0be97130eac
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2022-08-24 20:46:09 -0600

    create deck class

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,7 +1,8 @@
 require 'rspec'
 require './lib/card'
+require './lib/deck'
 
-RSpec.describe Card do
+describe Card do
   it "exists" do
     card = Card.new(:diamond, 'Queen', 12)
 
@@ -15,4 +16,13 @@ RSpec.describe Card do
     expect(card.value).to eq('Queen')
     expect(card.rank).to eq(12)
   end
+
+  describe '#rank_of_card_at' do
+    xit 'shows rank of card' do
+      card = Card.new(:diamond, 'Queen', 12)
+      card.rank_of_card_at(0)
+      expect(card).to eq()
+    end
+  end
 end
+
