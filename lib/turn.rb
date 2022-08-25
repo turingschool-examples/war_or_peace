9d43a94469f0e327a6365c79e44d78754e56b711
Mostafa Sakr <msakr3@gmail.com>
2022-08-25 14:49:02 -0600

    Add corresponding turn class code to turn.rb for test in previous commit

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,3 +3,17 @@ require './lib/deck'
 require './lib/player'
 require 'pry'
 
+class Turn
+
+    attr_reader :player1,
+                :player2
+    
+    def initialize(player1, player2)
+
+        @player1 = player1
+        @player2 = player2
+
+    end
+
+
+end
