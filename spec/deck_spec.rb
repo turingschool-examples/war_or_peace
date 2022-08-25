786e016d6baf80d9f5ef2b53825e46f0eb5b8f3f
Will Wang <willy.j.wang@gmail,com>
2022-08-25 09:39:48 -0500

    Modify tests to use before(:each) block so code to create cards/deck can be reused

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,87 +3,59 @@ require './lib/card'
 require './lib/deck'
 
 RSpec.describe Deck do
+  before(:each) do
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @cards = [@card1, @card2, @card3]
+    @deck = Deck.new(@cards)
+  end
+
   describe '#initialize' do
     it "exists" do
-      #any way to avoid having to retype all of this for every test?
-      card1 = Card.new(:diamond, 'Queen', 12)
-      card2 = Card.new(:spade, '3', 3)
-      card3 = Card.new(:heart, 'Ace', 14)
-      cards = [card1, card2, card3]
-      deck = Deck.new(cards)
-      expect(deck).to be_an_instance_of(Deck)
+      expect(@deck).to be_an_instance_of(Deck)
     end
     it "has readable cards attribute" do
-      card1 = Card.new(:diamond, 'Queen', 12)
-      card2 = Card.new(:spade, '3', 3)
-      card3 = Card.new(:heart, 'Ace', 14)
-      cards = [card1, card2, card3]
-      deck = Deck.new(cards)
-      expect(deck.cards).to eq(cards)
+      expect(@deck.cards).to eq(@cards)
     end
   end
 
   describe '#rank_of_card_at' do
     it "can read card rank at given position" do
-      card1 = Card.new(:diamond, 'Queen', 12)
-      card2 = Card.new(:spade, '3', 3)
-      card3 = Card.new(:heart, 'Ace', 14)
-      cards = [card1, card2, card3]
-      deck = Deck.new(cards)
-      expect(deck.rank_of_card_at(0)).to eq(12)
-      expect(deck.rank_of_card_at(2)).to eq(14)
+      expect(@deck.rank_of_card_at(0)).to eq(12)
+      expect(@deck.rank_of_card_at(2)).to eq(14)
     end
   end
 
   describe '#high_ranking_cards' do
     it "can return high ranking cards" do
-      card1 = Card.new(:diamond, 'Queen', 12)
-      card2 = Card.new(:spade, '3', 3)
-      card3 = Card.new(:heart, 'Ace', 14)
-      cards = [card1, card2, card3]
-      deck = Deck.new(cards)
-      expect(deck.high_ranking_cards).to eq([cards[0], cards[2]])
+      expect(@deck.high_ranking_cards).to eq([@cards[0], @cards[2]])
     end
   end
 
   describe '#percent_high_ranking' do
     it "can determine percent of high ranking cards" do
-      card1 = Card.new(:diamond, 'Queen', 12)
-      card2 = Card.new(:spade, '3', 3)
-      card3 = Card.new(:heart, 'Ace', 14)
-      cards = [card1, card2, card3]
-      deck = Deck.new(cards)
       #rounding?
-      expect(deck.percent_high_ranking).to eq(66.67)
+      expect(@deck.percent_high_ranking).to eq(66.67)
     end
   end
 
   describe '#remove_card' do
     it "can remove top card" do
-      card1 = Card.new(:diamond, 'Queen', 12)
-      card2 = Card.new(:spade, '3', 3)
-      card3 = Card.new(:heart, 'Ace', 14)
-      cards = [card1, card2, card3]
-      deck = Deck.new(cards)
       #require 'pry'; binding.pry
       #why is this step removing from cards- not a copy
       #expect(deck.remove_card).to eq(cards[0])
-      expect(deck.remove_card).to eq(card1)
+      expect(@deck.remove_card).to eq(@card1)
     end
   end
 
   describe '#add_card' do
     it "can add card to bottom(end)" do
-      card1 = Card.new(:diamond, 'Queen', 12)
-      card2 = Card.new(:spade, '3', 3)
-      card3 = Card.new(:heart, 'Ace', 14)
-      cards = [card1, card2, card3]
-      deck = Deck.new(cards)
-      deck.remove_card
+      @deck.remove_card
       card4 = Card.new(:club, '5', 5)
-      deck.add_card(card4)
+      @deck.add_card(card4)
       #deck.cards is the same as cards
-      expect(deck.cards).to eq([card2, card3, card4])
+      expect(@deck.cards).to eq([@card2, @card3, card4])
     end
   end
 end
