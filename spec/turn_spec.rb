3ffeb1ab3a12c6ed707287b78effef13bc6e6507
Darby Smith <newman.darby2@gmail.com>
2022-08-25 21:58:40 -0400

    Add initialize test

---
+++ spec/turn_spec.rb
@@ -0,0 +1,30 @@
+require 'rspec'
+require 'pry'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+
+RSpec.describe Turn do 
+  before (:each) do 
+    @card1 = Card.new(:heart, 'Jack', 11)
+    @card2 = Card.new(:heart, '10', 10) 
+    @card3 = Card.new(:heart, '9', 9) 
+    @card4 = Card.new(:diamond, 'Jack', 11)  
+    @card5 = Card.new(:heart, '8', 8) 
+    @card6 = Card.new(:diamond, 'Queen', 12)
+    @card7 = Card.new(:heart, '3', 3)
+    @card8 = Card.new(:diamond, '2', 2)
+    @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+    @player1 = Player.new('Dug', @deck1)
+    @player2 = Player.new('Fran', @deck2)
+  end 
+
+  describe '#initialize' do 
+    it 'is an instance of turn' do
+      turn = Turn.new(@player1, @player2)
+      expect(turn).to be_instance_of Turn
+    end
+  end 
+end 
