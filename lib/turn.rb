8c7a5ad7bacda8ebb9d882a4ae5273096fdc6018
Darby Smith <newman.darby2@gmail.com>
2022-08-26 16:43:21 -0400

    Pass type method

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,11 +1,23 @@
 class Turn 
   attr_reader :player1, 
-              :player2,
-              :spoils_of_war
+              :player2
+              
+  attr_accessor :spoils_of_war, 
+                :type
 
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
     @spoils_of_war = []
+  end
+
+  def type 
+    if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
+      :basic
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+      :mutually_assured_destruction
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+      :war
+    end
   end
 end
