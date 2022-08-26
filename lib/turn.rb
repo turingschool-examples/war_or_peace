83a7b82430f8b42cbf520a10a5f169d70bab07d0
Amanda Ross <amikaross@gmail.com>
2022-08-26 14:09:29 -0600

    Update turn_spec to include award_spoils method

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,13 +14,21 @@ class Turn
   end
 
   def type 
-    if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
-      :mutually_assured_destruction
-    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
-      :war 
+    if player1.deck.cards.length >= 2 && player2.deck.cards.length >= 2
+      if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+        :mutually_assured_destruction
+      elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+        :war 
+      else 
+        :basic 
+      end 
     else 
-      :basic 
-    end 
+      if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+        :war 
+      else 
+        :basic 
+      end 
+    end
   end
 
   def winner 
