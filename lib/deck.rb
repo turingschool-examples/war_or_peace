b2611552798933d694263a3372b47cdda7be8e32
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-08-26 16:28:48 -0700

    Add high ranking cards method, pass test

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,8 +1,32 @@
 require './lib/card'
+require 'pry'
+
 class Deck
   attr_reader :cards
 
   def initialize(cards)
     @cards = cards
   end
+
+  def rank_of_card_at(index)
+    @cards[index].rank
+  end
+
+  # Method high_ranking_cards should read rank of each card in the cards array, if the
+  # card rank is greater than 10, the card should go into the high_rank_cards array
+
+
+  def high_ranking_cards
+  high_rank_cards = []  
+    @cards.each do |card|
+      if card.rank > 10
+        then high_rank_cards << card
+      end
+    end
+    high_rank_cards
+  end
+
+  # def percent_high_ranking
+  #   *100
+  # end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -4,15 +4,44 @@ require './lib/deck'
 require 'pry'
 
 RSpec.describe Deck do
-  describe '#initialize' do
-    it 'exists' do
-      card1 = Card.new(:diamond, 'Queen', 12)
-      card2 = Card.new(:spade, '3', 3)
-      card3 = Card.new(:heart, 'Ace', 14)
-      cards = [card1, card2, card3]
-      deck = Deck.new(cards)
-      expect(card1).to be_an_instance_of(Card)
-      expect(deck).to be_an_instance_of(Deck)
-    end
+  it 'exists' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    expect(card1).to be_an_instance_of(Card)
+    expect(deck).to be_an_instance_of(Deck)
   end
+
+  it 'can read card rank' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    expect(deck.rank_of_card_at(0)).to eq(12)
+    expect(deck.rank_of_card_at(2)).to eq(14)
+  end
+
+  it 'can tell high ranking cards' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    expect(deck.high_ranking_cards).to eq([card1, card3])
+  end
+
+  # when method percent_high_ranking called on deck variable, it returns
+  # float equal to percentage of high_rank_cards in current deck, which is 66.67
+
+  # it 'can show percentage of high ranking cards' do
+  #   card1 = Card.new(:diamond, 'Queen', 12)
+  #   card2 = Card.new(:spade, '3', 3)
+  #   card3 = Card.new(:heart, 'Ace', 14)
+  #   cards = [card1, card2, card3]
+  #   deck = Deck.new(cards)
+  #   expect(deck.percent_high_ranking).to eq(66.67)
+  # end
 end
