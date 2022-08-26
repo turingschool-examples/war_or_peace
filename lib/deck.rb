533f469ef8b9ce837a2e9db43f5bacd18674cfb2
Emily Port <emilylport@gmail.com>
2022-08-26 12:27:59 -0600

    add passing high ranking cards test

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,9 +1,11 @@
 class Deck 
-  attr_reader :cards, :add_card, :new_card, :index_location, :rank_of_card_at
+  attr_reader :cards, :add_card, :high_cards, :new_card, :index_location, :rank_of_card_at, :high_ranking_cards
   def initialize(cards)
     @cards = cards  
     @new_card = Card.new(:suit, :value, :rank) 
-     
+    @high_ranking_cards = high_ranking_cards
+    @high_cards = high_cards
+
   end 
 
   def add_card(new_card)
@@ -15,12 +17,24 @@ class Deck
   end 
 
   def rank_of_card_at(index_location)
+    
     @cards[index_location].rank
   end 
 
   def high_ranking_cards
+    @high_cards = []
+    @cards.each do |card|
+      if card.rank >= 11
+        @high_cards << card 
+      end 
+    end
+    @high_cards 
+  end  
+  
+  def percent_high_ranking
+    ((@high_ranking_cards.count.to_f / @cards.count.to_f) * 100).round(2) 
   end 
-
+   
   def remove_card
     #use .shift method 
   end 
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -9,28 +9,12 @@ RSpec.describe Deck do
     card3 = Card.new(:heart, 'Ace', 14)
     cards = [card1, card2, card3]
     deck = Deck.new(cards)  
-    
 
     expect(deck.cards).to eq([card1, card2, card3])
-  
-  #   # deck = Deck.new([])
-  #   # expect(deck.cards).to eq([])
-
-  #   card1 = Card.new(:diamond, 'Queen', 12)
-  #   card2 = Card.new(:spade, '3', 3)
-  #   card3 = Card.new(:heart, 'Ace', 14)
-
-  #   deck = Deck.new([])
-  #   expect(deck.cards).to eq([])
-
 
     expect(card1).to be_an_instance_of(Card)
     expect(card2).to be_an_instance_of(Card)
     expect(card3).to be_an_instance_of(Card)
-
-  #   deck.add_card(card1)
-  #   deck.add_card(card2)
-  #   deck.add_card(card3)
 
     expect(deck.cards).to eq([card1, card2, card3])
 
@@ -45,14 +29,6 @@ RSpec.describe Deck do
     
 
     expect(deck.cards).to eq([card1, card2, card3])
-  #   card1 = Card.new(:diamond, 'Queen', 12)
-  #   card2 = Card.new(:spade, '3', 3)
-  #   card3 = Card.new(:heart, 'Ace', 14)
-  #   deck = Deck.new([])
-    
-  #   deck.add_card(card1)
-  #   deck.add_card(card2)
-  #   deck.add_card(card3)
 
     expect(deck.cards).to eq([card1, card2, card3])
     expect(deck.cards.find_index(card1)).to eq(0)
@@ -71,12 +47,55 @@ RSpec.describe Deck do
     card3 = Card.new(:heart, 'Ace', 14)
     cards = [card1, card2, card3]
     deck = Deck.new(cards)  
-    
+    # require 'pry'; binding.pry 
 
     expect(deck.cards).to eq([card1, card2, card3])
-    #_______________________________
-   
-    # card1 = Card.new(:diamond, 'Queen', 12)
+    
+    expect(deck.high_ranking_cards).to eq([card1, card3])
+  
+    # require 'pry'; binding.pry 
+
+  end 
+
+  it 'tests percent high ranking cards' do 
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards) 
+    # require 'pry'; binding.pry 
+    expect(deck.percent_high_ranking).to eq(66.67)
+
+  end 
+
+end 
+
+
+  #first test 
+  #   # deck = Deck.new([])
+  #   # expect(deck.cards).to eq([])
+
+  #   card1 = Card.new(:diamond, 'Queen', 12)
+  #   card2 = Card.new(:spade, '3', 3)
+  #   card3 = Card.new(:heart, 'Ace', 14)
+
+  #   deck = Deck.new([])
+  #   expect(deck.cards).to eq([])
+    #   deck.add_card(card1)
+  #   deck.add_card(card2)
+  #   deck.add_card(card3)
+
+  #second test 
+    #   card1 = Card.new(:diamond, 'Queen', 12)
+  #   card2 = Card.new(:spade, '3', 3)
+  #   card3 = Card.new(:heart, 'Ace', 14)
+  #   deck = Deck.new([])
+    
+  #   deck.add_card(card1)
+  #   deck.add_card(card2)
+  #   deck.add_card(card3)
+#third test
+      # card1 = Card.new(:diamond, 'Queen', 12)
     # card2 = Card.new(:spade, '3', 3)
     # card3 = Card.new(:heart, 'Ace', 14)
     # deck = Deck.new([])
@@ -90,9 +109,3 @@ RSpec.describe Deck do
 
     # expect(deck.cards.find_index(card2)).to eq(1)
     # require 'pry'; binding.pry 
- 
-  end 
-
- 
-
-end 
