e2bf6eb507a16e82c5b234e6aecfd182f594bcfa
Naomi Yocum <naomiyocum24@gmail.com>
2022-08-26 13:37:02 -0600

    Fix pile_cards method

--- lib/turn.rb
+++ lib/turn.rb
@@ -21,13 +21,13 @@ class Turn
 
   def winner
     if type == :basic && player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
-      return player1
+      player1
     elsif type == :basic && player1.deck.rank_of_card_at(0) < player2.deck.rank_of_card_at(0)
-      return player2
+      player2
     elsif type == :war && player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
-      return player1
+      player1
     elsif type == :war && player1.deck.rank_of_card_at(2) < player2.deck.rank_of_card_at(2)
-      return player2
+      player2
     else
       return 'No Winner'
     end
@@ -38,8 +38,10 @@ class Turn
       spoils_of_war << player1.deck.cards.shift
       spoils_of_war << player2.deck.cards.shift
     elsif type == :war
-      spoils_of_war << player1.deck.cards.take(3)
-      spoils_of_war << player2.deck.cards.take(3)
+      3.times do
+        spoils_of_war << player1.deck.cards.shift
+        spoils_of_war << player2.deck.cards.shift
+      end
     else
       3.times do
         player1.deck.cards.shift
