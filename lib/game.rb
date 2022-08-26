b62a5c9694755f6042cffbb27b5c8013d3c5dc2e
Eli Fuchsman <efuchsman@gmail.com>
2022-08-26 13:41:14 -0600

    Begin to define start method

--- lib/game.rb
+++ lib/game.rb
@@ -5,10 +5,35 @@ require 'turn.rb'
 require 'game.rb'
 
 class Game
-attr_reader :turn
+attr_reader :turn, :num_of_turns
+
   def initialize(turn)
     @turn = turn
+    @num_of_turns = 0
   end
+
+  def start
+    p "Welcome to War! (or Peace) This game will be played with 52 cards."
+    p "The players today are #{turn.player_1.name} and #{turn.player_2.name}."
+    p "Type 'GO' to start the game!"
+    p  "------------------------------------------------------------------"
+
+    driver = gets.chomp.upcase
+
+    if driver == "GO"
+      play
+    else
+     p "Type 'GO'"
+    end
+  end
+
+  # def play
+  #   @num_of_turns
+  #   @turn
+  #   until turn.player_1.has_lost? || turn.player_2.has_lost?
+
+
+  # end
 
 
 
