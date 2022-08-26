75cda29a93eaba4ea05d0940a3a1b888f746143e
James White <whitejaad@gmail.com>
2022-08-26 15:00:25 -0700

    "Has lost passes"

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,15 @@
 class Player
    #attr_reader allows accees to name and deck
-  attr_reader :name
+  attr_reader :name #attr_reader = methods for class
               :deck
 
-    def initialize(name, deck)
-      @name = name
+    def initialize(name, deck) #parameters
+      @name = name   #instance variables
       @deck = deck
     end
+
+     def has_lost?
+          false
+  end
+
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,17 +4,33 @@ require './lib/deck'
 require './lib/player'
 
 RSpec.describe Player do
-    @card1 = Card.new(:diamond, 'Queen', 12)
-    @card2 = Card.new(:spade, '3', 3)
-    @card3 = Card.new(:heart, 'Ace', 14)
-    @cards = [@card1, @card2, @card3]
-    @deck = Deck.new([@card1, @card2, @card3])
-
-  end
-
   describe"player creation" do
- it "exists" do
-   player = Player.new(Clarissa, deck)
-
+    it "exists" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new("Clarissa", deck)
+    expect(player1).to be_an_instance_of(Player)
+    expect(deck.cards).to eq([card1, card2, card3])
     end
+
+describe"player has lost" do
+  it "expect it to be false by default"do
+  card1 = Card.new(:diamond, 'Queen', 12)
+  card2 = Card.new(:spade, '3', 3)
+  card3 = Card.new(:heart, 'Ace', 14)
+  cards = [card1, card2, card3]
+  deck = Deck.new([card1, card2, card3])
+  player = Player.new("Clarissa", deck)
+  expect(player).to be_an_instance_of(Player)
+  expect(deck.cards).to eq([card1, card2, card3])
+  require "pry";binding.pry
+  expect(player.has_lost?).to eq false
+
+end
+end
+
+   end
   end
