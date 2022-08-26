206ee91bcdb238812ce657c89bcee0248865c0b5
Andrew Tsai <a168tsai@gmail.com>
2022-08-26 08:22:02 -0600

    Winner added

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,5 +4,29 @@ class Turn
     @player1 = player1
     @player2 = player2
     @spoils_of_war = []
+    @turn_type = nil
+  end
+
+  def type
+    if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
+      @turn_type = :basic
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
+      @turn_type = :war
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
+      @turn_type = :mutually_assured_destruction
+    end
+  end
+
+  def winner
+    if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
+      @player1
+    elsif @player1.deck.rank_of_card_at(0) < @player2.deck.rank_of_card_at(0)
+      @player2
+    elsif @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
+      @player1
+    elsif @player1.deck.rank_of_card_at(2) < @player2.deck.rank_of_card_at(2)
+      @player2
+    else No Winner  
+    end
   end
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -13,13 +13,9 @@ RSpec.describe Player do
     @card6 = Card.new(:diamond, 'Queen', 12)
     @card7 = Card.new(:heart, '3', 3)
     @card8 = Card.new(:diamond, '2', 2)
-    # deck1 = Deck.new([@card1, @card2, @card5, @card8])
-    # deck2 = Deck.new([@card3, @card4, @card6, @card7])
-    # player1 = Player.new("Megan", deck1)
-    # player2 = Player.new("Aurora", deck2)
   end
 
-  it 'exists' do
+  it "initialize" do
     deck1 = Deck.new([@card1, @card2, @card5, @card8])
     deck2 = Deck.new([@card3, @card4, @card6, @card7])
     player1 = Player.new("Megan", deck1)
@@ -37,6 +33,33 @@ RSpec.describe Player do
     expect(turn.player1).to eq(player1)
     expect(turn.player2).to eq(player2)
     expect(turn.spoils_of_war).to eq([])
-    # expect(turn.type).to eq(:basic)
   end
+
+  it "has a basic turn" do
+    deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    deck2 = Deck.new([@card3, @card4, @card6, @card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    expect(turn.type).to eq(:basic)
+  end
+
+  it "has a winner" do
+    deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    deck2 = Deck.new([@card3, @card4, @card6, @card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    expect(turn.winner).to eq(player1)
+  end
+
+  it 'pile cards' do
+    deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    deck2 = Deck.new([@card3, @card4, @card6, @card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+  end
+
+
 end
