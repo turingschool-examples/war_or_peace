c6137a6e1906a28b5d002771ff259133fda78d2e
Eli Fuchsman <efuchsman@gmail.com>
2022-08-26 11:58:28 -0600

    Initialize game class

---
+++ lib/game.rb
@@ -0,0 +1,22 @@
+require 'card.rb'
+require 'deck.rb'
+require 'player.rb'
+require 'turn.rb'
+require 'game.rb'
+
+class Game
+attr_reader :turn
+  def initialize(turn)
+    @turn = turn
+  end
+
+
+
+
+
+
+
+
+
+
+end
