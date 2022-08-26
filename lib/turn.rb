e2cd4bbb377408c6142a1cb4c6477dea40d7ea32
Mostafa Sakr <msakr3@gmail.com>
2022-08-25 20:40:58 -0600

    Add code for testing :basic turns in spec file and code to define type method in turn.rb

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,9 +14,27 @@ class Turn
         @player1 = player1
         @player2 = player2
         @spoils_of_war =[]
-
+        
     end
 
+    def type
+        
+        if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
+            if @player1.deck.rank_of_card_at(2) != @player2.deck.rank_of_card_at(2)
+            :basic
+            else
+            :mutually_assured_destruction
+            end
+        elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) != @player2.deck.rank_of_card_at(2)
+            :war
+        end
+    end
 
 end
 
+# binding.pry
+
+# type: a turn is :basic, :war, or :mutually_assured_destruction.
+# A :basic turn is one in which the rank_of_card_at(0) from the players’ decks are not the same rank.
+# A :war turn occurs when both players’ rank_of_card_at(0) are the same.
+# :mutually_assured_destruction occurs when both players’ rank_of_card_at(0) AND rank_of_card_at(2) are the same.
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -33,6 +33,7 @@ RSpec.describe Turn do
     end
 
     it "has 3 readable attributes: player1, player2, and spoils_of_war" do
+
         card1 = Card.new(:heart, 'Jack', 11)
         card2 = Card.new(:heart, '10', 10)
         card3 = Card.new(:heart, '9', 9)
@@ -57,5 +58,41 @@ RSpec.describe Turn do
         expect(turn.player2).to eq(player2)
         expect(turn.spoils_of_war).to eq([])
     end
+
+    it "has has type of turn ':basic'" do
+
+        card1 = Card.new(:heart, 'Jack', 11)
+        card2 = Card.new(:heart, '10', 10)
+        card3 = Card.new(:heart, '9', 9)
+        card4 = Card.new(:diamond, 'Jack', 11)
+        card5 = Card.new(:heart, '8', 8)
+        card6 = Card.new(:diamond, 'Queen', 12)
+        card7 = Card.new(:heart, '3', 3)
+        card8 = Card.new(:diamond, '2', 2)
+
+        
+
+        deck1 = Deck.new([card1, card2, card5, card8])
+        deck2 = Deck.new([card3, card4, card6, card7])
+
+
+        player1 = Player.new("Megan", deck1)
+        player2 = Player.new("Aurora", deck2)
+
+        turn = Turn.new(player1, player2)
+
+        expect(player1.deck.rank_of_card_at(0)).not_to eq (player2.deck.rank_of_card_at(0))
+
+        expect(turn.type).to eq(:basic)
+
+        
+    end
+
+
+    # expect(turn.type).to eq(:war)
+
+    # expect(turn.type).to eq(:mutually_assured_destruction)
+
+
 
 end
