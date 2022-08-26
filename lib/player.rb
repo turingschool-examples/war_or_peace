74445de2e6aa2175458be1ec8ce7066918eee4a1
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2022-08-25 20:21:22 -0600

    pass has lost test finally

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,23 @@
 class Player
-  attr_reader :deck
-  def initialize(deck)
+  attr_reader :name, :deck
+  def initialize(name, deck)
+    @name = name
     @deck = deck
   end
+
+  def has_lost?
+    if @deck.cards.length == 0
+       true
+    else
+        false
+    end
+  end
 end
+
+
+  
+
+    
+
+
+
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,8 +5,47 @@ require './lib/player'
 
 describe Player do
     it 'exists' do
-        player = Player.new(Deck)
-        expect(player).to be_an_instance_of(Player)
+        player = Player.new('Clarisa', @deck)
+        expect(player).to be_a(Player)
+    end
+        
+    it 'deck has cards' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      player = Player.new('Clarisa', deck)
+      expect(player.deck.cards).to eq([card1, card2, card3]) 
+    end
+
+  
+    describe '#has_lost?' do
+      it 'tells if player has lost' do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+        cards = [card1, card2, card3]
+        deck = Deck.new(cards)
+        player = Player.new('Clarisa', deck)
+        player.deck.remove_card
+        expect(player.deck.cards).to eq([card2, card3])
+        expect(player.has_lost?).to eq false
+        player.deck.remove_card
+        expect(player.deck.cards).to eq([card3])
+        expect(player.has_lost?).to eq false
+        player.deck.remove_card
+        expect(player.deck.cards).to eq([])
+        expect(player.has_lost?).to eq true
+      end
     end
 end
+
+
+
+
+        
+        
+      
+
 
