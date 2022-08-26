5d6d2c5d507370ca63878e59758d636011fc9a4e
Bryan Keener <bkeener7@gmail.com>
2022-08-26 09:05:25 -0600

    Add award_spoils_of_war method

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,10 +19,10 @@ class Turn
 
     def winner
         if type == :basic
-            return @player1 if @player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+            return @player1 if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
             @player2
         elsif type == :war
-            return @player1 if @player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+            return @player1 if @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
             @player2
         elsif type == :mutually_assured_destruction
             return No Winner
@@ -35,7 +35,25 @@ class Turn
             players.each do |card|
                 @spoils_of_war << card.deck.cards.shift
             end
+        elsif type == :war
+            players = [@player1, @player2]
+            3.times do
+                players.each do |card|
+                    @spoils_of_war << card.deck.cards.shift
+            end
+        end
+        elsif type == :mutually_assured_destruction
+            players = [@player1, @player2]
+            3.times do
+                players.each do |card|
+                    card.deck.cards.shift
+            end
         end        
+        end
     end
 
+    def award_spoils_of_war(winner)
+        (winner.deck.cards << spoils_of_war).flatten!
+        @spoils_of_war = []
+    end
 end
