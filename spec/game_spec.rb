d6497aa4b4d11773765d2e5b0020bc10298ba757
Rich Kaht <adamselene1776@protonmail.com>
2022-08-25 20:51:55 -0600

    Add game spec file

---
+++ spec/game_spec.rb
@@ -0,0 +1,28 @@
+require 'rspec'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+require './lib/game'
+
+RSpec.describe Game do
+  describe "initialize game" do
+    
+    it "#start" do
+      card1 = Card.new(:spade, 'Ace', 14)
+      card2 = Card.new(:heart, 'Jack', 11)
+      card3 = Card.new(:diamond, '8', 8)
+      card4 = Card.new(:club, '10', 10)
+      card5 = Card.new(:heart, '5', 5)
+      cards1 = [card1, card2, card3]
+      cards2 = [card4, card5]
+      deck1 = Deck.new(cards1)
+      deck2 = Deck.new(cards2)
+      player1 = Player.new('John', deck1)
+      player2 = Player.new('Paul', deck2)
+      # turn = Turn.new(player1, player2)
+      
+      expect(player2.has_lost?).to eq(true)
+    end
+  end
+end
