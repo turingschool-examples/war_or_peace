5420ee8861cc585b8987488243614374be1645d0
Darby Smith <newman.darby2@gmail.com>
2022-08-26 23:24:51 -0400

    Add test for winner method

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -7,18 +7,18 @@ require './lib/turn'
 
 RSpec.describe Turn do 
   # before (:each) do 
-  #   card1 = Card.new(:heart, 'Jack', 11)
-  #   card2 = Card.new(:heart, '10', 10) 
-  #   card3 = Card.new(:heart, '9', 9) 
-  #   card4 = Card.new(:diamond, 'Jack', 11)  
-  #   card5 = Card.new(:heart, '8', 8) 
-  #   card6 = Card.new(:diamond, 'Queen', 12)
-  #   card7 = Card.new(:heart, '3', 3)
-  #   card8 = Card.new(:diamond, '2', 2)
-  #   deck1 = Deck.new([card1, card2, card5, card8])
-  #   deck2 = Deck.new([card3, card4, card6, card7])
-  #   player1 = Player.new('Dug', deck1)
-  #   player2 = Player.new('Fran', deck2)
+  #   @card1 = Card.new(:heart, 'Jack', 11)
+  #   @card2 = Card.new(:heart, '10', 10) 
+  #   @card3 = Card.new(:heart, '9', 9) 
+  #   @card4 = Card.new(:diamond, 'Jack', 11)  
+  #   @card5 = Card.new(:heart, '8', 8) 
+  #   @card6 = Card.new(:diamond, 'Queen', 12)
+  #   @card7 = Card.new(:heart, '3', 3)
+  #   @card8 = Card.new(:diamond, '2', 2)
+  #   @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+  #   @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+  #   @player1 = Player.new('Dug', deck1)
+  #   @player2 = Player.new('Fran', deck2)
   # end 
 
   describe '#initialize' do 
@@ -158,4 +158,74 @@ RSpec.describe Turn do
       expect(turn.type).to eq(:mutually_assured_destruction)
     end 
   end 
+
+  describe '#winner'do 
+    it 'will return the player that won basic' do 
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10) 
+      card3 = Card.new(:heart, '9', 9) 
+      card4 = Card.new(:diamond, 'Jack', 11)  
+      card5 = Card.new(:heart, '8', 8) 
+      card6 = Card.new(:diamond, 'Queen', 12)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card3, card4, card6, card7])
+      player1 = Player.new('Dug', deck1)
+      player2 = Player.new('Fran', deck2)
+      turn = Turn.new(player1, player2)
+
+      expect(turn.player1.deck.rank_of_card_at(0)).to eq(11)
+      expect(turn.player2.deck.rank_of_card_at(0)).to eq(9)
+      expect(turn.type).to eq(:basic)
+      expect(turn.winner).to eq(player1)
+    end 
+
+    it 'will return the player the player that wins war' do 
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10) 
+      card3 = Card.new(:heart, '9', 9) 
+      card4 = Card.new(:diamond, 'Jack', 11)  
+      card5 = Card.new(:heart, '8', 8) 
+      card6 = Card.new(:diamond, 'Queen', 12)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card4, card3, card6, card7])
+      player1 = Player.new('Dug', deck1)
+      player2 = Player.new('Fran', deck2)
+      turn = Turn.new(player1, player2)
+      
+      expect(turn.player1.deck.rank_of_card_at(0)).to eq(11)
+      expect(turn.player2.deck.rank_of_card_at(0)).to eq(11)
+      expect(turn.type).to eq(:war)
+      expect(turn.player1.deck.rank_of_card_at(2)).to eq(8)
+      expect(turn.player2.deck.rank_of_card_at(2)).to eq(12)
+      expect(turn.winner).to eq(player2)
+    end 
+
+    it 'will return no winner when it is mutually assured destruction' do 
+      card1 = Card.new(:heart, 'Jack', 11) 
+      card2 = Card.new(:heart, '10', 10) 
+      card3 = Card.new(:heart, '9', 9) 
+      card4 = Card.new(:diamond, 'Jack', 11)  
+      card5 = Card.new(:heart, '8', 8) 
+      card6 = Card.new(:diamond, '8', 8)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card4, card3, card6, card7])
+      player1 = Player.new('Dug', deck1)
+      player2 = Player.new('Fran', deck2)
+      turn = Turn.new(player1, player2)
+
+      expect(turn.player1.deck.rank_of_card_at(0)).to eq(11)
+      expect(turn.player1.deck.rank_of_card_at(2)).to eq(8)
+      expect(turn.player2.deck.rank_of_card_at(0)).to eq(11)
+      expect(turn.player2.deck.rank_of_card_at(2)).to eq(8)
+      expect(turn.type).to eq(:mutually_assured_destruction)
+      expect(turn.winner).to eq("No Winner")
+
+    end 
+  end
 end 
