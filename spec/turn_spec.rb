52f931366b3db6ec310b42efefa91436e4553cc8
Darby Smith <newman.darby2@gmail.com>
2022-08-27 11:56:19 -0400

    Add award spoils test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -301,7 +301,28 @@ RSpec.describe Turn do
 
   describe '#award_spoils' do 
     it 'adds spoils to the winner' do
-      
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10) 
+      card3 = Card.new(:heart, '9', 9) 
+      card4 = Card.new(:diamond, 'Jack', 11)  
+      card5 = Card.new(:heart, '8', 8) 
+      card6 = Card.new(:diamond, 'Queen', 12)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card3, card4, card6, card7])
+      player1 = Player.new('Dug', deck1)
+      player2 = Player.new('Fran', deck2)
+      turn = Turn.new(player1, player2)
+
+      expect(turn.player1.deck.rank_of_card_at(0)).to eq(11)
+      expect(turn.player2.deck.rank_of_card_at(0)).to eq(9)
+      expect(turn.type).to eq(:basic)
+      expect(turn.winner).to eq(player1)
+      turn.pile_cards
+      expect(turn.spoils_of_war).to eq([card1, card3])
+      turn.award_spoils
+      expect(turn.player1.deck).to eq([card2, card5, card8, card1, card3])
     end
   end 
 end 
