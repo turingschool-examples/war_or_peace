f6ea1cde41f02e8d331095350bf04519bb20e79f
Darby Smith <newman.darby2@gmail.com>
2022-08-27 01:20:51 -0400

    Add test for basic and war pile_cards

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -91,7 +91,7 @@ RSpec.describe Turn do
       player2 = Player.new('Fran', deck2)
       turn = Turn.new(player1, player2)
       
-      expect(turn.spoils_of_war).to eq []
+      expect(turn.spoils_of_war).to eq([])
     end 
   end  
 
@@ -225,6 +225,56 @@ RSpec.describe Turn do
       expect(turn.player2.deck.rank_of_card_at(2)).to eq(8)
       expect(turn.type).to eq(:mutually_assured_destruction)
       expect(turn.winner).to eq("No Winner")
+    end 
+  end
+
+  describe '#pile_cards' do 
+    it 'each player sends the top card to the spoils pile' do
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10) 
+      card3 = Card.new(:heart, '9', 9) 
+      card4 = Card.new(:diamond, 'Jack', 11)  
+      card5 = Card.new(:heart, '8', 8) 
+      card6 = Card.new(:diamond, 'Queen', 12)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card3, card4, card6, card7])
+      player1 = Player.new('Dug', deck1)
+      player2 = Player.new('Fran', deck2)
+      turn = Turn.new(player1, player2)
+
+      expect(turn.player1.deck.rank_of_card_at(0)).to eq(11)
+      expect(turn.player2.deck.rank_of_card_at(0)).to eq(9)
+      expect(turn.type).to eq(:basic)
+      expect(turn.winner).to eq(player1)
+      turn.pile_cards
+      expect(turn.spoils_of_war).to eq([card1, card3])
+    end 
+
+    xit 'each player sends three cards to spoils pile' do 
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10) 
+      card3 = Card.new(:heart, '9', 9) 
+      card4 = Card.new(:diamond, 'Jack', 11)  
+      card5 = Card.new(:heart, '8', 8) 
+      card6 = Card.new(:diamond, 'Queen', 12)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card4, card3, card6, card7])
+      player1 = Player.new('Dug', deck1)
+      player2 = Player.new('Fran', deck2)
+      turn = Turn.new(player1, player2)
+
+      expect(turn.player1.deck.rank_of_card_at(0)).to eq(11)
+      expect(turn.player2.deck.rank_of_card_at(0)).to eq(11)
+      expect(turn.type).to eq(:war)
+      turn.pile_cards
+      expect(turn.spoils_of_war).to eq([card1, card4, card2, card3, card5, card6])
+    end
+
+    xit 'three cards are removed from each player deck and removed from game' do
 
     end 
   end
