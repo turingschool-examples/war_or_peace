cb1158a1aa01be0fe27dbfb712a243953c9185f7
Darby Smith <newman.darby2@gmail.com>
2022-08-26 23:24:25 -0400

    Pass winner method

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,14 +10,35 @@ class Turn
     @player2 = player2
     @spoils_of_war = []
   end
-
+ 
   def type 
-    if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
+ 
+    if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
       :basic
-    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) &&
+       @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
       :mutually_assured_destruction
-    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
       :war
     end
+  end
+
+  def winner 
+    # require 'pry';binding.pry
+    if type == :basic
+      if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
+        @player1
+      elsif @player2.deck.rank_of_card_at(0) > @player1.deck.rank_of_card_at(0)
+        @player2
+      end
+    elsif type == :war 
+      if @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
+        @player1
+      elsif @player2.deck.rank_of_card_at(2) > @player1.deck.rank_of_card_at(2)
+        @player2
+      end
+    elsif type == :mutually_assured_destruction
+      "No Winner"
+    end 
   end
 end
