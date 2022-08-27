859051167416a92eb18eb73a8b81a1000c2547c9
Amanda Ross <amikaross@gmail.com>
2022-08-27 12:40:17 -0600

    Create CardGenerator class to pass tests

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,6 +1,20 @@
+require 'csv'
+require './lib/card'
+
 class CardGenerator 
   def initialize(filename)
     @filename = filename 
     @cards = []
+  end
+
+  def cards
+    table = CSV.read(@filename)
+    table.each do |row|
+      suit = row[1].strip.downcase.to_sym
+      value = row[0].strip
+      rank = row[2].strip.to_i
+      @cards << Card.new(suit, value, rank)
+    end
+    @cards
   end
 end
