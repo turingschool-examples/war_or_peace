e83f02ae6b8d1b9c233a769ef7a24caec3e10542
Emily Port <emilylport@gmail.com>
2022-08-26 18:08:46 -0600

    add the winner to war

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,6 +3,44 @@ class Turn
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2 
+    @spoils_of_war = []
 
+  end 
+
+  def type 
+    # require 'pry'; binding.pry 
+    if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+      :war 
+    elsif player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
+      :basic 
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)  
+      :mutually_assured_destruction
+      
+    end 
+     
+
+  end 
+
+  def spoils_of_war
+    
+    
+  end 
+
+  def winner
+    if type == :basic 
+      if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0) 
+        player1 
+      elsif player1.deck.rank_of_card_at(0) < player2.deck.rank_of_card_at(0) 
+        player2
+      end
+    elsif type == :war 
+      if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2) 
+        player1 
+      elsif player1.deck.rank_of_card_at(2) < player2.deck.rank_of_card_at(2) 
+        player2
+      end
+    elsif type == :mutually_assured_destruction
+        'No winner'
+    end
   end 
 end
