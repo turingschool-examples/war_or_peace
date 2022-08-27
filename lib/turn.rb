24cb738379cc1d2b70eaac70fc6ff702fdf64c8d
James White <whitejaad@gmail.com>
2022-08-26 19:20:52 -0700

    "Turn exists"

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,10 +2,18 @@ class Turn
   attr_reader :player1,
               :player2,
               :spoils_of_war
-              
+
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
     @spoils_of_war = []
   end
+
+  def type(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @spoils_of_war = []
+  end
+
+  def
 end
