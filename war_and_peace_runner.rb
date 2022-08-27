e3fa246ab0de68c43e291e82053aa135a6c9a82c
Joseph Hilby <joseph.hilby@gmail.com>
2022-08-27 11:00:10 -0500

    Complete itteration 4

--- war_and_peace_runner.rb
+++ war_and_peace_runner.rb
@@ -5,25 +5,9 @@ require './lib/player'
 require './lib/turn'
 require './lib/player'
 require './lib/game'
-# require './lib/card_generator'
+require './lib/card_generator'
 
-# Create 52 cards.
-card_inputs = []
-suits = [:spade, :heart, :diamond, :club]
-values_ranks = [['Ace', 14], ['King', 13], ['Queen', 12], ['Jack', 11], 
-                ['Ten', 10], ['Nine', 9], ['Eight', 8], ['Seven', 7], ['Six', 6], 
-                ['Five', 5], ['Four', 4], ['Three', 3], ['Two', 2]]
-values_ranks.each do |card|
-  card_inputs << [suits[0], card].flatten
-  card_inputs << [suits[1], card].flatten
-  card_inputs << [suits[2], card].flatten
-  card_inputs << [suits[3], card].flatten
-end
-card_inputs.shuffle!
 
-cards = card_inputs.map do |input|
-  Card.new(input[0], input[1], input[2])
-end
 
 # Create two decks
 deck_1 = Deck.new(cards[0, 26])
@@ -42,23 +26,4 @@ game = Game.new(turn)
 # start
 puts 'Welcome to War! (or Peace). This game will be played with 52 cards.'
 puts 'The players today are Megan and Aurora.'
-game.start
-
-
-# # Create 52 cards.
-# card_inputs = []
-# suits = [:spade, :heart, :diamond, :club]
-# values_ranks = [['Ace', 14], ['King', 13], ['Queen', 12], ['Jack', 11], 
-#                 ['Ten', 10], ['Nine', 9], ['Eight', 8], ['Seven', 7], ['Six', 6], 
-#                 ['Five', 5], ['Four', 4], ['Three', 3], ['Two', 2]]
-# values_ranks.each do |card|
-#   card_inputs << [suits[0], card].flatten
-#   card_inputs << [suits[1], card].flatten
-#   card_inputs << [suits[2], card].flatten
-#   card_inputs << [suits[3], card].flatten
-# end
-# card_inputs.shuffle!
-
-# cards = card_inputs.map do |input|
-#   Card.new(input[0], input[1], input[2])
-# end
+# game.start
