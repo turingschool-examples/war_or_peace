a87714df0d3dd31eaab2fac6a0ee0a58965e8b2b
Bryan Flanagan <bflanagan78@yahoo.com>
2022-08-27 17:43:43 -0600

    Begin building game files

---
+++ lib/game.rb
@@ -0,0 +1,10 @@
+class Start
+    def Start
+        p "Welcome to War! (or Peace) This game will be played with 52 cards.
+        The players today are Megan and Aurora.
+        Type 'GO' to start the game!
+        ------------------------------------------------------------------"
+        gets.chomp
+    
+    end
+end
