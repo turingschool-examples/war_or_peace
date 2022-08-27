f30f0653669b883d51dad324b4e2487ddd169749
James White <whitejaad@gmail.com>
2022-08-27 12:21:35 -0700

    "winner passes"

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,17 +3,40 @@ class Turn
               :player2,
               :spoils_of_war
 
-  def initialize(player1, player2)
+   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
     @spoils_of_war = []
-  end
+   end
 
-  def type
+   def type
       #how the game is played. must have a winner or war! if two war than spoils of war! rank_of_cards_at
       #get discarded with no winner.
-    if @player1.deck.rank_of_cards_at(0) != @player2.deck.rank_of_cards_at(0)
+    if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
       :basic
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
+      :mutually_assured_destruction
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
+      :war
+
+       end
+      end
+
+    def winner
+      if type == :basic
+        if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
+          @player1
+        else
+          #@player1.deck.rank_of_cards_at(0) < @player2.deck.rank_of_cards_at(0)
+          @player2
+        end
+      end
     end
-  end
 end
+       #  def war
+       #    if turn.type == war
+       #    if @player1.deck.rank_of_cards_at(0) == @player2.deck.rank_of_cards_at(0)
+       #      @war
+       #    end
+       #
+       # end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -7,8 +7,8 @@ require './lib/turn'
 
 RSpec.describe Turn do
 
-  it "Turn exists" do
-
+  it "exists" do
+ # before (:each) do
    card1 = Card.new(:heart, 'Jack', 11)
    card2 = Card.new(:heart, '10', 10)
    card3 = Card.new(:heart, '9', 9)
@@ -18,6 +18,8 @@ RSpec.describe Turn do
    card7 = Card.new(:heart, '3', 3)
    card8 = Card.new(:diamond, '2', 2)
 
+   cards = [card1, card2, card5, card8, card3, card4, card6, card7]
+
    deck1 = Deck.new([card1, card2, card5, card8])
    deck2 = Deck.new([card3, card4, card6, card7])
 
@@ -25,13 +27,11 @@ RSpec.describe Turn do
    player2 = Player.new("Aurora", deck2)
 
    turn = Turn.new(player1, player2)
-binding.pry
    expect(turn).to be_an_instance_of(Turn)
+   end
 
- end
-
- describe 'type' do
-   it 'determines what type of turn is in play' do
+ # describe 'turn' do
+  it 'has turn type basic and a winner' do
      card1 = Card.new(:heart, 'Jack', 11)
      card2 = Card.new(:heart, '10', 10)
      card3 = Card.new(:heart, '9', 9)
@@ -46,6 +46,20 @@ binding.pry
 
      player1 = Player.new("Megan", deck1)
      player2 = Player.new("Aurora", deck2)
-  end
- end
+
+     turn = Turn.new(player1, player2)
+
+     expect(turn.type).to eq(:basic)
+     expect(turn.winner).to eq(player1)
+
+    end
+
+    
+
 end
+
+
+# describe'winner'
+#      it'has a winner' do
+#
+#      end
