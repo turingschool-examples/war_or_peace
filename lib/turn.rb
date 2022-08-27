0b901d538133a41861822e30bf692fdaee9eccac
Bryan Flanagan <bflanagan78@yahoo.com>
2022-08-27 12:39:56 -0600

    Add basic turn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,9 +1,10 @@
 class Turn
-    attr_reader :player1, :player2, :spoils_of_war, :type
+    attr_reader :player1, :player2, :spoils_of_war, :winner
     def initialize(player1, player2)
         @player1 = player1
         @player2 = player2
         @spoils_of_war = []
+        @winner = winner
     end
 
     def type
@@ -18,12 +19,34 @@ class Turn
 
     def winner
         if player1.deck.rank_of_card_at(0) >= player2.deck.rank_of_card_at(0)
-            winner = @player1
+            @winner = @player1
         else
-            winner = @player2
+            @winner = @player2
         end
     end
 
+    def pile_cards
+       if type == :basic
+        pile_basic
+       elsif type == :war
+        pile_war
+       elsif type == :mutually_assured_destruction
+        pile_mutually_assured_destruction
+       end
+    end
+    
+    def pile_basic
+        @spoils_of_war << @player1.deck.remove_card
+        @spoils_of_war << @player2.deck.remove_card
+    end
+
+    def award_spoils
+        # binding.pry
+        @spoils_of_war.each do |spoils|
+            @winner.deck.add_card(spoils)
+        end
+    end
+        
 end
 
 
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -6,10 +6,10 @@ require './lib/player'
 require './lib/turn'
 
 RSpec.describe Turn do
-  context "basic_type" do
+  context "war" do
     before do
       @card1 = Card.new(:heart, 'Jack', 11) 
-      @card2 = Card.new(:spade, '10', 10)
+      @card2 = Card.new(:heart, '10', 10)
       @card3 = Card.new(:heart, '9', 9)
       @card4 = Card.new(:diamond, 'Jack', 11)
       @card5 = Card.new(:heart, '8', 8)
@@ -31,11 +31,57 @@ RSpec.describe Turn do
     end
 
     it "#test_type" do
-    expect(@turn.type).to eq(:basic)
+      expect(@turn.type).to eq(:basic)
     end
 
-    it "#winner" do
-    expect(@turn.winner).to eq(@player1)
+    it "#winner :basic" do
+      expect(@turn.winner).to eq(@player1)
+    end
+
+    it "#pile_cards :basic" do
+      @turn.pile_cards
+      expect(@turn.spoils_of_war).to eq([@card1, @card3])
+    end
+
+    it "#award_spoils :basic" do
+    # binding.pry
+      @turn.pile_basic
+      @turn.award_spoils
+      expect(@player1.deck.cards).to eq([@card2, @card5, @card8, @card1, @card3])
+      expect(@player2.deck.cards).to eq([@card4, @card6, @card7])
     end
   end
+
+  # context "war_type" do
+  #   before do
+  #     @card1 = Card.new(:heart, 'Jack', 11) 
+  #     @card2 = Card.new(:spade, '10', 10)
+  #     @card3 = Card.new(:heart, '9', 9)
+  #     @card4 = Card.new(:diamond, 'Jack', 11)
+  #     @card5 = Card.new(:heart, '8', 8)
+  #     @card6 = Card.new(:diamond, 'Queen', 12) 
+  #     @card7 = Card.new(:heart, '3', 3)
+  #     @card8 = Card.new(:diamond, '2', 2)
+  #     @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+  #     @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+  #     @player1 = Player.new("Megan", @deck1)
+  #     @player2 = Player.new("Aurora", @deck2)
+  #     @turn = Turn.new(@player1, @player2)
+  #   end
+
+  #   it "#initialize" do
+  #     expect(@turn.player1).to eq(@player1)
+  #     expect(@turn.player2).to eq(@player2)
+  #     expect(@turn.spoils_of_war).to eq([])
+  #   end
+
+  #   it "#test_type" do
+  #   expect(@turn.type).to eq(:war)
+  #   end
+
+  #   it "#winner" do
+  #   expect(@turn.winner).to eq(@player2)
+  #   end
+
+  # end
 end
