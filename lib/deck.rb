10361533539048fa5e4e6f82da49dc68c47fc5d1
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-08-27 14:33:38 -0700

    Add add card method, pass test

--- lib/deck.rb
+++ lib/deck.rb
@@ -16,7 +16,6 @@ class Deck
   # Method high_ranking_cards should read rank of each card in the cards array, if the
   # card rank is greater than 10, the card should go into the high_rank_cards array
 
-
   def high_ranking_cards
     @cards.each do |card|
       if card.rank > 10
@@ -27,15 +26,14 @@ class Deck
   end
 
   def percent_high_ranking
-    @cards.each do |card|
-      if card.rank > 10
-        then @high_rank_cards << card
-      end
-    end
-  (@high_rank_cards.length.to_f / @cards.length.to_f * 100).round(2)
+    (@high_rank_cards.length.to_f / @cards.length.to_f * 100).round(2)
   end
 
   def remove_card
     @cards.shift
+  end
+
+  def add_card(card)
+    @cards.push(card)
   end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -42,6 +42,7 @@ RSpec.describe Deck do
     card3 = Card.new(:heart, 'Ace', 14)
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
+    expect(deck.high_ranking_cards).to eq([card1, card3])
     expect(deck.percent_high_ranking).to eq(66.67)
   end
 
@@ -53,8 +54,21 @@ RSpec.describe Deck do
     deck = Deck.new(cards)
     deck.remove_card
     expect(deck.cards).to eq([card2, card3])
+    expect(deck.high_ranking_cards).to eq([card3])
     expect(deck.percent_high_ranking).to eq(50.00)
   end
 
-  
+  it 'adds card to bottom of deck' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    deck.remove_card
+    card4 = Card.new(:club, '5', 5)
+    deck.add_card(card4)
+    expect(deck.cards).to eq([card2, card3, card4])
+    expect(deck.high_ranking_cards).to eq([card3])
+    expect(deck.percent_high_ranking).to eq(33.33)
+  end
 end
