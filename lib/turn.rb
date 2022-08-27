f5ae9b145282d5e16de1b64c1be517f6ca0f8698
Bryan Flanagan <bflanagan78@yahoo.com>
2022-08-27 15:11:36 -0600

    Add passing turn files

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,12 +8,12 @@ class Turn
     end
 
     def type
-        if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
-          :war 
-        elsif player1.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(0) && player2.deck.rank_of_card_at(2)
-          :mutually_assured_destruction
+        if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+            :mutually_assured_destruction
         elsif player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
-          :basic
+            :basic
+        elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+          :war 
         end
     end
 
@@ -33,7 +33,7 @@ class Turn
             end
 
         elsif type == :mutually_assured_destruction
-            return "No Winner"
+            return "No winner"
         end
     end
 
@@ -55,6 +55,11 @@ class Turn
     def pile_war
         3.times { @spoils_of_war << @player1.deck.remove_card}
         3.times { @spoils_of_war << @player2.deck.remove_card}
+    end
+
+    def pile_mutually_assured_destruction
+        3.times { @player1.deck.remove_card }
+        3.times { @player2.deck.remove_card }
     end
 
     def award_spoils
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -27,7 +27,6 @@ RSpec.describe Turn do
       expect(@turn.player1).to eq(@player1)
       expect(@turn.player2).to eq(@player2)
       expect(@turn.spoils_of_war).to eq([])
-      # require 'pry': binding.pry
     end
 
     it "#test_type :basic" do
@@ -44,7 +43,6 @@ RSpec.describe Turn do
     end
 
     it "#award_spoils :basic" do
-    # binding.pry
       @turn.pile_basic
       @turn.award_spoils
       expect(@player1.deck.cards).to eq([@card2, @card5, @card8, @card1, @card3])
@@ -85,16 +83,59 @@ RSpec.describe Turn do
     end
 
     it "#pile_cards :war" do
-    @turn.pile_cards
-    expect(@turn.spoils_of_war).to eq([@card1, @card2, @card5, @card4, @card3, @card6])
+      @turn.pile_cards
+      expect(@turn.spoils_of_war).to eq([@card1, @card2, @card5, @card4, @card3, @card6])
+    end
+
+    it "#award_spoils :war" do
+      @turn.pile_war
+      @turn.award_spoils
+      expect(@player1.deck.cards).to eq([@card8])
+      expect(@player2.deck.cards).to eq([@card7, @card1, @card2, @card5, @card4, @card3, @card6])
+    end
+  end
+  context "mutually_assured_destruction_type" do
+    before do
+      @card1 = Card.new(:heart, 'Jack', 11) 
+      @card2 = Card.new(:heart, '10', 10)
+      @card3 = Card.new(:heart, '9', 9)
+      @card4 = Card.new(:diamond, 'Jack', 11)
+      @card5 = Card.new(:heart, '8', 8)
+      @card6 = Card.new(:diamond, '8', 8) 
+      @card7 = Card.new(:heart, '3', 3)
+      @card8 = Card.new(:diamond, '2', 2)
+      @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+      @deck2 = Deck.new([@card4, @card3, @card6, @card7])
+      @player1 = Player.new("Megan", @deck1)
+      @player2 = Player.new("Aurora", @deck2)
+      @turn = Turn.new(@player1, @player2)
+    end
+
+
+    it "#initialize" do
+      expect(@turn.player1).to eq(@player1)
+      expect(@turn.player2).to eq(@player2)
+      expect(@turn.spoils_of_war).to eq([])
+    end
+
+    it "#test_type :mutually_assured_destruction" do
+      expect(@turn.type).to eq(:mutually_assured_destruction)
+    end
+
+    it "#winner :mutually_assured_destruction" do
+      expect(@turn.winner).to eq("No winner")
+    end
+
+    it "#pile_cards :mutually_assured_destruction" do
+      @turn.pile_cards
+      expect(@turn.spoils_of_war).to eq([])
+    end
+
+    it "#award_spoils :mutually_assured_destruction" do
+      @turn.pile_mutually_assured_destruction
+      expect(@player1.deck.cards).to eq([@card8])
+      expect(@player2.deck.cards).to eq([@card7])
+    end
   end
 
-  it "#award_spoils :war" do
-  # binding.pry
-    @turn.pile_war
-    @turn.award_spoils
-    expect(@player1.deck.cards).to eq([@card8])
-    expect(@player2.deck.cards).to eq([@card7, @card1, @card2, @card5, @card4, @card3, @card6])
-  end
-  end
 end
