9411892299a3f70d72310d84af0b004c82f0da8c
Aether Kerstens <philippekerstens@gmail.com>
2022-08-27 01:37:54 -0700

    Create spec/deck_spec.rb

---
+++ spec/deck_spec.rb
@@ -0,0 +1,47 @@
+require 'rspec'
+require './lib/deck'
+require './lib/card'
+
+RSpec.describe Deck do
+  it "exists" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck).to be_an_instance_of(Deck)
+  end
+  describe "#initialize" do
+    it "has readable attributes" do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+
+      expect(deck.cards).to eq(cards)
+    end
+  end
+  describe "#rank_of_card_at" do
+    it "return rank of card at a given index position" do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+
+      expect(deck.rank_of_card_at(0)).to eq(12)
+    end
+  end
+  describe  "#percent_high_ranking" do
+    it "return percventage of cards that are high" do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      deck.percent_high_ranking
+    end
+  end
+end
