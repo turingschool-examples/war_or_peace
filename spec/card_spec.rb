ca283f6908264d053bea14f00bba925372f4f31e
Aether Kerstens <philippekerstens@gmail.com>
2022-08-27 01:36:04 -0700

    Update spec/card_spec.rb

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -7,12 +7,13 @@ RSpec.describe Card do
 
     expect(card).to be_an_instance_of(Card)
   end
+  describe "#initialize" do
+    it "has readable attributes" do
+      card = Card.new(:diamond, 'Queen', 12)
 
-  it "has readable attributes" do
-    card = Card.new(:diamond, 'Queen', 12)
-
-    expect(card.suit).to eq(:diamond)
-    expect(card.value).to eq('Queen')
-    expect(card.rank).to eq(12)
+      expect(card.suit).to eq(:diamond)
+      expect(card.value).to eq('Queen')
+      expect(card.rank).to eq(12)
+    end
   end
 end
