8d0cf86e49fc01d98e83d8110d81476f3dc55579
Ryan Loughry <thecoderanger5280@icloud.com>
2022-08-27 19:43:08 -0600

    start I4

---
+++ lib/card_generator.rb
@@ -0,0 +1,30 @@
+class Card_Generator
+  attr_reader :file_name,
+              :file_data,
+              :cards
+  def initialize(file_name)
+    @file_name = file_name
+    @file_data = ''
+    @split_line_data = []
+    @split_comma_data = []
+    @cards = []
+  end
+
+  def read_file
+    file = File.open("./lib/#{@file_name}")
+    @file_data = file.read
+    file.close
+    @file_data
+  end
+
+  def split_by_line
+    @split_line_data = @file_data.split("\n")
+  end
+
+  def split_line
+    @split_line_data.each do |line|
+      @split_comma_data << line.split(", ")
+    end
+    @split_comma_data
+  end
+end
---
+++ spec/card_generator_spec.rb
@@ -0,0 +1,55 @@
+require 'rspec'
+require './lib/card_generator'
+
+RSpec.describe Card_Generator do
+  describe '#initialize' do
+    it 'exists' do
+      file_name = 'test.txt'
+      card_generator = Card_Generator.new(file_name)
+
+      expect(card_generator).to be_an_instance_of(Card_Generator)
+    end
+
+    it 'has a file name' do
+      file_name = 'test.txt'
+      card_generator = Card_Generator.new(file_name)
+
+      expect(card_generator.file_name).to eq('test.txt')
+    end
+  end
+
+  describe '#methods' do
+    it 'can read a file' do
+      file_name = 'test.txt'
+      card_generator = Card_Generator.new(file_name)
+      # require 'pry'; binding.pry
+      expect(card_generator.read_file).to eq("Hello, World\nHello, Turing\n")
+    end
+
+    it 'can split a file by line' do
+      file_name = 'test.txt'
+      card_generator = Card_Generator.new(file_name)
+      # require 'pry'; binding.pry
+      card_generator.read_file
+      expect(card_generator.split_by_line).to eq(['Hello, World', 'Hello, Turing'])
+    end
+
+    it 'can split each line by commas' do
+      file_name = 'test.txt'
+      card_generator = Card_Generator.new(file_name)
+      card_generator.read_file
+      card_generator.split_by_line
+      # require 'pry'; binding.pry
+      expect(card_generator.split_line).to eq([["Hello", "World"], ["Hello", "Turing"]])
+    end
+
+    it 'can create instances of card for each array within the array' do
+      file_name = 'test.txt'
+      card_generator = Card_Generator.new(file_name)
+      card_generator.read_file
+      card_generator.split_by_line
+      card_generator.create_cards
+      expect(card_generator.cards[0].suit).to eq()
+    end
+  end
+end
