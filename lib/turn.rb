e0472160d801ed0ba52c970e35360169c1c00b52
Naomi Yocum <naomiyocum24@gmail.com>
2022-08-28 12:30:06 -0600

    Redefine winner using spoils_of_war

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,5 @@
 class Turn
-  attr_reader :player1,
-              :player2,
-              :spoils_of_war
+  attr_reader :player1, :player2, :spoils_of_war
 
   def initialize(player1, player2)
     @player1 = player1
@@ -10,51 +8,54 @@ class Turn
   end
 
   def type
-    if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
-      :basic
-    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+    if (player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)) && (player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2))
       :mutually_assured_destruction
-    else
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
       :war
-    end
-  end
-
-  def winner
-    if type == :basic && player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
-      player1
-    elsif type == :basic && player1.deck.rank_of_card_at(0) < player2.deck.rank_of_card_at(0)
-      player2
-    elsif type == :war && player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
-      player1
-    elsif type == :war && player1.deck.rank_of_card_at(2) < player2.deck.rank_of_card_at(2)
-      player2
     else
-      return 'No Winner'
+      :basic
     end
   end
 
   def pile_cards
-    if type == :basic
-      spoils_of_war << player1.deck.cards.shift
-      spoils_of_war << player2.deck.cards.shift
+    if type == :mutually_assured_destruction
+      3.times do
+        player1.deck.cards.shift
+        player2.deck.cards.shift
+      end
     elsif type == :war
       3.times do
         spoils_of_war << player1.deck.cards.shift
         spoils_of_war << player2.deck.cards.shift
       end
-    else
-      3.times do
-        player1.deck.cards.shift
-        player2.deck.cards.shift
-      end
+    elsif type == :basic
+      spoils_of_war << player1.deck.cards.shift
+      spoils_of_war << player2.deck.cards.shift
+    end
+  end
+
+  def rank_in_spoils(index)
+    spoils_of_war[index].rank
+  end
+
+  def winner
+    if spoils_of_war.length == 2 && rank_in_spoils(0) > rank_in_spoils(1)
+      player1
+    elsif spoils_of_war.length == 2 && rank_in_spoils(0) < rank_in_spoils(1)
+      player2
+    elsif spoils_of_war.length == 6 && rank_in_spoils(4) > rank_in_spoils(5)
+      player1
+    elsif spoils_of_war.length == 6 && rank_in_spoils(4) < rank_in_spoils(5)
+      player2
+    elsif spoils_of_war.length == 0
+      'No Winner'
     end
   end
 
   def award_spoils(winner)
-    winner.deck.cards.concat(spoils_of_war)
-    spoils_of_war.clear()
+    spoils_of_war.each do |card|
+      winner.deck.cards << card
+    end
+    spoils_of_war.clear
   end
-
-
-
 end
