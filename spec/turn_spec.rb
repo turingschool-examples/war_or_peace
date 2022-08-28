1b08cbefd8d65cd0d35adbabb5cbc7f373bc7f89
Emily Port <emilylport@gmail.com>
2022-08-28 00:52:21 -0600

    confirm tests are still successful

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,6 +3,7 @@ require './lib/card'
 require './lib/deck'
 require './lib/player'
 require './lib/turn'
+require './lib/game'
 
 RSpec.describe Deck do
   it 'initialize' do
@@ -49,7 +50,7 @@ RSpec.describe Deck do
     # require 'pry'; binding.pry 
     expect(turn.type).to eq(:basic)
   # require 'pry'; binding.pry 
-    expect(turn.spoils_of_war).to eq([])
+    expect(turn.spoils_of_war).to eq(nil)
   
     # require 'pry'; binding.pry 
     expect(turn.winner).to eq(player1)
@@ -57,7 +58,8 @@ RSpec.describe Deck do
 #  require 'pry'; binding.pry 
     expect(turn.spoils_of_war).to eq([card1, card3])
     # require 'pry'; binding.pry 
-    turn.award_spoils
+    winner = turn.winner
+    turn.award_spoils(winner)
 
     # expect(turn.player1.deck.cards).to eq([card2, card5, card8])
     # expect(turn.player2.deck.cards).to eq([card4, card6, card7])
@@ -90,11 +92,54 @@ RSpec.describe Deck do
     # # winner= turn.winner 
     # turn.pile_cards
     # turn.spoils_of_war
-    require 'pry'; binding.pry 
+    # require 'pry'; binding.pry 
     # turn.award_spoils(winner)
 
   end 
 
+  it 'tests the MAD pattern' do 
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, '8', 8)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    # cards1= [card1, card2, card5, card8]
+    # cards2 = [card3, card4, card6, card7]
+    deck1 = Deck.new([card1, card2, card5, card8])  
+    deck2 = Deck.new([card4, card3, card6, card7])  
+    player1 = Player.new('Megan', deck1)
+    player2 = Player.new('Aurora', deck2)
+    turn = Turn.new(player1, player2) 
+    # # winner= turn.winner 
+    # turn.pile_cards
+    # turn.spoils_of_war
+    # require 'pry'; binding.pry 
+    # turn.award_spoils(winner)
 
+  end 
+  it 'a turn' do
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, '8', 8)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    # cards1= [card1, card2, card5, card8]
+    # cards2 = [card3, card4, card6, card7]
+    deck1 = Deck.new([card1, card2, card5, card8])  
+    deck2 = Deck.new([card4, card3, card6, card7])  
+    player1 = Player.new('Megan', deck1)
+    player2 = Player.new('Aurora', deck2)
+
+    turn = Turn.new(player1, player2) 
+
+    game1= Game.new(turn)
+    
+  end
 end 
 
