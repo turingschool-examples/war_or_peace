e8b79f8c09f9e6e0e1c9193c5563f44357111a0f
Naomi Yocum <naomiyocum24@gmail.com>
2022-08-28 15:52:57 -0600

    Add winner variable to game_go types, and complete

--- lib/start.rb
+++ lib/start.rb
@@ -1,10 +1,3 @@
-
-
-# start method - while no one has lost continue the turn...
-# print specific lines based on what kind of turn
-# if no player has all cards after 1,000,000 turns, the result of the game is a draw
-
-
 class Game
   def initialize(player1, player2, turn)
     @player1 = player1
@@ -13,66 +6,52 @@ class Game
   end
 
   def start
-    turn = 0
+    p "Welcome to War! (or Peace) This game will be played with 52 cards."
+    p "The players today are #{@player1.name} and #{@player2.name}."
+    p "Type 'GO' to start the game!"
+    start = gets.chomp
+    if start == 'GO'
+      game_go
+    else
+      p 'No GO? Okay, see ya later!'
+    end
+  end
 
+  def game_go
+    counter = 0
     until @player1.has_lost? == true || @player2.has_lost? == true
-      turn += 1
-      binding.pry
-      if @turn.type == :mutually_assured_destruction
-        p "TURN #{turn}: *mutually assured destruction* 6 cards removed from play"
+      counter += 1
+      if @turn.type == :player1_lost
+        p "*~*~*~* #{@player2.name} has won the game! *~*~*~*"
+        break
+      elsif @turn.type == :player2_lost
+        p "*~*~*~* #{@player1.name} has won the game! *~*~*~*"
+        break
+      elsif @turn.type == :mutually_assured_destruction
+        @turn.pile_cards
+        p "Turn #{counter}: *mutually assured destruction* 6 cards removed from play"
       elsif @turn.type == :war
-        p "TURN #{turn}: WAR - #{@turn.winner.name} won 6 cards"
+        winner = @turn.winner
         @turn.pile_cards
-        @turn.award_spoils(@turn.winner)
-      else
-        p "TURN #{turn}: #{@turn.winner.name} won 2 cards"
+        p "Turn #{counter}: WAR - #{winner.name} won 6 cards"
+        @turn.award_spoils(winner)
+      elsif @turn.type == :basic
+        winner = @turn.winner
         @turn.pile_cards
-        @turn.award_spoils(@turn.winner)
+        p "Turn #{counter}: #{winner.name} won 2 cards"
+        @turn.award_spoils(winner)
       end
 
-      if turn == 10_000
-        p '---- DRAW ----'
+      if counter == 1_000_000
+        p "---- DRAW ----"
         break
       end
     end
+
+    if @player1.has_lost? == true
+      p "*~*~*~* #{@player2.name} has won the game! *~*~*~*"
+    elsif @player2.has_lost? == true
+      p "*~*~*~* #{@player1.name} has won the game! *~*~*~*"
+    end
   end
 end
-
-    # if @player1.has_lost? == true
-    #   p "*~*~*~* #{player2.name} has won the game! *~*~*~*"
-    # elsif @player2.has_lost? == true
-    #   p "*~*~*~* #{player1.name} has won the game! *~*~*~*"
-    # elsif @turn.type == :mutually_assured_destruction
-    #   p '*mutually assured destruction* 6 cards removed from play'
-    # elsif turn == 10_000
-    #   p '---- DRAW ----'
-    # else
-    #   turn += 1
-    #   @turn.type
-    #   winner = @turn.winner
-    #   @turn.pile_cards
-    #   @turn.spoils_of_war
-    #   p "Turn #{turn}: #{@turn.winner.name} won #{@turn.spoils_of_war.length} cards."
-    #   @turn.award_spoils(winner)
-    # end
-
-
-
-#     while @player1.has_lost? == false && @player2.has_lost? == false
-#       turn += 1
-#       @turn.type
-#       winner = @turn.winner
-#       @turn.pile_cards
-#       if @turn.winner == 'No Winner'
-#         p '*mutually assured desctrucion* 6 cards removed from play'
-#       else
-#         p "Turn #{turn}: #{winner.name} won #{@turn.spoils_of_war.length} cards."
-#       end
-#       @turn.award_spoils(winner)
-#       if turn == 10_000
-#         p '---- DRAW ----'
-#         break
-#       end
-#     end
-#   end
-# end
