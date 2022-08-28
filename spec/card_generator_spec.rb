ec075b3a9d7e56e6c4a51dde28198c34289419a2
Sean Culliton <smculliton@gmail.com>
2022-08-28 11:52:56 -0600

    Update spec formatting

--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -2,17 +2,20 @@ require 'rspec'
 require './lib/card_generator'
 
 RSpec.describe CardGenerator do 
-    it 'opens a file' do 
-        filename = 'cards.txt'
-        cards = CardGenerator.new(filename)
-        expect(cards.file).to be_a(File)
+    describe "#initialize" do 
+        it 'opens a file' do 
+            filename = 'cards.txt'
+            cards = CardGenerator.new(filename)
+            expect(cards.file).to be_a(File)
+        end 
     end 
-
-    it 'makes a deck of cards' do 
-        filename = 'cards.txt'
-        cards = CardGenerator.new(filename)
-        expect(cards.generate_deck).to be_a(Array)
-        expect(cards.cards[0]).to be_an_instance_of(Card)
-        expect(cards.cards[0].suit).to eq(:heart)
+    describe "#generate_deck" do 
+        it 'makes a deck of cards' do 
+            filename = 'cards.txt'
+            cards = CardGenerator.new(filename)
+            expect(cards.generate_deck).to be_a(Array)
+            expect(cards.cards[0]).to be_an_instance_of(Card)
+            expect(cards.cards[0].suit).to eq(:heart)
+        end 
     end 
 end 
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -2,17 +2,19 @@ require 'rspec'
 require '../lib/card'
 
 RSpec.describe Card do
-  it "exists" do
-    card = Card.new(:diamond, 'Queen', 12)
+  describe "#initialize" do 
+    it "exists" do
+      card = Card.new(:diamond, 'Queen', 12)
 
-    expect(card).to be_an_instance_of(Card)
-  end
+      expect(card).to be_an_instance_of(Card)
+    end
 
-  it "has readable attributes" do
-    card = Card.new(:diamond, 'Queen', 12)
+    it "has readable attributes" do
+      card = Card.new(:diamond, 'Queen', 12)
 
-    expect(card.suit).to eq(:diamond)
-    expect(card.value).to eq('Queen')
-    expect(card.rank).to eq(12)
-  end
+      expect(card.suit).to eq(:diamond)
+      expect(card.value).to eq('Queen')
+      expect(card.rank).to eq(12)
+    end
+  end 
 end
