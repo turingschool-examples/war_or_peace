afa79f1f8b6f5d57cba2f55cce916a77e80837f6
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-08-27 23:47:10 -0700

    Add type method that pass test, add winner method that fail test

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,6 +13,41 @@ class Turn
   end
 
   def type
-    
+    if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
+      :basic
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) &&
+      player1.deck.rank_of_card_at(2) != player2.deck.rank_of_card_at(2)
+      :war
+    else
+      :mutually_assured_destruction
+    end
+  end
+
+  def winner
+    if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+      then player1
+    else player2
+    :basic
+    end
+
+    if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+      then player1
+    else player2
+    :war
+    end
+
+    if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) &&
+      player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+      then "No Winner"
+      :mutually_assured_destruction
+    end
+  end
+
+  def pile_cards
+
+  end
+
+  def awards_spoils(winner)
+
   end
 end
