2bb8b85de5df4ce3af7f133e10f367c90d1b5411
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2022-08-27 19:03:45 -0600

    created runner.rb file

---
+++ war_or_peace_runner.rb
@@ -0,0 +1,5 @@
+p "Welcome to War! (or Peace) This game will be played with 52 cards.
+The players today are Megan and Aurora.
+Type 'GO' to start the game!
+------------------------------------------------------------------"
+
