f2f52ef4fcd59866c28cfc7ab8edca7dc4dd9edf
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2022-08-28 15:19:48 -0400

    test/feat: 2nd interaction patterns-turn type :war -complete

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,44 +15,59 @@ class Turn
       return :basic
   
     elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
-
       return :war
-
+require 'pry';binding.pry
     elsif  @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.rank_of_card_at(2)
       return :mutually_assured_destruction
     end
   end
 
   def winner
-      if type == :mutually_assured_destruction
+        if type == :mutually_assured_destruction
         return "No Winner"
+        end
+      
         
-        elsif type == :basic
+        if type == :basic
         (@player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0))
         return @player1 
-
-        elsif type ==  :war 
+        else 
+        return @player2
+        end
+      
+        if type ==  :war 
         (@player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2))
-        return @player1 
-
+        return @player1
         else
         return @player2
-      end        
-    
+        end        
   end
 
   def pile_cards
     if type == :basic
-        @spoils_of_war << @player1.deck.remove_card
-        # require 'pry';binding.pry
-      
+      @spoils_of_war << @player1.deck.remove_card
       @spoils_of_war <<  @player2.deck.remove_card
-      # require 'pry';binding.pry
+    
+  elsif type == :war
+    # require 'pry';binding.pry
+    3.times { @spoils_of_war << ( @player1.deck.remove_card) }
+    3.times { @spoils_of_war << ( @player2.deck.remove_card) }
+    # require 'pry';binding.pry
+      
+  
+      end
+      # @player1.deck.cards.take_while 
+      # != @player1.deck.cards.index(2).empty?
+      
+      # remove_card
+
+
+      # @spoils_of_war <<  (@player2.deck.remove_card)
 
     # end
     # :mutually_assured_destruction turn, each player will remove three cards from play (the top three cards in their deck). These cards are not sent to the spoils pile, they are simply removed from each players deck.
     # turn.player2.deck.cards.delete_at(1..3)
-    end
+    
   end
 
   def award_spoils(winner)
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,22 +3,23 @@ require './lib/deck'
 require './lib/player'
 require './lib/turn'
 
-RSpec.describe Turn do
-let(:card1)  { Card.new(:heart, 'Jack', 11)}
-let(:card2)  { Card.new(:heart, '10', 10)}    
-let(:card3)  { Card.new(:heart, '9', 9)}    
-let(:card4)  { Card.new(:diamond, 'Jack', 11)}    
-let(:card5)  { Card.new(:heart, '8', 8)}    
-let(:card6)  { Card.new(:diamond, 'Queen', 12)}    
-let(:card7)  { Card.new(:heart, '3', 3)}    
-let(:card8)  { Card.new(:diamond, '2', 2)} 
-let(:deck1)  { Deck.new([card1, card2, card5, card8]) }
-let(:deck2)  { Deck.new([card3, card4, card6, card7]) }
-let(:player1)  { Player.new("Megan", deck1)}    
-let(:player2)  { Player.new("Aurora", deck2)}    
-let(:turn)  { Turn.new(player1, player2)}    
+RSpec.describe Turn do  
 
-  context "1st interaction patterns" do
+  context "1st interaction patterns-#turn type :basic" do
+    let(:card1)  { Card.new(:heart, 'Jack', 11)}
+    let(:card2)  { Card.new(:heart, '10', 10)}    
+    let(:card3)  { Card.new(:heart, '9', 9)}    
+    let(:card4)  { Card.new(:diamond, 'Jack', 11)}    
+    let(:card5)  { Card.new(:heart, '8', 8)}    
+    let(:card6)  { Card.new(:diamond, 'Queen', 12)}    
+    let(:card7)  { Card.new(:heart, '3', 3)}    
+    let(:card8)  { Card.new(:diamond, '2', 2)} 
+    let(:deck1)  { Deck.new([card1, card2, card5, card8]) }
+    let(:deck2)  { Deck.new([card3, card4, card6, card7]) }
+    let(:player1)  { Player.new("Megan", deck1)}    
+    let(:player2)  { Player.new("Aurora", deck2)}    
+    let(:turn)  { Turn.new(player1, player2)} 
+
     it 'should be a class, have two player attributes, a spoils array' do
       expect(turn).to be_a Turn
       expect(turn.player1).to eq(player1)
@@ -50,11 +51,50 @@ let(:turn)  { Turn.new(player1, player2)}
       # expect(player1.deck.cards).to eq ([card1, card2, card5, card8, card3])
       expect(player1.deck.cards.count).to eq 5
       expect(player1.deck).to eq(deck1)
-      expect(player2.deck).to eq(deck2)
+      # require 'pry';binding.pry
+      expect(player2.deck.cards).to eq([card4, card6, card7])
       # expect(player1.deck1.count).to eq 4
     end
+  end
 
+  context '2nd interaction patterns-#turn type :war' do
+    let(:card1) { Card.new(:heart, 'Jack', 11) }
+    let(:card2) { Card.new(:heart, '10', 10) }
+    let(:card3) { Card.new(:heart, '9', 9) }
+    let(:card4) { Card.new(:diamond, 'Jack', 11) }
+    let(:card5) { Card.new(:heart, '8', 8) }
+    let(:card6) { Card.new(:diamond, 'Queen', 12) }
+    let(:card7) { Card.new(:heart, '3', 3) }
+    let(:card8) { Card.new(:diamond, '2', 2) }
+    let(:deck1) { Deck.new([card1, card2, card5, card8]) }
+    let(:deck2) { Deck.new([card4, card3, card6, card7]) }
+    let(:player1) { Player.new("Megan", deck1) }
+    let(:player2) { Player.new("Aurora", deck2) }
+    let(:turn) { Turn.new(player1, player2)  }
 
+    it 'should have a :war turn type and a winner for that type' do 
+      expect(turn.type).to eq(:war)
+      winner = turn.winner
+      expect(winner).to eq (player2)
+    end
+
+    it 'gets 3 top cards from each player, puts them in #pile_cards, then moves them to  #spoil_of_war' do      
+      expect(turn.spoils_of_war.count).to eq 0
+      winner = turn.winner
+      turn.pile_cards
+      expect(turn.spoils_of_war).to eq([card1, card2,card5, card4, card3, card6,])
+    end
+
+    it 'should add cards from #spoil_of_war to the winners cards' do
+      expect(player1.deck.cards.count).to eq 4
+      winner = turn.winner
+      turn.pile_cards
+      turn.award_spoils(winner)
+      expect(player1.deck).to eq(deck1)
+      expect(player1.deck.cards.count).to eq 1
+      expect(player2.deck).to eq(deck2)
+      expect(player1.deck.cards).to eq([card8])
+    end
 
   end
 
