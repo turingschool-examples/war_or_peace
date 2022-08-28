51a6c1b3a7b9747d20cd9e0bc6f3822eb8179230
Darby Smith <newman.darby2@gmail.com>
2022-08-28 16:40:09 -0400

    Fix spacing

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,3 @@
-# require 'rspec'
-# require 'pry'
-# require './lib/card'
-# require './lib/deck'
-# require './lib/player'
-# require './lib/turn'
-
 class Game
 attr_reader :player1, 
             :player2
@@ -27,41 +20,41 @@ attr_reader :player1,
 
   def user_start
     type_go = gets.chomp 
-      if type_go.upcase == "GO"
-        turn = Turn.new(@player1, @player2)
-        until @turn_number >= 1000000 || (@player1.deck.cards.length == 0 || @player2.deck.cards.length == 0)
-          if turn.type == :basic 
-            @turn_number += 1
-            turn_winner = turn.winner
-            turn.pile_cards
-            turn.award_spoils(turn_winner)
-            p "Turn #{@turn_number}: #{turn.winner.name} won 2 cards" 
-          elsif turn.type == :war
-            @turn_number += 1
-            turn_winner = turn.winner
-            turn.pile_cards
-            turn.award_spoils(turn_winner)
-            p "Turn #{@turn_number}: WAR - #{turn.winner.name} won 6 cards"
-          elsif turn.type == :mutually_assured_destruction
-            @turn_number += 1
-            turn_winner = turn.winner
-            turn.pile_cards
-            p "Turn #{@turn_number}: *mutually assured destruction* 6 cards removed from play"
-          end 
-          end_game
-        end
-      else 
-        p "Please try again" 
-      end 
+    if type_go.upcase == "GO"
+      turn = Turn.new(@player1, @player2)
+      until @turn_number >= 1000000 || (@player1.deck.cards.length == 0 || @player2.deck.cards.length == 0)
+        if turn.type == :basic 
+          @turn_number += 1
+          turn_winner = turn.winner
+          turn.pile_cards
+          turn.award_spoils(turn_winner)
+          p "Turn #{@turn_number}: #{turn_winner.name} won 2 cards" 
+        elsif turn.type == :war
+          @turn_number += 1
+          turn_winner = turn.winner
+          turn.pile_cards
+          turn.award_spoils(turn_winner)
+          p "Turn #{@turn_number}: WAR - #{turn_winner.name} won 6 cards"
+        elsif turn.type == :mutually_assured_destruction
+          @turn_number += 1
+          turn_winner = turn.winner
+          turn.pile_cards
+          p "Turn #{@turn_number}: *mutually assured destruction* 6 cards removed from play"
+        end 
+        end_game
+      end
+    else 
+      p "Please try again" 
+    end 
   end 
 
   def end_game 
     if @turn_number == 1000000
       p "----DRAW----"
     elsif @player1.deck.cards.length == 0 
-      p "*~*~*~*#{@player1.name} has won the game!"
+      p "*~*~*~*#{@player1.name}*~*~*~* has won the game!"
     elsif @player2.deck.cards.length == 0 
-      p "*~*~*~*#{@player2.name} has won the game!"
+      p "*~*~*~*#{@player2.name}*~*~*~* has won the game!"
     end 
   end 
 end 
