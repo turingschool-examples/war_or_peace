90479c9ded751a4df8247bc170c06f10f83d8c9b
Annie Pulzone <ajpulzone@gmail.com>
2022-08-28 14:02:30 -0700

    Initialize greeting,2 new players, a shuffled deck and split the deck between the two players

---
+++ spec/game_spec.rb
@@ -0,0 +1,26 @@
+require "./lib/card.rb"
+require "./lib/deck.rb"
+require "./lib/player.rb"
+require "./lib/turn.rb"
+require "./lib/game_play.rb"
+
+Rspec.describe Game do
+
+it "has method of start to begin the game" do
+
+end
+
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+end
---
+++ war_or_peace_runner.rb
@@ -0,0 +1,35 @@
+require "./lib/game.rb"
+
+game = Game.new
+game.start
+
+
+
+# require "pry";binding.pry
+#Will need to run into something to manage all of the turns
+
+# full_deck = Deck.new(cards)
+# cards.shuffle!
+#
+# deck1 = Deck.new(full_deck.cards[0, 26])
+# deck1.cards.length
+# deck2 = Deck.new(full_deck.cards[26, 52])
+# deck2.cards.length
+# require "pry";binding.pry
+
+
+
+
+  #How to select group of items from array google search
+  #How to select group of itemfrom
+  # negative numbers for index positions will work from the back of an array
+  #Substrings
+  #.. up to and including (for strings and arrays) can us this in delete and slice to remove huge sections
+  #... is up to and NOT including (for strings and arrays) can use ths in delete and slice to remove huge sections
+#
+
+# def shuffle_deck
+#   full_deck.shuffle!
+# end
+
+# p full_deck.shuffle_deck
