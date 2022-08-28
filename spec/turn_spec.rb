4d0f8045cc65d14a8fcd17f379af7626d6750a58
Patricia Severance <patriciaseverance@gmail.com>
2022-08-28 15:06:04 -0600

    Add the rest of pile_cards and test cases for real this time

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -186,5 +186,26 @@ end
 
         expect(turn.spoils_of_war).to eq [card1, card3, card2, card4, card5, card6]
         expect(player1.deck.cards).to eq [card8]
+
+        card1 = Card.new(:heart, 'Jack', 11)
+        card2 = Card.new(:heart, '10', 10)
+        card3 = Card.new(:heart, 'Jack', 11)
+        card4 = Card.new(:diamond, 'Jack', 11)
+        card5 = Card.new(:heart, 'Jack', 11)
+        card6 = Card.new(:heart, 'Jack', 11)
+        card7 = Card.new(:heart, 'Jack', 11)
+        card8 = Card.new(:diamond, '2', 2)
+
+        deck1 = Deck.new([card1, card2, card5, card8])
+        deck2 = Deck.new([card3, card4, card6, card7])
+
+        player1 = Player.new("Megan", deck1)
+        player2 = Player.new("Aurora", deck2)
+        turn = Turn.new(player1, player2)
+
+        turn.pile_cards
+
+        expect(turn.spoils_of_war).to eq []
+        expect(player1.deck.cards).to eq [card8]
       end
     end
