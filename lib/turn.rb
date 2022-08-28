4e6f9a5885d3fad25bd4dda869bc880e3fbb7528
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2022-08-28 11:43:17 -0600

    fix start method introduction

--- lib/turn.rb
+++ lib/turn.rb
@@ -67,10 +67,10 @@ class Turn
     winner.deck.cards.concat(@spoils_of_war)  
   end  
   def start
-    p "Welcome to War! (or Peace) This game will be played with 52 cards.
-    p  The players today are Megan and Aurora.
-    p  Type 'GO' to start the game!
-    p ------------------------------------------------------------------" 
+    p "Welcome to War! (or Peace) This game will be played with 52 cards."
+    p  "The players today are Megan and Aurora."
+    p  "Type 'GO' to start the game!"
+    p "------------------------------------------------------------------" 
   
     input = gets.chomp.upcase
     if 
