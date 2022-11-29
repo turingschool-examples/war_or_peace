623d74ec5a8dfc5eb44c02910cd98a35ab1c0108
KaraJH <karas.jones@yahoo.com>
2022-11-29 16:06:52 -0700

    Creates passing basic for the type test

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,4 +6,11 @@ class Turn
     @player2 = player2 
     @spoils_of_war = []
   end
+
+  def type 
+    #require 'pry'; binding.pry
+    if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
+      :basic
+    end
+  end
 end
