c1623c7ca056e50965a89a56f4d041d4d367dede
KaraJH <karas.jones@yahoo.com>
2022-11-29 14:49:33 -0700

    Creates high ranking cards method

--- lib/deck.rb
+++ lib/deck.rb
@@ -8,4 +8,12 @@ class Deck
   def rank_of_card_at(given_index)
     @cards[given_index].rank
   end
+
+  def high_ranking_cards 
+    @cards.map do |card| 
+      if card.rank >= 11 
+        card
+      end
+    end.compact
+  end
 end
