6dbbe113d2b29fedc51e4651c033af5f5ea4bf74
KaraJH <karas.jones@yahoo.com>
2022-11-29 14:47:13 -0700

    Tests for high ranking cards method

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -26,4 +26,17 @@ RSpec.describe do Deck
       expect(deck.rank_of_card_at(2)).to eq(14)
     end
   end
+
+  describe '#high_ranking_cards' do 
+    it 'returns array of cards in the deck that have a rank of 11 or more' do 
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+
+      expect(deck.cards).to eq(cards)  
+      expect(deck.high_ranking_cards).to eq([card1, card3])
+    end
+  end
 end
