d2ed58cce00bbf75b342d80bac8dc31130b88ab9
Brian Hayes <Brian.hayes1200@gmail.com>
2022-11-29 14:41:24 -0700

    Test: add test for remove card method for class Deck

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -28,6 +28,19 @@ RSpec.describe Deck do
     end 
   end 
 
+  describe '#remove card' do 
+    it 'will remove a card from the deck' do 
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)   
+      card3 = Card.new(:heart, 'Ace', 14) 
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+    
+      expect(deck.remove_card).to eq(card1)
+    end 
+  end 
+
+
   describe '#high ranking cards' do 
     it 'will return an array of the high ranking cards' do 
       card1 = Card.new(:diamond, 'Queen', 12)
