a85f8cb47e46cce5f5f713224ae1582f651124b9
KaraJH <karas.jones@yahoo.com>
2022-11-29 15:15:53 -0700

    Tests for the has lost method

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -13,6 +13,23 @@ RSpec.describe Player do
 
     expect(player).to be_a(Player)
     expect(player.name).to eq('Clarisa')
-    expect(player.deck).to eq([card1, card2, card3])
+    expect(player.deck).to eq(deck)
+  end
+
+  describe '#has_lost?' do 
+    it 'returns true is the player has no more cards in their deck' do 
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)  
+      card3 = Card.new(:heart, 'Ace', 14)
+      deck = Deck.new([card1, card2, card3]) 
+      player = Player.new('Clarisa', deck) 
+
+      expect(player.has_lost?).to eq(false)
+
+      player.deck.remove_card
+
+      expect(player.has_lost?).to eq(false)
+      #expect(player.deck).to eq([card2, card3])
+    end
   end
 end
