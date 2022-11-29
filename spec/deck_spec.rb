e3e0e967461f02db3ad8fb85eb29c6a997fa3c9d
KaraJH <karas.jones@yahoo.com>
2022-11-29 15:00:15 -0700

    Tests add card method

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -66,5 +66,23 @@ RSpec.describe do Deck
       expect(deck.high_ranking_cards).to eq([card3])
       expect(deck.percent_high_ranking).to eq(50.0)
     end
+  end 
+
+  describe '#add_card' do 
+    it 'adds one card to the bottom of the deck' do 
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards) 
+
+      expect(deck.cards).to eq(cards)
+
+      card4 = Card.new(:club, '5', 5)
+      deck.add_card(card4)
+
+      expect(deck.cards).to eq([card1, card2, card3, card4])
+
+    end
   end
 end
