bb7ac306592dba63c53f28b03cdb1b35326714f7
Brian Hayes <Brian.hayes1200@gmail.com>
2022-11-29 14:25:58 -0700

    Test: add test for rank_of_card_at method for class deck

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -14,4 +14,17 @@ RSpec.describe Deck do
       expect(deck.cards).to eq(cards)
     end 
   end 
+
+  describe '#rank of card at' do 
+    it 'will return the rank of the card at index position' do 
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)   
+      card3 = Card.new(:heart, 'Ace', 14) 
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+      
+      expect(deck.rank_of_card_at(0)).to eq(12)
+      expect(deck.rank_of_card_at(2)).to eq(14)
+    end 
+  end 
 end
