bd08fcc09a9406cbb728f93d760d39544b62f73f
Brian Hayes <Brian.hayes1200@gmail.com>
2022-11-29 15:29:37 -0700

    Test: add test for intitialize method for class Player

---
+++ spec/player_spec.rb
@@ -0,0 +1,20 @@
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+
+RSpec.describe Player do 
+  describe '#initialize' do 
+    it 'will exist and have attributes' do 
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)   
+      card3 = Card.new(:heart, 'Ace', 14) 
+      deck = Deck.new([card1, card2, card3])
+      player = Player.new('Clarisa', deck)
+
+      expect(player).to be_a(Player)
+      expect(player.name).to eq('Clarisa')
+      expect(player.deck).to eq(deck)
+      expect(player.has_lost?).to eq(false)
+    end 
+  end 
+end 
