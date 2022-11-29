54f42416fdf7fa79b8cf9619bf28cbcaa774affc
KaraJH <karas.jones@yahoo.com>
2022-11-29 15:11:30 -0700

    Tests for player class and attributes

---
+++ spec/player_spec.rb
@@ -0,0 +1,18 @@
+require 'rspec' 
+require './lib/card'
+require './lib/deck' 
+require './lib/player'
+
+RSpec.describe Player do 
+  it 'exists and has a deck of cards' do 
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)  
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3]) 
+    player = Player.new('Clarisa', deck) 
+
+    expect(player).to be_a(Player)
+    expect(player.name).to eq('Clarisa')
+    expect(player.deck).to eq([card1, card2, card3])
+  end
+end
