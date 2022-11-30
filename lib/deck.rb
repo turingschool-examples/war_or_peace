125f97a729c75e05e7d397c75c7cafc95b3a309b
Hady Matar <hadyematar@gmail.com>
2022-11-30 08:00:51 -0700

    Commit before changes on test 4

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,12 +2,13 @@ class Deck
 attr_reader :cards 
             :high_rankers
 
-def initialize(cards)
-    @cards = cards 
+def initialize(arrayofcards)
+    @cards = arrayofcards
+    @high_rankers = []
 end 
 
 def cards
-    @cards
+    @cards 
 end 
 
 def rank_of_card_at(index) 
@@ -16,24 +17,41 @@ def rank_of_card_at(index)
 end 
 
 def high_ranking_cards 
-@high_rankers = []
     @cards.each do |card|
     if card.rank >= 11 
-        @high_rankers << card
-        
+    
+    @high_rankers << card
+    # require 'pry'; binding.pry
+    
     end 
 end 
 return @high_rankers
 end 
 
 def percent_high_ranking
-    ((@high_rankers.count.to_f / @cards.count.to_f)*100).round(2)
 
-   
+    rawnumber = (@high_rankers.count).to_f / (@cards.count).to_f
+    # require 'pry'; binding.pry
+    
+    percent = (rawnumber*100).round(2)
+    return percent
 end 
 
 def remove_card
-    @cards = @cards.shift
+    deleted_card = @cards.first 
+    @cards.delete(deleted_card) 
+    @cards 
+    
+    # deleted_card = @cards.(0)
+    # @cards.delete(deleted_card)
+    # @cards
+    # require 'pry'; binding.pry
+    # if @high_rankers.rank.include?(@cards[0].rank)
+        
+    #      @high_rankers.delete(@cards[0])
+    # end 
+    # return @high_rankers
+     
 end 
 
 def add_card(card) 
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -15,28 +15,48 @@ describe Deck do
         expect(deck.rank_of_card_at(0)).to eq(12)
     end 
 
-    it 'its opponent is ranked 14'do 
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-    deck.cards
+        it 'its opponent is ranked 14'do 
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+        cards = [card1, card2, card3]
+        deck = Deck.new(cards)
+        deck.cards
     
     expect(deck.rank_of_card_at(2)).to eq(14)
     end 
 
-    it 'it is 2/3 high ranking cards' do 
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-    deck.cards
-    deck.high_ranking_cards
+        it 'it is 2/3 high ranking cards' do 
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+        cards = [card1, card2, card3]
+        deck = Deck.new(cards)
+        deck.cards
+        deck.high_ranking_cards
 
     expect(deck.percent_high_ranking).to eq(66.67) 
+    end
+
+    it 'removes one high ranking card' do 
+
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+        cards = [card1, card2, card3]
+        deck = Deck.new(cards)
+        deck.cards
+        deck.high_ranking_cards
+        deck.remove_card
+        deck.cards
+        deck.high_ranking_cards
+        
+
+    expect(deck.percent_high_ranking).to eq(50.0)
     end 
+
+
+
 
     end 
 end 
