71419c01c0297971253c19c5b4f1c9bf7d35feb0
Matt Enyeart <menyeart1@gmail.com>
2022-11-29 18:41:22 -0700

    Add the high_ranking_cards method and test

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,8 +1,15 @@
 class Deck
   attr_reader :cards
 
-  def intialize(cards)
+  def initialize(cards)
     @cards = cards
   end
-  
+
+  def rank_of_card_at(index)
+    @cards[index].rank
+  end
+
+  def high_ranking_cards
+    @cards.select {|card| card.rank > 10}
+  end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -10,8 +10,28 @@ RSpec.describe Deck do
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
     cards = [card1, card2, card3]
-    deck = Deck.new()
+    deck = Deck.new(cards)
     expect(deck).to be_an_instance_of(Deck)
-    binding.pry
   end
+
+
+  it "contains an array of cards" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    expect(deck.rank_of_card_at(2)).to eql(14)
+  end
+
+  it "contains high ranking cards" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    expect(deck.high_ranking_cards).to eql([card1, card3])
+  end
+
+
 end
