1fd88704bd1850c73792f824cc76abc8ddb0b251
Caleb Thomas <cjthomas00@comcast.net>
2022-11-30 16:44:38 -0700

    initialize deck.

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,7 +1,12 @@
 class Deck
-    attr_reader :cards
+    attr_reader :cards, :high_ranking
     
     def initialize(cards = [])
         @cards = cards
+
+    end
+
+    def rank_of_card_at(index)
+        @cards[index].rank
     end
 end
