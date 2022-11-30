b6961ceef4ce19329f78ee7f57883dc8ceaf74fd
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-11-29 20:11:54 -0800

    Create and pass high_ranking_cards test

--- lib/deck.rb
+++ lib/deck.rb
@@ -3,7 +3,14 @@ class Deck
     def initialize(cards)
         @cards = cards
     end
+
     def rank_of_card_at(index)
         cards[index].rank
+    end
+
+    def high_ranking_cards
+        cards.select do |card|
+            card.rank >= 11
+        end 
     end
 end 
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -41,6 +41,19 @@ describe Deck do
             deck = Deck.new(cards)
             expect(deck.rank_of_card_at(0)).to be (12)
             expect(deck.rank_of_card_at(2)).to be (14)
+            expect(deck.cards).to eq([card1, card2, card3])
+        end 
+    end 
+
+    describe 'high_ranking_cards' do
+        it 'identifies the cards ranked 11 or above' do
+            card1 = Card.new(:diamond, 'Queen', 12)
+            card2 = Card.new(:spade, '3', 3)
+            card3 = Card.new(:heart, 'Ace', 14)
+
+            cards = [card1, card2, card3]
+            deck = Deck.new(cards)
+            expect(deck.high_ranking_cards).to eq ([card1, card3])
         end 
     end 
 end 
