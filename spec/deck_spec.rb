9ebb7ab966ba873ba2bcb2a85df8684e7e553c3c
Harrison Ryan <hwryan12@gmail.com>
2022-11-30 08:56:47 -0600

    Add 2nd test deck_spec.rb

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -12,4 +12,15 @@ Rspec.describe Deck do
 
         expect(deck).to be_an_instance_of(Deck)
     end
+
+    it "returns rank value at given index" do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+        cards = [card1, card2, card3]
+        deck = Deck.new(cards)
+
+        expect(deck.rank_of_card_at(0)).to eq 12
+        expect(deck.rank_of_card_at(2)).to eq 14
+    end
 end
