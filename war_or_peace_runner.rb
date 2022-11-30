e4702eae68f7c38eeae4f0884c77639064852c2e
Weston Sandfort <sandfortw@gmail.com>
2022-11-29 18:46:59 -0700

    Add diamond and spade cards

---
+++ war_or_peace_runner.rb
@@ -0,0 +1,34 @@
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require 'binding.pry'
+
+#THE DIAMONDS
+card1 = Card.new(:diamond, 'Two', 1)
+card2 = Card.new(:diamond, 'Three', 2)
+card3 = Card.new(:diamond, 'Four', 3)
+card4 = Card.new(:diamond, 'Five', 4)
+card5 = Card.new(:diamond, 'Six', 5)
+card6 = Card.new(:diamond, 'Seven', 6)
+card7 = Card.new(:diamond, 'Eight', 7)
+card8 = Card.new(:diamond, 'Nine', 8)
+card10 = Card.new(:diamond, 'Ten', 9)
+card11 = Card.new(:diamond, 'Jack', 10)
+card12 = Card.new(:diamond, 'Queen', 11)
+card13 = Card.new(:diamond, 'King', 12)
+card14 = Card.new(:diamond, 'Ace', 13)
+
+#THE SPADES
+card15 = Card.new(:spade, 'Two', 1)
+card16 = Card.new(:spade, 'Three', 2)
+card17 = Card.new(:spade, 'Four', 3)
+card18 = Card.new(:spade, 'Five', 4)
+card19 = Card.new(:spade, 'Six', 5)
+card20 = Card.new(:spade, 'Seven', 6)
+card21 = Card.new(:spade, 'Eight', 7)
+card22 = Card.new(:spade, 'Nine', 8)
+card23 = Card.new(:spade, 'Ten', 9)
+card24 = Card.new(:spade, 'Jack', 10)
+card25 = Card.new(:spade, 'Queen', 11)
+card26 = Card.new(:spade, 'King', 12)
+card27 = Card.new(:spade, 'Ace', 13)
