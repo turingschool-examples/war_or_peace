2368e5d3657d37e345c59b3dfbfbfd6d95b13afd
Huy Phan <huyphan520@gmail.com>
2022-11-29 23:17:36 -0600

    Deck class completed percent high ranking

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,7 +4,25 @@ class Deck
         @cards = cards
     end
 
-    def rank_of_cards_at
+    def rank_of_cards_at(index)
+        cards[index].rank
         
     end
+
+    def high_ranking_cards
+        #high_ranking_cards = []
+        cards.select do |card|
+            card.rank >= 11
+        
+        end
+        #high_ranking_cards
+        
+    end
+
+    def percent_high_ranking
+        ((high_ranking_cards.size * 100) / cards.size.to_f).round(2) 
+
+    end
+
+
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -35,7 +35,30 @@ describe Deck do
 
         deck = Deck.new(cards)
 
-        expect(rank_of_cards_at).to eq(12)
-        expect(rank_of_cards_at).to eq(14)
+        expect(deck.rank_of_cards_at(0)).to eq(12)
+        expect(deck.rank_of_cards_at(2)).to eq(14)
     end
+
+    it "high ranking cards" do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)    
+        card3 = Card.new(:heart, 'Ace', 14)   
+        cards = [card1, card2, card3]
+
+        deck = Deck.new(cards)
+
+        expect(deck.high_ranking_cards).to eq([card1, card3])
+    end
+
+    it "percent high ranking" do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)    
+        card3 = Card.new(:heart, 'Ace', 14)   
+        cards = [card1, card2, card3]
+
+        deck = Deck.new(cards)
+
+        expect(deck.percent_high_ranking).to eq(66.67)
+    end
+    
 end
