ac4d0923f8f54a43d3bf54c4891af656fdb9218b
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-11-29 20:53:55 -0800

    Create and pass 'is an instance of the Player class' test

--- lib/player.rb
+++ lib/player.rb
@@ -1,2 +1,7 @@
 class Player
+    attr_reader :name, :deck
+    def initialize(name, deck)
+        @name = name
+        @deck = deck
+    end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -14,5 +14,14 @@ describe Player do
             deck = Deck.new([card1, card2, card3])
             expect(deck).to be_a(Deck) 
         end
+
+        it 'is an instance of the Player class' do
+            card1 = Card.new(:diamond, 'Queen', 12)
+            card2 = Card.new(:spade, '3', 3)
+            card3 = Card.new(:heart, 'Ace', 14)
+            deck = Deck.new([card1, card2, card3])
+            player = Player.new('Clarisa', deck)
+            expect(player).to be_a(Player)
+        end
     end
 end
