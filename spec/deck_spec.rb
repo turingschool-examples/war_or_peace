27efe8d963f0432f8a240c22c5116e95aebe9fc2
Elle Majors <hellolovely@ellemajors>
2022-11-29 19:17:07 -0700

    test: #initialize

---
+++ spec/deck_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+require './lib/card'
+require './lib/deck'
+
+describe Deck do
+  describe '#initialize' do
+    it 'exists and has attributes' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3) 
+      card3 = Card.new(:heart, 'Ace', 14)
+
+      cards = [card1, card2, card3]
+
+      deck = Deck.new(cards)
+      
+      expect(deck).to be_a(Deck)
+      expect(deck.cards).to eq([card1, card2, card3])
+    end
+  end
+end
