2f283fd646afc0e36454a37877badd3f9ba605ec
KaraJH <karas.jones@yahoo.com>
2022-11-29 17:31:34 -0700

    Tests the winner method for a war type of turn

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -81,6 +81,7 @@ RSpec.describe Turn do
 
   describe '#winner' do 
     it 'returns who the winner of each round is' do 
+      #basic winner tested here
       card1 = Card.new(:heart, 'Jack', 11)
       card2 = Card.new(:heart, '10', 10)    
       card3 = Card.new(:heart, '9', 9)    
@@ -101,6 +102,28 @@ RSpec.describe Turn do
 
       expect(turn.type).to eq(:basic)
       expect(turn.winner).to eq(player1)
+    end
+
+    it 'returns the winner for WAR type, higher card at index (2)' do 
+      card1 = Card.new(:heart, 'Jack', 11)    
+      card2 = Card.new(:heart, '10', 10)    
+      card3 = Card.new(:heart, '9', 9)    
+      card4 = Card.new(:diamond, 'Jack', 11)    
+      card5 = Card.new(:heart, '8', 8)    
+      card6 = Card.new(:diamond, 'Queen', 12)    
+      card7 = Card.new(:heart, '3', 3)    
+      card8 = Card.new(:diamond, '2', 2)   
+
+      deck1 = Deck.new([card1, card2, card5, card8]) 
+      deck2 = Deck.new([card4, card3, card6, card7]) 
+
+      player1 = Player.new("Megan", deck1) 
+      player2 = Player.new("Aurora", deck2)
+
+      turn = Turn.new(player1, player2)
+
+      expect(turn.type).to eq(:war)
+      expect(turn.winner).to eq(player2)
     end
   end
 
