7254768cd45ac01f515dba08adb3f75a40f4e40d
Matt Enyeart <menyeart1@gmail.com>
2022-11-29 18:43:44 -0700

    Add the 4th test to evaluate the percentage of high ranking cards

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -33,5 +33,13 @@ RSpec.describe Deck do
     expect(deck.high_ranking_cards).to eql([card1, card3])
   end
 
+  it "contains a certain percentage of high ranking cards" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    expect(deck.percent_high_ranking).to eql(66.37)
+  end
 
 end
