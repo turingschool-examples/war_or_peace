6ac1b58c6d69fe6beacafbb272920147e7e0d43a
Matt Enyeart <menyeart1@gmail.com>
2022-11-30 16:53:11 -0700

    Add a test to determine if player has lost the game when the deck is empty

--- lib/player.rb
+++ lib/player.rb
@@ -6,4 +6,6 @@ def initialize(name, deck)
   @deck = deck
 end
 
+
+
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,6 +1,7 @@
 require './lib/card'
 require './lib/deck'
 require './lib/player'
+require 'pry'
 
 RSpec.describe Player do
   it "exists" do
@@ -11,4 +12,17 @@ RSpec.describe Player do
     player = Player.new('Clarisa', deck)
     expect(player).to be_an_instance_of(Player)
   end
+
+  it "can lose a game when the deck is empty" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+    expect(player.has_lost?). to eq(false)
+  end
+
+
+
+
 end
