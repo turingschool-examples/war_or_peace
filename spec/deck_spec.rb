7d5720c4edb44f3c1c142348132a86000b939da5
Axel De La Guardia <axeldelaguardia@gmail.com>
2022-11-29 19:58:45 -0700

    Add one more test to remove/add cards and return percentage.

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -53,4 +53,25 @@ RSpec.describe Deck do
     deck = Deck.new(cards)
     expect(deck.percent_high_ranking).to eq('66.67%')
   end
+
+  it 'can update the #percent_high_ranking when adding and removing cards' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+
+    expect(deck.percent_high_ranking).to eq('66.67%')
+
+    deck.remove_card
+    expect(deck.cards.count).to eq(2)
+    expect(deck.high_ranking_cards.count).to eq(1)
+    expect(deck.percent_high_ranking).to eq('50.0%')
+
+    card4 = Card.new(:club, '5', 5)
+    deck.add_card(card4)
+    expect(deck.cards.count).to eq(3)
+    expect(deck.high_ranking_cards.count).to eq(1)
+    expect(deck.percent_high_ranking).to eq('33.33%')
+  end
 end
