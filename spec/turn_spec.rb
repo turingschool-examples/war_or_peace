fc2d214ae77380a67fc60c6b24449b85b8fbb835
KaraJH <karas.jones@yahoo.com>
2022-11-29 18:17:10 -0700

    Tests award spoils for a war type of turn

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -128,7 +128,8 @@ RSpec.describe Turn do
   end
 
   describe '#pile_cards' do 
-    it 'sends players cards to the spoils of war based on the type of turn' do 
+    it 'sends players cards to the spoils of war based on the type of turn' do
+       #tests for basic type of turn here
       card1 = Card.new(:heart, 'Jack', 11)
       card2 = Card.new(:heart, '10', 10)    
       card3 = Card.new(:heart, '9', 9)    
@@ -218,6 +219,38 @@ RSpec.describe Turn do
       expect(player1.deck).to eq(deck1)
       expect(player2.deck).to eq(deck2)
       expect(player1.deck.cards).to eq([card2, card5, card8, card1, card3])
+    end
+
+    it 'awards spoils to the winner of the WAR turn' do 
+      card1 = Card.new(:heart, 'Jack', 11)    
+      card2 = Card.new(:heart, '10', 10)    
+      card3 = Card.new(:heart, '9', 9)    
+      card4 = Card.new(:diamond, 'Jack', 11)    
+      card5 = Card.new(:heart, '8', 8)    
+      card6 = Card.new(:diamond, 'Queen', 12)    
+      card7 = Card.new(:heart, '3', 3)    
+      card8 = Card.new(:diamond, '2', 2)   
+
+      deck1 = Deck.new([card1, card2, card5, card8]) 
+      deck2 = Deck.new([card4, card3, card6, card7]) 
+
+      player1 = Player.new("Megan", deck1) 
+      player2 = Player.new("Aurora", deck2)
+
+      turn = Turn.new(player1, player2)
+
+      expect(turn.type).to eq(:war)
+      expect(turn.winner).to eq(player2) 
+
+      winner = turn.winner
+      turn.pile_cards 
+    
+      expect(turn.spoils_of_war).to eq([card1, card2, card5, card4, card3, card6])
+
+      turn.award_spoils(winner)
+
+      expect(player1.deck).to eq(deck1)
+      expect(player2.deck).to eq(deck2)
     end
   end
 end
