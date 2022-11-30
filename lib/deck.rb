3bf31233e2a475b87ded5dd7aa956395484f1b60
Bobby Luly <bobbyluly@gmail.com>
2022-11-29 22:07:41 -0700

    Add percentage high ranking method

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,13 +1,22 @@
 class Deck
-    attr_reader :cards
+    attr_reader :cards, :high_ranking_cards
 
     def initialize(cards)
         @cards = cards
-        
     end
 
     def rank_of_card_at(index)
         cards[index].rank
+    end
+
+    def high_ranking_cards
+        cards.select do |card|
+        card.rank >= 11
+        end
+    end
+
+    def percent_high_ranking
+        ((high_ranking_cards.size) * 100 / cards.size.to_f).round(2)
     end
 
 end
