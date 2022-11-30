49b30afc9b1ab25700f5dc3c33ff509f6825ba92
Matt Enyeart <menyeart1@gmail.com>
2022-11-29 18:23:56 -0700

    Add code to complete the first test

---
+++ lib/deck.rb
@@ -0,0 +1,8 @@
+class Deck
+  attr_reader :cards
+
+  def intialize(cards)
+    @cards = cards
+  end
+  
+end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,11 +1,17 @@
 require 'rspec'
 require './lib/card'
 require './lib/deck'
+require 'pry'
 
 
 RSpec.describe Deck do
   it "exists" do
-    deck = Deck.new(cards)
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new()
     expect(deck).to be_an_instance_of(Deck)
+    binding.pry
   end
 end
