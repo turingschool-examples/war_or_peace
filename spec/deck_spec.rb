186bb036c97274b2ab94f09a636a646d7fdab4a0
Harrison Ryan <hwryan12@gmail.com>
2022-11-30 09:00:14 -0600

    Add 3rd test deck_spec.rb

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -23,4 +23,15 @@ Rspec.describe Deck do
         expect(deck.rank_of_card_at(0)).to eq 12
         expect(deck.rank_of_card_at(2)).to eq 14
     end
+
+    it "returns high ranking cards and the percent amount of them" do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+        cards = [card1, card2, card3]
+        deck = Deck.new(cards)
+
+        expect(deck.high_ranking_cards).to eq [card1, card3]
+        expect(deck.percent_high_ranking).to eq 66.67
+    end
 end
