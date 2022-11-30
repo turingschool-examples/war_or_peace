5b048fcc69aec4b9d82b81d495f7556eed859032
Caleb Thomas <cjthomas00@comcast.net>
2022-11-29 21:12:18 -0700

    create deck_spec file.

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,3 +1,15 @@
 require 'rspec'
-#require 'pry'; binding.pry
+require 'pry'; binding.pry
+require './lib/card'
 require './lib/deck'
+
+
+
+
+
+RSpec.describe Card do
+    it "adds one card" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    require 'pry'; binding.pry
+    end
+end
