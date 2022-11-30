47bb73ee36d8e9781a5e925ab6137359814aa488
KaraJH <karas.jones@yahoo.com>
2022-11-30 16:21:42 -0700

    Tests for game class and creates files for runner and game class

---
+++ spec/game_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require './lib/card'
+require './lib/deck' 
+require './lib/player'
+require './lib/turn'
+require './lib/game'
+
+RSpec.describe Game do 
+  it 'exists and has a turn count' do 
+    game = Game.new
+
+    expect(game).to be_a(Game)
+    expect(game.turn_count).to eq(0)
+  end
+end
