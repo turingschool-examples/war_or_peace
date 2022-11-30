08acd6117bd977cde240cb4320d44eb1dc640683
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-11-29 20:23:39 -0800

    Create and pass percent_high_ranking test

--- lib/deck.rb
+++ lib/deck.rb
@@ -13,4 +13,8 @@ class Deck
             card.rank >= 11
         end 
     end
+
+    def percent_high_ranking
+        ((high_ranking_cards.count.to_f / cards.count.to_f) * 100).round(2)
+    end
 end 
