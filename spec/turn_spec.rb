ba23be1a9a4ad760c4e294a47e0c6c5c7b09be1b
KaraJH <karas.jones@yahoo.com>
2022-11-29 18:30:54 -0700

    Tests the type method for mutually assured destruction

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -76,6 +76,27 @@ RSpec.describe Turn do
       turn = Turn.new(player1, player2)
 
       expect(turn.type).to eq(:war)
+    end 
+
+    it 'is a MUTUALLY ASSURED DESTRUCTION type when both players ranks at 0 and 2 are the same' do 
+      card1 = Card.new(:heart, 'Jack', 11)    
+      card2 = Card.new(:heart, '10', 10)    
+      card3 = Card.new(:heart, '9', 9)    
+      card4 = Card.new(:diamond, 'Jack', 11)    
+      card5 = Card.new(:heart, '8', 8)    
+      card6 = Card.new(:diamond, '8', 8)    
+      card7 = Card.new(:heart, '3', 3)    
+      card8 = Card.new(:diamond, '2', 2) 
+
+      deck1 = Deck.new([card1, card2, card5, card8])  
+      deck2 = Deck.new([card4, card3, card6, card7])  
+
+      player1 = Player.new("Megan", deck1) 
+      player2 = Player.new("Aurora", deck2) 
+
+      turn = Turn.new(player1, player2)
+
+      expect(turn.type).to eq(:mutually_assured_destruction)
     end
   end
 
@@ -200,21 +221,17 @@ RSpec.describe Turn do
       player1 = Player.new("Megan", deck1) 
       player2 = Player.new("Aurora", deck2)    
 
-
       turn = Turn.new(player1, player2) 
 
       expect(turn.type).to eq(:basic)
       expect(turn.winner).to eq(player1) 
 
       winner = turn.winner
-
       turn.pile_cards 
 
       expect(turn.spoils_of_war).to eq([card1, card3])
 
       turn.award_spoils(winner) 
-
-      #require 'pry'; binding.pry
 
       expect(player1.deck).to eq(deck1)
       expect(player2.deck).to eq(deck2)
