b86d1af474ddd9e6d3efaf3d59152742be9e84c1
Andra Helton <andrahelton@gmail.com>
2022-11-29 20:50:35 -0700

    Add rank of cards to deck_spec.rb

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -21,8 +21,21 @@ RSpec.describe Deck do
     cards = [card1, card2, card3]
 
     deck = Deck.new(cards)
+
     expect(deck.cards).to eq([card1, card2, card3])
 
+  end
+
+  it "rank of cards" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    expect(deck.rank_of_cards_at(0)).to eq(12)
+    expect(deck.rank_of_cards_at(2)).to eq(14)
   end
 
 end
