f5560ceae9dbf59afe13f1d8f6bcd8a01a3d4673
Axel De La Guardia <axeldelaguardia@gmail.com>
2022-11-30 10:32:00 -0700

    Add @award_spoils to winner test and method

--- lib/turn.rb
+++ lib/turn.rb
@@ -41,14 +41,26 @@ class Turn
 
     def pile_cards
         if type == :basic
-            @spoils_of_war << @player1.deck.cards[0]
-            @spoils_of_war << @player2.deck.cards[0]
+            @spoils_of_war << @player1.deck.cards.shift
+            @spoils_of_war << @player2.deck.cards.shift
         elsif type == :war
-            @spoils_of_war << @player1.deck.cards[0..2]
-            @spoils_of_war << @player2.deck.cards[0..2]
+            3.times {@spoils_of_war << @player1.deck.cards.shift}
+            3.times {@spoils_of_war << @player2.deck.cards.shift}
         elsif type == :war
             @player.deck.cards = @player.deck.cards.drop(3)
             @player.deck.cards = @player.deck.cards.drop(3)
+        end
+    end
+
+    def award_spoils(winner)
+        if winner == @player1
+            (@player1.deck.cards).concat(@spoils_of_war)
+            @spoils_of_war.clear
+            return @player1.deck.cards
+        elsif winner == @player2
+            (@player2.deck.cards).concat(@spoils_of_war)
+            @spoils_of_war.clear
+            return @player2.deck.cards
         end
     end
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -5,7 +5,7 @@ require './lib/turn'
 require 'rspec'
 
 describe Turn do
-    describe 'initialize' do
+    describe 'turn type :basic' do
         before(:each) do
             @card1 = Card.new(:heart, 'Jack', 11)
             @card2 = Card.new(:heart, '10', 10)
@@ -21,25 +21,61 @@ describe Turn do
             @player2 = Player.new("Aurora", @deck2)
             @turn = Turn.new(@player1, @player2)
         end
+
         it 'exists and has two player' do
             expect(@turn).to be_a(Turn)
             expect(@turn.player1).to eq(@player1)
             expect(@turn.player2).to eq(@player2)
         end
+
         it '#spoils_of_war is default empty' do
             expect(@turn.spoils_of_war).to be_empty
         end
+
         it 'current #type of turn' do
             expect(@turn.type).to eq(:basic)
         end
+
         it 'returns #winner of turn' do
             expect(@turn.winner).to eq(@player1)
         end
+
         it 'use #pile_cards to send cards to @spoils_of_war' do
             winner = @turn.winner
             @turn.pile_cards
 
             expect(@turn.spoils_of_war.count).to eq(2)
+        end
+
+        it "@award_spoils to the winner's deck" do
+            winner = @turn.winner
+            @turn.pile_cards
+            @turn.award_spoils(winner)
+
+            expect(@player1.deck.cards.count).to eq(5)
+            expect(@player2.deck.cards.count).to eq(3)
+        end
+    end
+
+    describe 'Turn type :war' do
+        before(:each) do
+            @card1 = Card.new(:heart, 'Jack', 11)
+            @card2 = Card.new(:heart, '10', 10)
+            @card3 = Card.new(:heart, '9', 9)
+            @card4 = Card.new(:diamond, 'Jack', 11)
+            @card5 = Card.new(:heart, '8', 8)
+            @card6 = Card.new(:diamond, 'Queen', 12)
+            @card7 = Card.new(:heart, '3', 3)
+            @card8 = Card.new(:diamond, '2', 2)
+            @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+            @deck2 = Deck.new([@card4, @card3, @card6, @card7])
+            @player1 = Player.new("Megan", @deck1)
+            @player2 = Player.new("Aurora", @deck2)
+            @turn = Turn.new(@player1, @player2)
+        end
+
+        it 'is turn type :war' do
+            expect(@turn.type).to eq(:war)
         end
     end
 end
