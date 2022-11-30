e63ada71d539577b1aaa363d24a07674d498d74d
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-11-29 20:59:23 -0800

    Create and pass 'has a name' test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -23,5 +23,14 @@ describe Player do
             player = Player.new('Clarisa', deck)
             expect(player).to be_a(Player)
         end
+
+        it 'has a name' do
+            card1 = Card.new(:diamond, 'Queen', 12)
+            card2 = Card.new(:spade, '3', 3)
+            card3 = Card.new(:heart, 'Ace', 14)
+            deck = Deck.new([card1, card2, card3])
+            player = Player.new('Clarisa', deck)
+            expect(player.name).to eq("Clarisa")
+        end
     end
 end
