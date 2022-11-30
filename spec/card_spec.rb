34f959c52360693f557b78f0140360646121f452
Chris Crane <chrisjcrane@gmail.com>
2022-11-29 19:53:22 -0700

    complete card and create deck.rb and deck_spec.rb

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -15,4 +15,13 @@ RSpec.describe Card do
     expect(card.value).to eq('Queen')
     expect(card.rank).to eq(12)
   end
+
+  it 'can be a different card' do
+    card = Card.new(:heart, 'Ace', 13)
+
+    expect(card.suit).to eq(:heart)
+    expect(card.value).to eq('Ace')
+    expect(card.rank).to eq(13)
+  end
+
 end
