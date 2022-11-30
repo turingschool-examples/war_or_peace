2ba237380121a3556b3e80131401a60b6a8cab2f
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-11-29 21:00:58 -0800

    Create and pass 'has a deck' test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -32,5 +32,14 @@ describe Player do
             player = Player.new('Clarisa', deck)
             expect(player.name).to eq("Clarisa")
         end
+
+        it 'has a deck' do
+            card1 = Card.new(:diamond, 'Queen', 12)
+            card2 = Card.new(:spade, '3', 3)
+            card3 = Card.new(:heart, 'Ace', 14)
+            deck = Deck.new([card1, card2, card3])
+            player = Player.new('Clarisa', deck)
+            expect(player.deck).to be_a(Deck)
+        end
     end
 end
