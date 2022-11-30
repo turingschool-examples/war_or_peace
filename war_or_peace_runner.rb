336df25ab3f46bbb22e656d00e0720264028305b
Weston Sandfort <sandfortw@gmail.com>
2022-11-29 19:59:39 -0700

    Create two shuffled decks, and assign them to our two players, Megan and Aurora

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -1,7 +1,8 @@
 require './lib/card'
 require './lib/deck'
 require './lib/player'
-require 'binding.pry'
+require 'pry'
+
 
 #THE DIAMONDS
 card1 = Card.new(:diamond, 'Two', 1)
@@ -62,3 +63,32 @@ card49 = Card.new(:club, 'Jack', 10)
 card50 = Card.new(:club, 'Queen', 11)
 card51 = Card.new(:club, 'King', 12)
 card52 = Card.new(:club, 'Ace', 13)
+
+big_deck = [card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11, card12, card13,
+  card14, card15, card16, card17, card18,  card19, card20, card21, card22, card23, card24, card25, card26, 
+   card27, card28, card29, card30, card31, card32, card33, card34, card35, card36, card37, card38, card39,
+    card40, card41, card42, card43, card44, card45, card46, card47, card48, card49, card50, card51, card52]
+
+
+shuffled_deck = Deck.new(big_deck.shuffle)
+
+array1 = []
+array2 = []
+27.times do |i|
+array1 << shuffled_deck.cards[i]
+array2 << shuffled_deck.cards[i+1]
+end
+
+player1_deck = Deck.new(array1)
+player2_deck = Deck.new(array2)
+
+
+
+puts "Welcome to War! \(or Peace\) This game will be played with 52 cards."
+puts "The players today are Megan and Aurora."
+puts "Type 'GO' to start the game!"
+puts "------------------------------------------------------------------"
+
+megan = Player.new("Megan", player1_deck)
+aurora = Player.new("Aurora", player2_deck)
+
