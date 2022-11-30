7dab8bf64185100c54ab28fd082d0b818ab34763
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-11-30 09:37:28 -0800

    Create and pass 'starts with no spoils of war' test

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,8 +1,10 @@
 class Turn 
     attr_reader :player1,
-                :player2
+                :player2,
+                :spoils_of_war
     def initialize (player1, player2)
         @player1 = player1
         @player2 = player2
+        @spoils_of_war = []
     end
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -42,6 +42,12 @@ describe Turn do
             expect(turn.player1).to eq(player1)
             expect(turn.player2).to eq(player2)
         end
+        
+        it 'has spoils of war' do
+            turn = Turn.new(player1, player2)
+
+            expect(turn.spoils_of_war).to eq([])
+        end
     end
     end 
 end 
