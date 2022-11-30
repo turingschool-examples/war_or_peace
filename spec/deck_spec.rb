db9282ad6bcdc06e106981d409ecee2d034d42a2
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-11-29 20:06:26 -0800

    Create and pass test that identifies the rank of the card

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -2,7 +2,7 @@ require './lib/card'
 require './lib/deck'
 
 describe Deck do
-    describe 'initialize' do
+    describe '#initialize' do
         it 'references the Card class' do
             card1 = Card.new(:diamond, 'Queen', 12)
             expect(card1).to be_a(Card)
@@ -11,7 +11,7 @@ describe Deck do
             card3 = Card.new(:heart, 'Ace', 14)
             expect(card3).to be_a(Card)
         end 
-        it 'contains multiple cards' do
+        it 'is an instance of Deck' do
             card1 = Card.new(:diamond, 'Queen', 12)
             card2 = Card.new(:spade, '3', 3)
             card3 = Card.new(:heart, 'Ace', 14)
@@ -30,4 +30,17 @@ describe Deck do
             expect(deck.cards).to eq([card1, card2, card3])
         end
     end
+
+    describe '#rank_of_card_at' do
+        it 'identifies the rank of the card' do
+            card1 = Card.new(:diamond, 'Queen', 12)
+            card2 = Card.new(:spade, '3', 3)
+            card3 = Card.new(:heart, 'Ace', 14)
+
+            cards = [card1, card2, card3]
+            deck = Deck.new(cards)
+            expect(deck.rank_of_card_at(0)).to be (12)
+            expect(deck.rank_of_card_at(2)).to be (14)
+        end 
+    end 
 end 
