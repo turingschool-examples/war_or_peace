ed7aba85395a4228e39b144a02c5a35518f0ea9c
Bobby Luly <bobbyluly@gmail.com>
2022-11-29 22:06:35 -0700

    Correct test 5 expectation

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -24,7 +24,7 @@ describe Deck do
         expect(deck.cards).to eq([card1, card2, card3])
     end
 
-    it 'rank of cards' do
+    it '#rank of cards' do
         card1 = Card.new(:diamond, 'Queen', 12)
         card2 = Card.new(:spade, '3', 3)
         card3 = Card.new(:heart, 'Ace', 14)
@@ -34,6 +34,28 @@ describe Deck do
 
         expect(deck.rank_of_card_at(0)).to eq(12)
         expect(deck.rank_of_card_at(2)).to eq(14)
+    end
+
+    it '#high ranking cards' do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+        cards = [card1, card2, card3]
+
+        deck = Deck.new(cards)
+
+        expect(deck.high_ranking_cards).to eq([card1, card3])
+    end
+
+    it '#percentage high ranking' do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+        cards = [card1, card2, card3]
+
+        deck = Deck.new(cards)
+
+        expect(deck.percent_high_ranking).to eq(66.67)
     end
 
 
