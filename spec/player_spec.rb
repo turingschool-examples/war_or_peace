3582d3d9b94b1da43422866a193e8e54c1559c08
Weston Sandfort <sandfortw@gmail.com>
2022-11-29 18:28:29 -0700

    Add completed player class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,7 +5,7 @@ require './lib/player'
 
 describe Player do
   
-  xit 'is an instance of player' do
+  it 'is an instance of player' do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)   
     card3 = Card.new(:heart, 'Ace', 14)   
@@ -15,7 +15,7 @@ describe Player do
     expect(player).to be_a(Player)
   end
 
-  xit 'has a name and has a deck' do
+  it 'has a name and has a deck' do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)   
     card3 = Card.new(:heart, 'Ace', 14)   
@@ -26,7 +26,7 @@ describe Player do
     expect(player.deck).to be_a(Deck)
   end
 
-  xit 'can lose' do
+  it 'can not lose' do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)   
     card3 = Card.new(:heart, 'Ace', 14)   
@@ -36,7 +36,7 @@ describe Player do
     expect(player.has_lost?).to be(false)
   end
 
-  xit 'can not lose' do
+  it 'can lose' do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)   
     card3 = Card.new(:heart, 'Ace', 14)   
