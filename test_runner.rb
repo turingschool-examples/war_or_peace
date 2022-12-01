a4cc69ae8d70622d702ba77340b88df743b7fd1b
Adam Bailey <ab67319@gmail.com>
2022-11-30 17:48:58 -0700

    Create test runner file and add Card interaction pattern

---
+++ test_runner.rb
@@ -0,0 +1,17 @@
+###################
+# Card Interaction Pattern
+###################
+
+require './lib/card'
+
+p card = Card.new(:heart, 'Jack', 11)
+puts
+
+p card.suit
+puts
+
+p card.value
+puts
+
+p card.rank
+puts
