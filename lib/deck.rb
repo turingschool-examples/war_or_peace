3886c70300582272d4f72f25d03eb9a4469ae15b
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2022-11-30 19:51:04 -0700

    Test set1 complete - deck_spec.rb

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,3 +1,7 @@
 class Deck
-    
+
+    attr_reader :cards_arr
+    def initialize(cards_arr)
+        @cards_arr = cards_arr
+    end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,20 +3,25 @@ require './lib/deck'
 require './lib/card'
 
 RSpec.describe Deck do
-    it "exists" do
-      card1 = Card.new(:spades, "Ace", 14)
-      card1 = Card.new(:diamonds, "Eight", 8)
-      card1 = Card.new(:clubs, "Ace", 14)
+    context 'Test Set 1' do
+        it "exists" do
+            card1 = Card.new(:spades, 'Ace', 14)
+            card2 = Card.new(:diamonds, 'Eight', 8)
+            card3 = Card.new(:clubs, 'Five', 5)
 
-      expect(card_arr).to be_an_instance_of(Deck)
+            cards_arr = [card1, card2, card3]
+            deck = Deck.new(cards_arr)
+
+            expect(deck).to be_an_instance_of(Deck)
+        end
     end
-  
-    it "has readable attributes" do
-      card = Card.new(:diamonds, 'Queen', 12)
-  
-      expect(card.suit).to eq(:diamonds)
-      expect(card.value).to eq('Queen')
-      expect(card.rank).to eq(12)
-    end
-  end
+
+    # xit "has readable attributes" do
+    #     card = Card.new(:diamonds, 'Queen', 12)
+
+    #     expect(card.suit).to eq(:diamonds)
+    #     expect(card.value).to eq('Queen')
+    #     expect(card.rank).to eq(12)
+    # end
+end
   
