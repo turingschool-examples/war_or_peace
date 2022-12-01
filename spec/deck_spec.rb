d164cbaefed2ffe7908d3bf457f996664698eb52
Elle Majors <hellolovely@ellemajors>
2022-11-30 17:48:00 -0700

    test: #high_ranking_cards

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -34,7 +34,22 @@ describe Deck do
       expect(deck.rank_of_card_at(2)).to eq(14)
     end
   end
+
+  describe '#high_ranking_cards' do
+    it 'returns cards in the deck that have a rank of 11 or above' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3) 
+      card3 = Card.new(:heart, 'Ace', 14)
+
+      cards = [card1, card2, card3]
+
+      deck = Deck.new(cards)
+
+      expect(deck.high_ranking_cards).to include(card1, card3)
+    end
+  end
 end
+
 
 
   
