8e7eb8544c0a1025aeafed28641a218460cfd49a
Melony Franchini <meltravelz@gmail.com>
2022-12-01 16:14:55 -0700

    Add test high_ranking_cards

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,10 +4,23 @@ class Deck
     def initialize(cards)
         # cards = [card1, card2, card3]
         @cards = cards
+
     end
     
     def rank_of_card_at(index)
         @cards[index].rank
+    end
+
+    def high_ranking_cards
+       high_ranking_pile = []
+        
+        @cards.each do |card|
+          if card.rank >= 11
+            high_ranking_pile << card
+          end
+        end
+       
+        high_ranking_pile
     end
 
 
