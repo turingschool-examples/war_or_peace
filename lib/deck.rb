615c36a6156db91dd0f2519e26e333d0f855ee58
Amy Spears <amspears007@gmail.com>
2022-12-01 16:28:20 -0700

    Add 2nd passing test

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,8 +1,14 @@
 class Deck
-    
+
     attr_reader :cards_arr
 
     def initialize (cards_arr)
         @cards = cards_arr
+        # require 'pry'; binding.pry
+    end
+
+    def rank_of_card_at(index)
+        # require 'pry'; binding.pry
+        @cards[index].rank
     end
 end
