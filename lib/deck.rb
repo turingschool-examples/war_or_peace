c79922054b9e6beac5c3f95b6b2a09d21beaaa2a
Dawson Timmons <DawsonTimmons@gmail.com>
2022-11-30 19:48:49 -0700

    Add class for high ranking cards

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,9 +1,25 @@
 class Deck
-    attr_reader :cards
+    attr_reader :cards,
+                :high_rank
 
     def initialize(cards)
         @cards = cards
+        @high_rank = []
     end
 
+    def rank_of_card_at(index)
+        @cards[index].rank
+    end
+
+    def high_ranking_cards
+        @cards.each do |card|
+            if card.rank >= 11
+                @high_rank << card
+            end
+        end
+            @high_rank = high_rank
+            return @high_rank
+        end
+    end
 end
 
