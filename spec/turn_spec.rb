48ad218528106467bb9f88fa1afda8c531e8b545
Tori Enyart <torienyart@gmail.com>
2022-12-01 16:13:57 -0700

    Fix :war test names and setup first mutually_assured_destruction test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -153,7 +153,7 @@ describe Turn do
         expect(turn.type).to eq(:war)
     end
 
-    it 'can have turn type :war' do
+    it 'can determine winner of :war' do
         card1 = Card.new(:heart, 'Jack', 11)
         card2 = Card.new(:heart, '10', 10)
         card3 = Card.new(:heart, '9', 9)
@@ -175,7 +175,7 @@ describe Turn do
         expect(winner = turn.winner).to eq(player2)
     end
 
-    it 'can have turn type :war' do
+    it 'can pile cards for :war turn' do
         card1 = Card.new(:heart, 'Jack', 11)
         card2 = Card.new(:heart, '10', 10)
         card3 = Card.new(:heart, '9', 9)
@@ -199,7 +199,7 @@ describe Turn do
         expect(turn.spoils_of_war).to eq([card1, card2, card5, card4, card3, card6])
     end
 
-    it 'can have turn type :war' do
+    it 'can still operate award_spoils method' do
         card1 = Card.new(:heart, 'Jack', 11)
         card2 = Card.new(:heart, '10', 10)
         card3 = Card.new(:heart, '9', 9)
@@ -224,4 +224,28 @@ describe Turn do
         expect(player1.deck.cards).to eq([card8])
         expect(player2.deck.cards).to eq([card7, card1, card2, card5, card4, card3, card6])
     end
+
+    ####:mutually_assured_destruction TURN TYPE TESTS####
+
+    it 'can have turn type :mutually_assured_destruction' do
+        card1 = Card.new(:heart, 'Jack', 11)
+        card2 = Card.new(:heart, '10', 10)
+        card3 = Card.new(:heart, '9', 9)
+        card4 = Card.new(:diamond, 'Jack', 11)
+        card5 = Card.new(:heart, '8', 8)
+        card6 = Card.new(:diamond, 'Queen', 12)
+        card7 = Card.new(:heart, '3', 3)
+        card8 = Card.new(:diamond, '2', 2)
+
+        deck1 = Deck.new([card1, card2, card5, card8])
+        deck2 = Deck.new([card4, card3, card6, card7])
+
+        player1 = Player.new("Megan", deck1)
+        player2 = Player.new("Aurora", deck2)
+
+        turn = Turn.new(player1, player2)
+
+        expect(turn.type).to eq(:mutually_assured_destruction)
+    end
+
 end
