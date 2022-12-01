c12e7e07e1aa5c2bfb77815312b238d3612719da
Kassandra Leyba <kassandra.leyba@gmail.com>
2022-11-30 18:01:10 -0700

    Add percent & remove

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,10 +2,27 @@ class Deck
     attr_reader :cards
     def initialize(cards)
         @cards = cards
+     
     end
 
     def rank_of_card_at(index)
-        # require "pry"; binding.pry
         cards[index].rank
     end
+
+    def high_ranking_cards
+        high_ranking_cards = cards.select do |card|
+        card.rank >= 11
+        end
+        high_ranking_cards
+    end
+
+    def percent_high_ranking
+        (high_ranking_cards.size.to_f / cards.size.to_f).round(4) * 100 
+    end
+
+    def remove_card
+        cards[0]
+    end
+
+    
 end
