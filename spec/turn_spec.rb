af98ccc3aa321f7c52d88f9d4eb7ca921857f657
Matt Enyeart <menyeart1@gmail.com>
2022-11-30 19:58:50 -0700

    Add preliminary code for the pile cards test and method

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -72,7 +72,7 @@ RSpec.describe Turn do
     expect(turn.type).to eq (:basic)
   end
 
-  it "it can determine a winner" do
+  it "can determine a winner" do
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)    
     card3 = Card.new(:heart, '9', 9)
@@ -86,9 +86,27 @@ RSpec.describe Turn do
     player1 = Player.new("Megan", deck1) 
     player2 = Player.new("Aurora", deck2) 
     turn = Turn.new(player1, player2) 
-    require 'pry'; binding.pry
     expect(turn.winner.name).to eq ("Megan")
   end
+
+  # it "can move cards from the deck to the spoils of war pile" do
+  #   card1 = Card.new(:heart, 'Jack', 11) 
+  #   card2 = Card.new(:heart, '10', 10) 
+  #   card3 = Card.new(:heart, '9', 9)
+  #   card4 = Card.new(:diamond, 'Jack', 11)  
+  #   card5 = Card.new(:heart, '8', 8)   
+  #   card6 = Card.new(:diamond, 'Queen', 12)
+  #   card7 = Card.new(:heart, '3', 3) 
+  #   card8 = Card.new(:diamond, '2', 2) 
+  #   deck1 = Deck.new([card1, card2, card5, card8]) 
+  #   deck2 = Deck.new([card4, card3, card6, card7]) 
+  #   player1 = Player.new("Megan", deck1)    
+  #   player2 = Player.new("Aurora", deck2)
+  #   turn = Turn.new(player1, player2) 
+  #   turn.pile_cards
+  #   expect(turn.spoils_of_war.size). to eq(6)
+   
+  # end 
 
 
 end
