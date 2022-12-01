7bccbe7c9da68cae329fa43201eb419841902264
Andra Helton <andrahelton@gmail.com>
2022-11-30 19:56:04 -0700

    Pass high ranking cards test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -34,8 +34,21 @@ RSpec.describe Deck do
 
     deck = Deck.new(cards)
 
-    expect(deck.rank_of_cards_at(0)).to eq(12)
-    expect(deck.rank_of_cards_at(2)).to eq(14)
+    expect(deck.rank_of_card_at(0)).to eq(12)
+    expect(deck.rank_of_card_at(1)).to eq(3)
+    expect(deck.rank_of_card_at(2)).to eq(14)
+  end
+
+  #not yet committed VV
+  it "high ranking cards" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+    expect(deck.high_ranking_cards).to eq([card1, card3])
+
   end
 
 end
