dd3948fa9c45feb325ecf2daf2f9bc99943968f6
Elle Majors <hellolovely@ellemajors>
2022-12-01 15:08:11 -0700

    refactor: dry up test with before each

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,38 +4,30 @@ require './lib/deck'
 require './lib/player'
 
 describe Player do
+  before(:each) do
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3) 
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @deck = Deck.new([@card1, @card2, @card3])
+    @player = Player.new('Clarisa', @deck)
+  end
+
   describe '#initialize' do
     it 'exists and has attributes' do
-      card1 = Card.new(:diamond, 'Queen', 12)
-      card2 = Card.new(:spade, '3', 3) 
-      card3 = Card.new(:heart, 'Ace', 14)
-
-      deck = Deck.new([card1, card2, card3])
-
-      player = Player.new('Clarisa', deck)
-      
-      expect(player).to be_a(Player)
-      expect(player.name).to eq('Clarisa')
-      expect(player.deck).to be_a(Deck)
+      expect(@player).to be_a(Player)
+      expect(@player.name).to eq('Clarisa')
+      expect(@player.deck).to be_a(Deck)
     end
   end
 
   describe '#has_lost?' do
     it 'can determine if player have lost the game' do
-      card1 = Card.new(:diamond, 'Queen', 12)
-      card2 = Card.new(:spade, '3', 3) 
-      card3 = Card.new(:heart, 'Ace', 14)
-
-      deck = Deck.new([card1, card2, card3])
-
-      player = Player.new('Clarisa', deck)
-
-      expect(player.has_lost?).to eq(false)
+      expect(@player.has_lost?).to eq(false)
       
-      player.deck.remove_card
-      player.deck.remove_card
-      player.deck.remove_card
-      expect(player.has_lost?).to be true
+      @player.deck.remove_card
+      @player.deck.remove_card
+      @player.deck.remove_card
+      expect(@player.has_lost?).to be true
     end
   end
 end
