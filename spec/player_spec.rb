d765820ab059f36ef87c00bbe7e3836a83908e60
Elle Majors <hellolovely@ellemajors>
2022-11-30 19:56:56 -0700

    test: #has_lost?

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -19,4 +19,23 @@ describe Player do
       expect(player.deck).to be_a(Deck)
     end
   end
+
+  describe '#has_lost?' do
+    it 'can determine if player have lost the game' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3) 
+      card3 = Card.new(:heart, 'Ace', 14)
+
+      deck = Deck.new([card1, card2, card3])
+
+      player = Player.new('Clarisa', deck)
+
+      expect(player.has_lost?).to eq(false)
+      
+      player.deck.remove_card
+      player.deck.remove_card
+      player.deck.remove_card
+      expect(player.has_lost?).to be true
+    end
+  end
 end
