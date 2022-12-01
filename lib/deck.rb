2c245d358299bf9992d857d5ba4f5fe75983dc24
Elle Majors <hellolovely@ellemajors>
2022-11-30 17:25:43 -0700

    feat: returns #rank_of_card_at index position

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,7 +1,17 @@
-class Deck
+class Deck 
   attr_reader :cards
 
   def initialize(cards)
     @cards = cards
   end
+
+  def rank_of_card_at(index)
+    @cards[index].rank
+  end
+
+
+# high_ranking_cards: this method will return an array of cards in the deck that have a rank of 11 or above (face cards and aces)
+# percent_high_ranking: this method will return the percentage of cards that are high ranking
+# remove_card: this method will remove the top card from the deck
+# add_card: this method will add one card to the bottom (end) of the deck
 end
