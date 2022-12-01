5b4ddd702e37890b5a9fbf95a8dee70aae9476c9
Bobby Luly <bobbyluly@gmail.com>
2022-11-30 22:49:18 -0700

    Add initial test and class

---
+++ spec/player_spec.rb
@@ -0,0 +1,19 @@
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+
+RSpec.describe Player do
+    describe 'Iteration 2' do
+        it 'exists and has attributes'
+          card1 = Card.new(:diamond, 'Queen', 12)
+          card2 = Card.new(:spade, '3', 3)
+          card3 = Card.new(:heart, 'Ace', 14)
+          deck = Deck.new([card1, card2, card3])
+          player = Player.new('Clarisa', deck)
+
+          expect(player).to be_an_instance_of(Player)
+          expect(player.name).to eq('Clarisa')
+          expect(player.deck).to eq([card1, card2, card3])
+        end
+    end
+end
