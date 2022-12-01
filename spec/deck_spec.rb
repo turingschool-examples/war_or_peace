eff17bee3f5c659ca001f5261a917eeaf8033808
Elle Majors <hellolovely@ellemajors>
2022-11-30 18:27:40 -0700

    test: #add_card and exand #remove_card coverage

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -74,9 +74,25 @@ describe Deck do
       deck = Deck.new(cards)
 
       expect(deck.remove_card).to eq(card1)
+      expect(deck.cards).to eq([card2, card3])
     end
   end
 
+  describe '#add_card' do
+    it 'add one card to the bottom of the deck' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3) 
+      card3 = Card.new(:heart, 'Ace', 14)
+      card4 = Card.new(:club, '5', 5)
+
+      cards = [card1, card2, card3]
+
+      deck = Deck.new(cards)
+
+      expect(deck.add_card(card4)).to include(card4)
+      expect(deck.cards).to eq([card1, card2, card3, card4])
+    end
+  end
 end
 
 
