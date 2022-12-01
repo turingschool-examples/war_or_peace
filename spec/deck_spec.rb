2130f2de0207ec0e2daf854b311c40d5b968ddca
Elle Majors <hellolovely@ellemajors>
2022-11-30 18:12:13 -0700

    test: #remove_card

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -63,6 +63,20 @@ describe Deck do
     end
   end
 
+  describe '#remove_card' do
+    it 'remove the top card from the deck' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3) 
+      card3 = Card.new(:heart, 'Ace', 14)
+
+      cards = [card1, card2, card3]
+
+      deck = Deck.new(cards)
+
+      expect(deck.remove_card).to eq(card1)
+    end
+  end
+
 end
 
 
