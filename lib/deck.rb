e128d93a95bb0ea97a3ae39fed11f48502c688cd
Elle Majors <hellolovely@ellemajors>
2022-11-30 17:50:02 -0700

    feat: #high_ranking_cards returns card rank of 11+

--- lib/deck.rb
+++ lib/deck.rb
@@ -9,9 +9,15 @@ class Deck
     @cards[index].rank
   end
 
+  def high_ranking_cards
+    @cards.select do |card|
+      card.rank > 10
+    end
+  end
+end
 
-# high_ranking_cards: this method will return an array of cards in the deck that have a rank of 11 or above (face cards and aces)
+
+
 # percent_high_ranking: this method will return the percentage of cards that are high ranking
 # remove_card: this method will remove the top card from the deck
 # add_card: this method will add one card to the bottom (end) of the deck
-end
