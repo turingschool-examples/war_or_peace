6f2822496d12695e7ba3cba441dcfd5565ab167e
Dawson Timmons <DawsonTimmons@gmail.com>
2022-12-01 16:21:13 -0700

    Add card4 to remove card test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -45,7 +45,8 @@ RSpec.describe Deck do
             card1 = Card.new(:diamond, 'Queen', 12)
             card2 = Card.new(:spade, '3', 3)
             card3 = Card.new(:heart, 'Ace', 14)
-            cards = [card1, card2, card3]
+            card4 = Card.new(:club, '5', 5)
+            cards = [card1, card2, card3, card4]
             deck = Deck.new(cards)
             expect(deck.remove_card).to eq(card1)
         end
