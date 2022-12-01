355529c466e26b43b86c91d7edc6d7ce634038d1
KaraJH <karas.jones@yahoo.com>
2022-11-30 17:03:03 -0700

    Tests for set up method in game class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -22,14 +22,34 @@ RSpec.describe Game do
   describe '#create_cards' do 
     it 'creates card objects' do 
 
-      # temporary test for the create crds method
+      # temporary test for the create cards method
       game = Game.new 
 
       expect(game.deck_of_cards).to eq([])
 
       game.create_cards
-        #  require 'pry'; binding.pry
+    
       expect(game.deck_of_cards.length).to eq(16)
+    end
+  end 
+
+  describe '#set_up' do 
+    it 'creates 2 players and gives them each a deck' do 
+      game = Game.new 
+
+      game.create_cards
+
+      deck = Deck.new(@deck_of_cards) 
+      #require 'pry'; binding.pry
+      deck1 = game.deck_of_cards[0..7]
+      deck2 = game.deck_of_cards[7..15] 
+
+      game.set_up
+
+      player1 = Player.new("Megan", deck1) 
+      player2 = Player.new("Aurora", deck2) 
+
+      expect(player1.deck.length).to eq(8)
     end
   end
 
