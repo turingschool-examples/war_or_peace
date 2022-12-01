882358c940041182f7bf60e290e5edc327e1f75d
Chris Crane <chrisjcrane@gmail.com>
2022-11-30 21:27:37 -0700

    add has_lost? method and passing test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,7 +5,7 @@ require './lib/player'
 
 RSpec.describe Player do
     it "exists" do
-        player = Player.new("Tim")
+        player = Player.new("Tim",[])
         expect(player).to be_an_instance_of(Player)
     end
 
@@ -19,6 +19,24 @@ RSpec.describe Player do
         player = Player.new('Clarisa', deck)
     end
 
-    
+    it "can lose" do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+        cards = [card1, card2, card3]
+
+        deck = Deck.new(cards)
+        player = Player.new('Clarisa', deck)
+
+        player.deck.remove_card
+        expect(player.has_lost?).to eq(false)
+
+        player.deck.remove_card
+        expect(player.has_lost?).to eq(false)
+
+        player.deck.remove_card
+        expect(player.has_lost?).to eq(true)
+
+    end
 
 end
