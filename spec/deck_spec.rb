4a6b31f14b111940d6d9f78b621e81dbf4c78e53
Elle Majors <hellolovely@ellemajors>
2022-11-30 18:05:50 -0700

    test: #percent_high_ranking

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -48,6 +48,21 @@ describe Deck do
       expect(deck.high_ranking_cards).to include(card1, card3)
     end
   end
+
+  describe '#percent_high_ranking' do
+    it 'returns the percentage of cards that are high ranking' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3) 
+      card3 = Card.new(:heart, 'Ace', 14)
+
+      cards = [card1, card2, card3]
+
+      deck = Deck.new(cards)
+
+      expect(deck.percent_high_ranking).to eq(66.67)
+    end
+  end
+
 end
 
 
