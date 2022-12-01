6b9b4b9de9639771dba04271b097607e2e54537f
Kassandra Leyba <kassandra.leyba@gmail.com>
2022-11-30 18:01:46 -0700

    Add percent & remove

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -33,4 +33,34 @@ RSpec.describe Deck do
         expect(deck.rank_of_card_at(0)).to eq(12)
         expect(deck.rank_of_card_at(2)).to eq(14)
     end
+
+    it "#high_ranking_cards" do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)    
+        card3 = Card.new(:heart, 'Ace', 14)    
+        cards = [card1, card2, card3]
+        deck = Deck.new(cards)
+
+        expect(deck.high_ranking_cards).to eq([card1, card3])
+    end
+    
+    it "#percent_high_rankings" do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)    
+        card3 = Card.new(:heart, 'Ace', 14)    
+        cards = [card1, card2, card3]
+        deck = Deck.new(cards)
+
+        expect(deck.percent_high_ranking). to eq(66.67)
+    end
+
+    it "removes card" do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)    
+        card3 = Card.new(:heart, 'Ace', 14)    
+        cards = [card1, card2, card3]
+        deck = Deck.new(cards)
+
+        expect(deck.remove_card).to eq(card1)
+    end
 end
