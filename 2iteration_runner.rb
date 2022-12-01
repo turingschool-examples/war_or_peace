c0bbde65e93f35c0da56c4c9f118e6d2a2d559eb
Adam Bailey <ab67319@gmail.com>
2022-11-30 19:38:36 -0700

    Add player iteration pattern to 2iteration_runner.rb

--- 2iteration_runner.rb
+++ 2iteration_runner.rb
@@ -1,4 +1,53 @@
-require './lib/card'
-require './lib/deck'
-require './lib/player'
+# ###################
+# # Player Interaction Pattern
+# ###################
 
+# require './lib/card'
+# require './lib/deck'
+# require './lib/player'
+
+# card1 = Card.new(:diamond, 'Queen', 12)
+# p card1
+# puts
+
+# card2 = Card.new(:spade, '3', 3)
+# p card2
+# puts
+
+# card3 = Card.new(:heart, 'Ace', 14) 
+# p card3
+# puts
+
+# p deck = Deck.new([card1, card2, card3])
+# puts
+
+# player = Player.new('Clarisa', deck)
+# p player
+# puts
+
+# p player.name
+# puts
+
+# p player.has_lost?
+# puts
+
+# p player.deck.remove_card
+# puts
+
+# p player.has_lost?
+# puts
+
+# p player.deck.remove_card
+# puts
+
+# p player.has_lost?
+# puts
+
+# p player.deck.remove_card
+# puts
+
+# p player.has_lost?
+# puts
+
+# p player.deck
+# puts
