29a2be5509befb2aaaf7e8d3eeaa287d7b5e736d
Meredith Trevino <meredithtrevino@gmail.com>
2022-12-01 13:29:44 -0700

    adds type method test

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,9 +5,46 @@ class Turn
 
     def initialize(player1, player2)
         @player1 = player1
-        @player2 = player2
-
+        @player2 = player2 
+        @spoils_of_war = []
     end
 
+    def spoils_of_war
+        @spoils_of_war
+    end
+    
+    def type
+        if 
+            @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
+            :basic
+        elsif
+            @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
+            :war
+        else 
+            @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
+            :mutually_assured_destruction
+        end
+    end
+
+    def winner
+        if @turn == :basic && @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
+            player1
+            require 'pry';binding.pry
+
+        elsif @turn == :basic && @player1.deck.rank_of_card_at(0) < @player2.deck.rank_of_card_at(0)
+            player2
+        elsif
+            @turn == :war &&
+            @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
+            player1
+        elsif
+            @turn == :war &&
+            @player1.deck.rank_of_card_at(2) < @player2.deck.rank_of_card_at(2)
+            player2
+        else
+            @turn == :mutually_assured_destruction
+            "No winner"
+        end
+    end
 
 end
