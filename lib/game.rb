72c1bf3cd60f2fb0867e9ddca450c90a22e566fa
Axel De La Guardia <axeldelaguardia@gmail.com>
2022-11-30 19:12:51 -0700

    Create a Game class and add 3 intialize tests

---
+++ lib/game.rb
@@ -0,0 +1,45 @@
+class Game
+    attr_reader :turn, :deck_of_cards
+
+    def initialize (turn)
+        @turn = turn
+        @deck_of_cards = []
+    end
+
+    def create_deck
+        heart_cards = []
+        diamond_cards = []
+        spade_cards = []
+        club_cards = []
+
+        13.times do |i|
+            heart_cards << Card.new(:heart, (i+1).to_s, i+1)
+        end
+
+        13.times do |i|
+            diamond_cards << Card.new(:diamond, (i+1).to_s, i+1)
+        end
+
+        13.times do |i|
+            spade_cards << Card.new(:spade, (i+1).to_s, i+1)
+        end
+
+        13.times do |i|
+            club_cards << Card.new(:club, (i+1).to_s, i+1)
+        end
+
+        @deck_of_cards = [heart_cards, spade_cards, club_cards, diamond_cards].flatten
+    end
+    # def start
+    #     ans = gets.chomp('')
+    #     if ans = 'GO'
+    # end
+
+    # def next_turn
+
+    # end
+
+    # def shuffle_cards
+
+    # end
+end
---
+++ spec/game_spec.rb
@@ -0,0 +1,40 @@
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+require './lib/game'
+require 'rspec'
+
+describe Game do
+    describe 'initialize' do
+        before(:each) do
+            @card1 = Card.new(:heart, 'Jack', 11)
+            @card2 = Card.new(:heart, '10', 10)
+            @card3 = Card.new(:heart, '9', 9)
+            @card4 = Card.new(:diamond, 'Jack', 11)
+            @card5 = Card.new(:heart, '8', 8)
+            @card6 = Card.new(:diamond, 'Queen', 12)
+            @card7 = Card.new(:heart, '3', 3)
+            @card8 = Card.new(:diamond, '2', 2)
+            @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+            @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+            @player1 = Player.new("Megan", @deck1)
+            @player2 = Player.new("Aurora", @deck2)
+            @turn = Turn.new(@player1, @player2)
+            @game = Game.new(@turn)
+        end
+
+        it 'is an instance of the Game class' do
+            expect(@game).to be_a Game
+        end
+
+        it 'contains a turn' do
+            expect(@game.turn).to eq(@turn)
+        end
+
+        it 'creates a deck of 52 cards' do
+            @game.create_deck
+            expect(@game.deck_of_cards.count).to eq(52)
+        end
+    end
+end
