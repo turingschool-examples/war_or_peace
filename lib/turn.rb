a6152d3b9b78500dc1d8fbd7d740f28650f63722
Harrison Ryan <hwryan12@gmail.com>
2022-11-30 19:53:32 -0600

    Initialize class Turn

---
+++ lib/turn.rb
@@ -0,0 +1,11 @@
+class Turn
+    attr_reader :player1, 
+                :player2, 
+                :spoils_of_war
+
+    def initialize(player1, player2)
+        @player1 = player1
+        @player2 = player2
+    end
+    
+end
