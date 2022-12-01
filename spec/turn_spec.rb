152bb740f474c16de39d47c310f150b55fc4feb9
Elle Majors <hellolovely@ellemajors>
2022-12-01 13:42:17 -0700

    test: #type has a :basic turn

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -31,4 +31,31 @@ describe Turn do
       expect(turn.spoils_of_war).to eq([])
     end
   end
+
+  describe '#type' do
+    it 'has a :basic turn types' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3) 
+      card3 = Card.new(:heart, 'Ace', 14)
+      card4 = Card.new(:diamond, 'Jack', 11)   
+      card5 = Card.new(:heart, '8', 8) 
+      card6 = Card.new(:diamond, 'Queen', 12)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+  
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card3, card4, card6, card7]) 
+  
+      player1 = Player.new('Megan', deck1)
+      player2 = Player.new('Aurora', deck2)
+        
+      turn = Turn.new(player1, player2) 
+  
+      expect(turn.type).to eq(:basic)
+      # create test for :winner and :spoils_of_war
+      # expect(turn.type).to eq(:war)
+      # expect(turn.type).to eq(:mutually_assured_destruction)
+      # expect(turn.).to eq()
+    end
+  end
 end
