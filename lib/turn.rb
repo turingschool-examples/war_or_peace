6474d40e474f624a3952dbd0ea0060b29023482b
Harrison Ryan <hwryan12@gmail.com>
2022-11-30 19:57:43 -0600

    define type method

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,5 +7,14 @@ class Turn
         @player1 = player1
         @player2 = player2
     end
-    
+
+    def type
+        if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+            p :war
+        elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+            p :mutually_assured_destruction
+        else
+            p :basic
+        end
+    end
 end
