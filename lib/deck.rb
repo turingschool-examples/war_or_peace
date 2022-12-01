6dd838609edbba83fcaf9f26af7443f5f51ab1fe
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2022-11-30 21:52:14 -0700

    Test set2 complete - deck_spec.rb/deck.rb

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,7 +1,13 @@
+# 'Test Set 1'
 class Deck
 
     attr_reader :cards_arr
     def initialize(cards_arr)
         @cards_arr = cards_arr
     end
+# 'Test Set 2'
+    def rank_of_card_at(index)
+        @cards_arr[index].rank
+    end
+
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -11,17 +11,21 @@ RSpec.describe Deck do
 
             cards_arr = [card1, card2, card3]
             deck = Deck.new(cards_arr)
-
-            expect(deck).to be_an_instance_of(Deck)
         end
     end
 
-    # xit "has readable attributes" do
-    #     card = Card.new(:diamonds, 'Queen', 12)
+    context 'Test Set 2' do
+        it "can distribute ranking" do
+            card1 = Card.new(:spades, 'Ace', 14)
+            card2 = Card.new(:diamonds, 'Eight', 8)
+            card3 = Card.new(:clubs, 'Five', 5)
 
-    #     expect(card.suit).to eq(:diamonds)
-    #     expect(card.value).to eq('Queen')
-    #     expect(card.rank).to eq(12)
-    # end
+            cards_arr = [card1, card2, card3]
+            deck = Deck.new(cards_arr)
+
+            expect(deck.rank_of_card_at(0)).to be(14)
+            expect(deck.rank_of_card_at(2)).to be(5)
+        end
+    end
 end
   
