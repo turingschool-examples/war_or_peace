2934c8528dda391d76e23ad0e5a08d7f00f892f0
Bobby Luly <bobbyluly@gmail.com>
2022-11-30 23:09:48 -0700

    Pass iteration2 first test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,7 +4,7 @@ require './lib/player'
 
 RSpec.describe Player do
     describe 'Iteration 2' do
-        it 'exists and has attributes'
+        it 'exists and has attributes' do
           card1 = Card.new(:diamond, 'Queen', 12)
           card2 = Card.new(:spade, '3', 3)
           card3 = Card.new(:heart, 'Ace', 14)
@@ -13,7 +13,7 @@ RSpec.describe Player do
 
           expect(player).to be_an_instance_of(Player)
           expect(player.name).to eq('Clarisa')
-          expect(player.deck).to eq([card1, card2, card3])
+          expect(player.deck).to eq(deck)
         end
     end
 end
