b1114c7f2c1793853d13fdf04454d61fd0b01971
Elle Majors <hellolovely@ellemajors>
2022-11-30 17:24:40 -0700

    test: #rank_of_card_at index position

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -17,4 +17,24 @@ describe Deck do
       expect(deck.cards).to eq([card1, card2, card3])
     end
   end
+
+  describe '#rank_of_card_at()' do
+    it 'returns the rank of that card' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3) 
+      card3 = Card.new(:heart, 'Ace', 14)
+
+      cards = [card1, card2, card3]
+
+      deck = Deck.new(cards)
+      deck.rank_of_card_at(0)
+      deck.rank_of_card_at(2)
+
+      expect(deck.rank_of_card_at(0)).to eq(12)
+      expect(deck.rank_of_card_at(2)).to eq(14)
+    end
+  end
 end
+
+
+  
