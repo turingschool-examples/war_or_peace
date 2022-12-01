3d94fd3c65df8ef3aa60a1da873abbdcdf0f6b91
Caleb Thomas <cjthomas00@comcast.net>
2022-11-30 19:07:57 -0700

    add rank and high ranking card tests.

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -10,15 +10,34 @@ RSpec.describe Deck do
     end
     
     it "can hold cards" do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+        cards = [card1, card2, card3,]
+        deck = Deck.new(cards)
+        #
+        expect(deck.cards).to eq(cards)
+    end
+    
+    it "can tell rank of cards" do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+        cards = [card1, card2, card3,]
+        deck = Deck.new(cards)
 
+        expect(deck.rank_of_card_at(0)).to eq(12)
+        expect(deck.rank_of_card_at(2)).to eq(14)
+    end
+
+    it "can determine high cards" do
         card1 = Card.new(:diamond, 'Queen', 12)
         card2 = Card.new(:spade, '3', 3)
         card3 = Card.new(:heart, 'Ace', 14)
         cards = [card1, card2, card3,]
         deck = Deck.new(cards)
         #require 'pry'; binding.pry
-        expect(deck.cards).to eq(cards)
+        expect(deck.high_ranking_cards).to eq([card1, card3])
     end
-    
 
 end
