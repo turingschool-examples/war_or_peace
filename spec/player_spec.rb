22e31da3a7952f75b727a9ac4c84652c6c422f6d
Chris Crane <chrisjcrane@gmail.com>
2022-11-30 21:00:42 -0700

    add player and player_rspec with 2 passing tests

---
+++ spec/player_spec.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+
+RSpec.describe Player do
+    it "exists" do
+        player = Player.new("Tim")
+        expect(player).to be_an_instance_of(Player)
+    end
+
+    it "has a deck" do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+        cards = [card1, card2, card3]
+
+        deck = Deck.new(cards)
+        player = Player.new('Clarisa', deck)
+    end
+
+    
+
+end
