8bd3d4a87b4105e77acc471612c8772c39f953e0
Adam Bailey <ab67319@gmail.com>
2022-12-01 12:02:16 -0700

    Create before block to establish 'player' instance variable and 'exists' example test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,7 +3,20 @@ require './lib/deck'
 require './lib/player'
 
 describe Player do
+    before(:each) do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14) 
 
+        cards = [card1, card2, card3]
 
-    
+        deck = Deck.new(cards)
+
+        @player = Player.new("Clarisa", deck)
+    end
+
+    it 'exists' do
+        expect(@player).to be_a(Player)
+    end
+
 end
