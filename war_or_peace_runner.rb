5668681f624a94506bb33e8a1758d2a96745d7ca
Harrison Ryan <hwryan12@gmail.com>
2022-12-01 14:29:37 -0600

    Create runner file with deck of 52

---
+++ war_or_peace_runner.rb
@@ -0,0 +1,63 @@
+require 'rspec'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+
+bicycle_deck = [
+    card1 = (:diamond, "2", 2)
+    card2 = (:diamond, "3", 3)
+    card3 = (:diamond, "4", 4)
+    card4 = (:diamond, "5", 5)
+    card5 = (:diamond, "6", 6)
+    card6 = (:diamond, "7", 7)
+    card7 = (:diamond, "8", 8)
+    card8 = (:diamond, "9", 9)
+    card9 = (:diamond, "10", 10)
+    card10 = (:diamond, "Jack", 11)
+    card11 = (:diamond, "Queen", 12)
+    card12 = (:diamond, "King", 13)
+    card13 = (:diamond, "Ace", 14)
+    card14 = (:club, "2", 2)
+    card15 = (:club, "3", 3)
+    card16 = (:club, "4", 4)
+    card17 = (:club, "5", 5)
+    card18 = (:club, "6", 6)
+    card19 = (:club, "7", 7)
+    card20 = (:club, "8", 8)
+    card21 = (:club, "9", 9)
+    card22 = (:club, "10", 10)
+    card23 = (:club, "Jack", 11)
+    card24 = (:club, "Queen", 12)
+    card25 = (:club, "King", 13)
+    card26 = (:club, "Ace", 14)
+    card27 = (:heart, "2", 2)
+    card28 = (:heart, "3", 3)
+    card29 = (:heart, "4", 4)
+    card30 = (:heart, "5", 5)
+    card31 = (:heart, "6", 6)
+    card32 = (:heart, "7", 7)
+    card33 = (:heart, "8", 8)
+    card34 = (:heart, "9", 9)
+    card35 = (:heart, "10", 10)
+    card36 = (:heart, "Jack", 11)
+    card37 = (:heart, "Queen", 12)
+    card38 = (:heart, "King", 13)
+    card39 = (:heart, "Ace", 14)
+    card40 = (:spade, "2", 2)
+    card41 = (:spade, "3", 3)
+    card42 = (:spade, "4", 4)
+    card43 = (:spade, "5", 5)
+    card44 = (:spade, "6", 6)
+    card45 = (:spade, "7", 7)
+    card46 = (:spade, "8", 8)
+    card47 = (:spade, "9", 9)
+    card48 = (:spade, "10", 10)
+    card49 = (:spade, "Jack", 11)
+    card50 = (:spade, "Queen", 12)
+    card51 = (:spade, "King", 13)
+    card52 = (:spade, "Ace", 14)
+]
+
+
+    
