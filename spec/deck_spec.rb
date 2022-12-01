fcfd9d704008f1061d5dc41e5165b4f2dc76a618
Caleb Thomas <cjthomas00@comcast.net>
2022-11-30 17:00:33 -0700

    pass deck can hold cards.

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -9,15 +9,16 @@ RSpec.describe Deck do
         expect(deck).to be_an_instance_of(Deck)
     end
     
-   # it "can hold cards" do
+    it "can hold cards" do
 
-        #card1 = Card.new(:diamond, 'Queen', 12)
-        #card2 = Card.new(:spade, '3', 3)
-        #card3 = Card.new(:heart, 'Ace', 14)
-        #cards = [card1, card2, card3,]
-        #deck = Deck.new(cards)
-        #expect(deck).to eq([:diamond, 'Queen', 12], [:spade, '3', #3], [:heart, 'Ace', 14])
-    #end
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+        cards = [card1, card2, card3,]
+        deck = Deck.new(cards)
+        #require 'pry'; binding.pry
+        expect(deck.cards).to eq(cards)
+    end
     
 
 end
