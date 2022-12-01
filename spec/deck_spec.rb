7ff437f40efee7b920d97e9c322acc37f1693e40
Jade Stewart <jadekstewart3@gmail.com>
2022-11-30 17:48:09 -0700

    Skip second deck test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -20,28 +20,28 @@ RSpec.describe Deck do
        expect(@deck.cards).to eq([@card1, @card2, @card3])
    end
  
-   it "accesses card rank" do
+   xit "accesses card rank" do
        expect(@deck.rank_of_cards_at(0)).to eq(12)
        expect(@deck.rank_of_cards_at(1)).to eq(3)
        expect(@deck.rank_of_cards_at(2)).to eq(14)
    end
  
-   it "has high ranking cards" do
+   xit "has high ranking cards" do
        expect(@deck.high_ranking_cards).to eq([@card1, @card3])
    end
  
-   it "can get percent of high ranking cards" do
+   xit "can get percent of high ranking cards" do
        expect(@deck.percent_high_ranking).to eq(66.67)
    end
 
-   it "can remove a card" do 
+   xit "can remove a card" do 
         expect(@deck.remove_card).to eq(@card1)
         expect(@deck.high_ranking_cards).to eq(@card3)
         expect(@deck.cards).to eq([@card2, @card3])
         expect(deck.percent_high_ranking).to eq(50.0)
    end
 
-   it "can add a card" do 
+   xit "can add a card" do 
         card4 = Card.new(:club, '5', 5)
 
         @deck.add_card(card4)
