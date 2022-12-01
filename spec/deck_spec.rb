38eb4e8cb33316bf03197ae40cd01c76ed2c4452
Adam Bailey <ab67319@gmail.com>
2022-12-01 11:54:28 -0700

    Adjust spacing

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -12,33 +12,35 @@ describe Deck do
     @deck = Deck.new(@cards)
   end
 
-  it "exists" do
-    expect(@deck).to be_a(Deck)
-  end
+  describe 'Iteration 1' do
+    it "exists" do
+        expect(@deck).to be_a(Deck)
+    end
 
-  it 'has readable attributes' do
-    expect(@deck.cards).to eq(@cards)
-  end
+    it 'has readable attributes' do
+        expect(@deck.cards).to eq(@cards)
+    end
 
-  it 'can get rank of card at index' do
-    expect(@deck.rank_of_card_at(0)).to eq(12)
-  end
+    it 'can get rank of card at index' do
+        expect(@deck.rank_of_card_at(0)).to eq(12)
+    end
 
-  it 'can get high ranking cards' do
-    expect(@deck.high_ranking_cards).to eq([@card1, @card3])
-  end
+    it 'can get high ranking cards' do
+        expect(@deck.high_ranking_cards).to eq([@card1, @card3])
+    end
 
-  it 'can get percent high ranking cards' do
-    expect(@deck.percent_high_ranking).to eq(66.67)
-  end
+    it 'can get percent high ranking cards' do
+        expect(@deck.percent_high_ranking).to eq(66.67)
+    end
 
-  it 'can remove card' do
-    expect(@deck.remove_card).to eq(@card1)
-  end
+    it 'can remove card' do
+        expect(@deck.remove_card).to eq(@card1)
+    end
 
-  it 'can add card' do
-    card4 = Card.new(:club, '5', 5)
+    it 'can add card' do
+        card4 = Card.new(:club, '5', 5)
 
-    expect(@deck.add_card(card4)).to eq(@deck.cards << card4)
+        expect(@deck.add_card(card4)).to eq(@deck.cards << card4)
+    end
   end
 end
