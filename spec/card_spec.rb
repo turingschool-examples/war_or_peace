dbe6a19fcd4397c82db5a37df021eb16f769ef2a
Melony Franchini <meltravelz@gmail.com>
2022-12-01 13:15:07 -0700

    Fix errors after asking for help

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -2,27 +2,18 @@ require 'rspec'
 require './lib/card'
 
 RSpec.describe Card do
-  describe "Make Cards" do
 
   it "exists" do
-    card = Card.new(:diamond, 'Queen', 12)
+    card = Card.new(:heart, 'Jack', 11)
     expect(card).to be_an_instance_of(Card)
   end
 
-  # Now I see this was an ind test; the test below tests for all attributes
-  # Therefore this does the same thing as below and is not needed.
-  # it 'has a suit' do
-  #   card = Card.new(:diamond, 'Queen', 12)
-  #   expect(card.suit).to eq(:diamond)
-  # end
-
   it "has readable attributes" do
-    card = Card.new(:diamond, 'Queen', 12)
+    card = Card.new(:heart, 'Jack', 11)
 
-    expect(card.suit).to eq(:diamond)
-    expect(card.value).to eq('Queen')
-    expect(card.rank).to eq(12)
+    expect(card.suit).to eq(:heart)
+    expect(card.value).to eq('Jack')
+    expect(card.rank).to eq(11)
   end
 
-  end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,17 +3,22 @@ require './lib/card'
 require './lib/deck'
 
 RSpec.describe Deck do
-    it "exsists" do
-        deck = Deck.new("cards")
+    before do
+        @card1 = Card.new(:diamond, 'Queen', 12)
+        @card2 = Card.new(:spade, '3', 3)
+        @card3 = Card.new(:heart, 'Ace', 14)
+        @cards = [@card1, @card2, @card3]    
 
-        expect(deck).to be_a(Deck)
-        expect(deck.card).to be_a([])
+        @deck = Deck.new(@cards)
     end
 
-    # it "holds an array" do
-    #     deck = Deck.new
-    #     expect(deck).to be_an_instance_of(Deck)
-    # end
+    it "exists" do
+        expect(@deck).to be_a(Deck)
+    end
+
+    it "has cards" do
+        expect(@deck.card).to be_a([])
+    end
 
 
 end
