10e456d8f8683d0e4c20644f3d08ad9460a60943
Bobby Luly <bobbyluly@gmail.com>
2022-12-01 12:45:12 -0700

    Add has_lost? method and pass it's test

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,16 @@
 class Player
-    attr_reader :name, :deck
+    attr_reader :name, :deck, :has_lost
 
     def initialize(name, deck)
         @name = name
         @deck = deck
+        @has_lost = false
+    end
+
+    def has_lost?
+        if deck.cards == []
+          @has_lost = true
+        end
+        @has_lost
     end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -15,5 +15,26 @@ RSpec.describe Player do
           expect(player.name).to eq('Clarisa')
           expect(player.deck).to eq(deck)
         end
+        
+        it 'can say when player lost' do
+          card1 = Card.new(:diamond, 'Queen', 12)
+          card2 = Card.new(:spade, '3', 3)
+          card3 = Card.new(:heart, 'Ace', 14)
+          deck = Deck.new([card1, card2, card3])
+          player = Player.new('Clarisa', deck)
+
+          # player.deck.remove_card
+          expect(player.deck.remove_card).to eq(card1)
+          expect(player.has_lost?).to be false
+
+          # player.deck.remove_card
+          expect(player.deck.remove_card).to eq(card2)
+          expect(player.has_lost?).to be false
+
+          # player.deck.remove_card
+          expect(player.deck.remove_card).to eq(card3)
+          expect(player.has_lost?).to be true
+          expect(player.deck).to eq(deck)
+        end
     end
 end
