d286f88ee1d8c3b00a3c9b46af324ba9ad36c741
Tori Enyart <torienyart@gmail.com>
2022-11-30 18:05:07 -0700

    Initialize parameter for instance variable cards

---
+++ spec/deck_spec.rb
@@ -0,0 +1,16 @@
+require './lib/card'
+require './lib/deck'
+
+describe Deck do
+    it 'can add cards to deck' do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3) 
+        card3 = Card.new(:heart, 'Ace', 14)
+        cards = [card1, card2, card3]
+        deck = Deck.new(cards)
+
+        expect(cards).to eq([card1, card2, card3])
+
+       # require 'pry'; binding.pry
+    end
+end
