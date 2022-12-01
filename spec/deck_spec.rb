b49547f5becf46b5b7409ffb440aa671263c3f4b
Nickolas Marquez <nickomarquez7@gmail.com>
2022-11-30 22:07:25 -0700

    Change cards to string

---
+++ spec/deck_spec.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+require './lib/card'
+require './lib/deck'
+
+RSpec.describe Deck do
+    it "exists" do
+      deck = Deck.new("cards")
+  
+      expect(deck).to be_an_instance_of(Deck)
+    end
+end    
