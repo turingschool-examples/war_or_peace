2d202df33fe4aba8872f6263dd93b0a23de6b329
Conner Van Loan <cdvanloan@gmail.com>
2022-11-30 18:44:15 -0700

    Adds tests to deck_spec

--- lib/deck.rb
+++ lib/deck.rb
@@ -14,4 +14,12 @@ class Deck
     def rank_of_card_at(card_position)
         @cards[card_position].rank
     end
+
+    def high_ranking_cards
+        @cards.each do |card|
+            if card.rank > 10
+                high_ranking_cards << card
+            end
+        end
+    end
 end 
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -41,5 +41,47 @@ RSpec.describe Deck do
         expect(deck1.rank_of_card_at(0)).to eq(12)
         expect(deck1.rank_of_card_at(2)).to eq(14)
     end
+
+    it 'can find only high ranking cards' do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+        deck1 = Deck.new('Deck 1')
+        deck1.add_card(card1)
+        deck1.add_card(card2)
+        deck1.add_card(card3)
+        expect(deck1.high_ranking_cards).to eq(card1, card3)
+    end
+
+    xit 'can tell what percent of deck is high ranking' do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+        deck1 = Deck.new('Deck 1')
+        deck1.add_card(card1)
+        deck1.add_card(card2)
+        deck1.add_card(card3)
+        expect(deck1.percent_high_ranking).to eq(66.67)
+    end
+
+    xit 'can remove a card from a deck' do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+        deck1 = Deck.new('Deck 1')
+        deck1.add_card(card1)
+        deck1.add_card(card2)
+        deck1.add_card(card3)
+        expect(deck1.remove_card).to eq(card1)         # is this correct test??
+        expect(deck1.high_ranking_cards).to include(card3)
+        expect(deck1.percent_high_ranking).to eq(50)
+        card4 = Card.new(:club, '5', 5)
+        deck1.add_card(card4)
+        expect(deck1.cards).to include(card2, card3, card4)
+        expect(deck1.high_ranking_cards).to include(card3)
+        expect(deck1.percent_high_ranking).to eq(33.33)
+    end
+    
+
 end
 
