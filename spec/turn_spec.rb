c894aef84337c118ed69ea96a7340a306e5dd298
Harrison Ryan <hwryan12@gmail.com>
2022-11-30 19:49:40 -0600

    Add award_spols.test to turn_spec.rb

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -112,6 +112,32 @@ Rspec.describe Turn do
 
         expect(turn.spoils_of_war).to eq(card1, card3)
     end
+    
+    it "can give the spoils of war to the winner" do
+        card1 = Card.new(:heart, 'Jack', 11)
+        card2 = Card.new(:heart, '10', 10)
+        card3 = Card.new(:heart, '9', 9)
+        card4 = Card.new(:diamond, 'Jack', 11)
+        card5 = Card.new(:heart, '8', 8) 
+        card6 = Card.new(:diamond, 'Queen', 12)
+        card7 = Card.new(:heart, '3', 3)
+        card8 = Card.new(:diamond, '2', 2)
+        deck1 = Deck.new([card1, card2, card5, card8])
+        deck2 = Deck.new([card3, card4, card6, card7])
+        player1 = Player.new("Megan", deck1)
+        player2 = Player.new("Aurora", deck2) 
+        turn = Turn.new(player1, player2)
+        
+        turn.player1
+        turn.player2
+        turn.pile_cards
+        turn.spoils_of_war
+        turn.award_spoils(winner)
+
+        expect(player1.deck).to eq(card1, card2, card3, card4, card5)
+        expect(player2.deck).to eq(card6, card7, card8)
+    end
+    
 end
 
        
