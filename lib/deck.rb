6b9660d5f20480cc61df4f7c3c27fa8a5fa63597
Andra Helton <andrahelton@gmail.com>
2022-12-01 14:56:43 -0700

    Add code for last tests in deck_spec.rb and deck.rb

--- lib/deck.rb
+++ lib/deck.rb
@@ -21,4 +21,17 @@ class Deck
         highs
     end
 
+
+    def percent_high_ranking
+       ((high_ranking_cards.count.to_f / @cards.count.to_f) * 100).round(2)
+    end
+
+    def remove_card
+        @cards.shift
+    end
+
+    def add_card(card)
+        @cards << card
+    end
+
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -39,7 +39,6 @@ RSpec.describe Deck do
     expect(deck.rank_of_card_at(2)).to eq(14)
   end
 
-  #not yet committed VV
   it "high ranking cards" do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
@@ -47,7 +46,47 @@ RSpec.describe Deck do
     cards = [card1, card2, card3]
 
     deck = Deck.new(cards)
+
     expect(deck.high_ranking_cards).to eq([card1, card3])
+
+  end
+
+  it "percent high ranking" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    expect(deck.percent_high_ranking).to eq(66.67)
+
+    
+  end
+
+  it "remove top card from deck" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    expect(deck.remove_card).to eq(card1)
+
+  end
+
+  it "add cards" do
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    card4 = Card.new(:club, '5', 5)
+    cards = [card2, card3]
+
+    deck = Deck.new(cards)
+
+    deck.add_card(card4)
+
+    expect(deck.cards).to eq([card2, card3, card4])
 
   end
 
