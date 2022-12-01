a2f49622c5997a3cf94fb79914b1469e8c632abe
Dawson Timmons <DawsonTimmons@gmail.com>
2022-12-01 15:47:16 -0700

    Adds method for high ranking cards and test

--- lib/deck.rb
+++ lib/deck.rb
@@ -11,5 +11,13 @@ class Deck
         @cards[index].rank
     end
 
+    def high_ranking_cards
+        @cards.each do |card|
+            if card.rank >= 11
+                @high_rank << card
+            end
+        end
+        @high_rank
+    end
 end
 
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -22,6 +22,15 @@ RSpec.describe Deck do
             expect(deck.rank_of_card_at(0)).to eq(12)
             expect(deck.rank_of_card_at(2)).to eq(14)
         end
+    
+        it 'shows us the high ranking cards' do
+            card1 = Card.new(:diamond, 'Queen', 12)
+            card2 = Card.new(:spade, '3', 3)
+            card3 = Card.new(:heart, 'Ace', 14)
+            cards = [card1, card2, card3]
+            deck = Deck.new(cards)
+            expect(deck.high_ranking_cards).to eq([card1, card3])  
+        end
     end
 end
 
