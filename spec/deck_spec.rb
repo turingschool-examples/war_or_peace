6fd02fc016692620d9c9ce675fe263df1ffe31d0
Adam Bailey <ab67319@gmail.com>
2022-11-30 18:23:47 -0700

    Refactor examples to include :each setup

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -2,27 +2,23 @@ require './lib/card'
 require './lib/deck'
 
 describe Deck do
+
+  before(:each) do
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14) 
+
+    @cards = [@card1, @card2, @card3]
+
+    @deck = Deck.new(@cards)
+  end
+
   it "exists" do
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14) 
-
-    cards = [card1, card2, card3]
-
-    deck = Deck.new(cards)
-
-    expect(deck).to be_a(Deck)
+    expect(@deck).to be_a(Deck)
   end
 
   it 'has readable attributes' do
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14) 
-
-    cards = [card1, card2, card3]
-
-    deck = Deck.new(cards)
-
-    expect(deck.cards).to eq(cards)
+    expect(@deck.cards).to eq(@cards)
   end
+
 end
