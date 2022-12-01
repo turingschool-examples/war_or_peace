40ce8398e2c30d720b4aba707bd5d48ccc715b7d
Adam Bailey <ab67319@gmail.com>
2022-11-30 18:12:13 -0700

    Add 'has readable attributes' test example

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -13,4 +13,16 @@ describe Deck do
 
     expect(deck).to be_a(Deck)
   end
+
+  it 'has readable attributes' do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14) 
+
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    expect(deck.cards).to eq(cards)
+  end
 end
