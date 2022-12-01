8ad83dc697fe94f4625d6ff71a2934a4e6155672
Matt Enyeart <menyeart1@gmail.com>
2022-11-30 17:16:49 -0700

    Modify code for organization

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -42,14 +42,14 @@ RSpec.describe Player do
   end
 
   it "will lose the game when the deck is empty " do 
-  card1 = Card.new(:diamond, 'Queen', 12)
-  card2 = Card.new(:spade, '3', 3)
-  card3 = Card.new(:heart, 'Ace', 14)
-  deck = Deck.new([card1, card2, card3])
-  player = Player.new('Clarisa', deck)
-  player.deck.remove_card
-  player.deck.remove_card
-  player.deck.remove_card
-  expect(player.has_lost?). to eq(true)
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+    player.deck.remove_card
+    player.deck.remove_card
+    player.deck.remove_card
+    expect(player.has_lost?). to eq(true)
   end
 end
