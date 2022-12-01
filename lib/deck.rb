85f32401a0b84b8d7fd01d9d1198c48c59f26a80
Caleb Thomas <cjthomas00@comcast.net>
2022-11-30 17:01:02 -0700

    update class code in deck.

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,12 +1,12 @@
 class Deck
-    attr_reader :cards, :high_ranking
+    attr_reader :cards
     
     def initialize(cards = [])
         @cards = cards
 
     end
 
-    def rank_of_card_at(index)
-        @cards[index].rank
-    end
+    #def rank_of_card_at(index)
+     #   @cards[index].rank
+    #end
 end
