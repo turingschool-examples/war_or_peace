79f22b2162899c54f276501801d3a1b250a561ac
Harrison Ryan <hwryan12@gmail.com>
2022-12-01 17:54:53 -0600

    Add test for M.A.D type

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -175,7 +175,7 @@ RSpec.describe Turn do
 
         turn.pile_cards
         turn.spoils_of_war
-# require "pry"; binding.pry
+
         expect(turn.spoils_of_war).to eq ([card1, card2, card5, card4, card3, card6])
 
         turn.award_spoils(winner)
@@ -202,10 +202,29 @@ RSpec.describe Turn do
         turn.type
 # require "pry"; binding.pry
         expect(turn.type).to eq(:mutually_assured_destruction)
-
-
     end
     
+    it "can run a turn for :mutually_assured_destruction" do
+        card1 = Card.new(:heart, 'Jack', 11)
+        card2 = Card.new(:heart, '10', 10)  
+        card3 = Card.new(:heart, '9', 9)
+        card4 = Card.new(:diamond, 'Jack', 11)
+        card5 = Card.new(:heart, '8', 8) 
+        card6 = Card.new(:diamond, '8', 8) 
+        card7 = Card.new(:heart, '3', 3) 
+        card8 = Card.new(:diamond, '2', 2)
+        deck1 = Deck.new([card1, card2, card5, card8])
+        deck2 = Deck.new([card4, card3, card6, card7])
+        player1 = Player.new("Megan", deck1) 
+        player2 = Player.new("Aurora", deck2)
+        turn = Turn.new(player1, player2) 
+        winner = turn.winner
+
+        turn.pile_cards
+        turn.spoils_of_war
+        expect(player1.deck).to eq(deck1)
+        expect(player2.deck).to eq(deck2)
+    end
 end
 
        
