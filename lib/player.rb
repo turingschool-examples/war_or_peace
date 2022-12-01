6a21a4a288ff290940d9dc4e379c47421df93a84
Kassandra Leyba <kassandra.leyba@gmail.com>
2022-12-01 11:40:40 -0700

    Add has lost

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,14 @@
 class Player
-    attr_reader :name, :deck, :player
+    attr_reader :name, :deck
     def initialize(name, deck)
         @name = name
         @deck = deck
+        @has_lost = false
+    end
+
+    def has_lost?
+        #reassigns
+        @has_lost = true if deck.cards.empty? 
+        @has_lost
     end
 end
