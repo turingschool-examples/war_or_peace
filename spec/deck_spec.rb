81167ec02a63ef11b71c932f7d1fba6fafd9ae36
Meredith Trevino <meredithtrevino@gmail.com>
2022-11-30 18:17:33 -0700

    adds working iteration 1 tests

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -8,11 +8,68 @@ RSpec.describe Deck do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3) 
     card3 = Card.new(:heart, 'Ace', 14) 
-    
+
     cards = [card1, card2, card3]
     deck = Deck.new(cards)
-
     expect(deck).to be_an_instance_of(Deck)
     end
+
+    it 'gives rank of card' do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3) 
+        card3 = Card.new(:heart, 'Ace', 14)
+        
+        cards = [card1, card2, card3]
+        deck = Deck.new(cards)
+        expect(deck.rank_of_card_at(0)).to eq(12)
+        expect(deck.rank_of_card_at(2)).to eq(14)
+    end
+
+    it 'identifies high ranking cards' do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3) 
+        card3 = Card.new(:heart, 'Ace', 14)
+        
+        cards = [card1, card2, card3]
+        deck = Deck.new(cards)
+
+        expect(deck.high_ranking_cards).to eq([card1, card3])
+    end
+
+
+    it 'shows percent high ranking cards' do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3) 
+        card3 = Card.new(:heart, 'Ace', 14)
+        
+        cards = [card1, card2, card3]
+        deck = Deck.new(cards)
+
+        expect(deck.percent_high_ranking).to eq(66.67)
+    end
+
+    it 'removes a card' do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3) 
+        card3 = Card.new(:heart, 'Ace', 14)
+        
+        cards = [card1, card2, card3]
+        deck = Deck.new(cards)
+
+        expect(deck.remove_card).to eq(card1)
+    end
+    it 'adds a card' do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3) 
+        card3 = Card.new(:heart, 'Ace', 14)
+
+        cards = [card1, card2, card3]
+        deck = Deck.new(cards)
+
+        card4 = Card.new(:club, '5', 5)
+        expect(deck.add_card(card4)).to eq([card1, card2, card3, card4])
+    end
+        
+
 
 end
