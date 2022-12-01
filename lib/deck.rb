929309506b5450a77ba414cf9d623cd623dc7e33
Caleb Thomas <cjthomas00@comcast.net>
2022-11-30 19:07:25 -0700

    complete rank, and high rank code.

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,12 +1,22 @@
 class Deck
-    attr_reader :cards
+    attr_reader :cards, :high_ranking
     
     def initialize(cards = [])
         @cards = cards
-
+        @high_ranking = []
     end
 
-    #def rank_of_card_at(index)
-     #   @cards[index].rank
-    #end
+    def rank_of_card_at(index)
+       @cards[index].rank
+    end
+
+    def high_ranking_cards
+        @cards.each do |card|
+            if card.rank >= 11
+                @high_ranking << card
+            end
+            
+        end
+        @high_ranking
+    end
 end
