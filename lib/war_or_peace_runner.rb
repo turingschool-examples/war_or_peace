83c8f9f740a858ab3789ab0549c26ca0d0eb7143
Hady Matar <hadyematar@gmail.com>
2022-12-01 18:42:09 -0700

    Complete shuffle the long way for iteration 3

---
+++ lib/war_or_peace_runner.rb
@@ -0,0 +1,100 @@
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+require 'rspec'
+
+card1 = Card.new(:diamond, '2', 2)
+card2 = Card.new(:spade, '2', 2)
+card3 = Card.new(:heart, '2', 2)
+card4 = Card.new(:club, '2', 2)
+
+card5 = Card.new(:diamond, '3', 3)
+card6 = Card.new(:spade, '3', 3)
+card7 = Card.new(:heart, '3', 3)
+card8 = Card.new(:club, '3', 3)
+
+card9 = Card.new(:diamond, '4', 4)
+card10 = Card.new(:spade, '4', 4)
+card11 = Card.new(:heart, '4', 4)
+card12 = Card.new(:club, '4', 4)
+
+card13 = Card.new(:diamond, '5', 5)
+card14 = Card.new(:spade, '5', 5)
+card15 = Card.new(:heart, '5', 5)
+card16 = Card.new(:club, '5', 5)
+
+card17 = Card.new(:diamond, '6', 6)
+card18 = Card.new(:spade, '6', 6)
+card19 = Card.new(:heart, '6', 6)
+card20 = Card.new(:club, '6', 6)
+
+card21 = Card.new(:diamond, '7', 7)
+card22 = Card.new(:spade, '7', 7)
+card23 = Card.new(:heart, '7', 7)
+card24 = Card.new(:club, '7', 7)
+
+card25 = Card.new(:diamond, '8', 8)
+card26 = Card.new(:spade, '8', 8)
+card27 = Card.new(:heart, '8', 8)
+card28 = Card.new(:club, '8', 8)
+
+card29 = Card.new(:diamond, '9', 9)
+card30 = Card.new(:spade, '9', 9)
+card31 = Card.new(:heart, '9', 9)
+card32 = Card.new(:club, '9', 9)
+
+card33 = Card.new(:diamond, '10', 10)
+card34 = Card.new(:spade, '10', 10)
+card35 = Card.new(:heart, '10', 10)
+card36 = Card.new(:club, '10', 10)
+
+card37 = Card.new(:diamond, 'Jack', 11)
+card38 = Card.new(:spade, 'Jack', 11)
+card39 = Card.new(:heart, 'Jack', 11)
+card40 = Card.new(:club, 'Jack', 11)
+
+card41 = Card.new(:club, 'Queen', 12)
+card42 = Card.new(:diamond, 'Queen', 12)
+card43 = Card.new(:spade, 'Queen', 12)
+card44 = Card.new(:heart, 'Queen', 12)
+
+card45 = Card.new(:club, 'King', 13)
+card46 = Card.new(:diamond, 'King', 13)
+card47 = Card.new(:spade, 'King', 13)
+card48 = Card.new(:heart, 'King', 13)
+
+card49 = Card.new(:club, 'Ace', 14)
+card50 = Card.new(:diamond, 'Ace', 14)
+card51 = Card.new(:spade, 'Ace', 14)
+card52 = Card.new(:heart, 'Ace', 14)
+
+all_cards = [card1, card2, card3, card4, card5, card6, card7, card8,card9, card10, card11, card12, card13, card14, card15, card16, card17, card18, card19, card20, card21, card22, card23, card24, card25, card26, card27, card28, card29, card30, card31, card32, card33, card34, card35, card36, card37, card38, card39, card40, card41, card42, card43, card44, card45, card46, card47, card48, card49, card50, card51, card52]
+
+set_of_cards_deck_1 = []
+set_of_cards_deck_2 = []
+
+26.times do 
+    deleted_card = all_cards.sample
+    set_of_cards_deck_1 << deleted_card
+    all_cards.delete(deleted_card)
+end
+
+26.times do 
+    remaining_cards = all_cards.sample
+    set_of_cards_deck_2 << remaining_cards
+    all_cards.delete(remaining_cards)
+end
+
+
+
+
+
+
+
+
+
+
+deck1 = Deck.new([set_of_cards_deck_1])
+deck2 = Deck.new([set_of_cards_deck_2])
+
