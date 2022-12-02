84dd637c12583f8faa1468edf938e1d4b069bbde
Adam Bailey <ab67319@gmail.com>
2022-12-01 19:55:23 -0700

    Update winner method to hand :war logic

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,23 +18,30 @@ class Turn
     end
 
     def winner
-        return @player1 if @player1.deck.cards[0].rank > @player2.deck.cards[0].rank
-        return @player2
+        if type == :basic
+            return @player1 if @player1.deck.cards[0].rank > @player2.deck.cards[0].rank
+            return @player2
+        elsif type == :war
+            return @player1 if @player1.deck.cards[2].rank > @player2.deck.cards[2].rank
+            return @player2
+        end
     end
 
     def pile_cards
         if type == :basic
-            spoils_of_war << @player1.deck.cards.shift
-            spoils_of_war << @player2.deck.cards.shift
+            @spoils_of_war << @player1.deck.cards.shift
+            @spoils_of_war << @player2.deck.cards.shift
         elsif type == :war
-                spoils_of_war << @player1.deck.cards.shift(3)
-                spoils_of_war << @player2.deck.cards.shift(3)
+            @spoils_of_war << @player1.deck.cards.shift(3)
+            @spoils_of_war << @player2.deck.cards.shift(3)
         end
 
-        spoils_of_war.flatten
+        @spoils_of_war = @spoils_of_war.flatten
     end
 
     def award_spoils(winner)
-            winner.deck.cards << pile_cards
+        require "pry"; binding.pry
+            winner_pile = winner.deck.cards
+            winner.deck.cards = winner_pile.flatten
     end
 end
