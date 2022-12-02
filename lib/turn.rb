3a72017365c308b92f919fdbf465c3465718c03e
Jade Stewart <jadekstewart3@gmail.com>
2022-12-01 19:10:21 -0700

    Add pile cards method

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,14 +1,15 @@
 class Turn
-    attr_reader :player1,
+        attr_reader :player1,
                 :player2,
                 :spoils_of_war
                 
                 
 
-    def initialize(player1, player2)
-        @player1 = player1
-        @player2 = player2
-        @spoils_of_war = []
+        def initialize(player1, player2)
+            @player1 = player1
+            @player2 = player2
+            @spoils_of_war = []
+        end
         
         def type
            if  @player1.deck.rank_of_cards_at(0) == @player2.deck.rank_of_cards_at(0) && @player1.deck.rank_of_cards_at(2) == @player2.deck.rank_of_cards_at(2)
@@ -36,12 +37,17 @@ class Turn
             else
                 "No Winner"
             end
-
         end
 
-        
-
-
-
-    end
+        def pile_cards
+            if type == :basic
+                @spoils_of_war << @player1.deck.cards.shift
+                @spoils_of_war << @player2.deck.cards.shift
+            elsif type == :war
+                @spoils_of_war << @player1.deck.cards.slice!(0..2)
+                @spoils_of_war << @player2.deck.cards.slice!(0..2)
+            end
+            @spoils_of_war = @spoils_of_war.flatten
+        end
+    
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -14,6 +14,9 @@ RSpec.describe Turn do
         @card6 = Card.new(:diamond, 'Queen', 12)
         @card7 = Card.new(:heart, '3', 3) 
         @card8 = Card.new(:diamond, '2', 2)
+        @card9 = Card.new(:spade, 'Jack', 11)
+        @card10 = Card.new(:spade, '8', 8)
+       
 
         @deck1 = Deck.new([@card1, @card2, @card5, @card8])
         @deck2 = Deck.new([@card3, @card4, @card6, @card7]) 
@@ -44,7 +47,7 @@ RSpec.describe Turn do
     
     it "dertermines a war turn type" do 
         deck1 = Deck.new([@card1, @card2, @card5, @card8])
-        deck2 = Deck.new([@card1, @card4, @card6, @card7]) 
+        deck2 = Deck.new([@card9, @card4, @card6, @card7]) 
         player1 = Player.new("Megan", deck1)
         player2 = Player.new("Aurora", deck2)
         
@@ -55,7 +58,7 @@ RSpec.describe Turn do
 
     it "determines a mututally assured destruction turn type" do
         deck1 = Deck.new([@card1, @card2, @card5, @card8])
-        deck2 = Deck.new([@card1, @card4, @card5, @card7]) 
+        deck2 = Deck.new([@card9, @card4, @card10, @card7]) 
         player1 = Player.new("Megan", deck1)
         player2 = Player.new("Aurora", deck2)
         
@@ -71,7 +74,7 @@ RSpec.describe Turn do
 
     it "determines the winner of a war turn" do 
         deck1 = Deck.new([@card1, @card2, @card5, @card8])
-        deck2 = Deck.new([@card1, @card4, @card6, @card7]) 
+        deck2 = Deck.new([@card9, @card4, @card6, @card7]) 
         player1 = Player.new("Megan", deck1)
         player2 = Player.new("Aurora", deck2)
 
@@ -82,46 +85,36 @@ RSpec.describe Turn do
 
     it "determines mutually assured destruction" do 
         deck1 = Deck.new([@card1, @card2, @card5, @card8])
-        deck2 = Deck.new([@card1, @card4, @card5, @card7]) 
+        deck2 = Deck.new([@card9, @card4, @card10, @card7]) 
         player1 = Player.new("Megan", deck1)
         player2 = Player.new("Aurora", deck2)
 
         turn = Turn.new(player1, player2)
 
         expect(turn.winner).to eq("No Winner")
-
     end
 
-    
-
-
-    xit "can pile cards" do 
+    it "can pile cards for basic turn type" do 
 
         @turn.pile_cards
         expect(@turn.spoils_of_war).to eq([@card1, @card3])
-
+        expect(@deck1.cards).to eq([@card2, @card5, @card8])
+        expect(@deck2.cards).to eq([@card4, @card6, @card7])
     end
 
-    xit "can award spoils to the winner" do 
+    it "can pile cards for war turn type" do 
+        deck1 = Deck.new([@card1, @card2, @card5, @card8])
+        deck2 = Deck.new([@card9, @card4, @card6, @card7]) 
+        player1 = Player.new("Megan", deck1)
+        player2 = Player.new("Aurora", deck2)
 
-        @turn.award_spoils(winner)
+        turn = Turn.new(player1, player2)
 
-        expect(@player1.deck).to eq([@card1, @card2, @card5, @card3, @card8])
-        expect(@player2.deck).to eq([@card4, @card6, @card7])
+        turn.pile_cards
+        expect(turn.spoils_of_war).to eq([@card1, @card2, @card5, @card9, @card4, @card6])
+        expect(deck1.cards).to eq([@card8])
+        expect(deck2.cards).to eq([@card7])
     end
-
-
-    xit "has a war turn type" do 
-
-        expect(turn.type).to eq(:war)
-        expect(turn.winner).to eq(@player1)
-    end
-
-
-
-
-
-
 
 
 end
