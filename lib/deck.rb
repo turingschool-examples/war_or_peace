ce35db615258b1aa46101de37fc1384d875121bf
Amy Spears <amspears007@gmail.com>
2022-12-01 18:39:51 -0700

    Add 3rd test passing

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,9 +1,10 @@
 class Deck
 
-    attr_reader :cards_arr
+    attr_reader :cards_arr, :high_cards
 
     def initialize (cards_arr)
         @cards = cards_arr
+        @high_cards = []
         # require 'pry'; binding.pry
     end
 
@@ -11,4 +12,15 @@ class Deck
         # require 'pry'; binding.pry
         @cards[index].rank
     end
+
+    def high_ranking_cards
+        # require 'pry'; binding.pry
+        @cards.each do |card_arr|
+            if card_arr.rank >= 11 
+                @high_cards << card_arr
+            end
+        end
+        @high_cards
+    end
 end
+
---
+++ spec/deck_spec.rb
@@ -0,0 +1,49 @@
+require 'rspec'
+require './lib/card'
+require './lib/deck'
+
+RSpec.describe Deck do
+it 'exists' do
+
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3) 
+    card3 = Card.new(:heart, 'Ace', 14)
+# require 'pry'; binding.pry
+    cards_arr = [card1, card2, card3]
+
+    deck = Deck.new(cards_arr)
+    expect(cards_arr).to eq([card1, card2, card3])
+  end
+end
+
+RSpec.describe Deck do
+it 'uses index to return rank' do
+
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3) 
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards_arr = [card1, card2, card3]
+
+    deck = Deck.new(cards_arr)
+# require 'pry'; binding.pry
+    expect(deck.rank_of_card_at(0)).to eq(12)
+    expect(deck.rank_of_card_at(2)).to eq(14)
+  end
+end
+
+RSpec.describe Deck do
+it 'will return high ranking cards' do
+# this method will return an array of cards in the deck that have a rank of 11 or above (face cards and aces)
+  
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3) 
+      card3 = Card.new(:heart, 'Ace', 14)
+  
+      cards_arr = [card1, card2, card3]
+  
+      deck = Deck.new(cards_arr)
+
+      expect(deck.high_ranking_cards).to eq([card1, card3])
+  end
+end
