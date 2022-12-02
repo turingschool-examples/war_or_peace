3f7690cbacbfb0edc77b76ccd851b9ad480ac324
Conner Van Loan <cdvanloan@gmail.com>
2022-12-02 10:46:21 -0700

    Completes :mutual tests

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -190,6 +190,27 @@ describe Turn do
             expect(turn.winner).to eq("No Winner")
         end
 
+        it 'can destroy top 3 cards' do
+            card1 = Card.new(:heart, 'Jack', 11)
+            card2 = Card.new(:heart, '10', 10)
+            card3 = Card.new(:heart, '9', 9)
+            card4 = Card.new(:diamond, 'Jack', 11)
+            card5 = Card.new(:heart, '8', 8)
+            card6 = Card.new(:diamond, '8', 8)
+            card7 = Card.new(:heart, '3', 3)  
+            card8 = Card.new(:diamond, '2', 2) 
+            deck1 = Deck.new([card1, card2, card5, card8])
+            deck2 = Deck.new([card4, card3, card6, card7])
+            player1 = Player.new("Megan", deck1)
+            player2 = Player.new("Aurora", deck2)
+            turn = Turn.new(player1, player2)
+            winner = turn.winner
+            turn.pile_cards
+            expect(turn.spoils_of_war).to eq([])
+            expect(turn.player1.deck.cards.count).to eq(1)
+            expect(turn.player2.deck.cards.count).to eq(1)
+        end
+
 
     end
 end
