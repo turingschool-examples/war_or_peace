4fcce9f703a35468818dde0bbeb7e9e2f583cb29
Andra Helton <andrahelton@gmail.com>
2022-12-01 19:04:03 -0700

    Add passing test for has a basic turn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,7 +4,13 @@ class Turn
         @player1 = player1
         @player2 = player2
         @spoils_of_war = []
+
     end
 
+    def type
+        if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
+            :basic
+        end
+    end
 
 end
