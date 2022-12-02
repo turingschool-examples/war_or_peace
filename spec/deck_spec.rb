209c418a7bb32c7824cc72f22c8eb968b1548852
Melony Franchini <meltravelz@gmail.com>
2022-12-01 17:31:49 -0700

    Add test show high_ranking_cards after remove_card

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -71,7 +71,19 @@ RSpec.describe Deck do
 
         expect(deck.remove_card).to eq(card1)
         expect(deck.cards).to eq([card2, card3])
+        expect(deck.high_ranking_cards).to eq([card3])
     end
+
+    # it "can " do
+    #     card1 = Card.new(:diamond, 'Queen', 12)
+    #     card2 = Card.new(:spade, '3', 3)
+    #     card3 = Card.new(:heart, 'Ace', 14)
+    #     cards = [card1, card2, card3]    
+
+    #     deck = Deck.new(cards)
+
+    #     expect(
+    # end
 
 
 end
