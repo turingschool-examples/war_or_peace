fcc2538fcce63abf49b15ad468c762fbc4002489
Melony Franchini <meltravelz@gmail.com>
2022-12-02 13:55:12 -0700

    Add test has_lost?

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -17,5 +17,18 @@ RSpec.describe Player do
         expect(player).to be_an_instance_of(Player)
     end
 
+    it "has_lost?" do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)    
+        deck = Deck.new([card1, card2, card3])
+        player = Player.new('Clarisa', deck)
+
+        expect(player.has_lost?).to eq(false)
+    end
+
+
+
+
 end
 
