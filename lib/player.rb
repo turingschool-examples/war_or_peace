f0a0e0de3cb52cdab11702e840bed22812cd1f55
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2022-12-01 17:57:13 -0700

    Test set 1, test 1 complete - player.rb/player_spec.rb

--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,15 @@
 class Player
-    
+
+    attr_reader :name, :deck, :has_lost
+    def initialize(name, deck)
+        @name = name
+        @deck = deck
+        @has_lost = false
+    end
+
+    def has_lost?
+        @has_lost
+    end
 end
+
+# require 'pry'; binding.pry
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,6 +2,19 @@ require './lib/card'
 require './lib/deck'
 require './lib/player'
 
-Rspec.describe Player do
+RSpec.describe Player do
+    context 'Test Set 1' do
+        it 'is a player' do
+            card1 = Card.new(:diamonds, 'Queen', 12)
+            card2 = Card.new(:spades, 'Three', 3)
+            card3 = Card.new(:hearts, 'Ace', 14) 
 
+            clarisa_deck = Deck.new([card1, card2, card3])
+            clarisa = Player.new('Clarisa', clarisa_deck)
+
+            expect(clarisa.name).to eq('Clarisa')
+            expect(clarisa.deck).to be(clarisa_deck)
+            expect(clarisa.has_lost?).to eq(false)
+        end
+    end
 end
