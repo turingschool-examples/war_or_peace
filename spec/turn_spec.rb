a7dc2104a8d0aabccad7c3111c5cc95376beb560
Brian Hayes <Brian.hayes1200@gmail.com>
2022-12-02 16:47:53 -0700

    Test: add test for pile cards method for class Turn

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -140,4 +140,66 @@ RSpec.describe Turn do
       expect(winner).to eq("No Winner")
     end 
   end 
+
+  describe '#pile cards' do 
+    it 'will send cards from players decks into spoils of war for turn basic' do 
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)  
+      card3 = Card.new(:heart, '9', 9)    
+      card4 = Card.new(:diamond, 'Jack', 11) 
+      card5 = Card.new(:heart, '8', 8)  
+      card6 = Card.new(:diamond, 'Queen', 12)   
+      card7 = Card.new(:heart, '3', 3)    
+      card8 = Card.new(:diamond, '2', 2)  
+      deck1 = Deck.new([card1, card2, card5, card8])    
+      deck2 = Deck.new([card3, card4, card6, card7]) 
+      player1 = Player.new("Megan", deck1)   
+      player2 = Player.new("Aurora", deck2)  
+      turn = Turn.new(player1, player2)
+      turn.pile_cards
+
+      expect(turn.spoils_of_war).to eq([card1, card3])
+    end 
+
+    it 'will send cards from players decks into spoils of war for turn war' do 
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)  
+      card3 = Card.new(:heart, '9', 9)    
+      card4 = Card.new(:diamond, 'Jack', 11) 
+      card5 = Card.new(:diamond, 'Jack', 11)  
+      card6 = Card.new(:diamond, 'Queen', 12)   
+      card7 = Card.new(:heart, '3', 3)    
+      card8 = Card.new(:diamond, '2', 2)  
+      deck1 = Deck.new([card1, card2, card3, card4])    
+      deck2 = Deck.new([card5, card6, card7, card8]) 
+      player1 = Player.new("Megan", deck1)   
+      player2 = Player.new("Aurora", deck2)  
+      turn = Turn.new(player1, player2)
+      turn.pile_cards
+
+
+      expect(turn.spoils_of_war).to eq([card1, card2, card3, card5, card6, card7])
+    end 
+
+    it 'will send cards from players deck into spoils of war for turn mutually assured destruction' do 
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)  
+      card3 = Card.new(:heart, '9', 9)    
+      card4 = Card.new(:diamond, 'Jack', 11) 
+      card5 = Card.new(:diamond, 'Jack', 11)  
+      card6 = Card.new(:diamond, 'Queen', 12)   
+      card7 = Card.new(:heart, '9', 9)    
+      card8 = Card.new(:diamond, '9', 9)  
+      deck1 = Deck.new([card1, card2, card3, card4])    
+      deck2 = Deck.new([card5, card6, card7, card8]) 
+      player1 = Player.new("Megan", deck1)   
+      player2 = Player.new("Aurora", deck2)  
+      turn = Turn.new(player1, player2)
+      turn.pile_cards
+      
+      expect(turn.spoils_of_war).to eq[[]]
+    end 
+  end 
+      
+
 end 
