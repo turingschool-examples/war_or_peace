fc7081ed440d17253a71cb1b97f1711f8ccd80d8
Kassandra Leyba <kassandra.leyba@gmail.com>
2022-12-01 18:53:20 -0700

    Add turn types

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,6 +10,12 @@ class Turn
     end
 
     def type
-        
+        if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+            :mutually_assured_destruction
+        elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+            :war
+        else
+            :basic
+        end
     end
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -5,6 +5,10 @@ require './lib/player'
 require './lib/turn'
 
 RSpec.describe Turn do
+    # describe "basic turn" do 
+    # describe "war" do
+    # describe "mutully_assured_destruction"
+
     it "exists" do
         card1 = Card.new(:heart, 'Jack', 11)
         card2 = Card.new(:heart, '10', 10)
@@ -27,7 +31,7 @@ RSpec.describe Turn do
     end
 
 
-    it "has a type" do
+    it "has basic a type" do
         card1 = Card.new(:heart, 'Jack', 11)
         card2 = Card.new(:heart, '10', 10)
         card3 = Card.new(:heart, '9', 9)
@@ -51,5 +55,47 @@ RSpec.describe Turn do
         expect(turn.spoils_of_war).to eq([])
         expect(turn.type).to eq(:basic)
     end
+
+     it "has a war type" do
+         card1 = Card.new(:heart, 'Jack', 11) 
+         card2 = Card.new(:heart, '10', 10)
+         card3 = Card.new(:heart, '9', 9)
+         card4 = Card.new(:diamond, 'Jack', 11)
+         card5 = Card.new(:heart, '8', 8) 
+         card6 = Card.new(:diamond, 'Queen', 12) 
+         card7 = Card.new(:heart, '3', 3) 
+         card8 = Card.new(:diamond, '2', 2)
+
+         deck1 = Deck.new([card1, card2, card5, card8]) 
+         deck2 = Deck.new([card4, card3, card6, card7])
+
+         player1 = Player.new("Megan", deck1)
+         player2 = Player.new("Aurora", deck2)
+
+         turn = Turn.new(player1, player2)
+
+         expect(turn.type).to eq(:war)
+      end
+
+     it "has a mutually_assured_destruction type" do
+        card1 = Card.new(:heart, 'Jack', 11) 
+        card2 = Card.new(:heart, '10', 10)
+        card3 = Card.new(:heart, '9', 9)
+        card4 = Card.new(:diamond, 'Jack', 11)
+        card5 = Card.new(:heart, '8', 8) 
+        card6 = Card.new(:diamond, 'Queen', 12) 
+        card7 = Card.new(:heart, '3', 3) 
+        card8 = Card.new(:diamond, '2', 2)
+
+        deck1 = Deck.new([card1, card2, card5, card8])
+        deck2 = Deck.new([card4, card3, card6, card7])
+
+        player1 = Player.new("Megan", deck1)
+        player2 = Player.new("Aurora", deck2) 
+
+        turn = Turn.new(player1, player2)
+
+        expect(turn.type).to eq(:war)
+     end
 end
     
