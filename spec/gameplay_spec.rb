cc281f8427a370d7f9f12f376a36291b1013c0fd
Tori Enyart <torienyart@gmail.com>
2022-12-02 10:56:47 -0700

    Test first step - create new turn based on conditional - in start method

--- spec/gameplay_spec.rb
+++ spec/gameplay_spec.rb
@@ -30,5 +30,31 @@ describe GamePlay do
         expect(game.turns).to eq([])
     
     end
+
+    it 'can start game with one turn' do
+        card1 = Card.new(:heart, 'Jack', 11)
+        card2 = Card.new(:heart, '10', 10)
+        card3 = Card.new(:heart, '9', 9)
+        card4 = Card.new(:diamond, 'Jack', 11)
+        card5 = Card.new(:heart, '8', 8)
+        card6 = Card.new(:diamond, 'Queen', 12)
+        card7 = Card.new(:heart, '3', 3)
+        card8 = Card.new(:diamond, '2', 2)
+
+        deck1 = Deck.new([card1, card2, card5, card8])
+        deck2 = Deck.new([card3, card4, card6, card7])
+
+        player1 = Player.new("Megan", deck1)
+        player2 = Player.new("Aurora", deck2)
+
+        game = GamePlay.new(player1, player2)
+        game.start
+# require 'pry'; binding.pry
+        expect(game.turns.size).to eq(1)
+        expect(game.turns.first.player1).to eq(player1)
+        expect(game.turns.first.player2).to eq(player2)
+
+
+    end
 end
 
