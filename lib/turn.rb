39656631cf85519dfe9e63893c64f47b9a2995c3
Huy Phan <huyphan520@gmail.com>
2022-12-01 19:48:06 -0600

    Iteration 2 finish types

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,7 +4,30 @@ class Turn
         @player1 = player1
         @player2 = player2
         @spoils_of_war = []
+
     end
+
+    def type
+        if @player1.deck.rank_of_cards_at(0) == @player2.deck.rank_of_cards_at(0) && @player1.deck.rank_of_cards_at(2) == @player2.deck.rank_of_cards_at(2)
+            :mutually_assured_destruction
+        elsif @player1.deck.rank_of_cards_at(0) == @player2.deck.rank_of_cards_at(0)
+            :war
+        else @player1.deck.rank_of_cards_at(0) != @player2.deck.rank_of_cards_at(0)
+            :basic
+        end
+    end
+        
+
+    
+    def winner
+        if @player1.deck.rank_of_cards_at(0) > @player2.deck.rank_of_cards_at(0)
+            player1
+        end
+
+    end
+
+    
+
 
 
 end
