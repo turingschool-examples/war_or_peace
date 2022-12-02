9a6ca3c03c0f4966fb0186d2c245e41e27f8f4df
Brad Dunlap <braddunlapdpt@gmail.com>
2022-12-01 19:40:35 -0500

    Test if can determine winner

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,7 @@ class Turn
     def initialize(player1, player2)
         @player1 = player1
         @player2 = player2
+        @spoils_of_war = []
 
     end
 
@@ -19,6 +20,26 @@ class Turn
         else
             :war
         end
+    end
+
+    def winner
+        if type == :basic 
+            if player1.deck.rank_of_card_at(0) > player2. deck.rank_of_card_at(0)
+                player1
+            elsif player1.deck.rank_of_card_at(0) < player2. deck.rank_of_card_at(0)
+                player2
+            end
+
+        elsif type == :mutually_assured_destruction
+
+        else type == :war
+            if player1.deck.rank_of_card_at(2) > player2. deck.rank_of_card_at(2)
+                player1
+            elsif player1.deck.rank_of_card_at(2) < player2. deck.rank_of_card_at(2)
+                player2
+            end
+        end
+        
 
         
     end
