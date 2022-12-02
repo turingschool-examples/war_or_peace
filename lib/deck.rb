50439108231bccf5fc3de9df3400589c6fb9afff
Bobby Luly <bobbyluly@gmail.com>
2022-12-01 22:22:52 -0700

    Correct indent

--- lib/deck.rb
+++ lib/deck.rb
@@ -12,9 +12,11 @@ class Deck
 
   def high_ranking_cards
     high_ranking_cards = []
+
     cards.each do |card|
-    high_ranking_cards << card if card.rank >= 11
+      high_ranking_cards << card if card.rank >= 11
     end
+
     high_ranking_cards
   end
 
