3e88004f009b141c13d8164c12b1e7d36b9c87c8
Matt Enyeart <menyeart1@gmail.com>
2022-12-02 10:02:25 -0700

    Add logic to the game class to deal with only several cards in a deck

--- lib/game.rb
+++ lib/game.rb
@@ -18,56 +18,34 @@ class Game
 
   def output_game
     i = 1
-    100.times do
+    100_000_000.times do
      i += 1
      if turn.type == :basic
       p "#{turn.winner.name} won #{turn.pile_cards.length} cards"
+      turn.pile_cards
       turn.award_spoils(turn.winner)
      elsif turn.type == :war 
       p "WAR - #{turn.winner.name} won #{turn.pile_cards.length} cards"
+      turn.pile_cards
       turn.award_spoils(turn.winner)
      else
       p "*mutually assured desctruction* #{turn.pile_cards.length} removed from play."
+      turn.pile_cards
      end
+   
+    p turn.player1.deck.cards.length 
+    p turn.player2.deck.cards.length
 
-
-
-
-
-    # if turn.winner.class != String
-    #   turn.winner
-    #   p turn.winner.name
-    #   turn.pile_cards
-    #   turn.award_spoils(turn.winner)
-    # else 
-    #   turn.winner
-    #   p turn.winner.name
-    #   turn.pile_cards
-    # end
-       
-    # p turn.winner
-    # p turn.pile_cards
-    # p turn.award_spoils(turn.winner)
-    
-      # if turn.winner != "No Winner"
-      #   turn.pile_cards
-      #   turn.award_spoils(turn.winner)
-      # else
-      #   turn.pile_cards
-      # end
-      
-
-      
-      # if turn.player1.deck.cards == []
-      #   game_winner = turn.player2.name
-      #   p '*~*~*~* #{game_winner} has won the game! *~*~*~*'
-      #   break
-      # elsif turn.player2.deck.cards == []
-      #   game_winner= turn.player1.name
-      #   p '*~*~*~* #{game_winner} has won the game! *~*~*~*'
-      #   break
-      # end
+    if turn.player1.deck.cards == []
+      game_winner = turn.player2.name
+      p '*~*~*~* #{game_winner} has won the game! *~*~*~*'
+      break
+    elsif turn.player2.deck.cards == []
+      game_winner= turn.player1.name
+      p '*~*~*~* #{game_winner} has won the game! *~*~*~*'
+      break
     end
+  end
 
     puts "*~*~*~* Aren't you bored of playing war? No one won the game! *~*~*~*"
      
