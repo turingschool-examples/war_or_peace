7a68ebd41b496abc2a6a3d8c862afbda95468f05
KaraJH <karas.jones@yahoo.com>
2022-12-02 14:51:33 -0700

    Tests for the game result display method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -124,8 +124,62 @@ RSpec.describe Game do
       player2 = Player.new("Aurora", deck2)
 
       turn = Turn.new(player1, player2)
+      game.initiate_war
 
       expect(turn.type).to eq(:war)
+    end
+  end
+
+  describe '#mutually_assured_destruction_turn_sequence' do 
+    it 'runs the output and sequence for a mutually assured destruction type of turn' do
+      game = Game.new
+      card1 = Card.new(:heart, 'Jack', 11)    
+      card2 = Card.new(:heart, '10', 10)    
+      card3 = Card.new(:heart, '9', 9)    
+      card4 = Card.new(:diamond, 'Jack', 11)    
+      card5 = Card.new(:heart, '8', 8)    
+      card6 = Card.new(:diamond, '8', 8)    
+      card7 = Card.new(:heart, '3', 3)    
+      card8 = Card.new(:diamond, '2', 2) 
+
+      deck1 = Deck.new([card1, card2, card5, card8])  
+      deck2 = Deck.new([card4, card3, card6, card7])  
+
+      player1 = Player.new("Megan", deck1) 
+      player2 = Player.new("Aurora", deck2) 
+
+      turn = Turn.new(player1, player2)
+      game.initiate_war
+
+      expect(turn.type).to eq(:mutually_assured_destruction)
+    end
+  end
+
+  describe '#game_result_display' do 
+    it 'displays who won the game or if it was a draw' do 
+      game = Game.new
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)    
+      card3 = Card.new(:heart, '9', 9)    
+      card4 = Card.new(:diamond, 'Jack', 11)    
+      card5 = Card.new(:heart, '8', 8)    
+      card6 = Card.new(:diamond, 'Queen', 12)    
+      card7 = Card.new(:heart, '3', 3)    
+      card8 = Card.new(:diamond, '2', 2)   
+
+      deck1 = Deck.new([card1, card2, card5, card8]) 
+      deck2 = Deck.new([card3, card4, card6, card7])   
+
+      player1 = Player.new("Megan", deck1) 
+      player2 = Player.new("Aurora", deck2)    
+
+      turn = Turn.new(player1, player2) 
+      game.initiate_war
+      game.game_result_display
+
+      expect(turn.type).to eq(:basic)
+      expect(turn.winner).to eq(player1)
+      expect(game.game_result_display).to be_a(String)
     end
   end
 
