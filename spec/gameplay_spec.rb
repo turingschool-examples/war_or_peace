eccb02b0f3f5067386296d82e4382f119a0d4d7a
Tori Enyart <torienyart@gmail.com>
2022-12-02 10:39:47 -0700

    Complete necessary require statements in gameplay_spec and runner file

---
+++ spec/gameplay_spec.rb
@@ -0,0 +1,4 @@
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -2,7 +2,7 @@ require './lib/card'
 require './lib/deck'
 require './lib/player'
 require './lib/turn'
-
+require './lib/gameplay'
 
 #create 52 cards
 cards = []
