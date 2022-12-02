39fed9ff61c7096587e7a86979700169585e212e
Tori Enyart <torienyart@gmail.com>
2022-12-01 18:14:14 -0700

    Create cards for game

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -3,3 +3,75 @@ require './lib/deck'
 require './lib/player'
 require './lib/turn'
 
+
+#create 52 cards
+cards = []
+value = 1
+rank = 1
+
+cards << Card.new(:diamond, 'Ace', 1)
+
+9.times do
+    suit = :diamond
+    value += 1
+    rank += 1
+    cards << Card.new(suit, value, rank)
+
+end
+
+cards << Card.new(:diamond, 'Jack', 11)
+cards << Card.new(:diamond, 'Queen', 12)
+cards << Card.new(:diamond, 'King', 13)
+
+value = 1
+rank = 1
+
+cards << Card.new(:heart, 'Ace', 1)
+
+9.times do
+    suit = :heart
+    value += 1
+    rank += 1
+    cards << Card.new(suit, value, rank)
+
+end
+
+cards << Card.new(:heart, 'Jack', 11)
+cards << Card.new(:heart, 'Queen', 12)
+cards << Card.new(:heart, 'King', 13)
+
+value = 1
+rank = 1
+
+cards << Card.new(:club, 'Ace', 1)
+
+9.times do
+    suit = :club
+    value += 1
+    rank += 1
+    cards << Card.new(suit, value, rank)
+
+end
+
+cards << Card.new(:club, 'Jack', 11)
+cards << Card.new(:club, 'Queen', 12)
+cards << Card.new(:club, 'King', 13)
+
+value = 1
+rank = 1
+
+cards << Card.new(:spade, 'Ace', 1)
+
+9.times do
+    suit = :spade
+    value += 1
+    rank += 1
+    cards << Card.new(suit, value, rank)
+
+end
+
+cards << Card.new(:spade, 'Jack', 11)
+cards << Card.new(:spade, 'Queen', 12)
+cards << Card.new(:spade, 'King', 13)
+
+
