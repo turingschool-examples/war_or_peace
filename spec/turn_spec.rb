e1057f4ca8980f90417565d5083fa49b8f4d65a7
Brian Hayes <Brian.hayes1200@gmail.com>
2022-12-01 19:58:27 -0700

    Test: add tests for winner method for class Turn

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -81,4 +81,63 @@ RSpec.describe Turn do
       expect(turn.type).to eq(:mutually_assured_destruction)
     end 
   end
+
+  describe '#winner' do 
+    it 'will check to see who the winner is for turn basic' do 
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)  
+      card3 = Card.new(:heart, '9', 9)    
+      card4 = Card.new(:diamond, 'Jack', 11) 
+      card5 = Card.new(:heart, '8', 8)  
+      card6 = Card.new(:diamond, 'Queen', 12)   
+      card7 = Card.new(:heart, '3', 3)    
+      card8 = Card.new(:diamond, '2', 2)  
+      deck1 = Deck.new([card1, card2, card5, card8])    
+      deck2 = Deck.new([card3, card4, card6, card7]) 
+      player1 = Player.new("Megan", deck1)   
+      player2 = Player.new("Aurora", deck2)  
+      turn = Turn.new(player1, player2)
+      winner = turn.winner
+
+      expect(winner).to eq(player1)
+    end 
+    
+    it 'will check to see winner for turn war' do 
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)  
+      card3 = Card.new(:heart, '9', 9)    
+      card4 = Card.new(:diamond, 'Jack', 11) 
+      card5 = Card.new(:diamond, 'Jack', 11)  
+      card6 = Card.new(:diamond, 'Queen', 12)   
+      card7 = Card.new(:heart, '3', 3)    
+      card8 = Card.new(:diamond, '2', 2)  
+      deck1 = Deck.new([card1, card2, card3, card4])    
+      deck2 = Deck.new([card5, card6, card7, card8]) 
+      player1 = Player.new("Megan", deck1)   
+      player2 = Player.new("Aurora", deck2)  
+      turn = Turn.new(player1, player2)
+      winner = turn.winner
+
+      expect(winner).to eq(player1)
+    end 
+
+    it 'will check winner for turn mutually assured destruction' do 
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)  
+      card3 = Card.new(:heart, '9', 9)    
+      card4 = Card.new(:diamond, 'Jack', 11) 
+      card5 = Card.new(:diamond, 'Jack', 11)  
+      card6 = Card.new(:diamond, 'Queen', 12)   
+      card7 = Card.new(:heart, '9', 9)    
+      card8 = Card.new(:diamond, '9', 9)  
+      deck1 = Deck.new([card1, card2, card3, card4])    
+      deck2 = Deck.new([card5, card6, card7, card8]) 
+      player1 = Player.new("Megan", deck1)   
+      player2 = Player.new("Aurora", deck2)  
+      turn = Turn.new(player1, player2)
+      winner = turn.winner
+
+      expect(winner).to eq("No winner")
+    end 
+  end 
 end 
