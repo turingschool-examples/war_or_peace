d999545a9c5ff943726e13b647a5cc0a0f0990af
Melony Franchini <meltravelz@gmail.com>
2022-12-02 14:49:33 -0700

    Clean up spacing on deck & player files

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -127,7 +127,7 @@ RSpec.describe Deck do
         expect(deck.add_card(card4)).to eq(cards)
     end
 
-    it "show add_card, high ranking & percent high ranking" do
+    it "show add_card & high ranking cards" do
         card1 = Card.new(:diamond, 'Queen', 12)
         card2 = Card.new(:spade, '3', 3)
         card3 = Card.new(:heart, 'Ace', 14)
@@ -143,6 +143,24 @@ RSpec.describe Deck do
         expect(deck.cards).to eq(cards)
         
         expect(deck.high_ranking_cards).to eq([card3])
+    end
+
+    it "show add_card, high ranking & percent high ranking" do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+        cards = [card1, card2, card3]    
+
+        deck = Deck.new(cards)
+
+        card4 = Card.new(:club, '5', 5)
+
+        expect(deck.remove_card).to eq(card1)
+        expect(deck.cards).to eq([card2, card3])
+        expect(deck.add_card(card4)).to eq(cards)
+        expect(deck.cards).to eq(cards)
+        expect(deck.high_ranking_cards).to eq([card3])
+        
         expect(deck.percent_high_ranking).to eq(33.33)
     end
 
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -9,9 +9,7 @@ RSpec.describe Player do
         card1 = Card.new(:diamond, 'Queen', 12)
         card2 = Card.new(:spade, '3', 3)
         card3 = Card.new(:heart, 'Ace', 14)    
-
         deck = Deck.new([card1, card2, card3])
-
         player = Player.new('Clarisa', deck)
 
         expect(player).to be_an_instance_of(Player)
@@ -68,5 +66,6 @@ RSpec.describe Player do
         expect(player.deck.remove_card).to eq(card3)
         expect(player.has_lost?).to eq(true)
     end
+ 
 end
 
