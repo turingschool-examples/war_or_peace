f346bb308830b257b61f5c635a173b37382d4459
Meredith Trevino <meredithtrevino@gmail.com>
2022-12-01 17:40:59 -0700

    adds working pile cards method

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,18 +14,19 @@ class Turn
             @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
             :basic
         elsif
+            @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
+            :mutually_assured_destruction 
+        else 
             @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
             :war
-        else 
-            @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
-            :mutually_assured_destruction
         end
     end
 
     def winner
         if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
             player1
-        elsif@player1.deck.rank_of_card_at(0) < @player2.deck.rank_of_card_at(0)
+        elsif 
+            @player1.deck.rank_of_card_at(0) < @player2.deck.rank_of_card_at(0)
             player2
         elsif
             @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
@@ -40,7 +41,18 @@ class Turn
     end
 
     def pile_cards
-
+        if type == :basic
+            # require 'pry';binding.pry
+           @spoils_of_war << @player1.deck.remove_card
+           @spoils_of_war << @player2.deck.remove_card
+        elsif type == :war
+            @spoils_of_war << @player1.deck(0) 
+            @player1.deck(2)
+            @player2.deck(0)
+            @player2.deck(2)
+        else
+            @spoils_of_war << @player1.deck(0) && @player1.deck(2) && @player2.deck(0) && @player2.deck(2)
+        end
     end
 
     def award_spoils
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -107,6 +107,26 @@ RSpec.describe Player do
         expect(turn.type).to eq(:war)
     end
 
+    it 'tests for :mult. assured dest. type of game' do
+        card1 = Card.new(:heart, 'Jack', 11)
+        card2 = Card.new(:heart, '10', 10)
+        card3 = Card.new(:diamond, 'Jack', 11)
+        card4 = Card.new(:diamond, 'Jack', 11)
+        card5 = Card.new(:heart, '8', 8)  
+        card6 = Card.new(:diamond, '8', 8) 
+        card7 = Card.new(:heart, '3', 3) 
+        card8 = Card.new(:diamond, '3', 3) 
+
+        deck1 = Deck.new([card1, card2, card5, card8])
+        deck2 = Deck.new([card3, card4, card6, card7])
+
+        player1 = Player.new("Megan", deck1) 
+        player2 = Player.new("Aurora", deck2)  
+
+        turn = Turn.new(player1, player2) 
+        expect(turn.type).to eq(:mutually_assured_destruction)
+    end
+
     it 'announces winner' do
         card1 = Card.new(:heart, 'Jack', 11)
         card2 = Card.new(:heart, '10', 10)
@@ -127,7 +147,7 @@ RSpec.describe Player do
         expect(turn.winner).to eq(player1)
     end
 
-    xit 'piles cards' do
+    it 'piles cards' do
         card1 = Card.new(:heart, 'Jack', 11)
         card2 = Card.new(:heart, '10', 10)
         card3 = Card.new(:heart, '9', 9)
@@ -144,9 +164,11 @@ RSpec.describe Player do
         player2 = Player.new("Aurora", deck2)  
 
         turn = Turn.new(player1, player2) 
-        expect(turn.winner).to eq(player1)
-        expect(turn.pile_cards)
 
+        turn.pile_cards
+        
+        expect(turn.type).to eq(:basic)
+        expect(turn.spoils_of_war).to eq([card1, card3])
     end
 
     xit 'awards spoils' do
