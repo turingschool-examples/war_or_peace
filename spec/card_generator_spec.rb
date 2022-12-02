7fc6cafb2ee7759c7ef2912e47e0865c4112efc1
KaraJH <karas.jones@yahoo.com>
2022-12-02 15:57:43 -0700

    Creates load card method for card generator class

--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -6,6 +6,8 @@ require './lib/turn'
 require './lib/game'
 require './lib/card_generator'
 
+
+
 RSpec.describe CardGenerator do 
   it 'exists and has a cards text file' do 
     card_generator = CardGenerator.new('cards.txt')
@@ -13,10 +15,11 @@ RSpec.describe CardGenerator do
     expect(card_generator).to be_a(CardGenerator)
   end
 
-  describe '#cards' do 
+  describe '#load_cards' do 
     it 'reads the file and creates card objects from the file' do 
       card_generator = CardGenerator.new('cards.txt')
-
+      card_generator.load_cards
+  
       expect(card_generator.cards.length).to eq(52)
     end
   end
