94fc35eaffa3f2c5473ceea89c0a619d3b63ee9f
Chris Crane <chrisjcrane@gmail.com>
2022-12-02 14:56:50 -0700

    rework winner, pass pile_cards, and pass award_spoils

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,22 +8,43 @@ class Turn
     end
 
     def type
-        if player1.deck.cards[0].rank != player2.deck.cards[0].rank
+        if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+            :mutually_assured_destruction
+        elsif player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
             :basic
+        else
+            :war
         end
     end
 
     def winner
-        if player1.deck.cards[0].rank > player2.deck.cards[0].rank
-            player1
+        if type == :mutually_assured_destruction
+            if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+                "No Winner"
+            end
+        elsif type == :basic
+            if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+                player1
+            else
+                player2
+            end
         else
-            player2
+            if player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+                player1
+            else
+                player2
+            end
         end
+
     end
 
     def pile_cards
-        spoils_of_war << player1.deck.cards[0]
-        spoils_of_war << player2.deck.cards[0]
+        @spoils_of_war << player1.deck.remove_card
+        @spoils_of_war << player2.deck.remove_card
+    end
+
+    def award_spoils(winner)
+        winner.deck.cards.concat(spoils_of_war)
     end
 
 
@@ -36,7 +57,7 @@ class Turn
         # occurs when both players’ rank_of_card_at(0) are the same.
     end
 
-    def mutually_assured_destruction
+    def mutually_assured_destruction_turn
         # when both players’
         # rank_of_card_at(0) AND rank_of_card_at(2) are the same.
     end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -104,7 +104,9 @@ RSpec.describe Player do
         player2 = Player.new("Aurora", deck2)
 
         turn = Turn.new(player1, player2)
-        expect(turn.winner).to eq(player1)
+        winner = turn.winner
+
+        expect(winner).to eq(player1)
     end
 
     it "will pile this turns cards into spoils of war" do
@@ -125,5 +127,30 @@ RSpec.describe Player do
 
         turn = Turn.new(player1, player2)
         expect(turn.pile_cards).to eq([card1, card3])
+    end
+
+    it "will award spoils of war to the winner" do
+        card1 = Card.new(:heart, 'Jack', 11)
+        card2 = Card.new(:heart, '10', 10)
+        card3 = Card.new(:heart, '9', 9)
+        card4 = Card.new(:diamond, 'Jack', 11)
+        card5 = Card.new(:heart, '8', 8)
+        card6 = Card.new(:diamond, 'Queen', 12)
+        card7 = Card.new(:heart, '3', 3)
+        card8 = Card.new(:diamond, '2', 2)
+
+        deck1 = Deck.new([card1, card2, card5, card8])
+        deck2 = Deck.new([card3, card4, card6, card7])
+
+        player1 = Player.new("Megan", deck1)
+        player2 = Player.new("Aurora", deck2)
+
+        turn = Turn.new(player1, player2)
+        winner = turn.winner
+        turn.pile_cards
+        turn.award_spoils(winner)
+        
+        # require "pry"; binding.pry
+        expect(player1.deck.cards).to include(card1, card3, card2, card5, card8)
     end
 end
