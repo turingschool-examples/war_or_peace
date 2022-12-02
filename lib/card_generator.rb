94cbde17724d4f3ab029d328c2030f1ba483e186
KaraJH <karas.jones@yahoo.com>
2022-12-02 15:58:05 -0700

    Creates load cards method and adds cards to the array from the file

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,7 +1,22 @@
+#require './lib/cards.txt'
 class CardGenerator 
-  attr_reader :filename 
+  attr_reader :filename, :cards
 
   def initialize(filename)
     @filename = filename
+    @cards =[]
+  end
+
+  def load_cards 
+  
+    card_details = File.readlines(@filename)
+      
+
+    card_details.each_with_index do |line, column| 
+      column = line.split(",")
+      
+      @cards << Card.new(column[1].downcase.to_sym, column[0], column[2].to_i)
+    end
+    @cards
   end
 end
