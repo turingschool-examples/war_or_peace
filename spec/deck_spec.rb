a47d1a6010d34077041397f1cc03f0177a440c04
Nickolas Marquez <nickomarquez7@gmail.com>
2022-12-02 21:47:46 -0700

    Changed high ranking cards method

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe Deck do
         cards = [card1, card2, card3]
 
         deck = Deck.new(cards)
-
+        
         expect(deck.cards).to eq(cards)
     end
 
@@ -35,10 +35,20 @@ RSpec.describe Deck do
         deck = Deck.new(cards)
         expect(deck.rank_of_card_at(0)).to eq(12)
         expect(deck.rank_of_card_at(2)).to eq(14)
-
+        # require "pry"; binding.pry
     end
 
-    it ""
+    it "shows high_ranking_cards" do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+        cards = [card1, card2, card3]
+#  require "pry"; binding.pry
+        deck = Deck.new(cards)
+        expect(deck.high_ranking_cards).to eq([card1, card3])
+
+    
+    end 
 
 
 
