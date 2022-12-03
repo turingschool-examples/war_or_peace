8e7bb46de1ee49b7675d4d4e914efbb820db1b71
Andra Helton <andrahelton@gmail.com>
2022-12-03 13:31:30 -0700

    Add passing test for has MAD turn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,7 +9,9 @@ class Turn
     end
 
     def type
-        if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+        if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+            :mutually_assured_destruction
+        elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
             :war
         else player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
             :basic
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -262,4 +262,26 @@ RSpec.describe Turn do
         expect(player2.deck.cards).to eq([card7, card1, card2, card5, card4, card3, card6])
     end
 
+    it 'has a mutually assured destruction turn' do
+        card1 = Card.new(:heart, 'Jack', 11)
+        card2 = Card.new(:heart, '10', 10)
+        card3 = Card.new(:heart, '9', 9)
+        card4 = Card.new(:diamond, 'Jack', 11)
+        card5 = Card.new(:heart, '8', 8)
+        card6 = Card.new(:diamond, '8', 8)
+        card7 = Card.new(:heart, '3', 3)
+        card8 = Card.new(:diamond, '2', 2)
+
+        deck1 = Deck.new([card1, card2, card5, card8])
+        deck2 = Deck.new([card4, card3, card6, card7])
+
+        player1 = Player.new("Megan", deck1)
+        player2 = Player.new("Aurora", deck2)
+
+        turn = Turn.new(player1, player2)
+
+        expect(turn.type).to eq(:mutually_assured_destruction)
+
+    end
+
 end
