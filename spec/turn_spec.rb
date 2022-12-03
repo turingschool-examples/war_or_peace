b3c435c357c4ac0827eabef8e77a45791f3c90ba
Melony Franchini <meltravelz@gmail.com>
2022-12-03 12:08:44 -0700

    Add 2 tests player1 and player2 cards left in/added to decks

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -221,14 +221,12 @@ RSpec.describe Turn do
         turn = Turn.new(player1, player2)
         winner = turn.winner
         turn.pile_cards
-        # turn.award_spoils(winner)
 
-        # expect(turn.spoils_of_war).to eq([card1, card2, card5, card4, card3, card6])
         expect(turn.spoils_of_war).to eq([card1, card4, card2, card3, card5, card6])
 
     end 
 
-    it "shows spoils_of_war for :war turn type" do
+    it "player1.deck.cards should show cards left in deck" do
         card1 = Card.new(:heart, 'Jack', 11)
         card2 = Card.new(:heart, '10', 10)    
         card3 = Card.new(:heart, '9', 9) 
@@ -250,10 +248,31 @@ RSpec.describe Turn do
         turn.award_spoils(winner)
 
         expect(player1.deck.cards).to eq([card8])
-
     end 
 
+    it "player2.deck.cards should show cards newly added in deck" do
+        card1 = Card.new(:heart, 'Jack', 11)
+        card2 = Card.new(:heart, '10', 10)    
+        card3 = Card.new(:heart, '9', 9) 
+        card4 = Card.new(:diamond, 'Jack', 11) 
+        card5 = Card.new(:heart, '8', 8)
+        card6 = Card.new(:diamond, 'Queen', 12) 
+        card7 = Card.new(:heart, '3', 3)
+        card8 = Card.new(:diamond, '2', 2)    
 
+        deck1 = Deck.new([card1, card2, card5, card8])  
+        deck2 = Deck.new([card4, card3, card6, card7]) 
+        
+        player1 = Player.new("Megan", deck1)    
+        player2 = Player.new("Aurora", deck2)  
+
+        turn = Turn.new(player1, player2)
+        winner = turn.winner
+        turn.pile_cards
+        turn.award_spoils(winner)
+
+        expect(player2.deck.cards).to eq([card7, card1, card4, card2, card3, card5, card6])
+    end 
 
     # require "pry"; binding.pry
 end
