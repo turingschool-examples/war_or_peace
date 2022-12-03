6d5a35aeaf60223b55802dfc30705a058d7df77b
Meredith Trevino <meredithtrevino@gmail.com>
2022-12-03 13:07:13 -0700

    adds updated game lib with correct structure

--- lib/game.rb
+++ lib/game.rb
@@ -1,16 +1,60 @@
-class Game
-    attr_accessor :start
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+# require 'pry'
 
-    def initialize
+class Game
+    attr_accessor :cards, :deck1, :deck2, :player1, :player2, :turn, :start
+
+    def initialize(cards)
+        @cards = cards.shuffle
+        @deck1 = Deck.new(@cards[0..25])
+        @deck2 = Deck.new(@cards[26..51])
+        @player1 = Player.new("Joe", @deck1)
+        @player2 = Player.new("Jess", @deck2)
+        @turn = Turn.new(@player1, @player2)
     end
 
     def start
-        "Welcome to War! (or Peace) This game will be played with 52 cards.
-        The players today are #{player1} and #{player2}. 
-        Type 'GO' to start the game!"
+        # binding.pry
+       p "Welcome to War! (or Peace) This game will be played with 52 cards."
+       p "The players today are #{player1.name} and #{player2.name}."
+       p "Type 'GO' to start the game!"
+       p "------------------------------------------------------------------"
+
+       message = gets.chomp
+         if message == "GO" || "go" || "gO" "Go"
+           begin_game
+         else
+            start
+        end
     end
 
-    # def deck
-    #     [(:heart, '2', 2),(:heart, '3', 3),(:heart, '4', 4),(:heart, '5', 5),(:heart, '6', 6),(:heart, '7', 7),(:heart, '8', 8),(:heart, '9', 9),(:heart, '10', 10),(:heart, 'Jack', 11),(:heart, 'Queen', 12),(:heart, 'King', 13),(:heart, 'Ace', 14),(:diamond, '2', 2),(:diamond, '3', 3),(:diamond, '4', 4),(:diamond, '5', 5),(:diamond, '6', 6),(:diamond, '7', 7),(:diamond, '8', 8),(:diamond, '9', 9),(:diamond, '10', 10),(:diamond, 'Jack', 11),(:diamond, 'Queen', 12),(:diamond, 'King', 13),(:diamond, 'Ace', 14), (:clubs, '2', 2),(:clubs, '3', 3),(:clubs, '4', 4),(:clubs, '5', 5),(:clubs, '6', 6),(:clubs, '7', 7),(:clubs, '8', 8),(:clubs, '9', 9),(:clubs, '10', 10),(:clubs, 'Jack', 11),(:clubs, 'Queen', 12),(:clubs, 'King', 13),(:clubs, 'Ace', 14),(:spade, '2', 2),(:spade, '3', 3),(:spade, '4', 4),(:spade, '5', 5),(:spade, '6', 6),(:spade, '7', 7),(:spade, '8', 8),(:spade, '9', 9),(:spade, '10', 10),(:spade, 'Jack', 11),(:spade, 'Queen', 12),(:spade, 'King', 13),(:spade, 'Ace', 14)]
-    # end
+    def begin_game
+        turn_num = 0
+
+        until turn_num == 1_000_000
+            winner = @turn.winner
+            @turn.pile_cards
+            turn_num +=1
+            type = @turn.type
+            if type == :basic
+                @turn.award_spoils(winner)
+                p "Turn #{turn_num}: #{winner.name} won 2 cards"
+            elsif type == :war
+                @turn.award_spoils(winner)
+                p "Turn #{turn_num}: WAR - #{winner.name} has won 6 cards"
+            else 
+                P "Turn #{turn_num}: *mutually assured destruction* 6 cards removed from play"
+            end
+            
+            check_winner 
+        end
+    end
+
+    def check_winner
+        
+
+    end
+
 end
