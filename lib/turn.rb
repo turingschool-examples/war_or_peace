4917b5e81845090a80be8da5d6ae5579d017e5b1
Melony Franchini <meltravelz@gmail.com>
2022-12-03 12:00:49 -0700

    Fixes :war turn method  & adds :war pile_cards to method

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,12 +10,12 @@ class Turn
     end
 
     def type
-        if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)    
-            :basic
-        elsif  player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)  && player1.deck.rank_of_card_at(2) != player2.deck.rank_of_card_at(2)
-            :war
-        else player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+        if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
             :mutually_assured_destruction
+        elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) != player2.deck.rank_of_card_at(2)
+            :war
+        else player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)    
+            :basic
         end
     end
     
@@ -41,9 +41,13 @@ class Turn
         if type == :basic
             spoils_of_war << player1.deck.remove_card
             spoils_of_war << player2.deck.remove_card
-        # elsif type == :war  
-        #     spoils_of_war << 3.times {player1.deck.remove_card}
-        #     spoils_of_war << 3.times {player2.deck.remove_card}
+        else type == :war  
+            3.times do
+            spoils_of_war << (player1.deck.remove_card)
+            spoils_of_war << (player2.deck.remove_card)
+            end
+            # spoils_of_war << (player1.deck.remove_card).3.times
+            # spoils_of_war << (player2.deck.remove_card).3.times
         # else type == :mutually_assured_destruction
         #     spoils_of_war << player1.deck.remove_card
         #     spoils_of_war << player2.deck.remove_card
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -6,8 +6,7 @@ require './lib/turn'
 
 RSpec.describe Turn do
 
-    # Learned about this technique, but couldn't get it to work quite yet.
-    # Would love to use it in the future!
+    # Learned about this technique but couldn't get it to work quite yet.
     # before do
         # @card1 = Card.new(:heart, 'Jack', 11)
         # @card2 = Card.new(:heart, '10', 10)    
@@ -157,6 +156,55 @@ RSpec.describe Turn do
 
         expect(player1.deck.cards).to eq([card2, card5, card8, card1, card3])
     end
+
+    it "adds :war turn type" do
+        card1 = Card.new(:heart, 'Jack', 11)
+        card2 = Card.new(:heart, '10', 10)    
+        card3 = Card.new(:heart, '9', 9) 
+        card4 = Card.new(:diamond, 'Jack', 11) 
+        card5 = Card.new(:heart, '8', 8)
+        card6 = Card.new(:diamond, 'Queen', 12) 
+        card7 = Card.new(:heart, '3', 3)
+        card8 = Card.new(:diamond, '2', 2)    
+
+        deck1 = Deck.new([card1, card2, card5, card8])  
+        deck2 = Deck.new([card4, card3, card6, card7]) 
+        
+        player1 = Player.new("Megan", deck1)    
+        player2 = Player.new("Aurora", deck2)  
+
+        turn = Turn.new(player1, player2)
+
+        expect(turn.type).to eq(:war)
+    end 
+
+    it "shows spoils_of_war for :war turn type" do
+        card1 = Card.new(:heart, 'Jack', 11)
+        card2 = Card.new(:heart, '10', 10)    
+        card3 = Card.new(:heart, '9', 9) 
+        card4 = Card.new(:diamond, 'Jack', 11) 
+        card5 = Card.new(:heart, '8', 8)
+        card6 = Card.new(:diamond, 'Queen', 12) 
+        card7 = Card.new(:heart, '3', 3)
+        card8 = Card.new(:diamond, '2', 2)    
+
+        deck1 = Deck.new([card1, card2, card5, card8])  
+        deck2 = Deck.new([card4, card3, card6, card7]) 
+        
+        player1 = Player.new("Megan", deck1)    
+        player2 = Player.new("Aurora", deck2)  
+
+        turn = Turn.new(player1, player2)
+        winner = turn.winner
+        turn.pile_cards
+        # turn.award_spoils(winner)
+
+        # expect(turn.spoils_of_war).to eq([card1, card2, card5, card4, card3, card6])
+        expect(turn.spoils_of_war).to eq([card1, card4, card2, card3, card5, card6])
+
+    end 
+
+
 
 
     # require "pry"; binding.pry
