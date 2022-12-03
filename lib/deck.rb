a8bd287760c1e7914cc6c710c1e30079a24913b0
Boston Lowrey <bostonlowrey@gmail.com>
2022-12-02 20:08:41 -0500

    Check percent of high ranking cards

--- lib/deck.rb
+++ lib/deck.rb
@@ -22,4 +22,6 @@ class Deck
   def percent_high_ranking
     ((high_ranking_cards.count.to_f/cards.count.to_f)*100).round(2)
   end
+
+  
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -41,5 +41,16 @@ RSpec.describe Deck do
     
       expect(deck.percent_high_ranking).to eq(66.67)
     end
+
+    it 'Shows percent of rank of cards' do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14) 
+      
+        cards = [card1, card2, card3]
+        deck = Deck.new(cards)
+      
+        
+      end
   end
 end
