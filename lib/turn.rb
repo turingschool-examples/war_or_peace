5441f3312cd79af5adc02378ddd16fd1d4e8ae83
Chris Crane <chrisjcrane@gmail.com>
2022-12-02 17:38:29 -0700

    complete iteration 2

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,32 +35,30 @@ class Turn
                 player2
             end
         end
-
     end
 
     def pile_cards
-        @spoils_of_war << player1.deck.remove_card
-        @spoils_of_war << player2.deck.remove_card
+        if type == :mutually_assured_destruction
+            player1.deck.remove_card
+            player1.deck.remove_card
+            player1.deck.remove_card
+            player2.deck.remove_card
+            player2.deck.remove_card
+            player2.deck.remove_card
+        elsif type == :basic
+            @spoils_of_war << player1.deck.remove_card
+            @spoils_of_war << player2.deck.remove_card
+        else # war
+            3.times do 
+                @spoils_of_war << player1.deck.remove_card
+            end
+            3.times do
+                @spoils_of_war << player2.deck.remove_card
+            end
+        end
     end
 
     def award_spoils(winner)
         winner.deck.cards.concat(spoils_of_war)
     end
-
-
-    def basic_turn
-        #turn is one in which the rank_of_card_at(0)
-        # from the players’ decks are not the same rank.
-    end
-
-    def war_turn
-        # occurs when both players’ rank_of_card_at(0) are the same.
-    end
-
-    def mutually_assured_destruction_turn
-        # when both players’
-        # rank_of_card_at(0) AND rank_of_card_at(2) are the same.
-    end
-
-
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -153,4 +153,58 @@ RSpec.describe Player do
         # require "pry"; binding.pry
         expect(player1.deck.cards).to include(card1, card3, card2, card5, card8)
     end
+
+    it "will properly run war" do
+        card1 = Card.new(:heart, 'Jack', 11)
+        card2 = Card.new(:heart, '10', 10)
+        card3 = Card.new(:club, 'Jack', 11)
+        card4 = Card.new(:diamond, 'Jack', 11)
+        card5 = Card.new(:heart, '8', 8)
+        card6 = Card.new(:diamond, 'Queen', 12)
+        card7 = Card.new(:heart, '3', 3)
+        card8 = Card.new(:diamond, '2', 2)
+
+        deck1 = Deck.new([card1, card2, card5, card8])
+        deck2 = Deck.new([card3, card4, card6, card7])
+
+        player1 = Player.new("Megan", deck1)
+        player2 = Player.new("Aurora", deck2)
+
+        turn = Turn.new(player1, player2)
+        winner = turn.winner
+        expect(turn.type).to eq(:war)
+        turn.pile_cards
+    
+        turn.award_spoils(winner)
+        # require "pry"; binding.pry
+       
+        expect(player2.deck.cards).to include(card3, card4, card6, card7, card1, card2, card5)
+    end
+
+    it "will properly run war" do
+        card1 = Card.new(:heart, 'Jack', 11)
+        card2 = Card.new(:heart, '10', 10)
+        card3 = Card.new(:club, 'Jack', 11)
+        card4 = Card.new(:diamond, 'Jack', 11)
+        card5 = Card.new(:heart, '8', 8)
+        card6 = Card.new(:diamond, '8', 8)
+        card7 = Card.new(:heart, '3', 3)
+        card8 = Card.new(:diamond, '2', 2)
+
+        deck1 = Deck.new([card1, card2, card5, card8])
+        deck2 = Deck.new([card3, card4, card6, card7])
+
+        player1 = Player.new("Megan", deck1)
+        player2 = Player.new("Aurora", deck2)
+
+        turn = Turn.new(player1, player2)
+        winner = turn.winner
+        expect(turn.type).to eq(:mutually_assured_destruction)
+        turn.pile_cards
+    
+        # turn.award_spoils(winner)
+        # require "pry"; binding.pry
+       
+        expect(player2.deck.cards).to include(card7)
+    end
 end
