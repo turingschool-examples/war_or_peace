4658f75067e7039762a3a5984b084436c79f5007
Amy Spears <amspears007@gmail.com>
2022-12-02 19:15:56 -0700

    Add passing 'exists' test

---
+++ lib/player.rb
@@ -0,0 +1,9 @@
+class Player
+
+    attr_reader :name, :deck
+    
+    def initialize(name, deck)
+        @name = name
+        @deck = deck
+    end
+end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -8,7 +8,7 @@ it 'exists' do
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3) 
     card3 = Card.new(:heart, 'Ace', 14)
-# require 'pry'; binding.pry
+
     cards = [card1, card2, card3]
 
     deck = Deck.new(cards)
