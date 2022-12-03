a7605535f507a39fcaf4b2753bfeb022d2218b89
Chris Crane <chrisjcrane@gmail.com>
2022-12-02 18:59:40 -0700

    add runner file with all cards hard coded in

---
+++ lib/war_or_peace_runner.rb
@@ -0,0 +1,68 @@
+# card = Card.new(:diamond, 'Queen', 12)
+
+suit = 
+
+i = 1
+k = 39
+
+13.times do
+    i+=1
+    k+=1
+    puts "card#{k} = Card.new(:club, '#{i}', #{i})"
+end
+
+# card1 = Card.new(:diamond, '2', 2)
+# card2 = Card.new(:diamond, '3', 3)
+# card3 = Card.new(:diamond, '4', 4)
+# card4 = Card.new(:diamond, '5', 5)
+# card5 = Card.new(:diamond, '6', 6)
+# card6 = Card.new(:diamond, '7', 7)
+# card7 = Card.new(:diamond, '8', 8)
+# card8 = Card.new(:diamond, '9', 9)
+# card9 = Card.new(:diamond, '10', 10)
+# card10 = Card.new(:diamond, '11', 11)
+# card11 = Card.new(:diamond, '12', 12)
+# card12 = Card.new(:diamond, '13', 13)
+# card13 = Card.new(:diamond, '14', 14)
+
+# card14 = Card.new(:heart, '2', 2)
+# card15 = Card.new(:heart, '3', 3)
+# card16 = Card.new(:heart, '4', 4)
+# card17 = Card.new(:heart, '5', 5)
+# card18 = Card.new(:heart, '6', 6)
+# card19 = Card.new(:heart, '7', 7)
+# card20 = Card.new(:heart, '8', 8)
+# card21 = Card.new(:heart, '9', 9)
+# card22 = Card.new(:heart, '10', 10)
+# card23 = Card.new(:heart, '11', 11)
+# card24 = Card.new(:heart, '12', 12)
+# card25 = Card.new(:heart, '13', 13)
+# card26 = Card.new(:heart, '14', 14)
+
+# card27 = Card.new(:spade, '2', 2)
+# card28 = Card.new(:spade, '3', 3)
+# card29 = Card.new(:spade, '4', 4)
+# card30 = Card.new(:spade, '5', 5)
+# card31 = Card.new(:spade, '6', 6)
+# card32 = Card.new(:spade, '7', 7)
+# card33 = Card.new(:spade, '8', 8)
+# card34 = Card.new(:spade, '9', 9)
+# card35 = Card.new(:spade, '10', 10)
+# card36 = Card.new(:spade, '11', 11)
+# card37 = Card.new(:spade, '12', 12)
+# card38 = Card.new(:spade, '13', 13)
+# card39 = Card.new(:spade, '14', 14)
+
+# card40 = Card.new(:club, '2', 2)
+# card41 = Card.new(:club, '3', 3)
+# card42 = Card.new(:club, '4', 4)
+# card43 = Card.new(:club, '5', 5)
+# card44 = Card.new(:club, '6', 6)
+# card45 = Card.new(:club, '7', 7)
+# card46 = Card.new(:club, '8', 8)
+# card47 = Card.new(:club, '9', 9)
+# card48 = Card.new(:club, '10', 10)
+# card49 = Card.new(:club, '11', 11)
+# card50 = Card.new(:club, '12', 12)
+# card51 = Card.new(:club, '13', 13)
+# card52 = Card.new(:club, '14', 14)
