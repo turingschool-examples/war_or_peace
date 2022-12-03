8edf5892befc0bbed73ee85b2f14c9b7325d5a2b
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-12-03 10:25:31 -0800

    Update turn to make game run

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,43 +7,50 @@ class Turn
         @player2 = player2
         @spoils_of_war = []
     end
-
+   
     def type
-        if player1.deck.cards[0].rank != player2.deck.cards[0].rank then :basic
-        elsif player1.deck.cards[0].rank == player2.deck.cards[0].rank && player1.deck.cards[2].rank == player2.deck.cards[2].rank then :mutually_assured_destruction
-        elsif player1.deck.cards[0].rank == player2.deck.cards[0].rank then :war
+        if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)&& player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+            :mutually_assured_destruction
+        elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+            :war
+        elsif player1.deck.rank_of_card_at(0)  != player2.deck.rank_of_card_at(0)
+            :basic
         end
     end
 
     def winner
-        if type == :basic then 
-            if player1.deck.cards[0].rank > player2.deck.cards[0].rank then player1
-            else player2
+        if type == :basic
+            if player1.deck.cards[0].rank > player2.deck.cards[0].rank 
+                player1
+            else 
+                player2
             end 
-        elsif type == :war then
-            if player1.deck.cards[2].rank > player2.deck.cards[2].rank then player1
-            else player2
+        elsif type == :war
+            if player1.deck.cards[2].rank > player2.deck.cards[2].rank 
+                player1
+            else 
+                player2
             end
-        elsif type == :mutually_assured_destruction then "No Winner"
+        elsif type == :mutually_assured_destruction  
+            "No Winner"
         end 
     end 
 
     def pile_cards
-        if type == :basic then
+        if type == :basic 
             spoils_of_war << player1.deck.cards.shift
             spoils_of_war << player2.deck.cards.shift
-        elsif type == :war then
+        elsif type == :war 
             spoils_of_war.concat(player1.deck.cards.shift(3))
             spoils_of_war.concat(player2.deck.cards.shift(3))
-        elsif type == :mutually_assured_destruction then
+        elsif type == :mutually_assured_destruction 
             player1.deck.cards.shift(3)
             player2.deck.cards.shift(3)
         end
     end
 
     def award_spoils(winner)
-        if type == :basic || type == :war
             winner.deck.cards.concat(spoils_of_war)
-        end
+            spoils_of_war.clear
     end 
 end
