13c66bc840b6e27d9180c33b9498d13624294465
Adam Bailey <ab67319@gmail.com>
2022-12-03 13:49:49 -0700

    Create start method

--- lib/game.rb
+++ lib/game.rb
@@ -6,33 +6,35 @@ class Game
     def initialize(player1, player2)
         @player1 = player1
         @player2 = player2
-        @turn_count = 1
+        @turn_count = 0
     end
 
     def game_ended?
-        return true if @player1.deck.cards.length == 0 || @player2.deck.cards.length == 0
+        return true if @player1.deck.cards.empty? || @player2.deck.cards.empty?
         return false
     end
 
     def play_turn
         turn = Turn.new(@player1, @player2)
-        
+        @turn_count += 1
         winner = turn.winner
+
         turn.pile_cards
         num_spoils = turn.spoils_of_war.count
-        turn.award_spoils(winner).class
 
+        turn.award_spoils(winner)
         puts "Turn #{turn_count}: #{winner.name} won #{num_spoils} cards"
+
+        winner
     end
 
     def start
-        play_turn
-
-        until game_ended? == true || @turn_count == 1_000_000
-            play_turn
+        until game_ended? == true || @turn_count == 1_000#_000
+            winner = play_turn
         end
-
         
+        return "---- DRAW ----" if @turn_count == 1_000#_000
+        return "*~*~*~* #{winner.name} has won the game! *~*~*~*"
     end
 
 end
