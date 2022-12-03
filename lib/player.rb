02e4f44504d8317c47350502a239ba66d5d9e374
James Taylor <jrtaylor0614@gmail.com>
2022-12-03 16:34:31 -0700

    Initial commit, all of iteration 1,'tests exists'

---
+++ lib/player.rb
@@ -0,0 +1,13 @@
+class Player
+    attr_reader :name, :deck
+    def initialize(name, deck)
+        @name = name
+        @deck = deck
+    end
+# require 'pry'; binding.pry
+    def has_lost? 
+        has_cards = false
+
+    end
+
+end
