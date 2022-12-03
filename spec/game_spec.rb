66cfd8cd934ba979a1ebb8c3655c3024e0d0f2f4
Elle Majors <hellolovely@ellemajors>
2022-12-03 13:51:29 -0700

    test: #full_deck

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,11 +7,19 @@ require './lib/game'
 
 describe Game do
   describe '#initialize' do
-    it 'exists and has empty full_deck' do
+    it 'exists' do
       game = Game.new
 
       expect(game).to be_a(Game)
-      expect(game.full_deck).to eq([])
     end
   end
+
+  describe '#full_deck' do
+    it 'has full_deck' do
+      game = Game.new
+
+      expect(game.full_deck.count).to eq(52)
+    end
+  end
+  
 end
