cd35d05249b50450acd3e864311e90225d2493c5
Amy Spears <amspears007@gmail.com>
2022-12-03 14:36:31 -0700

    Add passing 5th test pile_cards

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,5 +36,23 @@ class Turn
         return 'No Winner'    
         end
     end
+
+    def pile_cards
+        if  type == :basic
+                spoils_of_war << player1.deck.remove_card
+                spoils_of_war << player2.deck.remove_card
+        elsif type == :war
+                3.times do
+                spoils_of_war << player1.deck.remove_card
+                spoils_of_war << player2.deck.remove_card
+            end
+        else
+            type == :mutually_assured_destruction
+                3.times do
+                player1.deck.remove_card
+                player2.deck.remove_card
+            end
+        end
+    end
 end
 
