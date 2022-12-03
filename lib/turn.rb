e41d61691d7a170a05028447fba4fc9278c5e4e6
Caleb Thomas <cjthomas00@comcast.net>
2022-12-02 17:51:48 -0700

    complete basic turn code and test.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,51 @@
 class Turn
+   attr_reader :player1, :player2, :spoils_of_war
 
     def initialize(player1, player2)
         @player1 = player1
         @player2 = player2
+        @spoils_of_war = []
+    end
+
+    def basic 
+        player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
+    end
+
+    def war
+        player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
+    end
+
+    def mutually_assured_destruction
+        player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+    end
+
+    def type
+        if basic
+            :basic
+        elsif war
+            :war
+        elsif mutually_assured_destruction
+            :mutually_assured_destruction
+        end
+    end
+    
+    def winner
+       if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+            player1 
+       elsif player1.deck.rank_of_card_at(0) < player2.deck.rank_of_card_at(0)
+            player2
+       end
+    end
+    
+    def pile_cards
+        @spoils_of_war << player1.deck.cards.shift  
+        @spoils_of_war << player2.deck.cards.shift
+    end
+
+    def award_spoils(winner)
+        winner.deck.cards << @spoils_of_war
+        winner.deck.cards.flatten!
+        @spoils_of_war = []
+        #require 'pry'; binding.pry
     end
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -5,7 +5,7 @@ require './lib/player'
 require './lib/turn'
 
 RSpec.describe Turn do
-    it "exists" do
+    it "can have a basic turn" do
         card1 = Card.new(:heart, 'Jack', 11)
         card2 = Card.new(:heart, '10', 10)
         card3 = Card.new(:heart, '9', 9)
@@ -14,7 +14,6 @@ RSpec.describe Turn do
         card6 = Card.new(:diamond, 'Queen', 12)
         card7 = Card.new(:heart, '3', 3)
         card8 = Card.new(:diamond, '2', 2)
-        #require 'pry'; binding.pry
         
         deck1 = Deck.new([card1, card2, card5, card8]) 
         deck2 = Deck.new([card3, card4, card6, card7])
@@ -22,22 +21,51 @@ RSpec.describe Turn do
         player1 = Player.new("Megan", deck1)
         player2 = Player.new("Aurora", deck2)
         #require 'pry'; binding.pry
-        turn = Turn.new(player1, player2) 
-        #expect(player).to be_an_instance_of(Player)
+        turn = Turn.new(player1, player2)
+        
+        #require 'pry'; binding.pry
+        expect(turn.player1).to eq(player1)
+        expect(turn.player2).to eq(player2)
+        expect(turn.spoils_of_war).to eq([])
+        expect(turn.type).to eq(:basic)
     end
 
-    
-    # it "hasn't lost by default" do
-    #     card1 = Card.new(:diamond, 'Queen', 12)
-    #     card2 = Card.new(:spade, '3', 3)
-    #     card3 = Card.new(:heart, 'Ace', 14)
-    #     cards = [card1, card2, card3]
+    it "can have a winner" do
+        card1 = Card.new(:heart, 'Jack', 11)
+        card2 = Card.new(:heart, '10', 10)
+        card3 = Card.new(:heart, '9', 9)
+        card4 = Card.new(:diamond, 'Jack', 11)
+        card5 = Card.new(:heart, '8', 8) 
+        card6 = Card.new(:diamond, 'Queen', 12)
+        card7 = Card.new(:heart, '3', 3)
+        card8 = Card.new(:diamond, '2', 2)
         
-    #     deck = Deck.new(cards)
-    #     player = Player.new('Caleb', deck)
-    #     #require 'pry'; binding.pry
-    #     expect(player.has_lost?).to eq(false)  
-    # end
+        deck1 = Deck.new([card1, card2, card5, card8]) 
+        deck2 = Deck.new([card3, card4, card6, card7])
+        
+        player1 = Player.new("Megan", deck1)
+        player2 = Player.new("Aurora", deck2)
+        
+        turn = Turn.new(player1, player2)
+        
+        expect(turn.player1).to eq(player1)
+        expect(turn.player2).to eq(player2)
+        #require 'pry'; binding.pry
+        expect(turn.spoils_of_war).to eq([])
+        expect(turn.type).to eq(:basic)
+
+        expect(turn.winner).to eq(player1)
+        #require 'pry'; binding.pry
+        winner = turn.winner
+        expect(turn.pile_cards).to eq([card1, card3])
+        expect(turn.spoils_of_war).to eq([card1, card3])
+
+        turn.award_spoils(winner)
+
+        expect(player1.deck.cards).to eq([card2, card5, card8, card1, card3])
+        expect(player2.deck.cards).to eq([card4, card6, card7]) 
+
+    end
     
     # it "can lose" do
     #     card1 = Card.new(:diamond, 'Queen', 12)
