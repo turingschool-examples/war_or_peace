4a488c7a26fe2e639253d2cfd5fed5fea0a18825
Adam Bailey <ab67319@gmail.com>
2022-12-02 18:07:31 -0700

    Update type and winner methods to use Deck class rank_of_card_at method instead of hard code

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,21 +10,21 @@ class Turn
     end
     
     def type
-        if @player1.deck.cards[0].rank == @player2.deck.cards[0].rank && @player1.deck.cards[2].rank == @player2.deck.cards[2].rank
+        if @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
             return :mutually_assured_destruction
-        elsif @player1.deck.cards[0].rank != @player2.deck.cards[0].rank 
+        elsif @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0) 
             return :basic
-        elsif @player1.deck.cards[0].rank == @player2.deck.cards[0].rank
+        elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
             return :war
         end
     end
 
     def winner
         if type == :basic
-            return @player1 if @player1.deck.cards[0].rank > @player2.deck.cards[0].rank
+            return @player1 if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
             return @player2
         elsif type == :war
-            return @player1 if @player1.deck.cards[2].rank > @player2.deck.cards[2].rank
+            return @player1 if @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
             return @player2
         end
         return "No Winner"
