8bc7da3b0a0f66486d7c6aea1ac3de76fbf97861
Adam Bailey <ab67319@gmail.com>
2022-12-02 22:04:58 -0700

    Add turn_count instance variable and play_turn output

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,12 @@
 class Game 
     attr_reader :player1,
-                :player2 
+                :player2,
+                :turn_count
 
     def initialize(player1, player2)
         @player1 = player1
         @player2 = player2
+        @turn_count = 1
     end
 
     def game_ended?(player)
@@ -14,9 +16,14 @@ class Game
 
     def play_turn
         turn = Turn.new(@player1, @player2)
+        
         winner = turn.winner
-
         turn.pile_cards
-        turn.award_spoils(winner)
+        num_spoils = turn.spoils_of_war.count
+        turn.award_spoils(winner).class
+
+        puts "Turn #{turn_count}: #{winner.name} won #{num_spoils} cards"
     end
+
+
 end
