ad1c9fd9f01560435b54637403599ab7d271a1f1
Melony Franchini <meltravelz@gmail.com>
2022-12-02 19:11:17 -0700

    Add test type of turn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,8 +10,8 @@ class Turn
     end
 
     def type
-        if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
-            :basic
+        if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)    
+        :basic
         else 
             :war
         end
