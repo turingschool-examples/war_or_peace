bd074bbacc5c0b170edca7e530cca5938b297116
Melony Franchini <meltravelz@gmail.com>
2022-12-02 18:06:47 -0700

    Add test spoils_of_war

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,12 +1,16 @@
 class Turn
     attr_reader :player1,
-                :player2
+                :player2,
+                :spoils_of_war
 
     def initialize(player_1, player_2)
         @player1 = player_1
         @player2 = player_2
+        @spoils_of_war = []
     end
 
+    # def @spoils_of_war = []
+    # end
 
-
+    
 end
