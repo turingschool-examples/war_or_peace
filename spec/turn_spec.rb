70a1848beb9f67ceded7664938451f17d85fcef7
Caleb Thomas <cjthomas00@comcast.net>
2022-12-02 21:34:46 -0700

    complete mutually assured destruction code and tests.

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -102,6 +102,34 @@ RSpec.describe Turn do
         #require 'pry'; binding.pry
         expect(player1.deck.cards).to eq([card8])
         expect(player2.deck.cards).to eq([card7, card1, card4, card2, card3, card5, card6])    
+
+     end
+
+     it "can have mutual destruction" do
+        card1 = Card.new(:heart, 'Jack', 11)
+        card2 = Card.new(:heart, '10', 10)
+        card3 = Card.new(:heart, '9', 9)
+        card4 = Card.new(:diamond, 'Jack', 11)
+        card5 = Card.new(:heart, '8', 8) 
+        card6 = Card.new(:diamond, '8', 8)
+        card7 = Card.new(:heart, '3', 3)
+        card8 = Card.new(:diamond, '2', 2)
         
+        deck1 = Deck.new([card1, card2, card5, card8]) 
+        deck2 = Deck.new([card4, card3, card6, card7])
+        
+        player1 = Player.new("Megan", deck1)
+        player2 = Player.new("Aurora", deck2)
+        
+        turn = Turn.new(player1, player2)
+        #require 'pry'; binding.pry
+        turn.type
+        winner = turn.winner
+
+        turn.pile_cards
+        expect(turn.spoils_of_war).to eq([])
+        #require 'pry'; binding.pry
+        expect(player1.deck.cards).to eq([card8])
+        expect(player2.deck.cards).to eq([card7])
      end
  end
