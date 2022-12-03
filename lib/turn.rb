91265389e7ea70818a6e00b73db6df21baec7237
Melony Franchini <meltravelz@gmail.com>
2022-12-02 17:54:56 -0700

    Comment out before do/end in spec file

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,6 @@
 class Turn
+    attr_reader :player1,
+                :player2
 
     def initialize(player_1, player_2)
         @player1 = player_1
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -5,27 +5,47 @@ require './lib/player'
 require './lib/turn'
 
 RSpec.describe Turn do
-    before do
-        @card1 = Card.new(:heart, 'Jack', 11)
-        @card2 = Card.new(:heart, '10', 10)    
-        @card3 = Card.new(:heart, '9', 9) 
-        @card4 = Card.new(:diamond, 'Jack', 11) 
-        @card5 = Card.new(:heart, '8', 8)
-        @card6 = Card.new(:diamond, 'Queen', 12) 
-        @card7 = Card.new(:heart, '3', 3)
-        @card8 = Card.new(:diamond, '2', 2)    
 
-        @deck1 = Deck.new([@card1, @card2, @card5, @card8])  
-        @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+    #Learned about this technique, would love to use it in the future!
+    # before do
+        # @card1 = Card.new(:heart, 'Jack', 11)
+        # @card2 = Card.new(:heart, '10', 10)    
+        # @card3 = Card.new(:heart, '9', 9) 
+        # @card4 = Card.new(:diamond, 'Jack', 11) 
+        # @card5 = Card.new(:heart, '8', 8)
+        # @card6 = Card.new(:diamond, 'Queen', 12) 
+        # @card7 = Card.new(:heart, '3', 3)
+        # @card8 = Card.new(:diamond, '2', 2)    
 
-        @player1 = Player.new("Megan", @deck1)    
-        @player2 = Player.new("Aurora", @deck2)  
+        # @deck1 = Deck.new([@card1, @card2, @card5, @card8])  
+        # @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+
+        # @player1 = Player.new("Megan", @deck1)    
+        # @player2 = Player.new("Aurora", @deck2)  
         
-        @turn = Turn.new(@player1, @player2)
-    end
+        # @turn = Turn.new(@player1, @player2)
+    # end
 
     it "exsists" do
-        expect(@turn).to be_an_instance_of(Turn)
+        card1 = Card.new(:heart, 'Jack', 11)
+        card2 = Card.new(:heart, '10', 10)    
+        card3 = Card.new(:heart, '9', 9) 
+        card4 = Card.new(:diamond, 'Jack', 11) 
+        card5 = Card.new(:heart, '8', 8)
+        card6 = Card.new(:diamond, 'Queen', 12) 
+        card7 = Card.new(:heart, '3', 3)
+        card8 = Card.new(:diamond, '2', 2)    
+
+        deck1 = Deck.new([card1, card2, card5, card8])  
+        deck2 = Deck.new([card3, card4, card6, card7])
+
+        player1 = Player.new("Megan", deck1)    
+        player2 = Player.new("Aurora", deck2)  
+        
+        turn = Turn.new(player1, player2)
+
+        expect(turn).to be_an_instance_of(Turn)
+        require 'pry'; binding.pry
     end
 
 
