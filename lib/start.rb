d123ada8ef9bcc95fb803134fffe322e045c4883
Hady Matar <hadyematar@gmail.com>
2022-12-03 13:39:28 -0700

    Completed iteration 3 clean

--- lib/start.rb
+++ lib/start.rb
@@ -6,40 +6,41 @@ def initialize(player1, player2)
     @player1 = player1 
     @player2 = player2
     @turn = Turn.new(@player1, player2) 
-    # require 'pry'; binding.pry
 end 
 
 def start 
-    number = 0
-    1000.times do
-
+    @number = 0
+    until @player1.has_lost? || @player2.has_lost? || @number == 1_000_000
         @turn.type 
-        # require 'pry'; binding.pry
         winnerofturn = @turn.winner 
         @turn.pile_cards
         @turn.award_spoils(winnerofturn)
-        number += 1
-
+        @number += 1
             if @turn.cardtype == :basic
-                puts "BASIC"
+                puts "Type of Turn: BASIC"
             elsif @turn.cardtype == :war
-                puts "War"
+                puts "Type of Turn:War"
             elsif @turn.cardtype == :mutually_assured_destruction
-                puts "MAD"
+                puts "Type of Turn: MAD"
             end 
-
         puts "BREAK"
         puts "NEXT TURN IS BELOW"
         puts ""
         puts ""
         puts ""
-        puts ""
-
-        puts "Turn number #{number}
+        
+        puts "Turn number #{@number}
         player 1 cards: #{@player1.deck.cards.count}
         player 2 cards: #{@player2.deck.cards.count}"
         end 
 
+        if @player1.has_lost?
+            p "Sorry #{@player1.name} you have lost this game. Aun su amor para ti nunca mas sea para infinito, #{@player2.name} todavia te ama demasciadio."
+        elsif @player2.has_lost? 
+            p "Sorry #{@player2.name} you have lost this game. Aun su amor para ti nunca mas sea para infinito, #{@player1.name} todavia te ama demasciadio."
+        elsif @number == 1_000_000
+            p "This game will end with a draw."
+        end 
 
 
 end 
--- lib/turn.rb
+++ lib/turn.rb
@@ -3,7 +3,6 @@ class Turn
                 :player2, 
                 :cardtype
                 
-
     def initialize(player1, player2)
          @player1 = player1 
          @player2 = player2
@@ -11,34 +10,26 @@ class Turn
          @cardtype = nil 
     end
 
-#     def cardtype
-#      @cardtype
-#     end
-
     def type
      cardtype = nil 
           if @player1.deck.cards.count < 3 || @player2.deck.cards.count < 3
                cardtype = :basic
-               # require 'pry'; binding.pry
 
           elsif (@player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)) && (@player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2))
-               # require 'pry'; binding.pry
                cardtype = :mutually_assured_destruction
 
           elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
-               require 'pry'; binding.pry
                cardtype = :war 
 
           else 
-               require 'pry'; binding.pry
                cardtype = :basic
           end
+
           @cardtype = cardtype 
     end
 
     def winner
         if @cardtype == :basic 
-          require 'pry'; binding.pry
                if @player1.deck.cards[0].rank > @player2.deck.cards[0].rank
                     return @player1
                else
@@ -46,7 +37,6 @@ class Turn
                end
                
           elsif @cardtype == :war  
-               # require 'pry'; binding.pry
                if @player1.deck.cards[2].rank > @player2.deck.cards[2].rank
                     return @player1
                else
@@ -54,52 +44,32 @@ class Turn
                end
 
           elsif @cardtype == :mutually_assured_destruction
-               # require 'pry'; binding.pry
-               winner = "No Winner"
+                    winner = "No Winner"
         end 
-        
      end 
 
     def pile_cards
-     require 'pry'; binding.pry
           if @cardtype == :basic
-               # require 'pry'; binding.pry
                @spoils_of_war << @player1.deck.remove_card 
                @spoils_of_war << @player2.deck.remove_card 
-          end 
-
-          if  @cardtype == :mutually_assured_destruction 
+          elsif @cardtype == :mutually_assured_destruction 
                (@player1.deck.cards).replace(@player1.deck.cards.drop(3))
                (@player2.deck.cards).replace(@player2.deck.cards.drop(3))
-               # require 'pry'; binding.pry
-          end 
-
-          if  @cardtype == :war 
+          elsif @cardtype == :war 
                3.times do 
                @spoils_of_war << @player1.deck.remove_card 
                @spoils_of_war << @player2.deck.remove_card 
-               require 'pry'; binding.pry
                end 
           end 
-          end 
+     end 
      
 
     def award_spoils(winner)
- require 'pry'; binding.pry
-         if @cardtype == :mutually_assured_destruction
-               # "do nothing"
-               require 'pry'; binding.pry
-         end 
-           
-          if @cardtype == :basic 
-               require 'pry'; binding.pry
+          if @cardtype == :basic || @cardtype == :war
                (winner.deck.cards).concat(@spoils_of_war)
+               (@player1.deck.cards).shuffle!
+               (@player2.deck.cards).shuffle!
                @spoils_of_war.clear
           end 
-          if @cardtype == :war 
-               (winner.deck.cards).concat(@spoils_of_war)
-               # require 'pry'; binding.pry
-               @spoils_of_war.clear 
-          end
      end 
 end
