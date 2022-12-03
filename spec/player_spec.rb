1cab1cc4ca6febf541f7e2b3535b4484164447e4
Amy Spears <amspears007@gmail.com>
2022-12-02 19:47:00 -0700

    Add passing 'has lost?' test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -19,3 +19,25 @@ RSpec.describe Player do
 
     end
 end
+
+RSpec.describe Player do
+    it 'has lost?' do
+
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)    
+        card3 = Card.new(:heart, 'Ace', 14)    
+
+        deck = Deck.new([card1, card2, card3])
+
+        player = Player.new('Clarisa', deck)
+
+        expect(player.has_lost?).to eq(false)
+        expect(player.deck.remove_card).to eq(card1)
+        expect(player.has_lost?).to eq(false)
+        expect(player.deck.remove_card).to eq(card2)
+        expect(player.has_lost?).to eq(false)
+        expect(player.deck.remove_card).to eq(card3)
+        expect(player.has_lost?).to eq(true)
+        expect(player.deck.cards).to eq([])
+    end
+end
