f15a0bcbff5aa58abaa32d02dba5c880b4e758dc
Meredith Trevino <meredithtrevino@gmail.com>
2022-12-02 17:38:09 -0700

    adding more tests for winner

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -127,7 +127,7 @@ RSpec.describe Player do
         expect(turn.type).to eq(:mutually_assured_destruction)
     end
 
-    it 'announces winner' do
+    it 'announces winner for :basic' do
         card1 = Card.new(:heart, 'Jack', 11)
         card2 = Card.new(:heart, '10', 10)
         card3 = Card.new(:heart, '9', 9)
@@ -147,6 +147,45 @@ RSpec.describe Player do
         expect(turn.winner).to eq(player1)
     end
 
+    it 'announces winner for :war' do
+        card1 = Card.new(:heart, 'Jack', 11)
+        card2 = Card.new(:heart, '10', 10)
+        card3 = Card.new(:spade, 'Jack', 11)
+        card4 = Card.new(:diamond, 'Jack', 11)
+        card5 = Card.new(:heart, '8', 8)  
+        card6 = Card.new(:diamond, 'Queen', 12) 
+        card7 = Card.new(:heart, '3', 3) 
+        card8 = Card.new(:diamond, '2', 2) 
+
+        deck1 = Deck.new([card1, card2, card5, card8])
+        deck2 = Deck.new([card3, card4, card6, card7])
+
+        player1 = Player.new("Megan", deck1) 
+        player2 = Player.new("Aurora", deck2)  
+
+        turn = Turn.new(player1, player2) 
+        expect(turn.winner).to eq(player2)
+    end
+
+    it 'announces winner for :mul. as. destruction ' do
+        card1 = Card.new(:heart, 'Jack', 11)
+        card2 = Card.new(:heart, '10', 10)
+        card3 = Card.new(:spade, 'Jack', 11)
+        card4 = Card.new(:diamond, 'Jack', 11)
+        card5 = Card.new(:heart, 'Queen', 12)  
+        card6 = Card.new(:diamond, 'Queen', 12) 
+        card7 = Card.new(:heart, '3', 3) 
+        card8 = Card.new(:diamond, '2', 2) 
+
+        deck1 = Deck.new([card1, card2, card5, card8])
+        deck2 = Deck.new([card3, card4, card6, card7])
+
+        player1 = Player.new("Megan", deck1) 
+        player2 = Player.new("Aurora", deck2)  
+
+        turn = Turn.new(player1, player2) 
+        expect(turn.winner).to eq("No winner")
+    end
     it 'piles cards for :basic' do
         card1 = Card.new(:heart, 'Jack', 11)
         card2 = Card.new(:heart, '10', 10)
