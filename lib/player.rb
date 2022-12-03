6c24a5ea481117c2028fc71b0372e4dba45f2927
Boston Lowrey <bostonlowrey@gmail.com>
2022-12-03 16:12:06 -0500

    Will check if player has lost

--- lib/player.rb
+++ lib/player.rb
@@ -5,4 +5,12 @@ class Player
         @name = name
         @deck = deck
     end
+
+    def has_lost?
+        if (deck.cards.count) >= 1
+            false
+        else
+            true
+        end
+    end
 end
