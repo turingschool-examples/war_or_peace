20d05bcb5be5b6fb1b7a5dc1bf68999d5ec91293
Matt Enyeart <menyeart1@gmail.com>
2022-12-03 15:58:56 -0700

    Implement special rules via pile cards and type

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,7 +9,7 @@ class Turn
 
   def type
     if player1.deck.cards.length < 3 || player2.deck.cards.length < 3
-      return :basic
+      return :special
     elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
       return :mutually_assured_destruction
     elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
@@ -20,7 +20,9 @@ class Turn
   end
 
   def winner
-    if type == :basic
+    if type == :special
+      player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0) ? player1 : player2
+    elsif type == :basic
       player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0) ? player1 : player2
     elsif type == :war
       player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2) ? player1 : player2
@@ -40,7 +42,12 @@ class Turn
       lost_cards.each {|card| @spoils_of_war.unshift(card)}
       lost_cards = player2.deck.cards.shift(3)
       lost_cards.each {|card| @spoils_of_war.unshift(card)}
-    else
+    elsif type == :special
+      lost_cards = player1.deck.cards.shift(2)
+      lost_cards.each {|card| @spoils_of_war.unshift(card)}
+      lost_cards = player2.deck.cards.shift(2)
+      lost_cards.each {|card| @spoils_of_war.unshift(card)}
+    else 
       player1.deck.cards.shift(3)
       player2.deck.cards.shift(3)
     end   
