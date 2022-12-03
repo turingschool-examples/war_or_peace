7521d1e6c32f7dcff22f6eff122137852a92f7de
Adam Bailey <ab67319@gmail.com>
2022-12-03 00:05:43 -0700

    Add welcome message, user input functionality and game start

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -2,6 +2,7 @@ require './lib/card'
 require './lib/deck'
 require './lib/player'
 require './lib/turn'
+require './lib/game'
 
 # Create 52 Cards and shuffle
 deck = []
@@ -21,5 +22,27 @@ end
 deck.shuffle!
 
 # Create two players
-player1 = Player.new("Megan", deck.shift(26))
-player2 = Player.new("Aurora", deck)
+player1 = Player.new("Megan", Deck.new(deck.shift(26)))
+player2 = Player.new("Aurora", Deck.new(deck))
+
+# Create game Start
+game = Game.new(player1, player2)
+
+# Print welcome message and prompt user to start game
+puts
+puts "Welcome to War! (or Peace) This game will be played with 52 cards."
+puts "The players today are #{player1.name} and #{player2.name}"
+puts "Type 'GO' to start the game!"
+puts "------------------------------------------------------------------"
+input = gets.chomp!
+
+# If user input is 'GO' start game, otherwise prompt for valid input
+until input == "GO"
+    puts "Please enter 'GO' to start!"
+    input = gets.chomp!
+end
+
+# Start game and print results
+p game.start
+
+    
