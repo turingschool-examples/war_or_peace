67c7f393de3d11d2af37a5a3d5902889c21c9ccf
Amy Spears <amspears007@gmail.com>
2022-12-03 11:56:00 -0700

    Add passing 3rd test type

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,14 +4,18 @@ class Turn
     def initialize(player1, player2)
         @player1 = player1
         @player2 = player2
-        # require 'pry'; binding.pry
         @spoils_of_war = []
        
     end
 
-    # def type
-    #     :basic
-    #     player1 rank_of_card_at(0) != player2 rank_of_card_at(0)
-    # end
+    def type
+        if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+        :mutually_assured_destruction
+        elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)    
+        :war
+        else player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
+        :basic
+        end
+    end
 end
 
