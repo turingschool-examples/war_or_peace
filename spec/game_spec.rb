ed2f5810603463f9eb8adffa8f1912693cbbcd36
Matt Enyeart <menyeart1@gmail.com>
2022-12-03 11:55:24 -0700

    Remove tests attempting to compare console output

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -40,28 +40,4 @@ describe Game do
       game = Game.new(turn)
       expect(game.game_over?).to eql(nil)
     end
-
-    it "asks for user input" do
-      card1 = Card.new(:heart, 'Jack', 11)
-      card2 = Card.new(:heart, '10', 10)    
-      card3 = Card.new(:heart, '9', 9)
-      card4 = Card.new(:diamond, 'Jack', 11) 
-      card5 = Card.new(:heart, '8', 8) 
-      card6 = Card.new(:diamond, 'Queen', 12)
-      card7 = Card.new(:heart, '3', 3) 
-      card8 = Card.new(:diamond, '2', 2) 
-      deck1 = Deck.new([card1, card2, card5, card8]) 
-      deck2 = Deck.new([card3, card4, card6, card7]) 
-      player1 = Player.new("Megan", deck1) 
-      player2 = Player.new("Aurora", deck2) 
-      turn = Turn.new(player1, player2) 
-      game = Game.new(turn)
-      game.start
-      test_class = Game.new
-      expect(game.start).to output().to_stdout 
-    end
-
-
-
-
 end
