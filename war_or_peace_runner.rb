eeb6ff91d65320e727f21dd1d1bdc005b1a112dc
Adam Bailey <ab67319@gmail.com>
2022-12-02 17:43:06 -0700

    Create 52 unique playing cards, add to deck and shuffle

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -3,3 +3,19 @@ require './lib/deck'
 require './lib/player'
 require './lib/turn'
 
+# Create 52 Cards and shuffle
+deck = []
+suits = [:club, :diamond, :heart, :spade]
+
+4.times do |suit|
+    9.times do |value|    
+        deck << Card.new(suits[suit], (value + 2).to_s, value + 2)
+    end
+
+    deck << Card.new(suits[suit], "Jack", 11)
+    deck << Card.new(suits[suit], "Queen", 12)
+    deck << Card.new(suits[suit], "King", 13)
+    deck << Card.new(suits[suit], "Ace", 14)
+end
+
+deck.shuffle!
