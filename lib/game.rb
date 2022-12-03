1365046b74c67cf96cb2a142680781e18a345c88
Huy Phan <huyphan520@gmail.com>
2022-12-03 11:48:55 -0600

    Iteration 3 start method

--- lib/game.rb
+++ lib/game.rb
@@ -5,11 +5,20 @@ require './lib/turn'
 class Game
     def initialize(cards)
         @cards = cards.shuffle
-        require "pry"; binding.pry
         @deck1 = Deck.new(@cards[0..25])
         @deck2 = Deck.new(@cards[26..51])
         @player1 = Player.new("Alex", @deck1)
         @player2 = Player.new("Bob", @deck2)
         @turn = Turn.new(@player1, @player2)
     end
+
+    def start 
+       p  "Welcome to War! (or Peace) This game will be played with 52 cards."
+       p "The players today are #{@player1.name} and #{@player2.name}."
+       p "Type 'GO' to start the game!"
+       p "------------------------------------------------------------------"
+       
+    end
+
+
 end
--- lib/war_or_peace_runner.rb
+++ lib/war_or_peace_runner.rb
@@ -1,4 +1,3 @@
-require 'rspec'
 require './lib/card'
 require './lib/game'
 
@@ -57,6 +56,6 @@ require './lib/game'
         card52 = Card.new(:heart, '2', 2)
 
 
-        cards = [card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11, card12, card13, card14, card15, card16, card17, card18, card19, card20, card21, card22, card23, card24, card25, card26, card27, card28, card29, card30, card31, card32, card33, card34, card35, card36, card, card, card, card, card, card, card, card, card, card, card, card]
+        cards = [card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11, card12, card13, card14, card15, card16, card17, card18, card19, card20, card21, card22, card23, card24, card25, card26, card27, card28, card29, card30, card31, card32, card33, card34, card35, card36, card37, card38, card39, card40, card41, card42, card43, card44, card45, card46, card47, card48, card49, card50, card51, card52]
         game = Game.new(cards)
-        #game.start
+        game.start
