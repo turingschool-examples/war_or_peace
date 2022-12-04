1170c8727324ab9b2a5657b04a86743b4bec9e1a
Carol Rieping <carol.rieping@gmail.com>
2022-12-03 19:24:10 -0700

    Add turn setup

---
+++ lib/turn.rb
@@ -0,0 +1,25 @@
+class Turn
+
+    attr :basic,
+          :war,
+          :mutually_assured_destruction
+
+    def initialize(player1, player2, spoils_of_war)
+
+
+    end
+
+    def winner
+        if
+        end
+    end
+
+    def pile_cards
+        
+
+    end
+
+    def award_spoils
+
+    end
+end
