2d26ca94ca250ea15ecd4714a68f503d1abbf45b
Danae Mata <danae.mata1515@gmail.com>
2022-12-04 18:59:53 -0500

    Add code to player.rb file

---
+++ lib/player.rb
@@ -0,0 +1,14 @@
+class Player
+        attr_reader :name , :deck
+
+    def initialize (name, deck)
+        @name = name
+        @deck = deck
+    end
+
+    def has_lost?
+        if (@deck.cards.count) >= 1
+            puts false
+        end
+    end
+end
