4780a81089056d47bb00dfa7df5694a6707876a7
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2022-12-03 18:13:31 -0700

    Attribute :has_lost moved to attr_accessor - player.rb

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,7 @@
 class Player
 
-    attr_reader :name, :deck, :has_lost
+    attr_reader :name, :deck
+    attr_accessor :has_lost
     def initialize(name, deck)
         @name = name
         @deck = deck
