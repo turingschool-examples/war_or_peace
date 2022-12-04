0f9f41f76edf88740b5a066b476a19b3dacd261e
Adam Bailey <ab67319@gmail.com>
2022-12-04 11:45:14 -0700

    Add 'has readable atributes' and 'can create deck' example tests

--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -5,17 +5,13 @@ require 'csv'
 
 describe CardGenerator do
     before (:each) do
-        #Create example file to read
-        # file = CSV.generate do |csv|
-        #     csv << ["2", " Heart", " 2"]
-        #     csv << ["Jack", " Diamond", " 11"]
-        #     csv << ["Ace", " Spade", " 14"]
-        # end
-        # file = file.split("\n")
-        # file.each_with_index {|card, i| file[i] = card.split(",")}
-        
-        file = CSV.read("test_cards.txt")
-        
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:club, '8', 8)
+        card3 = Card.new(:heart, '10', 10)
+        card4 = Card.new(:spade, 'Ace', 14)
+
+        @cards = [card1, card2, card3, card4]
+   
         @card_generator = CardGenerator.new("test_cards.txt")
     end
 
@@ -23,7 +19,21 @@ describe CardGenerator do
         expect(@card_generator).to be_a(CardGenerator)
     end
 
-    xit 'can create deck' do
+    it 'has readable attributes' do
+        # Check if CardGenerator's card instance variable is an array to confirm it is readable
+        #   Unable to check if both arrays match as the Card objects in both arrays
+        #   are inherently different objects even though instance variables match
+        expect(@card_generator.cards).to be_a(Array)
+    end
 
+    it 'can create deck' do
+        # Loop through each CardGenerator card and example card to compare values
+        #   Unable to check if both arrays match as the Card objects in both arrays
+        #   are inherently different objects even though instance variables match
+        @card_generator.cards.each_index do |i|
+            expect(@card_generator.cards[i].rank).to eq(@cards[i].rank)
+            expect(@card_generator.cards[i].value).to eq(@cards[i].value)
+            expect(@card_generator.cards[i].suit).to eq(@cards[i].suit)
+        end
     end
 end
