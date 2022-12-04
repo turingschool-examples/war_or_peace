273e54e204d9d7588ac374ac93f07b918d9ff80a
James Taylor <jrtaylor0614@gmail.com>
2022-12-04 16:15:27 -0700

    attempts turn types

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,15 +1,15 @@
 
 class Turn
-    attr_reader :player1, :player2, :spoils_of_war
+    attr_reader :player1, :player2
 
     def initialize(player1, player2)
         @player1 = player1
         @player2 = player2
-        @spoils_of_war = []
     end
 
-    # def basic if
-    #     player1.deck.cards.size <=2 || @player2.deck.size <=2
-    # end
+    def type basic
+        player1.deck.rank_of_card_at(0) || player2.deck.rank_of_card_at(0)
+    end
 
-end
+end 
+        
