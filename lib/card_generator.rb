31a5efe3ffc91ac8a72881cef5cb424dd9d69037
Adam Bailey <ab67319@gmail.com>
2022-12-04 11:45:43 -0700

    Add attr_reader for cards and create create_deck method

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,8 +1,20 @@
 require 'csv'
 
 class CardGenerator
-    def initialize(file)
+    attr_reader :cards
 
+    def initialize(file)
+        @cards = []
+        create_deck(file)
     end
 
+    def create_deck(file)
+        file = CSV.read(file)
+
+        file.each do |card|
+            @cards << Card.new(card[1].strip.downcase.to_sym, card[0], card[2].to_i)
+        end
+
+        @cards
+    end
 end
