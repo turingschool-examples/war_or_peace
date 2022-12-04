32fab5a94005557709246d627c9f5d4fa8242226
Sam Walker <sg.walker327@gmail.com>
2022-12-04 14:59:34 -0700

    Complete winner portion of turn class/spec

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,7 +12,7 @@ class Turn
     def type
         if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
             :mutually_assured_destruction
-        elsif player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+        elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
             :war
         else player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
             :basic
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -82,6 +82,7 @@ RSpec.describe Turn do
             expect(player1.deck.rank_of_card_at(0)).to eq(player2.deck.rank_of_card_at(0))
             expect(player1.deck.rank_of_card_at(2)).to eq(player2.deck.rank_of_card_at(2))
             expect(turn.type).to eq(:mutually_assured_destruction)
+            
         end
     end
 
@@ -120,11 +121,26 @@ RSpec.describe Turn do
             turn = Turn.new(player1, player2)
 
             expect(turn.winner).to eq(player2)
+            
         end
 
-    end
+        it 'mutually_assured_destruction turn declares a winner' do
+            card1 = Card.new(:heart, 'Jack', 11)
+            card2 = Card.new(:heart, '10', 10)
+            card3 = Card.new(:heart, '9', 9)
+            card4 = Card.new(:diamond, 'Jack', 11)
+            card5 = Card.new(:heart, '8', 8)
+            card6 = Card.new(:diamond, '8', 8)
+            card7 = Card.new(:heart, '3', 3)
+            card8 = Card.new(:diamond, '2', 2) 
+            deck1 = Deck.new([card1, card2, card5, card8])
+            deck2 = Deck.new([card4, card3, card6, card7])
+            player1 = Player.new("Megan", deck1)
+            player2 = Player.new("Aurora", deck2)
+            turn = Turn.new(player1, player2)
 
-    describe 'mutual assured destruction turn' do
-        
+            expect(turn.winner).to eq("No Winner")
+        end
     end
 end
+# require 'pry'; binding.pry
