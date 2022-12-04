a1463f67e8fc459a175e6e5a38dccb13871def55
Adam Bailey <ab67319@gmail.com>
2022-12-03 19:05:15 -0700

    Create @turn_type instance variable and create announce_winner method

--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,14 @@
 class Game 
     attr_reader :player1,
                 :player2,
-                :turn_count
+                :turn_count,
+                :turn_type
 
     def initialize(player1, player2)
         @player1 = player1
         @player2 = player2
         @turn_count = 0
+        @turn_type = :start
     end
 
     def game_ended?
@@ -18,23 +20,36 @@ class Game
         turn = Turn.new(@player1, @player2)
         @turn_count += 1
         winner = turn.winner
+        @turn_type = turn.type
 
         turn.pile_cards
-        num_spoils = turn.spoils_of_war.count
-
+        turn.shuffle_spoils
         turn.award_spoils(winner)
-        puts "Turn #{turn_count}: #{winner.name} won #{num_spoils} cards"
+        puts announce_winner(winner)
 
         winner
     end
 
     def start
-        until game_ended? == true || @turn_count == 1_000#_000
-            winner = play_turn
+        until game_ended? || @turn_count == 1_000_000
+            winner = play_turn 
+        end
+
+        if @turn_count == 1_000_000
+            return "---- DRAW ----" 
+        else 
+            return "*~*~*~* #{winner.name} has won the game! *~*~*~*"
         end
         
-        return "---- DRAW ----" if @turn_count == 1_000#_000
-        return "*~*~*~* #{winner.name} has won the game! *~*~*~*"
     end
 
+    def announce_winner(winner)
+        if @turn_type == :basic
+            "Turn #{@turn_count}: #{winner.name} won 2 cards"
+        elsif @turn_type == :war
+            "Turn #{@turn_count}: WAR - #{winner.name} won 6 cards"
+        elsif @turn_type == :mutually_assured_destruction
+            "Turn #{@turn_count}: *mutually assured destruction* 6 cards removed from play"
+        end
+    end
 end
