897af39fac07d7bca8a48b4fcf5c4342a65ca051
Carol Rieping <carol.rieping@gmail.com>
2022-12-04 15:02:02 -0700

    Add spoils of war test

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,23 +2,30 @@ class Turn
 
     attr  :player1,
           :player2,
-          :spoils_of_war
+          :spoils_of_war,
+          :basic,
+          :war,
+          :mutually_assured_destruction
 
     def initialize(player1, player2)
         @player1 = player1
         @player2 = player2
         @spoils_of_war = []
+        @basic = basic
+        @war = war
+        @mutually_assured_destruction
+
     end
 
     def type
         if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
-            return :basic
+            :basic
 
         elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
-            return :war
+            :war
 
         elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
-            return :mutually_assured_destruction
+            :mutually_assured_destruction
         end
     end
 
@@ -49,15 +56,30 @@ class Turn
              "No winner"
 
         end
-
     end
 
 
-    # def pile_cards
-        
-    # end
+    def pile_cards
 
-    # def award_spoils
+        if type == :basic
+            
+            @spoils_of_war << player1.deck.remove_card
+            @spoils_of_war << player2.deck.remove_card
 
-    # end
+        elsif type == :war
+
+            @spoils_of_war << player1.deck.remove_card
+            @spoils_of_war << player2.deck.remove_card
+
+        elsif type == :mutually_assured_destruction
+
+            player1.deck.remove_card
+            player1.deck.remove_card
+            player1.deck.remove_card
+            player2.deck.remove_card
+            player2.deck.remove_card
+            player2.deck.remove_card
+            
+        end
+    end
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -8,9 +8,9 @@ require 'rspec'
 RSpec.describe Turn do
     describe 'exists' do
         it 'turn should be a instance of Turn' do
-            card1 = Card.new(:diamond, 'Queen', 12)
-            card2 = Card.new(:spade, '3', 3)
-            card3 = Card.new(:heart, 'Ace', 14)
+            card1 = Card.new(:heart, 'Jack', 11)
+            card2 = Card.new(:heart, '10', 10)
+            card3 = Card.new(:heart, '9', 9)
             card4 = Card.new(:diamond, 'Jack', 11)
             card5 = Card.new(:heart, '8', 8)
             card6 = Card.new(:diamond, 'Queen', 12)
@@ -50,10 +50,10 @@ RSpec.describe Turn do
             expect(turn.player2).to eq(player2)
         end
 
-        it 'initialize' do
-            card1 = Card.new(:diamond, 'Queen', 12)
-            card2 = Card.new(:spade, '3', 3)
-            card3 = Card.new(:heart, 'Ace', 14)
+        it 'turn.spoils_of_war should be empty' do
+            card1 = Card.new(:heart, 'Jack', 11)
+            card2 = Card.new(:heart, '10', 10)
+            card3 = Card.new(:heart, '9', 9)
             card4 = Card.new(:diamond, 'Jack', 11)
             card5 = Card.new(:heart, '8', 8)
             card6 = Card.new(:diamond, 'Queen', 12)
@@ -72,9 +72,9 @@ RSpec.describe Turn do
         end
 
         it 'turn.type should equal basic' do
-            card1 = Card.new(:diamond, 'Queen', 12)
-            card2 = Card.new(:spade, '3', 3)
-            card3 = Card.new(:heart, 'Ace', 14)
+            card1 = Card.new(:heart, 'Jack', 11)
+            card2 = Card.new(:heart, '10', 10)
+            card3 = Card.new(:heart, '9', 9)
             card4 = Card.new(:diamond, 'Jack', 11)
             card5 = Card.new(:heart, '8', 8)
             card6 = Card.new(:diamond, 'Queen', 12)
@@ -93,9 +93,9 @@ RSpec.describe Turn do
         end
 
         it 'winner should equal turn.winner' do
-            card1 = Card.new(:diamond, 'Queen', 12)
-            card2 = Card.new(:spade, '3', 3)
-            card3 = Card.new(:heart, 'Ace', 14)
+            card1 = Card.new(:heart, 'Jack', 11)
+            card2 = Card.new(:heart, '10', 10)
+            card3 = Card.new(:heart, '9', 9)
             card4 = Card.new(:diamond, 'Jack', 11)
             card5 = Card.new(:heart, '8', 8)
             card6 = Card.new(:diamond, 'Queen', 12)
@@ -109,47 +109,57 @@ RSpec.describe Turn do
             player2 = Player.new("Aurora", deck2)
             
             turn = Turn.new(player1, player2)
+            
             turn.type
             winner = turn.winner
             expect(winner).to eq(turn.winner)
         end
-    end    
-end
+
+        it 'spoils_of_war should equal card1 and card3' do
+            card1 = Card.new(:heart, 'Jack', 11)
+            card2 = Card.new(:heart, '10', 10)
+            card3 = Card.new(:heart, '9', 9)
+            card4 = Card.new(:diamond, 'Jack', 11)
+            card5 = Card.new(:heart, '8', 8)
+            card6 = Card.new(:diamond, 'Queen', 12)
+            card7 = Card.new(:heart, '3', 3)
+            card8 = Card.new(:diamond, '2', 2)    
+            
+            deck1 = Deck.new([card1, card2, card5, card8])
+            deck2 = Deck.new([card3, card4, card6, card7])
+            
+            player1 = Player.new("Megan", deck1)
+            player2 = Player.new("Aurora", deck2)
+            
+            turn = Turn.new(player1, player2)
+
+            turn.type 
+            turn.pile_cards
+            expect(turn.spoils_of_war).to eq([card1, card3])
+        end
     
-    #     xit 'winner should equal turn.winner' do
-    #         card1 = Card.new(:diamond, 'Queen', 12
-    #         card2 = Card.new(:spade, '3', 3)
-    #         card3 = Card.new(:heart, 'Ace', 14)
-    #         card4 = Card.new(:diamond, 'Jack', 11)
-    #         card5 = Card.new(:heart, '8', 8)
-    #         card6 = Card.new(:diamond, 'Queen', 12)
-    #         card7 = Card.new(:heart, '3', 3)
-    #         card8 = Card.new(:diamond, '2', 2)    
-    #         deck1 = Deck.new([card1, card2, card5, card8])
-    #         deck2 = Deck.new([card3, card4, card6, card7])
-    #         player1 = Player.new("Megan", deck1)
-    #         player2 = Player.new("Aurora", deck2)
-    #         turn = Turn.new(player1, player2)
-    #         expect(winner).to eq(turn.winner)
-    #     end
+        xit 'player1.deck should have added card1 and card3 and player2.deck should have lost card3'
+            card1 = Card.new(:heart, 'Jack', 11)
+            card2 = Card.new(:heart, '10', 10)
+            card3 = Card.new(:heart, '9', 9)
+            card4 = Card.new(:diamond, 'Jack', 11)
+            card5 = Card.new(:heart, '8', 8)
+            card6 = Card.new(:diamond, 'Queen', 12)
+            card7 = Card.new(:heart, '3', 3)
+            card8 = Card.new(:diamond, '2', 2)    
+            
+            deck1 = Deck.new([card1, card2, card5, card8])
+            deck2 = Deck.new([card3, card4, card6, card7])
+            
+            player1 = Player.new("Megan", deck1)
+            player2 = Player.new("Aurora", deck2)
+            turn.award_spoils(winner)
 
-        
-    #     # turn.pile_cards
-        
-
-    #     xit
-    #         expect(turn.spoils_of_war).to eq([card4, @rank=11, @suit=:heart, @value="Jack", card5 @rank=9, @suit=:heart, @value="9"])
-    #     end
-
-    #     xit
-    #         turn.award_spoils(winner)
-    #     end
-
-    #     xit
-    #         expect(player1.deck).to eq([card2, card5, card8, card1, card3])
-    #         expect(player2.deck).to eq([card4, card6, card7])
-    #     end
-    # end        
+            expect(player1.deck).to eq([card2, card5, card8, card1, card3])
+            expect(player2.deck).to eq([card4, card6, card7])
+        end
+    end 
+end    
 
     #  #turn type :war
 
