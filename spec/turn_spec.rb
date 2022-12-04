0d0807bdc5354534076b6108ae15fd3a8c928030
Carol Rieping <carol.rieping@gmail.com>
2022-12-04 16:43:49 -0700

    Add test for war type

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -170,70 +170,6 @@ end
 
 RSpec.describe Turn do
     describe 'exists' do
-        xit 'turn should be a instance of Turn' do
-            card1 = Card.new(:heart, 'Jack', 11)
-            card2 = Card.new(:heart, '10', 10)
-            card3 = Card.new(:heart, '9', 9)
-            card4 = Card.new(:diamond, 'Jack', 11)
-            card5 = Card.new(:heart, '8', 8)
-            card6 = Card.new(:diamond, 'Queen', 12)
-            card7 = Card.new(:heart, '3', 3)
-            card8 = Card.new(:diamond, '2', 2)    
-            
-            deck1 = Deck.new([card1, card2, card5, card8])
-            deck2 = Deck.new([card3, card4, card6, card7])
-            
-            player1 = Player.new("Megan", deck1)
-            player2 = Player.new("Aurora", deck2)
-            
-            turn = Turn.new(player1, player2)
-            
-            expect(turn).to be_a(Turn)
-        end
-
-        xit 'initialize' do    
-            card1 = Card.new(:heart, 'Jack', 11)
-            card2 = Card.new(:heart, '10', 10)
-            card3 = Card.new(:heart, '9', 9)
-            card4 = Card.new(:diamond, 'Jack', 11)
-            card5 = Card.new(:heart, '8', 8)
-            card6 = Card.new(:diamond, 'Queen', 12)
-            card7 = Card.new(:heart, '3', 3)
-            card8 = Card.new(:diamond, '2', 2)    
-            
-            deck1 = Deck.new([card1, card2, card5, card8])
-            deck2 = Deck.new([card3, card4, card6, card7])
-            
-            player1 = Player.new("Megan", deck1)
-            player2 = Player.new("Aurora", deck2)
-            
-            turn = Turn.new(player1, player2)
-            
-            expect(turn.player1).to eq(player1)
-            expect(turn.player2).to eq(player2)
-        end
-
-        xit 'turn.spoils_of_war should be empty' do
-            card1 = Card.new(:heart, 'Jack', 11)
-            card2 = Card.new(:heart, '10', 10)
-            card3 = Card.new(:heart, '9', 9)
-            card4 = Card.new(:diamond, 'Jack', 11)
-            card5 = Card.new(:heart, '8', 8)
-            card6 = Card.new(:diamond, 'Queen', 12)
-            card7 = Card.new(:heart, '3', 3)
-            card8 = Card.new(:diamond, '2', 2)    
-            
-            deck1 = Deck.new([card1, card2, card5, card8])
-            deck2 = Deck.new([card3, card4, card6, card7])
-            
-            player1 = Player.new("Megan", deck1)
-            player2 = Player.new("Aurora", deck2)
-            
-            turn = Turn.new(player1, player2)
-
-            expect(turn.spoils_of_war).to eq([])
-        end
-
         xit 'turn.type should equal basic' do
             card1 = Card.new(:heart, 'Jack', 11)
             card2 = Card.new(:heart, '10', 10)
@@ -245,14 +181,14 @@ RSpec.describe Turn do
             card8 = Card.new(:diamond, '2', 2)    
             
             deck1 = Deck.new([card1, card2, card5, card8])
-            deck2 = Deck.new([card3, card4, card6, card7])
+            deck2 = Deck.new([card4, card3, card6, card7])
             
             player1 = Player.new("Megan", deck1)
             player2 = Player.new("Aurora", deck2)
             
             turn = Turn.new(player1, player2)
             
-            expect(turn.type).to eq(:basic)
+            expect(turn.type).to eq(:war)
         end
 
         xit 'winner should equal turn.winner' do
