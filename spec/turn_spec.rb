1d65ed166e561153e605ccfdf7def68670e727e4
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2022-12-03 20:55:33 -0700

    Test set 1, test 1 + 2 refactored - turn.rb/turn_spec.rb

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -45,8 +45,8 @@ RSpec.describe Player do
 
             turn = Turn.new(player1, player2)
 
-            expect(turn.player1).to eq(player1)
-            expect(turn.player2).to eq(player2)
+            expect(turn.player1).to eq(@player1)
+            expect(turn.player2).to eq(@player2)
             expect(turn.spoils_of_war).to eq([])
         end
 
