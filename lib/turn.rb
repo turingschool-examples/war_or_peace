8c605959ce6df4b5093459c4584af79280e1431f
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-12-04 13:41:37 -0800

    Update pile_cards method to include remove_card method

--- lib/turn.rb
+++ lib/turn.rb
@@ -38,8 +38,8 @@ class Turn
 
     def pile_cards
         if type == :basic 
-            spoils_of_war << player1.deck.cards.shift
-            spoils_of_war << player2.deck.cards.shift
+            spoils_of_war << player1.deck.remove_card
+            spoils_of_war << player2.deck.remove_card
         elsif type == :war 
             spoils_of_war.concat(player1.deck.cards.shift(3))
             spoils_of_war.concat(player2.deck.cards.shift(3))
