c15c7ef05ac3fbd60ea554fd5c8625507c330e5b
Nickolas Marquez <nickomarquez7@gmail.com>
2022-12-04 11:42:55 -0700

    Added winner method

---
+++ lib/turn.rb
@@ -0,0 +1,49 @@
+class Turn 
+    
+    attr_reader :player1, 
+                :player2, 
+                :spoils_of_war
+
+    def initialize(player1, player2)
+        @player1 = player1
+        @player2 = player2
+        @spoils_of_war = []
+    end
+
+    def type
+        if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0) 
+             :basic
+        elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card(0)
+             :war
+        else player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card(0) && player1.deck.rank_of_card(2) == player2.deck.rank_of_card_at(2)        
+             :mutually_assured_destruction
+        end    
+    end
+
+    def winner
+        if type == :basic
+
+            if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+                player1
+            elsif player1.deck.rank_of_card_at(0) < player2.deck.rank_of_card_at(0)
+                player2
+            end
+
+        elsif type == :war
+            if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+                player1
+            elsif player1.deck.rank_of_card_at(2) < player2.deck.rank_of_card_at(2)
+                player 2
+            end
+        end
+    end
+    
+    
+
+
+
+
+
+
+    
+end
