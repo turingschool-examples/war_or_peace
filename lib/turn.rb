34a36db903638d5d9772156feffb42e5c1c3076b
Boston Lowrey <bostonlowrey@gmail.com>
2022-12-04 15:23:49 -0500

    Add pile cards method

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,24 +9,54 @@ class Turn
     
     def type
         if 
-            player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) &&
-            player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
-            :mutually_assured_destruction
-        elsif
-            (player1.deck.rank_of_card_at(0)) == (player2.deck.rank_of_card_at(0))
-            :war
+          player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) &&
+          player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
+          :mutually_assured_destruction
+        elsif 
+          (player1.deck.rank_of_card_at(0)) == (player2.deck.rank_of_card_at(0))
+          :war
         else
-            :basic
+          :basic
         end
+      end
+      
+      def winner 
+        if type == :basic 
+          if(player1.deck.rank_of_card_at(0)) > (player2.deck.rank_of_card_at(0))
+            player1
+          else 
+            player2
+          end 
+          
+          
+        elsif type == :war
+          if(player1.deck.rank_of_card_at(2)) > (player2.deck.rank_of_card_at(2))
+            player1
+          else 
+            player2
+          end 
+        else type == :mutually_assured_destruction
+        
+        end  
     end
 
-    def winner
+    def pile_cards
         if type == :basic
-            if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
-                player1
-            else
-                player2
-            end
-        end
-    end
+          spoils_of_war << player1.deck.remove_card
+          spoils_of_war << player2.deck.remove_card
+          
+        elsif type == :war 
+          3.times do
+          spoils_of_war << player1.deck.remove_card
+          spoils_of_war << player2.deck.remove_card
+          end
+          
+        else type == :mutually_assured_destruction
+          3.times do
+            player1.deck.remove_card
+            player2.deck.remove_card
+          end
+        
+        end 
+      end
 end
