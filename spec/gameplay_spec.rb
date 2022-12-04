880d82e755c1fd55cff9e97b115557844d4258df
Tori Enyart <torienyart@gmail.com>
2022-12-03 18:23:29 -0700

    Alter turn result test for better assertion use

--- spec/gameplay_spec.rb
+++ spec/gameplay_spec.rb
@@ -31,31 +31,6 @@ describe GamePlay do
     
     end
 
-    xit 'can start game with a new turn' do
-        card1 = Card.new(:heart, 'Jack', 11)
-        card2 = Card.new(:heart, '10', 10)
-        card3 = Card.new(:heart, '9', 9)
-        card4 = Card.new(:diamond, 'Jack', 11)
-        card5 = Card.new(:heart, '8', 8)
-        card6 = Card.new(:diamond, 'Queen', 12)
-        card7 = Card.new(:heart, '3', 3)
-        card8 = Card.new(:diamond, '2', 2)
-
-        deck1 = Deck.new([card1, card2, card5, card8])
-        deck2 = Deck.new([card3, card4, card6, card7])
-
-        player1 = Player.new("Megan", deck1)
-        player2 = Player.new("Aurora", deck2)
-
-        game = GamePlay.new(player1, player2)
-        game.start
-
-        expect(game.turns).to eq(1)
-        expect(game.turns.first.player1).to eq(player1)
-        expect(game.turns.first.player2).to eq(player2)
-
-    end
-
     it 'can give turn result' do
         card1 = Card.new(:heart, 'Jack', 11)
         card2 = Card.new(:heart, '10', 10)
@@ -74,11 +49,10 @@ describe GamePlay do
 
         game = GamePlay.new(player1, player2)
 
-        require 'pry'; binding.pry
-        game.start
+        # require 'pry'; binding.pry
+        game.new_turn
+        game.turn_result
         
-    # require 'pry'; binding.pry
-
         expect(game.turn_result).to eq("Megan won 2 cards. Player1:5 cards Player2:3 cards")
        
     end
