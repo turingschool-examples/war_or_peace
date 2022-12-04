3a40b041013f29547d83a74664e0924808ab12c8
Adam Bailey <ab67319@gmail.com>
2022-12-04 10:52:27 -0700

    Update before block and add 'exists' example test

--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -6,22 +6,24 @@ require 'csv'
 describe CardGenerator do
     before (:each) do
         #Create example file to read
-        file = CSV.generate do |csv|
-            csv << ["2", " Heart", " 2"]
-            csv << ["Jack", " Diamond", " 11"]
-            csv << ["Ace", " Spade", " 14"]
-        end
-        file = file.split("\n")
-        file.each_with_index {|card, i| file[i] = card.split(",")}
-
-
+        # file = CSV.generate do |csv|
+        #     csv << ["2", " Heart", " 2"]
+        #     csv << ["Jack", " Diamond", " 11"]
+        #     csv << ["Ace", " Spade", " 14"]
+        # end
+        # file = file.split("\n")
+        # file.each_with_index {|card, i| file[i] = card.split(",")}
+        
+        file = CSV.read("test_cards.txt")
+        
+        @card_generator = CardGenerator.new("test_cards.txt")
     end
 
     it 'exists' do
-
+        expect(@card_generator).to be_a(CardGenerator)
     end
 
-    it 'can create deck' do
+    xit 'can create deck' do
 
     end
 end
