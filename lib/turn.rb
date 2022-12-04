2d70c1d2ed9f36ed83e06242546a8c870bafd345
Caleb Thomas <cjthomas00@comcast.net>
2022-12-03 20:15:37 -0700

    update type method to include what happens with 2 or less cards.

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,6 +9,7 @@ class Turn
 
     def type
         return :basic if player1.deck.cards.count <= 2 || player2.deck.cards.count <= 2
+        
         if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
             :mutually_assured_destruction
         elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
