939b8771ba1a8f90e61eb66530614d3589bd2465
Adam Bailey <ab67319@gmail.com>
2022-12-04 10:17:03 -0700

    Add required file, describe block and example test template

---
+++ spec/card_generator_spec.rb
@@ -0,0 +1,15 @@
+require './lib/card_generator'
+
+describe CardGenerator do
+    before (:each_) do
+        #Create example file to read
+
+        it 'exists' do
+
+        end
+
+        it 'can create deck' do
+
+        end
+    end
+end
