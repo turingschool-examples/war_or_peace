c5433cab36fbea77f941e4b136bdf1d6fa2860e9
Elle Majors <hellolovely@ellemajors>
2022-12-04 16:55:49 -0700

    feat: play the game

--- lib/game.rb
+++ lib/game.rb
@@ -1,156 +1,99 @@
 class Game
-  attr_reader :full_deck
+  attr_reader :full_deck,
+              :deck1,
+              :deck2,
+              :player1,
+              :player2
 
   def initialize
     @full_deck = full_deck
+    @deck1 = Deck.new(shuffle_deck[0..25])
+    @deck2 = Deck.new(shuffle_deck[26..51])
+    @player1 = Player.new('Megan', @deck1)
+    @player2 = Player.new('Aurora', @deck2)
+    @counter = 0
+  end
+
+  def start
+    p "Welcome to War! (or Peace) This game will be played with 52 cards."
+    p "The players today are #{player1.name} and #{player2.name}."
+    p "Type 'GO' to start the game!"
+    p "------------------------------------------------------------------"
+
+    start_game = gets.chomp.upcase
+
+    if start_game == "GO"
+      play_game
+    else 
+      p "Wrong input. Try again."
+    end
+    # start
+  end   
+
+
+  def game_output
+    if @turn_type == :basic 
+      p "Turn #{@count}: #{@turn_winner.name} won 2 cards"
+    elsif @turn_type == :war
+      p "Turn #{@count}: WAR - #{@turn_winner.name} won 6 cards"
+    elsif @turn_type == :mutually_assured_destruction
+      p "Turn #{@count}: *mutually assured destruction* 6 cards removed from play"
+    else
+    end
   end
 
   def full_deck
-    [card1 = Card.new(:diamond, '2', 2),
-    card2 = Card.new(:diamond, '3', 3),
-    card3 = Card.new(:diamond, '4', 4) ,
-    card4 = Card.new(:diamond, '5', 5),
-    card5 = Card.new(:diamond, '6', 6),
-    card6 = Card.new(:diamond, '7', 7),
-    card7 = Card.new(:diamond, '8', 8),
-    card8 = Card.new(:diamond, '9', 9),
-    card9 = Card.new(:diamond, '10', 10),
-    card10 = Card.new(:diamond, 'Jack', 11),
-    card11 = Card.new(:diamond, 'Queen', 12),
-    card12 = Card.new(:diamond, 'King', 13),
-    card13 = Card.new(:diamond, 'Ace', 14),
-
-    @card14 = Card.new(:spade, '2', 2),
-    @card15 = Card.new(:spade, '3', 3),
-    @card16 = Card.new(:spade, '4', 4), 
-    @card17 = Card.new(:spade, '5', 5), 
-    @card18 = Card.new(:spade, '6', 6),
-    @card19 = Card.new(:spade, '7', 7),
-    @card20 = Card.new(:spade, '8', 8), 
-    @card21 = Card.new(:spade, '9', 9),
-    @card22 = Card.new(:spade, '10', 10),
-    @card23 = Card.new(:spade, 'Jack', 11),
-    @card24 = Card.new(:spade, 'Queen', 12),
-    @card25 = Card.new(:spade, 'King', 13),
-    @card26 = Card.new(:spade, 'Ace', 14),
-
-    @card27 = Card.new(:heart, '2', 2),
-    @card28 = Card.new(:heart, '3', 3),
-    @card29 = Card.new(:heart, '4', 4),
-    @card30 = Card.new(:heart, '5', 5),
-    @card31 = Card.new(:heart, '6', 6),
-    @card32 = Card.new(:heart, '7', 7),
-    @card33 = Card.new(:heart, '8', 8),
-    @card34 = Card.new(:heart, '9', 9),
-    @card35 = Card.new(:heart, '10', 10),
-    @card36 = Card.new(:heart, 'Jack', 11),
-    @card37 = Card.new(:heart, 'Queen', 12),
-    @card38 = Card.new(:heart, 'King', 13),
-    @card39 = Card.new(:heart, 'Ace', 14),
-
-    @card40 = Card.new(:club, '2', 2),
-    @card41 = Card.new(:club, '3', 3),
-    @card42 = Card.new(:club, '4', 4), 
-    @card43 = Card.new(:club, '5', 5),
-    @card44 = Card.new(:club, '6', 6),
-    @card45 = Card.new(:club, '7', 7),
-    @card46 = Card.new(:club, '8', 8), 
-    @card47 = Card.new(:club, '9', 9),
-    @card48 = Card.new(:club, '10', 10),
-    @card49 = Card.new(:club, 'Jack', 11),
-    @card50 = Card.new(:club, 'Queen', 12),
-    @card51 = Card.new(:club, 'King', 13),
-    @card52 = Card.new(:club, 'Ace', 14)]
+    build_deck
   end
 
-# Put those card into two Decks (some randomness would be nice here!) .shuffle 
-end
+  def build_deck 
+    deck = []
+    
+    suits = [:diamond, :spade, :heart, :club]
+    rank =  ('2'..'10').to_a + ['Jack', 'Queen', 'King', 'Ace']
 
-# @card1 = Card.new(:diamond, '2', 2)
-# @card2 = Card.new(:diamond, '3', 3)
-# @card3 = Card.new(:diamond, '4', 4) 
-# @card4 = Card.new(:diamond, '5', 5)
-# @card5 = Card.new(:diamond, '6', 6)
-# @card6 = Card.new(:diamond, '7', 7)
-# @card7 = Card.new(:diamond, '8', 8) 
-# @card8 = Card.new(:diamond, '9', 9)
-# @card9 = Card.new(:diamond, '10', 10)
-# @card10 = Card.new(:diamond, 'Jack', 11)
-# @card11 = Card.new(:diamond, 'Queen', 12)
-# @card12 = Card.new(:diamond, 'King', 13)
-# @card13 = Card.new(:diamond, 'Ace', 14)
+    rank.each_with_index do |rank, index|
+      suits.each do |suit|
+        card = Card.new(suit, rank, index + 2)
+        deck << card
+      end
+    end
+    deck
+  end
 
-# @card14 = Card.new(:spade, '2', 2)
-# @card15 = Card.new(:spade, '3', 3)
-# @card16 = Card.new(:spade, '4', 4) 
-# @card17 = Card.new(:spade, '5', 5) 
-# @card18 = Card.new(:spade, '6', 6)
-# @card19 = Card.new(:spade, '7', 7)
-# @card20 = Card.new(:spade, '8', 8) 
-# @card21 = Card.new(:spade, '9', 9)
-# @card22 = Card.new(:spade, '10', 10)
-# @card23 = Card.new(:spade, 'Jack', 11)
-# @card24 = Card.new(:spade, 'Queen', 12)
-# @card25 = Card.new(:spade, 'King', 13)
-# @card26 = Card.new(:spade, 'Ace', 14)
+  def shuffle_deck
+    full_deck.shuffle
+  end
 
-# @card27 = Card.new(:heart, '2', 2)
-# @card28 = Card.new(:heart, '3', 3)
-# @card29 = Card.new(:heart, '4', 4) 
-# @card30 = Card.new(:heart, '5', 5)
-# @card31 = Card.new(:heart, '6', 6)
-# @card32 = Card.new(:heart, '7', 7)
-# @card33 = Card.new(:heart, '8', 8) 
-# @card34 = Card.new(:heart, '9', 9)
-# @card35 = Card.new(:heart, '10', 10)
-# @card36 = Card.new(:heart, 'Jack', 11)
-# @card37 = Card.new(:heart, 'Queen', 12)
-# @card38 = Card.new(:heart, 'King', 13)
-# @card39 = Card.new(:heart, 'Ace', 14)
+  def play_game
+    until @counter == 1_000_000 || player1.has_lost? == true || player2.has_lost? == true
+      loop do
+        @counter += 1 
 
-# @card40 = Card.new(:club, '2', 2)
-# @card41 = Card.new(:club, '3', 3)
-# @card42 = Card.new(:club, '4', 4) 
-# @card43 = Card.new(:club, '5', 5)
-# @card44 = Card.new(:club, '6', 6)
-# @card45 = Card.new(:club, '7', 7)
-# @card46 = Card.new(:club, '8', 8) 
-# @card47 = Card.new(:club, '9', 9)
-# @card48 = Card.new(:club, '10', 10)
-# @card49 = Card.new(:club, 'Jack', 11)
-# @card50 = Card.new(:club, 'Queen', 12)
-# @card51 = Card.new(:club, 'King', 13)
-# @card52 = Card.new(:club, 'Ace', 14)
+      turn = Turn.new(player1, player2)  
+      @turn_type = turn.type
+        if @turn_type == :basic || @turn_type == :war
+          @turn_winner = turn.winner
+        end
+        turn.pile_cards
+        spoils_count = turn.spoils_of_war.count
+        turn.award_spoils(turn.winner)
+        game_output
+        if player1.has_lost?
+          p "*~*~*~* #{turn.player2.name} has won the game! *~*~*~*"
+          break
+        elsif player2.has_lost?
+          p "*~*~*~* #{turn.player1.name} has won the game! *~*~*~*"
+          break
+        end
+
+        if @count == 1_000_000
+          p "DRAW"
+          break
+        end
+      end
+    end 
+  end
+end  
 
 
-
-# Create two players with the Decks you created
-
-# Start the game using a new method called start
-# This method will need to be included in a class - it is up to you which class to include it in - either existing or ✨new ✨
-
-# start the game print
-# p Welcome to War! (or Peace) This game will be played with 52 cards.
-# The players today are Megan and Aurora.
-# Type 'GO' to start the game!
-# ------------------------------------------------------------------
-
-# user will be able to type GO, and a game will start
-# p Turn 1: Megan won 2 cards
-# Turn 2: WAR - Aurora won 6 cards
-# Turn 3: *mutually assured destruction* 6 cards removed from play
-# ...
-# ...
-# ...
-# Turn 9324: Aurora won 2 cards
-# *~*~*~* Aurora has won the game! *~*~*~*
-
-# If no player has all cards after 1,000,000 turns, the result of the game is a draw
-# p Turn 1: Megan won 2 cards
-# Turn 2: WAR - Aurora won 6 cards
-# Turn 3: *mutually assured destruction* 6 cards removed from play
-# ...
-# ...
-# ...
-# Turn 1000000: Aurora won 2 cards
-# ---- DRAW ----
