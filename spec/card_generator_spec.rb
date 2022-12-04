90ef35363d8643e2754d3f4c2f97039d6dffdc20
Adam Bailey <ab67319@gmail.com>
2022-12-04 10:42:03 -0700

    Add requirement for card and deck files and create example file in before block

--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -1,15 +1,27 @@
+require './lib/card'
+require './lib/deck'
 require './lib/card_generator'
+require 'csv'
 
 describe CardGenerator do
-    before (:each_) do
+    before (:each) do
         #Create example file to read
-
-        it 'exists' do
-
+        file = CSV.generate do |csv|
+            csv << ["2", " Heart", " 2"]
+            csv << ["Jack", " Diamond", " 11"]
+            csv << ["Ace", " Spade", " 14"]
         end
+        file = file.split("\n")
+        file.each_with_index {|card, i| file[i] = card.split(",")}
 
-        it 'can create deck' do
 
-        end
+    end
+
+    it 'exists' do
+
+    end
+
+    it 'can create deck' do
+
     end
 end
