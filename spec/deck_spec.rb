5bbd5b6592eee33aa25146f6697c5885fbb2e1e4
Michael Callahan <mcalla242@gmail.com>
2023-03-23 10:57:41 -0600

    Make changes to spec files, add before(:each) method to clean up testing

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,75 +3,46 @@ require './lib/card'
 require './lib/deck'
 
 RSpec.describe Deck do
-  it "stores multiple cards" do
-    card1 = Card.new(:diamond, "Queen", 12)
-    card2 = Card.new(:spade, "3", 3)
-    card3 = Card.new(:heart, "Ace", 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
+  before(:each) do
+    @card1 = Card.new(:diamond, "Queen", 12)
+    @card2 = Card.new(:spade, "3", 3)
+    @card3 = Card.new(:heart, "Ace", 14)
+    @cards = [@card1, @card2, @card3]
+    @deck = Deck.new(@cards)
+  end
 
-    expect(deck.cards.length).to eq(3)
+  it "stores multiple cards" do
+    expect(@deck.cards).to eq(@cards)
   end
 
   it "returns the values of the cards in the deck" do
-    card1 = Card.new(:diamond, "Queen", 12)
-    card2 = Card.new(:spade, "3", 3)
-    card3 = Card.new(:heart, "Ace", 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-
-    expect(deck.rank_of_card_at(0)).to eq(12)
-    expect(deck.rank_of_card_at(2)).to eq(14)
+    expect(@deck.rank_of_card_at(0)).to eq(12)
+    expect(@deck.rank_of_card_at(2)).to eq(14)
   end
 
   it "returns an array of the high ranking cards" do
-    card1 = Card.new(:diamond, "Queen", 12)
-    card2 = Card.new(:spade, "3", 3)
-    card3 = Card.new(:heart, "Ace", 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-
-    expected = [card1, card3]
-    expect(deck.high_ranking_cards).to eq(expected)
+    expected = [@card1, @card3]
+    expect(@deck.high_ranking_cards).to eq(expected)
   end
 
   it "returns percent of high ranking cards in the deck" do
-    card1 = Card.new(:diamond, "Queen", 12)
-    card2 = Card.new(:spade, "3", 3)
-    card3 = Card.new(:heart, "Ace", 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-    #deck.high_ranking_cards
-
-    expect(deck.percent_high_ranking).to eq(66.67)
+    expect(@deck.percent_high_ranking).to eq(66.67)
   end
 
   it "removes cards from the deck" do
-    card1 = Card.new(:diamond, "Queen", 12)
-    card2 = Card.new(:spade, "3", 3)
-    card3 = Card.new(:heart, "Ace", 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-
-    expect(deck.remove_card).to eq(card1)
-    expect(deck.cards).to eq([card2, card3])
-    expect(deck.high_ranking_cards).to eq([card3])
-    expect(deck.percent_high_ranking).to eq(50.0)
+    expect(@deck.remove_card).to eq(@card1)
+    expect(@deck.cards).to eq([@card2, @card3])
+    expect(@deck.high_ranking_cards).to eq([@card3])
+    expect(@deck.percent_high_ranking).to eq(50.0)
   end
 
   it "can add new cards to the deck" do
-    card1 = Card.new(:diamond, "Queen", 12)
-    card2 = Card.new(:spade, "3", 3)
-    card3 = Card.new(:heart, "Ace", 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-    deck.remove_card
+    @deck.remove_card
+    @card4 = Card.new(:club, "5", 5)
+    @deck.add_card(@card4)
 
-    card4 = Card.new(:club, "5", 5)
-    deck.add_card(card4)
-
-    expect(deck.cards).to eq([card2, card3, card4])
-    expect(deck.high_ranking_cards).to eq([card3])
-    expect(deck.percent_high_ranking).to eq(33.33)
+    expect(@deck.cards).to eq([@card2, @card3, @card4])
+    expect(@deck.high_ranking_cards).to eq([@card3])
+    expect(@deck.percent_high_ranking).to eq(33.33)
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,34 +3,32 @@ require "./deck"
 require "./player"
 
 RSpec.describe Player do
-  it "creates new player" do
-    card1 = Card.new(:diamond, "Queen", 12)
-    card2 = Card.new(:spade, "3", 3)
-    card3 = Card.new(:heart, "Ace", 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-    
-    player_1 = Player.new("Michael", deck)
+  before(:each) do
+    @card1 = Card.new(:diamond, "Queen", 12)
+    @card2 = Card.new(:spade, "3", 3)
+    @card3 = Card.new(:heart, "Ace", 14)
+    @cards = [@card1, @card2, @card3]
+    @deck = Deck.new(@cards)
+    @player_1 = Player.new("Michael", @deck)
+  end
+  
+  it "initializes" do
+    expect(@player_1).to be_an_instance_of(Player)
+  end
 
-    expect(player_1.name).to eq("Michael")
+  it "stores the players name" do
+    expect(@player_1.name).to eq("Michael")
   end
 
   it "checks when a player has lost" do
-    card1 = Card.new(:diamond, "Queen", 12)
-    card2 = Card.new(:spade, "3", 3)
-    card3 = Card.new(:heart, "Ace", 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-    player_1 = Player.new("Michael", deck)
+    expect(@player_1.has_lost?).to be(false)
+    @deck.remove_card
+    expect(@player_1.has_lost?).to be(false)
+    @deck.remove_card
+    expect(@player_1.has_lost?).to be(false)
+    @deck.remove_card
+    expect(@player_1.has_lost?).to be(true)
 
-    expect(player_1.has_lost?).to be(false)
-    deck.remove_card
-    expect(player_1.has_lost?).to be(false)
-    deck.remove_card
-    expect(player_1.has_lost?).to be(false)
-    deck.remove_card
-    expect(player_1.has_lost?).to be(true)
-
-    expect(player_1.deck).to eq(deck)
+    expect(@player_1.deck).to eq(@deck)
   end
 end
