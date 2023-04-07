019ec10c0d1837d9b8a7b5bbbcf171e42d27d474
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-04-07 12:26:05 -0600

    troubleshooting pry debugger during testing

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ source 'https://rubygems.org'
 gem "simplecov", "~> 0.21.2"
 
 gem "pry", "~> 0.14.1"
+
+gem "rspec", "~> 3.12"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,8 +22,8 @@ require './lib/game'
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  config.before { allow($stdout).to receive(:write) }
-  config.filter_run_excluding skip: true
+  # config.before { allow($stdout).to receive(:write) }
+  # config.filter_run_excluding skip: true
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
