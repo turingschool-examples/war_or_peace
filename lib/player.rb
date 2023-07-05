ed808a4dcde1c7da1a90e0db58113a75dabcc7ba
Blake Sergesketter <blakesergesketter@gmail.com>
2023-07-05 14:15:07 -0500

    add and test name, deck, no_cards attributes, and has_lost? method.

--- lib/player.rb
+++ lib/player.rb
@@ -2,10 +2,19 @@ class Player
 
   attr_reader :name, 
               :deck,
-              :has_lost
+              :no_cards
   def initialize(name, deck)
     @name = name 
     @deck = deck 
-    @has_lost = false 
+    @no_cards = false 
+  end
+
+  def has_lost?
+    if @deck.cards.length == 0 
+      @no_cards = true
+    else 
+      @no_cards
+    end
+    # require 'pry'; binding.pry
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -19,8 +19,24 @@ RSpec.describe Player do
 
     expect(player.name).to eq("Clarisa")
     expect(player.deck).to eq(deck)
-    expect(player.has_lost).to eq(false) 
+    expect(player.has_lost?).to eq(false) 
   end 
 
-  
+  it 'goes through deck to check win or loss' do 
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+
+    expect(player.deck).to eq(deck)
+    expect(player.has_lost?).to eq(false) 
+    player.deck.remove_card
+    expect(player.has_lost?).to eq(false) 
+    player.deck.remove_card
+    expect(player.has_lost?).to eq(false) 
+    player.deck.remove_card
+    expect(player.has_lost?).to eq(true) 
+    expect(player.deck).to eq(deck)
+  end
 end
