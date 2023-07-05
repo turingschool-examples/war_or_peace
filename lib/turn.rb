ae2d4e2fddc0ed34b1938d7ccb4b8a69823e1e1d
Blake Sergesketter <blakesergesketter@gmail.com>
2023-07-05 15:00:39 -0500

    create turn class with player  and spoils_of_war attributes; winnter, pile_cards, and award_spoils methods; and write test for initialization.

---
+++ lib/turn.rb
@@ -0,0 +1,23 @@
+class Turn 
+
+  attr_reader :player1,
+              :player2,
+              :spoils_of_war
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @spoils_of_war = []
+  end
+
+  def winner
+
+  end
+
+  def pile_cards
+
+  end
+
+  def award_spoils
+    
+  end
+end
