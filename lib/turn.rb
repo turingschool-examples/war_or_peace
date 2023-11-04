fa68d6bfbbc0af6e7cc075b958388dc650f4e2da
Jesse Thomas <jgthomas3@gmail.com>
2023-11-03 21:29:21 -0600

    Add war turn tests and functionality

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,28 +12,56 @@ class Turn
   def type
     if player1.deck.cards[0].rank != player2.deck.cards[0].rank
       :basic
+    elsif player1.deck.cards[0].rank == player2.deck.cards[0].rank
+      :war
+    else
+      :mutually_assured_destruction
     end
   end
 
   def winner
     if type == :basic
-      if player1.deck.cards[0].rank > player2.deck.cards[0].rank
-        winner = player1
-      else
-        winner = player2
-      end
+      basic_play
+    elsif type == :war
+      war_play
+    # else
+    #   mutually_assured_destruction_play
     end
   end
 
   def pile_cards
     if type == :basic
-      spoils_of_war << player1.deck.cards.shift
-      spoils_of_war << player2.deck.cards.shift
+      basic_pile
+    elsif type == :war
+      war_pile
     end
   end
 
   def award_spoils(winner)
     winner.deck.cards << spoils_of_war
     winner.deck.cards.flatten!
+  end
+
+  def basic_play
+    return player1 if player1.deck.cards[0].rank > player2.deck.cards[0].rank
+    player2
+  end
+
+  def war_play
+    return player1 if player1.deck.cards[2].rank > player2.deck.cards[2].rank
+    player2
+  end
+
+  def basic_pile
+    spoils_of_war << player1.deck.cards.shift
+    spoils_of_war << player2.deck.cards.shift
+  end
+
+  def war_pile
+    spoils_of_war.concat(player1.deck.cards[0..2])
+    player1.deck.cards.slice!(0..2)
+    spoils_of_war.concat(player2.deck.cards[0..2])
+    player2.deck.cards.slice!(0..2)
+    spoils_of_war.flatten!
   end
 end
