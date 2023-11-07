bef2dae08dfdee93325d3898b942735ebfad0e4e
Dana Howell <howelld115@gmail.com>
2023-11-07 14:13:08 -0700

    feat: Add #high_ranking_cards

--- lib/deck.rb
+++ lib/deck.rb
@@ -9,4 +9,13 @@ class Deck
     cards[index].rank
   end
 
+  def high_ranking_cards
+    high_rank_cards = []
+    cards.find_all do |card|
+      if card.rank >= 11
+        high_rank_cards << card
+      end
+     end
+    high_rank_cards 
+  end
 end
