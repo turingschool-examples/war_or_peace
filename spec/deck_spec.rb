6689da6f2d312c193a1c5e1590d30293834a2938
Dana Howell <howelld115@gmail.com>
2023-11-07 14:20:04 -0700

    test: Add test for high ranking cards

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -22,8 +22,9 @@ RSpec.describe Deck do
       card1 = Card.new(:diamond, 'Queen', 12)
       card2 = Card.new(:spade, '3', 3)   
       card3 = Card.new(:heart, 'Ace', 14)    
-      cards = [card1, card2]
+      cards = [card1, card2, card3]
       deck = Deck.new(cards)
+
       expect(deck.rank_of_card_at(0)).to eq(12)
       expect(deck.rank_of_card_at(1)).to eq(3)
     end
@@ -32,10 +33,23 @@ RSpec.describe Deck do
       card1 = Card.new(:diamond, 'Queen', 12)
       card2 = Card.new(:spade, '3', 3)   
       card3 = Card.new(:heart, 'Ace', 14)    
-      cards = [card1, card2]
+      cards = [card1, card2, card3]
       deck = Deck.new(cards)
 
-      expect(deck.high_ranking_cards).to eq(card1, card2)
+      deck.high_ranking_cards
+      expect(deck.high_ranking_cards).to eq([card1, card3])
+    end
+
+    it 'determines percentage of high ranking cards' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)   
+      card3 = Card.new(:heart, 'Ace', 14)    
+      cards = [card1, card2, card3]
+      deck = Deck.new(cards)
+
+      deck.percent_high_ranking_cards
+
+      expect(deck.percent_high_ranking_cards).to eq(66.7)
     end
   end
 end
