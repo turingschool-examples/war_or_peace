1f30b42247bf3cb4784123694b13e7a5745170c8
Dana Howell <howelld115@gmail.com>
2023-11-07 13:24:22 -0700

    test: Add test for high ranking cards

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe Deck do
       card2 = Card.new(:spade, '3', 3)   
       card3 = Card.new(:heart, 'Ace', 14)    
       cards = [card1, card2, card3]
-
       deck = Deck.new(cards)
 
       expect(deck).to be_a(Deck)
@@ -18,15 +17,25 @@ RSpec.describe Deck do
     end
   end
   
-  describe 'rank_of_card_at' do
-    it 'ranks the card' do
+  describe 'rank of card' do
+    it '#rank_of_card_at' do
       card1 = Card.new(:diamond, 'Queen', 12)
       card2 = Card.new(:spade, '3', 3)   
+      card3 = Card.new(:heart, 'Ace', 14)    
       cards = [card1, card2]
       deck = Deck.new(cards)
-    
       expect(deck.rank_of_card_at(0)).to eq(12)
       expect(deck.rank_of_card_at(1)).to eq(3)
+    end
+
+    it 'determines high ranking cards' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)   
+      card3 = Card.new(:heart, 'Ace', 14)    
+      cards = [card1, card2]
+      deck = Deck.new(cards)
+
+      expect(deck.high_ranking_cards).to eq(card1, card2)
     end
   end
 end
