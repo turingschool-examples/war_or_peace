2e3f3ab166e4c1e0b68801966778ef5b6d3a1d62
Mel Langhoff <mlanghoff@uwalumni.com>
2023-11-09 13:57:53 -0700

    Fixed # rank_of_card_at

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,23 +6,15 @@ class Deck
     end
 
     def rank_of_card_at(index)
-        # @cards arr.find_index(index)
-        # @cards = arr.fetch(index)
-        # arrayIndex = array.find_index { |index| item[rank] == index}
-        # rank_of_card = @cards[index]
-        @cards.each do |rank|
-            
-        end
+        @cards[index].rank
     end
     
-    def high_ranking_cards
-    end
+    # def high_ranking_cards
+    # end
 
-    def remove_card
-    end
+    # def remove_card
+    # end
 
-    def add_card
-    end
-
-    
+    # def add_card
+    # end
 end
