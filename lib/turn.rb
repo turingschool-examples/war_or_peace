f98acf2083d9146c8694306ef3fbacf6ffe561f4
Dana Howell <howelld115@gmail.com>
2023-11-09 16:48:24 -0700

    Completes #winner, #pile_cards and writes spec for #award_spoils

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,7 +14,7 @@ class Turn
       :basic
     elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
       :war 
-    else #don't NEED an else statement.  But it seems like I do need the conditional here. 
+    else #don't NEED an else statement.  But it seems like I do need the conditional here: 
 
     # else player1.rank_of_card_at(0) == player2.rank_of_card_at(0) && player1.rank_of_card_at(0) == player2.rank_of_card_at(2)
       :mutually_assured_destruction
@@ -24,8 +24,19 @@ class Turn
   def winner
     if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0) == true
       player1
+      # later:     if player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0) || if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) == true
     else
       player2
+    end
+  end
+
+  def pile_cards
+    if self.type == :basic
+      @spoils_of_war << player1.deck.cards[0]
+      @spoils_of_war << player2.deck.cards[0]
+
+      #remove_card from player1 and player2
+      #that card will go into the @spoils_of_war array
     end
   end
 
