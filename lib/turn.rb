4024727066a7d29275a386bbb592ee38c83b8b90
Mel Langhoff <mlanghoff@uwalumni.com>
2023-11-10 11:18:42 -0700

    Add winner method

--- lib/turn.rb
+++ lib/turn.rb
@@ -26,4 +26,27 @@ class Turn
         @player1.deck.cards.first.rank == @player2.deck.cards.first.rank
     end
 
+    def winner
+        if type == :basic
+            basic_turn_winner
+        else type == :war
+            war_turn_winner            
+        end
+    end
+
+    def basic_turn_winner
+        if @player1.deck.cards.first.rank > @player2.deck.cards.first.rank
+            @player1
+        else
+            @player2
+        end
+    end
+
+    def war_turn_winner
+        if @player1.deck.cards.second.rank > @player2.deck.cards.second.rank
+            @player1
+        else
+            @player2
+        end
+    end
 end
