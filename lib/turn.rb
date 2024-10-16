d9e62d011cc31df70f1feaa64358fd6eec5689fc
Coral Ruschak <coralruschak@gmail.com>
2024-10-15 20:00:54 -0400

    Really sad final turn code

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,7 +6,7 @@ def initialize(player1, player2)
   @player2 = player2
   @spoils_of_war = spoils_of_war = []
 end
-initialize: A Turn is created with two players - player1 and player2
+initialize: A Turn is created with two players - @player1 and player2
 
 def type
 
@@ -16,35 +16,47 @@ A :war turn occurs when both players’ rank_of_card_at(0) are the same.
 :mutually_assured_destruction occurs when both players’ rank_of_card_at(0) AND rank_of_card_at(2) are the same.
 
 def winner
-  if type :basic 
-    winner = player1.deck.rank_of_card_at(0)
-  elseif type :war
-    winner = player1.deck.rank_of_card_at(2)
-  elseif type :mutually_assured_destruction
+  if @type == :basic 
+    winner = @player1.deck.rank_of_card_at(0)
+  elseif @type == :war
+    winner = @player1.deck.rank_of_card_at(2)
+  elseif @type == :mutually_assured_destruction
     No Winner
 winner: this method will determine the winner of the turn.
-if the turn has a type of :basic, it will return whichever player has a higher rank_of_card_at(0)
-if the turn has a type of :war the winner will be whichever player has a higher rank_of_card_at(2)
-if the turn has a type of :mutually_assured_destruction the method will return No Winner.
+if the turn has a @type == of :basic, it will return whichever player has a higher rank_of_card_at(0)
+if the turn has a @type == of :war the winner will be whichever player has a higher rank_of_card_at(2)
+if the turn has a @type == of :mutually_assured_destruction the method will return No Winner.
 end
 
 def pile_cards
-  if type :basic
-    player1.deck.remove_card(0) >> spoils_of_war
-    player2.deck.remove_card(0) >> spoils_of_war
-  elseif type :war
-    player1.deck.remove_card(0, 1, 2) >> spoils_of_war
-    player2.deck.remove_card(0, 1, 2) >> spoils_of_war
-  elseif type :mutually_assured_destruction
-    player1.deck.remove_card(0, 1, 2)
-    player2.deck.remove_card(0, 1, 2)
+  if @type == :basic
+    @player1.deck.remove_card >> @spoils_of_war
+    @player2.deck.remove_card >> @spoils_of_war
+
+  elseif @type == :war
+    3.times do
+    @player1.deck.remove_card >> @spoils_of_war
+    end
+    3.times do
+    @player2.deck.remove_card >> @spoils_of_war
+    end
+  
+  elseif @type == :mutually_assured_destruction
+    3.times do
+    @player1.deck.remove_card
+    end
+    3.times do
+    @player2.deck.remove_card
+    end
   end
 end
 
 
   
 
-  def award_spoils
-    spoils_of_war >> winner.deck ?
+  def award_spoils(winner)
+    winner.deck.cards.concat(@spoils_of_war)
+    @spoils_of_war.clear
   end
+end
  
